116ca866179b4861f0c35012e936281ed3700e11
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-24 04:17:30 +0000

    Add the missing toolchain master.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@63668 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.client.nacl.toolchain/run_master.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+title NativeClient Master
+set PYTHONPATH=..\scripts;..\scripts\master;..\scripts\common;..\scripts\private;..\pylibs
+set PATH=%~dp0..\depot_tools;%~dp0..\depot_tools\release\python_24;%~dp0..\depot_tools\python;%PATH%
+python ..\scripts\common\twistd --no_save -y buildbot.tac
