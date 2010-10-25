bba6feb9fd46843d34d64830b40e8c55b558c4f1
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-25 18:35:15 +0000

    Initial new v8 setup, this does not include windows setup. I will add this setup in a seperate change - this is just to sanety check all the linux builders.
    Review URL: http://codereview.chromium.org/4079003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@63755 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/v8_commands.py
+++ scripts/master/factory/v8_commands.py
@@ -17,14 +17,14 @@ class V8Commands(commands.FactoryCommands):
   """Encapsulates methods to add v8 commands to a buildbot factory."""
 
   def __init__(self, factory=None, identifier=None, target=None,
-               build_dir=None, target_platform=None):
+               build_dir=None, target_platform=None, target_arch=None):
 
     commands.FactoryCommands.__init__(self, factory, identifier,
                                       target, build_dir, target_platform)
 
-    # override _script_dir - one below because we run our build inside
-    # the bleeding_edge directory
-    self._script_dir = self.PathJoin('..', self._script_dir)
+    # Override _script_dir - one below because we run our build inside
+    # the bleeding_edge directory.
+    self._script_dir = self.PathJoin('..', self._script_dir);
 
     # Where to point waterfall links for builds and test results.
     self._archive_url = config.Master.archive_url
@@ -36,6 +36,9 @@ class V8Commands(commands.FactoryCommands):
     self._es5conform_dir = self.PathJoin('bleeding_edge/test/es5conform/data')
     self._es5conform_url = config.Master.es5conform_root_url
     self._es5conform_revision = config.Master.es5conform_revision
+    self._arch = None;
+    if target_arch:
+      self._arch = target_arch;
 
     if self._target_platform == 'win32':
       # Override to use the right python
@@ -44,53 +47,76 @@ class V8Commands(commands.FactoryCommands):
 
     # Create smaller name for the functions and vars to siplify the code below.
     J = self.PathJoin
-    s_dir = self._v8_script_dir
 
+    s_dir = self._v8_script_dir
+    p_dir = self._private_script_dir
+    self._archive_tool = J(s_dir, 'archive_v8.py')
     self._v8_test_tool = J(self._build_dir, 'tools')
 
     # Scripts in the v8 scripts dir.
     self._v8testing_tool = J(s_dir, 'v8testing.py')
-    self._v8_es5conform_tool = J(s_dir, 'v8_es5conform.py')
 
-  def AddV8Testing(self, properties=None):
+
+  def AddV8Testing(self, properties=None, simulator=None, testname=None):
+    if (self._target_platform == 'win32'):
+      self.AddTaskkillStep();
     cmd = [self._python, self._v8testing_tool,
            '--target', self._target]
-    if (self._target_platform == 'win32'):
-      self.AddTaskkillStep()
+    if (testname):
+      cmd += ['--testname', testname]
+    if (simulator):
+      cmd += ['--simulator', simulator]
+    if (self._arch):
+      cmd += ['--arch', self._arch]
     self.AddTestStep(shell.ShellCommand,
                      'Check', cmd,
                      workdir='build/bleeding_edge/')
 
-  def AddV8ES5Conform(self, properties=None):
+  def AddV8ES5Conform(self, properties=None, simulator=None):
     cmd = [self._python, self._v8testing_tool,
            '--target', self._target,
            '--testname', 'es5conform']
     if (self._target_platform == 'win32'):
-      self.AddTaskkillStep()
+      self.AddTaskkillStep();
+    if (self._arch):
+      cmd += ['--arch', self._arch]
+    if (simulator):
+      cmd += ['--simulator', simulator]
     self.AddTestStep(shell.ShellCommand,
                      'ES5-Conform',
                      cmd,
                      workdir='build/bleeding_edge/')
 
-  def AddV8Mozilla(self, properties=None):
+  def AddV8Mozilla(self, properties=None, simulator=None):
     cmd = [self._python, self._v8testing_tool,
            '--target', self._target,
-           '--build-dir', self._build_dir,
            '--testname', 'mozilla']
-
+    if (self._target_platform == 'win32'):
+      self.AddTaskkillStep();
+    if (self._arch):
+      cmd += ['--arch', self._arch]
+    if (simulator):
+      cmd += ['--simulator', simulator]
     self.AddTestStep(shell.ShellCommand, 'Mozilla', cmd,
                      workdir='build/bleeding_edge/')
 
-  def AddV8Sputnik(self, properties=None):
+  def AddV8Sputnik(self, properties=None, simulator=None):
     cmd = [self._python, self._v8testing_tool,
            '--target', self._target,
            '--testname', 'sputnik']
     if (self._target_platform == 'win32'):
-      self.AddTaskkillStep()
+      self.AddTaskkillStep();
+    if (self._arch):
+      cmd += ['--arch', self._arch]
+    if (simulator):
+      cmd += ['--simulator', simulator]
     self.AddTestStep(shell.ShellCommand, 'Sputnik', cmd,
                      workdir='build/bleeding_edge/')
 
-  def AddArmSimTest(self, properties=None):
+  def AddrAmSimTest(self, properties=None):
+    self.AddV8Sputnik(simulator='arm')
+    self.AddV8ES5Conform(simulator='arm')
+    self.AddV8Mozilla(simulator='arm')
     cmd = [self._python, self._v8testing_tool,
            '--target', self._target,
            '--build-dir', self._build_dir,
@@ -103,3 +129,46 @@ class V8Commands(commands.FactoryCommands):
            '--testname', 'presubmit']
     self.AddTestStep(shell.ShellCommand, 'Presubmit', cmd,
                      workdir='build/bleeding_edge/')
+
+  def AddFuzzer(self, properties=None):
+    cmd = ['./fuzz-v8.sh']
+    self.AddTestStep(shell.ShellCommand, 'Fuzz', cmd,
+                     workdir='build/bleeding_edge/')
+
+  def AddLeakTests(self, properties=None):
+    cmd = [self._python, self._v8testing_tool,
+           '--testname', 'leak']
+    env = {
+      'PATH': (
+        self.build_dir + '../src/third_party/valgrind/linux_x86/bin;'
+      ),
+      'VALGRIND_LIB': (
+        self.build_dir + '../src/third_party/valgrind/linux_x86/lib/valgrind'
+      ),
+    }
+    self.AddTestStep(shell.ShellCommand, 'leak', cmd,
+                     env=env,
+                     workdir='build/bleeding_edge/')
+
+  def AddArchiveBuild(self, mode='dev', show_url=True, extra_archive_paths=None):
+    """Adds a step to the factory to archive a build."""
+    if show_url:
+      url = '%s/%s/%s' %  (self._archive_url, 'snapshots', self._identifier)
+      text = 'download'
+    else:
+      url = None
+      text = None
+
+    cmd = [self._python, self._archive_tool,
+           '--target', self._target]
+    self.AddTestStep(shell.ShellCommand, 'Archiving', cmd,
+                 workdir='build/bleeding_edge')
+
+  def AddMoveExtracted(self):
+    """Adds a step to download and extract a previously archived build."""
+    cmd = ('cp -R sconsbuild/release/* bleeding_edge/.')
+    self._factory.addStep(shell.ShellCommand,
+                          description='Move extracted to bleeding',
+                          timeout=600,
+                          workdir='build',
+                          command=cmd)
--- scripts/master/factory/v8_factory.py
+++ scripts/master/factory/v8_factory.py
@@ -23,16 +23,22 @@ class V8Factory(gclient_factory.GClientFactory):
                         '/deps/third_party/python_24')
 
   CUSTOM_DEPS_ES5CONFORM = ('bleeding_edge/test/es5conform/data',
-                            'https://es5conform.svn.codeplex.com/svn@62998')
+                            'https://es5conform.svn.codeplex.com/svn@71525')
 
   CUSTOM_DEPS_SPUTNIK = ('bleeding_edge/test/sputnik/sputniktests',
                          'http://sputniktests.googlecode.com/svn/trunk@28')
 
   CUSTOM_DEPS_SCONS = ('third_party/scons',
-                       'svn://chrome-svn.corp.google.com/chrome/trunk/src/third_party/scons')
+                       config.Master.trunk_url +
+                       '/src/third_party/scons')
 
   CUSTOM_DEPS_VALGRIND = ('src/third_party/valgrind',
-     config.Master.trunk_url + '/deps/third_party/valgrind/binaries')
+                          config.Master.trunk_url +
+                          '/deps/third_party/valgrind/binaries')
+
+  CUSTOM_DEPS_WIN7SDK = ('third_party/win7sdk',
+                         config.Master.trunk_internal_url +
+                         '/third_party/platformsdk_win7/files')
 
   CUSTOM_DEPS_MOZILLA = ('bleeding_edge/test/mozilla/data',
                           config.Master.trunk_url +
@@ -46,8 +52,8 @@ class V8Factory(gclient_factory.GClientFactory):
     gclient_factory.GClientFactory.__init__(self, build_dir, custom_deps_list,
                                             target_platform=target_platform)
 
-  @staticmethod
-  def _AddTests(factory_cmd_obj, tests, mode=None,
+
+  def _AddTests(self, factory_cmd_obj, tests, mode=None,
                 factory_properties=None):
     """Add the tests listed in 'tests' to the factory_cmd_obj."""
     factory_properties = factory_properties or {}
@@ -61,18 +67,24 @@ class V8Factory(gclient_factory.GClientFactory):
     if R('presubmit'): f.AddPresubmitTest()
     if R('v8testing'): f.AddV8Testing()
     if R('v8_es5conform'): f.AddV8ES5Conform()
+    if R('fuzz'): f.AddFuzzer()
 
     # Mozilla tests should be moved to v8 repo
     if R('mozilla'):
       f.AddV8Mozilla()
 
     if R('sputnik'): f.AddV8Sputnik()
-    if R('arm'): f.AddArmSimTest()
+    if R('arm'):
+      f.AddV8Testing(simulator='arm')
+      f.AddV8ES5Conform(simulator='arm')
+      f.AddV8Mozilla(simulator='arm')
+      f.AddV8Sputnik(simulator='arm')
+
 
   def V8Factory(self, identifier, target='release', clobber=False,
                       tests=None, mode=None, slave_type='BuilderTester',
                       options=None, compile_timeout=1200, build_url=None,
-                      project=None, factory_properties=None):
+                      project=None, factory_properties=None, target_arch=None):
     tests = tests or []
     factory_properties = factory_properties or {}
 
@@ -82,6 +94,7 @@ class V8Factory(gclient_factory.GClientFactory):
     # If we are on win32 add extra python executable
     if (self._target_platform == 'win32'):
       self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_PYTHON)
+      self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_WIN7SDK)
 
     if (gclient_factory.ShouldRunTest(tests, 'v8_es5conform')):
       self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_ES5CONFORM)
@@ -95,6 +108,11 @@ class V8Factory(gclient_factory.GClientFactory):
     if (gclient_factory.ShouldRunTest(tests, 'mozilla')):
       self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_MOZILLA)
 
+    if (gclient_factory.ShouldRunTest(tests, 'arm')):
+      self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_MOZILLA)
+      self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_SPUTNIK)
+      self._solutions[0].custom_deps_list.append(self.CUSTOM_DEPS_ES5CONFORM)
+
     factory = self.BuildFactory(identifier, target, clobber, tests, mode,
                                 slave_type, options, compile_timeout, build_url,
                                 project, factory_properties)
@@ -104,7 +122,15 @@ class V8Factory(gclient_factory.GClientFactory):
     v8_cmd_obj = v8_commands.V8Commands(factory, identifier,
                                         target,
                                         '',
-                                        self._target_platform)
+                                        self._target_platform,
+                                        target_arch)
+    if factory_properties.get('archive_build'):
+      v8_cmd_obj.AddArchiveBuild(
+          extra_archive_paths=factory_properties.get('extra_archive_paths'))
+
+    if (slave_type == 'Tester'):
+      v8_cmd_obj.AddMoveExtracted()
+
     # Add all the tests.
     self._AddTests(v8_cmd_obj, tests, mode, factory_properties)
     return factory
