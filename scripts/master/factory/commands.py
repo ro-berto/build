ro-berto/build

commit ae9b9bcc9a7701977239c3d93bb2c475b8dedbe7

Change PathJoin() to call normpath so the paths are more readable.
TEST=nothing breaks
BUG=none

Review URL: http://codereview.chromium.org/4131002

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@63931 0039d316-1c4b-4281-b951-d872f2087c98

scripts/master/factory/commands.py
@@ -8,6 +8,8 @@ All the utility functions to add steps to a build factory here are not
 project-specific. See the other *_commands.py for project-specific commands.
 """
 
+import ntpath
+import posixpath
 import re
 
 from buildbot.locks import SlaveLock
@@ -190,9 +192,9 @@ class FactoryCommands(object):
 
   def PathJoin(self, *args):
     if self._target_platform == 'win32':
-      return '\\'.join(args)
+      return ntpath.normpath(ntpath.join(args))
     else:
-      return '/'.join(args)
+      return posixpath.normpath(posixpath.join(args))
 
 
   # Basic commands

commit 106d97050dd67332dfe1d1df108d6669226d1551

Fix a bug introduced in r63931 in PathJoin().
TBR=nsylvain
BUG=
TEST=

Review URL: http://codereview.chromium.org/4196002

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@63933 0039d316-1c4b-4281-b951-d872f2087c98

scripts/master/factory/commands.py
@@ -192,9 +192,9 @@ class FactoryCommands(object):
 
   def PathJoin(self, *args):
     if self._target_platform == 'win32':
-      return ntpath.normpath(ntpath.join(args))
+      return ntpath.normpath(ntpath.join(*args))
     else:
-      return posixpath.normpath(posixpath.join(args))
+      return posixpath.normpath(posixpath.join(*args))
 
 
   # Basic commands