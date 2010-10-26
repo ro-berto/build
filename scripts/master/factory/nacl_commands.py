5861cb6ab32171d191ec30788483e9097673e7de
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-26 01:44:59 +0000

    Do not use hardcoded path when referencing script on the slave!
    Use relative path instead.
    
    TEST=more nacl steps should pass
    BUG=none
    
    Review URL: http://codereview.chromium.org/4104003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@63842 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/nacl_commands.py
+++ scripts/master/factory/nacl_commands.py
@@ -9,7 +9,7 @@ Contains the Native Client specific commands. Based on commands.py"""
 
 import os
 import re
-    
+
 from buildbot.process import buildstep
 from buildbot.process.properties import WithProperties
 from buildbot.steps import shell
@@ -50,6 +50,8 @@ class NativeClientCommands(commands.FactoryCommands):
     self._private_script_dir = self.PathJoin(self._script_dir, '..', 'private')
     self._gsutil_dir = self.PathJoin(self._script_dir, '..', '..',
                                      'third_party', 'gsutil')
+    self._depot_tools = self.PathJoin(self._script_dir, '..', '..', '..',
+        'depot_tools')
 
     self._build_dir = self.PathJoin('build', build_dir)
     self._toolchain_build_dir = self.PathJoin('build', build_dir, 'tools')
@@ -59,10 +61,9 @@ class NativeClientCommands(commands.FactoryCommands):
         r'c:\cygwin\bin;'
         r'c:\cygwin\usr\bin;'
         r'c:\WINDOWS\system32;'
-        r'c:\WINDOWS;'
-        r'c:\b\depot_tools;'
-        r'c:\b\depot_tools\python_bin;'
-      ),
+        r'c:\WINDOWS;') +
+        self._depot_tools + ';' +
+        self.PathJoin(self._depot_tools, 'python_bin;'),
     }
     self._runhooks_env = None
     self._build_compile_name = 'compile'
@@ -182,10 +183,10 @@ class NativeClientCommands(commands.FactoryCommands):
       self._build_env = {
         'PATH': (
           r'c:\WINDOWS\system32;'
-          r'c:\WINDOWS;'
-          r'c:\b\depot_tools;'
-          r'c:\b\depot_tools\python_bin;'
-          r'c:\Program Files\Microsoft Visual Studio 9.0\VC;'
+          r'c:\WINDOWS;') +
+          self._depot_tools + ';' +
+          self.PathJoin(self._depot_Tools, 'python_bin;') +
+          (r'c:\Program Files\Microsoft Visual Studio 9.0\VC;'
           r'c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC;'
           r'c:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools;'
           r'c:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools;'
@@ -650,10 +651,10 @@ class NativeClientCommands(commands.FactoryCommands):
 
     env = self._build_env.copy()
     if self._target_platform.startswith('win'):
-      env['GSUTIL'] = r'c:\b\scripts\slave\gsutil.bat'
+      env['GSUTIL'] = self.PathJoin(self._script_dir, r'gsutil.bat')
       env['HOME'] = r'c:\Users\chrome-bot'
 
-    cmd = [self._python, '/b/scripts/slave/gsutil_cp_dir.py',
+    cmd = [self._python, self.PathJoin(self._script_dir, 'gsutil_cp_dir.py'),
            '--message', url, src, dst]
     cmd = WithProperties(' '.join(cmd))
     self._factory.addStep(AnnotatedCommand,
@@ -677,9 +678,13 @@ class NativeClientCommands(commands.FactoryCommands):
     full_dst = WithProperties(
         'gs://nativeclient-archive/%s/%s' % (dst_base, dst))
 
-    cmd = [self._python, '/b/scripts/command_wrapper/bin/command_wrapper.py',
+    cmd = [self._python, self.PathJoin(self._script_dir, '..',
+                                       'command_wrapper', 'bin',
+                                       'command_wrapper.py'),
            '--', self._python, self._gsutil_tool, 'cp', src, full_dst]
-    cmd2 = [self._python, '/b/scripts/command_wrapper/bin/command_wrapper.py',
+    cmd2 = [self._python, self.PathJoin(self_script_dir, '..',
+                                        'command_wrapper', 'bin',
+                                        'command_wrapper.py'),
             '--', self._python, self._gsutil_tool,
             'setacl', 'public-read', full_dst]
 
@@ -690,7 +695,7 @@ class NativeClientCommands(commands.FactoryCommands):
                      command2=None):
     if self._target_platform.startswith('win'):
       env = self._cygwin_env.copy()
-      env['PATH'] = r'c:\b\depot_tools;' + env['PATH']
+      env['PATH'] = self._depot_tools + ';' + env['PATH']
       env['HOME'] = r'c:\Users\chrome-bot'
     else:
       env = self._build_env
