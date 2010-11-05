ab2ff88142b0388a04d1611bdcb3ee50fee221d2
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-05 00:42:57 +0000

    Add buildbot 0.8.2 in third_party.
    Tests were removed but no patch has been applied.
    
    I plan to switch the slaves first and then gradularily switch the masters,
    starting with FYI.
    
    Doing this since I want to add a functionality in StatusJson that requires the
    0.8.x branch.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/4451002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@65154 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/buildbot_8_2/buildbot/status/web/hooks/base.py
@@ -0,0 +1,77 @@
+# code inspired/copied from contrib/github_buildbot
+#  and inspired from code from the Chromium project
+# otherwise, Andrew Melo <andrew.melo@gmail.com> wrote the rest
+
+# but "the rest" is pretty minimal
+from twisted.web import resource
+from buildbot.status.builder import FAILURE
+import re
+from buildbot import util, interfaces
+import logging
+import traceback
+import sys
+from buildbot.process.properties import Properties
+from buildbot.changes.changes import Change
+from twisted.python.reflect import namedModule
+from buildbot.util import json
+from twisted.python.log import msg,err
+    
+def getChanges(request, options=None):
+        """
+        Consumes a naive build notification (the default for now)
+        basically, set POST variables to match commit object parameters:
+        revision, revlink, comments, branch, who, files, links
+        
+        files, links and properties will be de-json'd, the rest are interpreted as strings
+        """
+        
+        def firstOrNothing( value ):
+            """
+            Small helper function to return the first value (if value is a list)
+            or return the whole thing otherwise
+            """
+            if ( type(value) == type([])):
+                return value[0]
+            else:
+                return value
+
+        args = request.args
+
+        # first, convert files, links and properties
+        files = None
+        if args.get('files'):
+            files = json.loads( args.get('files')[0] )
+        else:
+            files = []
+                
+        links = None
+        if args.get('links'):
+            links = json.loads( args.get('links')[0] )
+        else:
+            links = []
+
+        properties = None
+        if args.get('properties'):
+            properties = json.loads( args.get('properties')[0] )
+        else:
+            properties = {}
+            
+        revision = firstOrNothing(args.get('revision'))
+        when     = firstOrNothing(args.get('when'))
+        who = firstOrNothing(args.get('who'))
+        comments = firstOrNothing(args.get('comments'))
+        isdir = firstOrNothing(args.get('isdir',0))
+        branch = firstOrNothing(args.get('branch'))
+        category = firstOrNothing(args.get('category'))
+        revlink = firstOrNothing(args.get('revlink'))
+        repository = firstOrNothing(args.get('repository'))
+        project = firstOrNothing(args.get('project'))
+              
+        ourchange = Change(who = who, files = files, comments = comments, isdir = isdir, links = links,
+                        revision=revision, when = when, branch = branch, category = category,
+                        revlink = revlink, properties = properties, repository = repository,
+                        project = project)
+        return [ourchange]
+
+
+
