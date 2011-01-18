55339e440b69b904776f695253e084e8c5127da1
chase@chromium.org <chase@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-18 06:54:33 +0000

    Revert "Disable browser_test sharding."
    This reverts commit r71591.  Previously failing
    browser_tests should now pass.
    
    BUG=69423
    TEST=browser_tests run and pass on (dbg)(2-4) Windows
    test systems
    TBR=chase@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@71630 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/runtest.py
+++ scripts/slave/runtest.py
@@ -447,14 +447,8 @@ def main():
 
   # Set the number of shards environement variables.
   if options.total_shards and options.shard_index:
-    if options.test_type == 'browser_tests':
-      if options.shard_index != 3:
-        print 'browser_test on this host is disabled'
-        return
-      print 'browser_test sharding disabled, running all tests'
-    else:
-      os.environ['GTEST_TOTAL_SHARDS'] = str(options.total_shards)
-      os.environ['GTEST_SHARD_INDEX'] = str(options.shard_index - 1)
+    os.environ['GTEST_TOTAL_SHARDS'] = str(options.total_shards)
+    os.environ['GTEST_SHARD_INDEX'] = str(options.shard_index - 1)
 
   if options.results_directory:
     options.test_output_xml = os.path.normpath(os.path.join(
