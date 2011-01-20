e3c54a76c6086d073db494f4728aa8f0eb86c972
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 22:42:16 +0000

    Replace spaces with underscores for layout test archive directory.
    TBR=nsylvain
    
    Review URL: http://codereview.chromium.org/6240008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@72024 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/archive_layout_test_results.py
+++ scripts/slave/chromium/archive_layout_test_results.py
@@ -107,7 +107,8 @@ def archive_layout(options, args):
 
   # Extract the build name of this slave (e.g., 'chrome-release') from its
   # configuration file if not provided as a param.
-  build_name = options.builder_name or slave_utils.SlaveBuildName(chrome_dir)
+  build_name = options.builder_name.replace or slave_utils.SlaveBuildName(chrome_dir)
+  build_name = build_name..replace(' ', '_')
 
   last_change = str(slave_utils.SubversionRevision(chrome_dir))
   print 'last change: %s' % last_change
