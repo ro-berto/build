e23ff97fb8a53f3b103d0a72c6771c0308e227e6
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 14:42:36 +0000

    Make the master_config work when there is no gatekeeper information for a bot.
    Review URL: http://codereview.chromium.org/6240009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@72138 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/master_config.py
+++ scripts/master/master_config.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -71,9 +71,10 @@ class Helper(object):
 
       # Construct the category.
       categories = []
-      if builder['category']:
+      if builder.get('category', None):
         categories.append(builder['category'])
-      categories.extend(builder['gatekeeper'].split('|'))
+      if builder.get('gatekeeper', None):
+        categories.extend(builder['gatekeeper'].split('|'))
       category = '|'.join(categories)
 
       # Append the builder to the list.
