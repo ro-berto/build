a27458ab7b873a306498f73fab42d73333e49c4e
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 12:21:33 +0000

    Shard more the XP and Vista testers.
    Review URL: http://codereview.chromium.org/6376016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@72952 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium/master_win_cfg.py
+++ masters/master.chromium/master_win_cfg.py
@@ -47,18 +47,31 @@ F('rel', win().ChromiumFactory(
 #
 # Win Rel testers
 #
-B('XP Tests', 'rel_unit', 'testers|windows', 'win_rel_dep', auto_reboot=True)
-F('rel_unit', win().ChromiumFactory(
+B('XP Tests (1)', 'rel_unit_1', 'testers|windows', 'win_rel_dep',
+  auto_reboot=True)
+F('rel_unit_1', win().ChromiumFactory(
     slave_type='Tester',
     build_url=rel_archive,
-    tests=['unit', 'ui', 'media', 'printing', 'remoting', 'gpu',
-           'browser_tests', 'courgette', 'googleurl', 'installer',
-           'safe_browsing', 'base', 'net', 'sandbox'],
+    tests=['unit', 'media', 'printing', 'remoting', 'gpu', 'browser_tests',
+           'courgette', 'googleurl', 'safe_browsing', 'sandbox'],
     factory_properties={'process_dumps': True,
                         'start_crash_handler': True,
                         'generate_gtest_json': True}))
 
-B('Vista Tests', 'rel_unit', 'testers|windows', 'win_rel_dep', auto_reboot=True)
+B('XP Tests (2)', 'rel_unit_2', 'testers|windows', 'win_rel_dep',
+  auto_reboot=True)
+F('rel_unit_2', win().ChromiumFactory(
+    slave_type='Tester',
+    build_url=rel_archive,
+    tests=['ui', 'installer', 'base', 'net'],
+    factory_properties={'process_dumps': True,
+                        'start_crash_handler': True,
+                        'generate_gtest_json': True}))
+
+B('Vista Tests (1)', 'rel_unit_1', 'testers|windows', 'win_rel_dep',
+  auto_reboot=True)
+B('Vista Tests (2)', 'rel_unit_2', 'testers|windows', 'win_rel_dep',
+  auto_reboot=True)
 
 B('Win7 Sync', 'rel_sync', 'testers|windows', 'win_rel_dep')
 F('rel_sync', win().ChromiumFactory(
@@ -149,8 +162,8 @@ F('dbg_unit_2', win().ChromiumFactory(
     slave_type='Tester',
     build_url=dbg_archive,
     tests=['ui', 'net', 'browser_tests'],
-    factory_properties={'ui_total_shards': 3, 'ui_shard_index': 1,
-                        'browser_total_shards': 3, 'browser_shard_index': 1,
+    factory_properties={'ui_total_shards': 4, 'ui_shard_index': 1,
+                        'browser_total_shards': 4, 'browser_shard_index': 1,
                         'process_dumps': True,
                         'start_crash_handler': True,
                         'generate_gtest_json': True}))
@@ -162,8 +175,8 @@ F('dbg_unit_3', win().ChromiumFactory(
       slave_type='Tester',
       build_url=dbg_archive,
       tests=['ui', 'sandbox', 'browser_tests'],
-      factory_properties={'ui_total_shards': 3, 'ui_shard_index': 2,
-                          'browser_total_shards': 3, 'browser_shard_index': 2,
+      factory_properties={'ui_total_shards': 4, 'ui_shard_index': 2,
+                          'browser_total_shards': 4, 'browser_shard_index': 2,
                           'process_dumps': True,
                           'start_crash_handler': True,
                           'generate_gtest_json': True}))
@@ -175,8 +188,21 @@ F('dbg_unit_4', win().ChromiumFactory(
     slave_type='Tester',
     build_url=dbg_archive,
     tests=['ui', 'browser_tests'],
-    factory_properties={'ui_total_shards': 3, 'ui_shard_index': 3,
-                        'browser_total_shards': 3, 'browser_shard_index': 3,
+    factory_properties={'ui_total_shards': 4, 'ui_shard_index': 3,
+                        'browser_total_shards': 4, 'browser_shard_index': 3,
+                        'process_dumps': True,
+                        'start_crash_handler': True,
+                        'generate_gtest_json': True}))
+
+B('XP Tests (dbg)(5)', 'dbg_unit_5', 'testers|windows', 'win_dbg_dep',
+  auto_reboot=True)
+F('dbg_unit_5', win().ChromiumFactory(
+    target='Debug',
+    slave_type='Tester',
+    build_url=dbg_archive,
+    tests=['ui', 'browser_tests'],
+    factory_properties={'ui_total_shards': 4, 'ui_shard_index': 4,
+                        'browser_total_shards': 4, 'browser_shard_index': 4,
                         'process_dumps': True,
                         'start_crash_handler': True,
                         'generate_gtest_json': True}))
@@ -188,6 +214,8 @@ B('Vista Tests (dbg)(2)', 'dbg_unit_2', 'testers|windows', 'win_dbg_dep',
 B('Vista Tests (dbg)(3)', 'dbg_unit_3', 'testers|windows', 'win_dbg_dep',
   auto_reboot=True)
 B('Vista Tests (dbg)(4)', 'dbg_unit_4', 'testers|windows', 'win_dbg_dep',
+  auto_reboot=True)
+B('Vista Tests (dbg)(5)', 'dbg_unit_5', 'testers|windows', 'win_dbg_dep',
   auto_reboot=True)
 
 #
