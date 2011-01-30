bb1968a38bfcd6c79d62a01e3c395d2c738656bb
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-30 15:55:48 +0000

    the LKGR config has been wrong for a long time. Update the list of bots and steps to match reality. Review URL: http://codereview.chromium.org/6312024
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@73108 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium/master_good_revisions_cfg.py
+++ masters/master.chromium/master_good_revisions_cfg.py
@@ -8,55 +8,48 @@ from master import goodrevisions
 # that all need to succeed to mark a revision as successful. A single failure
 # in any of the steps of any of the builders will mark the revision as failed.
 good_revision_steps = {
-  # Dependent on 'Chromium Builder (dbg)'
+  # Dependent on 'Win Builder (dbg)'
   'XP Tests (dbg)(1)': [
-    'check deps', 'courgette_unittests', 'googleurl_unittests',
-    'media_unittests', 'printing_unittests', 'remoting_unittests',
-    'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests',
-    'installer_util_unittests',
+    'check deps', 'base_unittests', 'courgette_unittests',
+    'googleurl_unittests', 'media_unittests', 'printing_unittests',
+    'remoting_unittests', 'ipc_tests', 'sync_unit_tests', 'unit_tests',
+    'app_unittests', 'installer_util_unittests',
   ],
   'XP Tests (dbg)(2)': [
-    'ui_tests',
+    'net_unittests', 'ui_tests', 'browser_tests',
   ],
   'XP Tests (dbg)(3)': [
-    'ui_tests',
+    'ui_tests', 'browser_tests',
   ],
   'XP Tests (dbg)(4)': [
     'ui_tests', 'browser_tests',
   ],
-  'Modules XP (dbg)': [
-    'base_unittests', 'net_unittests',
+  'XP Tests (dbg)(5)': [
+    'ui_tests', 'browser_tests',
   ],
   'Interactive Tests (dbg)': [
     'interactive_ui_tests',
   ],
-  # Dependent on 'Chromium Mac Builder (dbg)'
+  # Dependent on 'Mac Builder (dbg)'
   'Mac10.5 Tests (dbg)(1)': [
-    'check deps', 'googleurl_unittests', 'media_unittests',
-    'printing_unittests', 'remoting_unittests',
-    'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests',
-    'interactive_ui_tests',
+    'check deps', 'base_unittests', 'googleurl_unittests', 'media_unittests',
+    'printing_unittests', 'remoting_unittests', 'ipc_tests', 'sync_unit_tests',
+    'unit_tests', 'app_unittests', 'interactive_ui_tests',
   ],
   'Mac10.5 Tests (dbg)(2)': [
-    'ui_tests',
+    'net_unittests', 'ui_tests',
   ],
   'Mac10.5 Tests (dbg)(3)': [
     'browser_tests',
   ],
-  'Modules Mac10.5 (dbg)': [
-    'base_unittests', 'net_unittests',
-  ],
-  # Dependent on 'Chromium Linux Builder (dbg)'
+  # Dependent on 'Linux Builder (dbg)'
   'Linux Tests (dbg)(1)': [
-    'check deps', 'googleurl_unittests', 'media_unittests',
+    'check deps', 'base_unittests', 'googleurl_unittests', 'media_unittests',
     'printing_unittests', 'remoting_unittests', 'browser_tests', 'ui_tests',
   ],
   'Linux Tests (dbg)(2)': [
-    'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests', 'ui_tests',
-    'interactive_ui_tests',
-  ],
-  'Modules Linux (dbg)': [
-    'base_unittests', 'net_unittests',
+    'net_unittests', 'ipc_tests', 'sync_unit_tests', 'unit_tests',
+    'app_unittests', 'ui_tests', 'interactive_ui_tests',
   ],
   'Linux Builder (ChromiumOS)': [
     'compile',
@@ -64,12 +57,13 @@ good_revision_steps = {
     'net_unittests', 'printing_unittests', 'remoting_unittests',
     'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests',
   ],
-  'Linux Builder (ChromiumOS dbg)': [
-    'compile',
-    'base_unittests', 'googleurl_unittests', 'media_unittests',
-    'net_unittests', 'printing_unittests', 'remoting_unittests',
-    'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests',
-  ],
+  # Disabled - It takes an hour to cycle.
+  #'Linux Builder (ChromiumOS dbg)': [
+  #  'compile',
+  #  'base_unittests', 'googleurl_unittests', 'media_unittests',
+  #  'net_unittests', 'printing_unittests', 'remoting_unittests',
+  #  'ipc_tests', 'sync_unit_tests', 'unit_tests', 'app_unittests',
+  #],
 }
 
 def Update(config, active_master, c):
