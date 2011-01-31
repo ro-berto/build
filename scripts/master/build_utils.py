0381d3a9c3b1cf5f7bfa5a941bcbb0634fc1d0d7
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-31 01:06:32 +0000

    The caller of getAllRevisions expects to receive a list. In some cases we return a number, which breaks the caller.
    Review URL: http://codereview.chromium.org/6260032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@73123 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/build_utils.py
+++ scripts/master/build_utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -23,11 +23,11 @@ def getAllRevisions(build):
   if source_stamp and source_stamp.changes:
     return [change.revision for change in source_stamp.changes]
   try:
-    return build.getProperty('got_revision')
+    return [build.getProperty('got_revision')]
   except KeyError:
     pass
   try:
-    return build.getProperty('revision')
+    return [build.getProperty('revision')]
   except KeyError:
     pass
 
