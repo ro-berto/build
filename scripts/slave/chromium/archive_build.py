af3a7619ce8f58e47f52d04d6053070928ca973e
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 01:43:20 +0000

    Make sure we upload the LATEST file to google storage, not the master server, when gs_bucket is used.
    Review URL: http://codereview.chromium.org/6559002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@75694 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/archive_build.py
+++ scripts/slave/chromium/archive_build.py
@@ -709,7 +709,15 @@ class StagerBase(object):
       latest_file_path = os.path.join(self._www_dir_base, 'LATEST')
       if chromium_utils.IsWindows():
         print 'Saving revision to %s' % latest_file_path
-        self.SaveBuildRevisionToSpecifiedFile(latest_file_path)
+        if gs_base:
+          # The filename on Google Storage needs to match the filename locally.
+          # Create a LATEST file locally.
+          local_latest = os.path.join(os.path.dirname(self.last_change_file),
+                                      'LATEST')
+          self.SaveBuildRevisionToSpecifiedFile(local_latest)
+          GSUtilCopyFile(local_latest, gs_base, '..')
+        else:
+          self.SaveBuildRevisionToSpecifiedFile(latest_file_path)
       elif chromium_utils.IsLinux() or chromium_utils.IsMac():
         # Files are created umask 077 by default, so make it world-readable
         # before pushing to web server.
