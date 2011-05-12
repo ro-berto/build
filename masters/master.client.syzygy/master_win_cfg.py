98a6cc27c8e641a2bde1814b50c13aa3a8d12e75
jeffbailey@chromium.org <jeffbailey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 04:20:49 +0000

    Add master config for Syzygy Review URL: http://codereview.chromium.org/7001016
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@85099 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.client.syzygy/master_win_cfg.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from master import master_config
+from master.factory import syzygy_factory
+
+defaults = {}
+
+helper = master_config.Helper(defaults)
+B = helper.Builder
+D = helper.Dependent
+F = helper.Factory
+S = helper.Scheduler
+
+def win(): return syzygy_factory.SyzygyFactory('trunk', target_platform='win32')
+
+
+defaults['category'] = 'windows'
+
+
+#
+# Main Scheduler for Syzygy
+#
+S('syzygy_rel', branch='trunk', treeStableTimer=60)
+
+
+#
+# Linux Release Builder
+#
+B('Syzygy Windows', 'f_syzygy_win_rel', scheduler='syzygy_rel')
+F('f_syzygy_win_rel', win().SyzygyFactory())
+
+
+def Update(config, active_master, c):
+  return helper.Update(c)
