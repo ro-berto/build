ed2a5962e96d120e3717ff80cd23b8e9dfd29d5e
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 22:12:16 +0000

    Fix notifier and add a no-op script.
    package_source.py will upload the source checkout to
    Google Storage.  Until then, exit without error to keep
    the build from turning red as a result of this script
    being missing.
    
    
    Review URL: http://codereview.chromium.org/7074035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@87538 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/chromium/package_source.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A tool to package a checkout's source and upload it to Google Storage."""
+
+
+import sys
+
+
+if '__main__' == __name__:
+  sys.exit(0)
