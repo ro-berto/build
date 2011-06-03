9eded33c6183d3f8ff68dee70f22e8673fd2b845
ericdingle@chromium.org <ericdingle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 21:26:48 +0000

    Syzygy: Revert 82829.
    Review URL: http://codereview.chromium.org/7056057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@87867 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/syzygy_commands.py
+++ scripts/master/factory/syzygy_commands.py
@@ -9,7 +9,6 @@ This is based on commands.py and adds Syzygy-specific commands."""
 from buildbot.steps import shell
 
 from master.factory import commands
-from master.log_parser import process_log
 
 
 class SyzygyCommands(commands.FactoryCommands):
@@ -34,10 +33,7 @@ class SyzygyCommands(commands.FactoryCommands):
     self.AddTestStep(shell.ShellCommand, 'randomize', command,
                      'Randomly Reorder Chrome')
 
-  def AddBenchmarkChromeStep(self, factory_properties=None):
-    factory_properties = factory_properties or {}
-    command_class = self.GetPerfStepClass(
-        factory_properties, 'benchmark', process_log.GraphingLogProcessor)
+  def AddBenchmarkChromeStep(self):
     # Benchmark script path.
     script_path = self.PathJoin(self._build_dir, 'internal', 'build',
                                 'benchmark_chrome.py')
@@ -45,4 +41,5 @@ class SyzygyCommands(commands.FactoryCommands):
                '--build-dir=%s' % self._build_dir,
                '--target=%s' % self._target,
                '--verbose']
-    self.AddTestStep(command_class, 'benchmark', command, 'Benchmark Chrome')
+    self.AddTestStep(shell.ShellCommand, 'benchmark', command,
+                     'Benchmark Chrome')
--- scripts/master/factory/syzygy_factory.py
+++ scripts/master/factory/syzygy_factory.py
@@ -62,6 +62,6 @@ class SyzygyFactory(gclient_factory.GClientFactory):
     
     if target == 'release':
       syzygy_cmd_obj.AddRandomizeChromeStep()
-      syzygy_cmd_obj.AddBenchmarkChromeStep(factory_properties)
+      syzygy_cmd_obj.AddBenchmarkChromeStep()
       
     return factory
