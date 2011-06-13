b565ba6006c1001539166dc91b16964868153300
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-13 16:20:11 +0000

    Fail the script if no mac binaries are found. Add some logging to sort out official builder issue.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/7104137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@88828 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/sizes.py
+++ scripts/slave/chromium/sizes.py
@@ -97,7 +97,14 @@ RESULT %(framework_name)s-__DATA: __DATA= %(framework_data)s bytes
 RESULT %(framework_name)s-__OBJC: __OBJC= %(framework_objc)s bytes
 *RESULT %(app_bundle)s: %(app_bundle)s= %(app_bundle_size)s bytes""") % (
         print_dict)
+    # Temp logging code to figure out official builder problems.
+    else:
+      print "For basename '%s', didn't find '%s'" % \
+        (base_name, chromium_executable)
+    # Found a match, don't check the other base_names.
     return result
+  # If no base_names matched, fail script.
+  return 66
 
 
 def main_linux(options, args):
