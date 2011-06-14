f2573473de831e66ab052c153cd7834213479c1f
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 18:15:42 +0000

    First part to convert GoodRevisions to use the step name instead of the step text.
    Add check_revisions to support GIT poller.
    
    This will bring back sanity to this code.
    
    BUG=
    TEST=Not much
    
    
    Review URL: http://codereview.chromium.org/7003161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@89027 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/goodrevisions.py
+++ scripts/master/goodrevisions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -22,7 +22,8 @@ from master import get_password
 class GoodRevisions(base.StatusReceiverMultiService):
   """This is a status notifier which stores good revision numbers."""
 
-  def __init__(self, store_revisions_url, good_revision_steps=None):
+  def __init__(self, store_revisions_url, good_revision_steps=None,
+               use_getname=False, check_revisions=True):
     """Constructor with following specific arguments.
 
     @type good_revision_steps: Dictionary of builder name string mapped to a
@@ -30,11 +31,17 @@ class GoodRevisions(base.StatusReceiverMultiService):
     @param good_revision_steps: The list of all the steps of all the builders
                                 we want to validate before storing this
                                 revision as good.
-    @param store_revisions_url: URL where revision info is stored.
+    @param store_revisions_url: URL where revision info is stored.a
+
+    @type check_revisions: Boolean, default to True.
+    @param check_revisions: Check revisions and users for closing the tree.
     """
     base.StatusReceiverMultiService.__init__(self)
     self.good_revision_steps = good_revision_steps or {}
     self.store_revisions_url = store_revisions_url
+    # TODO(maruel): Enforce use_getname == True
+    self.use_getname = use_getname
+    self.check_revisions = check_revisions
 
     # We remember the success of interesting steps in a dictionary index by the
     # revision number. As soon as one of the interesting steps fail we flush
@@ -74,14 +81,38 @@ class GoodRevisions(base.StatusReceiverMultiService):
       w.unsubscribe(self)
     return base.StatusReceiverMultiService.disownServiceParent(self)
 
+  def getInterestingBuilders(self):
+    if not self.good_revision_steps:
+      return self.status.getBuilderNames()
+    return self.good_revision_steps.keys()
+
+  def getInterestingBuildSteps(self, builder_name, build):
+    if (not self.good_revision_steps or
+        (builder_name in self.good_revision_steps and
+          not self.good_revision_steps[builder_name])):
+      # All builders are interesting, or all steps for this builder are
+      # interesting.
+      if self.use_getname:
+        return [step.getName() for step in build.getSteps()]
+      else:
+        return [step.getText()[0] for step in build.getSteps()]
+
+    return self.good_revision_steps[builder_name]
+
+  def isInterestingBuilder(self, name):
+    return name in self.getInterestingBuilders()
+
+  def isInterestingBuildStep(self, builder_name, build, step_text):
+    return step_text in self.getInterestingBuildSteps(builder_name, build)
+
   def builderAdded(self, name, builder):
     # Only subscribe to builders we are interested in.
-    if name in self.good_revision_steps:
+    if self.isInterestingBuilder(name):
       return self
 
   def buildStarted(self, name, build):
     """A build has started allowing us to register for stepFinished."""
-    if name in self.good_revision_steps:
+    if self.isInterestingBuilder(name):
       return self
 
   def stepFinished(self, build, step, results):
@@ -89,31 +120,43 @@ class GoodRevisions(base.StatusReceiverMultiService):
     builder_name = build.getBuilder().getName()
 
     # For some reason we sometimes get called even if we didn't subscribe.
-    if builder_name not in self.good_revision_steps:
+    if not self.isInterestingBuilder(builder_name):
       log.msg('Was called for %s even if not subscribed' % builder_name)
       return
 
-    steps_text = step.getText()
-    interesting_steps = self.good_revision_steps[builder_name]
-
+    if self.use_getname:
+      step_text = step.getName()
+    else:
+      step_text = step.getText()[0]
     # We only need to deal with interesting steps.
-    if not max([step_text in interesting_steps for step_text in steps_text]):
+    if not self.isInterestingBuildStep(builder_name, build, step_text):
+      log.msg('not interested in step %s' % step_text)
       return
 
     # TODO(maruel): Support git.
-    latest_revision = int(build_utils.getLatestRevision(build))
+    latest_revision = build_utils.getLatestRevision(build)
     if not latest_revision:
+      log.msg('no lastest revision for build %s' % build.asDict())
       return
 
-    # If we already succeeded for a more recent revision,
-    # let's just forget about this one.
-    if latest_revision <= self.last_known_good_revision:
-      return
+    # If check_revisions=False that means that the tree closure request is
+    # coming from nightly scheduled bots or a git poller, that need not
+    # necessarily have the revision info or the revision is a hash that cannot
+    # be compared.
+    if self.check_revisions:
+      latest_revision = int(latest_revision)
+
+      # If we already succeeded for a more recent revision,
+      # let's just forget about this one.
+      if latest_revision <= self.last_known_good_revision:
+        log.msg('revision too old')
+        return
 
     # If we already failed for this revision,
     # there is nothing else we need to do.
     if latest_revision in self.failed_revisions:
       assert latest_revision not in self.succeeded_steps
+      log.msg('revision already failed')
       return
 
     # If we have failed, we add this revision to our failure list and flush it
@@ -125,45 +168,51 @@ class GoodRevisions(base.StatusReceiverMultiService):
       # pop() with a default value allows us to remove an element
       # without having to test if it is there in the first place.
       self.succeeded_steps.pop(latest_revision, None)
-      self.PostData(revision=latest_revision, success=0, steps_text=steps_text)
+      self.PostData(revision=latest_revision, success=0,
+                    steps_text=step.getText())
       return
 
     # Now let's add the succeeded steps to our success dict.
     self.succeeded_steps.setdefault(latest_revision, {})
     revision_status = self.succeeded_steps[latest_revision]
     revision_status.setdefault(builder_name, [])
-    revision_status[builder_name].extend(steps_text)
+    revision_status[builder_name].extend(step_text)
 
     # We must complete all the requested steps for all builds, before we can
     # store this revision as a successful one and then forget about all
     # previous revisions info.
-    for builder in self.good_revision_steps:
+    for builder in self.getInterestingBuilders():
       if builder not in revision_status:
         log.msg('Still missing builder %s to declare %s a good revision' %
                 (builder, str(latest_revision)))
         return
       succeeded_steps = revision_status[builder]
-      for required_step in self.good_revision_steps[builder]:
+      for required_step in self.getInterestingBuildSteps(builder, build):
         if required_step not in succeeded_steps:
           log.msg('Still missing step %s\%s to declare %s a good revision' %
                   (builder, required_step, str(latest_revision)))
           return
 
     # Start by remembering this success.
+    log.msg('Found LKGR = %s' % latest_revision)
     self.last_known_good_revision = latest_revision
 
     # Store it on the status server.
     self.PostData(revision=latest_revision, success=1)
 
-    # And now cleanup residual information from earlier revisions
-    # Iterate through a list of keys to allow removal while we iterate.
-    for revision in list(self.succeeded_steps.keys()):
-      if revision <= latest_revision:
-        del self.succeeded_steps[revision]
-    for revision in self.failed_revisions:
-      assert revision != latest_revision
-      if revision < latest_revision:
-        self.failed_revisions.remove(revision)
+    if self.check_revisions:
+      # And now cleanup residual information from earlier revisions
+      # Iterate through a list of keys to allow removal while we iterate.
+      for revision in list(self.succeeded_steps.keys()):
+        if revision <= latest_revision:
+          del self.succeeded_steps[revision]
+      for revision in self.failed_revisions:
+        assert revision != latest_revision
+        if revision < latest_revision:
+          self.failed_revisions.remove(revision)
+    else:
+      # TODO(maruel): Use LRU discarding. Right now it's a memory leak.
+      pass
 
   def PostData(self, revision, success, steps_text=None):
     """Post the revision data to the server store."""
