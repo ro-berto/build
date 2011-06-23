b20d55cd51fb86a173a6a003e36fe1aacf5512a2
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 20:01:07 +0000

    Archive the produced remoting-it2me.zip webapp artifact to allow for easy downloads.
    This will streamline testing of ToT changes by allowing harvesting of results for each platform.
    Review URL: http://codereview.chromium.org/7190020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@90253 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/archive_build.py
+++ scripts/slave/chromium/archive_build.py
@@ -559,20 +559,20 @@ class StagerBase(object):
           MySshCopyFiles(test_file, self.options.archive_host, test_dir,
                         gs_base, '/'.join([UPLOAD_DIR, relative_dir]))
 
-  def _UploadDevtoolsFrontendZip(self, www_dir, gs_base):
-    zipfile = os.path.join(self._build_dir, 'devtools_frontend.zip')
-    if not os.path.exists(zipfile):
+  def _UploadFile(self, filename, www_dir, gs_base):
+    path = os.path.join(self._build_dir, filename)
+    if not os.path.exists(path):
       return
     if chromium_utils.IsWindows():
-      print 'chromium_utils.CopyFileToDir(%s, %s)' % (zipfile, www_dir)
+      print 'chromium_utils.CopyFileToDir(%s, %s)' % (path, www_dir)
       if not self.options.dry_run:
-        MyCopyFileToDir(zipfile, www_dir, gs_base)
+        MyCopyFileToDir(path, www_dir, gs_base)
     elif chromium_utils.IsLinux() or chromium_utils.IsMac():
-      print 'SshCopyFiles(%s, %s, %s)' % (zipfile, self.options.archive_host,
+      print 'SshCopyFiles(%s, %s, %s)' % (path, self.options.archive_host,
                                           www_dir)
       if not self.options.dry_run:
-        MyMakeWorldReadable(zipfile, gs_base)
-        MySshCopyFiles(zipfile, self.options.archive_host, www_dir, gs_base)
+        MyMakeWorldReadable(path, gs_base)
+        MySshCopyFiles(path, self.options.archive_host, www_dir, gs_base)
 
   def _GenerateChangeLog(self, previous_revision, changelog_path):
     """We need to generate change log for both chrome and webkit repository."""
@@ -708,7 +708,9 @@ class StagerBase(object):
         raise NotImplementedError(
               'Platform "%s" is not currently supported.' % sys.platform)
 
-    self._UploadDevtoolsFrontendZip(www_dir, gs_base)
+    # Upload extra build artifacts.
+    self._UploadFile('devtools_frontend.zip', www_dir, gs_base)
+    self._UploadFile('remoting-it2me.zip', www_dir, gs_base)
 
     if len(not_found):
       sys.stderr.write('\n\nWARNING: File(s) not found: %s\n' %
