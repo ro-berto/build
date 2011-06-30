3ca1ffe790f370ac9b15ab8f98a83376eaa64573
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-30 05:47:31 +0000

    V8 Buildbot: Update sputnik tests to revision 97
    V8 test expectations will be rebaselined against revision 97 shortly.
    Review URL: http://codereview.chromium.org/7248026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@91092 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/v8_factory.py
+++ scripts/master/factory/v8_factory.py
@@ -26,7 +26,7 @@ class V8Factory(gclient_factory.GClientFactory):
                             'https://es5conform.svn.codeplex.com/svn@71525')
 
   CUSTOM_DEPS_SPUTNIK = ('v8/test/sputnik/sputniktests',
-                         'http://sputniktests.googlecode.com/svn/trunk@94')
+                         'http://sputniktests.googlecode.com/svn/trunk@97')
 
   # Pinned at revision 65044 to allow scons to be removed from repository.
   CUSTOM_DEPS_SCONS = ('third_party/scons',
