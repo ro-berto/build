851065f96a2f7403c187b4ecb2d8aa9e69fd9987
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-06 23:43:05 +0000

    Only use clang for version >= 15.
    This is required to let the official builders use clang. It should have
    no impact on all the bots currently using clang, as they're all building
    version 15.
    Review URL: http://codereview.chromium.org/7541063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@95754 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/compile.py
+++ scripts/slave/compile.py
@@ -61,10 +61,17 @@ def common_mac_settings(command, options, env, compiler=None):
     return
 
   if compiler == 'clang':
-    clang_binary = os.path.join(os.path.dirname(options.build_dir),
-        'third_party', 'llvm-build', 'Release+Asserts', 'bin', 'clang++')
-    env['CC'] = os.path.abspath(clang_binary)
-    return
+    # The official release builder wobbles across release branches.
+    # Chromes prior to m15 were built with gcc and we want to keep it
+    # that way, so only enable clang on m15 and up.
+    exec(open('src/chrome/VERSION').read())
+    if int(MAJOR) >= 15:
+      clang_binary = os.path.join(os.path.dirname(options.build_dir),
+          'third_party', 'llvm-build', 'Release+Asserts', 'bin', 'clang++')
+      env['CC'] = os.path.abspath(clang_binary)
+      return
+    else:
+      print 'Not using clang for version', MAJOR
 
   # Most of the bot hostnames are in one of two patterns:
   #   base###.subnet.domain
