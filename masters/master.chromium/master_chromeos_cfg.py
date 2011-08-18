f3658ad362cde6a8cf0aedb3308fe69cd73db47a
petermayo@chromium.org <petermayo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 03:00:23 +0000

    Rationalize ChromeOS builders for Chrome.
    BUG=chromium-os:19234
    TEST=run FYI locally with a slave impersonating build14-m1
    
    Review URL: http://codereview.chromium.org/7624004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@97273 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium/master_chromeos_cfg.py
+++ masters/master.chromium/master_chromeos_cfg.py
@@ -16,43 +16,7 @@ T = helper.Triggerable
 
 def chromeos(): return chromium_factory.ChromiumFactory('src/build', 'linux2')
 
-
-################################################################################
-## Release
-################################################################################
-
 defaults['category'] = '8chromiumos'
-
-#
-# Main release scheduler for src/
-#
-S('chromeos_rel', branch='src', treeStableTimer=60)
-
-#
-# ChromeOS Rel Builder
-#
-B('Linux Builder (ChromiumOS)', 'rel', 'compile', 'chromeos_rel')
-F('rel', chromeos().ChromiumOSFactory(
-    tests=['unit',
-           'base',
-           'net',
-           'googleurl',
-           'media',
-           'ui',
-           'printing',
-           'remoting',
-           'browser_tests',
-           'interactive_ui',
-           'views',
-           'crypto',
-           'cacheinvalidation',
-           'jingle'],
-    options=['--compiler=goma', 'chromeos_builder'],
-    factory_properties={
-        'archive_build': True,
-        'extra_archive_paths': 'chrome/tools/build/chromeos',
-        'gclient_env': { 'GYP_DEFINES':'chromeos=1 target_arch=ia32'},
-        'generate_gtest_json': True}))
 
 
 ################################################################################
@@ -67,61 +31,7 @@ S('chromeos_dbg', branch='src', treeStableTimer=60)
 #
 # Triggerable scheduler for the builders
 #
-T('linux_cros_dbg_trigger')
 T('linux_views_dbg_trigger')
-
-#
-# ChromeOS Dbg Builders and Testers
-#
-B('Linux Builder (ChromiumOS dbg)', 'cros_dbg', 'compile', 'chromeos_dbg')
-F('cros_dbg', chromeos().ChromiumOSFactory(
-    slave_type='NASBuilder',
-    target='Debug',
-    options=['--compiler=goma', 'chromeos_builder'],
-    factory_properties={
-        'gclient_env': { 'GYP_DEFINES':'chromeos=1 target_arch=ia32'},
-        'trigger': 'linux_cros_dbg_trigger'}))
-
-B('Linux Tests (ChromiumOS dbg)(1)', 'cros_dbg_tests_1', 'testers',
-  'linux_cros_dbg_trigger', auto_reboot=True)
-F('cros_dbg_tests_1', chromeos().ChromiumOSFactory(
-    slave_type='NASTester',
-    target='Debug',
-    tests=['base',
-           'browser_tests',
-           'crypto',
-           'googleurl',
-           'interactive_ui',
-           'media',
-           'printing',
-           'remoting',
-           'ui',
-           'views',
-           'cacheinvalidation',
-           'jingle'],
-    factory_properties={'generate_gtest_json': True,
-                        'ui_total_shards': 3, 'ui_shard_index': 1,
-                        'browser_total_shards': 3, 'browser_shard_index': 1,}))
-
-B('Linux Tests (ChromiumOS dbg)(2)', 'cros_dbg_tests_2', 'testers',
-  'linux_cros_dbg_trigger', auto_reboot=True)
-F('cros_dbg_tests_2', chromeos().ChromiumOSFactory(
-    slave_type='NASTester',
-    target='Debug',
-    tests=['browser_tests', 'ui', 'unit',],
-    factory_properties={'generate_gtest_json': True,
-                        'ui_total_shards': 3, 'ui_shard_index': 2,
-                        'browser_total_shards': 3, 'browser_shard_index': 2,}))
-
-B('Linux Tests (ChromiumOS dbg)(3)', 'cros_dbg_tests_3', 'testers',
-  'linux_cros_dbg_trigger', auto_reboot=True)
-F('cros_dbg_tests_3', chromeos().ChromiumOSFactory(
-    slave_type='NASTester',
-    target='Debug',
-    tests=['browser_tests', 'net', 'ui',],
-    factory_properties={'generate_gtest_json': True,
-                        'ui_total_shards': 3, 'ui_shard_index': 3,
-                        'browser_total_shards': 3, 'browser_shard_index': 3,}))
 
 B('Linux Builder (Views dbg)', 'view_dbg', 'compile', 'chromeos_dbg')
 F('view_dbg', chromeos().ChromiumOSFactory(
