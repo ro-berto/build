60b5ca943159034a1c7e5409bf449b9701d7d068
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 13:08:59 +0000

    Make runtest.py invoke shell scripts using Bash. This is controversial, because it requires every buildbot to have Bash installed. On the other hand, currently the build system does not allow Bash syntax to be used in shell scripts, which is quite a strong restriction.
    Review URL: http://codereview.chromium.org/7859031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@100398 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/runtest.py
+++ scripts/slave/runtest.py
@@ -179,7 +179,7 @@ def main_mac(options, args):
     command.extend(supervisor_args)
     command.append(test_exe_path)
   elif options.run_shell_script:
-    command = ['sh', test_exe_path]
+    command = ['bash', test_exe_path]
   elif options.run_python_script:
     command = [sys.executable, test_exe]
   else:
