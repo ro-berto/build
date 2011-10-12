eae8fd61844714292cdc3dc8446ee040beddda5a
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 16:40:59 +0000

    Split off _bb7 to ease transition more.
    R=szager@chromium.org
    BUG=
    TEST=Start local TS and slave, start a build with HTTP port.
    
    
    Review URL: http://codereview.chromium.org/8221008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@105094 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/try_job_base.py
+++ scripts/master/try_job_base.py
@@ -2,22 +2,26 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 import re
 
-from buildbot import buildset
-from buildbot.changes.changes import Change
+import buildbot
 from buildbot.process.properties import Properties
-from buildbot.scheduler import BadJobfile
-from buildbot.scheduler import TryBase
+
 from twisted.python import log
-from twisted.web import http
 
-from master.try_job_stamp import TryJobStamp
+buildbot_0_8 = int(buildbot.version.split('.')[1]) >= 8
+if buildbot_0_8:
+  # TODO(maruel): IMPLEMENT ME!
+  # pylint: disable=E0611,F0401
+  from master.try_job_base_bb8 import BadJobfile, TryBase, TryJobBaseMixIn
+else:
+  from master.try_job_base_bb7 import BadJobfile, TryBase, TryJobBaseMixIn
 
 
-class TryJobBase(TryBase):
-  """Implement ParseJob."""
+class TryJobBase(TryBase, TryJobBaseMixIn):
+  # 0.7.12 uses list and 0.8.x uses tuple...
+  compare_attrs = tuple(list(TryBase.compare_attrs) + [
+      'pools', 'last_good-urls', 'code_review_sites'])
 
   # Simplistic email matching regexp.
   _EMAIL_VALIDATOR = re.compile(
@@ -25,8 +29,10 @@ class TryJobBase(TryBase):
 
   def __init__(self, name, pools, properties,
                last_good_urls, code_review_sites):
-    # TryBase.__init__ expects {}
+    # pylint has a false positive: it thinks base constructors aren't called.
+    # pylint: disable=W0231
     TryBase.__init__(self, name, pools.ListBuilderNames(), properties or {})
+    TryJobBaseMixIn.__init__(self)
     self.pools = pools
     pools.SetParent(self)
     self.last_good_urls = last_good_urls
@@ -75,31 +81,6 @@ class TryJobBase(TryBase):
         'Choose %s for job %s' % (','.join(options['bot']), options['reason']))
     return options
 
-  def ParseJob(self, options):
-    """Grab try job settings."""
-    options = self.parse_options(options)
-    # There can be only one to blame per change.
-    fake_changes = [
-        Change(email, [''], '', revision=options['revision'])
-        for email in options['email']
-    ]
-    patch = None
-    if options['patch']:
-      patch = (options['patchlevel'], options['patch'], options['root'])
-    last_good_url = (self.last_good_urls or {}).get(options['project'])
-    code_review_site = (self.code_review_sites or {}).get(options['project'])
-    jobstamp = TryJobStamp(
-        branch=options['branch'],
-        revision=options['revision'],
-        patch=patch,
-        changes=fake_changes,
-        last_good_url=last_good_url,
-        code_review_site=code_review_site,
-        job_name=options['name'],
-        patchset=options['patchset'],
-        issue=options['issue'])
-    return options['bot'], jobstamp, options['name'], self.get_props(options)
-
   def get_props(self, options):
     """Current job extra properties that are not related to the source stamp.
     Initialize with the Scheduler's base properties.
@@ -110,80 +91,3 @@ class TryJobBase(TryBase):
     if options['testfilter']:
       props.setProperty('testfilters', options['testfilter'], 'Scheduler')
     return props
-
-  def CancelJobsMatching(self, source_stamp, builder_name):
-    """Cancels any jobs with the same job and owner."""
-
-    if (not isinstance(source_stamp, TryJobStamp) or
-        len(source_stamp.changes) != 1):
-      # Not a try job.
-      return
-
-    # TODO(maruel): remove once pylint improves its type inference.
-    # pylint: disable=E1101
-    builder = self.parent.botmaster.builders.get(builder_name, None)
-    if not builder:
-      # TODO(maruel): Send an email to the user. There's no reference to a
-      # mail notifier here.
-      return
-    # Cancel any pending jobs on this builder that haven't yet started.
-    for buildable in builder.buildable:
-      if source_stamp.canReplace(buildable.source):
-        builder.cancelBuildRequest(buildable)
-        log.msg('Canceling job %s on %s' % (source_stamp.job_name,
-                                            builder.name))
-        break # There should only be one try with a given name.
-
-    # Mark any running builds as canceled. In theory we could cancel these too
-    # in practice cancelling might leave the bot in a weird state. Instead we
-    # set canceled to true so that after the current step finishes the builder
-    # doesn't execute any more steps.
-    for build in builder.building:
-      for request in build.requests:
-        if source_stamp.canReplace(request.source):
-          # The request copies the stamp. Set the stamp for both so
-          # that the canceled state is reflected in both places.
-          build.source.canceled = True
-          request.source.canceled = True
-          log.msg('Marking %s as canceled on %s' %
-                  (source_stamp.job_name, builder_name))
-
-  def SubmitJob(self, options):
-    """Queues the buildset.
-
-    Args:
-      options: an optparse set of options.
-
-    Returns: an HTTP error code.
-
-    The content of options is decoded by ParseJob. If the file 'disable_try'
-    exists in the current directory, the try server is disabled.
-    """
-    if os.path.exists('disable_try'):
-      log.msg('Try job cancelled because the server is disabled!')
-      return http.SERVICE_UNAVAILABLE
-
-    try:
-      builder_names, jobstamp, buildset_id, props = self.ParseJob(options)
-    except BadJobfile:
-      log.msg('%s reports a bad job connection' % (self))
-      log.err()
-      return http.BAD_REQUEST
-    reason = "'%s' try job" % buildset_id
-    # Send one build request per builder, otherwise the cancelation logic
-    # doesn't work.
-    build_sets = []
-    for builder_name in builder_names:
-      build_set = buildset.BuildSet(
-          [builder_name],
-          jobstamp,
-          reason=reason,
-          bsid=buildset_id,
-          properties=props)
-      build_sets.append(build_set)
-      self.CancelJobsMatching(build_set, builder_name)
-    for build_set in build_sets:
-      # Type inference error.
-      # pylint: disable=E1101
-      self.parent.submitBuildSet(build_set)
-    return http.OK
---
+++ scripts/master/try_job_base_bb7.py
@@ -0,0 +1,120 @@
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+from buildbot import buildset
+from buildbot.changes.changes import Change
+from buildbot.scheduler import BadJobfile
+from buildbot.scheduler import TryBase  # pylint: disable=W0611
+from twisted.python import log
+from twisted.web import http
+
+from master.try_job_stamp import TryJobStamp
+
+
+class TryJobBaseMixIn:
+  def __init__(self):
+    pass
+
+  def ParseJob(self, options):
+    """Grab try job settings."""
+    options = self.parse_options(options)
+    fake_changes = [
+        Change(email, [''], '', revision=options['revision'])
+        for email in options['email']
+    ]
+    patch = None
+    if options['patch']:
+      patch = (options['patchlevel'], options['patch'], options['root'])
+    last_good_url = (self.last_good_urls or {}).get(options['project'])
+    code_review_site = (self.code_review_sites or {}).get(options['project'])
+    jobstamp = TryJobStamp(
+        branch=options['branch'],
+        revision=options['revision'],
+        patch=patch,
+        changes=fake_changes,
+        last_good_url=last_good_url,
+        code_review_site=code_review_site,
+        job_name=options['name'],
+        patchset=options['patchset'],
+        issue=options['issue'])
+    return options['bot'], jobstamp, options['name'], self.get_props(options)
+
+  def CancelJobsMatching(self, source_stamp, builder_name):
+    """Cancels any jobs with the same job and owner."""
+
+    if (not isinstance(source_stamp, TryJobStamp) or
+        len(source_stamp.changes) != 1):
+      # Not a try job.
+      return
+
+    # TODO(maruel): remove once pylint improves its type inference.
+    # pylint: disable=E1101
+    builder = self.parent.botmaster.builders.get(builder_name, None)
+    if not builder:
+      # TODO(maruel): Send an email to the user. There's no reference to a
+      # mail notifier here.
+      return
+    # Cancel any pending jobs on this builder that haven't yet started.
+    for buildable in builder.buildable:
+      if source_stamp.canReplace(buildable.source):
+        builder.cancelBuildRequest(buildable)
+        log.msg('Canceling job %s on %s' % (source_stamp.job_name,
+                                            builder.name))
+        break # There should only be one try with a given name.
+
+    # Mark any running builds as canceled. In theory we could cancel these too
+    # in practice cancelling might leave the bot in a weird state. Instead we
+    # set canceled to true so that after the current step finishes the builder
+    # doesn't execute any more steps.
+    for build in builder.building:
+      for request in build.requests:
+        if source_stamp.canReplace(request.source):
+          # The request copies the stamp. Set the stamp for both so
+          # that the canceled state is reflected in both places.
+          build.source.canceled = True
+          request.source.canceled = True
+          log.msg('Marking %s as canceled on %s' %
+                  (source_stamp.job_name, builder_name))
+
+  def SubmitJob(self, options):
+    """Queues the buildset.
+
+    Args:
+      options: an optparse set of options.
+
+    Returns: an HTTP error code.
+
+    The content of options is decoded by ParseJob. If the file 'disable_try'
+    exists in the current directory, the try server is disabled.
+    """
+    if os.path.exists('disable_try'):
+      log.msg('Try job cancelled because the server is disabled!')
+      return http.SERVICE_UNAVAILABLE
+
+    try:
+      builder_names, jobstamp, buildset_id, props = self.ParseJob(options)
+    except BadJobfile:
+      log.msg('%s reports a bad job connection' % (self))
+      log.err()
+      return http.BAD_REQUEST
+    reason = "'%s' try job" % buildset_id
+    # Send one build request per builder, otherwise the cancelation logic
+    # doesn't work.
+    build_sets = []
+    for builder_name in builder_names:
+      build_set = buildset.BuildSet(
+          [builder_name],
+          jobstamp,
+          reason=reason,
+          bsid=buildset_id,
+          properties=props)
+      build_sets.append(build_set)
+      self.CancelJobsMatching(build_set, builder_name)
+    for build_set in build_sets:
+      # Type inference error.
+      # pylint: disable=E1101
+      self.parent.submitBuildSet(build_set)
+    return http.OK
