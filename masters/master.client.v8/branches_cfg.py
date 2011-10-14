ff85c92a1074f7d1fb723f2dd99ffb7b213eecb3
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 06:31:31 +0000

    V8 Buildbot: Update branches, and remove presubmit checks from beta and stable branch builders.
    Since the update to the linter the presubmit step has been red on these builders. We don't want to risk merging back these cosmethic changes to fix it on the branches (there are a lot). 
    
    Also, increase compilation timeout on vista perf.
    
    TBR=mstarzinger
    Review URL: http://codereview.chromium.org/8271026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@105465 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8/branches_cfg.py
+++ masters/master.client.v8/branches_cfg.py
@@ -9,9 +9,9 @@
 # Note that stable, beta and dev might not always correspond to what is
 # used in chromium, but here we simple use it to mean the last 3 branches.
 
-stable_branch = '3.3'
-beta_branch = '3.4'
-dev_branch = '3.5'
+stable_branch = '3.4'
+beta_branch = '3.5'
+dev_branch = '3.6'
 branch_names = {
     'stable': {'ia32': 'V8 Linux - ' + stable_branch + ' branch',
                'arm': 'V8 arm - sim - ' + stable_branch + ' branch',
