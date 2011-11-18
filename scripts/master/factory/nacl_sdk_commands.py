a124f226952ee4d350006de0d8dbb48d868b90a5
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 18:29:55 +0000

    Fix recently introducted regression on pylint.
    ************* Module master.factory.nacl_sdk_commands
    W0611: 10,0: Unused import WithProperties
    W0611: 13,0: Unused import archive_command
    
    Review URL: http://codereview.chromium.org/8558024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@110718 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/nacl_sdk_commands.py
+++ scripts/master/factory/nacl_sdk_commands.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,10 +7,8 @@
 
 Contains the Native Client SDK specific commands. Based on commands.py"""
 
-from buildbot.process.properties import WithProperties
 from master import chromium_step
 from master.factory import commands
-from master.log_parser import archive_command
 
 
 class NativeClientSDKCommands(commands.FactoryCommands):
