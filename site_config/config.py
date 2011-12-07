04a70012cd6b358ddac0b5be89a27f8eb1482406
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 20:00:31 +0000

    Skip symbol processing on the redistributable xinput1_3.dll. Review URL: http://codereview.chromium.org/8852006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@113445 0039d316-1c4b-4281-b951-d872f2087c98

--- site_config/config.py
+++ site_config/config.py
@@ -176,7 +176,8 @@ class Archive(config_private.Archive):
   symbols_to_skip_upload = [
       'icudt38.dll', 'icudt42.dll', 'icudt46.dll', 'icudt.dll', 'rlz.dll',
       'avcodec-53.dll', 'avformat-53.dll', 'avutil-51.dll', 'd3dx9_42.dll',
-      'd3dx9_43.dll', 'D3DCompiler_42.dll', 'D3DCompiler_43.dll',]
+      'd3dx9_43.dll', 'D3DCompiler_42.dll', 'D3DCompiler_43.dll',
+      'xinput1_3.dll',]
 
   if os.environ.get('CHROMIUM_BUILD', '') == '_google_chrome':
     exes_to_skip_entirely = []
