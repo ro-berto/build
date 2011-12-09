5e031e1d1634643502c08848586817af4328d459
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 19:43:03 +0000

    Bring back chromium.perf.
    Hardcoding a branch inside a revision with @ without reseting branch confuses
    the Gclient step.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8873045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@113838 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/chromium_step.py
+++ scripts/master/chromium_step.py
@@ -74,8 +74,7 @@ class GClient(source.Source):
     try:
       # parent_cr_revision might be set, but empty.
       if self.getProperty('parent_cr_revision'):
-        revision = '%s@%s'% (str(branch),
-                             self.getProperty('parent_cr_revision'))
+        revision = self.getProperty('parent_cr_revision')
     except KeyError:
       pass
     args['revision'] = revision
