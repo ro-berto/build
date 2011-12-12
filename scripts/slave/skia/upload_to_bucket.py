9c91b187ec0e5183f494c3ac356fa43a07c553d5
epoger@google.com <epoger@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-12 22:12:13 +0000

    Make upload_to_bucket convert source_filepath to absolute path, to help debugging
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@114096 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/skia/upload_to_bucket.py
+++ scripts/slave/skia/upload_to_bucket.py
@@ -3,19 +3,31 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Upload a single file to a Google Storage Bucket."""
+"""Upload a single file to a Google Storage Bucket.
+
+To test:
+  cd .../build/scripts/slave/skia
+  PYTHONPATH=$PWD/../..:$PWD/../../../site_config python upload_to_bucket.py \
+    --source_filepath=../../../DEPS --dest_gsbase=gs://chromium-skia-gm
+"""
 
 import optparse
+import os
 import sys
 
 from slave import slave_utils
 
 
 def upload_to_bucket(source_filepath, dest_gsbase):
-  status = slave_utils.GSUtilCopyFile(source_filepath, dest_gsbase)
+  abs_source_filepath = os.path.abspath(source_filepath)
+  print 'translated source_filepath %s to absolute path %s' % (
+      source_filepath, abs_source_filepath)
+  if not os.path.exists(abs_source_filepath):
+    raise Exception('ERROR: file not found: %s' % abs_source_filepath)
+  status = slave_utils.GSUtilCopyFile(abs_source_filepath, dest_gsbase)
   if status != 0:
     raise Exception('ERROR: GSUtilCopyFile error %d. "%s" -> "%s"' % (
-        status, source_filepath, dest_gsbase))
+        status, abs_source_filepath, dest_gsbase))
   (status, _output) = slave_utils.GSUtilListBucket(dest_gsbase)
   if status != 0:
     raise Exception('ERROR: failed to get list of %s, exiting' % dest_gsbase)
