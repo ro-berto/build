b9c15508836f939b058d703aa7ac0aa14af14fb9
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 17:25:58 +0000

    Print aggregate totals for each size item across all Linux binaries
    Collecting aggregate totals will let us add perf expectations that
    catch bad things like TEXTREL counts increasing without manually
    adding a perf expectation for the TEXTREL count of each binary.
    
    BUG= 109318
    TEST= ran sizes.py by hand
    
    R=cmp@chromium.org
    
    Review URL: http://codereview.chromium.org/9121018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@116871 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/sizes.py
+++ scripts/slave/chromium/sizes.py
@@ -121,14 +121,21 @@ RESULT %(framework_name)s-__OBJC: __OBJC= %(framework_objc)s bytes
 
 
 def check_linux_binary(target_dir, binary_name):
-  """Print appropriate size information about the built Linux binary given.
+  """Collect appropriate size information about the built Linux binary given.
 
-  Returns the first non-zero exit status of any command it executes,
-  or zero on success.
+  Returns a tuple (result, sizes).  result is the first non-zero exit
+  status of any command it executes, or zero on success.  sizes is a list
+  of tuples (star_flag, name, identifier, totals_identifier, value, units).
+  star_flag is a Boolean that's true if this item prints as '*RESULT' rather
+  than 'RESULT'.  After that, the printed line looks like:
+    name: identifier= value units
+  When this same data is used for totals across all the binaries, then
+  totals_identifier is the identifier to use, or '' to just use identifier.
   """
   binary_file = os.path.join(target_dir, binary_name)
 
   result = 0
+  sizes = []
 
   def run_process(result, command):
     p = subprocess.Popen(command, stdout=subprocess.PIPE)
@@ -139,14 +146,16 @@ def check_linux_binary(target_dir, binary_name):
         result = p.returncode
     return result, stdout
 
-  print "*RESULT %s: %s= %s bytes" % (binary_name, binary_name,
-                                      get_size(binary_file))
+  sizes.append((True, binary_name, binary_name, 'size',
+                get_size(binary_file), 'bytes'))
 
   result, stdout = run_process(result, ['size', binary_file])
   text, data, bss = re.search('(\d+)\s+(\d+)\s+(\d+)', stdout).groups()
-  print "RESULT %s-text: text= %s bytes" % (binary_name, text)
-  print "RESULT %s-data: data= %s bytes" % (binary_name, data)
-  print "RESULT %s-bss: bss= %s bytes" % (binary_name, bss)
+  sizes += [
+      (False, binary_name + '-text', 'text', '', text, 'bytes'),
+      (False, binary_name + '-data', 'data', '', data, 'bytes'),
+      (False, binary_name + '-bss', 'bss', '', bss, 'bytes'),
+      ]
 
   # Find the number of files with at least one static initializer.
   # First determine if we're 32 or 64 bit
@@ -170,7 +179,7 @@ def check_linux_binary(target_dir, binary_name):
     # The first entry is always 0 and the last is -1 as guards.
     # So subtract 2 from the count.
     count = (size / word_size) - 2
-  print "*RESULT %s-si: initializers= %s files" % (binary_name, count)
+  sizes.append((True, binary_name + '-si', 'initializers', '', count, 'files'))
 
   # Determine if the binary has the DT_TEXTREL marker.
   result, stdout = run_process(result, ['readelf', '-Wd', binary_file])
@@ -181,9 +190,9 @@ def check_linux_binary(target_dir, binary_name):
     # There are some, so count them.
     result, stdout = run_process(result, ['eu-findtextrel', binary_file])
     count = stdout.count('\n')
-  print "*RESULT %s-textrel: textrel= %s relocs" % (binary_name, count)
+  sizes.append((True, binary_name + '-textrel', 'textrel', '', count, 'relocs'))
 
-  return result
+  return result, sizes
 
 
 def main_linux(options, args):
@@ -197,10 +206,17 @@ def main_linux(options, args):
 
   result = 0
 
+  totals = {}
+
   for binary in ['chrome', 'nacl_helper', 'nacl_helper_bootstrap']:
-    this_result = check_linux_binary(target_dir, binary)
+    this_result, this_sizes = check_linux_binary(target_dir, binary)
     if result == 0:
       result = this_result
+    for starred, name, identifier, totals_id, value, units in this_sizes:
+      print "%sRESULT %s: %s= %s %s" % ('*' if starred else '',
+                                        name, identifier, value, units)
+      totals_id = totals_id or identifier, units
+      totals[totals_id] = totals.get(totals_id, 0) + int(value)
 
   files = [
     'chrome.pak',
@@ -220,6 +236,16 @@ def main_linux(options, args):
         continue  # Don't print anything for missing files.
       raise
     print "*RESULT %s: %s= %s bytes" % (filename, filename, size)
+    totals['size', 'bytes'] += size
+
+  # TODO(mcgrathr): This should all be refactored so the mac and win flavors
+  # also deliver data structures rather than printing, and the logic for
+  # the printing and the summing totals is shared across all three flavors.
+  star = '*'
+  for (identifier, units), value in sorted(totals.iteritems()):
+    print "%sRESULT totals-%s: %s= %s %s" % (star, identifier, identifier,
+                                             value, units)
+    star = ''
 
   return result
 
