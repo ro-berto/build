9adbf22b9040bbbf9bf8326c56ad1467d465cd63
shadi@google.com <shadi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 00:10:06 +0000

    A perf notifier that emails regressions only when they happen continously.
    BUG=118341
    TEST=Unittests, ran on a local master/slave build.
    
    Review URL: http://codereview.chromium.org/9706051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@127039 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/master/perf_count_notifier.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import re
+
+from twisted.python import log
+
+from buildbot.status.builder import FAILURE, SUCCESS
+
+from master.chromium_notifier import ChromiumNotifier
+from master.failures_history import FailuresHistory
+
+# The history of results expire every day.
+_EXPIRATION_TIME = 24 * 3600
+
+
+class PerfCountNotifier(ChromiumNotifier):
+  """This is a status notifier that only alerts on consecutive perf changes.
+
+  The notifier only notifies when a number of consecutive REGRESS or IMPROVE
+  perf results are recorded.
+
+  See builder.interfaces.IStatusReceiver for more information about
+  parameters type.
+  """
+
+  def __init__(self, step_names, minimum_count=5, **kwargs):
+    """Initializes the PerfCountNotifier on tests starting with test_name.
+
+    Args:
+      step_names: List of perf steps names. This is needed to know perf steps
+          from other steps especially when the step is successful.
+      minimum_count: The number of minimum consecutive (REGRESS|IMPROVE) needed
+          to notify.
+    """
+    # Set defaults.
+    ChromiumNotifier.__init__(self, **kwargs)
+
+    self.minimum_count = minimum_count
+    self.step_names = step_names
+    self.recent_results = None
+    self._InitRecentResults()
+    self.notifications = FailuresHistory(expiration_time=_EXPIRATION_TIME,
+                                         size_limit=1000)
+
+  def _InitRecentResults(self):
+    """Initializes a new failures history object to store results."""
+    self.recent_results = FailuresHistory(expiration_time=_EXPIRATION_TIME,
+                                          size_limit=1000)
+
+  def _StoreResult(self, result):
+    """Stores the result value and removes counter results.
+
+    Example: if this is a REGRESS result then it is stored and its counter
+    IMPROVE result, if any, is reset.
+
+    Args:
+      result: A tuple of the form ('REGRESS|IMPROVE', 'value_name').
+    """
+    self.recent_results.Put(' '.join(result))
+    if result[0] == 'REGRESS':
+      counter_id = 'IMPROVE ' + result[1]
+    else:
+      counter_id = 'REGRESS ' + result[1]
+    # Reset counter_id count since this breaks the consecutive count of it.
+    num_counter_results = self.recent_results.GetCount(counter_id)
+    if num_counter_results > 0:
+      # This is a hack into FailuresHistory since it does not allow to delete
+      # entries in its history unless they are expired.
+      # FailuresHistory.failures_count is the total number of entries in the
+      # history limitted by FailuresHistory.size_limit.
+      del self.recent_results.failures[counter_id]
+      self.recent_results.failures_count -= num_counter_results
+
+  def _IsPerfStep(self, step_status):
+    """Checks if the step name is one of the defined perf tests names."""
+    return self.getName(step_status) in self.step_names
+
+  def isInterestingStep(self, build_status, step_status, results):
+    """Ignore the step if it is not one of the perf results steps.
+
+    Returns:
+      True: - if a REGRESS|IMPROVE happens consecutive minimum number of times.
+            - if it is not a SUCCESS step and neither REGRESS|IMPROVE.
+      False: - if it is a SUCCESS step.
+             - if it is a notification which has already been notified.
+    """
+    if not self._IsPerfStep(step_status):
+      return False
+
+    # In case of exceptions, sometimes results output is empty.
+    if not results:
+      results = [FAILURE]
+
+    # If it is a success step, i.e. not interesting, then reset counters.
+    if results[0] == SUCCESS:
+      self._InitRecentResults()
+      return False
+
+    # step_text is similar to:
+    # media_tests_av_perf <div class="BuildResultInfo"> PERF_REGRESS:
+    # time/t (89.07%) PERF_IMPROVE: fps/video (5.40%) </div>
+    #
+    # regex would return tuples of the form:
+    # ('REGRESS', 'time/t')
+    # ('IMPROVE', 'fps/video')
+    step_text = ' '.join(step_status.getText())
+    log.msg('[PerfCountNotifier] Analyzing failure text: %s.' % step_text)
+
+    perf_results = re.findall('PERF_(REGRESS|IMPROVE): (\S+)', step_text)
+
+    # If there is no regress or improve then this could be warning or exception.
+    if not perf_results:
+      if not self.notifications.GetCount(step_text):
+        log.msg('[PerfCountNotifier] Unrecognized step status encountered. '
+                'Reporting status as interesting.')
+        self.notifications.Put(step_text)
+        return True
+      else:
+        log.msg('[PerfCountNotifier] This problem has already been notified.')
+        return False
+
+    is_interesting = False
+    for result in perf_results:
+      if len(result) != 2:
+        # We expect a tuple similar to ('REGRESS', 'time/t')
+        continue
+      result_id = ' '.join(result)
+      self._StoreResult(result)
+      log.msg('[PerfCountNotifier] Result: %s happened %d times in a row.' %
+              (result_id, self.recent_results.GetCount(result_id)))
+      if self.recent_results.GetCount(result_id) >= self.minimum_count:
+        # This is an interesting result! We got the minimum consecutive count of
+        # this result, however we still need to check if its been notified.
+        if not self.notifications.GetCount(result_id):
+          log.msg('[PerfCountNotifier] Result: %s happened enough consecutive '
+                  'times to be reported.' % result_id)
+          self.notifications.Put(result_id)
+          is_interesting = True
+        else:
+          log.msg('[PerfCountNotifier] Result: %s has already been notified.' %
+                  result_id)
+
+    return is_interesting
---
+++ scripts/master/unittests/perf_count_notifier_test.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import test_env  # pylint: disable=W0611
+import unittest
+
+from buildbot.status.builder import FAILURE, SUCCESS
+
+from master.chromium_notifier import ChromiumNotifier
+from master.perf_count_notifier import PerfCountNotifier
+
+
+TEST_STATUS_TEXT = (
+    'media_tests_av_perf <div class="BuildResultInfo"> PERF_REGRESS: time/t '
+    '(89.07%) PERF_IMPROVE: fps/video (5.40%) </div>')
+
+TEST_STATUS_TEXT_COUNTER = (
+    'media_tests_av_perf <div class="BuildResultInfo"> PERF_IMPROVE: time/t '
+    '(44.07%) PERF_REGRESS: fps/video (3.0%) </div>')
+
+TEST_STATUS_TEXT_EXCEPTION = ('media_tests_av_perf exception')
+
+
+class PerfCountNotifierTest(unittest.TestCase):
+
+  def setUp(self):
+    self.email_sent = False
+    self.notifier = PerfCountNotifier(
+        fromaddr='buildbot@test',
+        forgiving_steps=[],
+        lookup='test',
+        sendToInterestedUsers=False,
+        extraRecipients=['extra@test'],
+        status_header='Failure on test.',
+        step_names='test_tests',
+        minimum_count=3)
+    self.old_getName = None
+    self.mockDefaultFunctions()
+
+  def tearDown(self):
+    self.resetMockDefaultFunctions()
+
+  def mockDefaultFunctions(self):
+    self.old_getName = ChromiumNotifier.getName
+    ChromiumNotifier.getName = self.getNameMock
+
+  def resetMockDefaultFunctions(self):
+    ChromiumNotifier.getName = self.old_getName
+
+  def getNameMock(self, step_status):
+    """Mocks the getName which returns the build_status step name."""
+    return self.notifier.step_names[0]
+
+  def testSuccessIsNotInteresting(self):
+    """Test success step is not interesting."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    results = [SUCCESS]
+    for _ in range(self.notifier.minimum_count):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+
+  def testIsInterestingAfterMinimumResults(self):
+    """Test step is interesting only after minimum consecutive results."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    results = [FAILURE]
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+
+  def testIsInterestingResetByCounterResults(self):
+    """Test step is not interesting if a counter result appears."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    results = [FAILURE]
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    # Reset the counters by having counter results.
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT_COUNTER)
+    self.assertFalse(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+    # Now check that we need to count back from the start.
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+
+  def testIsInterestingResetBySuccess(self):
+    """Test step count reset after a successful pass."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    results = [FAILURE]
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    # Reset the counters by having a success step.
+    results = [SUCCESS]
+    self.assertFalse(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+    # Now check that we need to count back from the start.
+    results = [1]
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+
+  def testIsInterestingException(self):
+    """Test step is interesting when step has exception."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT_EXCEPTION)
+    results = [FAILURE]
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+
+  def testNotificationOnce(self):
+    """Test isInsteresting happens only once."""
+    build_status = None
+    step_status = BuildStepStatusMock(TEST_STATUS_TEXT)
+    results = [FAILURE]
+    for _ in range(self.notifier.minimum_count - 1):
+      self.assertFalse(self.notifier.isInterestingStep(
+          build_status, step_status, results))
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+    self.assertFalse(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+    # Force expiration of notifications
+    self.notifier.notifications.expiration_time = -1
+    self.assertTrue(self.notifier.isInterestingStep(
+        build_status, step_status, results))
+
+
+class BuildStepStatusMock:
+  def __init__(self, text):
+    self.text = text
+
+  def getText(self):
+    return [self.text]
+
+
+if __name__ == '__main__':
+  unittest.main()
