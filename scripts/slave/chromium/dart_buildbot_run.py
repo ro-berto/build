75ff51970dca095af5f864da500c4716777d946f
ricow@google.com <ricow@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 12:50:37 +0000

    Remove master check from dart_buildbot_run.py
    TBR=foo
    
    Review URL: https://chromiumcodereview.appspot.com/10451079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@139530 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/dart_buildbot_run.py
+++ scripts/slave/chromium/dart_buildbot_run.py
@@ -9,26 +9,16 @@ This script is called from buildbot and reports results using the buildbot
 annotation scheme.
 """
 
-import optparse
 import sys
 
 from common import chromium_utils
 
 
 def main():
-  parser = optparse.OptionParser()
-  chromium_utils.AddPropertiesOptions(parser)
-  options, _ = parser.parse_args()
-  master_name = options.build_properties.get('mastername')
-
-  if master_name == 'client.dart':
-    return chromium_utils.RunCommand(
-        [sys.executable,
-         'src/build/buildbot_annotated_steps.py',
-        ])
-  else:
-    raise RuntimeError('Unrecognized master: ' + master_name)
-
+  return chromium_utils.RunCommand(
+      [sys.executable,
+       'src/build/buildbot_annotated_steps.py',
+      ])
 
 if __name__ == '__main__':
   sys.exit(main())
