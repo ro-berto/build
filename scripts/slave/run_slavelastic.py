ro-berto/build

commit 0443a7b582b6028184d4856e9f12a85666efb667

Allow specifying different shard counts for each swarm test.
R=maruel@chromium.org
BUG=
TEST=


Review URL: https://chromiumcodereview.appspot.com/10698098

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@145474 0039d316-1c4b-4281-b951-d872f2087c98

scripts/slave/run_slavelastic.py
@@ -40,10 +40,11 @@ class Manifest(object):
   RUN_TEST_PATH = os.path.join(
       'src', 'tools', 'isolate', 'run_test_from_archive.py')
 
-  def __init__(self, filename, test_name, switches):
+  def __init__(self, filename, test_name, shards, switches):
     """Populates a manifest object.
       Args:
         filename - The manifest with the test details.
+        shards - The number of swarm shards to request.
         test_name - The name to give the test request.
         switches - An object with properties to apply to the test request.
     """
@@ -55,8 +56,8 @@ class Manifest(object):
       }
 
     self.manifest_name = filename
+    self.shards = shards
 
-    self.g_shards = switches.num_shards
     # Random name for the output zip file
     self.zipfile_name = test_name + '.zip'
     self.tasks = []
@@ -122,8 +123,7 @@ class Manifest(object):
       },
       'configurations': [
         {
-          'min_instances': self.g_shards,
-          'max_instances': self.g_shards,
+          'min_instances': self.shards,
           'config_name': self.target_platform,
           'dimensions': {
             'os': self.target_platform,
@@ -184,17 +184,11 @@ def main():
   """
   # Parses arguments
   parser = optparse.OptionParser(
-      usage='%prog [options] [filenames]',
+      usage='%prog [options]',
       description=DESCRIPTION)
   parser.add_option('-w', '--working_dir', default='swarm_tests',
                     help='Desired working direction on the swarm slave side. '
                     'Defaults to %default.')
-  parser.add_option('-m', '--min_shards', type='int', default=1,
-                    help='Minimum number of shards to request. CURRENTLY NOT '
-                    'SUPPORTED.')
-  parser.add_option('-s', '--num_shards', type='int', default=1,
-                    help='Desired number of shards to request. Must be '
-                    'greater than or equal to min_shards.')
   parser.add_option('-o', '--os_image',
                     help='Swarm OS image to request.')
   parser.add_option('-u', '--swarm-url', default='http://localhost:8080',
@@ -213,12 +207,18 @@ def main():
   parser.add_option('-t', '--test-name-prefix', default='',
                     help='Specify the prefix to give the swarm test request. '
                     'Defaults to %default')
+  parser.add_option('-n', '--manifest_name', action='append',
+                    help='The name of a manifest to send to swarm. This may '
+                    'be given multiple times to send multiple manifests.')
+  parser.add_option('-s', '--shards', type='int', action='append',
+                    help='The number of shards to request for a manifest. '
+                    'This must be listed once for each -n.')
   parser.add_option('-v', '--verbose', action='store_true',
                     help='Print verbose logging')
   (options, args) = parser.parse_args()
 
-  if not args:
-    parser.error('Must specify at least one filename')
+  if args:
+    parser.error('Unknown args, ' + args)
 
   if not options.os_image:
     parser.error('Must specify an os image')
@@ -226,6 +226,10 @@ def main():
     parser.error('Must specify the hashtable directory')
   if not options.data_dest_dir:
     parser.error('Must specify the server directory')
+
+  if len(options.manifest_name) != len(options.shards):
+    parser.error('Number of min shards given doesn\'t match the number '
+                 'of manifests')
 
   # Remove the old data if there is any
   if os.path.isdir(options.data_dest_dir):
@@ -235,9 +239,12 @@ def main():
 
   # Send off the swarm test requests.
   highest_exit_code = 0
-  for filename in args:
+  for i in range(len(options.manifest_name)):
     highest_exit_code = max(highest_exit_code,
-                            ProcessManifest(filename, options))
+                            ProcessManifest(options.manifest_name[i],
+                                            filename,
+                                            options.shards[i],
+                                            options))
 
   return highest_exit_code
 

masters/master.chromium.swarm/master.cfg
@@ -96,15 +96,17 @@ def SetupSwarmTests(machine, data_dest_dir, data_server_dir, options=None,
     # Build until death.
     options = ['--build-tool=ninja'] + options + ['--', '-k', '0']
 
+  tests = [
+      # They must be in the reverse order of latency to get results, e.g. the
+      # slowest test should be last.
+      swarm_factory.SwarmTest('base_unittests', 1),
+      swarm_factory.SwarmTest('net_unittests', 3),
+      swarm_factory.SwarmTest('unit_tests', 4),
+      swarm_factory.SwarmTest('browser_tests', 5),
+      ]
+
   return machine.SwarmFactory(
-      tests=[
-        # They must be in the reverse order of latency to get results, e.g. the
-        # slowest test should be last.
-        'base_unittests',
-        'net_unittests',
-        'unit_tests',
-        'browser_tests',
-      ],
+      tests=tests,
       options=options,
       project=project,
       factory_properties=factory_properties,

scripts/master/factory/swarm_commands.py
@@ -16,21 +16,25 @@ class SwarmCommands(commands.FactoryCommands):
   """Encapsulates methods to add swarm commands to a buildbot factory"""
 
   def AddTriggerSwarmTestStep(self, target_platform, swarm_server, data_server,
-                              hashtable_dir, data_dest_dir,
-                              min_shards, max_shards,
-                              manifest_files):
+                              hashtable_dir, data_dest_dir, manifest_directory,
+                              tests):
     script_path = self.PathJoin(self._script_dir, 'run_slavelastic.py')
 
     swarm_request_name_prefix = WithProperties('%s-%s-',
                                                'buildername:-None',
                                                'buildnumber:-None')
 
-    command = [self._python, script_path, '-m', min_shards, '-s', max_shards,
-               '-o', target_platform, '-u', swarm_server, '-d', data_server,
+    command = [self._python, script_path, '-o', target_platform,
+               '-u', swarm_server, '-d', data_server,
                '--hashtable-dir', hashtable_dir,
                '--data-dest-dir', data_dest_dir,
                '-t', swarm_request_name_prefix]
-    command.extend(manifest_files)
+
+    # Add the tests to run, along with the minimum and maximum number of
+    # shards to request.
+    for test in tests:
+      command.extend(['-n', self.PathJoin(manifest_directory, test.test_name),
+                      '-s', test.shards])
 
     self.AddTestStep(shell.ShellCommand, 'trigger_swarm_tests', command)
 

scripts/master/factory/swarm_factory.py
@@ -6,10 +6,18 @@
 
 Based on chromium_factory.py and adds chromium-specific steps."""
 
-import os
-
 from master.factory import chromium_factory
 from master.factory import swarm_commands
+
+
+class SwarmTest(object):
+  """A small helper class containing any required details to run a
+     swarm test.
+  """
+  def __init__(self, test_name, shards):
+    self.test_name = test_name
+    self.shards = shards
+
 
 class SwarmFactory(chromium_factory.ChromiumFactory):
   def SwarmFactory(self, target='Release', clobber=False, tests=None,
@@ -64,20 +72,19 @@ class SwarmFactory(chromium_factory.ChromiumFactory):
         out_dir = 'out'
 
       # Send of all the test requests as a single step.
-      swarm_inputs = [os.path.join('src', out_dir, target, test + '.results')
-                      for test in tests]
-      hashtable_directory = os.path.join('src', out_dir, target, 'hashtable')
+      manifest_directory = swarm_command_obj.PathJoin('src', out_dir, target)
+      hashtable_directory = swarm_command_obj.PathJoin('src', out_dir, target,
+                                                       'hashtable')
       swarm_command_obj.AddTriggerSwarmTestStep(self._target_platform,
             swarm_server,
             data_server,
             hashtable_directory,
             data_dest_dir,
-            factory_properties.get('min_swarm_shards', '3'),
-            factory_properties.get('max_swarm_shards', '3'),
-            swarm_inputs)
+            manifest_directory,
+            tests)
 
       # Each test has its output returned as its own step.
       for test in tests:
-        swarm_command_obj.AddGetSwarmTestStep(swarm_server, test)
+        swarm_command_obj.AddGetSwarmTestStep(swarm_server, test.test_name)
 
     return factory

scripts/slave/unittests/run_slavelastic_test.py
@@ -19,13 +19,12 @@ ENV_VARS = {'GTEST_TOTAL_SHARDS': '%(num_instances)s',
 
 
 class Options(object):
-  def __init__(self, working_dir="swarm_tests", min_shards=1, num_shards=1,
+  def __init__(self, working_dir="swarm_tests", shards=1,
                os_image='win32', url='http://localhost:8080',
                data_url='http://www.google.com/data',
                data_dest_dir='temp_data'):
     self.working_dir = working_dir
-    self.min_shards = min_shards
-    self.num_shards = num_shards
+    self.shards = shards
     self.os_image = os_image
     self.url = url
     self.data_url = data_url
@@ -64,8 +63,7 @@ def GenerateExpectedJSON(options):
     'env_vars': ENV_VARS,
     'configurations': [
       {
-        'min_instances': 1,
-        'max_instances': 1,
+        'min_instances': options.shards,
         'config_name': platform_mapping[options.os_image],
         'dimensions': {
           'os': platform_mapping[options.os_image],
@@ -95,8 +93,9 @@ def GenerateExpectedJSON(options):
 
 class ManifestTest(unittest.TestCase):
   def test_basic_manifest(self):
-    options = Options()
-    manifest = run_slavelastic.Manifest(FILE_NAME, TEST_NAME, options)
+    options = Options(shards=2)
+    manifest = run_slavelastic.Manifest(FILE_NAME, TEST_NAME,
+                                        options.shards, options)
 
     manifest_json = json.loads(manifest.to_json())
 
@@ -109,7 +108,8 @@ class ManifestTest(unittest.TestCase):
     """
 
     options = Options(os_image='linux2')
-    manifest = run_slavelastic.Manifest(FILE_NAME, TEST_NAME, options)
+    manifest = run_slavelastic.Manifest(FILE_NAME, TEST_NAME,
+                                        options.shards, options)
 
     manifest_json = json.loads(manifest.to_json())
 

commit 4b3dc9779256908977fe0c55bdc5cd3be41565a9

Fix run_slavelastic.py that regressed in r145474.
TBR=csharp@chromium.org
BUG=
TEST=step 'trigger' shouldn't crash anymore.


Review URL: https://chromiumcodereview.appspot.com/10735006

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@145491 0039d316-1c4b-4281-b951-d872f2087c98

scripts/slave/run_slavelastic.py
@@ -137,7 +137,7 @@ class Manifest(object):
     return json.dumps(test_case)
 
 
-def ProcessManifest(filename, options):
+def ProcessManifest(filename, shards, options):
   """Process the manifest file and send off the swarm test request."""
   file_name_tail = os.path.split(filename)[1]
   test_name = os.path.splitext(file_name_tail)[0]
@@ -150,7 +150,7 @@ def ProcessManifest(filename, options):
 
   # Parses manifest file
   print "Parsing file %s..." % filename
-  manifest = Manifest(filename, test_full_name, options)
+  manifest = Manifest(filename, test_full_name, shards, options)
 
   # Zip up relevent files
   print "Zipping up files..."
@@ -242,7 +242,6 @@ def main():
   for i in range(len(options.manifest_name)):
     highest_exit_code = max(highest_exit_code,
                             ProcessManifest(options.manifest_name[i],
-                                            filename,
                                             options.shards[i],
                                             options))
 