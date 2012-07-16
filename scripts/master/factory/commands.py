15b6f3e3dd0a2598b008f0f2a4ea176427d05b7f
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 21:07:34 +0000

    When adding the apply_issue command, use -e= instead of -e '', since the windows bots don't like the empty string.
    BUG=43563
    TEST=When sending a job from rietveld, make sure that the windows jobs
    don't get stuck at the apply_issue step.
    
    Review URL: https://chromiumcodereview.appspot.com/10784023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@146882 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/commands.py
+++ scripts/master/factory/commands.py
@@ -562,13 +562,14 @@ class FactoryCommands(object):
     # TODO(rogerta): for now using an empty string for email address, so
     # that apply_issue does not prompt for an email address and password.
     # Eventually we may want to have the buildbot authenticate with some
-    # @chromium.org account.
+    # @chromium.org account.  Use the form -e= instead of -e '' since windows
+    # bots don't like the latter.
     cmd = [
       'apply_issue.py',
       '-r', WithProperties('%(root:-)s'),
       '-i', WithProperties('%(issue:-)s'),
       '-p', WithProperties('%(patchset:-)s'),
-      '-e', '',
+      '-e=',
     ]
     self._factory.addStep(
         shell.ShellCommand,
