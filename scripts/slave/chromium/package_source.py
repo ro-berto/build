49cae7a6f4049906aacd01b6912cd050e2ee4c88
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 16:16:51 +0000

    Exclude tools/perf/data from the tarball, it's 10G of uninteresting data
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10784032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@147011 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/package_source.py
+++ scripts/slave/chromium/package_source.py
@@ -155,10 +155,11 @@ def main():
                        '-regex', '^src/out/[^/]*/obj/gen/.*', '-o',
                        '!', '-regex', '^src/out/.*', ')', '-a',
                   # Exclude all .svn directories, the native client toolchain
-                  # and the llvm build directory.
+                  # and the llvm build directory, and perf/data files.
                   '!', '-regex', r'.*\.svn.*', '-a',
                   '!', '-regex', '^src/native_client/toolchain/.*', '-a',
-                  '!', '-regex', '^src/third_party/llvm-build/.*']
+                  '!', '-regex', '^src/third_party/llvm-build/.*', '-a',
+                  '!', '-regex', '^tools/perf/data/.*']
 
   try:
     if chromium_utils.RunCommand(find_command,
