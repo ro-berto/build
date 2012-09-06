54e96afa8035e9155e355a3a1c593aeb3f70bc11
loislo@chromium.org <loislo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 07:07:04 +0000

    remove unnecessary helper. It hides the logic of configuration. add two additional builders for clang and for debug.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10922003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@155138 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.devtools/master_webkit_latest_cfg.py
+++ masters/master.devtools/master_webkit_latest_cfg.py
@@ -2,43 +2,93 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from master import master_config
 from master.factory import chromium_factory
+from buildbot.scheduler import Scheduler
 
-defaults = {}
-
-helper = master_config.Helper(defaults)
-B = helper.Builder
-F = helper.Factory
-S = helper.Scheduler
 
 def linux(): return chromium_factory.ChromiumFactory('src/build', 'linux2')
 
-################################################################################
-## Release
-################################################################################
+scheduler_devtools = Scheduler(name='devtools_perf_scheduler',
+    branch='trunk',
+    treeStableTimer=60,
+    builderNames = ['x64 Release',
+                    'x64 Debug',
+                    'x64 Release Clang'])
 
-#
-# Main release scheduler for webkit
-#
-S('s6_chrome_rel', branch='src', treeStableTimer=60)
-S('s6_webkit_rel', branch='trunk', treeStableTimer=60)
-
-#
-# Linux Rel Builder
-#
-F('f_webkit_linux_rel', linux().ChromiumWebkitLatestFactory(
+factory_release = linux().ChromiumWebkitLatestFactory(
     target='Release',
     tests=['devtools_perf'],
-    options=['--compiler=goma', 'DumpRenderTree', 'pyautolib', 'chromedriver'],
+    options=['--build-tool=ninja',
+             '--compiler=goma',
+             'DumpRenderTree',
+             'chrome',
+             'pyautolib',
+             'chromedriver'],
     factory_properties={
-      'perf_id': 'chromium-devtools-perf',
+      'perf_id': 'chromium-devtools-perf-debug',
       'show_perf_results': True,
     }
   )
-)
 
-B('Webkit Linux 64', 'f_webkit_linux_rel', scheduler='s6_webkit_rel')
+builder_release = {
+  'name': 'x64 Release',
+  'builddir': 'DevTools_Release',
+  'factory': factory_release,
+  'category': 'Linux'
+}
+
+
+factory_debug = linux().ChromiumWebkitLatestFactory(
+    target='Debug',
+    tests=['devtools_perf'],
+    options=['--build-tool=ninja',
+             '--compiler=goma',
+             'DumpRenderTree',
+             'chrome',
+             'pyautolib',
+             'chromedriver'],
+    factory_properties={
+      'perf_id': 'chromium-devtools-perf-debug',
+      'show_perf_results': True,
+    }
+  )
+
+builder_debug = {
+  'name': 'x64 Debug',
+  'builddir': 'DevTools_Debug',
+  'factory': factory_debug,
+  'category': 'Linux'
+}
+
+factory_release_clang = linux().ChromiumWebkitLatestFactory(
+    target='Release',
+    tests=['devtools_perf'],
+    options=['--build-tool=ninja',
+             '--compiler=goma-clang',
+             'DumpRenderTree',
+             'chrome',
+             'pyautolib',
+             'chromedriver'],
+    factory_properties={
+      'perf_id': 'chromium-devtools-perf-clang-release',
+      'show_perf_results': True,
+      'gclient_env': {
+          'GYP_DEFINES':'clang=1 clang_use_chrome_plugins=1'
+      }
+    }
+  )
+
+builder_release_clang = {
+  'name': 'x64 Release Clang',
+  'builddir': 'DevTools_Release_Clang',
+  'factory': factory_release_clang,
+  'category': 'Linux'
+}
+
 
 def Update(config, active_master, c):
-  return helper.Update(c)
+  c["builders"] = [builder_release,
+                   builder_debug,
+                   builder_release_clang]
+  c["schedulers"] = [scheduler_devtools]
+  return c
