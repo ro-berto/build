eac50bece980c9488253cd3c64c359bc2f6322cb
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-08 02:04:17 +0000

    Add WebKit debug builder & tester
    - purpose is to mirror chromium main waterfall as closely
      as possible, so adding this tester in prep for unit tests on main waterfall
    R=peter@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10915056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@155558 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webkit/master_android_latest_cfg.py
+++ masters/master.chromium.webkit/master_android_latest_cfg.py
@@ -13,6 +13,7 @@ B = helper.Builder
 D = helper.Dependent
 F = helper.Factory
 S = helper.Scheduler
+T = helper.Triggerable
 
 def linux_android(): return chromium_factory.ChromiumFactory('',
     'linux2', nohooks_on_update=True, target_os='android')
@@ -25,17 +26,40 @@ def linux_android(): return chromium_factory.ChromiumFactory('',
 defaults['category'] = '9android latest'
 
 #
-# Main release scheduler for webkit
+# Android scheduler
 #
-S('s9_android_webkit_rel', branch='trunk', treeStableTimer=60)
+S('s9_android_webkit', branch='trunk', treeStableTimer=60)
 
+#
+# Triggerable scheduler for the builder
+#
+T('android_trigger')
+
+chromium_android_archive = master_config.GetGSUtilUrl(
+    'chromium-android', 'webkit_latest_dbg')
 #
 # Linux Rel Builder
 #
 B('Android Builder', 'f_android_rel', auto_reboot=True,
-  scheduler='s9_android_webkit_rel')
+  scheduler='s9_android_webkit')
 F('f_android_rel', linux_android().ChromiumWebkitLatestAnnotationFactory(
     annotation_script='src/build/android/buildbot/bb_webkit_latest_builder.sh'))
+
+B('Android Builder (dbg)', 'f_android_dbg', auto_reboot=True,
+  scheduler='s9_android_webkit')
+F('f_android_dbg', linux_android().ChromiumWebkitLatestAnnotationFactory(
+    target='Debug',
+    annotation_script='src/build/android/buildbot/bb_webkit_latest_builder.sh',
+    factory_properties={
+        'trigger': 'android_trigger',
+        'build_url': chromium_android_archive,
+        }))
+
+B('Android Tests (dbg)', 'f_android_dbg_tests', None, 'android_trigger')
+F('f_android_dbg_tests', linux_android().ChromiumWebkitLatestAnnotationFactory(
+    target='Debug',
+    annotation_script='src/build/android/buildbot/bb_webkit_latest_tester.sh',
+    factory_properties={'build_url': chromium_android_archive}))
 
 def Update(config, active_master, c):
   return helper.Update(c)
