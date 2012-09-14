0985aab3cf337b08ec1be9e385f38e994786eb64
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 09:39:18 +0000

    Added content_browsertests to ASAN builder.
    The reason is that we are about to deploy WebRTC tests on the clusterfuzz cluster, and we need to run content_browsertests since it alone has the ability to run WebRTC with fake devices (chrome cannot do it since we don't want to ship what's essentially test code there).
    
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911254
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@156773 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.lkgr/master_lkgr_cfg.py
+++ masters/master.chromium.lkgr/master_lkgr_cfg.py
@@ -90,7 +90,8 @@ F('linux64_full', linux().ChromiumFactory(
 B('ASAN Release', 'linux_asan_rel', 'compile', 'chromium_lkgr')
 F('linux_asan_rel', linux().ChromiumASANFactory(
     clobber=True,
-    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree'],
+    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree',
+             'content_browsertests'],
     factory_properties={
        'asan_archive_build': True,
        'gs_bucket': 'gs://chromium-browser-asan',
@@ -103,7 +104,8 @@ asan_gyp = ('asan=1 linux_use_tcmalloc=0 '
 B('ASAN Release (symbolized)', 'linux_asan_rel_sym', 'compile', 'chromium_lkgr')
 F('linux_asan_rel_sym', linux().ChromiumASANFactory(
     clobber=True,
-    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree'],
+    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree',
+             'content_browsertests'],
     factory_properties={
        'asan_archive_build': True,
        'asan_archive_name': 'asan-symbolized',
@@ -115,7 +117,8 @@ B('ASAN Debug', 'linux_asan_dbg', 'compile', 'chromium_lkgr')
 F('linux_asan_dbg', linux().ChromiumASANFactory(
     clobber=True,
     target='Debug',
-    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree'],
+    options=['--compiler=clang', 'chrome', 'dns_fuzz_stub', 'DumpRenderTree',
+             'content_browsertests'],
     factory_properties={
        'asan_archive_build': True,
        'gs_bucket': 'gs://chromium-browser-asan',
