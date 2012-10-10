263c34e5035cf0b44e4ae0440e895bb5fd4fcad9
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 01:52:56 +0000

    Add WebKit Android Latest bot
    - Moving android rel builder to WebKit android category
    - Renaming some triggers for clarity
    
    R=cmp,peter@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11076014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@161008 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webkit/master_android_latest_cfg.py
+++ masters/master.chromium.webkit/master_android_latest_cfg.py
@@ -33,32 +33,29 @@ S('s9_android_webkit', branch='trunk', treeStableTimer=60)
 #
 # Triggerable scheduler for the builder
 #
-T('android_trigger')
+T('android_dbg_trigger')
 
-chromium_android_archive = master_config.GetGSUtilUrl(
+android_dbg_archive = master_config.GetGSUtilUrl(
     'chromium-android', 'webkit_latest_dbg')
-#
-# Linux Rel Builder
-#
-B('Android Builder', 'f_android_rel', scheduler='s9_android_webkit')
-F('f_android_rel', linux_android().ChromiumWebkitLatestAnnotationFactory(
-    annotation_script='src/build/android/buildbot/bb_webkit_latest_builder.sh'))
 
+#
+# Android dbg builder
+#
 B('Android Builder (dbg)', 'f_android_dbg', scheduler='s9_android_webkit')
 F('f_android_dbg', linux_android().ChromiumWebkitLatestAnnotationFactory(
     target='Debug',
     annotation_script='src/build/android/buildbot/bb_webkit_latest_builder.sh',
     factory_properties={
-        'trigger': 'android_trigger',
-        'build_url': chromium_android_archive,
+        'trigger': 'android_dbg_trigger',
+        'build_url': android_dbg_archive,
         }))
 
-B('Android Tests (dbg)', 'f_android_dbg_tests', None, 'android_trigger',
+B('Android Tests (dbg)', 'f_android_dbg_tests', None, 'android_dbg_trigger',
   auto_reboot=False)
 F('f_android_dbg_tests', linux_android().ChromiumWebkitLatestAnnotationFactory(
     target='Debug',
     annotation_script='src/build/android/buildbot/bb_webkit_latest_tester.sh',
-    factory_properties={'build_url': chromium_android_archive}))
+    factory_properties={'build_url': android_dbg_archive}))
 
 def Update(config, active_master, c):
   return helper.Update(c)
