f191e86d962e5f99bf876c8dea270896169cb415
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 10:46:50 +0000

    Remove asan.DEPS which are used no more.
    BUG=159775
    
    Review URL: https://chromiumcodereview.appspot.com/11364146
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@166654 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_factory.py
+++ scripts/master/factory/chromium_factory.py
@@ -1157,11 +1157,6 @@ class ChromiumFactory(gclient_factory.GClientFactory):
                           mode=None, slave_type='BuilderTester', options=None,
                           compile_timeout=1200, build_url=None, project=None,
                           factory_properties=None):
-    # Make sure the solution is not already there.
-    if 'asan.DEPS' not in [s.name for s in self._solutions]:
-      self._solutions.append(gclient_factory.GClientSolution(
-          config.Master.trunk_url + '/deps/asan.DEPS',
-          'asan.DEPS'))
     return self.ChromiumFactory(target, clobber, tests, mode, slave_type,
                                 options, compile_timeout, build_url, project,
                                 factory_properties)
