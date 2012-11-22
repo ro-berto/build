7c207c61fcc3a3c37e34cd28af0984011bfbd412
mstarzinger@chromium.org <mstarzinger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-22 11:54:25 +0000

    V8 Buildbot: Make branch builders use only one target.
    This drops coverage of the 'dev' branch from the waterfall and splits
    the other branch builders so that each builder uses either the release
    or the debug target, but not both at once.
    
    Review URL: https://chromiumcodereview.appspot.com/11416140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@169256 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8/branches_cfg.py
+++ masters/master.client.v8/branches_cfg.py
@@ -6,19 +6,15 @@
 # found in the LICENSE file.
 
 # This file defines the current branches that we test on the v8 waterfall.
-# Note that stable, beta and dev might not always correspond to what is
-# used in chromium, but here we simple use it to mean the last 3 branches.
+# Note that stable and beta might not always correspond to what is used in
+# Chromium, but here we simple use it to mean the last 2 branches.
 
-stable_branch = '3.12'
-beta_branch = '3.13'
-dev_branch = '3.14'
+stable_branch = '3.13'
+beta_branch = '3.14'
 branch_names = {
     'stable': {'ia32': 'V8 Linux - ' + stable_branch + ' branch',
                'arm': 'V8 arm - sim - ' + stable_branch + ' branch',
                'x64': 'V8 Linux64 - ' + stable_branch + ' branch'},
     'beta': {'ia32': 'V8 Linux - ' + beta_branch + ' branch',
              'arm': 'V8 arm - sim - ' + beta_branch + ' branch',
-             'x64': 'V8 Linux64 - ' + beta_branch + ' branch'},
-    'dev':  {'ia32': 'V8 Linux - ' + dev_branch + ' branch',
-             'arm': 'V8 arm - sim - ' + dev_branch + ' branch',
-             'x64': 'V8 Linux64 - ' + dev_branch + ' branch'}}
+             'x64': 'V8 Linux64 - ' + beta_branch + ' branch'}}
