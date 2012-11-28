ro-berto/build

commit 912ee91356f2d577050526ff3a4a6e31311a8b3f

Gatekeeper should do better.
ChromiumNotifier doesn't look at the previous build's result.

R=csharp@chromium.org
BUG=


Review URL: https://chromiumcodereview.appspot.com/11415117

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@169269 0039d316-1c4b-4281-b951-d872f2087c98

masters/master.chromium.swarm/master.cfg
@@ -9,7 +9,7 @@ from buildbot.changes import svnpoller
 from buildbot.scheduler import Scheduler
 
 from master import build_utils
-from master import chromium_notifier
+from master import gatekeeper
 from master import chromium_step
 from master import master_utils
 from master import slaves_list
@@ -187,7 +187,7 @@ master_utils.AutoSetupMaster(c, ActiveMaster,
     enable_http_status_push=ActiveMaster.is_production_host)
 
 if ActiveMaster.is_production_host:
-  c['status'].append(chromium_notifier.ChromiumNotifier(
+  c['status'].append(gatekeeper.Gatekeeper(
         fromaddr='nobody@chromium.org',  # Reply-To address
         relayhost=config.Master.smtp,
         status_header='%(steps)s failed on "%(builder)s"',

commit 7b377d4f0d63c9b9e1cd00feedd15921d4bcca46

Do not read .status_password if tree_status_url is None in GateKeeper.
The password is not going to be used anyway.

Fix chromium.swarm that was broken by r169269.

R=szager@chromium.org
BUG=


Review URL: https://chromiumcodereview.appspot.com/11416154

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@169919 0039d316-1c4b-4281-b951-d872f2087c98

masters/master.chromium.swarm/master.cfg
@@ -187,7 +187,8 @@ master_utils.AutoSetupMaster(c, ActiveMaster,
     enable_http_status_push=ActiveMaster.is_production_host)
 
 if ActiveMaster.is_production_host:
-  c['status'].append(gatekeeper.Gatekeeper(
+  c['status'].append(gatekeeper.GateKeeper(
+        None,
         fromaddr='nobody@chromium.org',  # Reply-To address
         relayhost=config.Master.smtp,
         status_header='%(steps)s failed on "%(builder)s"',

scripts/master/gatekeeper.py
@@ -65,7 +65,9 @@ class GateKeeper(chromium_notifier.ChromiumNotifier):
         'Tree is closed (Automatic: "%(steps)s" on "%(builder)s"%(blame)s)')
     self._last_closure_revision = 0
 
-    self.password = get_password.Password('.status_password').GetPassword()
+    self.password = None
+    if tree_status_url:
+      self.password = get_password.Password('.status_password').GetPassword()
 
 
   def isInterestingStep(self, build_status, step_status, results):
@@ -173,6 +175,10 @@ class GateKeeper(chromium_notifier.ChromiumNotifier):
 
     # isInterestingStep verified that latest_revision has expected properties.
     latest_revision = build_utils.getLatestRevision(build_status)
+
+    if not self.tree_status_url:
+      self._last_closure_revision = latest_revision
+      return
 
     # Don't blame last committers if they are not to blame.
     blame_text = ''