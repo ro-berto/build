8cac0c660eee774c32b87d2511e4d2eeddf0ffe8
ricow@google.com <ricow@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 17:17:00 +0000

    Call dartium_tools/buildbot_annotated_steps.py directly, there is no need for moving this as part of the dartium patching process.
    Additionally, start calling a new script for release builds (there are none yet, but this is what will be used to build the sdk and editor)
    
    TBR=foo
    
    Review URL: https://chromiumcodereview.appspot.com/11466003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@171512 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/chromium/dart_buildbot_run.py
+++ scripts/slave/chromium/dart_buildbot_run.py
@@ -9,16 +9,21 @@ This script is called from buildbot and reports results using the buildbot
 annotation scheme.
 """
 
+import os
 import sys
 
 from common import chromium_utils
 
-
 def main():
-  return chromium_utils.RunCommand(
-      [sys.executable,
-       'src/build/buildbot_annotated_steps.py',
-      ])
+  builder_name = os.getenv('BUILDBOT_BUILDERNAME', default='')
+  is_release_bot = builder_name.startswith('release')
+  script = ''
+  if is_release_bot:
+    script = 'src/dartium_tools/buildbot_release_annotated_steps.py'
+  else:
+    script = 'src/dartium_tools/buildbot_annotated_steps.py'
+
+  return chromium_utils.RunCommand([sys.executable, script])
 
 if __name__ == '__main__':
   sys.exit(main())
