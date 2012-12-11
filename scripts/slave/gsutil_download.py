aa99a9d04f737b8719f1df5dc5ab682712c79bd8
frankf@google.com <frankf@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 21:55:57 +0000

    Add utility method for polling Google Storage for a specified file.
    BUG=164905
    Review URL: https://codereview.chromium.org/11467030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@172421 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/gsutil_download.py
+++ scripts/slave/gsutil_download.py
@@ -7,8 +7,13 @@
 
 import distutils.version
 import optparse
+import time
 
 from slave import slave_utils
+
+
+_POLL_INTERVAL_DEFAULT = 60 * 15  # 15 minutes
+_TIMEOUT_DEFAULT = 60 * 60 * 2  # 2 hours
 
 
 def DownloadLatestFile(base_url, partial_name, dst):
@@ -31,34 +36,82 @@ def DownloadLatestFile(base_url, partial_name, dst):
   files = [b.split()[2] for b in result[1].split('\n')
            if partial_name in b]
 
+  if not files:
+    raise Exception('Could not find any matching files.')
+
   files = [distutils.version.LooseVersion(x) for x in files]
   newest_file = str(max(files))
   slave_utils.GSUtilDownloadFile(newest_file, dst)
 
 
+def DownloadFileWithPolling(url, dst,
+                            poll_interval=_POLL_INTERVAL_DEFAULT,
+                            timeout=_TIMEOUT_DEFAULT):
+  """Download the archived file with given |url|.
+
+  If not found, keep trying at |poll_interval| until we reach |timeout|.
+
+  Args:
+    url: Google Storage URL (gs://...).
+    dst: Destination file/directory where the file will be downloaded.
+    poll_interval: Polling interval in seconds.
+    timeout: Timeout in seconds.
+
+  Raises:
+    Exception: If there is a timeout.
+  """
+  start_time = time.time()
+  time_passed = 0
+  while time_passed < timeout:
+    if not slave_utils.GSUtilDownloadFile(url, dst):
+      return
+    print 'Retrying in %d seconds...' % poll_interval
+    time.sleep(poll_interval)
+    time_passed = time.time() - start_time
+  raise Exception('Timed out trying to download %s' % url)
+
+
 def main():
-  desc = ('Download the file with the given base URL and partial name. '
-          'If there are multiple matching files, the newest is downloaded '
-          'assuming the full URL follows a loose versioning format.')
+  desc = ('There are 2 modes of downloading a file from Google Storage.\n'
+          'If --poll is specified, poll Google Storage for the exact |url|\n'
+          'at regular intervals until success or timeout.\n'
+          'Otherwise, download the file with the given base URL and partial\n'
+          'name. If there are multiple matching files, the newest is\n'
+          'downloaded assuming the full URL follows a loose versioning format.'
+         )
   parser = optparse.OptionParser(description=desc)
 
-  parser.add_option('--base-url',
-                    help='Google Storage base URL (gs://...) containing the '
-                         'file.')
+  parser.add_option('--url',
+                    help='Google Storage URL (gs://...).')
   parser.add_option('--partial-name',
                     help='Partial name of the file to download.')
   parser.add_option('--dst', help='Path to the destination file/directory.')
+  parser.add_option('--poll', action='store_true',
+                    help='If specified poll for the exact url.')
+  parser.add_option('--poll-interval', type=int, default=_POLL_INTERVAL_DEFAULT,
+                    help='Poll interval in seconds [default: %default].')
+  parser.add_option('--timeout', type=int, default=_TIMEOUT_DEFAULT,
+                    help='Timeout in seconds [default: %default].')
   (options, args) = parser.parse_args()
 
   if args:
     parser.error('Unknown arguments: %s.' % args)
 
-  if not (options.base_url and options.partial_name and options.dst):
+  if not (options.url and options.dst):
     parser.error('Missing one or more required arguments.')
 
-  DownloadLatestFile(base_url=options.base_url,
-                     partial_name=options.partial_name,
-                     dst=options.dst)
+  if options.poll:
+    DownloadFileWithPolling(url=options.url,
+                            dst=options.dst,
+                            poll_interval=options.poll_interval,
+                            timeout=options.timeout)
+  else:
+    if not options.partial_name:
+      parser.error('Missing --partial_name.')
+
+    DownloadLatestFile(base_url=options.url,
+                       partial_name=options.partial_name,
+                       dst=options.dst)
 
 
 if __name__ == '__main__':
