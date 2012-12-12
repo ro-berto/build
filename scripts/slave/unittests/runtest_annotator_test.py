2957b482db6f035179d15b1a6da14c26fef148d7
xusydoc@chromium.org <xusydoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 03:01:28 +0000

    Added perf expectation unittests.
    These are ported over from scripts/master/unittests/process_log_test.py
    
    Review URL: https://chromiumcodereview.appspot.com/11416256
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@172510 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/unittests/runtest_annotator_test.py
+++ scripts/slave/unittests/runtest_annotator_test.py
@@ -20,6 +20,9 @@ from slave import process_log_utils
 
 TEST_PERCENTILES = [.05, .3, .8]
 
+# From buildbot.status.builder:
+SUCCESS, WARNINGS, FAILURE, SKIPPED, EXCEPTION, RETRY = range(6)
+
 class LoggingStepBase(unittest.TestCase):
   """Logging testcases superclass.
 
@@ -39,9 +42,15 @@ class LoggingStepBase(unittest.TestCase):
     factory_properties['perf_filename'] = perf_expectations_path
     factory_properties['perf_name'] = 'test-system'
     factory_properties['test_name'] = 'test-name'
-    return log_processor_class(revision=self._revision, build_property={},
-                               factory_properties=factory_properties,
-                               webkit_revision=self._webkit_revision)
+    parser = log_processor_class(revision=self._revision, build_property={},
+                                 factory_properties=factory_properties,
+                                 webkit_revision=self._webkit_revision)
+
+    # Set custom percentiles if we're testing GraphingLogProcessor.
+    if hasattr(parser, '_percentiles'):
+      parser._percentiles = TEST_PERCENTILES
+
+    return parser
 
   def _ProcessLog(self, log_processor, logfile):  # pylint: disable=R0201
     for line in open(os.path.join(test_env.DATA_PATH, logfile)):
@@ -56,9 +65,6 @@ class LoggingStepBase(unittest.TestCase):
                                       log_processor_class, *args, **kwargs):
     parser = self._ConstructDefaultProcessor(log_processor_class, *args,
                                              **kwargs)
-    # Set custom percentiles if we're testing GraphingLogProcessor.
-    if hasattr(parser, "_percentiles"):
-      parser._percentiles = TEST_PERCENTILES
     for inputfile in inputfiles:
       self._ProcessLog(parser, inputfile)
 
@@ -107,25 +113,6 @@ class BenchpressPerformanceTestStepTest(LoggingStepBase):
 
 
 class GraphingLogProcessorTest(LoggingStepBase):
-  def _TestPerfExpectations(self, perf_expectations_file):
-    perf_expectations_path = os.path.join(
-        test_env.DATA_PATH, perf_expectations_file)
-
-    input_files = ['graphing_processor.log']
-    graphfile = 'graphs.dat'
-    output_files = [graphfile]
-
-    logs = self._ConstructParseAndCheckLogfiles(input_files, output_files,
-        process_log_utils.GraphingLogProcessor,
-        factory_properties={'expectations': True, 'perf_id': 'tester'},
-        perf_expectations_path=perf_expectations_path)
-
-    actual = json.load('\n'.join(logs[graphfile]))
-    expected = json.load(open(
-        os.path.join(test_env.DATA_PATH, 'graphing_processor-graphs.dat')))
-    self.assertEqual(expected, actual)
-    return logs
-
   def testSummary(self):
     input_files = ['graphing_processor.log']
     output_files = ['%s-summary.dat' % graph for graph in ('commit_charge',
@@ -204,6 +191,169 @@ class GraphingLogProcessorTest(LoggingStepBase):
       expected = json.load(open(os.path.join(test_env.DATA_PATH, filename)))
       self.assertEqual(actual, expected, 'Filename %s did not contain expected '
           'data.' % filename)
+
+class GraphingLogProcessorPerfTest(LoggingStepBase):
+  def _TestPerfExpectations(self, perf_expectations_file):
+    perf_expectations_path = os.path.join(
+        test_env.DATA_PATH, perf_expectations_file)
+
+    input_file = 'graphing_processor.log'
+    graph_file = 'graphs.dat'
+
+    parser = self._ConstructDefaultProcessor(
+        process_log_utils.GraphingLogProcessor,
+        factory_properties={'expectations': True, 'perf_id': 'tester'},
+        perf_expectations_path=perf_expectations_path)
+
+    self._ProcessLog(parser, input_file)
+
+    actual = json.loads('\n'.join(parser.PerformanceLogs()[graph_file]))
+    expected = json.load(open(
+        os.path.join(test_env.DATA_PATH, 'graphing_processor-graphs.dat')))
+
+    self.assertEqual(len(actual), len(expected))
+
+    for graph in expected:
+      self.assertTrue(graph['name'] in actual)
+      for element in graph:
+        self.assertEqual(actual[graph['name']][element], graph[element])
+    return parser
+
+  def testPerfExpectationsImproveRelative(self):
+    step = self._TestPerfExpectations('perf_improve_relative.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (25.00%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressRelative(self):
+    step = self._TestPerfExpectations('perf_regress_relative.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (50.00%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsImproveRelativeFloat(self):
+    step = self._TestPerfExpectations('perf_improve_relative_float.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (25.10%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsImproveRelativeFloatNonSci(self):
+    step = self._TestPerfExpectations(
+        'perf_improve_relative_float_nonscientific.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (25.10%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressRelativeFloat(self):
+    step = self._TestPerfExpectations('perf_regress_relative_float.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (49.96%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressAbsolute(self):
+    step = self._TestPerfExpectations('perf_regress_absolute.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (2.49%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsImproveAbsolute(self):
+    step = self._TestPerfExpectations('perf_improve_absolute.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (3.20%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressAbsoluteFloat(self):
+    step = self._TestPerfExpectations('perf_regress_absolute_float.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (2.55%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressAbsoluteFloatNonSci(self):
+    step = self._TestPerfExpectations(
+        'perf_regress_absolute_float_nonscientific.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (2.55%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsImproveAbsoluteFloat(self):
+    step = self._TestPerfExpectations('perf_improve_absolute_float.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (3.21%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsNochangeRelative(self):
+    step = self._TestPerfExpectations('perf_nochange_relative.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsNochangeAbsolute(self):
+    step = self._TestPerfExpectations('perf_nochange_absolute.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsNochangeRelativeFloat(self):
+    step = self._TestPerfExpectations('perf_nochange_relative_float.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsNochangeAbsoluteFloat(self):
+    step = self._TestPerfExpectations('perf_nochange_absolute_float.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterLowerSuccess(self):
+    step = self._TestPerfExpectations('perf_test_better_lower_success.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterLowerImprove(self):
+    step = self._TestPerfExpectations('perf_test_better_lower_improve.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (0.01%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterLowerRegress(self):
+    step = self._TestPerfExpectations('perf_test_better_lower_regress.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (0.01%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterHigherSuccess(self):
+    step = self._TestPerfExpectations('perf_test_better_higher_success.json')
+    expected = ('12t_cc: 50.2k')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(SUCCESS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterHigherImprove(self):
+    step = self._TestPerfExpectations('perf_test_better_higher_improve.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (0.01%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsBetterHigherRegress(self):
+    step = self._TestPerfExpectations('perf_test_better_higher_regress.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (0.01%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsRegressZero(self):
+    step = self._TestPerfExpectations(
+        'perf_test_better_lower_regress_zero.json')
+    expected = ('PERF_REGRESS: vm_final_browser/1t_vm_b (inf%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(FAILURE, step.evaluateCommand('mycommand'))
+
+  def testPerfExpectationsImproveZero(self):
+    step = self._TestPerfExpectations(
+        'perf_test_better_higher_improve_zero.json')
+    expected = ('PERF_IMPROVE: vm_final_browser/1t_vm_b (inf%)')
+    self.assertEqual(expected, step.PerformanceSummary()[0])
+    self.assertEqual(WARNINGS, step.evaluateCommand('mycommand'))
 
 if __name__ == '__main__':
   unittest.main()
