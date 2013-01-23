23573b4155d287c3bd3548948777a6bde35208ef
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 12:52:22 +0000

    Add configuration for content shell / android bot
    BUG=156214
    
    
    Review URL: https://chromiumcodereview.appspot.com/12042047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@178286 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.chromium.webkit/master_android_contentshell_latest_cfg.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+from master import master_config
+from master.factory import chromium_factory
+
+defaults = {}
+
+helper = master_config.Helper(defaults)
+B = helper.Builder
+F = helper.Factory
+S = helper.Scheduler
+
+def linux_android(): return chromium_factory.ChromiumFactory('',
+    'linux2', nohooks_on_update=True, target_os='android', full_checkout=True)
+
+
+################################################################################
+## Release
+################################################################################
+
+defaults['category'] = '9content shell'
+
+#
+# Main release scheduler for WebKit
+#
+S('s4_contentshell_webkit_rel', branch='trunk', treeStableTimer=60)
+
+#
+# Content Shell Layouttests
+#
+B('WebKit (Content Shell) Android', 'f_contentshell_android_rel',
+  scheduler='s4_contentshell_webkit_rel')
+
+F('f_contentshell_android_rel',
+  linux_android().ChromiumWebkitLatestAnnotationFactory(
+    annotation_script='src/build/android/buildbot/bb_run_bot.py',
+    factory_properties={
+        'android_bot_id': 'webkit-latest-contentshell-rel',
+        'archive_webkit_results': True,
+        'generate_gtest_json': True,
+        'test_results_server': 'test-results.appspot.com',
+        'additional_expectations_files': [
+            ['content', 'shell', 'layout_tests', 'TestExpectations' ],
+        ],
+        'additional_drt_flag': '--dump-render-tree',
+        'driver_name': 'content_shell',
+        }))
+
+def Update(config, active_master, c):
+  return helper.Update(c)
