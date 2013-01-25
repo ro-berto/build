5f72174a54af65b76d452afbf7428a7e12731bae
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 09:04:33 +0000

    Run Chrome Endure with automatic annotation.
    BUG=chromium-os:32302
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/12036079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@178786 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_commands.py
+++ scripts/master/factory/chromium_commands.py
@@ -960,15 +960,29 @@ class ChromiumCommands(commands.FactoryCommands):
       env['ENDURE_NO_WPR'] = '1'
 
     for pyauto_test_name in pyauto_test_list:
-      pyauto_cmd = ['python', pyauto_script, '-v']
-      if factory_properties.get('use_xvfb_on_linux'):
-        # Run through runtest.py on linux to launch virtual x server.
-        pyauto_cmd = self.GetTestCommand('/usr/bin/python',
-                                         [pyauto_script, '-v'])
+      tool_opts = []
+      if not factory_properties.get('use_xvfb_on_linux'):
+        tool_opts = ['--no-xvfb']
+
+      test_name = test_class_name.replace('_', '-')
+      if test_class_name.endswith('_test'):
+        test_name = test_name[0:-5]
+
+      pyauto_cmd = self.GetAnnotatedPerfCmd(
+          gtest_filter=None,
+          log_type='endure',
+          test_name=test_name,
+          cmd_name='/usr/bin/python',
+          tool_opts=tool_opts,
+          options=[pyauto_script, '-v'],
+          factory_properties=factory_properties,
+          py_script=False)
+
       pyauto_cmd.append(pyauto_test_name)
       test_step_name = (test_class_name + ' ' +
                         pyauto_test_name[pyauto_test_name.rfind('.') + 1:])
-      self.AddTestStep(retcode_command.ReturnCodeCommand,
+
+      self.AddTestStep(chromium_step.AnnotatedCommand,
                        test_step_name,
                        pyauto_cmd,
                        env=env,
