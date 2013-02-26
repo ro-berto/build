70d6a3c2d97de862e9f678745d0763b14d385c18
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 02:53:13 +0000

    Add infrastructure for AnnotatedFactory
    The CL puts in place infrastructure for a fully-annotated factory. This
    consists of a factory class that serves the same purpose as, say,
    GClientFactory; a commands class to provide various forms of annotated commands
    to the factory (currently only an "execute this script" command is implemented);
    and support scripts in scripts/slave to actually execute the specified script.
    
    It also portends a change in information location. In particular, the
    factory_properties that used to be specified in master.cfg can now be specified
    in "recipe" files (generally found in scripts/slave/recipes). This allows
    factory_properties changes to be made on the slave without requiring a master
    restart.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12320028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@184563 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/master/factory/annotator_factory.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utility class to generate and manage a factory to be passed to a
+builder dictionary as the 'factory' member, for each builder in c['builders'].
+
+Specifically creates a basic factory that will execute an arbirary annotator
+script.
+"""
+
+from master.factory import commands
+from master.factory.build_factory import BuildFactory
+
+
+class AnnotatorFactory(object):
+  """Encapsulates data and methods common to all annotators."""
+
+  def __init__(self):
+    self._factory_properties = None
+
+  def BaseFactory(self, build_properties=None, factory_properties=None):
+    self._factory_properties = factory_properties
+    factory = BuildFactory(build_properties)
+    cmd_obj = commands.AnnotatorCommands(factory)
+    cmd_obj.AddAnnotatedScript(factory_properties)
