dc337e1b0d995f208bd358fb11e9b89d81c082c4
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 01:00:32 +0000

    Fix archive_master_logs script
    - Accidentally committed a version without changes from review.
    - Also fix lkgr log path to include 'masters'
    
    TBR=xusydoc@chromium.org, agable@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12450010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@186834 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/archive_master_logs.py
+++ scripts/master/archive_master_logs.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Upload buildbot logfiles to google storage."""
+
 import datetime
 import glob
 import os
@@ -15,26 +17,33 @@ BUILD_DIR = os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.pardir, os.pardir))
 
 
-def GSUtil(from_path, to_path):
-  print from_path, to_path
-  subprocess.call([
+def GSUtilCopy(from_path, to_path):
+  """Upload a file to Google Storage."""
+  # -z <ext> instructs gsutil to gzip files with that extension
+  # and add 'Content-Encoding: gzip' to header.
+  # gsutil determines file extension by looking a last segment
+  # after spliting path on '.'.
+  return subprocess.call([
       os.path.join(BUILD_DIR, 'third_party', 'gsutil', 'gsutil'),
       'cp', '-z', from_path.split('.')[-1], from_path,
       'gs://chromium-logs/%s/%s/%s' % (HOSTNAME, TIMESTAMP, to_path)])
 
 
 def main():
+  returncode = 0
   # See also: crbug.com/177922
-  lkgr_path = os.path.join(BUILD_DIR, 'master.chromium.lkgr', 'lkgr_finder.log')
+  lkgr_path = os.path.join(
+      BUILD_DIR, 'masters', 'master.chromium.lkgr', 'lkgr_finder.log')
   if os.path.isfile(lkgr_path):
-    GSUtil(lkgr_path, 'lkgr.log')
-  for logpath in glob.glob(os.path.join(BUILD_DIR, 'masters', '*',
-                                        'actions.log')):
+    returncode |= GSUtilCopy(lkgr_path, 'lkgr.log')
+  for logpath in glob.glob(
+      os.path.join(BUILD_DIR, 'masters', '*', 'actions.log')):
     master = logpath.split(os.sep)[-2]
-    print master
     if master.startswith('master.'):
       master = master[7:]
-    GSUtil(logpath, '%s-actions.log' % master)
+    returncode |= GSUtilCopy(logpath, '%s-actions.log' % master)
+  return returncode
+
 
 if __name__ == '__main__':
-  main()
+  exit(main())
