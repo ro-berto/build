1eb24a10fc9f0b658fb81c54a6e294ff2269501d
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 08:26:18 +0000

    Parallelize master_cfg_test
    On Z620: 20s -> 2s.
    
    Review URL: https://codereview.chromium.org/13926017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@194544 0039d316-1c4b-4281-b951-d872f2087c98

--- test/masters_cfg_test.py
+++ test/masters_cfg_test.py
@@ -5,6 +5,7 @@
 
 """Tests all master.cfgs to make sure they load properly."""
 
+import multiprocessing
 import os
 import sys
 
@@ -19,49 +20,47 @@ from common import master_cfg_utils
 
 # Masters which do not currently load from the default configuration. These need
 # to be fixed and removed from the list!
-BLACKLIST = ['chromium.swarm',
-             'chromium.chromebot',
-             'chromiumos.tryserver',
-             'client.nacl.chrome']
+BLACKLIST = set(['chromium.swarm',
+                 'chromium.chromebot',
+                 'chromiumos.tryserver',
+                 'client.nacl.chrome'])
 
 
-def TestMaster(mastername):
+def TestMaster(args):
+  mastername, path = args
+  apply_issue_pwd_path = os.path.join(path, '.apply_issue_password')
+  with masters_util.temporary_password(apply_issue_pwd_path):
+    cmd = [sys.executable,
+           os.path.join(BASE_DIR, 'scripts', 'slave', 'runbuild.py'),
+           mastername,
+           '--test-config']
+    env = os.environ.copy()
+    buildpaths = ['scripts', 'third_party', 'site_config']
+    thirdpartypaths = ['buildbot_8_4p1', 'buildbot_slave_8_4', 'jinja2',
+                       'mock-1.0.1', 'twisted_10_2']
 
-  cmd = [sys.executable,
-         os.path.join(BASE_DIR, 'scripts', 'slave', 'runbuild.py'),
-         mastername,
-         '--test-config']
-  env = os.environ.copy()
-  buildpaths = ['scripts', 'third_party', 'site_config']
-  thirdpartypaths = ['buildbot_8_4p1', 'buildbot_slave_8_4', 'jinja2',
-                     'mock-1.0.1', 'twisted_10_2']
+    pythonpaths = [os.path.join(BASE_DIR, p) for p in buildpaths]
+    pythonpaths.extend(os.path.join(BASE_DIR, 'third_party', p)
+                       for p in thirdpartypaths)
+    if env.get('PYTHONPATH'):
+      pythonpaths.append(env.get('PYTHONPATH'))
 
-  pythonpaths = [os.path.join(BASE_DIR, p) for p in buildpaths]
-  pythonpaths.extend(os.path.join(BASE_DIR, 'third_party', p)
-                     for p in thirdpartypaths)
-  if env.get('PYTHONPATH'):
-    pythonpaths.append(env.get('PYTHONPATH'))
-
-  env['PYTHONPATH'] = os.pathsep.join(pythonpaths)
-  return chromium_utils.RunCommand(cmd, print_cmd=False, env=env)
+    env['PYTHONPATH'] = os.pathsep.join(pythonpaths)
+    return chromium_utils.RunCommand(cmd, print_cmd=False, env=env)
 
 
 def main():
   masters = master_cfg_utils.GetMasters(include_internal=False)
   failures = []
+  pool = multiprocessing.Pool()
   bot_pwd_path = os.path.join(BASE_DIR, 'site_config', '.bot_password')
+  assert BLACKLIST <= set(m for m, _ in masters)
+  for master in BLACKLIST:
+    print 'Skipping %s, fix and enable in masters_cfg_test.py!' % master
   with masters_util.temporary_password(bot_pwd_path):
-    for mastername, path in masters:
-      if mastername in BLACKLIST:
-        print 'Skipping %s, fix and enable in masters_cfg_test.py!' % mastername
-        continue
-
-      print 'Parsing', mastername
-
-      apply_issue_pwd_path = os.path.join(path, '.apply_issue_password')
-      with masters_util.temporary_password(apply_issue_pwd_path):
-        if TestMaster(mastername):
-          failures.append((mastername, path))
+    masters_to_test = [mpair for mpair in masters if mpair[0] not in BLACKLIST]
+    results = pool.map_async(TestMaster, masters_to_test).get(999)
+    failures = [mpair for mpair, r in zip(masters_to_test, results) if r]
 
   if failures:
     print 'The following master.cfgs did not load:'
