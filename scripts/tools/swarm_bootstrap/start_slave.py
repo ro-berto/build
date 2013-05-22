3824ce3ae06bba8e8d51313b04f149ef76af17ee
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 15:58:58 +0000

    Add start_slave.py to swarm_bootstrap files
    This makes it easy for the c4 slaves to have this file.
    
    R=hinoka@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/15293012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@201544 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/tools/swarm_bootstrap/start_slave.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import slave_machine  # pylint: disable-msg=F0401
+
+
+if __name__ == '__main__':
+  slave_machine.Restart()
