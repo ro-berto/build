05980e8cf1defeb414e4af7b656bc269c78ea628
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 18:59:30 +0000

    Fix cwd for polymer npm.
    TBR=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/15921002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@201842 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/polymer.py
+++ scripts/slave/recipes/polymer.py
@@ -9,7 +9,8 @@ def GetSteps(api, _factory_properties, build_properties):
   return (
     steps.git_checkout(build_properties['repository'] + '.git', recursive=True),
     steps.step('mktmp', ['mkdir', tmp_path]),
-    steps.step('update-install', ['npm', 'install', '--tmp', tmp_path]),
+    steps.step('update-install', ['npm', 'install', '--tmp', tmp_path],
+               cwd=api.checkout_path()),
     steps.step('test', test_prefix+['grunt', 'test-buildbot'],
                cwd=api.checkout_path())
   )
