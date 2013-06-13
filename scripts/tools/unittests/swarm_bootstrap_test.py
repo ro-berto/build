ro-berto/build

commit 06125b8183950294d4ca4e757044c1201094de53

Replace vlan starting with 'c' to 'm'.
From a task scheduling purpose, they can be considered the same.

R=csharp@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/16548003

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@204486 0039d316-1c4b-4281-b951-d872f2087c98

scripts/tools/swarm_bootstrap/swarm_bootstrap.py
@@ -82,6 +82,10 @@ def GetChromiumDimensions():
   # '<host>-<vlan>'.
   if '-' in hostname:
     dimensions['dimensions']['vlan'] = hostname.split('-')[1]
+    # Replace vlan starting with 'c' to 'm'.
+    if dimensions['dimensions']['vlan'][0] == 'c':
+      dimensions['dimensions']['vlan'] = (
+          'm' + dimensions['dimensions']['vlan'][:1])
   return dimensions
 
 

commit 13080fed7aa90b19f0e6b976afcbe47b058184f6

Fix error introduced in r204486.
Properly switch vlan names so 'c' and 'm' are synonyms.

Add a unit test!

R=csharp@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/16922008

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@206069 0039d316-1c4b-4281-b951-d872f2087c98

scripts/tools/swarm_bootstrap/swarm_bootstrap.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # A mapping between sys.platform values and the corresponding swarm name
 # for that platform.
 PLATFORM_MAPPING = {
-    'darwin': 'Mac',
-    'cygwin': 'Windows',
-    'linux2': 'Linux',
-    'win32': 'Windows'
+  'cygwin': 'Windows',
+  'darwin': 'Mac',
+  'linux2': 'Linux',
+  'win32': 'Windows',
 }
 
 
@@ -51,7 +51,7 @@ def WriteToFile(filepath, content):
     return False
 
 
-def GetDimensions():
+def GetDimensions(hostname, platform):
   """Returns a dictionary of attributes representing this machine.
 
   Returns:
@@ -62,18 +62,17 @@ def GetDimensions():
                   'generate dimensions', sys.platform)
     return {}
 
-  hostname = socket.gethostname().lower().split('.', 1)[0]
   return {
     'dimensions': {
-      'os': PLATFORM_MAPPING[sys.platform],
+      'os': PLATFORM_MAPPING[platform],
     },
     'tag': hostname,
   }
 
 
-def GetChromiumDimensions():
+def GetChromiumDimensions(hostname, platform):
   """Returns chromium infrastructure specific dimensions."""
-  dimensions = GetDimensions()
+  dimensions = GetDimensions(hostname, platform)
   if not dimensions:
     return dimensions
 
@@ -81,11 +80,11 @@ def GetChromiumDimensions():
   # Get the vlan of this machine from the hostname when it's in the form
   # '<host>-<vlan>'.
   if '-' in hostname:
-    dimensions['dimensions']['vlan'] = hostname.split('-')[1]
+    dimensions['dimensions']['vlan'] = hostname.split('-')[-1]
     # Replace vlan starting with 'c' to 'm'.
     if dimensions['dimensions']['vlan'][0] == 'c':
       dimensions['dimensions']['vlan'] = (
-          'm' + dimensions['dimensions']['vlan'][:1])
+          'm' + dimensions['dimensions']['vlan'][1:])
   return dimensions
 
 
@@ -249,7 +248,9 @@ def main():
   options.dimensionsfile = os.path.abspath(options.dimensionsfile)
 
   print('Generating the machine dimensions...')
-  if not WriteJsonToFile(options.dimensionsfile, GetChromiumDimensions()):
+  hostname = socket.gethostname().lower().split('.', 1)[0]
+  dimensions = GetChromiumDimensions(hostname, sys.platform)
+  if not WriteJsonToFile(options.dimensionsfile, dimensions):
     return 1
 
   print('Downloading newest swarm_bot code...')

scripts/tools/unittests/swarm_bootstrap_test.py
@@ -1,0 +1,35 @@
+#!/usr/bin/env python
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import unittest
+
+SWARM_BOOTSTRAP_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'swarm_bootstrap')
+sys.path.insert(0, SWARM_BOOTSTRAP_DIR)
+
+import swarm_bootstrap
+
+
+class SwarmBootTest(unittest.TestCase):
+  def test_dimensions(self):
+    actual = swarm_bootstrap.GetChromiumDimensions('s33-c4', 'darwin')
+    expected = {'dimensions': {'os': 'Mac', 'vlan': 'm4'}, 'tag': 's33-c4'}
+    self.assertEqual(expected, actual)
+
+    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m4', 'linux2')
+    expected = {'dimensions': {'os': 'Linux', 'vlan': 'm4'}, 'tag': 'vm1-m4'}
+    self.assertEqual(expected, actual)
+
+    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m1', 'win32')
+    expected = {'dimensions': {'os': 'Windows', 'vlan': 'm1'}, 'tag': 'vm1-m1'}
+    self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+  unittest.TestCase.maxDiff = None
+  unittest.main()