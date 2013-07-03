04258a9693bab834e7e987265163438ae9c489b0
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 17:33:12 +0000

    Update polymer recipe for flat repo layout.
    R=iannucci@chromium.org,mmcnulty@chromium.org
    BUG=chromium:248603
    
    Review URL: https://chromiumcodereview.appspot.com/17749003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@210002 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/polymer.py
+++ scripts/slave/recipes/polymer.py
@@ -2,18 +2,59 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
 DEPS = [
+  'gclient',
   'git',
   'path',
   'platform',
   'properties',
+  'python',
   'step',
 ]
 
-def GenSteps(api):
-  yield api.git.checkout(api.properties['repository'] + '.git', recursive=True)
 
-  tmp_path = api.path.checkout('.tmp')
+REPOS = (
+  'CustomElements',
+  'HTMLImports',
+  'PointerEvents',
+  'PointerGestures',
+  'ShadowDOM',
+  'mdv',
+  'platform',
+  'polymer',
+)
+
+
+def _CheckoutSteps(api):
+  repo_url = api.properties['repository']
+  url_base = 'https://github.com/Polymer/'
+  assert repo_url.startswith(url_base)
+  repo = repo_url[len(url_base):]
+
+  api.path.set_checkout(api.path.slave_build(repo))
+
+  cfg = api.gclient.make_config()
+  for name in REPOS:
+    soln = cfg.solutions.add()
+    soln.name = name
+    soln.url = url_base + name + '.git'
+    soln.deps_file = ''
+
+  submodule_command = api.python(
+      'submodule update', api.path.depot_tools('gclient.py'),
+      ['recurse', 'git', 'submodule', 'update', '--init', '--recursive'])
+
+  return (
+      api.gclient.checkout(cfg),
+      submodule_command
+  )
+
+
+def GenSteps(api):
+  yield _CheckoutSteps(api)
+
+  tmp_path = api.path.slave_build('.tmp')
   yield api.step('mktmp', ['mkdir', tmp_path])
   yield api.step('update-install', ['npm', 'install', '--tmp', tmp_path],
              cwd=api.path.checkout())
@@ -24,7 +65,7 @@ def GenSteps(api):
 
 
 def GenTests(api):
-  yield 'basic', {
+  yield 'polymer', {
     'properties': api.properties_scheduled(
         repository='https://github.com/Polymer/polymer'),
   }
