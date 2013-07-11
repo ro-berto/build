0186fcff7d04d274aab8208d24994f09733ddc7e
xusydoc@chromium.org <xusydoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 22:49:52 +0000

    Clean up runit.py, remove runit.sh.
    Review URL: https://chromiumcodereview.appspot.com/18682005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@211238 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/tools/runit.py
+++ scripts/tools/runit.py
@@ -1,20 +1,40 @@
+#!/usr/bin/env python
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
+"""Runs a command with PYTHONPATH set up for the Chromium build setup.
+
+This is helpful for running scripts locally on a development machine.
+
+Try `scripts/common/runit.py python`
+or  (in scripts/slave): `../common/runit.sh runtest.py --help`
+"""
+
+import optparse
 import os
 import subprocess
+import sys
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 BUILD_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..', '..'))
 
-def main(argv):
-  path = os.environ.get('PYTHONPATH', '')
-  path = path.split(':')
+USAGE = '%s [options] <command to run>' % os.path.basename(sys.argv[0])
+
+def main():
+  option_parser = optparse.OptionParser(usage=USAGE)
+  option_parser.add_option('-s', '--show-path', action='store_true',
+                           help='display new PYTHONPATH before running command')
+  option_parser.disable_interspersed_args()
+  options, args = option_parser.parse_args()
+  if not args:
+    option_parser.error('Must provide a command to run.')
+
+  path = os.environ.get('PYTHONPATH', '').split(os.pathsep)
+
   def add(new_path):
     if new_path not in path:
-      path.append(new_path)
+      path.insert(0, new_path)
 
   third_party = os.path.join(BUILD_DIR, 'third_party')
   for d in os.listdir(third_party):
@@ -27,13 +47,15 @@ def main(argv):
   add(os.path.join(BUILD_DIR, '..', 'build_internal', 'site_config'))
   add('.')
   os.environ['PYTHONPATH'] = os.pathsep.join(path)
-  print 'Set PYTHONPATH: %s' % os.environ['PYTHONPATH']
+
+  if options.show_path:
+    print 'Set PYTHONPATH: %s' % os.environ['PYTHONPATH']
 
   # Use subprocess instead of execv because otherwise windows destroys quoting.
-  p = subprocess.Popen(argv[1:])
+  p = subprocess.Popen(args)
   p.wait()
   return p.returncode
 
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv))
+  sys.exit(main())
