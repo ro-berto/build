0a097271ae749b3fa30bb481dc19dffe89d805a8
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-16 18:43:27 +0000

    Log a warning if not enough swarm test keys are found.
    R=maruel@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19284014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@211819 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/get_swarm_results.py
+++ scripts/slave/get_swarm_results.py
@@ -119,8 +119,13 @@ def main():
       options.url, test_name, options.timeout)
   if not test_keys:
     parser.error('No test keys to get results with.')
+
   if options.shards == -1:
     options.shards = len(test_keys)
+  elif len(test_keys) < options.shard:
+    print >> sys.stderr, ('Warning: Test should have %d shards, but only %d '
+                          'test keys were found' % (options.shard,
+                                                    len(test_keys)))
 
   return GetSwarmResults(options.url, test_keys, options.timeout, None)
 
