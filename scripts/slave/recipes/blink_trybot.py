d0665f38c81dc5cea32b4632391a745dce02ad1c
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 01:30:04 +0000

    Add step_text annotations to blink trybots.
    R=agable@chromium.org, dpranke@chromium.org
    BUG=266188
    
    Review URL: https://chromiumcodereview.appspot.com/21720007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@215757 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/blink_trybot.py
+++ scripts/slave/recipes/blink_trybot.py
@@ -14,6 +14,42 @@ DEPS = [
   'step_history',
 ]
 
+
+def html_results(*name_vals):
+  ret = ''
+  for name, val in name_vals:
+    if val:
+      ret += '<br/>%s:<br/>' % name
+    for test in val:
+      ret += test + '<br/>'
+  return ret
+
+
+def followup_fn(step_result):
+  r = step_result.json.results
+  p = step_result.presentation
+
+  p.step_text += html_results(
+    ('unexpected_failures', r.unexpected_failures),
+    ('failures', r.failures)
+  )
+
+  p.step_text += '<br/>Total executed: %s' % r.num_passes
+
+  if step_result.retcode > 0:
+    p.status = 'WARNING'
+
+
+def summarize_failures(ignored, new):
+  def summarize_failures_inner(step_result):
+    p = step_result.presentation
+    p.step_text += html_results(
+      ('new', new),
+      ('ignored', ignored),
+    )
+  return summarize_failures_inner
+
+
 def GenSteps(api):
   api.chromium.set_config('blink')
   api.chromium.apply_config('trybot_flavor')
@@ -28,17 +64,13 @@ def GenSteps(api):
 
 
   def BlinkTestsStep(with_patch):
-    def followup_fn(step_result):
-      if step_result.retcode > 0:
-        step_result.presentation.status = 'WARNING'
-
     name = 'webkit_tests (with%s patch)' % ('' if with_patch else 'out')
     test = api.path.build('scripts', 'slave', 'chromium',
                           'layout_test_wrapper.py')
     args = ['--target', api.chromium.c.BUILD_CONFIG,
             '-o', api.path.slave_build('layout-test-results'),
             '--build-dir', api.path.checkout(api.chromium.c.build_dir),
-            api.json.output()]
+            api.json.results()]
     return api.chromium.runtests(test, args, name=name, can_fail_build=False,
                                  followup_fn=followup_fn)
 
@@ -64,7 +96,7 @@ def GenSteps(api):
     yield api.python.inline('webkit_tests', 'print "ALL IS WELL"')
     return
 
-  failing_tests = api.step_history.last_step().json.output
+  with_patch = api.step_history.last_step().json.results
 
   yield (
     api.gclient.revert(),
@@ -72,32 +104,85 @@ def GenSteps(api):
     api.chromium.compile(),
     BlinkTestsStep(with_patch=False),
   )
-  base_failing_tests = api.step_history.last_step().json.output
+  clean = api.step_history.last_step().json.results
 
-  yield api.python(
+  ignored_failures = set(clean.unexpected_failures)
+  new_failures = (set(with_patch.unexpected_failures) -
+                  ignored_failures)
+
+  ignored_failures = sorted(ignored_failures)
+  new_failures = sorted(new_failures)
+
+  yield api.python.inline(
     'webkit_tests',
-    api.path.checkout('third_party', 'WebKit', 'Tools', 'Scripts',
-                      'print-json-test-results'),
-    ['--ignored-failures-path', api.json.input(base_failing_tests),
-      api.json.input(failing_tests)]
+    r"""
+    import sys, json
+    failures = json.load(open(sys.argv[1], 'rb'))
+
+    if failures['new']:
+      print 'New failures:'
+      print '\n'.join(failures['new'])
+    if failures['ignored']:
+      print 'Ignored failures:'
+      print '\n'.join(failures['ignored'])
+
+    sys.exit(bool(failures['new']))
+    """,
+    args=[
+      api.json.input({
+        'new': new_failures,
+        'ignored': ignored_failures
+      })
+    ],
+    followup_fn=summarize_failures(ignored_failures, new_failures)
   )
 
 
 def GenTests(api):
-  SUCCESS_DATA = lambda: {}
-
-  FAIL_DATA = lambda: {
-    'webkit_tests (with patch)': {
-      'json': {'output': {'crazy': ['data', 'format']}},
-      '$R': 1
+  TEST_OUTPUT = lambda good: {
+    "tests": {
+      "good": {
+        "totally-awesome.html": {
+          "expected": "PASS",
+          "actual": "PASS",
+        }
+      },
+      "flake": {
+        "totally-flakey.html": {
+          "expected": "PASS",
+          "actual": "TIMEOUT PASS",
+          "is_unexpected": True,
+        }
+      },
+      "tricky": {
+        "totally-maybe-not-awesome.html": {
+          "expected": "PASS",
+          "actual": "PASS" if good else "FAIL",
+          "is_unexpected": True,
+        }
+      },
+      "bad": {
+        "totally-bad-probably.html": {
+          "expected": "PASS",
+          "actual": "PASS" if good else "FAIL",
+        }
+      }
     },
-    'webkit_tests (without patch)': {
-      'json': {'output': {'crazy': ['data', 'format']}},
-      '$R': 1
-    },
+    "num_passes": 9001,
   }
 
-  for result, step_mocks in [('success', SUCCESS_DATA), ('fail', FAIL_DATA)]:
+  DATA = lambda good: dict((
+    ('webkit_tests (with patch)', {
+      'json': {'results': TEST_OUTPUT(good) },
+      '$R': 0 if good else 1
+    }),)+((
+    ('webkit_tests (without patch)', {
+      'json': {'results': TEST_OUTPUT(good) },
+      '$R': 1
+    }),) if not good else ()),
+  )
+
+  for result, good in [('success', True), ('fail', False)]:
     for build_config in ['Release', 'Debug']:
       for plat in ('win', 'mac', 'linux'):
         for git_mode in True, False:
@@ -109,7 +194,7 @@ def GenTests(api):
               root='src/third_party/WebKit',
               GIT_MODE=git_mode,
             ),
-            'step_mocks': step_mocks(),
+            'step_mocks': DATA(good),
             'mock': {
               'platform': {
                 'name': plat
