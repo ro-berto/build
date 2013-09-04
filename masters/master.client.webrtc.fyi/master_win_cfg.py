c4df9ffd6626b964a8790ad1b946990c165aafb4
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-04 07:22:28 +0000

    Add Dr Memory and TSan Windows bots to WebRTC FYI master
    This CL is finally possible after a lot of work running tests,
    creating suppressions and filing issues for
    http://review.webrtc.org/2149004/
    
    It will be nice to get some memory leak and race condition
    protection in place for our Windows-specific code!
    
    BUG=webrtc:2318
    TEST=a large number of runs with local master and slave.
    
    Review URL: https://chromiumcodereview.appspot.com/23451008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@221157 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.client.webrtc.fyi/master_win_cfg.py
@@ -0,0 +1,78 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from master import master_config
+from master.factory import webrtc_factory
+
+defaults = {}
+
+
+def win():
+  return webrtc_factory.WebRTCFactory('src/out', 'win32')
+
+helper = master_config.Helper(defaults)
+B = helper.Builder
+F = helper.Factory
+S = helper.Scheduler
+
+scheduler = 'webrtc_win_scheduler'
+S(scheduler, branch='trunk', treeStableTimer=0)
+
+tests = [
+    'audio_decoder_unittests',
+    'common_audio_unittests',
+    'common_video_unittests',
+    'metrics_unittests',
+    'modules_tests',
+    'modules_unittests',
+    'neteq_unittests',
+    'system_wrappers_unittests',
+    'test_support_unittests',
+    'tools_unittests',
+    'video_engine_core_unittests',
+    'voice_engine_unittests',
+]
+
+ninja_options = ['--build-tool=ninja']
+dr_memory_factory_properties = {
+   'gclient_env': {'GYP_DEFINES': 'build_for_tool=drmemory'},
+   'needs_drmemory': True,
+}
+
+defaults['category'] = 'win'
+
+B('Win DrMemory Light', 'win_drmemory_light_factory', scheduler=scheduler)
+F('win_drmemory_light_factory', win().WebRTCFactory(
+    target='Debug',
+    options=ninja_options,
+    tests=['drmemory_light_' + test for test in tests],
+    factory_properties=dr_memory_factory_properties))
+
+B('Win DrMemory Full', 'win_drmemory_full_factory', scheduler=scheduler)
+F('win_drmemory_full_factory', win().WebRTCFactory(
+    target='Debug',
+    options=ninja_options,
+    tests=['drmemory_full_' + test for test in tests],
+    factory_properties=dr_memory_factory_properties))
+
+B('Win DrMemory Pattern', 'win_drmemory_pattern_factory', scheduler=scheduler)
+F('win_drmemory_pattern_factory', win().WebRTCFactory(
+    target='Debug',
+    options=ninja_options,
+    tests=['drmemory_pattern_' + test for test in tests],
+    factory_properties=dr_memory_factory_properties))
+
+B('Win Tsan', 'win_tsan_factory', scheduler=scheduler)
+F('win_tsan_factory', win().WebRTCFactory(
+    target='Debug',
+    options=ninja_options,
+    tests=['tsan_' + test for test in tests],
+      factory_properties={
+          'needs_tsan_win': True,
+          'gclient_env': { 'GYP_DEFINES' : 'build_for_tool=tsan' },
+      }))
+
+
+def Update(c):
+  helper.Update(c)
