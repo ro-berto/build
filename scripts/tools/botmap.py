1eaf1cd4ba84c695a8648c57e63bb76bebe53f94
petermayo@chromium.org <petermayo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-06 02:23:21 +0000

    Add a tool to list slave names by criteria.
    Works by reading the current checkout.
    
    R=davidjames@chromium.org, xusydoc@chromium.org
    BUG=None
    TEST=local
    
    Review URL: https://chromiumcodereview.appspot.com/23474003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@221561 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/tools/botmap.py
+++ scripts/tools/botmap.py
@@ -9,32 +9,12 @@ import os
 import sys
 path = os.path.join(os.path.dirname(__file__), os.path.pardir)
 sys.path.append(path)
-from common import chromium_utils
+
+from tools import list_slaves
 
 
 def main():
-  # remove slaves with blank or no hostname
-  slaves = filter(lambda x: x.get('hostname'), chromium_utils.GetAllSlaves())
-  slaves.sort(key=lambda x: (x.get('mastername'), x['hostname']))
-  for slave in slaves:
-    if slave.get('os') == 'win':
-      pathsep = '\\'
-    else:
-      pathsep = '/'
-    if 'subdir' in slave:
-      slavedir = pathsep + 'c' + pathsep + slave['subdir']
-    else:
-      slavedir = pathsep + 'b'
-    builder = slave.get('builder') or '?'
-    if type(builder) is not list:
-      builder = [builder]
-    for b in sorted(builder):
-      print '%-30s %-20s %-35s %-35s %-10s' % (
-          slave['hostname'],
-          slavedir,
-          slave.get('mastername', '?'),
-          b,
-          slave.get('os', '?'))
+  list_slaves.Main(['--botmap', '-x', 'all'])
 
 
 if __name__ == '__main__':
