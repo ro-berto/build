5d6b9d736cb6cd406809a8760644b5ecdb6ccec8
sukolsak@chromium.org <sukolsak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 13:07:43 +0000

    Pass the --force-clean option to the mini_installer test framework.
    Pass the --force-clean option to the mini_installer test framework so that it uninstalls existing Chrome or Chromium before running the tests.
    
    BUG=264859
    
    Review URL: https://chromiumcodereview.appspot.com/23546012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@223601 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_commands.py
+++ scripts/master/factory/chromium_commands.py
@@ -1606,7 +1606,7 @@ class ChromiumCommands(commands.FactoryCommands):
   def AddMiniInstallerTestStep(self, factory_properties):
     cmd = [self._python, self._mini_installer_tests_runner,
            self._mini_installer_tests_config, '--build-dir', self._build_dir,
-           '--target', self._target]
+           '--target', self._target, '--force-clean']
     self.AddTestStep(chromium_step.AnnotatedCommand, 'test_mini_installer', cmd,
                      halt_on_failure=True, timeout=600,
                      do_step_if=self.TestStepFilter)
