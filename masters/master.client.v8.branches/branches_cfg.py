1fae9acfb9e92112adfffc631dd3ddcdf5d6d70b
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 13:54:39 +0000

    V8 Buildbot: Upgrading stable and beta branch versions to 3.20/21.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/24366002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@224712 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8.branches/branches_cfg.py
+++ masters/master.client.v8.branches/branches_cfg.py
@@ -9,8 +9,8 @@
 # Note that stable and beta might not always correspond to what is used in
 # Chromium, but here we simple use it to mean the last 2 branches.
 
-stable_branch = '3.19'
-beta_branch = '3.20'
+stable_branch = '3.20'
+beta_branch = '3.21'
 branch_names = {
     'stable': {'ia32': 'V8 Linux - ' + stable_branch + ' branch',
                'arm': 'V8 arm - sim - ' + stable_branch + ' branch',
