a1afb1ab432c718bd058ff7d5500581c0bef50e3
hinoka@chromium.org <hinoka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 20:43:14 +0000

    Make Linux Clang (dbg) a tree closer on test failures
    Also changed it to be kwargs for readability.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/24822002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@225554 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.linux/master_linux_cfg.py
+++ masters/master.chromium.linux/master_linux_cfg.py
@@ -424,8 +424,11 @@ F('dbg_unit_2', linux_tester().ChromiumFactory(
 #
 # Linux Dbg Clang bot
 #
-B('Linux Clang (dbg)', 'dbg_linux_clang', 'compile', 'linux_dbg',
-  notify_on_missing=True)
+B('Linux Clang (dbg)',
+    factory='dbg_linux_clang',
+    gatekeeper='compile|testers',
+    scheduler='linux_dbg',
+    notify_on_missing=True)
 F('dbg_linux_clang', linux().ChromiumFactory(
     target='Debug',
     options=goma_clang_ninja_options,
