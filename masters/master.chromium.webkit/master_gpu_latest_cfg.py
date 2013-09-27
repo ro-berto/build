f74ee378ae26af516d729981495d2eec80fb35a3
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-27 20:31:36 +0000

    Converted the remaining GPU bots to recipes.
    This includes the GPU bots on the chromium.gpu.fyi,
    tryserver.chromium, and chromium.webkit waterfalls.
    
    Tested with runit.py/runbuild.py --show-commands against each of the
    affected bots, and ensured the correct annotator step was printed.
    Also tested the try job path through the GPU recipe locally.
    
    Note that some of the affected bots are in bad shape right now, but
    the plan is to work on these bots after the switch.
    
    BUG=286398
    
    Review URL: https://chromiumcodereview.appspot.com/24918002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@225769 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.chromium.webkit/master_gpu_latest_cfg.py
@@ -0,0 +1,66 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+from master import master_config
+from master.factory import annotator_factory
+
+defaults = {}
+
+helper = master_config.Helper(defaults)
+B = helper.Builder
+F = helper.Factory
+
+# TODO(kbr): it would be better if this waterfall were refactored so
+# that we could access the slaves_list here.
+gpu_bot_info = [
+  {
+    'builder': 'GPU Win7 (NVIDIA)',
+    'perf_id': 'gpu-webkit-win7-nvidia',
+    'factory_id': 'f_gpu_win_rel',
+  },
+  {
+    'builder': 'GPU Win7 (dbg) (NVIDIA)',
+    'factory_id': 'f_gpu_win_dbg',
+  },
+  {
+    'builder': 'GPU Mac10.7',
+    'perf_id': 'gpu-webkit-mac',
+    'factory_id': 'f_gpu_mac_rel',
+  },
+  {
+    'builder': 'GPU Mac10.7 (dbg)',
+    'factory_id': 'f_gpu_mac_dbg',
+  },
+  {
+    'builder': 'GPU Linux (NVIDIA)',
+    'perf_id': 'gpu-webkit-linux-nvidia',
+    'factory_id': 'f_gpu_linux_rel',
+  },
+  {
+    'builder': 'GPU Linux (dbg) (NVIDIA)',
+    'factory_id': 'f_gpu_linux_dbg',
+  },
+]
+
+m_annotator = annotator_factory.AnnotatorFactory()
+
+defaults['category'] = 'gpu'
+
+for bot in gpu_bot_info:
+  factory_properties = {
+    'test_results_server': 'test-results.appspot.com',
+    'generate_gtest_json': True,
+    'build_config': 'Debug'
+  }
+  if 'perf_id' in bot:
+    factory_properties['show_perf_results'] = True
+    factory_properties['perf_id'] = bot['perf_id']
+    factory_properties['build_config'] = 'Release'
+  B(bot['builder'], bot['factory_id'], scheduler='global_scheduler')
+  F(bot['factory_id'], m_annotator.BaseFactory('gpu', factory_properties))
+
+
+def Update(_config, _active_master, c):
+  return helper.Update(c)
