0af46ed8a9bd7ce3dd43f3a47cfd4c00075b3062
sebmarchand@chromium.org <sebmarchand@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 04:23:48 +0000

    Reduce the link concurrency on the Win ASan builder.
    R=iannucci@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/26565011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@227875 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.fyi/master_win_asan_cfg.py
+++ masters/master.chromium.fyi/master_win_asan_cfg.py
@@ -84,6 +84,7 @@ builder_factory_properties = {
       'component=static_library '
     ),
     'GYP_GENERATORS': 'ninja',
+    'GYP_LINK_CONCURRENCY_MAX': '1',
   },
   'trigger': 'win_asan_rel_trigger',
 }
