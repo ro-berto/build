ro-berto/build

commit f649022de4d20ea3a56347169612a6f0fe7be46a

Add bitness and more OS details to Swarm Slave Dimensions
R=maruel@chromium.org
BUG=

Review URL: https://chromiumcodereview.appspot.com/24074006

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@223692 0039d316-1c4b-4281-b951-d872f2087c98

scripts/tools/swarm_bootstrap/swarm_bootstrap.py
@@ -14,6 +14,7 @@ import json
 import logging
 import optparse
 import os
+import platform
 import socket
 import subprocess
 import sys
@@ -51,28 +52,80 @@ def WriteToFile(filepath, content):
     return False
 
 
-def GetDimensions(hostname, platform):
+def ConvertMacVersion(version):
+  """Returns the major OSX version, like 10.7, 10.8, etc."""
+  version_parts = version.split('.')
+
+  assert len(version_parts) >= 2, 'Unable to determine Mac version'
+  return '.'.join(version_parts[:2])
+
+
+def ConvertWindowsVersion(version):
+  """Returns the major Windows version, like 5.0, 5.1, 6.2, etc."""
+  if '-' in version:
+    version = version.split('-')[1]
+
+  version_parts = version.split('.')
+  assert len(version_parts) >= 2,  'Unable to determine Windows version'
+
+  return '.'.join(version_parts[:2])
+
+
+def GetPlatformVersion():
+  if sys.platform == 'cygwin':
+    return ConvertWindowsVersion(platform.system())
+
+  elif sys.platform == 'win32':
+    return ConvertWindowsVersion(platform.version())
+
+  elif sys.platform == 'darwin':
+    return ConvertMacVersion(platform.mac_ver()[0])
+
+  elif sys.platform == 'linux2':
+    # No need to convert the linux value since it already returns what we
+    # want (like '12.04' or '10.04' for ubuntu slaves).
+    return platform.linux_distribution()[1]
+
+  raise Exception('Unable to determine platform version')
+
+
+def GetArchitectureSize():
+  """Returns the number of bits in the systems architecture.
+
+  Currently this only works on 32-bit or 64-bit systems.
+  """
+  return '64' if sys.maxsize > 2**32 else '32'
+
+
+def GetDimensions(hostname, platform_id, platform_version):
   """Returns a dictionary of attributes representing this machine.
 
   Returns:
     A dictionary of the attributes of the machine.
   """
-  if sys.platform not in PLATFORM_MAPPING:
+  if platform_id not in PLATFORM_MAPPING:
     logging.error('Running on an unknown platform, %s, unable to '
-                  'generate dimensions', sys.platform)
+                  'generate dimensions', platform_id)
     return {}
+
+  platform_name = PLATFORM_MAPPING[platform_id]
 
   return {
     'dimensions': {
-      'os': PLATFORM_MAPPING[platform],
+      'bits': GetArchitectureSize(),
+      'machine': os.uname()[4],
+      'os': [
+          platform_name,
+          platform_name + '-' + platform_version,
+      ],
     },
     'tag': hostname,
   }
 
 
-def GetChromiumDimensions(hostname, platform):
+def GetChromiumDimensions(hostname, platform_id, platform_version):
   """Returns chromium infrastructure specific dimensions."""
-  dimensions = GetDimensions(hostname, platform)
+  dimensions = GetDimensions(hostname, platform_id, platform_version)
   if not dimensions:
     return dimensions
 
@@ -250,7 +303,8 @@ def main():
 
   print('Generating the machine dimensions...')
   hostname = socket.gethostname().lower().split('.', 1)[0]
-  dimensions = GetChromiumDimensions(hostname, sys.platform)
+  dimensions = GetChromiumDimensions(hostname, sys.platform,
+                                     GetPlatformVersion())
   if not WriteJsonToFile(options.dimensionsfile, dimensions):
     return 1
 

scripts/tools/unittests/swarm_bootstrap_test.py
@@ -17,17 +17,55 @@ import swarm_bootstrap
 
 class SwarmBootTest(unittest.TestCase):
   def test_dimensions(self):
-    actual = swarm_bootstrap.GetChromiumDimensions('s33-c4', 'darwin')
-    expected = {'dimensions': {'os': 'Mac', 'vlan': 'm4'}, 'tag': 's33-c4'}
+    bits = swarm_bootstrap.GetArchitectureSize()
+    machine = os.uname()[4]
+
+    actual = swarm_bootstrap.GetChromiumDimensions('s33-c4', 'darwin', '10.8')
+    expected = {'dimensions': {'bits': bits, 'machine': machine,
+                               'os': ['Mac', 'Mac-10.8'], 'vlan': 'm4'},
+                'tag': 's33-c4'}
     self.assertEqual(expected, actual)
 
-    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m4', 'linux2')
-    expected = {'dimensions': {'os': 'Linux', 'vlan': 'm4'}, 'tag': 'vm1-m4'}
+    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m4', 'linux2', '12.04')
+    expected = {'dimensions': {'bits': bits, 'machine': machine,
+                               'os': ['Linux', 'Linux-12.04'], 'vlan': 'm4'},
+                'tag': 'vm1-m4'}
     self.assertEqual(expected, actual)
 
-    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m1', 'win32')
-    expected = {'dimensions': {'os': 'Windows', 'vlan': 'm1'}, 'tag': 'vm1-m1'}
+    actual = swarm_bootstrap.GetChromiumDimensions('vm1-m1', 'win32', '7')
+    expected = {'dimensions': {'bits': bits, 'machine': machine,
+                               'os': ['Windows', 'Windows-7'], 'vlan': 'm1'},
+                'tag': 'vm1-m1'}
     self.assertEqual(expected, actual)
+
+  def test_mac_mapping(self):
+    self.assertEqual('10.7', swarm_bootstrap.ConvertMacVersion('10.7.2'))
+    self.assertEqual('10.8', swarm_bootstrap.ConvertMacVersion('10.8.4'))
+    self.assertEqual('10.9', swarm_bootstrap.ConvertMacVersion('10.9'))
+
+  def test_windows_mapping(self):
+    self.assertEqual('5.1', swarm_bootstrap.ConvertWindowsVersion('5.1.2505'))
+    self.assertEqual('5.1',
+                     swarm_bootstrap.ConvertWindowsVersion('5.1.2600.2180'))
+    self.assertEqual(
+        '5.1',
+        swarm_bootstrap.ConvertWindowsVersion('CYGWIN_NT-5.1.2600'))
+
+    self.assertEqual('6.0', swarm_bootstrap.ConvertWindowsVersion('6.0.5048'))
+    self.assertEqual(
+        '6.0',
+        swarm_bootstrap.ConvertWindowsVersion('CYGWIN_NT-6.0.5048'))
+
+    self.assertEqual('6.1',
+                     swarm_bootstrap.ConvertWindowsVersion('6.1.7600.16385'))
+    self.assertEqual(
+        '6.1',
+        swarm_bootstrap.ConvertWindowsVersion('CYGWIN_NT-6.1.7601'))
+
+    self.assertEqual('6.2', swarm_bootstrap.ConvertWindowsVersion('6.2.9200'))
+    self.assertEqual(
+        '6.2',
+        swarm_bootstrap.ConvertWindowsVersion('CYGWIN_NT-6.2.9200'))
 
 
 if __name__ == '__main__':

commit 438e8caf94f77e5d1efa89318b6b7c2297765c57

Update swarm deployment script to work on Windows.
It was broken by r223692 because os.uname() is unavailable.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.chromium.org/26207003

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@228277 0039d316-1c4b-4281-b951-d872f2087c98

scripts/tools/swarm_bootstrap/swarm_bootstrap.py
@@ -94,6 +94,7 @@ def GetArchitectureSize():
 
   Currently this only works on 32-bit or 64-bit systems.
   """
+  # TODO(maruel): Returns the python build bitness, not the OS bitness.
   return '64' if sys.maxsize > 2**32 else '32'
 
 
@@ -113,7 +114,7 @@ def GetDimensions(hostname, platform_id, platform_version):
   return {
     'dimensions': {
       'bits': GetArchitectureSize(),
-      'machine': os.uname()[4],
+      'machine': platform.machine(),
       'os': [
           platform_name,
           platform_name + '-' + platform_version,