204939b2207055929c77705e7e0ada2886ca5045
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 23:37:08 +0000

    Disable parallel execution for some WebRTC browser tests
    The changes in http://crrev.com/231359 made all tests being
    executed in parallel. This works for most tests, but not for
    the WebRTC browser tests that are relying on the peerconnection
    server for setting up a WebRTC video call between two tabs.
    
    Adding  --test-launcher-jobs=1 to the test arguments should make
    all the tests for this step to execut in sequence according to
    phajdan.jr@.
    
    TEST=none
    BUG=none
    
    Review URL: https://codereview.chromium.org/47923012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@231667 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_commands.py
+++ scripts/master/factory/chromium_commands.py
@@ -1614,7 +1614,10 @@ class ChromiumCommands(commands.FactoryCommands):
                               cmd_name='content_unittests')
 
   def AddWebRtcPerfManualBrowserTests(self, factory_properties=None):
-    cmd_options = ['--run-manual', '--ui-test-action-max-timeout=300000']
+    # These tests needs --test-launcher-jobs=1 since some of them are not able
+    # to run in parallel (due to the usage of the peerconnection server).
+    cmd_options = ['--run-manual', '--ui-test-action-max-timeout=300000',
+                   '--test-launcher-jobs=1']
     self.AddAnnotatedPerfStep(test_name='webrtc_manual_browser_tests',
                               gtest_filter="Webrtc*",
                               log_type='graphing',
