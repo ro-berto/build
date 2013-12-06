f91bec5c549ab9db9d529691cfadda544a05d61c
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 00:10:58 +0000

    Refactor gclients test_api
    R=agable@chromium.org, smut@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/107533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@239081 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gclient/test_api.py
+++ scripts/slave/recipe_modules/gclient/test_api.py
@@ -3,14 +3,24 @@ from slave import recipe_test_api
 
 class GclientTestApi(recipe_test_api.RecipeTestApi):
   def sync(self, cfg, **kwargs):
+    return self.output_json(cfg.got_revision_mapping.keys(), cfg.GIT_MODE)
+
+  def output_json(self, projects, git_mode=False):
+    """Deterministically synthesize json.output test data for gclient's
+    --output-json option.
+
+    Args:
+      projects - a list of project paths (e.g. ['src', 'src/dependency'])
+      git_mode - Return git hashes instead of svn revs.
+    """
     # TODO(iannucci): Account for parent_got_revision_mapping. Right now the
     # synthesized json output from this method will always use
     # gen_revision(project), but if parent_got_revision and its ilk are
     # specified, we should use those values instead.
     return self.m.json.output({
       'solutions': dict(
-        (k+'/', {'revision': self.gen_revision(k, cfg.GIT_MODE)})
-        for k in cfg.got_revision_mapping
+        (p+'/', {'revision': self.gen_revision(p, git_mode)})
+        for p in projects
       )
     })
 
