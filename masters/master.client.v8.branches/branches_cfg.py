6ff08ae01a11b6e1254799a6ec937fb80c21ca7a
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-10 10:26:58 +0000

    V8 Buildbot: Upgrade beta/stable branches to 3.22/23.
    BUG=
    
    Review URL: https://codereview.chromium.org/110383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@239749 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8.branches/branches_cfg.py
+++ masters/master.client.v8.branches/branches_cfg.py
@@ -9,8 +9,8 @@
 # Note that stable and beta might not always correspond to what is used in
 # Chromium, but here we simple use it to mean the last 2 branches.
 
-stable_branch = '3.21'
-beta_branch = '3.22'
+stable_branch = '3.22'
+beta_branch = '3.23'
 branch_names = {
     'stable': {'ia32': 'V8 Linux - ' + stable_branch + ' branch',
                'arm': 'V8 arm - sim - ' + stable_branch + ' branch',
