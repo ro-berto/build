963df53bfa4f7ab13956fb47588ad6cb0683d770
ykyyip@chromium.org <ykyyip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 23:22:00 +0000

    polymer: remove unneeded 'git clean' step
    BUG=
    
    Review URL: https://codereview.chromium.org/113453004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@241060 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/polymer.py
+++ scripts/slave/recipes/polymer.py
@@ -92,9 +92,6 @@ def GenSteps(api):
   if api.platform.is_linux:
     test_prefix = ['xvfb-run']
 
-  yield api.git('clean', '-xdf', cwd=api.path.checkout,
-                can_fail_build=False)
-
   yield api.step('update-install', ['npm' + cmd_suffix, 'install'] + tmp_args,
                  cwd=api.path.checkout, env=node_env)
 
