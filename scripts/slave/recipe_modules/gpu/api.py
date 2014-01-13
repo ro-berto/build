82d75f731ae5d96e7d35472f726158d7d4797d51
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-13 22:17:19 +0000

    Added support for properly building and running isolates in recipes.
    Switched the GPU recipe to run gl_tests via an isolate. Will convert
    remaining tests to run via isolates once this is working well on the bots.
    
    In addition to recipes_test, tested by running gpu/build_and_test recipe
    locally on Linux. Isolate is properly uploaded to the server during the
    build steps and executed via run_isolated.py during the test steps.
    
    BUG=321878
    
    Review URL: https://codereview.chromium.org/132853003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@244596 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gpu/api.py
+++ scripts/slave/recipe_modules/gpu/api.py
@@ -7,7 +7,6 @@ from slave import recipe_api
 SIMPLE_TESTS_TO_RUN = [
   'content_gl_tests',
   'gles2_conform_test',
-  'gl_tests',
   'angle_unittests'
 ]
 
@@ -60,6 +59,13 @@ class GpuApi(recipe_api.RecipeApi):
 
     self.m.chromium.c.gyp_env.GYP_DEFINES['internal_gles2_conform_tests'] = 1
 
+    # Isolates don't work with the component build yet.
+    # Fortunately, we can easily tell which GPU bots are using the
+    # component build -- all of those building or testing Debug.
+    self._use_isolates = self.m.chromium.is_release_build
+    if self._use_isolates:
+      self.m.isolate.set_isolate_environment(self.m.chromium.c)
+
   def checkout_steps(self):
     # Always force a gclient-revert in order to avoid problems when
     # directories are added to, removed from, and re-added to the repo.
@@ -77,6 +83,9 @@ class GpuApi(recipe_api.RecipeApi):
     # Since performance tests aren't run on the debug builders, it isn't
     # necessary to build all of the targets there.
     build_tag = '' if self.m.chromium.is_release_build else 'debug_'
+    # It's harmless to process the isolate-related targets even if they
+    # aren't supported on the current configuration (because the component
+    # build is used).
     yield self.m.chromium.compile(
         targets=['chromium_gpu_%sbuilder' % build_tag, 'gl_tests_run'])
     # This is only an initial test of the isolate upload path; the
@@ -122,7 +131,12 @@ class GpuApi(recipe_api.RecipeApi):
 
     # Note: --no-xvfb is the default.
     for test in SIMPLE_TESTS_TO_RUN:
-      yield self.m.chromium.runtests(test, spawn_dbus=True)
+      yield self.m.chromium.runtests(test)
+
+    if self._use_isolates:
+      yield self.m.isolate.run_isolate_test('gl_tests')
+    else:
+      yield self.m.chromium.runtests('gl_tests')
 
     # Choose a reasonable default for the location of the sandbox binary
     # on the bots.
