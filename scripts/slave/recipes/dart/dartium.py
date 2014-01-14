91ff55cbfcd9f8fa4060b9b8e0f54c62a5e3b96b
whesse@google.com <whesse@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-14 10:11:19 +0000

    Add dartium builder recipe, that builds and tests dartium.
    BUG=
    
    Review URL: https://codereview.chromium.org/128713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@244665 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/dart/dartium.py
@@ -0,0 +1,64 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'chromium',
+  'gclient',
+  'path',
+  'platform',
+  'properties',
+]
+
+def GenSteps(api):
+  config_vals = {}
+  config_vals.update(
+    dict((str(k),v) for k,v in api.properties.iteritems() if k.isupper())
+  )
+
+  api.chromium.set_config('chromium', **config_vals)
+
+  api.chromium.c.gyp_env.GYP_DEFINES['linux_strip_binary'] = 1
+  api.chromium.c.gyp_env.GYP_DEFINES['target_arch'] = 'x64'
+
+  s = api.gclient.c.solutions[0]
+
+  USE_MIRROR = api.gclient.c.USE_MIRROR
+  def DartRepositoryURL(*pieces):
+    BASES = ('https://dart.googlecode.com/svn',
+             'svn://svn-mirror.golo.chromium.org')
+    return '/'.join((BASES[USE_MIRROR],) + pieces)
+
+  s.url = DartRepositoryURL('branches/bleeding_edge/deps/dartium.deps')
+  s.name = 'dartium.deps'
+  s.custom_deps = api.properties.get('gclient_custom_deps') or {}
+  if USE_MIRROR:
+    s.custom_vars.update({
+      'dartium_base': 'svn://svn-mirror.golo.chromium.org'})
+
+  yield api.gclient.checkout()
+
+  # gclient api incorrectly sets Path('[CHECKOUT]') to build/src/dartium.deps
+  # because Dartium has its DEPS file in dartium.deps, not directly in src.
+  api.path.set_dynamic_path('checkout', api.path.slave_build(('src')))
+
+  yield api.chromium.runhooks()
+  yield api.chromium.compile()
+
+  results_dir = api.path.slave_build('layout-test-results')
+  test = api.path.build('scripts', 'slave', 'chromium',
+                        'layout_test_wrapper.py')
+  args = ['--target', api.chromium.c.BUILD_CONFIG,
+          '-o', results_dir,
+          '--build-dir', api.chromium.c.build_dir]
+  yield api.chromium.runtests(test, args, name='webkit_tests')
+
+def GenTests(api):
+  for plat in ('win', 'mac', 'linux'):
+    for bits in (32, 64):
+      for use_mirror in (True, False):
+        yield (
+          api.test('basic_%s_%s_Mirror%s' % (plat, bits, use_mirror)) +
+          api.properties(TARGET_BITS=bits, USE_MIRROR=use_mirror) +
+          api.platform(plat, bits)
+      )
