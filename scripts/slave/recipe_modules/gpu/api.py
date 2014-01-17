a3c4ec02f4c061658c28ce1fdb70259561e7d0a8
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 17:07:31 +0000

    Run gl_tests via isolates.
    This is the remainder of the re-land of
    https://codereview.chromium.org/132853003/ . The necessary Chromium CL has
    landed to let this work on the GPU bots.
    
    BUG=321878
    
    Review URL: https://codereview.chromium.org/140743007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@245540 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gpu/api.py
+++ scripts/slave/recipe_modules/gpu/api.py
@@ -7,7 +7,6 @@ from slave import recipe_api
 SIMPLE_TESTS_TO_RUN = [
   'content_gl_tests',
   'gles2_conform_test',
-  'gl_tests',
   'angle_unittests'
 ]
 
@@ -132,7 +131,12 @@ class GpuApi(recipe_api.RecipeApi):
 
     # Note: --no-xvfb is the default.
     for test in SIMPLE_TESTS_TO_RUN:
-      yield self.m.chromium.runtests(test, spawn_dbus=True)
+      yield self.m.chromium.runtests(test)
+
+    if self._use_isolates:
+      yield self.m.isolate.run_isolate_test('gl_tests')
+    else:
+      yield self.m.chromium.runtests('gl_tests')
 
     # Choose a reasonable default for the location of the sandbox binary
     # on the bots.
