9c353fcfc38ffb7c4d25a758374264113fdd1e10
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-22 00:26:48 +0000

    Set xvfb=True on oilpan recipe.
    R=dpranke@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/144463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@246184 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/oilpan.py
+++ scripts/slave/recipes/oilpan.py
@@ -89,7 +89,7 @@ def GenSteps(api):
     args = ['--target', api.chromium.c.BUILD_CONFIG,
             '-o', results_dir,
             '--build-dir', api.chromium.c.build_dir]
-    yield api.chromium.runtests(test, args, name='webkit_tests')
+    yield api.chromium.runtests(test, args, name='webkit_tests', xvfb=True)
 
     factory_properties = {
       'blink_config':  'chromium',
