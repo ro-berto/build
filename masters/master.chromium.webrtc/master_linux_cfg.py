07f7992aecfd3617c80b3cf12b5ae198b879a471
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-23 08:12:54 +0000

    Remove WebRTC bot building with enable_webrtc=0
    The're no longer a need for this bot. The only projects
    building with this flag have their own bots, which should
    detect any problems related to it.
    
    TEST=none
    BUG=none
    
    Review URL: https://codereview.chromium.org/143983014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@246539 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webrtc/master_linux_cfg.py
+++ masters/master.chromium.webrtc/master_linux_cfg.py
@@ -11,7 +11,6 @@ helper = master_config.Helper(defaults)
 B = helper.Builder
 F = helper.Factory
 S = helper.Scheduler
-P = helper.Periodic
 T = helper.Triggerable
 
 
@@ -19,7 +18,6 @@ def linux():
   return chromium_factory.ChromiumFactory('src/out', 'linux2')
 
 S('linux_rel_scheduler', branch='src', treeStableTimer=60)
-P('linux_daily_scheduler', periodicBuildTimer=24*60*60)
 T('linux_rel_trigger')
 
 chromium_rel_archive = master_config.GetGSUtilUrl('chromium-webrtc',
@@ -58,17 +56,6 @@ F('linux_tester_factory', linux().ChromiumWebRTCFactory(
         'perf_id': 'chromium-webrtc-rel-linux',
     }))
 
-# Builder to catch errors when enable_webrtc=0.
-B('Linux Daily WebRTC Disabled', 'linux_webrtc_disabled_factory',
-  scheduler='linux_daily_scheduler')
-F('linux_webrtc_disabled_factory', linux().ChromiumWebRTCFactory(
-    slave_type='BuilderTester',
-    options=['--compiler=goma', '--build-tool=ninja', '--',
-             'chromium_builder_webrtc'],
-    factory_properties={
-        'gclient_env': {'GYP_DEFINES': ('enable_webrtc=0 '
-                                        'component=static_library')},
-    }))
 
 def Update(config, active_master, c):
   helper.Update(c)
