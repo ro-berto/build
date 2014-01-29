22fd975efb6f8183b81d1e6e07fb5b76baa3f486
stip@chromium.org <stip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-29 01:16:06 +0000

    Move gatekeeper parsing logic into its own file.
    This shouldn't change functionality and is to make it easier to navigate the file.
    
    R=agable@chromium.org
    
    Review URL: https://codereview.chromium.org/147593005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@247560 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/gatekeeper_ng.py
+++ scripts/slave/gatekeeper_ng.py
@@ -29,6 +29,8 @@ import time
 import urllib
 import urllib2
 
+from slave import gatekeeper_ng_config
+
 SCRIPTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                            '..', '..')
 
@@ -154,7 +156,8 @@ def check_builds(master_builds, master_jsons, build_db, gatekeeper_config):
       tree_notify = set(gatekeeper.get('tree_notify', []))
       sheriff_classes = set(gatekeeper.get('sheriff_classes', []))
       subject_template = gatekeeper.get('subject_template',
-                                        DEFAULTS['subject_template'])
+                                        gatekeeper_ng_config.DEFAULTS[
+                                            'subject_template'])
       finished = [s for s in steps if s.get('isFinished')]
       close_tree = gatekeeper.get('close_tree', True)
       respect_build_status = gatekeeper.get('respect_build_status', False)
@@ -210,205 +213,6 @@ def check_builds(master_builds, master_jsons, build_db, gatekeeper_config):
 
   return failed_builds
 
-
-def allowed_keys(test_dict, *keys):
-  keys = keys + ('comment',)
-  assert all(k in keys for k in test_dict), (
-      'not valid: %s; allowed: %s' % (
-          ', '.join(set(test_dict.keys()) - set(keys)),
-          ', '.join(keys)))
-
-
-# Keys which have defaults besides None or set([]).
-DEFAULTS = {
-    'subject_template': ('buildbot %(result)s in %(projectName)s on '
-                         '%(builder)s, revision %(revision)s'),
-}
-
-
-def load_gatekeeper_config(filename):
-  """Loads and verifies config json, constructs builder config dict.
-
-  The gatekeeper json configuration file has two main sections: 'masters'
-  and 'categories.' The following shows the breakdown of a possible config,
-  but note that all nodes are optional (including the root 'masters' and
-  'categories' nodes).
-
-  A builder ultimately needs 4 lists (sets):
-    closing_steps: steps which close the tree on failure or omission
-    forgiving_steps: steps which close the tree but don't email committers
-    tree_notify: any additional emails to notify on tree failure
-    sheriff_classes: classes of sheriffs to notify on build failure
-
-  Builders can inherit these properties from categories, they can inherit
-  tree_notify and sheriff_classes from their master, and they can have these
-  properties assigned in the builder itself. Any property not specified
-  is considered blank (empty set), and inheritance is always constructive (you
-  can't remove a property by inheriting or overwriting it). Builders can inherit
-  categories from their master.
-
-  A master consists of zero or more sections, which specify which builders are
-  watched by the section and what action should be taken. A section can specify
-  tree_closing to be false, which causes the section to only send out emails
-  instead of closing the tree. A section or builder can also specify to respect
-  a build's failure status with respect_build_status.
-
-  The 'subject_template' key is the template used for the email subjects. Its
-  formatting arguments are found at https://chromium.googlesource.com/chromium/
-    tools/chromium-build/+/master/gatekeeper_mailer.py, but the list is
-  reproduced here:
-
-    %(result)s: 'warning' or 'failure'
-    %(projectName): 'Chromium', 'Chromium Perf', etc.
-    %(builder): the builder name
-    %(reason): reason for launching the build
-    %(revision): build revision
-    %(buildnumber): buildnumber
-
-  'forgive_all' converts all closing_steps to be forgiving_steps. Since
-  forgiving_steps only email sheriffs + watchlist (not the committer), this is a
-  great way to set up experimental or informational builders without spamming
-  people. It is enabled by providing the string 'true'.
-
-  The 'comment' key can be put anywhere and is ignored by the parser.
-
-  # Python, not JSON.
-  {
-    'masters': {
-      'http://build.chromium.org/p/chromium.win': [
-        {
-          'sheriff_classes': ['sheriff_win'],
-          'tree_notify': ['a_watcher@chromium.org'],
-          'categories': ['win_extra'],
-          'builders': {
-            'XP Tests (1)': {
-              'categories': ['win_tests'],
-              'closing_steps': ['xp_special_step'],
-              'forgiving_steps': ['archive'],
-              'tree_notify': ['xp_watchers@chromium.org'],
-              'sheriff_classes': ['sheriff_xp'],
-            }
-          }
-        }
-      ]
-    },
-    'categories': {
-      'win_tests': {
-        'comment': 'this is for all windows testers',
-        'closing_steps': ['startup_test'],
-        'forgiving_steps': ['boot_windows'],
-        'tree_notify': ['win_watchers@chromium.org'],
-        'sheriff_classes': ['sheriff_win_test']
-      },
-      'win_extra': {
-        'closing_steps': ['extra_win_step']
-        'subject_template': 'windows heads up on %(builder)',
-      }
-    }
-  }
-
-  In this case, XP Tests (1) would be flattened down to:
-    closing_steps: ['startup_test', 'win_tests']
-    forgiving_steps: ['archive', 'boot_windows']
-    tree_notify: ['xp_watchers@chromium.org', 'win_watchers@chromium.org',
-                  'a_watcher@chromium.org']
-    sheriff_classes: ['sheriff_win', 'sheriff_win_test', 'sheriff_xp']
-
-  Again, fields are optional and treated as empty lists/sets/strings if not
-  present.
-  """
-
-  # Keys which are allowed in a master or builder section.
-  master_keys = ['forgive_all', 'tree_notify', 'sheriff_classes',
-                 'subject_template']
-  builder_keys = ['closing_steps', 'forgiving_steps', 'forgive_all',
-                  'sheriff_classes', 'subject_template', 'tree_notify']
-
-
-  # These keys are strings instead of sets. Strings can't be merged,
-  # so more specific (master -> category -> builder) strings clobber
-  # more generic ones.
-  strings = ['forgive_all', 'subject_template']
-
-  with open(filename) as f:
-    raw_gatekeeper_config = json.load(f)
-
-  allowed_keys(raw_gatekeeper_config, 'categories', 'masters')
-
-  categories = raw_gatekeeper_config.get('categories', {})
-  masters = raw_gatekeeper_config.get('masters', {})
-
-  for category in categories.values():
-    allowed_keys(category, *builder_keys)
-
-  gatekeeper_config = {}
-  for master_url, master_sections in masters.iteritems():
-    for master_section in master_sections:
-      gatekeeper_config.setdefault(master_url, []).append({})
-      allowed_keys(master_section, 'builders', 'categories', 'close_tree',
-                   'respect_build_status', *master_keys)
-
-      builders = master_section.get('builders', {})
-      for buildername, builder in builders.iteritems():
-        allowed_keys(builder, 'categories', *builder_keys)
-        for key, item in builder.iteritems():
-          if key in strings:
-            assert isinstance(item, basestring)
-          else:
-            assert isinstance(item, list)
-            assert all(isinstance(elem, basestring) for elem in item)
-
-        gatekeeper_config[master_url][-1].setdefault(buildername, {})
-        gatekeeper_builder = gatekeeper_config[master_url][-1][buildername]
-
-        # Populate with specified defaults.
-        for k in builder_keys:
-          if k in DEFAULTS:
-            gatekeeper_builder.setdefault(k, DEFAULTS[k])
-          elif k in strings:
-            gatekeeper_builder.setdefault(k, '')
-          else:
-            gatekeeper_builder.setdefault(k, set())
-
-        # Inherit any values from the master.
-        for k in master_keys:
-          if k in strings:
-            if k in master_section:
-              gatekeeper_builder[k] = master_section[k]
-          else:
-            gatekeeper_builder[k] |= set(master_section.get(k, []))
-
-        gatekeeper_builder['close_tree'] = master_section.get('close_tree',
-                                                              True)
-        gatekeeper_builder['respect_build_status'] = master_section.get(
-            'respect_build_status', False)
-
-        # Inherit any values from the categories.
-        all_categories = (builder.get('categories', []) +
-                          master_section.get( 'categories', []))
-        for c in all_categories:
-          for k in builder_keys:
-            if k in strings:
-              if k in categories[c]:
-                gatekeeper_builder[k] = categories[c][k]
-            else:
-              gatekeeper_builder[k] |= set(categories[c].get(k, []))
-
-        # Add in any builder-specific values.
-        for k in builder_keys:
-          if k in strings:
-            if k in builder:
-              gatekeeper_builder[k] = builder[k]
-          else:
-            gatekeeper_builder[k] |= set(builder.get(k, []))
-
-        # Builder postprocessing.
-        if gatekeeper_builder['forgive_all'] == 'true':
-          gatekeeper_builder['forgiving_steps'] |= gatekeeper_builder[
-              'closing_steps']
-          gatekeeper_builder['closing_steps'] = set([])
-
-  return gatekeeper_config
 
 
 def parse_sheriff_file(url):
@@ -687,22 +491,13 @@ def main():
 
   logging.basicConfig(level=logging.DEBUG if options.verbose else logging.INFO)
 
-  gatekeeper_config = load_gatekeeper_config(options.json)
+  gatekeeper_config = gatekeeper_ng_config.load_gatekeeper_config(options.json)
 
   if options.verify:
     return 0
 
   if options.flatten_json:
-    # Python's sets aren't JSON-encodable, so we convert them to lists here.
-    class SetEncoder(json.JSONEncoder):
-      # pylint: disable=E0202
-      def default(self, obj):
-        if isinstance(obj, set):
-          return list(obj)
-        return json.JSONEncoder.default(self, obj)
-
-    json.dump(gatekeeper_config, sys.stdout, cls=SetEncoder)
-    print
+    gatekeeper_ng_config.flatten_to_json(gatekeeper_config, sys.stdout)
     return 0
 
   masters = set(args)
