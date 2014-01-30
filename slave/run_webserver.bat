465391e0cc575d1454b847db2b4c7435ce9f831e
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 18:43:12 +0000

    Add /Y to xcopy of cygwin1.dll in lighttpd dir
    So that when we make a copy in the win\ folder the batch file
    won't hang waiting for confirmation.
    
    The 5-ish step process:
    - Make this batch file not hang if the dest already exists
    - Copy no_dll\cygwin1.dll to the win\ folder in the lighttpd DEPS tree
    - Remove third_party/cygwin
    - Remove the xcopy in run_webserver.bat
    - Remove the copy of cygwin1.dll in no_dll subdirectory.
    
    BUG=123026
    R=dpranke@chromium.org, iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/138633005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@248004 0039d316-1c4b-4281-b951-d872f2087c98

--- slave/run_webserver.bat
+++ slave/run_webserver.bat
@@ -6,11 +6,11 @@ REM killed by taskkill.
 
 REM Since xcopy is stupid, tell it that lighttpd_server.exe is a file and not
 REM a directory using the echo hack.
-echo f | xcopy /D %lighttpd_dir%\lighttpd.exe %lighttpd_dir%\lighttpd_server.exe
+echo f | xcopy /D /Y %lighttpd_dir%\lighttpd.exe %lighttpd_dir%\lighttpd_server.exe
 
 REM copy the cygwin dll to the lighttpd folder because otherwise it wont work.
 
-xcopy /D %lighttpd_dir%\..\no_dll\CygWin1.dll %lighttpd_dir%
+xcopy /D /Y %lighttpd_dir%\..\no_dll\CygWin1.dll %lighttpd_dir%
 
 REM Start the server, using the conf file in the slave directory.
 
