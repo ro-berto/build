7626cd36ce4c12441ca59f95ce44c087e3ad709f
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 14:34:31 +0000

    Document the android_webview_aosp recipe.
    This add some documentation to the recipe.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/168583015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@252232 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/android_webview_aosp.py
+++ scripts/slave/recipes/android_webview_aosp.py
@@ -9,20 +9,44 @@ DEPS = [
   'tryserver',
 ]
 
+# This recipe describes building the Android framework WebView component inside
+# an Android build environment.
+# The developer instructions for building Android are available on the wiki:
+# http://www.chromium.org/developers/how-tos/build-instructions-android-webview
+#
+# In order to build a fully functional Android WebView it is necessary to
+# compile against parts of the Android system that are not part of the Android
+# SDK that the 'regular' Chromium build uses. This is why the recipe requires
+# checking out the Android source tree and building using the Android build
+# system.
 def GenSteps(api):
   droid = api.android
   droid.set_config('AOSP_webview')
+
+  # Not all third party dependencies have licenses compatible with the Android
+  # license. To prevent developers adding dependencies from the WebView to
+  # incompatibly licensed third parties we check out a subset of DEPS.
+  # The whitelist is in the android_webview/buildbot/deps_whitelist.py file.
+  # This does not cover code checked into the main Chromium repository directly
+  # for that we have the incompatible_directories_check_step below.
   yield droid.chromium_with_trimmed_deps()
   yield droid.lastchange_steps()
 
   yield api.tryserver.maybe_apply_issue()
 
+  # Check out the Android source.
   yield droid.repo_init_steps()
   yield droid.repo_sync_steps()
 
+  # The Android build system requires the Chromium sources be in the Android
+  # tree. Luckily it doesn't mind if the tree is symlinked in.
   yield droid.symlink_chromium_into_android_tree_step()
+  # This generates the Android.mk files needed by the Android build system to
+  # build the parts of Chromium that are needed by the WebView.
   yield droid.gyp_webview_step()
 
+  # This check is in place to detect incompatibly-licensed new code that's
+  # checked into the main Chromium repository.
   yield droid.incompatible_directories_check_step()
 
   # TODO(android): use api.chromium.compile for this
