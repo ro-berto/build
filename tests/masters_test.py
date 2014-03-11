525bed069a1f27ae5415a323d413f178aaab6bf8
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 22:58:10 +0000

    Add experimental master.tryserver.blink
    This will allow us to put dedicated slaves there, and actually throw
    more slaves at it. This wouldn't work in a single master.tryserver.chromium
    which is already overloaded.
    
    BUG=334892
    
    Review URL: https://codereview.chromium.org/196003002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@256351 0039d316-1c4b-4281-b951-d872f2087c98

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -246,6 +246,7 @@ def main(argv):
       'master.experimental': 'Experimental',
       'master.push.canary': 'PushCanary',
       'master.tryserver.chromium': 'TryServer',
+      'master.tryserver.blink': 'BlinkTryServer',
       'master.tryserver.libyuv': 'LibyuvTryServer',
       'master.tryserver.nacl': 'NativeClientTryServer',
       'master.tryserver.v8': 'V8TryServer',
