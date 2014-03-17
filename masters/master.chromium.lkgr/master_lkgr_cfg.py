d2a7bd80715bf442a48d44e06e654615c97cdfae
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 11:30:31 +0000

    Build the 32-bit ASan + V8-ARM binaries with component=shared_library, as issue 351672 is now fixed.
    This effectively reverts r256528.
    
    BUG=351672
    TBR=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/197813010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@257410 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.lkgr/master_lkgr_cfg.py
+++ masters/master.chromium.lkgr/master_lkgr_cfg.py
@@ -167,8 +167,7 @@ F('linux_asan_dbg', linux().ChromiumASANFactory(
 asan_ia32_v8_arm = ('asan=1 linux_use_tcmalloc=0 disable_nacl=1 '
                     'v8_target_arch=arm host_arch=x86_64 target_arch=ia32 '
                     'sysroot=/var/lib/chroot/precise32bit chroot_cmd=precise32 '
-                    'v8_enable_verify_heap=1 enable_ipc_fuzzer=1 '
-                    'component=static_library ')
+                    'v8_enable_verify_heap=1 enable_ipc_fuzzer=1 ')
 
 asan_ia32_v8_arm_rel_sym = ('%s release_extra_cflags="-gline-tables-only -O1 '
                             '-fno-inline-functions -fno-inline"' %
