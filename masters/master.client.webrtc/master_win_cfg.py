bfc0bd1d10b480ecd1c1722217ab0a7e28299584
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 15:11:38 +0000

    Revert Goma switch for WebRTC Windows bots.
    In https://codereview.chromium.org/197873038/
    all WebRTC client buildbots were switched to Goma.
    Despite local testing, the buildbots failed with
    errors like this:
    
    [119/3002] CC obj\third_party\libjpeg_turbo\libjpeg.jcdctmgr.obj
    FAILED: ninja -t msvc -e environment.x86 -- E:\b\build\goma\gomacc.exe "E:\b\depot_tools\win_toolchain\vs2013_files\VC\bin\amd64_x86\cl.exe" /nologo /showIncludes /FC @obj\third_party\expat\files\lib\expat.xmltok.obj.rsp /c ..\..\third_party\expat\files\lib\xmltok.c /Foobj\third_party\expat\files\lib\expat.xmltok.obj /Fdobj\third_party\expat\expat.c.pdb
    e:\b\build\slave\win\build\src\third_party\expat\files\lib\xmltok_impl.c(103) : fatal error C1090: PDB API call failed, error code '23' : '(
    
    BUG=None
    TEST=None
    TBR=stip
    
    Review URL: https://codereview.chromium.org/208683005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@259196 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.webrtc/master_win_cfg.py
+++ masters/master.client.webrtc/master_win_cfg.py
@@ -47,22 +47,18 @@ baremetal_tests = [
     'webrtc_perf_tests',
 ]
 
-options=['--compiler=goma']
-
 defaults['category'] = 'win'
 
 B('Win32 Debug', 'win32_debug_factory', 'compile|testers|windows', scheduler,
   slavebuilddir='win')
 F('win32_debug_factory', win().WebRTCFactory(
     target='Debug',
-    options=options,
     tests=tests))
 
 B('Win32 Release', 'win32_release_factory', 'compile|testers|windows',
   scheduler, slavebuilddir='win')
 F('win32_release_factory', win().WebRTCFactory(
     target='Release',
-    options=options,
     tests=tests,
     # No point having more than one bot complaining about missing sources.
     factory_properties={
@@ -75,7 +71,6 @@ B('Win64 Debug', 'win64_debug_factory', 'compile|testers|windows', scheduler,
   slavebuilddir='win')
 F('win64_debug_factory', win().WebRTCFactory(
     target='Debug_x64',
-    options=options,
     tests=tests,
     factory_properties={
         'gclient_env': {'GYP_DEFINES': 'target_arch=x64'},
@@ -85,7 +80,6 @@ B('Win64 Release', 'win64_release_factory', 'compile|testers|windows',
   scheduler, slavebuilddir='win')
 F('win64_release_factory', win().WebRTCFactory(
     target='Release_x64',
-    options=options,
     tests=tests,
     factory_properties={
         'gclient_env': {'GYP_DEFINES': 'target_arch=x64'},
@@ -95,7 +89,6 @@ B('Win32 Release [large tests]', 'win32_largetests_factory',
   'compile|baremetal|windows', scheduler)
 F('win32_largetests_factory', win().WebRTCFactory(
     target='Release',
-    options=options,
     tests=baremetal_tests,
     factory_properties={
         'virtual_webcam': True,
