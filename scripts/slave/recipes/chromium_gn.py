051965c00218003d21e11569210972928561de20
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 01:08:00 +0000

    Add tryserver support to chromium_gn.
    R=iannucci@chromium.org, phajdan.jr@chromium.org, brettw@chromium.org
    BUG=353855
    
    Review URL: https://codereview.chromium.org/219963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@261248 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_gn.py
+++ scripts/slave/recipes/chromium_gn.py
@@ -5,8 +5,12 @@
 DEPS = [
   'chromium',
   'gclient',
+  'path',
   'platform',
   'properties',
+  'step',
+  'step_history',
+  'tryserver',
 ]
 
 
@@ -16,14 +20,40 @@ def GenSteps(api):
   # lookup mechanism to map bot name to build_config.
   build_config = {
     'Linux GN (dbg)': 'Debug',
+    'linux_chromium_gn': 'Debug',
   }[api.properties.get('buildername')]
+
+  is_tryserver = api.tryserver.is_tryserver
 
   api.gclient.set_config('chromium')
   api.chromium.set_config('chromium', BUILD_CONFIG=build_config)
+  api.step.auto_resolve_conflicts = True
 
-  yield api.gclient.checkout(revert=True)
+  yield api.gclient.checkout(revert=True,
+                             abort_on_failure=False,
+                             can_fail_build=False)
 
-  yield api.chromium.runhooks(run_gyp=False)
+  if is_tryserver:
+    # maybe_apply_issue should work fine even w/o a patch, but this
+    # is a little more explicit.
+    yield api.tryserver.maybe_apply_issue()
+    yield api.chromium.runhooks(run_gyp=False,
+                                abort_on_failure=False,
+                                can_fail_build=False)
+
+    if any(step.retcode != 0 for step in api.step_history.values()):
+      # Nuke the whole slave checkout and try again.
+      yield api.path.rmcontents('slave build directory',
+                                api.path['slave_build'])
+      yield api.gclient.checkout(revert=False,
+                                 abort_on_failure=True,
+                                 can_fail_build=True)
+      yield api.tryserver.maybe_apply_issue()
+      yield api.chromium.runhooks(run_gyp=False,
+                                  abort_on_failure=True,
+                                  can_fail_build=True)
+  else:
+    yield api.chromium.runhooks(run_gyp=False)
 
   yield api.chromium.run_gn('//out/' + build_config)
 
@@ -40,8 +70,30 @@ def GenTests(api):
       api.platform.name('linux')
   )
 
+  yield (
+      api.test('unittest_sync_fails') +
+      api.properties.tryserver(buildername='linux_chromium_gn') +
+      api.platform.name('linux') +
+      api.step_data('gclient revert', retcode=1)
+  )
+
+  # This test should abort before running GN and trying to compile.
+  yield (
+      api.test('unittest_second_sync_fails') +
+      api.properties.tryserver(buildername='linux_chromium_gn') +
+      api.platform.name('linux') +
+      api.step_data('gclient revert', retcode=1) +
+      api.step_data('gclient sync (2)', retcode=1)
+  )
+
   # TODO: crbug.com/354674. Figure out where to put "simulation"
   # tests. We should have one test for each bot this recipe runs on.
+  yield (
+      api.test('full_linux_chromium_gn') +
+      api.properties.tryserver(buildername='linux_chromium_gn') +
+      api.platform.name('linux')
+  )
+
   yield (
       api.test('full_chromium_linux_Linux_GN__dbg_') +
       api.properties.generic(buildername='Linux GN (dbg)') +
