6e5d08a42568ed4f5eaf44a4b660211700afa321
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-04 20:56:18 +0000

    Upgrade to coverage 3.7.1 and have it auto-build itself on first use.
    R=vadimsh@chromium.org, agable@chromium.org, dpranke@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/225633007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@261861 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -34,7 +34,7 @@ def CommonChecks(input_api, output_api):
         join('third_party'),
         join('third_party', 'buildbot_8_4p1'),
         join('third_party', 'buildbot_slave_8_4'),
-        join('third_party', 'coverage-3.6'),
+        join('third_party', 'coverage-3.7.1'),
         join('third_party', 'decorator_3_3_1'),
         join('third_party', 'jinja2'),
         join('third_party', 'mock-1.0.1'),
