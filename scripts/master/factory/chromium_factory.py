e84cb799102768547eb513e03efd7967843c2829
hinoka@google.com <hinoka@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 00:20:03 +0000

    Remove cros_deps
    This isn't used anymore, it just links to a DEPS file with a "Remove me" comment.
    
    BUG=361346
    
    Review URL: https://codereview.chromium.org/248463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@265438 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_factory.py
+++ scripts/master/factory/chromium_factory.py
@@ -1296,11 +1296,6 @@ class ChromiumFactory(gclient_factory.GClientFactory):
                         mode=None, slave_type='BuilderTester', options=None,
                         compile_timeout=1200, build_url=None, project=None,
                         factory_properties=None):
-    # Make sure the solution is not already there.
-    if 'cros_deps' not in [s.name for s in self._solutions]:
-      self._solutions.append(gclient_factory.GClientSolution(
-          config.Master.trunk_url + '/src/tools/cros.DEPS', name='cros_deps'))
-
     return self.ChromiumFactory(target, clobber, tests, mode, slave_type,
                                 options, compile_timeout, build_url, project,
                                 factory_properties)
@@ -1354,10 +1349,6 @@ class ChromiumFactory(gclient_factory.GClientFactory):
                             mode=None, slave_type='BuilderTester', options=None,
                             compile_timeout=1200, build_url=None, project=None,
                             factory_properties=None):
-    # Make sure the solution is not already there.
-    if 'cros_deps' not in [s.name for s in self._solutions]:
-      self._solutions.append(gclient_factory.GClientSolution(
-          config.Master.trunk_url + '/src/tools/cros.DEPS', name='cros_deps'))
     return self.ChromiumFactory(target, clobber, tests, mode, slave_type,
                                 options, compile_timeout, build_url, project,
                                 factory_properties)
