92aeffe058bfd724309ddcdbdab9226057074afe
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 02:36:20 +0000

    Switch master.chromium.lkgr to poll the chromium-status app.
    Using a PBChangeSource is silly, opaque, and potentially dangerous. We already
    have a URLPoller for exactly this use-case (already in use by chromium.endure)
    so let's use it here too. This also has the advantage of making sure
    the LKGR waterfall picks up *all* updates to LKGR, including manual ones.
    
    R=iannucci@chromium.org, phajdan.jr@chromium.org
    BUG=366954
    
    Review URL: https://codereview.chromium.org/255753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@266093 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.lkgr/master_source_cfg.py
+++ masters/master.chromium.lkgr/master_source_cfg.py
@@ -1,9 +1,13 @@
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from buildbot.changes.pb import PBChangeSource
+from master.url_poller import URLPoller
+
+
+LKGR_URL = 'https://chromium-status.appspot.com/lkgr'
 
 def Update(config, active_master, c):
-  # Polls config.Master.trunk_url for changes
-  c['change_source'].append(PBChangeSource())
+  c['change_source'].append(
+      URLPoller(changeurl=LKGR_URL, pollInterval=300,
+                category='lkgr', include_revision=True))
