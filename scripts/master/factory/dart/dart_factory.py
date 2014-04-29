b70b6688c1fe7d6eacf5ec0ea0d66ffc44d46542
kustermann@google.com <kustermann@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 05:59:28 +0000

    Only activate HttpStatusPush on production
    Review URL: https://codereview.chromium.org/260083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@266755 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/dart/dart_factory.py
+++ scripts/master/factory/dart/dart_factory.py
@@ -637,8 +637,6 @@ class DartUtils(object):
     master_port = self._active_master.master_port
     master_port_alt = self._active_master.master_port_alt
 
-    http_status_push_url = self._active_master.http_status_push_url
-
     statuses = []
     statuses.append(master_utils.CreateWebStatus(master_port,
                                                  allowForce=True,
@@ -646,7 +644,9 @@ class DartUtils(object):
                                                  templates=templates))
     statuses.append(
         master_utils.CreateWebStatus(master_port_alt, allowForce=False))
-    if http_status_push_url:
+
+    http_status_push_url = self._active_master.http_status_push_url
+    if self._active_master.is_production_host and http_status_push_url:
       statuses.append(HttpStatusPush(serverUrl=http_status_push_url))
     return statuses
 
