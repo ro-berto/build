c0ba3712b00b24fa98fcca36afe497c5cf68b8b4
machenbach@chromium.org <machenbach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 12:10:57 +0000

    Refactoring: Extract builder config from chromium_gn recipe.
    This makes the recipe more uniform with the standard chromium recipe. It will simplify adding new bots from other waterfalls with potentially different gclient/chromium configs.
    
    BUG=376692
    
    Review URL: https://codereview.chromium.org/294243002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@273192 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_gn.py
+++ scripts/slave/recipes/chromium_gn.py
@@ -15,32 +15,99 @@ DEPS = [
 ]
 
 
+BUILDERS = {
+  'chromium.linux': {
+    'builders': {
+      'Android GN': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'android',
+          'TARGET_ARCH': 'arm',
+        },
+        'gclient_apply_config': ['android'],
+      },
+      'Android GN (dbg)': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_PLATFORM': 'android',
+          'TARGET_ARCH': 'arm',
+        },
+        'gclient_apply_config': ['android'],
+      },
+      'Linux GN': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+        },
+      },
+      'Linux GN (dbg)': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+        },
+      },
+    },
+  },
+  'tryserver.chromium': {
+    'builders': {
+      'android_chromium_gn_compile_rel': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'android',
+          'TARGET_ARCH': 'arm',
+        },
+        'gclient_apply_config': ['android'],
+      },
+      'android_chromium_gn_compile_dbg': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_PLATFORM': 'android',
+          'TARGET_ARCH': 'arm',
+        },
+        'gclient_apply_config': ['android'],
+      },
+      'linux_chromium_gn_rel': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+        },
+      },
+      'linux_chromium_gn_dbg': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+        },
+      },
+    },
+  },
+  'fake_tryserver': {
+    'builders': {
+      'unittest_fake_trybotname': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+        },
+      },
+    },
+  },
+}
+
 def GenSteps(api):
   # TODO: crbug.com/358481 . The build_config should probably be a property
   # passed in from slaves.cfg, but that doesn't exist today, so we need a
   # lookup mechanism to map bot name to build_config.
+  mastername = api.properties.get('mastername')
   buildername = api.properties.get('buildername')
+  master_dict = BUILDERS.get(mastername, {})
+  bot_config = master_dict.get('builders', {}).get(buildername)
   is_tryserver = api.tryserver.is_tryserver
   if is_tryserver:
     api.step.auto_resolve_conflicts = True
 
-  kwargs = {}
-  if '(dbg)' in buildername or '_dbg' in buildername:
-    build_config = 'Debug'
-  else:
-    build_config = 'Release'
-  kwargs['BUILD_CONFIG'] = build_config
-  if 'Android ' in buildername or 'android_' in buildername:
-    kwargs['TARGET_PLATFORM'] = 'android'
-    kwargs['TARGET_ARCH'] = 'arm'
-  api.chromium.set_config('chromium', **kwargs)
+  api.chromium.set_config('chromium',
+                          **bot_config.get('chromium_config_kwargs', {}))
 
   # Note that we have to call gclient.set_config() and apply_config() *after*
   # calling chromium.set_config(), above, because otherwise the chromium
   # call would reset the gclient config to its defaults.
   api.gclient.set_config('chromium')
-  if 'Android ' in buildername or 'android_' in buildername:
-    api.gclient.apply_config('android')
+  for c in bot_config.get('gclient_apply_config', []):
+    api.gclient.apply_config(c)
 
   # TODO(dpranke): crbug.com/358435. We need to figure out how to separate
   # out the retry and recovery logic from the rest of the recipe.
@@ -75,32 +142,20 @@ def _sanitize_nonalpha(text):
 
 
 def GenTests(api):
-  yield (
-      api.test('unittest_linux_rel_success') +
-      api.properties.generic(buildername='unittest_fake_buildername',
-                             mastername='chromium.linux') +
-      api.platform.name('linux')
-  )
-
-  yield (
-      api.test('unittest_android_dbg_trybot_success') +
-      api.properties.tryserver(
-          buildername='unittest_fake_android_dbg_trybot_name') +
-      api.platform.name('linux')
-  )
-
   # api.gclient.checkout() actually contains a revert call and a sync call,
   # so we test that either of them failing is handled correctly.
   yield (
       api.test('unittest_sync_fails') +
-      api.properties.tryserver(buildername='unittest_fake_trybotname') +
+      api.properties.tryserver(buildername='unittest_fake_trybotname',
+                               mastername='fake_tryserver') +
       api.platform.name('linux') +
       api.step_data('gclient sync', retcode=1)
   )
 
   yield (
       api.test('unittest_revert_fails') +
-      api.properties.tryserver(buildername='linux_chromium_gn_dbg') +
+      api.properties.tryserver(buildername='unittest_fake_trybotname',
+                               mastername='fake_tryserver') +
       api.platform.name('linux') +
       api.step_data('gclient revert', retcode=1)
   )
@@ -109,7 +164,8 @@ def GenTests(api):
   # to apply the patch.
   yield (
       api.test('unittest_second_sync_fails') +
-      api.properties.tryserver(buildername='unittest_fake_trybotname') +
+      api.properties.tryserver(buildername='unittest_fake_trybotname',
+                               mastername='fake_tryserver') +
       api.platform.name('linux') +
       api.step_data('gclient sync', retcode=1) +
       api.step_data('gclient sync (2)', retcode=1)
@@ -118,6 +174,8 @@ def GenTests(api):
   # TODO: crbug.com/354674. Figure out where to put "simulation"
   # tests. We should have one test for each bot this recipe runs on.
 
+  # TODO(machenbach): Prefix test names with try server name like in other bot
+  # specific tests. Get bot names from BUILDERS dict.
   trybot_names = [
     'android_chromium_gn_compile_dbg',
     'android_chromium_gn_compile_rel',
