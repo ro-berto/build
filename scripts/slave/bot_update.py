37a58abec442c30f84154251fb17c2826e0ed60a
hinoka@chromium.org <hinoka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-11 18:38:55 +0000

    Enable bot_update on chromium.endure bots
    This enables a git-based checkout on the bots.
    
    BUG=261741
    
    Review URL: https://codereview.chromium.org/330453004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@276464 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/bot_update.py
+++ scripts/slave/bot_update.py
@@ -155,6 +155,7 @@ RECOGNIZED_PATHS.update(internal_data.get('RECOGNIZED_PATHS', {}))
 
 ENABLED_MASTERS = [
     'bot_update.always_on',
+    'chromium.endure',
     'chromium.fyi',
     'chromium.git',
     'chromium.gpu.fyi',
