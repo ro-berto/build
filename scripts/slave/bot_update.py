1db0c06e01fabecfe9d7cee2e5e0918d28ab6749
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 07:57:17 +0000

    Revert of Enable bot_update on chromium.webrtc.fyi (https://codereview.chromium.org/332523002/)
    Reason for revert:
    Reverting due to breaking the waterfall for the testers: The resources that we download as a part of the webrtc.DEPS/DEPS file are no longer downloaded during the runhooks step. Compare http://chromegw/i/chromium.webrtc.fyi/builders/Linux/builds/9190/steps/gclient%20runhooks/logs/stdio
    with
    http://chromegw/i/chromium.webrtc.fyi/builders/Linux/builds/9191/steps/gclient%20runhooks/logs/stdio
    
    This causes the tests to fail since they cannot find the needed resources.
    I think we (or myself) have to work out a better way to handle these resources before we can switch these bots to bot_update.
    
    Original issue's description:
    > Enable bot_update on chromium.webrtc.fyi
    > 
    > This enables a git-based checkout on the bots.  Windows might be a bit slower
    > but since this is an fyi waterfall that's probably okay.
    > 
    > BUG=261741
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=276860
    
    TBR=agable@chromium.org,hinoka@google.com,hinoka@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=261741
    
    Review URL: https://codereview.chromium.org/332713005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@276966 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/bot_update.py
+++ scripts/slave/bot_update.py
@@ -167,7 +167,6 @@ ENABLED_MASTERS = [
     'chromium.memory',
     'chromium.perf',
     'chromium.swarm',
-    'chromium.webrtc.fyi',
     'tryserver.chromium',
 ]
 ENABLED_MASTERS += internal_data.get('ENABLED_MASTERS', [])
@@ -326,11 +325,6 @@ DISABLED_BUILDERS = {
         'Win XP Perf (3)',
         'Win XP Perf (4)',
         'Win XP Perf (5)',
-    ],
-    'chromium.webrtc.fyi': [
-        'Win Builder',
-        'Win7 Tester',
-        'WinXP Tester',
     ],
     'chromium.swarm': [
         'Windows Swarm Tests',
