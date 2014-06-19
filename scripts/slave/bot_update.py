c650314f8198d28d8713ae3ebd34aa6f0b95d18b
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 01:46:07 +0000

    Revert of Turn bot_update on for all non-deps chromium.webkit builders (https://codereview.chromium.org/343773002/)
    Reason for revert:
    This breaks the flakiness dashboard which is necessary for gardening.
    
    Original issue's description:
    > Turn bot_update on for all non-deps chromium.webkit builders
    > 
    > The DEPS builders are a bit special, so excluding them for now.
    > But otherwise I see Linux Oilpan successfully deal with the chromium/webkit
    > revision split, so I think its ready for all other bots.
    > http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Oilpan
    > 
    > BUG=373456
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=278189
    
    TBR=agable@chromium.org,hinoka@google.com,hinoka@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=373456
    
    Review URL: https://codereview.chromium.org/340323003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@278235 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/bot_update.py
+++ scripts/slave/bot_update.py
@@ -169,7 +169,6 @@ ENABLED_MASTERS = [
     'chromium.memory.fyi',
     'chromium.perf',
     'chromium.swarm',
-    'chromium.webkit',
     'client.nacl.sdk.mono',
     'tryserver.chromium',
 ]
@@ -179,6 +178,9 @@ ENABLED_BUILDERS = {
     'tryserver.chromium.gpu': [
         'linux_gpu',
         'mac_gpu',
+    ],
+    'chromium.webkit': [
+        'WebKit Linux Oilpan',
     ],
     'chromium.lkgr': [
         'Android',
@@ -287,13 +289,6 @@ DISABLED_BUILDERS = {
     'chromium.swarm': [
         'Windows Swarm Tests',
         'Windows Swarm Tests (dbg)',
-    ],
-    'chromium.webkit': [
-        'WebKit Win Builder (deps)',
-        'WebKit XP (deps)',
-        'WebKit Mac Builder (deps)',
-        'WebKit Mac10.6 (deps)',
-        'WebKit Linux (deps)',
     ],
     'tryserver.chromium': [
         # These don't exist, but are just here to satisfy recipes.
