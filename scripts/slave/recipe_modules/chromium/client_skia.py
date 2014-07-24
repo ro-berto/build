6179e6d6ebd3ad3171c8e946f8fc366ceec81dfd
borenet@google.com <borenet@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-24 19:16:43 +0000

    Add Win and Mac canaries to client.skia, use correct hostnames
    BUG=skia:761
    
    Review URL: https://codereview.chromium.org/411063005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@285348 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium/client_skia.py
+++ scripts/slave/recipe_modules/chromium/client_skia.py
@@ -3,11 +3,21 @@
 # found in the LICENSE file.
 
 from . import chromium_linux
+from . import chromium_mac
+from . import chromium_win
 import copy
 
 # The Skia config just clones some regular Chromium builders, except that they
 # use an up-to-date Skia.
-_builders = ['Linux Builder', 'Linux Tests']
+
+# This list specifies which Chromium builders to "copy".
+_builders = [
+#  SPEC Module     Test Spec File         Builder Names
+  (chromium_linux, 'chromium.linux.json', ['Linux Builder', 'Linux Tests']),
+  (chromium_win,   'chromium.win.json',   ['Win Builder', 'Win7 Tests (1)']),
+  (chromium_mac,   'chromium.mac.json',   ['Mac Builder', 'Mac10.7 Tests (1)']),
+]
+
 SPEC = {
   'settings': {
     'build_gs_bucket': 'chromium-skia-gm',
@@ -15,8 +25,9 @@ SPEC = {
   'builders': {},
 }
 
-for builder_name in _builders:
-  builder_cfg = copy.deepcopy(chromium_linux.SPEC['builders'][builder_name])
-  builder_cfg['recipe_config'] = 'chromium_skia'
-  builder_cfg['testing']['test_spec_file'] = 'chromium.linux.json'
-  SPEC['builders'][builder_name] = builder_cfg
+for spec_module, test_spec_file, builders_list in _builders:
+  for builder_name in builders_list:
+    builder_cfg = copy.deepcopy(spec_module.SPEC['builders'][builder_name])
+    builder_cfg['recipe_config'] = 'chromium_skia'
+    builder_cfg['testing']['test_spec_file'] = test_spec_file
+    SPEC['builders'][builder_name] = builder_cfg
