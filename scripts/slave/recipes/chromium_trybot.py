f75279b0db933889d88795bcb41ee36a405df541
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 12:58:19 +0000

    Run the windows mini_installer tests on chromium win trybots.
    BUG=264859
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=286840
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=286883
    
    Review URL: https://codereview.chromium.org/423533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@287765 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_trybot.py
+++ scripts/slave/recipes/chromium_trybot.py
@@ -610,6 +610,10 @@ def GenSteps(api):
   tests.append(api.chromium.steps.NaclIntegrationTest())
   tests.append(api.chromium.steps.MojoPythonTests())
 
+  # test_installer only works on 32-bit builds; http://crbug.com/399643
+  if api.platform.is_win and api.chromium.c.TARGET_BITS == 32:
+    tests.append(api.chromium.steps.MiniInstallerTest())
+
   compile_targets.extend(bot_config.get('compile_targets', []))
   compile_targets.extend(api.itertools.chain(
       *[t.compile_targets(api) for t in tests]))
