01bd1f45288b85b6b2fbf378878f7d1c7a54bd1a
borenet@google.com <borenet@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 00:28:22 +0000

    Add file recipe api
    BUG=skia:761
    
    Review URL: https://codereview.chromium.org/425063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@288175 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipe_modules/file/api.py
@@ -0,0 +1,40 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from slave import recipe_api
+
+
+class FileApi(recipe_api.RecipeApi):
+  """FileApi contains helper functions for reading and writing files."""
+
+  def __init__(self, **kwargs):
+    super(FileApi, self).__init__(**kwargs)
+
+  def read(self, name, path, **kwargs):
+    """Read a file and return its contents."""
+    return self.m.python.inline(
+        name,
+        """
+        import shutil
+        import sys
+        shutil.copy(sys.argv[1], sys.argv[2])
+        """,
+        args=[path, self.m.raw_io.output()],
+        add_python_log=False,
+        **kwargs
+    ).raw_io.output
+
+  def write(self, name, path, data, **kwargs):
+    """Write the given data to a file."""
+    return self.m.python.inline(
+        name,
+        """
+        import shutil
+        import sys
+        shutil.copy(sys.argv[1], sys.argv[2])
+        """,
+        args=[self.m.raw_io.input(data), path],
+        **kwargs
+    )
+
---
+++ scripts/slave/recipe_modules/file/example.py
@@ -0,0 +1,36 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+DEPS = [
+  'file',
+  'raw_io',
+]
+
+
+TEST_CONTENTS = {
+  'simple': 'abcde',
+  'spaces': 'abcde fgh',
+  'symbols': '! ~&&',
+  'multiline': '''ab
+cd
+efg
+''',
+}
+
+
+def GenSteps(api):
+  for name, content in TEST_CONTENTS.iteritems():
+    api.file.write('write_%s' % name, 'tmp_file.txt', content)
+    actual_content = api.file.read(
+        'read_%s' % name, 'tmp_file.txt',
+        step_test_data=lambda: api.raw_io.test_api.output(content)
+    )
+    msg = 'expected %s but got %s' % (content, actual_content)
+    assert actual_content == content, msg
+
+
+def GenTests(api):
+  yield api.test('file_io')
+
