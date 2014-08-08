b4c5dfcac7bb3c14cca7c22e031eafb3bbafaf47
nodir@chromium.org <nodir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-08 23:10:16 +0000

    WebRTC: bot_update in standalone
    Added bot_update step to webrtc/standalone recipe.
    
    TBR=iannucci@google.com, kjellander@google.com
    BUG=393786
    
    Review URL: https://codereview.chromium.org/456023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@288462 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/webrtc/standalone.py
+++ scripts/slave/recipes/webrtc/standalone.py
@@ -5,6 +5,7 @@
 # Recipe for building and running tests for WebRTC stand-alone.
 
 DEPS = [
+  'bot_update',
   'chromium',
   'gclient',
   'path',
@@ -44,7 +45,7 @@ def GenSteps(api):
   if api.tryserver.is_tryserver:
     api.chromium.apply_config('trybot_flavor')
 
-  step_result = api.gclient.checkout()
+  step_result = api.bot_update.ensure_checkout(force=True)
   # Whatever step is run right before this line needs to emit got_revision.
   got_revision = step_result.presentation.properties['got_revision']
 
