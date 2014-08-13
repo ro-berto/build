37075d7ca1050c1ee760ad8e6b1fd59215e68955
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 20:01:00 +0000

    Add very simple recipe for infra trybot
    BUG=402776
    
    Review URL: https://codereview.chromium.org/465253004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@289355 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gclient/config.py
+++ scripts/slave/recipe_modules/gclient/config.py
@@ -349,3 +349,9 @@ def pdfium(c):
   soln = c.solutions.add()
   soln.name = 'pdfium'
   soln.url = 'https://pdfium.googlesource.com/pdfium.git'
+
+@config_ctx()
+def infra(c):
+  soln = c.solutions.add()
+  soln.name = 'infra'
+  soln.url = 'https://chromium.googlesource.com/infra/infra.git'
