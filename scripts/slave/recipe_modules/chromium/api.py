ro-berto/build

commit 54a26cb14edb5c2971669c6c8887cd42305cc227

Re-land r289084 - "Make telemetry*_unittests upload to test-results.appspot.com"
This version differs from the prior attempt by

a) changing the logic so that we only upload the results if we're not running on a trysever.

b) retrieving the repo revisions from the data that bot_update got, rather than whatever was passed in as part of build_properties. Turns out that the build_properties would've only had 'parent_got_revision' set and not 'got_revision', so we were likely referencing keys that didn't exist.

As part of (b), this changes adds a "properties" property to the bot_update recipe module so that we can retrieve what bot_update actually did.

R=agable@chromium.org
BUG=323212, 403564

Review URL: https://codereview.chromium.org/476503002

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@289434 0039d316-1c4b-4281-b951-d872f2087c98

scripts/slave/recipe_modules/chromium/api.py
@@ -309,35 +309,47 @@ class ChromiumApi(recipe_api.RecipeApi):
         **kwargs)
 
   def run_telemetry_unittests(self, suffix=None, cmd_args=None, **kwargs):
-    name = 'telemetry_unittests'
-    if suffix:
-      name += ' (%s)' % suffix
-    cmd_args = cmd_args or []
-    args = ['--browser=%s' % self.c.build_config_fs.lower(),
-            '--retry-limit=3'] + cmd_args
-    return self.runtest(
+    return self._run_telemetry_script(
+        'telemetry_unittests',
         self.m.path['checkout'].join('tools', 'telemetry', 'run_tests'),
-        args=args,
-        annotate='gtest',
-        name=name,
-        test_type='telemetry_unittests',
-        python_mode=True,
-        xvfb=True,
-        **kwargs)
+        suffix, cmd_args, **kwargs)
 
   def run_telemetry_perf_unittests(self, suffix=None, cmd_args=None, **kwargs):
-    name = 'telemetry_perf_unittests'
+    return self._run_telemetry_script(
+        'telemetry_perf_unittests',
+        self.m.path['checkout'].join('tools', 'perf', 'run_tests'),
+        suffix, cmd_args, **kwargs)
+
+  def _run_telemetry_script(self, name, script_path, suffix,
+                            cmd_args, **kwargs):
+    test_type = name
     if suffix:
       name += ' (%s)' % suffix
     cmd_args = cmd_args or []
+
     args = ['--browser=%s' % self.c.build_config_fs.lower(),
-            '--retry-limit=3'] + cmd_args
+            '--retry-limit=3']
+
+    if not self.m.tryserver.is_tryserver:
+      chromium_revision = self.m.bot_update.properties['got_revision']
+      blink_revision = self.m.bot_update.properties['got_webkit_revision']
+      args += [
+          '--builder-name=%s' % self.m.properties['buildername'],
+          '--master-name=%s' % self.m.properties['mastername'],
+          '--test-results-server=%s' % 'test-results.appspot.com',
+          '--metadata', 'chromium_revision=%s' % chromium_revision,
+          '--metadata', 'blink_revision=%s' % blink_revision,
+          '--metadata', 'build_number=%s' % self.m.properties['buildnumber'],
+      ]
+
+    args += cmd_args
+
     return self.runtest(
-        self.m.path['checkout'].join('tools', 'perf', 'run_tests'),
+        script_path,
         args=args,
         annotate='gtest',
         name=name,
-        test_type='telemetry_perf_unittests',
+        test_type=test_type,
         python_mode=True,
         xvfb=True,
         **kwargs)

scripts/slave/recipe_modules/bot_update/api.py
@@ -48,12 +48,20 @@ def jsonish_to_python(spec, is_top=False):
 
 class BotUpdateApi(recipe_api.RecipeApi):
 
+  def __init__(self, *args, **kwargs):
+      self._properties = {}
+      super(BotUpdateApi, self).__init__(*args, **kwargs)
+
   def __call__(self, name, cmd, **kwargs):
     """Wrapper for easy calling of bot_update."""
     assert isinstance(cmd, (list, tuple))
     bot_update_path = self.m.path['build'].join(
         'scripts', 'slave', 'bot_update.py')
     return self.m.python(name, bot_update_path, cmd, **kwargs)
+
+  @property
+  def properties(self):
+      return self._properties
 
   def ensure_checkout(self, gclient_config=None, suffix=None,
                       patch=True, update_presentation=True,
@@ -158,12 +166,12 @@ class BotUpdateApi(recipe_api.RecipeApi):
     finally:
       step_result = self.m.step.active_result
 
+      self._properties = step_result.json.output.get('properties', {})
+
       if update_presentation:
         # Set properties such as got_revision.
-        if 'properties' in step_result.json.output:
-          properties = step_result.json.output['properties']
-          for prop_name, prop_value in properties.iteritems():
-            step_result.presentation.properties[prop_name] = prop_value
+        for prop_name, prop_value in self.properties.iteritems():
+          step_result.presentation.properties[prop_name] = prop_value
       # Add helpful step description in the step UI.
       if 'step_text' in step_result.json.output:
         step_text = step_result.json.output['step_text']

scripts/slave/recipe_modules/chromium/__init__.py
@@ -1,4 +1,5 @@
 DEPS = [
+  'bot_update',
   'gclient',  # in order to have set_config automatically populate gclient
   'json',
   'path',
@@ -7,4 +8,5 @@ DEPS = [
   'python',
   'raw_io',
   'step',
+  'tryserver',
 ]

scripts/slave/runtest.py
@@ -43,7 +43,10 @@ sys.path.insert(0, os.path.abspath('src/tools/python'))
 
 from common import chromium_utils
 from common import gtest_utils
+
+# TODO(crbug.com/403564). We almost certainly shouldn't be importing this.
 import config
+
 from slave import annotation_utils
 from slave import build_directory
 from slave import crash_utils

commit 6eb236d4105f4e2cc5821b4e59a57f2cf10acdda

Fix regression introduced in r289434 - I missed the test_type.
We need to specify the '--test-type' field in the telemetry*
unittests, and I missed that from the recipe.

TBR=tonyg@chromium.org
BUG=323212

Review URL: https://codereview.chromium.org/466203005

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@289446 0039d316-1c4b-4281-b951-d872f2087c98

scripts/slave/recipe_modules/chromium/api.py
@@ -337,6 +337,7 @@ class ChromiumApi(recipe_api.RecipeApi):
           '--builder-name=%s' % self.m.properties['buildername'],
           '--master-name=%s' % self.m.properties['mastername'],
           '--test-results-server=%s' % 'test-results.appspot.com',
+          '--test-type=%s' % test_type,
           '--metadata', 'chromium_revision=%s' % chromium_revision,
           '--metadata', 'blink_revision=%s' % blink_revision,
           '--metadata', 'build_number=%s' % self.m.properties['buildnumber'],