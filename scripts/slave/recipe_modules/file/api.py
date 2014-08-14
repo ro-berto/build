80859ceaaf260a09d71a0fac5651ae24a96a745e
borenet@google.com <borenet@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 17:50:33 +0000

    Add copy and copytree to file api
    Split out from https://codereview.chromium.org/442863002
    BUG=
    
    Review URL: https://codereview.chromium.org/463533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@289646 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/file/api.py
+++ scripts/slave/recipe_modules/file/api.py
@@ -11,14 +11,8 @@ class FileApi(recipe_api.RecipeApi):
   def __init__(self, **kwargs):
     super(FileApi, self).__init__(**kwargs)
 
-  def read(self, name, path, test_data=None):
-    """Read a file and return its contents."""
-    kwargs = {}
-    if test_data is not None:
-      kwargs['step_test_data'] = (
-          lambda: self.m.raw_io.test_api.output(test_data)
-      )
-
+  def copy(self, name, source, dest, step_test_data=None):
+    """Copy a file."""
     return self.m.python.inline(
         name,
         """
@@ -26,10 +20,32 @@ class FileApi(recipe_api.RecipeApi):
         import sys
         shutil.copy(sys.argv[1], sys.argv[2])
         """,
-        args=[path, self.m.raw_io.output()],
+        args=[source, dest],
+        add_python_log=False,
+        step_test_data=step_test_data
+    )
+
+  def copytree(self, name, source, dest, **kwargs):
+    """Run shutil.copytree in a step."""
+    return self.m.python.inline(
+        name,
+        """
+        import shutil
+        import sys
+        shutil.copytree(sys.argv[1], sys.argv[2])
+        """,
+        args=[source, dest],
         add_python_log=False,
         **kwargs
-    ).raw_io.output
+    )
+
+  def read(self, name, path, test_data=None):
+    """Read a file and return its contents."""
+    step_test_data = None
+    if test_data is not None:
+      step_test_data = lambda: self.m.raw_io.test_api.output(test_data)
+    return self.copy(name, path, self.m.raw_io.output(),
+                     step_test_data=step_test_data).raw_io.output
 
   def write(self, name, path, data, **kwargs):
     """Write the given data to a file."""
@@ -41,6 +57,7 @@ class FileApi(recipe_api.RecipeApi):
         shutil.copy(sys.argv[1], sys.argv[2])
         """,
         args=[self.m.raw_io.input(data), path],
+        add_python_log=False,
         **kwargs
     )
 
--- scripts/slave/recipe_modules/file/example.py
+++ scripts/slave/recipe_modules/file/example.py
@@ -5,6 +5,7 @@
 
 DEPS = [
   'file',
+  'path',
   'raw_io',
 ]
 
@@ -29,6 +30,21 @@ def GenSteps(api):
     )
     msg = 'expected %s but got %s' % (content, actual_content)
     assert actual_content == content, msg
+
+  # copytree
+  content = 'some file content'
+  tmp_dir = api.path['slave_build'].join('copytree_example_tmp')
+  api.path.makedirs('makedirs', tmp_dir)
+  path = tmp_dir.join('dummy_file')
+  api.file.write('write %s' % path, path, content)
+  new_tmp = api.path['slave_build'].join('copytree_example_tmp2')
+  new_path = new_tmp.join('dummy_file')
+  api.file.copytree('copytree', tmp_dir, new_tmp)
+  actual_content = api.file.read('read %s' % new_path, new_path,
+                                 test_data=content)
+  api.path.rmtree('cleanup', tmp_dir)
+  api.path.rmtree('cleanup2', new_tmp)
+  assert actual_content == content
 
 
 def GenTests(api):
