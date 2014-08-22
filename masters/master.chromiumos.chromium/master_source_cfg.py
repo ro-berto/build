4bbec2afa087ef3f449838aa843f022ff5ebf4ac
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 18:36:37 +0000

    Switch chromiumos.chromium to use GitilesPoller in Git mode.
    The SyncChrome step is failing because it tries to access /+/<svn-rev>/ urls
    on googlesource. That obviously doesn't work. It's getting the svn-rev from
    the revision build property, which is set by the poller. This switches it to
    start using git now, so that the revision property is a git hash instead.
    
    TBR=dnj@chromium.org, szager@chromium.org
    BUG=406454
    
    Review URL: https://codereview.chromium.org/499563005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291457 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromiumos.chromium/master_source_cfg.py
+++ masters/master.chromiumos.chromium/master_source_cfg.py
@@ -5,6 +5,7 @@
 from common import chromium_utils
 
 from master import chromium_svn_poller
+from master import gitiles_poller
 from master import master_config
 
 defaults = {}
@@ -13,6 +14,13 @@ helper = master_config.Helper(defaults)
 helper.Scheduler('chromium_src', branch='src', treeStableTimer=60)
 
 def Update(config, _active_master, c):
-  poller = chromium_svn_poller.ChromiumSvnPoller()
+  poller = gitiles_poller.GitilesPoller(
+    repo_url='https://chromium.googlesource.com/chromium/src',
+    branches=['master'],
+    revlinktmpl='http://src.chromium.org/viewvc/chrome?view=rev&revision=%s',
+    pollInterval=10,
+    svn_mode=False,
+    change_filter=chromium_svn_poller.ChromiumChangeFilter,
+  )
   c['change_source'].append(poller)
   return helper.Update(c)
