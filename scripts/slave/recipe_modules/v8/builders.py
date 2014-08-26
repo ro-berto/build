7df85dd69d04bab00b1c19f0194d93a50ce7313b
machenbach@chromium.org <machenbach@chromium.org>
2014-08-26 06:58:01 +0000

    Revert of Revert of V8 Buildbot: Use MSVS 2013 on windows bots. (patchset #1 of https://codereview.chromium.org/462633004/)
    Reason for revert:
    XP slaves are ready.
    
    Original issue's description:
    > Revert of V8 Buildbot: Use MSVS 2013 on windows bots. (https://codereview.chromium.org/458263002/)
    > 
    > Reason for revert:
    > XP slaves can't execute the builds.
    > 
    > Original issue's description:
    > > V8 Buildbot: Use MSVS 2013 on windows bots.
    > > 
    > > BUG=400033,400289
    > > TBR=jochen@chromium.org
    > > 
    > > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=288720
    > 
    > TBR=jochen@chromium.org,svenpanne@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=400033,400289
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=288748
    
    TBR=jochen@chromium.org,svenpanne@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=400033,400289
    
    Review URL: https://codereview.chromium.org/501283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291630 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -292,6 +292,7 @@ BUILDERS = {
       },
 ####### Category: Windows
       'V8 Win32 - builder': {
+        'v8_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 32,
@@ -301,6 +302,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'V8 Win32 - debug builder': {
+        'v8_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 32,
@@ -336,7 +338,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'V8 Win32 - nosnap - shared': {
-        'v8_apply_config': ['no_snapshot'],
+        'v8_apply_config': ['msvs2013', 'no_snapshot'],
         'chromium_apply_config': ['shared_library', 'no_snapshot'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
@@ -386,6 +388,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'V8 Win64': {
+        'v8_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 64,
