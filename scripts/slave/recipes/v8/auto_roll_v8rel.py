6c8c5093914f7329b7dcb24acc579ffd08028241
machenbach@chromium.org <machenbach@chromium.org>
2014-09-02 07:57:20 +0000

    V8 Buildbot: Let v8rel recipe upload the release data.
    BUG=408523
    
    Review URL: https://codereview.chromium.org/526093003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291763 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/v8/auto_roll_v8rel.py
+++ scripts/slave/recipes/v8/auto_roll_v8rel.py
@@ -23,8 +23,6 @@ def GenSteps(api):
                   api.path['slave_build'].join('v8.zip'),
                   api.path['slave_build'].join('v8'))
   api.gclient.set_config('chromium')
-  # FIXME(machenbach): Branch heads are checked out for sub projects
-  # not chromium.
   api.bot_update.ensure_checkout(
       force=True, no_shallow=True, with_branch_heads=True)
   api.step(
@@ -32,9 +30,14 @@ def GenSteps(api):
       [api.path['slave_build'].join(
            'v8', 'tools', 'push-to-trunk', 'releases.py'),
        '-c', api.path['checkout'],
+       '--json', api.path['slave_build'].join('v8-releases-update.json'),
        '--branch', 'recent'],
       cwd=api.path['slave_build'].join('v8'),
     )
+  api.gsutil.upload(api.path['slave_build'].join('v8-releases-update.json'),
+                    'chromium-v8-auto-roll',
+                    api.path.join('v8rel', 'v8-releases-update.json'))
+
 
 def GenTests(api):
   yield api.test('standard') + api.properties.generic(mastername='client.v8')
