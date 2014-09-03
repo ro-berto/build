0eeb52aed5fe87196e6e7e4466107a735ecf8746
machenbach@chromium.org <machenbach@chromium.org>
2014-09-03 12:26:31 +0000

    V8 Buildbot: Add deps roll recipe.
    This wraps the v8-side deps roller and calls it as dry run.
    
    BUG=408523
    
    Review URL: https://codereview.chromium.org/534173002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291783 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/v8/auto_roll_deps.py
@@ -0,0 +1,33 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'chromium',
+  'gclient',
+  'path',
+  'properties',
+  'python',
+]
+
+def GenSteps(api):
+  api.gclient.set_config('chromium')
+  api.gclient.apply_config('v8_bleeding_edge_git')
+  api.bot_update.ensure_checkout(force=True, no_shallow=True)
+
+  api.python(
+      'roll deps',
+      api.path['checkout'].join(
+          'v8', 'tools', 'push-to-trunk', 'auto_roll.py'),
+      ['--chromium', api.path['checkout'],
+       '--author', 'v8-autoroll@chromium.org',
+       '--reviewer', 'v8-autoroll@chromium.org',
+       '--roll', '--dry-run'],
+      cwd=api.path['checkout'].join('v8'),
+    )
+  api.chromium.cleanup_temp()
+
+
+def GenTests(api):
+  yield api.test('standard') + api.properties.generic(mastername='client.v8')
