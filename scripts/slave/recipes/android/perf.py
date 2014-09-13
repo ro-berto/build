1a058e53e7b8ec6886b92dc4681072127dc9f572
tonyg@chromium.org <tonyg@chromium.org>
2014-09-13 00:56:48 +0000

    Shard the MotoE perf bot across 8 devices.
    BUG=334683
    
    Review URL: https://codereview.chromium.org/539763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@291960 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/android/perf.py
+++ scripts/slave/recipes/android/perf.py
@@ -59,7 +59,7 @@ BUILDERS = {
     'bucket': 'chrome-perf',
     'path': lambda api: ('android_perf_rel/full-build-linux_%s.zip'
                                % api.properties['parent_revision']),
-    'num_device_shards': 1,
+    'num_device_shards': 8,
   },
 }
 
