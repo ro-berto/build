a53f2ae415f16202001f543ee28be1eefec8227e
kjellander@chromium.org <kjellander@chromium.org>
2014-09-23 07:26:20 +0000

    Switch client.libyuv and tryserver.libyuv to recipes.
    Using the recipe module and recipe added in
    https://codereview.chromium.org/589883002/
    
    This changes one bot: TSan is removed and TSan v2 is introduced.
    
    BUG=416301
    TBR=agable@chromium.org
    
    Review URL: https://codereview.chromium.org/587633004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292086 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.client.libyuv/master_android_cfg.py
@@ -0,0 +1,35 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from buildbot.schedulers.basic import SingleBranchScheduler
+
+from master.factory import annotator_factory
+
+m_annotator = annotator_factory.AnnotatorFactory()
+
+def Update(c):
+  c['schedulers'].extend([
+      SingleBranchScheduler(name='libyuv_android_scheduler',
+                            branch='trunk',
+                            treeStableTimer=0,
+                            builderNames=[
+          'Android Debug',
+          'Android Release',
+      ]),
+  ])
+
+  specs = [
+    {'name': 'Android Debug'},
+    {'name': 'Android Release'},
+  ]
+
+  c['builders'].extend([
+      {
+        'name': spec['name'],
+        'factory': m_annotator.BaseFactory('libyuv/libyuv'),
+        'notify_on_missing': True,
+        'category': 'android',
+        'slavebuilddir': spec.get('slavebuilddir', 'android'),
+      } for spec in specs
+  ])
