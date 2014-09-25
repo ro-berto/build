4783ba9690b9e21fd5881aec1c133cd681ad8234
jam@chromium.org <jam@chromium.org>
2014-09-25 00:52:05 +0000

    Don't swarm Ozone builds. Apparently they need specific graphics drivers and hardware.
    Output:
    Failed to load ../out/Release/xdisplaycheck with cwd=/tmp/run_tha_testSvCWuD/chrome
    Xvfb exited, code 1
    Xvfb output:
    > 
    > Fatal server error:
    > Server is already active for display 9
    > 	If this server is no longer running, remove /tmp/.X9-lock
    > 	and start again.
    > 
    
    BUG=414808
    TBR=phajdan.jr@chromium.org
    
    Review URL: https://codereview.chromium.org/600123003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292133 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium/chromium_chromiumos.py
+++ scripts/slave/recipe_modules/chromium/chromium_chromiumos.py
@@ -172,8 +172,6 @@ SPEC = {
       'testing': {
         'platform': 'linux',
       },
-      'enable_swarming': True,
-      'use_isolate': True,
     },
     'Linux ChromiumOS Ozone Tests (1)': {
       'recipe_config': 'chromium_chromeos_ozone',
@@ -189,7 +187,6 @@ SPEC = {
       'testing': {
         'platform': 'linux',
       },
-      'enable_swarming': True,
     },
     'Linux ChromiumOS Ozone Tests (2)': {
       'recipe_config': 'chromium_chromeos_ozone',
@@ -205,7 +202,6 @@ SPEC = {
       'testing': {
         'platform': 'linux',
       },
-      'enable_swarming': True,
     },
 
     'Linux ChromiumOS Builder (dbg)': {
