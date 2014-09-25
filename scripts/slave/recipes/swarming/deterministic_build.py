018ca925e6161e8c2727cb303eef1e09bf690bae
sebmarchand@chromium.org <sebmarchand@chromium.org>
2014-09-25 17:54:31 +0000

    Update the deterministic build recipe to support Android and IOS.
    BUG=383388
    
    Review URL: https://codereview.chromium.org/601593003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292140 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/swarming/deterministic_build.py
+++ scripts/slave/recipes/swarming/deterministic_build.py
@@ -17,11 +17,57 @@ DEPS = [
   'step',
 ]
 
+DETERMINISTIC_BUILDERS = {
+  'Android deterministic build': {
+    'chromium_config': 'android',
+    'gclient_config': 'chromium',
+    'gclient_apply_config': ['android'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 32,
+      'TARGET_PLATFORM': 'android',
+    },
+    'platform': 'linux',
+  },
+  'IOS deterministic build': {
+    'chromium_config': 'chromium_ios_device',
+    'gclient_config': 'ios',
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_PLATFORM': 'ios',
+      'TARGET_BITS': 32,
+    },
+    'platform': 'mac',
+  },
+  'Linux deterministic build': {
+    'chromium_config': 'chromium_no_goma',
+    'gclient_config': 'chromium',
+    'platform': 'linux',
+  },
+  'Mac deterministic build': {
+    'chromium_config': 'chromium_no_goma',
+    'gclient_config': 'chromium',
+    'platform': 'mac',
+  },
+  'Windows deterministic build': {
+    'chromium_config': 'chromium_no_goma',
+    'gclient_config': 'chromium',
+    'platform': 'win',
+  },
+}
 
 def GenSteps(api):
-  api.chromium.set_config('chromium_no_goma',
-                          api.properties.get('configuration', 'Release'))
-  api.gclient.set_config('chromium')
+  buildername = api.properties['buildername']
+  recipe_config = DETERMINISTIC_BUILDERS[buildername]
+
+  api.chromium.set_config(recipe_config['chromium_config'],
+                          **recipe_config.get('chromium_config_kwargs',
+                                              {'BUILD_CONFIG': 'Release'}))
+
+  api.gclient.set_config(recipe_config['gclient_config'],
+                         **recipe_config.get('gclient_config_kwargs', {}))
+  for c in recipe_config.get('gclient_apply_config', []):
+    api.gclient.apply_config(c)
 
   # Checkout chromium.
   api.bot_update.ensure_checkout(force=True)
@@ -30,12 +76,20 @@ def GenSteps(api):
   api.chromium.compile(targets=['base_unittests'], force_clobber=True)
 
 
+def _sanitize_nonalpha(text):
+  return ''.join(c if c.isalnum() else '_' for c in text)
+
+
 def GenTests(api):
-  for platform in ('linux', 'win', 'mac'):
-    for configuration in ('Debug', 'Release'):
-      yield (
-        api.test('%s_%s' % (platform, configuration)) +
-        api.platform.name(platform) +
-        api.properties.scheduled() +
-        api.properties(configuration=configuration)
-      )
+  mastername = 'chromium.swarm'
+  for buildername in DETERMINISTIC_BUILDERS:
+    test_name = 'full_%s_%s' % (_sanitize_nonalpha(mastername),
+                                _sanitize_nonalpha(buildername))
+    yield (
+      api.test(test_name) +
+      api.properties.scheduled() +
+      api.properties.generic(buildername=buildername,
+                             mastername=mastername) +
+      api.platform(DETERMINISTIC_BUILDERS[buildername]['platform'], 32) +
+      api.properties(configuration='Release')
+    )
