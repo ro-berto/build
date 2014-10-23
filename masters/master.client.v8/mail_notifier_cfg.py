32b1e006676f8b823e00ee13276737e722ea6355
ishell@chromium.org <ishell@chromium.org>
2014-10-23 10:02:31 +0000

    mem_sheriff mailing category added.
    Review URL: https://codereview.chromium.org/670253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292615 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8/mail_notifier_cfg.py
+++ masters/master.client.v8/mail_notifier_cfg.py
@@ -6,33 +6,37 @@ from buildbot.status.builder import FAILURE
 from master import chromium_notifier
 from master import master_utils
 
+
+v8_steps = [
+  'update',
+  'runhooks',
+  'gn',
+  'compile',
+  'Presubmit',
+  'Static-Initializers',
+  'Check',
+  'Unittests',
+  'OptimizeForSize',
+  'Mjsunit',
+  'Webkit',
+  'Benchmarks',
+  'Test262',
+  'Mozilla',
+  'GCMole',
+  'Fuzz',
+  'Deopt Fuzz',
+  'Simple Leak Check',
+  # TODO(machenbach): Enable mail notifications as soon as a try builder is
+  # set up.
+  # 'webkit_tests',
+]
+
 # This is the list of the builder categories and the corresponding critical
 # steps. If one critical step fails, the blame list will be notified.
 # Note: don't include 'update scripts' since we can't do much about it when
 # it's failing and the tree is still technically fine.
 categories_steps = {
-  '': [
-    'update',
-    'runhooks',
-    'gn',
-    'compile',
-    'Presubmit',
-    'Static-Initializers',
-    'Check',
-    'Unittests',
-    'OptimizeForSize',
-    'Mjsunit',
-    'Webkit',
-    'Benchmarks',
-    'Test262',
-    'Mozilla',
-    'GCMole',
-    'Fuzz',
-    'Deopt Fuzz',
-    # TODO(machenbach): Enable mail notifications as soon as a try builder is
-    # set up.
-    # 'webkit_tests',
-  ],
+  '': v8_steps,
   'asan': [
     'browser_tests',
     'net',
@@ -55,6 +59,7 @@ forgiving_steps = ['update_scripts', 'update', 'svnkill', 'taskkill',
                    'gclient_revert']
 
 x87_categories_steps = {'x87': ['runhooks', 'compile', 'Check']}
+mem_sheriff_categories_steps = {'mem_sheriff': v8_steps}
 
 class V8Notifier(chromium_notifier.ChromiumNotifier):
   def isInterestingStep(self, build_status, step_status, results):
@@ -79,6 +84,16 @@ def Update(config, active_master, c):
       relayhost=config.Master.smtp,
       sendToInterestedUsers=False,
       extraRecipients=['weiliang.lin@intel.com', 'chunyang.dai@intel.com'],
+      status_header='buildbot failure in %(project)s on %(builder)s, %(steps)s',
+      lookup=master_utils.FilterDomain(),
+      forgiving_steps=forgiving_steps))
+  c['status'].append(V8Notifier(
+      fromaddr=active_master.from_address,
+      categories_steps=mem_sheriff_categories_steps,
+      exclusions={},
+      relayhost=config.Master.smtp,
+      sendToInterestedUsers=False,
+      extraRecipients=['ishell@chromium.org'],
       status_header='buildbot failure in %(project)s on %(builder)s, %(steps)s',
       lookup=master_utils.FilterDomain(),
       forgiving_steps=forgiving_steps))
