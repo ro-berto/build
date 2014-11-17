de12d20fc5e1004aa6e6e8859b2547e45d6b4dc6
aneeshm@chromium.org <aneeshm@chromium.org>
2014-11-17 23:17:22 +0000

    Change pdfium ports
    BUG=375773
    
    Review URL: https://codereview.chromium.org/733253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@292979 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.pdfium/master_site_config.py
+++ masters/master.client.pdfium/master_site_config.py
@@ -9,7 +9,7 @@ from config_bootstrap import Master
 class Pdfium(Master.Master3):
   project_name = 'Pdfium'
   project_url = 'https://code.google.com/p/pdfium/'
-  master_port = 30300
-  slave_port = 40300
-  master_port_alt = 50300
+  master_port = 21411
+  slave_port = 31411
+  master_port_alt = 41411
   buildbot_url = 'http://build.chromium.org/p/master.pdfium/'
