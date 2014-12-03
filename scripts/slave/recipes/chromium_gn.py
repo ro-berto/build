196c19cb8ab44856ae7690b668d8df848eb4c585
dpranke@chromium.org <dpranke@chromium.org>
2014-12-03 01:20:48 +0000

    Enable goma on the win GN bots.
    R=scottmg@chromium.org
    BUG=354261
    
    Review URL: https://codereview.chromium.org/774763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293206 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_gn.py
+++ scripts/slave/recipes/chromium_gn.py
@@ -313,12 +313,7 @@ def GenSteps(api):
 
   api.chromium.runhooks()
 
-  # TODO(scottmg): goma doesn't work on windows GN builds yet.
-  is_windows = ('Win8' in buildername or 'win8_' in buildername)
-  api.chromium.run_gn(use_goma=not is_windows)
-  if is_windows:
-    api.chromium.c.compile_py.compiler = None
-    api.chromium.c.compile_py.goma_dir = None
+  api.chromium.run_gn(use_goma=True)
 
   api.chromium.compile(targets=['all'])
 
