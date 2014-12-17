9b38f7fd1b38d3bf2336a992b081dfe4a62fcb06
dpranke@chromium.org <dpranke@chromium.org>
2014-12-17 00:19:45 +0000

    Add a set of optional tryservers that will build and upload GN binaries.
    These can be used to stage new versions of GN to be rolled into Chromium.
    
    R=iannucci@chromium.org, scottmg@chromium.org, brettw@chromium.org
    BUG=430215
    
    Review URL: https://codereview.chromium.org/798803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293412 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/chromium_gn_upload.py
@@ -0,0 +1,94 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'chromium',
+  'file',
+  'gsutil',
+  'path',
+  'platform',
+  'properties',
+  'step',
+]
+
+
+BUILDERS = {
+  'tryserver.chromium.linux': {
+    'builders': {
+      'linux_chromium_gn_upload_x64': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'linux',
+          'TARGET_BITS': 32,
+        },
+
+        # We need this to pull the Linux sysroots.
+        'gclient_apply_config': ['chrome_internal'],
+      },
+      'linux_chromium_gn_upload_x86': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'linux',
+          'TARGET_BITS': 32,
+        },
+
+        # We need this to pull the Linux sysroots.
+        'gclient_apply_config': ['chrome_internal'],
+      },
+    },
+  },
+  'tryserver.chromium.mac': {
+    'builders': {
+      'mac_chromium_gn_upload': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'mac',
+          'TARGET_BITS': 64,
+        },
+      },
+    },
+  },
+  'tryserver.chromium.win': {
+    'builders': {
+      'win8_chromium_gn_upload': {
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'win',
+          'TARGET_BITS': 64,
+        },
+      },
+    },
+  },
+}
+
+
+def GenSteps(api):
+  _, bot_config = api.chromium.configure_bot(BUILDERS, ['gn_for_uploads'])
+
+  api.bot_update.ensure_checkout(
+      force=True, patch_root=bot_config.get('root_override'))
+
+  api.chromium.runhooks()
+
+  api.chromium.run_gn()
+
+  api.chromium.compile(targets=['gn', 'gn_unittests'], force_clobber=True)
+
+  path_to_binary = str(api.path['build'].join('Release').join('gn'))
+  if api.platform.is_win:
+    path_to_binary += '.exe'
+
+  api.step('gn version', [path_to_binary, '--version'])
+
+  api.chromium.runtest('gn_unittests')
+
+  sha1 = api.file.sha1('compute sha1', path_to_binary, display_result=True)
+
+  api.gsutil.upload(path_to_binary, 'chromium-gn', sha1)
+
+
+def GenTests(api):
+  for test in api.chromium.gen_tests_for_builders(BUILDERS):
+    yield test
