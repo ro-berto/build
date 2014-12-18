852742b92c2cf7e7d760f297347a303e746aa7ba
akuegel@chromium.org <akuegel@chromium.org>
2014-12-18 08:38:28 +0000

    Replace optparse by argparse in archive_source_codesearch.py
    This CL replaces the usage of optparse with argparse.
    Also, the script is reformatted with pyformat, and a relict from
    package_source.py is removed (there are no index files that need
    to be included when using this script).
    
    Review URL: https://codereview.chromium.org/803443003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293435 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/archive_source_codesearch.py
+++ scripts/slave/archive_source_codesearch.py
@@ -9,40 +9,43 @@ This script is created instead of just running commands from recipe because
 recipe does not support pipe."""
 
 
-import optparse
+import argparse
 import os
 import subprocess
 import sys
 import time
 
+
 def main():
-  option_parser = optparse.OptionParser()
-  option_parser.add_option('-f', '--file', dest='filename',
-                          help='archive file name', metavar='FILE')
-  options, dirs = option_parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--file', dest='filename',
+                      help='archive file name', metavar='FILE')
+  parser.add_argument('directories', nargs='+',
+                      help='directories to be included in the archive')
+  options = parser.parse_args()
   filename = options.filename or 'chromium.tar.bz2'
 
-  for d in dirs:
+  for d in options.directories:
     if not os.path.exists(d):
       raise Exception('ERROR: no %s directory to package, exiting' % d)
 
   print '%s: Creating tar file %s...' % (time.strftime('%X'), filename)
-  find_command = ['find'] + dirs + ['-type', 'f', '-size', '-4M',
-                  # The only files under src/out we want to package up
-                  # are index files and generated sources.
-                  '(', '-regex', '^src/out/.*index$', '-o',
-                       '-regex', '^src/out/[^/]*/gen/.*', '-o',
-                       '!', '-regex', '^src/out/.*', ')',
-                  # Exclude all .svn directories, the native client toolchain
-                  # and the llvm build directory, and perf/data files.
-                  '-a', '!', '-regex', r'.*\.svn.*',
-                  '-a', '!', '-regex', r'.*\.git.*',
-                  '-a', '!', '-regex', '^src/data/.*',
-                  '-a', '!', '-regex', '^src/native_client/toolchain/.*',
-                  '-a', '!', '-regex', '^src/third_party/llvm-build/.*',
-                  '-a', '!', '-regex',
-                    '^src/chrome/tools/test/reference_build/.*',
-                  '-a', '!', '-regex', '^tools/perf/data/.*']
+  find_command = ['find'] + options.directories + [
+      '-type', 'f', '-size', '-4M',
+      # The only files under src/out we want to package up are generated
+      # sources.
+      '(', '-regex', '^src/out/[^/]*/gen/.*', '-o',
+      '!', '-regex', '^src/out/.*', ')',
+      # Exclude all .svn directories, the native client toolchain and the llvm
+      # build directory, and perf/data files.
+      '-a', '!', '-regex', r'.*\.svn.*',
+      '-a', '!', '-regex', r'.*\.git.*',
+      '-a', '!', '-regex', '^src/data/.*',
+      '-a', '!', '-regex', '^src/native_client/toolchain/.*',
+      '-a', '!', '-regex', '^src/third_party/llvm-build/.*',
+      '-a', '!', '-regex', '^src/chrome/tools/test/reference_build/.*',
+      '-a', '!', '-regex', '^tools/perf/data/.*'
+  ]
 
   find_proc = subprocess.Popen(find_command, stdout=subprocess.PIPE)
   tar_proc = subprocess.Popen(['tar', '-T-', '-cjvf', filename],
