5b68241d334295b7f1dde49bd2c4b9132e249807
kjellander@chromium.org <kjellander@chromium.org>
2015-01-04 18:55:38 +0000

    WebRTC: Add Win and Mac FYI bots running tests in parallel.
    Borrowing a Win and Mac VM from client.webrtc master since
    these bots hopefully should exist only for a short while until
    we're ready to run tests in parallel for all platforms.
    
    BUG=445880
    
    Review URL: https://codereview.chromium.org/829063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293515 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.client.webrtc.fyi/master_mac_cfg.py
@@ -0,0 +1,34 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from buildbot.schedulers.basic import SingleBranchScheduler
+
+from master.factory import annotator_factory
+
+m_annotator = annotator_factory.AnnotatorFactory()
+
+def Update(c):
+  c['schedulers'].extend([
+      SingleBranchScheduler(name='webrtc_mac_scheduler',
+                            branch='trunk',
+                            treeStableTimer=0,
+                            builderNames=['Mac64 Debug (parallel)']),
+  ])
+
+  specs = [
+    {
+      'name': 'Mac64 Debug (parallel)',
+      'slavebuilddir': 'mac64',
+    },
+  ]
+
+  c['builders'].extend([
+      {
+        'name': spec['name'],
+        'factory': m_annotator.BaseFactory('webrtc/standalone'),
+        'notify_on_missing': True,
+        'category': 'mac',
+        'slavebuilddir': spec['slavebuilddir'],
+      } for spec in specs
+  ])
--- scripts/slave/recipe_modules/webrtc/builders.py
+++ scripts/slave/recipe_modules/webrtc/builders.py
@@ -883,6 +883,24 @@ BUILDERS = {
   },
   'client.webrtc.fyi': {
     'builders':  {
+      'Win32 Debug (parallel)': {
+        'recipe_config': 'webrtc_parallel',
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_BITS': 32,
+        },
+        'bot_type': 'builder_tester',
+        'testing': {'platform': 'win'},
+      },
+      'Mac64 Debug (parallel)': {
+        'recipe_config': 'webrtc_parallel',
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_BITS': 64,
+        },
+        'bot_type': 'builder_tester',
+        'testing': {'platform': 'mac'},
+      },
       'Linux64 Debug (parallel)': {
         'recipe_config': 'webrtc_parallel',
         'chromium_config_kwargs': {
