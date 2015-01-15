af9cf41b78140c804038f8d0285d8a8eaca2d602
nodir@chromium.org <nodir@chromium.org>
2015-01-15 22:11:53 +0000

    buildbucket test fix
    Adds missing depenendecies to test_env and also includes buildbucket
    tests into PRESUBMIT.py
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/856493003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293659 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -52,6 +52,7 @@ def CommonChecks(input_api, output_api):
         # pylint is fetched in memory with setuptools, it seems it caches
         # sys.path so modifications to sys.path aren't kept.
         join('scripts', 'master', 'unittests'),
+        join('scripts', 'master', 'buildbucket', 'unittests'),
         join('scripts', 'slave', 'unittests'),
         join('scripts', 'tools', 'deps2git'),
         join('site_config'),
