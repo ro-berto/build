b02295827e72d99215bfa1f32ab19d01a3950180
blundell@chromium.org <blundell@chromium.org>
2015-02-02 10:48:28 +0000

    Revert "Upload Mojo shell on Android"
    This reverts commit 4364f13f8c0ee69cdf19f898a6860bfa824fade2.
    
    This change broke the bots due to the argument to the script being specified
    incorrectly.
    
    TBR=dpranke
    
    Review URL: https://codereview.chromium.org/891313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293899 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/mojo.py
+++ scripts/slave/recipes/mojo.py
@@ -117,14 +117,10 @@ def _TestSteps(api):
       pass
 
 
-def _UploadShell(api, buildername):
+def _UploadShell(api):
   upload_path = api.path['checkout'].join('mojo', 'tools',
       'upload_shell_binary.py')
-  is_android = 'Android' in buildername
-  args = ''
-  if is_android:
-    args = '--android'
-  api.python('upload shell binary', upload_path, args)
+  api.python('upload shell binary', upload_path)
 
 
 def GenSteps(api):
@@ -135,28 +131,19 @@ def GenSteps(api):
 
   is_linux = 'Linux' in buildername
   is_win = 'Win' in buildername
-  is_android = 'Android' in buildername
   is_tester = 'Tests' in buildername
-  is_try = api.tryserver.is_tryserver
-  is_asan = 'ASan' in buildername
-  is_perf = 'Perf' in buildername
-  is_nacl = 'NaCl' in buildername
-  upload_shell = ((is_linux or is_android) and build_type == '--release'
-      and not is_try and not is_perf and not is_asan and not is_nacl)
   if not is_tester and not is_linux and not is_win:
-    # TODO(blundell): Eliminate this special case
-    # once there's an Android release tester bot.
-    if upload_shell and is_android:
-      _UploadShell(api, buildername)
     return
 
   _TestSteps(api)
 
-  # TODO(blundell): Remove the "and not is_android" once there's an
-  # Android release tester bot and I've removed the logic uploading the
-  # shell on Android above.
-  if upload_shell and not is_android:
-    _UploadShell(api, buildername)
+  is_try = api.tryserver.is_tryserver
+  is_asan = 'ASan' in buildername
+  is_perf = 'Perf' in buildername
+  is_nacl = 'NaCl' in buildername
+  if (is_linux and build_type == '--release' and not is_try and not is_perf and
+      not is_asan and not is_nacl):
+    _UploadShell(api)
 
 def GenTests(api):
   tests = [
@@ -166,7 +153,6 @@ def GenTests(api):
       ['mojo_linux_asan_dbg', 'Mojo Linux ASan (dbg)'],
       ['mojo_linux_nacl', 'Mojo Linux NaCl'],
       ['mojo_linux_nacl_dbg', 'Mojo Linux NaCl (dbg)'],
-      ['mojo_android_builder', 'Mojo Android Builder'],
       ['mojo_android_dbg', 'Mojo Android (dbg)'],
       ['mojo_android_builder_tests_dbg', 'Mojo Android Builder Tests (dbg)'],
       ['mojo_chromeos_dbg', 'Mojo ChromeOS (dbg)'],
