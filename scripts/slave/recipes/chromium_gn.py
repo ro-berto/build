d75634e2f73d8d72d96bca2bfc6ed706ad3952b2
dpranke@chromium.org <dpranke@chromium.org>
2015-02-03 22:50:12 +0000

    Revert "Try to enable goma on the Win8 GN bots."
    This reverts commit 189b7f7f06c47769a41d41f6ca1516fb46fd02b9.
    
    GOMA appears to not be working :(.
    
    TBR=scottmg@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/882883005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293931 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_gn.py
+++ scripts/slave/recipes/chromium_gn.py
@@ -322,7 +322,12 @@ def GenSteps(api):
 
   api.chromium.runhooks()
 
-  api.chromium.run_gn(use_goma=True)
+  # TODO(dpranke): goma doesn't work on windows GN builds yet.
+  is_windows = ('Win' in buildername or 'win' in buildername)
+  api.chromium.run_gn(use_goma=not is_windows)
+  if is_windows:
+    api.chromium.c.compile_py.compiler = None
+    api.chromium.c.compile_py.goma_dir = None
 
   # crbug.com/451227 - building 'all' on android builds too many
   # things. Really we should be building the 'default' target
