04563b6168c344e4394ffa6c327cb4c32752065a
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-02-09 09:12:33 +0000

    Add a simple nacl sdk recipe
    BUG=453443
    
    Review URL: https://codereview.chromium.org/868463009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@293988 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/nacl/sdk.py
@@ -0,0 +1,36 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'chromium',
+  'gclient',
+  'path',
+  'platform',
+  'properties',
+  'python',
+  'tryserver',
+]
+
+
+def GenSteps(api):
+  api.chromium.set_config('chromium')
+  api.bot_update.ensure_checkout(force=True)
+  api.tryserver.maybe_apply_issue()
+  api.gclient.runhooks()
+  api.chromium.cleanup_temp()
+  api.chromium.compile(['chromium_builder_tests'])
+  api.python(
+      'annotated_steps',
+      api.path['build'].join(
+          'scripts', 'slave', 'chromium', 'nacl_sdk_buildbot_run.py'),
+      allow_subannotations=True)
+
+
+def GenTests(api):
+  yield (
+    api.test('basic') +
+    api.properties.generic() +
+    api.platform('linux', 64)
+  )
