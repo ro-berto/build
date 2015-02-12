d92861bcb5cbf7add439221817da4f252b433ae0
machenbach@chromium.org <machenbach@chromium.org>
2015-02-12 14:40:27 +0000

    V8 Buildbot: Run static-initializer check on trybots.
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/925553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294042 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -1104,7 +1104,13 @@ BUILDERS = {
           'TARGET_BITS': 64,
         },
         'bot_type': 'builder_tester',
-        'tests': ['unittests', 'v8testing', 'optimize_for_size', 'webkit'],
+        'tests': [
+          'v8initializers',
+          'unittests',
+          'v8testing',
+          'optimize_for_size',
+          'webkit',
+        ],
         'testing': {'platform': 'linux'},
       },
       'v8_linux64_nacl_rel': {
