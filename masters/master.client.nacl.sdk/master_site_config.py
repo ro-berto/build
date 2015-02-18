e12b2d97acfd74f306cd3aa598b29ff0c54b2a5f
tandrii@chromium.org <tandrii@chromium.org>
2015-02-18 11:43:21 +0000

    Gatekeeper-ng for master.client.nacl.sdk .
    No gatekeeping for this master, just mailing.
    
    BUG=452589
    R=stip@chromium.org
    
    Review URL: https://codereview.chromium.org/930293004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294105 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.nacl.sdk/master_site_config.py
+++ masters/master.client.nacl.sdk/master_site_config.py
@@ -11,8 +11,4 @@ class NativeClientSDK(Master.NaClBase):
   master_port = 8034
   slave_port = 8134
   master_port_alt = 8234
-  base_app_url = 'https://naclsdk-status.appspot.com'
-  tree_status_url = base_app_url + '/status'
-  store_revisions_url = base_app_url + '/revisions'
-  last_good_url = base_app_url + '/lkgr'
   buildbot_url = 'http://build.chromium.org/p/client.nacl.sdk/'
