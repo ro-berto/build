0940515c7fa3cd0d4e4330fa1f4e0835c25488a0
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-03-04 14:06:09 +0000

    Add recipe code for Linux chromium.webkit builders
    BUG=460949
    
    Review URL: https://codereview.chromium.org/978763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294323 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium/chromium_webkit.py
+++ scripts/slave/recipe_modules/chromium/chromium_webkit.py
@@ -37,4 +37,103 @@ SPEC['builders'].update({
     'enable_swarming': True,
     'use_isolate': True,
   },
+  'WebKit Linux 32': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 32,
+    },
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'linux',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Linux ASAN': {
+    'recipe_config': 'chromium_clang',
+    'chromium_apply_config': ['asan'],
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(extra_args=[
+          '--additional-expectations',
+          'src/third_party/WebKit/LayoutTests/ASANExpectations',
+          # ASAN is roughly 8x slower than Release.
+          '--time-out-ms', '48000',
+          '--options=--enable-sanitizer',
+      ]),
+    ],
+    'testing': {
+      'platform': 'linux',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Linux MSAN': {
+    'recipe_config': 'chromium_clang',
+    'chromium_apply_config': [
+      'instrumented_libraries',
+      'msan',
+      'msan_full_origin_tracking',
+    ],
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(extra_args=[
+          '--additional-expectations',
+          'src/third_party/WebKit/LayoutTests/MSANExpectations',
+          # Because JS code is run on a simulator, the slowdown in JS-heavy
+          # tests will be much higher than MSan's average of 3x.
+          '--time-out-ms', '66000',
+          '--options=--enable-sanitizer',
+      ]),
+    ],
+    'testing': {
+      'platform': 'linux',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Linux (dbg)': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Debug',
+      'TARGET_BITS': 64,
+    },
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'linux',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
 })
