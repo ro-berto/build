f6e3f1fd18ec5db5aea7e93daa2308d960d9503d
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-03-05 15:39:53 +0000

    Add recipe code for non-oilpan Mac chromium.webkit builders
    BUG=460949
    R=tandrii@chromium.org
    
    Review URL: https://codereview.chromium.org/978333002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294350 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium/chromium_webkit.py
+++ scripts/slave/recipe_modules/chromium/chromium_webkit.py
@@ -163,6 +163,188 @@ SPEC['builders'].update({
     'enable_swarming': True,
     'use_isolate': True,
   },
+  'WebKit Mac Builder': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'builder',
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.6': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.7': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.8': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.8 (retina)': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.9': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Release',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac Builder (dbg)': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Debug',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'builder',
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.6 (dbg)': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Debug',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder (dbg)',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
+  'WebKit Mac10.7 (dbg)': {
+    'recipe_config': 'chromium',
+    'gclient_apply_config': ['blink_or_chromium'],
+    'chromium_config_kwargs': {
+      'BUILD_CONFIG': 'Debug',
+      'TARGET_BITS': 64,
+    },
+    'bot_type': 'tester',
+    'parent_buildername': 'WebKit Mac Builder (dbg)',
+    'test_generators': [
+      steps.generate_gtest,
+      steps.generate_script,
+    ],
+    'tests': [
+      steps.BlinkTest(),
+    ],
+    'testing': {
+      'platform': 'mac',
+    },
+    'enable_swarming': True,
+    'use_isolate': True,
+  },
   'WebKit Linux': {
     'recipe_config': 'chromium',
     'gclient_apply_config': ['blink_or_chromium'],
