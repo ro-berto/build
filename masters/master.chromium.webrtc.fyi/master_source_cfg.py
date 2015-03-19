20347e44df2abc263e6d5cb8cd8062e9d405da3e
kjellander@chromium.org <kjellander@chromium.org>
2015-03-19 15:01:28 +0000

    WebRTC: Switch Gitiles pollers to git_mode.
    BUG=438169
    
    Review URL: https://codereview.chromium.org/773733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294497 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webrtc.fyi/master_source_cfg.py
+++ masters/master.chromium.webrtc.fyi/master_source_cfg.py
@@ -7,10 +7,7 @@ from master import gitiles_poller
 
 def Update(config, c):
   webrtc_repo_url = config.Master.git_server_url + '/external/webrtc/'
-  webrtc_poller = gitiles_poller.GitilesPoller(
-      webrtc_repo_url,
-      project='trunk', # This translates to branch of changes.
-      svn_mode=True)
+  webrtc_poller = gitiles_poller.GitilesPoller(webrtc_repo_url)
   c['change_source'].append(webrtc_poller)
 
   samples_poller = gitiles_poller.GitilesPoller(
--- masters/master.client.webrtc.fyi/master_source_cfg.py
+++ masters/master.client.webrtc.fyi/master_source_cfg.py
@@ -7,9 +7,5 @@ from master import gitiles_poller
 
 def Update(config, c):
   webrtc_repo_url = config.Master.git_server_url + '/external/webrtc'
-  poller = gitiles_poller.GitilesPoller(
-      webrtc_repo_url,
-      svn_branch='trunk',
-      revlinktmpl='http://code.google.com/p/webrtc/source/detail?r=%s',
-      svn_mode=True)
+  poller = gitiles_poller.GitilesPoller(webrtc_repo_url)
   c['change_source'].append(poller)
