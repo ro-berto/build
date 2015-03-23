c9a5b78c53efc5d1b7fd535216b62ae42a6c5c0f
tandrii@google.com <tandrii@google.com>
2015-03-23 15:45:08 +0000

    Delete master.client.nacl.sdk.mono .
    BUG=461771
    R=sergiyb@chromium.org
    
    Review URL: https://codereview.chromium.org/1004733005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294531 0039d316-1c4b-4281-b951-d872f2087c98

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -240,7 +240,6 @@ def main(argv):
       'master.client.nacl.ports': 'NativeClientPorts',
       'master.client.nacl.sdk': 'NativeClientSDK',
       'master.client.nacl.sdk.addin': 'NativeClientSDKAddIn',
-      'master.client.nacl.sdk.mono': 'NativeClientSDKMono',
       'master.client.nacl.toolchain': 'NativeClientToolchain',
       'master.client.pdfium': 'Pdfium',
       'master.client.sfntly': 'Sfntly',
