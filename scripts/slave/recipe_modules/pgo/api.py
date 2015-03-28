6412a0d8f5ee86aa6773cd43f35584c9eed1060a
sebmarchand@chromium.org <sebmarchand@chromium.org>
2015-03-28 17:07:30 +0000

    Update the PGO recipe.
    Remove the 'use-live-sites' flag as some benchmarks don't work because of it.
    
    TBR=iannucci@chromium.org
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1040993002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294602 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/pgo/api.py
+++ scripts/slave/recipe_modules/pgo/api.py
@@ -59,7 +59,7 @@ class PGOApi(recipe_api.RecipeApi):
     pgosweep_path = self.m.path['depot_tools'].join(
         'win_toolchain', 'vs2013_files', 'VC', 'bin')
     pgo_env = {'PATH': '%s;%s' % (pgosweep_path, '%(PATH)s')}
-    pgo_args = ['--profiler=win_pgo_profiler', '--use-live-sites']
+    pgo_args = ['--profiler=win_pgo_profiler']
 
     for benchmark in _BENCHMARKS_TO_RUN:
       try:
