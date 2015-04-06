e194b4e88b5e526b3c788e7b0cbdc297354b8591
dpranke@chromium.org <dpranke@chromium.org>
2015-04-06 21:12:42 +0000

    Add a recipe to check out gyp and run its tests.
    This will be used by the new 'client.gyp' master.
    
    The recipe is a bit more complicated than it needs to be, in that
    we tell it to use gclient when we really only need to check out
    a single repo, but using gclient will make things more consistent
    with 'fetch gyp' and may make this recipe slightly more useful as
    a simple template for other small public projects.
    
    The recipe does *not* use a BUILDERS dict or a per-builder set
    of tests, since there is no branching in the recipe and since
    (I think?) we're gradually moving control flow out of the recipes
    and into checkout-side scripts and configuration, and so the
    per-builder baselines are less and less useful.
    
    R=iannucci@chromium.org, bradnelson@chromium.org
    BUG=472266
    
    Review URL: https://codereview.chromium.org/1060093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294695 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/gyp.py
@@ -0,0 +1,26 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'path',
+  'properties',
+  'python',
+]
+
+
+def GenSteps(api):
+  api.gclient.set_config('gyp')
+
+  api.bot_update.ensure_checkout(force=True)
+
+  api.python('run tests',
+             api.path['checkout'].join('gyp_tests.py'))
+
+
+def GenTests(api):
+  yield(api.test('fake_test') +
+        api.properties.generic(mastername='fake_mastername',
+                               buildername='fake_buildername'))
