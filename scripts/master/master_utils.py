41374028d2c9376f0a6f63f919a9042d6a41cd08
nodir@chromium.org <nodir@chromium.org>
2015-04-14 23:30:20 +0000

    buildbucket: remove max_lease_count limit
    Currently we don't have masters that poll same bucket and we need
    developers to see entire build queue on buildbot. Therefore, this CL
    changes max_lease_count to a <BIG number>.
    
    R=sheyang@chromium.org, vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1089083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294805 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/master_utils.py
+++ scripts/master/master_utils.py
@@ -4,6 +4,7 @@
 
 import os
 import re
+import sys
 
 import buildbot
 from buildbot import interfaces, util
@@ -420,6 +421,8 @@ def AutoSetupMaster(c, active_master, mail_notifier=False,
         c,
         active_master,
         buckets=[active_master.buildbucket_bucket],
+        # Set limit absurdly high so all buildbucket builds show up as Pending.
+        max_lease_count=sys.maxint,
     )
 
 def DumpSetup(c, important=None, filename='config.current.txt'):
