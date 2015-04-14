5f87c2fc53de0c54cfaf38c6a508905e3be3a937
machenbach@chromium.org <machenbach@chromium.org>
2015-04-14 17:43:21 +0000

    V8 Buildbot: Make mips compile a tree closer.
    BUG=
    
    Review URL: https://codereview.chromium.org/1054633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294797 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8/mail_notifier_cfg.py
+++ masters/master.client.v8/mail_notifier_cfg.py
@@ -40,12 +40,9 @@ categories_steps = {
 }
 
 exclusions = {
-  'V8 Linux - mipsel - sim - builder': [],
-  'V8 Linux - mips64el - sim - builder': [],
   'V8 Linux - mipsel - sim': [],
   'V8 Linux - ppc - sim': [],
   'V8 Linux - ppc64 - sim': [],
-  'V8 Mips - builder': [],
   'V8 Mips - big endian - nosnap - 1': [],
   'V8 Mips - big endian - nosnap - 2': [],
   'V8 Linux - x87 - nosnap - debug builder': [],
