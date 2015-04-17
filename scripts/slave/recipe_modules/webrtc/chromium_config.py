21164e15fc95a4a1e8d49275edf853f7ce10f55c
kjellander@chromium.org <kjellander@chromium.org>
2015-04-17 11:39:26 +0000

    WebRTC: Fix iOS Simulator recipe.
    Since we use ninja to build all our iOS builds,
    we cannot use the chromium or ios recipes configs.
    
    Also simplify the iOS config, which I forgot to do
    in https://codereview.chromium.org/1088423004/
    
    Review URL: https://codereview.chromium.org/1092013002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294873 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/webrtc/chromium_config.py
+++ scripts/slave/recipe_modules/webrtc/chromium_config.py
@@ -25,19 +25,18 @@ def webrtc_standalone(c):
 def webrtc_clang(c):
   _compiler_defaults(c)
 
-@CONFIG_CTX(includes=['chromium', 'dcheck', '_webrtc_ios'])
+@CONFIG_CTX(includes=['chromium', 'dcheck', 'static_library'])
 def webrtc_ios(c):
-  pass
-
-@CONFIG_CTX(includes=['static_library'])
-def _webrtc_ios(c):
   if c.HOST_PLATFORM != 'mac':
     raise BadConf('Only "mac" host platform is supported for iOS (got: "%s")' %
                   c.HOST_PLATFORM)  # pragma: no cover
   if c.TARGET_PLATFORM != 'ios':
     raise BadConf('Only "ios" target platform is supported (got: "%s")' %
                   c.TARGET_PLATFORM)  # pragma: no cover
-  c.build_config_fs = c.BUILD_CONFIG + '-iphoneos'
+  if c.TARGET_ARCH == 'arm':
+    c.build_config_fs = c.BUILD_CONFIG + '-iphoneos'
+  if c.TARGET_ARCH == 'intel':
+    c.build_config_fs = c.BUILD_CONFIG + '-iphonesimulator'
 
   gyp_defs = c.gyp_env.GYP_DEFINES
   gyp_defs['build_with_libjingle'] = 1
