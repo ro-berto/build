de81d4745df35abecb47af5af32c59b2b1efbf85
machenbach@chromium.org <machenbach@chromium.org>
2015-04-20 21:22:58 +0000

    V8 Buildbot: Switch on goma for mac gc stress bot.
    TBR=tandrii@chromium.org
    
    Review URL: https://codereview.chromium.org/1100483004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@294905 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -973,7 +973,7 @@ BUILDERS = {
         'testing': {'platform': 'linux'},
       },
       'V8 Mac GC Stress': {
-        'chromium_apply_config': ['clang'],
+        'chromium_apply_config': ['clang', 'goma'],
         'v8_apply_config': ['gc_stress'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
