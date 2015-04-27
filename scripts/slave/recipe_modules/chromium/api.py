1283ce1a33ad023f01682f580cd0a90f706b6fcb
dpranke@chromium.org <dpranke@chromium.org>
2015-04-27 23:50:35 +0000

    Change the build step text from 'mb gen' to 'Generate build files'.
    This patch changes the step text for the 'mb gen' step
    (api.run_mb in the chromium module) from 'mb gen' to 'Generate build
    files', which is way more intelligible.
    
    TBR=brettw@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1110603003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295016 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium/api.py
+++ scripts/slave/recipe_modules/chromium/api.py
@@ -544,7 +544,7 @@ class ChromiumApi(recipe_api.RecipeApi):
 
     # This runs with no env being passed along, so we get a clean environment
     # without any GYP_DEFINES being present to cause confusion.
-    self.m.python(name='mb gen',
+    self.m.python(name='Generate build files',
                   script=self.m.path['checkout'].join('tools', 'mb', 'mb.py'),
                   args=args)
 
