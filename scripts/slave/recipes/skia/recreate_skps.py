3f3e78265448fdbc5cb9499fea90f88c2f44e222
rmistry@google.com <rmistry@google.com>
2015-04-30 16:44:38 +0000

    Revert of Skia: Define SK_PICTURE_FORCE_FONT_EMBEDDING when we build our SKPs. (patchset #1 id:1 of https://codereview.chromium.org/915733002/)
    Reason for revert:
    mtklein@ recommends removing this flag to be able to test our SP enabled SKPs:
    https://code.google.com/p/skia/issues/detail?id=3763#c26
    
    Original issue's description:
    > Skia: Define SK_PICTURE_FORCE_FONT_EMBEDDING when we build our SKPs.
    > 
    > I just switched this from the default on Linux to a compile time option.
    > Adding this define when we build Chrome should continue the status quo,
    > that we embed fonts when serializing pictures during our RecreateSKP runs.
    > 
    > BUG=skia:3417
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=294015
    
    TBR=mtklein@google.com,mtklein@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:3417
    
    Review URL: https://codereview.chromium.org/1120673002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295095 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/skia/recreate_skps.py
+++ scripts/slave/recipes/skia/recreate_skps.py
@@ -31,8 +31,7 @@ def GenSteps(api):
   gclient_cfg.got_revision_mapping['skia'] = 'got_revision'
   api.gclient.checkout(gclient_config=gclient_cfg)
   api.gclient.runhooks(
-      env={'CPPFLAGS': ('-DSK_ALLOW_CROSSPROCESS_PICTUREIMAGEFILTERS=1 ' +
-                        '-DSK_PICTURE_FORCE_FONT_EMBEDDING') })
+      env={'CPPFLAGS': '-DSK_ALLOW_CROSSPROCESS_PICTUREIMAGEFILTERS=1'})
 
   # Build Chrome.
   api.step('Build Chrome',
