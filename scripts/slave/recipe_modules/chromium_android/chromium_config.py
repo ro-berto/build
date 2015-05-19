5c48bbe27640d59a896d1165e09e6bb4049a4d8f
jbudorick@chromium.org <jbudorick@chromium.org>
2015-05-19 18:31:18 +0000

    [Android] Add v8_enable_verify_heap=1 to clang ASAN release builds.
    BUG=431601
    
    Review URL: https://codereview.chromium.org/1146653003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295313 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_android/chromium_config.py
+++ scripts/slave/recipe_modules/chromium_android/chromium_config.py
@@ -36,6 +36,7 @@ def clang_asan_release_builder(c):  # pragma: no cover
   c.gyp_env.GYP_DEFINES['sanitizer_coverage'] = 3
   c.gyp_env.GYP_DEFINES['fastbuild'] = 0
   c.gyp_env.GYP_DEFINES['use_allocator'] = 'none'
+  c.gyp_env.GYP_DEFINES['v8_enable_verify_heap'] = 1
   c.compile_py.default_targets = ['chrome_apk']
 
 @CONFIG_CTX(includes=['clang_asan_release_builder'])
