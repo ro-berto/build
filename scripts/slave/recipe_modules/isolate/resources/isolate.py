b9487ee71ca2aac0d7ae3be955a9c7629ca35956
maruel@chromium.org <maruel@chromium.org>
2015-06-01 23:53:26 +0000

    Check the isolate version before running it.
    This permits falling back to the (albeit slower) python implementation if we
    mess up with the Go implementation.
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1152983005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295482 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/isolate/resources/isolate.py
+++ scripts/slave/recipe_modules/isolate/resources/isolate.py
@@ -11,8 +11,11 @@ import subprocess
 import sys
 
 
-def main():
-  path = sys.argv[1]
+def try_go(path, args):
+  """Tries to run the Go implementation of isolate.
+
+  Returns None if it should fall back to the python implementation.
+  """
   luci_go = os.path.join(os.path.dirname(path), 'luci-go')
   if sys.platform == 'win32':
     exe = os.path.join(luci_go, 'win64', 'isolate.exe')
@@ -20,13 +23,31 @@ def main():
     exe = os.path.join(luci_go, 'mac64', 'isolate')
   else:
     exe = os.path.join(luci_go, 'linux64', 'isolate')
-  if os.access(exe, os.X_OK):
-    # Use Go implementation. We'd prefer to build on-the-fly but the bots do not
-    # all have yet the Go toolset.
-    return subprocess.call([exe] + sys.argv[2:])
+  if not os.access(exe, os.X_OK):
+    return None
 
-  return subprocess.call(
-      [sys.executable, os.path.join(path, 'isolate.py')] + sys.argv[2:])
+  # Try to use Go implementation.
+  try
+    version = subprocess.check_output([exe, 'version']).strip()
+    version = tuple(map(int, version.split('.')))
+  except OSError, subprocess.CalledProcessError, ValueError:
+    return None
+
+  # Key behavior based on version if necessary.
+  if version < (0, 1):
+    return None
+
+  return subprocess.call([exe] + args)
+
+
+def main():
+  path = sys.argv[1]
+  args = sys.argv[2:]
+  ret = try_go(path, args)
+  if ret is None:
+    return subprocess.call(
+        [sys.executable, os.path.join(path, 'isolate.py')] + args)
+  return ret
 
 
 if __name__ == '__main__':
