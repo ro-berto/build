1d9ca70ba0032764a919a301a8e559fdeec935eb
kbr@chromium.org <kbr@chromium.org>
2015-06-04 22:49:57 +0000

    Run audio_unittests instead of media_unittests on chromium.gpu.fyi bots. (Re-land #1)
    This smaller test suite avoids redundancy with tests already running
    on other Chromium bots. A follow-on CL will add a flag verifying that
    the tests run correctly on physical hardware.
    
    Stopped adding media_unittests and content_unittests as unconditional
    dependencies from the GPU recipe, since on the GPU bots, they're only
    run on the FYI waterfall. This changed a lot of expectations due to
    the changes to the dummy swarming hashes, and also required an update
    to one of the tests in the chromium_trybot recipe.
    
    BUG=416594
    TBR=iannucci@chromium.org,sergeyberezin@chromium.org,sergiyb@chromium.org
    
    Review URL: https://codereview.chromium.org/1150183013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295536 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gpu/common.py
+++ scripts/slave/recipe_modules/gpu/common.py
@@ -5,13 +5,20 @@
 GPU_ISOLATES = (
   'angle_unittests',
   'content_gl_tests',
-  'content_unittests',
   'gl_tests',
   'gles2_conform_test',
   'gpu_unittests',
-  'media_unittests',
   'tab_capture_end2end_tests',
   'telemetry_gpu_test',
+)
+
+# Until the media-only tests are extracted from content_unittests and
+# these both can be run on the commit queue with
+# --require-audio-hardware-for-testing, run them only on the FYI
+# waterfall.
+FYI_ONLY_GPU_ISOLATES = (
+  'audio_unittests',
+  'content_unittests',
 )
 
 # This will be folded into the list above once ANGLE is running on all
