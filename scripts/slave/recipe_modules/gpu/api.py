c00135c1ec527a7fa5b3c7605bfe8137be9cea34
jmadill@chromium.org <jmadill@chromium.org>
2015-06-11 13:50:11 +0000

    Revert of Add WebGL CTS and ES conformance tests targeting ANGLE's OpenGL renderer. (patchset #5 id:80001 of https://codereview.chromium.org/1148713003/)
    Reason for revert:
    Failures in the CTS on a bunch of Windows bots. Some seem to be consistent. Will wait for Geoff to get back so he can investigate.
    
    TBR=kbr@chromium.org
    
    Original issue's description:
    > Add WebGL CTS and ES conformance tests targeting ANGLE's OpenGL renderer.
    > 
    > BUG=490149
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=295559
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=295604
    
    TBR=kbr@chromium.org,zmo@chromium.org,geofflang@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=490149
    
    Review URL: https://codereview.chromium.org/1180773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295623 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/gpu/api.py
+++ scripts/slave/recipe_modules/gpu/api.py
@@ -26,10 +26,6 @@ D3D9_TEST_NAME_MAPPING = freeze({
   'webgl_conformance': 'webgl_conformance_d3d9'
 })
 
-GL_TEST_NAME_MAPPING = freeze({
-  'webgl_conformance': 'webgl_conformance_gl'
-})
-
 class GpuApi(recipe_api.RecipeApi):
   def setup(self):
     """Call this once before any of the other APIs in this module."""
@@ -377,26 +373,14 @@ class GpuApi(recipe_api.RecipeApi):
         'webgl_conformance', chrome_revision, webkit_revision, enable_swarming,
         swarming_dimensions))
 
-    # Run extra WebGL conformance tests in Windows FYI GPU bots with
-    # ANGLE OpenGL/D3D9.
-    # This ensures the ANGLE OpenGL/D3D9 gets some testing
+    # Run extra D3D9 conformance in Windows FYI GPU bots
+    # This ensures the ANGLE/D3D9 gets some testing
     if self.is_fyi_waterfall and self.m.platform.is_win:
       tests.append(self._create_telemetry_test(
           D3D9_TEST_NAME_MAPPING['webgl_conformance'], chrome_revision,
           webkit_revision, enable_swarming, swarming_dimensions,
           target_name='webgl_conformance',
           extra_browser_args=['--disable-d3d11']))
-
-      tests.append(self._create_telemetry_test(
-          GL_TEST_NAME_MAPPING['webgl_conformance'], chrome_revision,
-          webkit_revision, enable_swarming, swarming_dimensions,
-          target_name='webgl_conformance',
-          extra_browser_args=[
-            '--use-gpu-in-tests',
-            '--use-angle=gl',
-            '--disable-gpu-sandbox', # TODO(geofflang): Remove dependency on
-                                     # the sandbox being disabled to use WGL
-          ]))
 
     # Context lost tests.
     tests.append(self._create_telemetry_test(
