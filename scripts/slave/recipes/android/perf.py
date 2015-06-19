cca137fe909b6d7db236290d1f6407ae74bf4f5d
jdduke@chromium.org <jdduke@chromium.org>
2015-06-19 21:40:26 +0000

    Revert of Make Android perf bots on chromium.perf use ChromePublic.apk (patchset #2 id:20001 of https://codereview.chromium.org/1180833003/)
    Reason for revert:
    Uncovered a number of perf "regressions" (differences) at an inopportune
    time relative to other major changes. Temporarily reverting until
    the perf impact from other changes can be analyzed.
    
    Original issue's description:
    > Make Android perf bots on chromium.perf use ChromePublic.apk
    > 
    > BUG=495854
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=295681
    
    TBR=eakuefner@chromium.org,jbudorick@chromium.org,luqui@chromium.org,nednguyen@google.com,mlliu@chromium.org
    BUG=495854,501015
    
    Review URL: https://codereview.chromium.org/1201453003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295770 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/android/perf.py
+++ scripts/slave/recipes/android/perf.py
@@ -152,14 +152,14 @@ def RunSteps(api):
   api.chromium_android.common_tests_setup_steps(perf_setup=True)
 
   api.chromium_android.adb_install_apk(
-      'ChromePublic.apk',
-      'org.chromium.chrome')
+      'ChromeShell.apk',
+      'org.chromium.chrome.shell')
 
   test_runner = api.chromium_tests.create_test_runner(
       api, builder.get('tests', []))
 
   perf_tests = api.chromium.list_perf_tests(
-      browser='android-chromium',
+      browser='android-chrome-shell',
       num_shards=builder['num_device_shards'],
       devices=api.chromium_android.devices[0:1]).json.output
   try:
