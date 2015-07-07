cb89d8f0dd5fad8b5fc935639fc59a7317679001
erikchen@chromium.org <erikchen@chromium.org>
2015-07-07 23:28:25 +0000

    Update master.chromium.webkit to use a dedicated mac builder.
    Previously, the 10.6 and 10.8 bots were both building and testing Chrome
    targets. This won't work in the near future, since Chrome targets must be built
    on OSX 10.9+.
    
    This CL changes the archived build from being a release target to a debug
    target.
    
    BUG=507513
    
    Review URL: https://codereview.chromium.org/1216383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295945 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webkit/master_mac_latest_cfg.py
+++ masters/master.chromium.webkit/master_mac_latest_cfg.py
@@ -10,27 +10,45 @@ defaults = {}
 helper = master_config.Helper(defaults)
 B = helper.Builder
 F = helper.Factory
+T = helper.Triggerable
 
 def mac():
-  return chromium_factory.ChromiumFactory('src/xcodebuild', 'darwin')
-
-def mac_out():
   return chromium_factory.ChromiumFactory('src/out', 'darwin')
-
-
-################################################################################
-## Release
-################################################################################
 
 defaults['category'] = 'nonlayout'
 
+################################################################################
+## Debug
+################################################################################
+
+# Triggerable scheduler for testers
+T('mac_builder_dbg_trigger')
+
 #
-# Mac Rel Builder
+# Mac Dbg Builder
 #
-B('Mac10.6 Tests', 'f_mac_tests_rel', scheduler='global_scheduler')
-F('f_mac_tests_rel', mac_out().ChromiumFactory(
-    options=['--build-tool=ninja', '--compiler=goma-clang', '--',
-             'chromium_builder_tests'],
+B('Mac Builder (dbg)', 'f_mac_builder_dbg', scheduler='global_scheduler')
+F('f_mac_builder_dbg', mac().ChromiumFactory(
+    target='Debug',
+    # Build 'all' instead of 'chromium_builder_tests' so that archiving works.
+    # TODO: Define a new build target that is archive-friendly?
+    options=['--build-tool=ninja', '--compiler=goma-clang', 'all'],
+    factory_properties={
+        'trigger': 'mac_builder_dbg_trigger',
+        'gclient_env': {
+            'GYP_DEFINES': 'fastbuild=1',
+            'GYP_GENERATORS': 'ninja',
+        },
+        'archive_build': True,
+        'blink_config': 'blink',
+        'build_name': 'Mac',
+        'gs_bucket': 'gs://chromium-webkit-snapshots',
+        'gs_acl': 'public-read',
+    }))
+
+B('Mac10.6 Tests', 'f_mac_tester_10_06_dbg',
+  scheduler='mac_builder_dbg_trigger')
+F('f_mac_tester_10_06_dbg', mac().ChromiumFactory(
     tests=[
       'browser_tests',
       'cc_unittests',
@@ -41,19 +59,13 @@ F('f_mac_tests_rel', mac_out().ChromiumFactory(
     ],
     factory_properties={
         'generate_gtest_json': True,
-        'gclient_env': {
-            'GYP_GENERATORS':'ninja',
-            'GYP_DEFINES':'fastbuild=1',
-        },
         'blink_config': 'blink',
     }))
 
 
-B('Mac10.8 Tests', 'f_mac_tests_rel_108', scheduler='global_scheduler')
-F('f_mac_tests_rel_108', mac_out().ChromiumFactory(
-    # Build 'all' instead of 'chromium_builder_tests' so that archiving works.
-    # TODO: Define a new build target that is archive-friendly?
-    options=['--build-tool=ninja', '--compiler=goma-clang', '--', 'all'],
+B('Mac10.8 Tests', 'f_mac_tester_10_08_dbg',
+  scheduler='mac_builder_dbg_trigger')
+F('f_mac_tester_10_08_dbg', mac().ChromiumFactory(
     tests=[
       'browser_tests',
       'content_browsertests',
@@ -62,30 +74,7 @@ F('f_mac_tests_rel_108', mac_out().ChromiumFactory(
       'unit',
     ],
     factory_properties={
-        'archive_build': True,
-        'blink_config': 'blink',
-        'build_name': 'Mac',
         'generate_gtest_json': True,
-        'gclient_env': {
-            'GYP_GENERATORS':'ninja',
-            'GYP_DEFINES':'fastbuild=1',
-        },
-        'gs_bucket': 'gs://chromium-webkit-snapshots',
-    }))
-
-
-################################################################################
-## Debug
-################################################################################
-
-#
-# Mac Dbg Builder
-#
-B('Mac Builder (dbg)', 'f_mac_dbg', scheduler='global_scheduler')
-F('f_mac_dbg', mac().ChromiumFactory(
-    target='Debug',
-    options=['--compiler=goma-clang', '--', '-target', 'blink_tests'],
-    factory_properties={
         'blink_config': 'blink',
     }))
 
