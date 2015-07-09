96c4dd7f9989d0009283d436578f311a76ecc912
machenbach@chromium.org <machenbach@chromium.org>
2015-07-09 11:38:00 +0000

    Revert of V8 Buildbot: Use ninja on windows bots. (patchset #1 id:1 of https://codereview.chromium.org/1229703004/)
    Reason for revert:
    Fails hooks
    
    Original issue's description:
    > V8 Buildbot: Use ninja on windows bots.
    > 
    > BUG=
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=295964
    
    TBR=phajdan.jr@chromium.org,sergiyb@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1228953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@295965 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -513,7 +513,7 @@ BUILDERS = {
       },
 ####### Category: Windows
       'V8 Win32 - builder': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 32,
@@ -527,7 +527,7 @@ BUILDERS = {
         ],
       },
       'V8 Win32 - debug builder': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 32,
@@ -569,12 +569,7 @@ BUILDERS = {
       },
       'V8 Win32 - nosnap - shared': {
         'v8_apply_config': ['no_snapshot'],
-        'chromium_apply_config': [
-          'v8_ninja',
-          'msvs2013',
-          'shared_library',
-          'no_snapshot',
-         ],
+        'chromium_apply_config': ['msvs2013', 'shared_library', 'no_snapshot'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 32,
@@ -623,7 +618,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'V8 Win64': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 64,
@@ -633,7 +628,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'V8 Win64 - debug': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 64,
@@ -1639,7 +1634,7 @@ BUILDERS = {
         'testing': {'platform': 'linux'},
       },
       'v8_win_rel': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 32,
@@ -1649,7 +1644,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'v8_win_dbg': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 32,
@@ -1659,7 +1654,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'v8_win_compile_dbg': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 32,
@@ -1669,12 +1664,7 @@ BUILDERS = {
       },
       'v8_win_nosnap_shared_compile_rel': {
         'v8_apply_config': ['no_snapshot'],
-        'chromium_apply_config': [
-          'v8_ninja',
-          'msvs2013',
-          'shared_library',
-          'no_snapshot',
-         ],
+        'chromium_apply_config': ['msvs2013', 'shared_library', 'no_snapshot'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 32,
@@ -1685,7 +1675,6 @@ BUILDERS = {
       'v8_win_nosnap_shared_rel': {
         'v8_apply_config': ['no_snapshot'],
         'chromium_apply_config': [
-          'v8_ninja',
           'msvs2013',
           'no_dcheck',
           'no_snapshot',
@@ -1700,7 +1689,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'v8_win64_compile_rel': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 64,
@@ -1709,7 +1698,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'v8_win64_rel': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_BITS': 64,
@@ -1719,7 +1708,7 @@ BUILDERS = {
         'testing': {'platform': 'win'},
       },
       'v8_win64_dbg': {
-        'chromium_apply_config': ['v8_ninja', 'msvs2013'],
+        'chromium_apply_config': ['msvs2013'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_BITS': 64,
@@ -1940,7 +1929,7 @@ dart_mac_release = BUILDERS['client.dart.fyi']['builders']['v8-mac-release']
 dart_mac_release['chromium_apply_config'].extend(['v8_ninja', 'clang', 'goma'])
 
 dart_win_release = BUILDERS['client.dart.fyi']['builders']['v8-win-release']
-dart_win_release['chromium_apply_config'].extend(['v8_ninja', 'msvs2013'])
+dart_win_release['chromium_apply_config'].extend(['msvs2013'])
 
 BUILDERS = freeze(BUILDERS)
 BRANCH_BUILDERS = freeze(BRANCH_BUILDERS)
