3d6ee24c0c1d42efd277f4f121d6b5beda8637ca
dnj@chromium.org <dnj@chromium.org>
2015-07-22 22:22:46 +0000

    CrOS: Update R45 Chromite pin.
    Update pin to: 42440f29e49e646837ec3fc0bfae2bca9b70f369
    
    TBR=stip@chromium.org
    BUG=brillo:823
    TEST=None
    
    Review URL: https://codereview.chromium.org/1256453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296089 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/common/cros_chromite.py
+++ scripts/common/cros_chromite.py
@@ -54,7 +54,7 @@ PINS = collections.OrderedDict((
   # keep one branch around for stability, since internal waterfall updates are
   # not atomic. Therefore, we should prune all but the FOUR newest release
   # branches.
-  ('release-R45-7262.B', 'f39dae9d71fa20c3ae1d2c80291527db4b1f6e3c'),
+  ('release-R45-7262.B', '42440f29e49e646837ec3fc0bfae2bca9b70f369'),
   ('release-R44-7077.B', '466a1a39a891e4762ce27eaf3eccd19e692bd3d0'),
   ('release-R43-6946.B', '504196e05c2d8cb5448646a5f036431ec2ee5da1'),
   ('release-R42-6812.B', '719914944802dede1a0dd1cd93376b76880c63f4'),
