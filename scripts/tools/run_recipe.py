9670eb044e3af3b2c639769acda27568ab9eb1f9
luqui@chromium.org <luqui@chromium.org>
2015-07-23 19:48:57 +0000

    RELAND of Whitelist environment entering recipe engine to match run_slave.
    This is mainly for local run_recipe invocations, that we don't accidentally
    incorporate environment variables that won't be available on the bots, such
    as GYP defines.
    
    Original patch: https://codereview.chromium.org/1252053002/
    Revert: https://codereview.chromium.org/1252513003/
    
    BUG=392992,513109
    R=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/1257433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296100 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/tools/run_recipe.py
+++ scripts/tools/run_recipe.py
@@ -116,12 +116,7 @@ def main(args):
   if not os.path.exists(SLAVE_DIR):
     os.makedirs(SLAVE_DIR)
 
-  # Remove any GYP environment variables for the run.
   env = os.environ.copy()
-  for k in env.keys():
-    if k.startswith('GYP'):
-      del env[k]
-
   env['RUN_SLAVE_UPDATED_SCRIPTS'] = '1'
   env['PYTHONUNBUFFERED'] = '1'
   env['PYTHONIOENCODING'] = 'UTF-8'
