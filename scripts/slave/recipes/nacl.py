6ca34a1f86ad9676d1871447c9daf2c7cda6d7d1
aneeshm@chromium.org <aneeshm@chromium.org>
2015-07-25 01:44:13 +0000

    Add nacl recipe and a builder to test it with
    This is the first step in (eventually) converting nacl to use recipes.
    This CL adds a nacl recipe which runs the build script in the nacl
    repo that builds nacl on builders (buildbot_selector.py).
    
    (Eventually, after recipe code can live in a project's repo instead of
    the infra repo, this *should* be replaced with a recipe living in the
    nacl repo.)
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1256083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296117 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/nacl.py
@@ -0,0 +1,59 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'gclient',
+  'path',
+  'platform',
+  'properties',
+  'python',
+  'step',
+]
+
+# TODO list:
+# Convert this to use bot_update.
+# Convert this to use GOMA.
+# Consolidate the build process as much as possible. (optional, but strongly
+#   desired.)
+# e-mail thread from Pawel about the Mac bots not using compile.py, causing
+#   goma errors.
+# Check if gclient runhooks should run before or after the cleanuptemp and
+# clobber steps.
+# Check whether allow_subannotations=True propagates environment variables;
+#   fix it if it doesn't.
+# Remove the test env data.
+# Remove this todo list.
+
+def _CheckoutSteps(api):
+  api.gclient.set_config('nacl')
+  # TODO: check if the 'DEPS' in the solution in my home directory means
+  # anything.
+  api.gclient.checkout()
+  api.gclient.runhooks()
+
+def _AnnotatedStepsSteps(api):
+  api.python('annotated steps',
+      api.path['checkout'].join('buildbot', 'buildbot_selector.py'),
+      allow_subannotations=True,
+      cwd = api.path['build'],
+      env = {'BUILDBOT_MASTERNAME': api.properties['mastername'],
+        'BUILDBOT_BUILDERNAME': api.properties['buildername'],
+        'BUILDBOT_REVISION': api.properties['revision'],
+        'RUNTEST': api.path['build'].join('scripts', 'slave', 'runtest.py')})
+#      env = {'BUILDBOT_MASTERNAME': 'client.nacl',
+#        'BUILDBOT_BUILDERNAME': 'precise-64-newlib-opt-test',
+#        'BUILDBOT_REVISION': ,
+#        # Required by the buildbot_selector.
+#        'RUNTEST': api.path['build'].join('scripts', 'slave', 'runtest.py')})
+
+def RunSteps(api):
+  _CheckoutSteps(api)
+  _AnnotatedStepsSteps(api)
+
+def GenTests(api):
+  # yield api.test('win') + api.platform('win', 64)
+  yield api.test('linux') + api.platform('linux', 64) + \
+    api.properties(mastername = 'client.nacl') + \
+    api.properties(buildername = 'precise-64-newlib-opt-test') +\
+    api.properties(revision = 'abcd')
