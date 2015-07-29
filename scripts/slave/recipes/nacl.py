7cebdcecb11b5ac33ad484e24976ac8f5405552c
aneeshm@chromium.org <aneeshm@chromium.org>
2015-07-29 00:00:29 +0000

    Fix NaCl recipe to add env vars in triggered bots
    The NaCl triggered (the ARM hardware testers, currently) require a
    number of buildbot properties in the form of environment variables to
    work correctly; this is because the buildbot_pnacl script gets this
    information from them.
    
    This change makes these build properties (those with a prefix
    'triggered_by_') available to all triggerd builders.
    
    BUG=
    R=bradnelson@chromium.org
    
    Review URL: https://codereview.chromium.org/1260753006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296148 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/nacl.py
+++ scripts/slave/recipes/nacl.py
@@ -20,6 +20,17 @@ DEPS = [
 #   goma errors.
 # Remove this todo list.
 
+# Maps from triggering builder to triggered builder;
+# key builder triggers value builder.
+trigger_map = {
+    'precise_64-newlib-arm_qemu-pnacl-dbg':
+      'oneiric_32-newlib-arm_hw-pnacl-panda-dbg',
+    'precise_64-newlib-arm_qemu-pnacl-opt':
+      'oneiric_32-newlib-arm_hw-pnacl-panda-opt',
+    'precise_64-newlib-arm_qemu-pnacl-buildonly-spec':
+      'oneiric_32-newlib-arm_hw-pnacl-panda-spec',
+}
+
 def _CheckoutSteps(api):
   api.gclient.set_config('nacl')
   # TODO: check if the 'DEPS' in the solution in my home directory means
@@ -28,28 +39,36 @@ def _CheckoutSteps(api):
   api.gclient.runhooks()
 
 def _AnnotatedStepsSteps(api):
+  # Default environemnt; required by all builders.
+  env = {
+      'BUILDBOT_MASTERNAME': api.properties['mastername'],
+      'BUILDBOT_BUILDERNAME': api.properties['buildername'],
+      'BUILDBOT_REVISION': api.properties['revision'],
+      'RUNTEST': api.path['build'].join('scripts', 'slave', 'runtest.py'),
+      # NOTE: If this recipe is used for a tryserver/trybots, please change
+      # 'BuilderTester' to 'Trybot'.
+      'BUILDBOT_SLAVE_TYPE': 'BuilderTester',
+  }
+  # Set up env for the triggered builders.
+  if api.properties['buildername'] in trigger_map.values():
+    env.update({
+        'BUILDBOT_TRIGGERED_BY_BUILDERNAME':
+          api.properties['triggered_by_buildername'],
+        'BUILDBOT_TRIGGERED_BY_BUILDNUMBER':
+          api.properties['triggered_by_buildnumber'],
+        'BUILDBOT_TRIGGERED_BY_REVISION':
+          api.properties['triggered_by_revision'],
+        'BUILDBOT_TRIGGERED_BY_SLAVENAME':
+          api.properties['triggered_by_slavename'],
+    })
   api.python('annotated steps',
       api.path['checkout'].join('buildbot', 'buildbot_selector.py'),
       allow_subannotations=True,
       cwd = api.path['checkout'],
-      env = {'BUILDBOT_MASTERNAME': api.properties['mastername'],
-        'BUILDBOT_BUILDERNAME': api.properties['buildername'],
-        'BUILDBOT_REVISION': api.properties['revision'],
-        'RUNTEST': api.path['build'].join('scripts', 'slave', 'runtest.py'),
-        # NOTE: If this recipe is used for a tryserver/trybots, please change
-        # 'BuilderTester' to 'Trybot'.
-        'BUILDBOT_SLAVE_TYPE': 'BuilderTester',
-        })
+      env = env,
+    )
 
 def _TriggerTestsSteps(api):
-  trigger_map = {
-      'precise_64-newlib-arm_qemu-pnacl-dbg':
-        'oneiric_32-newlib-arm_hw-pnacl-panda-dbg',
-      'precise_64-newlib-arm_qemu-pnacl-opt':
-        'oneiric_32-newlib-arm_hw-pnacl-panda-opt',
-      'precise_64-newlib-arm_qemu-pnacl-buildonly-spec':
-        'oneiric_32-newlib-arm_hw-pnacl-panda-spec',
-      }
 
   if api.properties['buildername'] in trigger_map:
     api.trigger(
@@ -61,9 +80,20 @@ def RunSteps(api):
   _TriggerTestsSteps(api)
 
 def GenTests(api):
-  # yield api.test('win') + api.platform('win', 64)
-  yield api.test('linux') + api.platform('linux', 64) + \
-    api.properties(mastername = 'client.nacl') + \
+  yield api.test('linux_triggering') +\
+    api.platform('linux', 64) +\
+    api.properties(mastername = 'client.nacl') +\
     api.properties(buildername = 'precise_64-newlib-arm_qemu-pnacl-dbg') +\
     api.properties(revision = 'abcd') +\
     api.properties(slavename='TestSlave')
+  yield api.test('linux_triggered') +\
+    api.platform('linux', 32) +\
+    api.properties(mastername = 'client.nacl') +\
+    api.properties(buildername = 'oneiric_32-newlib-arm_hw-pnacl-panda-dbg') +\
+    api.properties(revision = 'abcd') +\
+    api.properties(slavename='TestSlave') +\
+    api.properties(triggered_by_revision = 'abcd') +\
+    api.properties(triggered_by_slavename = 'TestSlave') +\
+    api.properties(triggered_by_buildername =
+        'precise_64-newlib-arm_qemu-pnacl-dbg') +\
+    api.properties(triggered_by_buildnumber = '1')
