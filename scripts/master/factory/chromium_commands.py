8b8ba720376c8ff30ed49934caf66b4d66f6acf6
luqui@chromium.org <luqui@chromium.org>
2015-08-07 21:56:10 +0000

    Pass active_master through chromium_step AddAnnotation Command for buildbucket
    Related: https://chromereviews.googleplex.com/236037014
    
    BUG=502324
    
    Review URL: https://codereview.chromium.org/1281033002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296247 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/factory/chromium_commands.py
+++ scripts/master/factory/chromium_commands.py
@@ -944,7 +944,7 @@ class ChromiumCommands(commands.FactoryCommands):
                           command=cmd)
 
   def AddAnnotationStep(self, name, cmd, factory_properties=None, env=None,
-                        timeout=6000, maxTime=8*60*60):
+                        timeout=6000, maxTime=8*60*60, active_master=None):
     """Add an @@@BUILD_STEP step@@@ annotation script build command.
 
     This function allows the caller to specify the name of the
@@ -971,7 +971,8 @@ class ChromiumCommands(commands.FactoryCommands):
                           command=cmd,
                           env=env,
                           maxTime=maxTime,
-                          factory_properties=factory_properties)
+                          factory_properties=factory_properties,
+                          active_master=active_master)
 
   def AddMiniInstallerTestStep(self, factory_properties):
     cmd = [self._python, self._mini_installer_tests_tool,
