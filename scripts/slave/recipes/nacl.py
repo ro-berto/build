e9ab279a3e94014b10853adb5428d6569d202e1b
iannucci@chromium.org <iannucci@chromium.org>
2015-08-12 19:53:44 +0000

    Fix nacl purpleness.
    got_revision is not a buildbot-provided property (it's emitted via annotation).
    This retrieves the value directly from bot_update.ensure_checkout.
    
    TBR=bradnelson@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1290853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296283 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/nacl.py
+++ scripts/slave/recipes/nacl.py
@@ -13,13 +13,6 @@ DEPS = [
   'trigger',
 ]
 
-# TODO list:
-# Consolidate the build process as much as possible. (optional, but strongly
-#   desired.)
-# e-mail thread from Pawel about the Mac bots not using compile.py, causing
-#   goma errors.
-# Remove this todo list.
-
 # Maps from triggering builder to triggered builder;
 # key builder triggers value builder.
 trigger_map = {
@@ -33,18 +26,21 @@ trigger_map = {
 
 def _CheckoutSteps(api):
   api.gclient.set_config('nacl')
-  # TODO: check if the 'DEPS' in the solution in my home directory means
-  # anything.
-  api.bot_update.ensure_checkout(force=True)
-  api.gclient.runhooks()
+  result = api.bot_update.ensure_checkout(force=True)
 
-def _AnnotatedStepsSteps(api):
+  # HACK(iannucci): bot_update.ensure_checkout should return an actual meaninful
+  # object with actual meaningful semantics.
+  got_revision = result.presentation.properties['got_revision']
+  api.gclient.runhooks()
+  return got_revision
+
+def _AnnotatedStepsSteps(api, got_revision):
   # Default environemnt; required by all builders.
   env = {
       'BUILDBOT_MASTERNAME': api.properties['mastername'],
       'BUILDBOT_BUILDERNAME': api.properties['buildername'],
       'BUILDBOT_REVISION': api.properties['revision'],
-      'BUILDBOT_GOT_REVISION': api.properties['got_revision'],
+      'BUILDBOT_GOT_REVISION': got_revision,
       'RUNTEST': api.path['build'].join('scripts', 'slave', 'runtest.py'),
       # NOTE: If this recipe is used for a tryserver/trybots, please change
       # 'BuilderTester' to 'Trybot'.
@@ -68,34 +64,38 @@ def _AnnotatedStepsSteps(api):
     )
 
 def _TriggerTestsSteps(api):
-
   if api.properties['buildername'] in trigger_map:
     api.trigger(
         {'builder_name': trigger_map[api.properties['buildername']],
          'properties': {'parent_slavename': api.properties['slavename']}})
 
 def RunSteps(api):
-  _CheckoutSteps(api)
-  _AnnotatedStepsSteps(api)
+  got_revision = _CheckoutSteps(api)
+  _AnnotatedStepsSteps(api, got_revision)
   _TriggerTestsSteps(api)
 
 def GenTests(api):
-  yield api.test('linux_triggering') +\
-    api.platform('linux', 64) +\
-    api.properties(mastername = 'client.nacl') +\
-    api.properties(buildername = 'precise_64-newlib-arm_qemu-pnacl-dbg') +\
-    api.properties(got_revision = 'abcde') +\
-    api.properties(revision = 'abcd') +\
-    api.properties(slavename = 'TestSlave') +\
-    api.properties(buildnumber = '1234')
-  yield api.test('linux_triggered') +\
-    api.platform('linux', 32) +\
-    api.properties(mastername = 'client.nacl') +\
-    api.properties(buildername = 'oneiric_32-newlib-arm_hw-pnacl-panda-dbg') +\
-    api.properties(got_revision = 'abcde') +\
-    api.properties(revision = 'abcd') +\
-    api.properties(slavename='TestSlave') +\
-    api.properties(parent_slavename = 'TestSlave') +\
-    api.properties(parent_buildername =
-        'precise_64-newlib-arm_qemu-pnacl-dbg') +\
-    api.properties(parent_buildnumber = '1')
+  yield (
+    api.test('linux_triggering') +
+    api.platform('linux', 64) +
+    api.properties(
+      mastername = 'client.nacl',
+      buildername = 'precise_64-newlib-arm_qemu-pnacl-dbg',
+      revision = 'abcd',
+      slavename = 'TestSlave',
+      buildnumber = 1234,
+    ))
+
+  yield (
+    api.test('linux_triggered') +
+    api.platform('linux', 32) +
+    api.properties(
+      mastername = 'client.nacl',
+      buildername = 'oneiric_32-newlib-arm_hw-pnacl-panda-dbg',
+      revision = 'abcd',
+      slavename='TestSlave',
+      buildnumber = 5678,
+      parent_slavename = 'TestSlave',
+      parent_buildername = 'precise_64-newlib-arm_qemu-pnacl-dbg',
+      parent_buildnumber = 1,
+    ))
