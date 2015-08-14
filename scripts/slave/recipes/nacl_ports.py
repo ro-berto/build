74a4f5a4422e4df0fa7631e3b9a95bf64e1addf8
aneeshm@chromium.org <aneeshm@chromium.org>
2015-08-14 23:04:14 +0000

    Convert client.nacl.ports waterfall to recipes
    BUG=
    R=bradnelson@chromium.org
    
    Review URL: https://codereview.chromium.org/1296743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296329 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/nacl_ports.py
@@ -0,0 +1,55 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'bot_update',
+  'gclient',
+  'path',
+  'platform',
+  'properties',
+  'python',
+  'step',
+  'trigger',
+]
+
+
+def _CheckoutSteps(api):
+  api.gclient.set_config('naclports')
+  result = api.bot_update.ensure_checkout(force=True)
+  api.gclient.runhooks()
+  # HACK(aneeshm): Borrowed from iannucci's hack in nacl.py.
+  got_revision = result.presentation.properties['got_revision']
+  return got_revision
+
+def _AnnotatedStepsSteps(api, got_revision):
+  # Default environemnt; required by all builders.
+  env = {
+      'BUILDBOT_MASTERNAME': api.properties['mastername'],
+      'BUILDBOT_BUILDERNAME': api.properties['buildername'],
+      'BUILDBOT_REVISION': api.properties['revision'],
+      'BUILDBOT_GOT_REVISION': got_revision,
+      # NOTE: If this recipe is used for a tryserver/trybots, please change
+      # 'BuilderTester' to 'Trybot'.
+      'BUILDBOT_SLAVE_TYPE': 'BuilderTester',
+  }
+  api.step('annotated steps',
+      [api.path['checkout'].join('build_tools',
+        'buildbot_selector.sh')],
+      allow_subannotations=True,
+      cwd = api.path['checkout'],
+      env = env,
+    )
+
+
+def RunSteps(api):
+  got_revision = _CheckoutSteps(api)
+  _AnnotatedStepsSteps(api, got_revision)
+
+def GenTests(api):
+  yield api.test('linux') +\
+    api.platform('linux', 64) +\
+    api.properties(mastername = 'client.nacl.ports') +\
+    api.properties(buildername = 'linux-glibc-0') +\
+    api.properties(revision = 'abcd') +\
+    api.properties(slavename = 'TestSlave')
