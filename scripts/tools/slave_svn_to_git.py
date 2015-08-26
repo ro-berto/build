4a111aa307ad37f56192c2b05469685871106192
sergiyb@chromium.org <sergiyb@chromium.org>
2015-08-26 23:08:19 +0000

    Restored logging of success results
    Logging was originally removed to prevent log file endlessly growing on
    non-whitelisted hosts, however, we still output "Return code" and date when the
    script is run and result logs grow very slowly (hunderds bytes per day). I
    expect we will be done with SVN-to-Git conversion and disable this script long
    before the size of the logs will cause any issues. Additionally, having this
    output is useful for debugging purposes.
    
    R=sheyang@chromium.org
    BUG=498256
    
    Review URL: https://codereview.chromium.org/1301333004 .
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296441 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/tools/slave_svn_to_git.py
+++ scripts/tools/slave_svn_to_git.py
@@ -106,6 +106,7 @@ def main():
   if not options.manual:
     if not any(cur_host == h if isinstance(h, basestring) else h.match(cur_host)
                for h in WHITELISTED_HOSTS):
+      print 'Host %s is not whitelisted for SVN-to-Git conversion' % cur_host
       return 0
 
   # Set up credentials for the download_from_google_storage hook.
@@ -125,6 +126,7 @@ def main():
     solutions = exec_env['solutions']
   assert len(solutions) == 1, 'Number of solutions in .gclient is not 1'
   if not solutions[0]['url'].startswith('svn:'):
+    print 'Non-SVN URL in .gclient: %s' % solutions[0]['url']
     return 0
   sol_name = solutions[0]['name']
   assert sol_name in GCLIENT_CONFIGS, 'Unknown type of checkout: ' % sol_name
