d00e4f6489b70e99c2b875cfc7f3dc87eb71bb04
shinyak@google.com <shinyak@google.com>
2015-08-31 04:03:45 +0000

    Revert of Choose preferred build slave if any. (Win experimental bots case) (patchset #4 id:60001 of https://codereview.chromium.org/1319433004/ )
    Reason for revert:
    Looks wrong. master is not working.
    
    Original issue's description:
    > Choose preferred build slave if any. (Win trybot case)
    > 
    > Some trybots are sharing many build slaves for the different
    > configurations.
    > 
    > Accoding to shinyak's investigation, when we can reuse the same
    > configuration slave, we can reduce the number of necessary compiles.
    > So compile time can be improved, and goma executor load can be reduced.
    > It is estimated that we can save 20~30% of compile requests, and 10% of
    > goma executor load.
    > 
    > buildbot can configure how to choose the slave by setting 'nextSlave'
    > function. In this patch, we achieve this by:
    > 1) setting 'preferred_builder' value in slave.
    > 2) if a builder name matches the preferred_builder, the builder uses
    > that builder preferrably.
    > 
    > In this patch, we set preferred_builder for win exp bots. exp bots will
    > use the same configuration slave if possible. If no preferred builder,
    > master will choose other slave at random.
    > 
    > When this succeeds, we try to set nextSlave for all CQ builders.
    > 
    > BUG=524581
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=296463
    
    TBR=phajdan.jr@chromium.org,maruel@chromium.org,sergeyberezin@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=524581
    
    Review URL: https://codereview.chromium.org/1305933003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296482 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/unittests/master_utils_test.py
+++ scripts/master/unittests/master_utils_test.py
@@ -23,47 +23,5 @@ class MasterUtilsTest(unittest.TestCase):
     self.assertEquals([['a', 'b'], ['c']], partitions)
 
 
-class MockBuilder(object):
-  def __init__(self, name):
-    self.name = name
-
-
-class PreferredBuilderNextSlaveFuncTest(unittest.TestCase):
-  def testNextSlave(self):
-    builder1 = MockBuilder('builder1')
-    builder2 = MockBuilder('builder2')
-    builder3 = MockBuilder('builder3')
-    slaves = [{
-      'name': 'slave1',
-      'preferred_builder': 'builder1'
-    }, {
-      'name': 'slave2',
-      'preferred_builder': 'builder2',
-    }, {
-      'name': 'slave3',
-      'preferred_builder': 'builder3',
-    }]
-
-    f = master_utils.PreferredBuilderNextSlaveFunc()
-    self.assertEqual('slave1', f(builder1, slaves).get('name'))
-    self.assertEqual('slave2', f(builder2, slaves).get('name'))
-    self.assertEqual('slave3', f(builder3, slaves).get('name'))
-
-    # Remove slave 3.
-    del slaves[2]
-
-    # When there is no slave that matches preferred_builder,
-    # any slave builder might be chosen.
-    self.assertTrue(f(builder3, slaves).get('name') in ['slave1', 'slave2'])
-
-  def testNextSlaveEmpty(self):
-    builder = MockBuilder('builder')
-    slaves = []
-
-    f = master_utils.PreferredBuilderNextSlaveFunc()
-
-    self.assertIsNone(f(builder, slaves))
-
-
 if __name__ == '__main__':
   unittest.main()
