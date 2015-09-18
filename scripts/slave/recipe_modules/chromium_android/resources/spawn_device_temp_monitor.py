2a7d9d7df3fa8979336c19eac0a88fcca948ff01
bpastene@chromium.org <bpastene@chromium.org>
2015-09-18 23:57:16 +0000

    Add timeouts to adb commands in android device monitor
    This cl adds a few things to the device monitoring dameon:
    
    - Adds a timeout to adb device commands since some devices can
    hang (those device_utils in chromium/src sure would be useful)
    - Adds buildername and mastername to the metrics (and removes
    slavename since that's already being provided by hostname)
    - Creates another metric for battery charge level
    
    BUG=519884
    
    Review URL: https://codereview.chromium.org/1358583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296776 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_android/resources/spawn_device_temp_monitor.py
+++ scripts/slave/recipe_modules/chromium_android/resources/spawn_device_temp_monitor.py
@@ -10,6 +10,7 @@ temperatures every 60 seconds via adb and uploads them for monitoring
 through infra's ts_mon.
 """
 
+import argparse
 import json
 import logging
 import os
@@ -35,9 +36,38 @@ _CPU_TEMP_SENSORS = [
 
 # Location of the infra-python package's run script.
 _RUN_PY = '/opt/infra-python/run.py'
+ 
+_TEMP_REGEX = re.compile('^\s*temperature: ([0-9]+)\s*$')
+_CHARGE_REGEX = re.compile('^\s*level: ([0-9]+)\s*$')
 
 
-def get_device_args(adb_path, slave_name, device):
+class AdbDeviceTimeout(Exception):
+  pass
+
+
+def alarm_handler(signum, frame):
+  raise AdbDeviceTimeout
+
+
+def run_adb_command(cmd, timeout=None):
+  """Wrapper function around adb commands that supports timeouts."""
+  if timeout:
+    try:
+      signal.alarm(timeout)
+      proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+      stdout, stderr = proc.communicate()
+      signal.alarm(0)
+      return stdout
+    except AdbDeviceTimeout:
+      os.kill(proc.pid, signal.SIGTERM)
+      raise subprocess.CalledProcessError(1, None, None)
+  else:
+    return subprocess.check_output(cmd)
+
+
+def get_device_args(adb_path, master_name, builder_name, device):
+  bat_charge = None
   bat_temp = None
   cpu_temp = None
   # Search for the file that the _CPU_TEMP_SENSOR dumps to and cat it.
@@ -45,12 +75,12 @@ def get_device_args(adb_path, slave_name, device):
          'grep -lE "%s" /sys/class/thermal/thermal_zone*/type'
            % ('|'.join(_CPU_TEMP_SENSORS))]
   try:
-    cpu_temp_files = subprocess.check_output(cmd)
+    cpu_temp_files = run_adb_command(cmd, timeout=5)
     if (len(cpu_temp_files.splitlines()) == 1):
         cpu_temp_file = re.sub('type$', 'temp', cpu_temp_files.strip())
         cmd = [adb_path, '-s', device, 'shell',
                'cat %s' % (cpu_temp_file)]
-        file_contents = subprocess.check_output(cmd).strip()
+        file_contents = run_adb_command(cmd, timeout=5).strip()
         # Most devices report cpu temp in degrees (C), but a few
         # can report it in thousandths of a degree. If this is in thousandths,
         # chop off the trailing three digits to convert to degrees
@@ -63,48 +93,68 @@ def get_device_args(adb_path, slave_name, device):
   # Dump system battery info and grab the temp.
   cmd = [adb_path, '-s', device, 'shell', 'dumpsys battery']
   try:
-    battery_info = subprocess.check_output(cmd)
+    battery_info = run_adb_command(cmd, timeout=5)
     for line in battery_info.splitlines():
-      m = re.match('^\s*temperature: ([0-9]+)\s*$', line)
-      if m:
-        bat_temp = int(m.group(1))
+      temp_match = _TEMP_REGEX.match(line)
+      charge_match = _CHARGE_REGEX.match(line)
+      if temp_match:
+        bat_temp = int(temp_match.group(1))
+      elif charge_match:
+        bat_charge = int(charge_match.group(1))
+
   except (subprocess.CalledProcessError, TypeError, ValueError):
     bat_temp = None
+    bat_charge = None
 
   cpu_dict = {'name': "dev/cpu/temperature",
               'value': cpu_temp,
               'device_id': device,
-              'slave': slave_name}
+              'master': master_name,
+              'builder': builder_name}
   cpu_temp_args = ['--float', json.dumps(cpu_dict)] if cpu_temp else []
-  battery_dict = {'name': 'dev/battery/temperature',
-                  'value': bat_temp,
-                  'device_id': device,
-                  'slave': slave_name}
+  battery_temp_dict = {'name': 'dev/battery/temperature',
+                       'value': bat_temp,
+                       'device_id': device,
+                       'master': master_name,
+                       'builder': builder_name}
   bat_temp_args = ['--float', 
-                   json.dumps(battery_dict)] if bat_temp else []
-  return cpu_temp_args + bat_temp_args
+                   json.dumps(battery_temp_dict)] if bat_temp else []
+  battery_charge_dict = {'name': 'dev/battery/charge',
+                         'value': bat_charge,
+                         'device_id': device,
+                         'master': master_name,
+                         'builder': builder_name}
+  bat_charge_args = ['--float',
+                     json.dumps(battery_charge_dict)] if bat_charge else []
+  return cpu_temp_args + bat_temp_args + bat_charge_args
 
 
-def main(adb_path,
-         devices_json,
-         slave_name):
+def main(argv):
   """Launches the device temperature monitor.
 
   Polls the devices for their battery and cpu temperatures
   every 60 seconds and uploads them for monitoring through infra's
   ts_mon. Fully qualified, the metric names would be
-  /chrome/infra/dev/(cpu|battery)/temperature
-
-  Args:
-    adb_path: Path to adb binary.
-    devices_json: Json list of device serials to poll.
-    slave_name: Name of the buildbot slave.
+  /chrome/infra/dev/(cpu|battery)/temperature &
+  /chrome/infra/dev/battrery/charge
   """
 
-  devices = json.loads(devices_json)
+  parser = argparse.ArgumentParser(
+      description='Launches the device temperature monitor.')
+  parser.add_argument('adb_path', help='Path to adb binary.')
+  parser.add_argument('devices_json',
+                      help='Json list of device serials to poll.')
+  parser.add_argument('master_name', help='Name of the buildbot master.')
+  parser.add_argument('builder_name', help='Name of the buildbot builder.')
+  args = parser.parse_args(argv)
+
+  signal.signal(signal.SIGALRM, alarm_handler)
+
+  devices = json.loads(args.devices_json)
   while True:
     for device in devices:
-      upload_cmd_args = get_device_args(adb_path, slave_name, device)
+      upload_cmd_args = get_device_args(args.adb_path, args.master_name,
+                                        args.builder_name, device)
 
       cmd = [_RUN_PY, 'infra.tools.send_ts_mon_values'] + upload_cmd_args
       try:
@@ -116,4 +166,4 @@ def main(adb_path,
 
 
 if __name__ == '__main__':
-  sys.exit(main(*sys.argv[1:]))
+  sys.exit(main(sys.argv[1:]))
--- scripts/slave/recipe_modules/chromium_android/tests/spawn_device_temp_monitor_test.py
+++ scripts/slave/recipe_modules/chromium_android/tests/spawn_device_temp_monitor_test.py
@@ -34,17 +34,21 @@ class SimulatedSigterm(Exception):
   pass
 
 
+class mocked_Popen(object):
+  def __init__(self, **kwargs):
+    self.pid = None
+    self.stdout = kwargs.get('stdout', None)
+    self.stderr = kwargs.get('stderr', None)
+
+  def communicate(self):
+    if not self.stderr:
+      return [self.stdout, self.stderr]
+    else:
+      raise self.stderr
+
+
 class MainFuncTest(auto_stub.TestCase):
   def setUp(self):
-    # Collect calls to 'subprocess.Popen', which calls send_ts_mon_values.py.
-    self.send_ts_mon_call = []
-    def mocked_ts_mon_calls(args):
-      self.send_ts_mon_call.extend(args)
-    self.mock(
-        spawn_device_temp_monitor.subprocess,
-        'Popen',
-        mocked_ts_mon_calls)
-
     # Make sleep throw an exception to simulate a sigterm
     # and break out of loop.
     def mocked_sleep_call(duration):
@@ -56,43 +60,53 @@ class MainFuncTest(auto_stub.TestCase):
         mocked_sleep_call)
 
   def test_main_responsive_device(self):
-    # Collect calls to 'subprocess.check_output', which calls adb, and
-    # simulate a responsive device.
-    adb_calls = []
-    def mocked_adb_calls(args):
-      if (args[2] == 'device_serial_1'):
+    # Collect calls to 'subprocess.Popen' and simulate a responsive device.
+    def mocked_popen_calls(args, **kwargs):
+      if (args[0] == spawn_device_temp_monitor._RUN_PY):
+        # ts_mon was called, so collect args and return
+        send_ts_mon_call.extend(args)
+        return None
+      elif (args[0] == '/some/adb/path'):
+        # adb was called, so collect args and define the output
         adb_calls.append(args)
-        if args[4].startswith('grep'):
-          return "some_thermal_file_name"
-        elif args[4].startswith('cat'):
-          return "12"
-        elif args[4].startswith('dumpsys'):
-          return "temperature: 456"
+
+        if (args[2] == 'device_serial_1'):
+          if args[4].startswith('grep'):
+            return mocked_Popen(stdout='some_thermal_file_name', stderr=None)
+          elif args[4].startswith('cat'):
+            return mocked_Popen(stdout='12', stderr=None)
+          elif args[4].startswith('dumpsys'):
+            return mocked_Popen(stdout='temperature: 456\nlevel: 96',
+                                stderr=None)
+          else:
+            self.fail('Unexpected adb command: %s' % (' '.join(args)))
+        elif (args[2] == 'device_serial_2'):
+          if args[4].startswith('grep'):
+            return mocked_Popen(stdout='some_thermal_file_name', stderr=None)
+          elif args[4].startswith('cat'):
+            return mocked_Popen(stdout='56789', stderr=None)
+          elif args[4].startswith('dumpsys'):
+            return mocked_Popen(stdout='level: 11\ntemperature: 987',
+                                stderr=None)
+          else:
+            self.fail('Unexpected adb command: %s' % (' '.join(args)))
         else:
-          self.fail('Unexpected adb command: %s' % (' '.join(args)))
-      elif (args[2] == 'device_serial_2'):
-        adb_calls.append(args)
-        if args[4].startswith('grep'):
-          return "some_thermal_file_name"
-        elif args[4].startswith('cat'):
-          return "56789"
-        elif args[4].startswith('dumpsys'):
-          return "temperature: 987"
-        else:
-          self.fail('Unexpected adb command: %s' % (' '.join(args)))
+          self.fail('Unexpected device serial: %s' % (' '.join(args)))
       else:
-        self.fail('Unexpected device serial: %s' % (' '.join(args)))
+        self.fail('Unexpected Popen call: %s' % (' '.join(args)))
 
     self.mock(
         spawn_device_temp_monitor.subprocess,
-        'check_output',
-        mocked_adb_calls)
+        'Popen',
+        mocked_popen_calls)
     try:
-      self.send_ts_mon_call = []
-      spawn_device_temp_monitor.main(
+      send_ts_mon_call = []
+      adb_calls = []
+      spawn_device_temp_monitor.main([
           '/some/adb/path',
           '["device_serial_1", "device_serial_2"]',
-          'some_slave_name')
+          'some_master_name',
+          'some_builder_name'])
     except SimulatedSigterm:
       pass
 
@@ -100,53 +114,132 @@ class MainFuncTest(auto_stub.TestCase):
     expected_cmd = [spawn_device_temp_monitor._RUN_PY,
         'infra.tools.send_ts_mon_values',
         '--float',
-        '{"slave": "some_slave_name", "name": "dev/cpu/temperature", '
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/cpu/temperature", '
         '"value": 12, "device_id": "device_serial_1"}',
         '--float',
-        '{"slave": "some_slave_name", "name": "dev/battery/temperature", '
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/battery/temperature", '
         '"value": 456, "device_id": "device_serial_1"}',
+        '--float',
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/battery/charge", '
+        '"value": 96, "device_id": "device_serial_1"}',
         spawn_device_temp_monitor._RUN_PY,
         'infra.tools.send_ts_mon_values',
         '--float',
-        '{"slave": "some_slave_name", "name": "dev/cpu/temperature", '
+        '{"builder": "some_builder_name", '
+        '"master": "some_master_name", "name": "dev/cpu/temperature", '
         '"value": 56, "device_id": "device_serial_2"}',
         '--float',
-        '{"slave": "some_slave_name", "name": "dev/battery/temperature", '
-        '"value": 987, "device_id": "device_serial_2"}'
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/battery/temperature", '
+        '"value": 987, "device_id": "device_serial_2"}',
+        '--float',
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/battery/charge", '
+        '"value": 11, "device_id": "device_serial_2"}',
     ]
-    self.assertEquals(expected_cmd, self.send_ts_mon_call)
-
+    self.assertEquals(expected_cmd, send_ts_mon_call)
   
-  def test_main_unresponsive_device(self):
-    # Collect calls to 'subprocess.check_output', which calls adb, and
-    # simulate an unresponsive device.
-    adb_calls = []
-    def mocked_adb_calls(args):
-      adb_calls.extend(args)
-      raise subprocess.CalledProcessError
+  def test_main_dead_device(self):
+    # Collect calls to 'subprocess.Popen' and simulate an unresponsive device.
+    def mocked_popen_calls(args, **kwargs):
+      if (args[0] == spawn_device_temp_monitor._RUN_PY):
+        # ts_mon was called, so collect args and return
+        send_ts_mon_call.extend(args)
+        return None
+      elif (args[0] == '/some/adb/path'):
+        # adb was called, so collect args and throw exception
+        adb_calls.extend(args)
+        raise subprocess.CalledProcessError(1, None, None)
+      else:
+        self.fail('Unexpected Popen call: %s' % (' '.join(args)))
 
     self.mock(
         spawn_device_temp_monitor.subprocess,
-        'check_output',
-        mocked_adb_calls)
+        'Popen',
+        mocked_popen_calls)
     try:
-      self.send_ts_mon_call = []
-      spawn_device_temp_monitor.main(
+      send_ts_mon_call = []
+      adb_calls = []
+      spawn_device_temp_monitor.main([
           '/some/adb/path',
           '["device_serial_1"]',
-          'some_slave_name')
+          'some_master_name',
+          'some_builder_name'])
     except SimulatedSigterm:
       pass
 
     # Should build args to send_ts_mon_values without any metrics.
-    self.assertEquals(2, len(self.send_ts_mon_call))
+    self.assertEquals(2, len(send_ts_mon_call))
     self.assertEquals(
         spawn_device_temp_monitor._RUN_PY,
-        self.send_ts_mon_call[0])
+        send_ts_mon_call[0])
     self.assertEquals(
         'infra.tools.send_ts_mon_values',
-        self.send_ts_mon_call[1])
+        send_ts_mon_call[1])
 
+  def test_main_hung_device(self):
+    # Collect calls to 'subprocess.Popen' and
+    # simulate a device that hangs on dumpsys.
+    def mocked_popen_calls(args, **kwargs):
+      if (args[0] == spawn_device_temp_monitor._RUN_PY):
+        # ts_mon was called, so collect args and return
+        send_ts_mon_call.extend(args)
+        return None
+      elif (args[0] == '/some/adb/path'):
+        # adb was called, so collect args and define the output,
+        # but simulate a timeout when dumpsys is called
+        adb_calls.append(args)
+        if args[4].startswith('grep'):
+          return mocked_Popen(stdout='some_thermal_file_name', stderr=None)
+        elif args[4].startswith('cat'):
+          return mocked_Popen(stdout='12', stderr=None)
+        elif args[4].startswith('dumpsys'):
+          return mocked_Popen(
+              stdout=None,
+              stderr=spawn_device_temp_monitor.AdbDeviceTimeout
+          )
+        else:
+          self.fail('Unexpected adb command: %s' % (' '.join(args)))
+      else:
+        self.fail('Unexpected Popen call: %s' % (' '.join(args)))
+
+    self.mock(
+        spawn_device_temp_monitor.subprocess,
+        'Popen',
+        mocked_popen_calls)
+
+    # Collect calls to os.kill so that no process is actually killed
+    def mocked_kill(*args):
+      pass
+
+    self.mock(
+        spawn_device_temp_monitor.os,
+        'kill',
+        mocked_kill)
+
+    try:
+      send_ts_mon_call = []
+      adb_calls = []
+      spawn_device_temp_monitor.main([
+          '/some/adb/path',
+          '["device_serial_1"]',
+          'some_master_name',
+          'some_builder_name'])
+    except SimulatedSigterm:
+      pass
+
+
+    expected_cmd = [spawn_device_temp_monitor._RUN_PY,
+        'infra.tools.send_ts_mon_values',
+        '--float',
+        '{"builder": "some_builder_name", "master": "some_master_name", '
+        '"name": "dev/cpu/temperature", '
+        '"value": 12, "device_id": "device_serial_1"}'
+    ]
+    self.assertEquals(expected_cmd, send_ts_mon_call)
 
 if __name__ == '__main__':
   unittest.main()
