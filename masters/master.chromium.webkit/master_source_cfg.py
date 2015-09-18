5aba92fff0303546be0850f786a25659453674a6
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-09-18 08:27:14 +0000

    Remove blink scheduler from chromium.webkit
    For context, please see:
    
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/S-P3N0kdkMM/ohfRyTNyAwAJ
    
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/3APcgCM52JQ/OyqNugnFAAAJ
    
    BUG=431478
    
    Review URL: https://codereview.chromium.org/1351623005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296754 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.webkit/master_source_cfg.py
+++ masters/master.chromium.webkit/master_source_cfg.py
@@ -2,18 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from buildbot.changes import svnpoller
 from buildbot.scheduler import AnyBranchScheduler
 
-from common import chromium_utils
-
-from master import build_utils
 from master import gitiles_poller
 
-def WebkitFileSplitter(path):
-  """split_file for webkit.org repository."""
-  projects = ['trunk']
-  return build_utils.SplitPath(projects, path)
 
 def Update(config, _active_master, c):
   # Polls config.Master.trunk_url for changes
@@ -22,21 +14,6 @@ def Update(config, _active_master, c):
       pollInterval=30, project='chromium')
   c['change_source'].append(cr_poller)
 
-  webkit_url = 'http://src.chromium.org/viewvc/blink?view=rev&revision=%s'
-  webkit_poller = svnpoller.SVNPoller(
-      svnurl=config.Master.webkit_root_url,
-      svnbin=chromium_utils.SVN_BIN,
-      split_file=WebkitFileSplitter,
-      pollinterval=30,
-      revlinktmpl=webkit_url,
-      cachepath='webkit.svnrev',
-      project='webkit')
-  c['change_source'].append(webkit_poller)
-
   c['schedulers'].append(AnyBranchScheduler(
       name='global_scheduler', branches=['trunk', 'master'], treeStableTimer=60,
-      builderNames=[]))
-
-  c['schedulers'].append(AnyBranchScheduler(
-      name='global_deps_scheduler', branches=['master'], treeStableTimer=60,
       builderNames=[]))
