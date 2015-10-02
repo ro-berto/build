597b92c2b00c114dace6933d7d1238a688d0bd2e
machenbach@chromium.org <machenbach@chromium.org>
2015-10-02 13:36:07 +0000

    V8 Buildbot: Make greedy allocator a non-tree-closer.
    BUG=chromium:484208
    TBR=danno@chromium.org
    
    Review URL: https://codereview.chromium.org/1365943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@296982 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.v8/mail_notifier_cfg.py
+++ masters/master.client.v8/mail_notifier_cfg.py
@@ -40,6 +40,8 @@ categories_steps = {
 }
 
 exclusions = {
+  'V8 Linux - debug - greedy allocator': [],
+  'V8 Linux64 - debug - greedy allocator': [],
   'V8 Linux64 - cfi': [],
   'V8 Linux - ppc - sim': [],
   'V8 Linux - ppc64 - sim': [],
