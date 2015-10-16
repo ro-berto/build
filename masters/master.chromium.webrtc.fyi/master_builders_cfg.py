4d0188987605f065fe72478aa1b1c6e2394b6ff0
kjellander@chromium.org <kjellander@chromium.org>
2015-10-16 10:12:47 +0000

    WebRTC: Refactor builder configs to reduce duplication.
    This removes a lot of duplication and makes it easier to
    get an overview of what builders are configured for these masters.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1393893006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@297163 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ masters/master.chromium.webrtc.fyi/master_builders_cfg.py
@@ -0,0 +1,127 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from buildbot.changes.filter import ChangeFilter
+from buildbot.scheduler import Periodic
+from buildbot.schedulers.basic import SingleBranchScheduler
+
+from master.factory import annotator_factory
+
+m_annotator = annotator_factory.AnnotatorFactory()
+
+def Update(c):
+  hourly_builders = [
+    'Android Builder (dbg)',
+    'Android Builder ARM64 (dbg)',
+    'Android GN',
+    'Android GN (dbg)',
+    'Linux',
+    'Linux GN',
+    'Linux GN (dbg)',
+    'Mac Builder',
+    'Mac GN',
+    'Mac GN (dbg)',
+  ]
+  win_builders = [
+    'Win Builder',
+    'Win GN',
+    'Win GN (dbg)',
+  ]
+  all_builders = hourly_builders + win_builders
+
+  c['schedulers'].extend([
+      SingleBranchScheduler(name='webrtc_scheduler',
+                            change_filter=ChangeFilter(project='webrtc',
+                                                       branch='master'),
+                            treeStableTimer=0,
+                            builderNames=all_builders),
+      Periodic(name='hourly_periodic_scheduler',
+               periodicBuildTimer=60*60,
+               builderNames=hourly_builders),
+      Periodic(name='4hours_periodic_scheduler',
+               periodicBuildTimer=4*60*60,
+               builderNames=win_builders),
+  ])
+
+  specs = [
+    {'name': 'Win Builder', 'category': 'win'},
+    {'name': 'WinXP Tester', 'category': 'win'},
+    {'name': 'Win7 Tester', 'category': 'win'},
+    {'name': 'Win10 Tester', 'category': 'win'},
+    {
+      'name': 'Win GN',
+      'recipe': 'chromium_gn',
+      'category': 'win',
+      'slavebuilddir': 'win_gn',
+    },
+    {
+      'name': 'Win GN (dbg)',
+      'recipe': 'chromium_gn',
+      'category': 'win',
+      'slavebuilddir': 'win_gn',
+    },
+    {'name': 'Mac Builder', 'category': 'mac'},
+    {'name': 'Mac Tester', 'category': 'mac'},
+    {
+      'name': 'Mac GN',
+      'recipe': 'chromium_gn',
+      'category': 'mac',
+      'slavebuilddir': 'mac_gn',
+    },
+    {
+      'name': 'Mac GN (dbg)',
+      'recipe': 'chromium_gn',
+      'category': 'mac',
+      'slavebuilddir': 'mac_gn',
+    },
+    {'name': 'Linux', 'category': 'linux'},
+    {
+      'name': 'Linux GN',
+      'recipe': 'chromium_gn',
+      'category': 'linux',
+      'slavebuilddir': 'linux_gn',
+    },
+    {
+      'name': 'Linux GN (dbg)',
+      'recipe': 'chromium_gn',
+      'category': 'linux',
+      'slavebuilddir': 'linux_gn',
+    },
+    {'name': 'Android Builder (dbg)', 'category': 'android'},
+    {
+      'name': 'Android Builder ARM64 (dbg)',
+      'category': 'android',
+      'slavebuilddir': 'android_arm64',
+    },
+    {'name': 'Android Tests (dbg) (J Nexus4)', 'category': 'android'},
+    {'name': 'Android Tests (dbg) (K Nexus5)', 'category': 'android'},
+    {'name': 'Android Tests (dbg) (L Nexus5)', 'category': 'android'},
+    {'name': 'Android Tests (dbg) (L Nexus6)', 'category': 'android'},
+    {'name': 'Android Tests (dbg) (L Nexus7.2)', 'category': 'android'},
+    {'name': 'Android Tests (dbg) (L Nexus9)', 'category': 'android'},
+    {
+      'name': 'Android GN',
+      'recipe': 'chromium_gn',
+      'category': 'android',
+      'slavebuilddir': 'android_gn',
+    },
+    {
+      'name': 'Android GN (dbg)',
+      'recipe': 'chromium_gn',
+      'category': 'android',
+      'slavebuilddir': 'android_gn',
+    },
+  ]
+
+  for spec in specs:
+    builder_dict = {
+      'name': spec['name'],
+      'factory': m_annotator.BaseFactory(spec.get('recipe', 'webrtc/chromium')),
+      'category': spec['category'],
+      'notify_on_missing': True,
+    }
+    if 'slavebuilddir' in spec:
+      builder_dict['slavebuilddir'] = spec['slavebuilddir']
+
+    c['builders'].append(builder_dict)
---
+++ masters/master.chromium.webrtc/master_builders_cfg.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from buildbot.changes.filter import ChangeFilter
+from buildbot.schedulers.basic import SingleBranchScheduler
+
+from master.factory import annotator_factory
+
+m_annotator = annotator_factory.AnnotatorFactory()
+
+def Update(c):
+  c['schedulers'].append(
+      SingleBranchScheduler(name='chromium_scheduler',
+                            change_filter=ChangeFilter(project='chromium',
+                                                       branch='master'),
+                            treeStableTimer=60,
+                            builderNames=[
+                              'Win Builder',
+                              'Mac Builder',
+                              'Linux Builder',
+                            ]),
+  )
+  specs = [
+    {'name': 'Win Builder', 'category': 'win'},
+    {'name': 'WinXP Tester', 'category': 'win'},
+    {'name': 'Win7 Tester', 'category': 'win'},
+    {'name': 'Win8 Tester', 'category': 'win'},
+    {'name': 'Win10 Tester', 'category': 'win'},
+    {'name': 'Mac Builder', 'category': 'mac'},
+    {'name': 'Mac Tester', 'category': 'mac'},
+    {'name': 'Linux Builder', 'category': 'linux'},
+    {'name': 'Linux Tester', 'category': 'linux'},
+  ]
+
+  c['builders'].extend([
+      {
+        'name': spec['name'],
+        'factory': m_annotator.BaseFactory('webrtc/chromium'),
+        'category': spec['category'],
+        'notify_on_missing': True,
+      } for spec in specs
+  ])
+
