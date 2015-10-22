86bce10431927ac76084dba141dca31cbf954b90
bustamante@google.com <bustamante@google.com>
2015-10-22 19:21:13 +0000

    Desktop Waterfall - Add steps to build Chrome and run a sample test case
    I want to check this in and see if it all works as expected, the desktop
    waterfall is up at:
    
    https://build.chromium.org/p/client.chromeproxy/console
    
    BUG=462326,462328
    
    Review URL: https://codereview.chromium.org/1418983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@297254 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromeproxy.py
+++ scripts/slave/recipes/chromeproxy.py
@@ -5,15 +5,34 @@
 """Recipe for running ChromeProxy integration tests on desktop VM's."""
 
 DEPS = [
+    'bot_update',
+    'chromium',
+    'gclient',
+    'path',
     'properties',
     'step',
 ]
 
 
 def RunSteps(api):
-  # TODO(bustamante): Add ChromeProxy tests.
-  api.step('Placeholder Step', ['echo', 'Placeholder Step'])
+  # Get the latest checkout, retrieves test code.
+  api.step('Pulling down Chromium', ['echo', 'Pulling down Chromium'])
+  api.chromium.set_config('chromium')
+  api.gclient.set_config('chromium')
+  api.bot_update.ensure_checkout(force=True)
 
+  # Build Chrome with Ninja
+  api.step('Build Chrome', ['echo', 'Building Chrome'])
+  api.chromium.c.gyp_env.GYP_GENERATORS.add('ninja')
+  api.chromium.runhooks()
+  api.chromium.compile(targets=['chrome'])
+
+  # Try running a sample test
+  api.step('Run Sample Smoke Test',
+      [api.path['checkout'].join('tools', 'chrome_proxy', 'run_benchmark'),
+       '--use-live-sites', '--browser=release',
+       '--extra-browser-args="--no-sandbox"',
+       'chrome_proxy_benchmark.smoke.smoke'])
 
 def GenTests(api):
   yield (
