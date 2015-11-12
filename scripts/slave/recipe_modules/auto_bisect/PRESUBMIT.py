9abc57b7cc76ad0b9d196485ca7f69e1c46bfcaa
qyearsley@chromium.org <qyearsley@chromium.org>
2015-11-12 19:01:41 +0000

    Add a PRESUBMIT.py to recipe_modules/auto_bisect.
    R=robertocn
    BUG=455447
    
    Review URL: https://codereview.chromium.org/1439093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@297545 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipe_modules/auto_bisect/PRESUBMIT.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Local presubmit script for the auto_bisect recipe module directory.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts.
+"""
+
+def CommonChecks(input_api, output_api):  # pragma: no cover
+  results = []
+  # TODO(qyearsley): Add pylint checks (input_api.canned_checks.RunPylint).
+  results.extend(
+      input_api.canned_checks.RunUnitTestsInDirectory(
+          input_api, output_api,
+          input_api.PresubmitLocalPath(),
+          whitelist=[r'.+_test\.py$']))
+  return results
+
+
+def CheckChangeOnUpload(input_api, output_api):  # pragma: no cover
+  return CommonChecks(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):  # pragma: no cover
+  return CommonChecks(input_api, output_api)
