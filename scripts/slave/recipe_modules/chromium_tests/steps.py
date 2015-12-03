9e025b59a0090be6288adc64df8e7a0d652d29f0
stip@chromium.org <stip@chromium.org>
2015-12-03 08:04:16 +0000

    Revert of Use None to unset swarming dimensions. (patchset #2 id:20001 of https://chromiumcodereview.appspot.com/1494573003/ )
    Reason for revert:
    this CL was committed without honor. reverting to send in for a proper review
    
    Original issue's description:
    > Use None to unset swarming dimensions.
    > 
    > BUG=525873
    > TBR=sergiyb@chromium.org
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=297810
    
    TBR=jbudorick@chromium.org,maruel@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=525873
    
    Review URL: https://codereview.chromium.org/1498473006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@297824 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_tests/steps.py
+++ scripts/slave/recipe_modules/chromium_tests/steps.py
@@ -543,13 +543,7 @@ class SwarmingTest(Test):
 
     # Add custom dimensions.
     if self._dimensions:
-      for k, v in self._dimensions.iteritems():
-        if v is None:  # pragma: no cover
-          # Remove key if it exists. This allows one to use None to remove
-          # default dimensions.
-          self._tasks[suffix].dimensions.pop(k, None)
-        else:
-          self._tasks[suffix].dimensions[k] = v
+      self._tasks[suffix].dimensions.update(self._dimensions)
 
     # Add config-specific tags.
     self._tasks[suffix].tags.update(api.chromium.c.runtests.swarming_tags)
