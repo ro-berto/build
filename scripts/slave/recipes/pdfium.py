1966c77aca13c6537e7f867a1fb1d5f9e220db88
ochang@chromium.org <ochang@chromium.org>
2015-12-15 00:54:03 +0000

    Add Linux ASan PDFium builders.
    BUG=pdfium:1
    R=dpranke@chromium.org,thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1522273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298016 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/pdfium.py
+++ scripts/slave/recipes/pdfium.py
@@ -14,7 +14,7 @@ DEPS = [
 from recipe_engine.recipe_api import Property
 
 PROPERTIES = {
-  'drmemory': Property(default=False, kind=bool),
+  'memory_tool': Property(default=None),
 }
 
 def _CheckoutSteps(api):
@@ -24,7 +24,13 @@ def _CheckoutSteps(api):
   if branch:
     api.gclient.c.solutions[0].revision = 'origin/' + branch
   api.gclient.checkout()
-  api.gclient.runhooks()
+
+  memory_tool = api.properties.get('memory_tool')
+  env = {}
+  if memory_tool == 'asan':
+    env.update({'GYP_DEFINES': 'asan=1'})
+
+  api.gclient.runhooks(env=env)
 
 
 def _BuildSteps(api):
@@ -33,61 +39,68 @@ def _BuildSteps(api):
   api.step('compile with ninja', ['ninja', '-C', debug_path])
 
 
-def _RunTests(api):
-  drmemory = api.properties.get('drmemory')
+def _RunDrMemoryTests(api):
   pdfium_tests_py = str(api.path['checkout'].join('tools',
                                                   'drmemory',
                                                   'scripts',
                                                   'pdfium_tests.py'))
-  if drmemory:
-    api.python('unittests', pdfium_tests_py,
-               args=['--test', 'pdfium_unittests'],
-               cwd=api.path['checkout'])
-  else:
-    unittests_path = str(api.path['checkout'].join('out', 'Debug',
-                                                   'pdfium_unittests'))
-    if api.platform.is_win:
-      unittests_path += '.exe'
-    api.step('unittests', [unittests_path], cwd=api.path['checkout'])
-
-  if drmemory:
-    api.python('embeddertests', pdfium_tests_py,
-               args=['--test', 'pdfium_embeddertests'],
-               cwd=api.path['checkout'])
-  else:
-    embeddertests_path = str(api.path['checkout'].join('out', 'Debug',
-                                                       'pdfium_embeddertests'))
-    if api.platform.is_win:
-      embeddertests_path += '.exe'
-    api.step('embeddertests', [embeddertests_path],
+  api.python('unittests', pdfium_tests_py,
+             args=['--test', 'pdfium_unittests'],
+             cwd=api.path['checkout'])
+  api.python('embeddertests', pdfium_tests_py,
+             args=['--test', 'pdfium_embeddertests'],
+             cwd=api.path['checkout'])
+  api.python('javascript tests', pdfium_tests_py,
+             args=['--test', 'pdfium_javascript'],
+             cwd=api.path['checkout'])
+  api.python('pixel tests', pdfium_tests_py,
+             args=['--test', 'pdfium_pixel'],
+             cwd=api.path['checkout'])
+  api.python('corpus tests', pdfium_tests_py,
+             args=['--test', 'pdfium_corpus'],
              cwd=api.path['checkout'])
 
-  if drmemory:
-    api.python('javascript tests', pdfium_tests_py,
-               args=['--test', 'pdfium_javascript'],
-               cwd=api.path['checkout'])
-  else:
-    javascript_path = str(api.path['checkout'].join('testing', 'tools',
-                                                    'run_javascript_tests.py'))
-    api.python('javascript tests', javascript_path, cwd=api.path['checkout'])
 
-  if drmemory:
-    api.python('pixel tests', pdfium_tests_py,
-               args=['--test', 'pdfium_pixel'],
-               cwd=api.path['checkout'])
-  else:
-    pixel_tests_path = str(api.path['checkout'].join('testing', 'tools',
-                                                     'run_pixel_tests.py'))
-    api.python('pixel tests', pixel_tests_path, cwd=api.path['checkout'])
+def _RunTests(api):
+  memory_tool = api.properties.get('memory_tool')
+  if memory_tool == 'drmemory':
+    _RunDrMemoryTests(api)
+    return
 
-  if drmemory:
-    api.python('corpus tests', pdfium_tests_py,
-               args=['--test', 'pdfium_corpus'],
-               cwd=api.path['checkout'])
-  else:
-    corpus_tests_path = str(api.path['checkout'].join('testing', 'tools',
-                                                      'run_corpus_tests.py'))
-    api.python('corpus tests', corpus_tests_path, cwd=api.path['checkout'])
+  env = {}
+  if memory_tool == 'asan':
+    # TODO(ochang): Once PDFium is less leaky, remove the detect_leaks flag.
+    env.update({
+        'ASAN_OPTIONS': 'detect_leaks=0:allocator_may_return_null=1'})
+
+  unittests_path = str(api.path['checkout'].join('out', 'Debug',
+                                                 'pdfium_unittests'))
+  if api.platform.is_win:
+    unittests_path += '.exe'
+  api.step('unittests', [unittests_path], cwd=api.path['checkout'], env=env)
+
+  embeddertests_path = str(api.path['checkout'].join('out', 'Debug',
+                                                     'pdfium_embeddertests'))
+  if api.platform.is_win:
+    embeddertests_path += '.exe'
+  api.step('embeddertests', [embeddertests_path],
+           cwd=api.path['checkout'],
+           env=env)
+
+  javascript_path = str(api.path['checkout'].join('testing', 'tools',
+                                                  'run_javascript_tests.py'))
+  api.python('javascript tests', javascript_path,
+             cwd=api.path['checkout'], env=env)
+
+  pixel_tests_path = str(api.path['checkout'].join('testing', 'tools',
+                                                   'run_pixel_tests.py'))
+  api.python('pixel tests', pixel_tests_path,
+             cwd=api.path['checkout'], env=env)
+
+  corpus_tests_path = str(api.path['checkout'].join('testing', 'tools',
+                                                    'run_corpus_tests.py'))
+  api.python('corpus tests', corpus_tests_path,
+             cwd=api.path['checkout'], env=env)
 
 
 def RunSteps(api):
@@ -101,6 +114,8 @@ def GenTests(api):
   yield api.test('win') + api.platform('win', 64)
   yield api.test('linux') + api.platform('linux', 64)
   yield api.test('mac') + api.platform('mac', 64)
+  yield (api.test('linux_asan') + api.platform('linux', 64) +
+         api.properties(memory_tool='asan'))
 
   yield (api.test('win_xfa') + api.platform('win', 64) +
          api.properties(branch='xfa'))
@@ -109,4 +124,6 @@ def GenTests(api):
   yield (api.test('mac_xfa') + api.platform('mac', 64) +
          api.properties(branch='xfa'))
   yield (api.test('drm_win_xfa') + api.platform('win', 64) +
-         api.properties(branch='xfa', drmemory=True))
+         api.properties(branch='xfa', memory_tool='drmemory'))
+  yield (api.test('linux_xfa_asan') + api.platform('linux', 64) +
+         api.properties(branch='xfa', memory_tool='asan'))
