018c698ce88c121e1066e34da78880dc43bd2674
yzshen@chromium.org <yzshen@chromium.org>
2015-12-15 23:21:23 +0000

    Disable mojo_js_unittests on chromium_mojo FYI Android bots.
    BUG=536669
    
    Review URL: https://codereview.chromium.org/1525983004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298029 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium_mojo.py
+++ scripts/slave/recipes/chromium_mojo.py
@@ -172,7 +172,11 @@ def _RunUnitAndAppTests(api):
     api.chromium.runtest('html_viewer_unittests')
     api.chromium.runtest('ipc_mojo_unittests')
     api.chromium.runtest('mojo_common_unittests')
-    api.chromium.runtest('mojo_js_unittests')
+
+    # TODO(yzshen): fix missing JS files on Android. crbug.com/536669
+    if api.chromium.c.TARGET_PLATFORM != 'android':
+      api.chromium.runtest('mojo_js_unittests')
+
     api.chromium.runtest('mojo_public_application_unittests')
     api.chromium.runtest('mojo_public_bindings_unittests')
     api.chromium.runtest('mojo_public_environment_unittests')
