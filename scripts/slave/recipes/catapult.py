0658568197699a155d44523765ad8ad9d56eb325
aiolos@chromium.org <aiolos@chromium.org>
2015-12-18 02:07:53 +0000

    Catapult: Move base/util/run_tests.py to catapult_base/run_tests.py
    TBR=sullivan
    
    Review URL: https://codereview.chromium.org/1538633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298059 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/catapult.py
+++ scripts/slave/recipes/catapult.py
@@ -51,7 +51,7 @@ def RunSteps(api):
   api.python('Build Python Tests',
              api.path['checkout'].join('catapult_build', 'bin', 'run_py_tests'))
   api.python('Util Tests',
-             api.path['checkout'].join('base', 'util', 'run_tests.py'))
+             api.path['checkout'].join('catapult_base', 'run_tests.py'))
   api.python('Dashboard Dev Server Tests Stable',
               api.path['checkout'].join(
                   'dashboard', 'bin', 'run_dev_server_tests'),
