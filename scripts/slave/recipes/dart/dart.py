461bec9451f839899b5a679ade8ce7f81b2b6e43
whesse@google.com <whesse@google.com>
2016-01-04 08:53:49 +0000

    Add recipe-based Dart builder to Dart FYI buildbot
    BUG=
    
    Review URL: https://codereview.chromium.org/1548893003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298112 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/dart/dart.py
@@ -0,0 +1,43 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine.recipe_api import Property
+
+DEPS = [
+  'bot_update',
+  'file',
+  'gclient',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+  'test_utils',
+]
+
+def RunSteps(api):
+  # Enable dart config when it is committed.
+  # api.gclient.set_config('dart')
+  # Until then, fix up a boringssl config
+  api.gclient.set_config('boringssl')
+  s = api.gclient.c.solutions[0]
+  s.name = 'dart'
+  s.url = ('https://chromium.googlesource.com/external/github.com/' +
+           'dart-lang/sdk.git')
+  s.deps_file = 'DEPS'
+  s.managed = False
+  # End of fixes to boringssl config
+  api.bot_update.ensure_checkout(force=True)
+  api.gclient.runhooks()
+  api.python('build dart',
+             api.path['checkout'].join('sdk', 'tools', 'build.py'),
+             args= ['-mrelease', 'runtime'])
+  api.python('test vm',
+             api.path['checkout'].join('sdk', 'tools', 'test.py'),
+             args= ['-mrelease', '-cnone', '-rvm'])
+
+def GenTests(api):
+   yield (
+      api.test('linux64') + api.platform('linux', 64) +
+      api.properties.generic(mastername='client.dart.FYI'))
