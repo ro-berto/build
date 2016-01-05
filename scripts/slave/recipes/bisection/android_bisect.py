9062ccefefb15141371b911b7880d272c37bcb5f
prasadv@chromium.org <prasadv@chromium.org>
2016-01-05 22:17:40 +0000

    Add android bisect fyi bot to tryserver.chromium.perf.
    BUG=574519
    
    Review URL: https://codereview.chromium.org/1564503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298129 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/bisection/android_bisect.py
+++ scripts/slave/recipes/bisection/android_bisect.py
@@ -66,6 +66,13 @@ BUILDERS = freeze({
         'path': lambda api: ('android_perf_rel/full-build-linux_%s.zip' %
                               api.properties['parent_got_revision']),
       },
+      'android_fyi_perf_bisect': {
+        'recipe_config': 'perf',
+        'gclient_apply_config': ['android', 'perf'],
+        'bucket': 'chrome-perf',
+        'path': lambda api: ('android_perf_rel/full-build-linux_%s.zip' %
+                              api.properties['parent_got_revision']),
+      },
     },
   },
 })
