4b523ca499e740fcefd9895eb7ce5298cf238024
zhaoqin@chromium.org <zhaoqin@chromium.org>
2016-01-07 17:13:31 +0000

    Setup LKGR Dr. Memory archive build for ClusterFuzz
    - use 'Windows LKGR (DrMemory)' for 32-bit LKGR archive build
    - recycle 'Windows Tests (DrMemory XP)' for 64-bit LKGR archive build
    
    R=oliver@chromium.org,aarya@chromium.org
    BUG=574167
    
    Review URL: https://codereview.chromium.org/1554073004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298154 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.chromium.fyi/master_win_drmemory_cfg.py
+++ masters/master.chromium.fyi/master_win_drmemory_cfg.py
@@ -15,8 +15,6 @@ helper = master_config.Helper(defaults)
 B = helper.Builder
 F = helper.Factory
 S = helper.Scheduler
-T = helper.Triggerable
-U = helper.URLScheduler
 
 win = lambda: chromium_factory.ChromiumFactory('src/out', 'win32')
 
@@ -25,23 +23,20 @@ defaults['category'] = 'drmemory'
 #
 # Main Dr. Memory release scheduler for src/
 #
-S('win_lkgr_drmemory', branch='master', treeStableTimer=60)
+S('chromium_drmemory_lkgr', branch='lkgr')
 
 #
 # Windows LKGR DrMemory Builder
 #
-U('CR-LKGR', 'https://chromium-status.appspot.com/lkgr', include_revision=True)
-B('Windows LKGR (DrMemory)', 'win_lkgr_drmemory', 'lkgr', 'CR-LKGR',
+B('Windows LKGR (DrMemory)', 'win_lkgr_drmemory',
+  # not use a gatekeeper yet
+  scheduler='chromium_drmemory_lkgr',
   notify_on_missing=True)
 F('win_lkgr_drmemory', win().ChromiumFactory(
     clobber=True,
-    slave_type='BuilderTester',
-    options=['--build-tool=ninja', '--', 'chromium_builder_lkgr_drmemory_win'],
-    compile_timeout=7200,
-    tests=[
-      'drmemory_light_webkit',
-      'drmemory_full_webkit',
-    ],
+    slave_type='Builder',
+    options=['--build-tool=ninja', '--', 'chromium_builder_dbg_drmemory_win'],
+    compile_timeout=9600, # Release build is LONG
     factory_properties={
       'package_pdb_files': True,
       'gclient_env': {
@@ -49,10 +44,37 @@ F('win_lkgr_drmemory', win().ChromiumFactory(
                         'component=shared_library '),
         'GYP_GENERATORS': 'ninja',
       },
-      'late_cf_archive_build': ActiveMaster.is_production_host,
+      'cf_archive_build': ActiveMaster.is_production_host,
       'cf_archive_name': 'drmemory',
       'gs_acl': 'public-read',
-      'gs_bucket': 'gs://chromium-browser-drmemory',
+      'gs_bucket': 'gs://chromium-browser-drfuzz/chromium_win32',
+    }))
+
+#
+# Windows LKGR DrMemory X64 Builder
+#
+B('Windows LKGR (DrMemory x64)', 'win_lkgr_drmemory_x64',
+  # not use a gatekeeper yet
+  scheduler='chromium_drmemory_lkgr',
+  notify_on_missing=True)
+F('win_lkgr_drmemory_x64', win().ChromiumFactory(
+    clobber=True,
+    slave_type='Builder',
+    target='Release_x64',
+    options=['--build-tool=ninja', '--', 'chromium_builder_dbg_drmemory_win'],
+    compile_timeout=9600, # Release build is LONG
+    factory_properties={
+      'package_pdb_files': True,
+      'gclient_env': {
+        'GYP_DEFINES': ('build_for_tool=drmemory '
+                        'component=shared_library '
+                        'target_arch=x64'),
+        'GYP_GENERATORS': 'ninja',
+      },
+      'late_cf_archive_build': ActiveMaster.is_production_host,
+      'cf_archive_name': 'drmemory_x64',
+      'gs_acl': 'public-read',
+      'gs_bucket': 'gs://chromium-browser-drfuzz/chromium_win64',
     }))
 
 def Update(_update_config, _active_master, c):
