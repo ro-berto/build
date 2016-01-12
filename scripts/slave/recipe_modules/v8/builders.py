3be541f330363f670180f7bf454d39f565847051
machenbach@chromium.org <machenbach@chromium.org>
2016-01-12 18:03:21 +0000

    Revert of V8 Buildbot: Switch continuous avx2 bots to swarming. (patchset #2 id:20001 of https://codereview.chromium.org/1584453002/ )
    Reason for revert:
    need to remove the gpu:none default dimension as these bots have a gpu and they starve.
    
    Original issue's description:
    > V8 Buildbot: Switch continuous avx2 bots to swarming.
    > 
    > BUG=575556
    > TBR=tandrii@chromium.org, kjellander@chromium.org
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=298219
    
    TBR=tandrii@chromium.org,kjellander@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=575556
    
    Review URL: https://codereview.chromium.org/1574313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298223 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -203,15 +203,10 @@ BUILDERS = {
           'TARGET_BITS': 32,
         },
         'bot_type': 'tester',
-        'enable_swarming': True,
         'parent_buildername': 'V8 Linux - debug builder',
         'build_gs_archive': 'linux_dbg_archive',
         'tests': [V8Testing, Benchmarks, Mozilla, SimdJs],
         'testing': {'platform': 'linux'},
-        'swarming_dimensions': {
-          'os': 'Ubuntu-14.04',
-          'pool': 'V8-AVX2',
-        },
       },
       'V8 Linux - shared': {
         'chromium_apply_config': [
@@ -484,7 +479,6 @@ BUILDERS = {
         'bot_type': 'tester',
         'parent_buildername': 'V8 Linux64 - builder',
         'build_gs_archive': 'linux64_rel_archive',
-        'enable_swarming': True,
         'tests': [
           V8Testing,
           Benchmarks,
@@ -492,10 +486,6 @@ BUILDERS = {
           SimdJs,
         ],
         'testing': {'platform': 'linux'},
-        'swarming_dimensions': {
-          'os': 'Ubuntu-14.04',
-          'pool': 'V8-AVX2',
-        },
       },
       'V8 Linux64 - debug': {
         'v8_config_kwargs': {
@@ -525,7 +515,6 @@ BUILDERS = {
         'bot_type': 'tester',
         'parent_buildername': 'V8 Linux64 - debug builder',
         'build_gs_archive': 'linux64_dbg_archive',
-        'enable_swarming': True,
         'tests': [
           V8Testing,
           Benchmarks,
@@ -533,10 +522,6 @@ BUILDERS = {
           SimdJs,
         ],
         'testing': {'platform': 'linux'},
-        'swarming_dimensions': {
-          'os': 'Ubuntu-14.04',
-          'pool': 'V8-AVX2',
-        },
       },
       'V8 Linux64 - internal snapshot': {
         'chromium_apply_config': [
