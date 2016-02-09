d7cc8bef3bbb821484198ef3b79e85994f9f6aaa
hinoka@chromium.org <hinoka@chromium.org>
2016-02-09 07:44:16 +0000

    Revert of Enable pubsub status pusher to chromium.fyi (patchset #4 id:60001 of https://codereview.chromium.org/1670323002/ )
    Reason for revert:
    Doesn't work, fails with:
    
    2016-02-08 23:39:42-0800 [-] Unhandled Error
            Traceback (most recent call last):
              File "/home/chrome-bot/buildbot/build/third_party/buildbot_8_4p1/buildbot/master.py", line 241, in loadTheConfigFile
                d = self.loadConfig(f)
              File "/home/chrome-bot/buildbot/build/third_party/buildbot_8_4p1/buildbot/master.py", line 615, in loadConfig
                d.addCallback(do_load)
              File "/home/chrome-bot/buildbot/build/third_party/twisted_10_2/twisted/internet/defer.py", line 297, in addCallback
                callbackKeywords=kw)
              File "/home/chrome-bot/buildbot/build/third_party/twisted_10_2/twisted/internet/defer.py", line 286, in addCallbacks
                self._runCallbacks()
            --- <exception caught here> ---
              File "/home/chrome-bot/buildbot/build/third_party/twisted_10_2/twisted/internet/defer.py", line 542, in _runCallbacks
                current.result = callback(current.result, *args, **kw)
              File "/home/chrome-bot/buildbot/build/third_party/buildbot_8_4p1/buildbot/master.py", line 270, in do_load
                exec f in localDict
              File "/home/chrome-bot/buildbot/build/masters/master.chromium.fyi/master.cfg", line 1698, in <module>
                order_console_by_time=True)
              File "/home/chrome-bot/buildbot/build/scripts/master/master_utils.py", line 337, in AutoSetupMaster
                pubsub_json_status_push.StatusPush(activeMaster=active_master))
              File "/home/chrome-bot/buildbot/build/scripts/master/pubsub_json_status_push.py", line 249, in __init__
                self.topic_url, activeMaster.service_account_file)
              File "/home/chrome-bot/buildbot/build/scripts/master/pubsub_json_status_push.py", line 89, in __init__
                self.client = self._create_pubsub_client(credentials=self.credentials)
              File "/home/chrome-bot/buildbot/build/scripts/master/pubsub_json_status_push.py", line 154, in _create_pubsub_client
                credentials = oauth2client.GoogleCredentials.get_application_default()
              File "/home/chrome-bot/buildbot/build/third_party/oauth2client/oauth2client/client.py", line 1069, in get_application_default
                raise ApplicationDefaultCredentialsError(ADC_HELP_MSG)
            oauth2client.client.ApplicationDefaultCredentialsError: The Application Default Credentials are not available. They are available if running in Google Compute Engine. Otherwise, the environment variable GOOGLE_APPLICATION_CREDENTIALS must be defined pointing to a file defining the credentials. See https://developers.google.com/accounts/docs/application-default-credentials for more information.
    
    
    Original issue's description:
    > Enable pubsub status pusher to chromium.fyi
    > 
    > BUG=
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=298657
    
    TBR=estaab@chromium.org,dnj@chromium.org,hinoka@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1684523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298666 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/master/master_utils.py
+++ scripts/master/master_utils.py
@@ -332,11 +332,13 @@ def AutoSetupMaster(c, active_master, mail_notifier=False,
   # topic.  This will not run unless is_production_host is set to True.
   # This will fail on a production host if it cannot find the service
   # account file.
-  try:
+  topic_url = getattr(active_master, 'pubsub_topic_url', None)
+  if topic_url:
     c['status'].append(
-      pubsub_json_status_push.StatusPush(activeMaster=active_master))
-  except pubsub_json_status_push.NotEnabled:
-    log.msg('Pubsub not enabled.')
+      pubsub_json_status_push.StatusPush(
+          activeMaster=active_master, topic_url=topic_url))
+  else:
+    log.msg('No pubsub topic url found, not enabling pubsub json publishing.')
 
   # For all production masters, notify our health-monitoring webapp.
   if enable_http_status_push:
