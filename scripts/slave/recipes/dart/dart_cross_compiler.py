f6dd2abdf16a3a82c3515a83c78e928d31b89e03
whesse@google.com <whesse@google.com>
2016-02-15 10:46:25 +0000

    Add Dart cross-compilation recipe
    BUG=
    
    Review URL: https://codereview.chromium.org/1689573006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@298790 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ scripts/slave/recipes/dart/dart_cross_compiler.py
@@ -0,0 +1,92 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/gclient',
+  'file',
+  'gsutil',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+  'test_utils',
+  'trigger',
+]
+
+GCS_BUCKET = 'gs://dart-cross-compiled-binaries'
+
+builders = {
+  'cross-arm-vm-linux-release': {
+    'mode': 'release',
+    'target_arch': 'arm'},
+  'test-coverage': {
+    'mode': 'release',
+    'target_arch': 'x64',
+    'clobber': True},
+}
+
+# TODO: remove "recipe"
+def tarball_name(arch, mode, revision):
+  return 'cross_recipe_build_%s_%s_%s.tar.bz2' % (arch, mode, revision)
+
+def RunSteps(api):
+  api.gclient.set_config('dart')
+  api.path.c.dynamic_paths['tools'] = None
+  update_step = api.bot_update.ensure_checkout(force=True)
+  revision = update_step.json.output['fixed_revisions']['sdk']
+
+  api.path['tools'] = api.path['checkout'].join('tools')
+  buildername = api.properties.get('buildername')
+  (buildername, _, channel) = buildername.rpartition('-')
+  assert channel in ['be', 'dev', 'stable', 'integration']
+  buildername = buildername.replace('-recipe', '')
+  b = builders[buildername]
+
+  if b.get('clobber', False):
+      api.python('clobber',
+                 api.path['tools'].join('clean_output_directory.py'),
+                 cwd=api.path['checkout'])
+
+  api.gclient.runhooks()
+
+  build_args = ['-m%s' % b['mode'], '--arch=%s' % b['target_arch'], 'runtime']
+  api.python('build dart',
+             api.path['checkout'].join('tools', 'build.py'),
+             args=build_args,
+             cwd=api.path['checkout'])
+  tarball = tarball_name(b['target_arch'], b['mode'], revision)
+  api.step('create tarball',
+           ['tar', '-cjf', tarball, '--exclude=**/obj',
+             '--exclude=**/obj.host', '--exclude=**/obj.target',
+             '--exclude=**/*analyzer*', 'out/', 'tools/testing/bin/'],
+           cwd=api.path['checkout'])
+
+  uri = "%s/%s" % (GCS_BUCKET, tarball)
+  api.gsutil(['cp', '-a', 'public-read', tarball, uri],
+             name='upoad tarball',
+             cwd=api.path['checkout'])
+
+  # Trigger slaves
+  trigger_spec = [{
+    # TODO: Remove "recipe"
+    'builder_name': 'target-arm-vm-linux-release-recipe-%s' % channel
+  }]
+  api.trigger(*trigger_spec)
+
+
+def GenTests(api):
+  yield (
+    api.test('cross-arm-vm-linux-release-recipe') +
+    api.platform('linux', 64) +
+    api.properties.generic(mastername='client.dart',
+                           buildername='cross-arm-vm-linux-release-recipe-be',
+                           revision='abcd1234efef5656'))
+  yield (
+    api.test('clobber-coverage') +
+    api.platform('linux', 64) +
+    api.properties.generic(mastername='client.dart',
+                           buildername='test-coverage-stable',
+                           revision='abcd1234efef5656'))
---
+++ scripts/slave/recipes/dart/dart_cross_tester.py
@@ -0,0 +1,89 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/gclient',
+  'file',
+  'gsutil',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+  'test_utils',
+]
+
+GCS_BUCKET = 'gs://dart-cross-compiled-binaries'
+
+builders = {
+  'target-arm-vm-linux-release': {
+    'mode': 'release',
+    'target_arch': 'arm'},
+}
+
+# TODO: remove "recipe"
+def tarball_name(arch, mode, revision):
+  return 'cross_recipe_build_%s_%s_%s.tar.bz2' % (arch, mode, revision)
+
+def RunSteps(api):
+  api.gclient.set_config('dart')
+  api.path.c.dynamic_paths['tools'] = None
+  revision = api.properties['revision']
+  api.bot_update.ensure_checkout(force=True)
+
+  api.path['tools'] = api.path['checkout'].join('tools')
+  buildername = api.properties.get('buildername')
+  (buildername, _, channel) = buildername.rpartition('-')
+  assert channel in ['be', 'dev', 'stable', 'integration']
+  buildername = buildername.replace('-recipe', '')
+  b = builders[buildername]
+
+  api.python('clobber',
+             api.path['tools'].join('clean_output_directory.py'),
+             cwd=api.path['checkout'], ok_ret='any')
+  api.gclient.runhooks()
+
+  tarball = tarball_name(b['target_arch'], b['mode'], revision)
+  uri = "%s/%s" % (GCS_BUCKET, tarball)
+  api.gsutil(['cp', uri, tarball],
+             name='download tarball',
+             cwd=api.path['checkout'])
+  api.step('untar tarball', ['tar', '-xjf', tarball], cwd=api.path['checkout'])
+
+  with api.step.defer_results():
+    test_args = ['--mode=%s' % b['mode'],
+                 '--arch=%s' % b['target_arch'],
+                 '--progress=line',
+                 '--report',
+                 '--time',
+                 '--failure-summary',
+                 '--write-debug-log',
+                 '--write-test-outcome-log',
+                 '--exclude-suite=pkg']
+    test_args.extend(b.get('test_args', []))
+    api.python('vm tests',
+               api.path['tools'].join('test.py'),
+               args=test_args,
+               cwd=api.path['checkout'])
+    test_args.extend(['--checked', '--append_logs'])
+    api.python('checked vm tests',
+               api.path['tools'].join('test.py'),
+               args=test_args,
+               cwd=api.path['checkout'])
+    api.step('delete tarball',
+             ['rm', tarball],
+             cwd=api.path['checkout'])
+    api.python('clobber',
+               api.path['tools'].join('clean_output_directory.py'),
+               cwd=api.path['checkout'])
+
+
+def GenTests(api):
+  yield (
+    api.test('target-arm-vm-linux-release') +
+    api.platform('linux', 64) +
+    api.properties.generic(mastername='client.dart',
+                           buildername='target-arm-vm-linux-release-be',
+                           revision='abcd1234efef5656'))
