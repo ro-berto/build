c6fa0355eaacfa8fbed6502cda7043895bd693fb
sullivan@chromium.org <sullivan@chromium.org>
2016-03-10 00:02:45 +0000

    Revert of Rewrote catapult recipe to use generator_script to read the test list from the catapult repo (patchset #4 id:60001 of https://codereview.chromium.org/1672193002/ )
    Reason for revert:
    Breakages on Windows and Linux
    https://build.chromium.org/p/tryserver.client.catapult/builders/Catapult%20Windows%20Tryserver/builds/2192
    https://build.chromium.org/p/tryserver.client.catapult/builders/Catapult%20Linux%20Tryserver/builds/2340
    
    Original issue's description:
    > Rewrote catapult recipe to use generator_script to read the test list from the catapult repo.
    > 
    > This depends on https://codereview.chromium.org/1777723003/ being committed in the catapult repo.
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=299193
    
    TBR=iannucci@chromium.org,agable@chromium.org,luqui@chromium.org,martiniss@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1771853006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@299199 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/catapult.py
+++ scripts/slave/recipes/catapult.py
@@ -7,7 +7,6 @@ DEPS = [
   'depot_tools/bot_update',
   'depot_tools/gclient',
   'gitiles',
-  'recipe_engine/generator_script',
   'recipe_engine/path',
   'recipe_engine/platform',
   'recipe_engine/properties',
@@ -15,7 +14,7 @@ DEPS = [
 ]
 
 
-def _CheckoutSteps(api):
+def _CheckoutSteps(api, buildername):
   """Checks out the catapult repo (and any dependencies) using gclient."""
   api.gclient.set_config('catapult')
   api.bot_update.ensure_checkout(force=True)
@@ -39,38 +38,99 @@ def _FetchAppEngineSDKSteps(api):
   return api.path['slave_build'].join('google_appengine')
 
 
-def _RemoteSteps(api, app_engine_sdk_path):
-  """Runs the build steps specified in catapult_build/build_steps.py.
-
-  Steps are specified in catapult repo in order to avoid multi-sided patches
-  when updating tests and adding/moving directories.
-
-  This step uses the generator_script; see documentation at
-  github.com/luci/recipes-py/blob/master/recipe_modules/generator_script/api.py
-
-  Use the test_checkout_path property in local tests to run against a local
-  copy of catapult_build/build_steps.py.
-  """
-  base = api.properties.get('test_checkout_path', str(api.path['checkout']))
-  script = api.path.join(base, 'catapult_build', 'build_steps.py')
-  args = [
-      script,
-      '--api-path-checkout', api.path['checkout'],
-      '--app-engine-sdk-pythonpath', app_engine_sdk_path,
-      '--platform', api.platform.name,
-  ]
-  return api.generator_script(*args)
-
-
 def RunSteps(api):
-  _CheckoutSteps(api)
+  buildername = api.properties.get('buildername')
+  _CheckoutSteps(api, buildername)
 
   # The dashboard unit tests depend on Python modules in the App Engine SDK,
   # and the unit test runner script assumes that the SDK is in PYTHONPATH.
   sdk_path = _FetchAppEngineSDKSteps(api)
-  app_engine_sdk_path = api.path.pathsep.join([
-      '%(PYTHONPATH)s', str(sdk_path)])
-  _RemoteSteps(api, app_engine_sdk_path)
+  modified_env = {
+    'PYTHONPATH': api.path.pathsep.join(['%(PYTHONPATH)s', str(sdk_path)])
+  }
+
+  # The sandbox is needed to run chrome on linux, but is harmless to set on
+  # other platforms.
+  sandbox_env = {
+    'CHROME_DEVEL_SANDBOX': '/opt/chromium/chrome_sandbox'
+  }
+
+  api.python('Build Python Tests',
+             api.path['checkout'].join('catapult_build', 'bin', 'run_py_tests'))
+  api.python('Catapult Base Tests',
+             api.path['checkout'].join('catapult_base', 'bin', 'run_tests'))
+  api.python('Dependency Manager Tests',
+             api.path['checkout'].join(
+                 'dependency_manager', 'bin', 'run_tests'))
+  api.python('Dashboard Dev Server Tests Stable',
+              api.path['checkout'].join(
+                  'dashboard', 'bin', 'run_dev_server_tests'),
+              ['--no-install-hooks', '--no-use-local-chrome',
+               '--channel=stable'])
+  api.python('Dashboard Dev Server Tests Canary',
+              api.path['checkout'].join(
+                  'dashboard', 'bin', 'run_dev_server_tests'),
+              ['--no-install-hooks', '--no-use-local-chrome',
+               '--channel=canary'])
+  api.python('Dashboard Python Tests',
+             api.path['checkout'].join('dashboard', 'bin', 'run_py_tests'),
+             ['--no-install-hooks'],
+             env=modified_env)
+  api.python('Tracing Python Tests',
+             api.path['checkout'].join('tracing', 'bin', 'run_py_tests'),
+             ['--no-install-hooks'])
+  api.python('Tracing Dev Server Tests Stable',
+             api.path['checkout'].join(
+                 'tracing', 'bin', 'run_dev_server_tests'),
+             ['--no-install-hooks',
+              '--no-use-local-chrome',
+              '--channel=stable'])
+  api.python('Py-vulcanize Tests',
+             api.path['checkout'].join(
+                 'third_party', 'py_vulcanize', 'bin', 'run_py_tests'),
+             ['--no-install-hooks'])
+  api.python('Perf Insights Dev Server Tests Stable',
+             api.path['checkout'].join(
+                 'perf_insights', 'bin', 'run_dev_server_tests'),
+             ['--no-install-hooks',
+              '--no-use-local-chrome',
+              '--channel=stable'])
+  api.python('Perf Insights Dev Server Tests Canary',
+             api.path['checkout'].join(
+                 'perf_insights', 'bin', 'run_dev_server_tests'),
+             ['--no-install-hooks',
+              '--no-use-local-chrome',
+              '--channel=canary'])
+  api.python('Systrace Tests',
+             api.path['checkout'].join('systrace', 'bin', 'run_tests'))
+  api.python('Telemetry Tests with Stable Browser',
+             api.path['checkout'].join('telemetry', 'bin', 'run_tests'),
+             ['--browser=reference',
+              '--start-xvfb'],
+              env=sandbox_env)
+  api.python('Vinn Tests',
+             api.path['checkout'].join('third_party', 'vinn', 'run_test'))
+  api.python('Tracing D8 Tests',
+             api.path['checkout'].join('tracing', 'bin', 'run_vinn_tests'))
+  api.python('Perf Vinn Insights Tests',
+             api.path['checkout'].join(
+                 'perf_insights', 'bin', 'run_vinn_tests'))
+  api.python('Perf Insights Python Tests',
+             api.path['checkout'].join(
+                 'perf_insights', 'bin', 'run_py_tests'),
+             ['--no-install-hooks'])
+  if not api.platform.is_win:
+    # Test failing on Windows:
+    # https://github.com/catapult-project/catapult/issues/1816
+    api.python('Tracing Dev Server Tests Canary',
+               api.path['checkout'].join(
+                   'tracing', 'bin', 'run_dev_server_tests'),
+               ['--no-install-hooks',
+                '--no-use-local-chrome',
+                '--channel=canary'])
+  if api.platform.is_linux:
+    api.python('Devil Python Tests',
+               api.path['checkout'].join('devil', 'bin', 'run_py_tests'))
 
 
 def GenTests(api):
@@ -81,9 +141,5 @@ def GenTests(api):
                    slavename='windows_slave') +
     api.step_data('Fetch SDK downloader',
                   api.gitiles.make_encoded_file(
-                      '"<simulated contents of get_appengine.py>"')) +
-    api.generator_script(
-      'build_steps.py',
-      {'name': 'Dashboard Tests', 'cmd': ['run_py_tests', '--no-hooks']},
-    )
+                      '"<simulated contents of get_appengine.py>"'))
   )
