417b410572052a1916bc6beb35be7450291ef5b6
ukai@chromium.org <ukai@chromium.org>
2016-03-18 02:24:19 +0000

    Revert of ensure_goma for goma canary (patchset #1 id:1 of https://codereview.chromium.org/1805923005/ )
    Reason for revert:
    oops
    Traceback (most recent call last):
      File "/b/build/scripts/slave/recipe_modules/goma/resources/ensure_goma.py", line 69, in <module>
        sys.exit(main(sys.argv[1:]))
      File "/b/build/scripts/slave/recipe_modules/goma/resources/ensure_goma.py", line 50, in main
        if args.canary:
    AttributeError: 'Namespace' object has no attribute 'canary'
    step returned non-zero exit code: 1
    
    Original issue's description:
    > ensure_goma for goma canary
    > 
    > for bot configured as goma_canary, it will use head of goma repo,
    > instead of pinned version configured in ensure_goma.py
    > 
    > fix typo s/arg/args/
    > 
    > BUG=595234
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=299376
    
    TBR=phajdan.jr@chromium.org,iannucci@chromium.org,scottmg@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=595234
    
    Review URL: https://codereview.chromium.org/1814953003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@299377 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/goma/api.py
+++ scripts/slave/recipe_modules/goma/api.py
@@ -9,7 +9,6 @@ class GomaApi(recipe_api.RecipeApi):
 
   def update_goma_canary(self):
     """Returns a step for updating goma canary."""
-    # deprecated? switch to use ensure_goma with canary=True.
     # for git checkout, should use @refs/heads/master to use head.
     head = 'refs/heads/master'
     # TODO(phajdan.jr): Remove path['build'] usage, http://crbug.com/437264 .
@@ -18,17 +17,14 @@ class GomaApi(recipe_api.RecipeApi):
                     '--revision', 'build/goma@%s' % head],
                    cwd=self.m.path['build'])
 
-  def ensure_goma(self, goma_dir, canary=False):
+  def ensure_goma(self, goma_dir):
     # TODO(iannucci): switch to CIPD (https://goto.google.com/toxxq).
-    args=[
-        '--target-dir', goma_dir,
-        '--download-from-google-storage-path',
-        self.m.depot_tools.download_from_google_storage_path
-    ]
-    if canary:
-      args += ['--canary']
     self.m.python(
       name='ensure_goma',
       script=self.resource('ensure_goma.py'),
-      args=args,
+      args=[
+        '--target-dir', goma_dir,
+        '--download-from-google-storage-path',
+        self.m.depot_tools.download_from_google_storage_path
+      ],
       infra_step=True)
