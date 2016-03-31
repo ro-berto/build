95fef71b5ff39eca4ae47463a1f74ba7982df060
wfh@chromium.org <wfh@chromium.org>
2016-03-31 07:31:12 +0000

    Make win10_chromium_x64_rel_ng use correct 64-bit builder.
    BUG=513354
    
    Review URL: https://codereview.chromium.org/1842263003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@299583 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_tests/trybots.py
+++ scripts/slave/recipe_modules/chromium_tests/trybots.py
@@ -566,7 +566,7 @@ TRYBOTS = freeze({
       },
       'win10_chromium_x64_rel_ng': simple_bot({
         'mastername': 'chromium.win',
-        'buildername': 'Win Builder',
+        'buildername': 'Win x64 Builder',
         'tester': 'Win10 Tests x64',
       }),
       'win_chromium_compile_dbg_ng': simple_bot({
