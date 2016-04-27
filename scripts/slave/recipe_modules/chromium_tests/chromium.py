0e146baffd757ebb80480437724506953c6e8f0f
aneeshm@chromium.org <aneeshm@chromium.org>
2016-04-27 22:57:09 +0000

    Enable MB on chromium Mac and Win builders
    BUG=481692
    R=dpranke@chromium.org
    
    Review-Url: https://codereview.chromium.org/1924643003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@300250 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_tests/chromium.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium.py
@@ -11,6 +11,7 @@ SPEC = {
       'chromium_apply_config': [
         'clobber',
         'isolation_mode_noop',
+        'mb',
         'ninja_confirm_noop',
         'no_dump_symbols',
       ],
@@ -67,6 +68,7 @@ SPEC = {
       'chromium_apply_config': [
         'clobber',
         'isolation_mode_noop',
+        'mb',
         'ninja_confirm_noop',
         'no_dump_symbols',
       ],
