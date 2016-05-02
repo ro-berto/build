25b1131c9daa7c8ad17caa3968913c547ef09664
aneeshm@chromium.org <aneeshm@chromium.org>
2016-05-02 23:43:08 +0000

    Remove obsolete builder from the chromium.fyi misc autogen recipe
    Specifically, the 'Android Asan Builder Tests (dbg)' builder.
    
    BUG=607727
    R=dpranke@chromium.org
    
    Review-Url: https://codereview.chromium.org/1937853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@300415 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipes/chromium.fyi.misc_block.recipe_autogen.py
+++ scripts/slave/recipes/chromium.fyi.misc_block.recipe_autogen.py
@@ -62,48 +62,6 @@ def Android_ChromeDriver_Tests__dbg__steps(api):
         allow_subannotations=True)
 
 
-def Android_Asan_Builder_Tests__dbg__steps(api):
-    build_properties = api.properties.legacy()
-    # update scripts step; implicitly run by recipe engine.
-    # bot_update step
-    src_cfg = api.gclient.make_config(GIT_MODE=True)
-    soln = src_cfg.solutions.add()
-    soln.name = "src"
-    soln.url = "https://chromium.googlesource.com/chromium/src.git"
-    soln.custom_deps = {'src/third_party/WebKit/LayoutTests': None}
-    soln.custom_vars = {'webkit_trunk': 'http://src.chromium.org/blink/trunk',
-                        'googlecode_url': 'http://%s.googlecode.com/svn',
-                        'nacl_trunk':
-                        'http://src.chromium.org/native_client/trunk',
-                        'sourceforge_url':
-                        'https://svn.code.sf.net/p/%(repo)s/code',
-                        'llvm_url': 'http://llvm.org/svn/llvm-project'}
-    src_cfg.target_os = set(['android'])
-    src_cfg.got_revision_mapping.update(
-        {'src': 'got_revision',
-         'src/third_party/WebKit': 'got_webkit_revision',
-         'src/tools/swarming_client': 'got_swarming_client_revision',
-         'src/v8': 'got_v8_revision'})
-    api.gclient.c = src_cfg
-    result = api.bot_update.ensure_checkout(force=True)
-    build_properties.update(result.json.output.get("properties", {}))
-    # gclient revert step; made unnecessary by bot_update
-    # gclient update step; made unnecessary by bot_update
-    # cleanup_temp step
-    api.chromium.cleanup_temp()
-    # slave_steps step
-    api.python(
-        "slave_steps",
-        "src/build/android/buildbot/bb_run_bot.py",
-        args=
-        ['--build-properties=%s' % api.json.dumps(build_properties,
-                                                  separators=(',', ':')),
-         ('--factory-properties={"GYP_DEFINES":" component=shared_library",'
-          '"android_bot_id":"asan-builder-tests-dbg","clobber":false,'
-          '"gclient_env":{},"target":"Debug","target_os":"android"}')],
-        allow_subannotations=True)
-
-
 def CFI_Linux_CF_steps(api):
     build_properties = api.properties.legacy()
     # update scripts step; implicitly run by recipe engine.
@@ -169,7 +127,6 @@ def CFI_Linux_CF_steps(api):
 
 dispatch_directory = {
     'Android ChromeDriver Tests (dbg)': Android_ChromeDriver_Tests__dbg__steps,
-    'Android Asan Builder Tests (dbg)': Android_Asan_Builder_Tests__dbg__steps,
     'CFI Linux CF': CFI_Linux_CF_steps,
 }
 
@@ -185,14 +142,6 @@ def GenTests(api):
   yield (api.test('Android_ChromeDriver_Tests__dbg_') +
     api.properties(mastername='chromium.fyi') +
     api.properties(buildername='Android ChromeDriver Tests (dbg)') +
-    api.properties(revision='123456789abcdef') +
-    api.properties(got_revision='123456789abcdef') +
-    api.properties(buildnumber='42') +
-    api.properties(slavename='TestSlave')
-        )
-  yield (api.test('Android_Asan_Builder_Tests__dbg_') +
-    api.properties(mastername='chromium.fyi') +
-    api.properties(buildername='Android Asan Builder Tests (dbg)') +
     api.properties(revision='123456789abcdef') +
     api.properties(got_revision='123456789abcdef') +
     api.properties(buildnumber='42') +
