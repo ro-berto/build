9ec437e843d1f220c745ee60c1b0b3ac203985ce
kbr@chromium.org <kbr@chromium.org>
2016-05-03 00:34:01 +0000

    Deploy Nexus 5X onto chromium.gpu.fyi waterfall.
    BUG=591449
    
    Review-Url: https://codereview.chromium.org/1941213002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@300420 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_tests/chromium_gpu_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_gpu_fyi.py
@@ -750,6 +750,31 @@ SPEC = {
       },
       'enable_swarming': True,
     },
+    'Android Debug (Nexus 5X)': {
+      'chromium_config': 'android',
+      'chromium_apply_config': ['chrome_with_codecs', 'mb'],
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['android'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Debug',
+        'TARGET_BITS': 32,
+        'TARGET_PLATFORM': 'android',
+      },
+      'android_config': 'main_builder',
+      'bot_type': 'builder_tester',
+      'compile_targets': [
+      ],
+      'test_generators': [
+        steps.generate_gtest,
+        steps.generate_isolated_script,
+        steps.generate_script,
+      ],
+      'testing': {
+        'platform': 'linux',
+      },
+      'use_isolate': False,
+      'enable_swarming': False,
+    },
     'Android Debug (Nexus 9)': {
       'chromium_config': 'android',
       'chromium_apply_config': ['chrome_with_codecs', 'mb'],
