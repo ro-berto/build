b3483fc2fc9b5743a888da6c13b6705581f0f7a5
dtu@chromium.org <dtu@chromium.org>
2016-05-13 22:02:05 +0000

    Re-enable sizes test on tryserver.chromium.perf.
    BUG=610772
    
    Review-Url: https://codereview.chromium.org/1974703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@300583 0039d316-1c4b-4281-b951-d872f2087c98

--- scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_perf.py
+++ scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_perf.py
@@ -11,23 +11,35 @@ from . import steps
 def _Builders():
   # These should build with the same configs as the main perf builders.
   builders = {}
-  for bisect_bot_name, perf_bot_name in _PERF_BOT_MAPPINGS.iteritems():
+
+  for bisect_bot_name, perf_bot_name in _BUILDER_MAPPINGS.iteritems():
+    config = copy.deepcopy(chromium_perf.SPEC['builders'][perf_bot_name])
+    config['tests'] = [steps.SizesStep(None, None)]
+    builders[bisect_bot_name] = config
+
+  for bisect_bot_name, perf_bot_name in _TESTER_MAPPINGS.iteritems():
     config = copy.deepcopy(chromium_perf.SPEC['builders'][perf_bot_name])
     del config['tests']  # Don't run the same tests as the perf waterfall.
     builders[bisect_bot_name] = config
+
   return builders
 
 
-_PERF_BOT_MAPPINGS = {
+_BUILDER_MAPPINGS = {
     'linux_perf_bisect_builder': 'Linux Builder',
+    'win_perf_bisect_builder': 'Win Builder',
+    'winx64_bisect_builder': 'Win x64 Builder',
+    'mac_perf_bisect_builder': 'Mac Builder',
+}
+
+
+_TESTER_MAPPINGS = {
     'linux_perf_bisect': 'Linux Builder',
     'linux_perf_cq': 'Linux Builder',
     'linux_fyi_perf_bisect': 'Linux Builder',
-    'win_perf_bisect_builder': 'Win Builder',
     'win_perf_bisect': 'Win Builder',
     'win_8_perf_bisect': 'Win Builder',
     'winx64_10_perf_bisect': 'Win x64 Builder',
-    'winx64_bisect_builder': 'Win x64 Builder',
     'win_x64_perf_bisect': 'Win x64 Builder',
     'winx64ati_perf_bisect': 'Win x64 Builder',
     'winx64nvidia_perf_bisect': 'Win x64 Builder',
@@ -35,7 +47,6 @@ _PERF_BOT_MAPPINGS = {
     'winx64_zen_perf_bisect': 'Win x64 Builder',
     'winx64_10_perf_cq': 'Win x64 Builder',
     'win_fyi_perf_bisect': 'Win Builder',
-    'mac_perf_bisect_builder': 'Mac Builder',
     'mac_10_11_perf_bisect': 'Mac Builder',
     'mac_10_10_perf_bisect': 'Mac Builder',
     'mac_retina_perf_bisect': 'Mac Builder',
