c3ce4677836fd2d503dce25759b001dd2dd341d9
davidben@chromium.org <davidben@chromium.org>
2016-05-18 18:46:10 +0000

    Convert master.client.boringssl to builders.pyl.
    BUG=boringssl:32
    
    Review-Url: https://codereview.chromium.org/1936723002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/build@300639 0039d316-1c4b-4281-b951-d872f2087c98

--- masters/master.client.boringssl/master_site_config.py
+++ masters/master.client.boringssl/master_site_config.py
@@ -2,14 +2,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This file was generated from
+# scripts/tools/buildbot_tool_templates/master_site_config.py
+# by "../../build/scripts/tools/buildbot-tool gen .".
+# DO NOT EDIT BY HAND!
+
+
 """ActiveMaster definition."""
 
 from config_bootstrap import Master
 
 class Boringssl(Master.Master3):
   project_name = 'Boringssl'
-  project_url = 'https://boringssl.googlesource.com/boringssl/'
   master_port = 20311
   slave_port = 30311
   master_port_alt = 25311
-  buildbot_url = 'http://build.chromium.org/p/client.boringssl/'
+  buildbot_url = 'https://build.chromium.org/p/client.boringssl/'
+  buildbucket_bucket = None
+  service_account_file = None
