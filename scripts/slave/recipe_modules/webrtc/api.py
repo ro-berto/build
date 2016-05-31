8f0ba2f84667ab6c17b55d8d3edec3fcb5e0aa56
kjellander <kjellander@chromium.org>
2016-05-31 13:38:47 -0700

    Disable stack_tool_* steps until rolling is unblocked
    Since rolling of chromium_revision in our DEPS is blocked, the
    stack_tool_* steps stopped working due to a flag change in tombstones.py.
    
    Disable the steps for now until rolling is unblocked.
    
    BUG=webrtc:5956
    TBR=phoglund@chromium.org
    
    Review-Url: https://codereview.chromium.org/2026883002

--- scripts/slave/recipe_modules/webrtc/api.py
+++ scripts/slave/recipe_modules/webrtc/api.py
@@ -172,7 +172,11 @@ class WebRTCApi(recipe_api.RecipeApi):
           test.run(self, suffix='')
 
         if self.m.chromium.c.TARGET_PLATFORM == 'android':
-          self.m.chromium_android.common_tests_final_steps()
+          # TODO(kjellander): Replace the three steps below with a call to
+          # common_tests_final_steps once webrtc:5956 is fixed.
+          self.m.chromium_android.shutdown_device_monitor()
+          self.m.chromium_android.logcat_dump(gs_bucket='chromium-android')
+          self.m.chromium_android.test_report()
 
     with self.m.step.defer_results():
       for test in tests:
