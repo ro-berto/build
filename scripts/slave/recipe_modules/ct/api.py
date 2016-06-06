ebed9e0cd754dda2d3394c338495476bb8646f2f
rmistry <rmistry@google.com>
2016-06-06 11:40:31 -0700

    Delete no longer used "Linux CT Top1K RR Perf" bot
    Context:
    This bot has not been running for about 2 months now. When it was running it did not generate any useful alerts.
    These files were originally added in https://codereview.chromium.org/1423993007/
    
    BUG=skia:4503
    
    Review-Url: https://codereview.chromium.org/2044543002

--- scripts/slave/recipe_modules/ct/api.py
+++ scripts/slave/recipe_modules/ct/api.py
@@ -12,17 +12,6 @@ class CTApi(recipe_api.RecipeApi):
 
   CT_GS_BUCKET = 'cluster-telemetry'
 
-  @property
-  def downloads_dir(self):
-    """Path to where artifacts should be downloaded from Google Storage."""
-    return self.m.path['slave_build'].join('src', 'content', 'test', 'ct')
-
-  def checkout_dependencies(self):
-    """Checks out all repositories required for CT to run."""
-    self.m.chromium.set_config('chromium')
-    self.m.gclient.set_config('chromium')
-    self.m.bot_update.ensure_checkout(force=True)
-
   def download_skps(self, page_type, slave_num, skps_chromium_build, dest_dir):
     """Downloads SKPs corresponding to the specified page type, slave and build.
 
@@ -42,50 +31,3 @@ class CTApi(recipe_api.RecipeApi):
     full_source = 'gs://%s/skps/%s/%s/slave%s' % (
         self.CT_GS_BUCKET, page_type, skps_chromium_build, slave_num)
     self.m.gsutil(['-m', 'rsync', '-d', '-r', full_source, skps_dir])
-
-  def download_CT_binary(self, ct_binary_name):
-    """Downloads the specified CT binary from GS into the downloads_dir."""
-    binary_dest = self.downloads_dir.join(ct_binary_name)
-    self.m.gsutil.download(
-        name="download %s" % ct_binary_name,
-        bucket=self.CT_GS_BUCKET,
-        source='swarming/binaries/%s' % ct_binary_name,
-        dest=binary_dest)
-    # Set executable bit on the binary.
-    self.m.python.inline(
-        name='Set executable bit on %s' % ct_binary_name,
-        program='''
-import os
-import stat
-
-os.chmod('%s', os.stat('%s').st_mode | stat.S_IEXEC)
-''' % (str(binary_dest), str(binary_dest))
-    )
-
-  def download_page_artifacts(self, page_type, slave_num):
-    """Downloads all the artifacts needed to run benchmarks on a page.
-
-    The artifacts are downloaded into subdirectories in the downloads_dir.
-
-    Args:
-      page_type: str. The CT page type. Eg: 1k, 10k.
-      slave_num: int. The number of the slave used to determine which GS
-                 directory to download from. Eg: for the top 1k, slave1 will
-                 contain webpages 1-10, slave2 will contain 11-20.
-    """
-    # Download page sets.
-    page_sets_dir = self.downloads_dir.join('slave%s' % slave_num, 'page_sets')
-    self.m.file.makedirs('page_sets dir', page_sets_dir)
-    self.m.gsutil.download(
-        bucket=self.CT_GS_BUCKET,
-        source='swarming/page_sets/%s/slave%s/*' % (page_type, slave_num),
-        dest=page_sets_dir)
-
-    # Download archives.
-    wpr_dir = page_sets_dir.join('data')
-    self.m.file.makedirs('WPR dir', wpr_dir)
-    self.m.gsutil.download(
-        bucket=self.CT_GS_BUCKET,
-        source='swarming/webpage_archives/%s/slave%s/*' % (page_type,
-                                                           slave_num),
-        dest=wpr_dir)
