86f129da1b411424a7241faaf846deca9bb3380c
machenbach <machenbach@chromium.org>
2016-06-20 05:19:39 -0700

    V8: Remove obsolete environment variables
    The target compiler for arm is controlled in v8's gyp files
    now.
    
    BUG=474921
    
    Committed: https://chromium.googlesource.com/chromium/tools/build/+/4e6b1681e8c0a5674c5d5f6ad3acd403dedba3e7
    Review-Url: https://codereview.chromium.org/2074403002

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -1073,7 +1073,6 @@ BUILDERS = {
       'V8 Arm - builder': {
         'chromium_apply_config': [
             'v8_ninja', 'default_compiler', 'goma', 'arm_hard_float'],
-        'v8_apply_config': ['arm_hard_float'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Release',
           'TARGET_ARCH': 'arm',
@@ -1091,7 +1090,6 @@ BUILDERS = {
       'V8 Arm - debug builder': {
         'chromium_apply_config': [
             'v8_ninja', 'default_compiler', 'goma', 'arm_hard_float'],
-        'v8_apply_config': ['arm_hard_float'],
         'v8_config_kwargs': {
           'BUILD_CONFIG': 'Debug',
           'TARGET_ARCH': 'arm',
