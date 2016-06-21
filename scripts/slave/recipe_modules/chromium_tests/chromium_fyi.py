464ef1c9ce6e3b0df296f3ada6507430e6192cf6
dcampb <dcampb@google.com>
2016-06-21 14:50:47 -0700

    Creating informational bots with wptserve (Mac and Windows) on Chromium_FYI this cl is closely related to cl https://codereview.chromium.org/2032903005 more files added for setting up of bots. BUG=620871
    Review-Url: https://codereview.chromium.org/2081173003

--- scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
@@ -188,6 +188,60 @@ SPEC = {
       'enable_swarming': True,
       'use_isolate': True,
     },
+    'WebKit Mac - WPTServe':{
+      'chromium_config': 'chromium',
+      'chromium_apply_config': ['mb','ninja_confirm_noop'],
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['blink_or_chromium'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Release',
+        'TARGET_BITS': 64,
+      },
+      'compile_targets': [
+        'blink_tests',
+      ],
+      'test_generators': [
+        steps.generate_gtest,
+        steps.generate_script,
+      ],
+      'tests': [
+        steps.BlinkTest(extra_args=[
+          '--enable-wptserve',
+        ]),
+      ],
+      'testing': {
+          'platform': 'mac',
+      },
+      'enable_swarming': True,
+      'use_isolate': True,
+    },
+    'WebKit Win - WPTServe':{
+      'chromium_config': 'chromium',
+      'chromium_apply_config': ['mb','ninja_confirm_noop'],
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['blink_or_chromium'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Release',
+        'TARGET_BITS': 64,
+      },
+      'compile_targets': [
+        'blink_tests',
+      ],
+      'test_generators': [
+        steps.generate_gtest,
+        steps.generate_script,
+      ],
+      'tests': [
+        steps.BlinkTest(extra_args=[
+          '--enable-wptserve',
+        ]),
+      ],
+      'testing': {
+          'platform': 'win',
+      },
+      'enable_swarming': True,
+      'use_isolate': True,
+    },
     'MD Top Chrome ChromeOS non-material': {
       'chromium_config': 'chromium',
       'chromium_apply_config': ['chromeos', 'mb'],
