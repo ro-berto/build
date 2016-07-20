4d30cb1a645ad31468a2af390bbbc9b550b391d5
hzl <hzl@google.com>
2016-07-20 14:59:11 -0700

    Add up and down arrows for ascend and descend sort.
    BUG=605572
    
    Review-Url: https://codereview.chromium.org/2160153002

--- scripts/slave/recipe_modules/chromium_android/resources/test_results_presentation.py
+++ scripts/slave/recipe_modules/chromium_android/resources/test_results_presentation.py
@@ -25,55 +25,113 @@ RESULT_DETAILS_TEMPLATE = """<!DOCTYPE html>
       }
     </style>
     <script type="text/javascript">
-      // Default is sort by test name.
-      var previousClickedColumn = 0;
-      var previousClickedIsAsc = true;
+      var previousClickedColumn;
+      var previousClickedIsAsc;
 
-      function sort_table(tbody, col, type) {
+      function sortByColumn(head, index) {
+        var tbody = head.parentNode.parentNode.nextElementSibling;
         var rows = tbody.rows,
             arr = new Array();
-        // fill the array with values from the table
+
+        // Fill the array with values from the table.
         for (var i = 0; i < rows.length; i++) {
-            var cells = rows[i].cells;
-            arr[i] = new Array();
-            for (var j = 0; j < cells.length; j++) {
-                arr[i][j] = cells[j].innerHTML;
-            }
+          var cells = rows[i].cells;
+          arr[i] = new Array();
+          for (var j = 0; j < cells.length; j++) {
+            arr[i][j] = cells[j].innerHTML;
+          }
         }
-        var asc = (col == previousClickedColumn) ? ((previousClickedIsAsc) ? -1 : 1) : 1; 
-        // sort the array by the specified column number (col) and order (asc)
+
+        // Determine whether to ascend or descend.
+        if (previousClickedColumn != undefined) {
+          var asc = (index == previousClickedColumn) ? ((previousClickedIsAsc) ? -1 : 1) : 1;
+        } else {
+          var asc = 1;
+        }
+
+        // Sort the array by the specified column number (col) and order (asc).
         arr.sort(function (a, b) {
-            if (type == "number") {
-                var avalue = Number(a[col]);
-                var bvalue = Number(b[col]);
-            } else if (type == "text") {
-                var avalue = a[col];
-                var bvalue = b[col];
-            }
-            return (avalue == bvalue) ? 0 : ((avalue > bvalue) ? asc : -1 * asc);
+          if (head.className == "number") {
+            var avalue = Number(a[index]);
+            var bvalue = Number(b[index]);
+          } else if (head.className == "text") {
+            var avalue = a[index];
+            var bvalue = b[index];
+          }
+          return (avalue == bvalue) ? 0 : ((avalue > bvalue) ? asc : -1 * asc);
         });
-        // replace existing rows with new rows created from the sorted array
+
+        // Replace existing rows with new rows created from the sorted array.
         for (var i = 0; i < rows.length; i++) {
-            rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
+          rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
         }
-        previousClickedColumn = col;
+
+        // Make previous arrow invisible.
+        if (previousClickedColumn != undefined) {
+          if (previousClickedIsAsc == 1) {
+            head.parentNode.getElementsByClassName("up")[previousClickedColumn].style.display = 'none';
+          } else {
+            head.parentNode.getElementsByClassName("down")[previousClickedColumn].style.display = 'none';
+          }
+        }
+
+        // Make current arrow visible.
+        if (asc == 1) {
+          head.getElementsByClassName("up")[0].style.display = "inline";
+        } else {
+          head.getElementsByClassName("down")[0].style.display = "inline";
+        }
+
+        previousClickedColumn = index;
         previousClickedIsAsc = (asc == 1) ? true : false;
       }
     </script>
   </head>
   <body>
-    <table style="width:100%%">
-      <tr>
-        <th onclick="sort_table(tests, 0, 'text');">test_name</th>
-        <th onclick="sort_table(tests, 1, 'text');">status</th>
-        <th onclick="sort_table(tests, 2, 'number');">elapsed_time_in_ms</th>
-        <th onclick="sort_table(tests, 3, 'text');">output_snippet</th>
-      </tr>
-      <tbody id="tests">
+    <table id="test_details" style="width:100%%">
+      <thead class="heads">
+        <tr>
+          <th class="text">
+              test_name
+              <span class="up" style="display:none;"> &#8593</span>
+              <span class="down" style="display:none;"> &#8595</span>
+          </th>
+
+          <th class="text">
+              status
+              <span class="up" style="display:none;"> &#8593</span>
+              <span class="down" style="display:none;"> &#8595</span>
+          </th>
+
+          <th class="number">
+              elapsed_time_in_ms
+              <span class="up" style="display:none;"> &#8593</span>
+              <span class="down" style="display:none;"> &#8595</span>
+          </th>
+
+          <th class="text">
+              output_snippet
+              <span class="up" style="display:none;"> &#8593</span>
+              <span class="down" style="display:none;"> &#8595</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="body">
       %s
       </tbody>
     </table>
   </body>
+  <script>
+    Array.prototype.slice.call(document.getElementsByTagName('th')).forEach(
+        function(head, index) {
+            head.addEventListener(
+                "click",
+                function() { sortByColumn(head, index); },
+                false
+            );
+        }
+    );
+  </script>
 </html>
 """
 
@@ -99,7 +157,6 @@ def result_details(json_path):
                 'duration': tr['elapsed_time_ms'],
                 'output_snippet' : tr['output_snippet']
             } for tr in test_runs])
-    results_list.sort(key=lambda x: x['name'])
     return results_to_html(results_list)
 
 def results_to_html(results):
