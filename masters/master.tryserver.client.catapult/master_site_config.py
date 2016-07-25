0d5107bcbc9dc11dcc105595ed74a6b260cabd94
sullivan <sullivan@chromium.org>
2016-07-25 08:27:45 -0700

    Add per-builder timeouts to catapult waterfall
    Timeouts are based on looking at current runtimes of the builders:
    Presubmit takes 15-40 seconds
    Android takes 10-15 minutes (but is more consistent since there are no browser tests)
    Linux and max take 8-15 minutes
    Windows bot takes 20-30 minutes
    
    BUG=516784
    
    Review-Url: https://codereview.chromium.org/2182493002

--- masters/master.tryserver.client.catapult/master_site_config.py
+++ masters/master.tryserver.client.catapult/master_site_config.py
@@ -1,4 +1,4 @@
-# Copyright 2014 The Chromium Authors. All rights reserved.
+# Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -16,7 +16,11 @@ class CatapultTryserver(Master.Master4a):
   project_name = 'CatapultTryserver'
   master_port = 21400
   slave_port = 31400
-  master_port_alt = 26400
+  master_port_alt = 41400
   buildbot_url = 'https://build.chromium.org/p/tryserver.client.catapult/'
   buildbucket_bucket = 'master.tryserver.client.catapult'
   service_account_file = 'service-account-chromium-tryserver.json'
+  # To enable outbound pubsub event streaming.
+  pubsub_service_account_file = None
+  pubsub_topic = None
+  name = 'tryserver.client.catapult'
