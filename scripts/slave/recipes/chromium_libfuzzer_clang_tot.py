e0d6ce3e5ab8c9c27ee2147334802a41502f7eb6
ochang <ochang@chromium.org>
2016-09-01 09:31:09 -0700

    Add ClangToTLinuxAsanLibfuzzer
    This bot builds a fuzzer with the pinned libFuzzer revision
    and ToT clang.
    
    BUG=635952
    
    Review-Url: https://codereview.chromium.org/2293913004

---
+++ scripts/slave/recipes/chromium_libfuzzer_clang_tot.py
@@ -0,0 +1,63 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import re
+from recipe_engine.types import freeze
+
+DEPS = [
+  'archive',
+  'depot_tools/bot_update',
+  'chromium',
+  'file',
+  'recipe_engine/json',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/raw_io',
+  'recipe_engine/step',
+]
+
+
+BUILDERS = freeze({
+  'chromium.fyi': {
+    'builders': {
+      'ClangToTLinuxAsanLibfuzzer': {
+        'chromium_config': 'chromium_clang',
+        'chromium_apply_config': [ 'proprietary_codecs' ],
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'linux',
+          'TARGET_BITS': 64,
+        },
+      },
+    },
+  },
+})
+
+
+def RunSteps(api):
+  mastername = api.m.properties['mastername']
+  buildername, bot_config = api.chromium.configure_bot(BUILDERS, ['mb'])
+
+  api.bot_update.ensure_checkout(
+      force=True, patch_root=bot_config.get('root_override'))
+
+  api.chromium.runhooks()
+  api.chromium.run_mb(mastername, buildername, use_goma=False)
+
+  api.chromium.compile(targets=['empty_fuzzer'])
+
+  config_kwargs = bot_config.get('chromium_config_kwargs', dict())
+  build_config = config_kwargs.get('BUILD_CONFIG', 'Release')
+  build_dir=api.path['slave_build'].join('src', 'out', build_config)
+
+  api.step('running empty_fuzzer', [api.path.join(build_dir, 'empty_fuzzer'),
+                                    '-runs=1'])
+
+
+def GenTests(api):
+  for test in api.chromium.gen_tests_for_builders(BUILDERS):
+    yield test
+
