1fe477abb23e4c451ca02baa25e85428f6a94ec5
phajdan.jr <phajdan.jr@chromium.org>
2016-09-08 02:37:08 -0700

    chromium.webkit: use chromium recipe in chromium/src
    This is similar to earlier https://codereview.chromium.org/2315313002
    
    BUG=584079
    
    Review-Url: https://codereview.chromium.org/2324593004

--- masters/master.chromium.webkit/master_linux_webkit_latest_cfg.py
+++ masters/master.chromium.webkit/master_linux_webkit_latest_cfg.py
@@ -2,7 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from buildbot.process.properties import WithProperties
+
 from master import master_config
+from master import master_utils
 from master.factory import remote_run_factory
 
 import master_site_config
@@ -16,13 +19,19 @@ B = helper.Builder
 F = helper.Factory
 T = helper.Triggerable
 
+revision_getter = master_utils.ConditionalProperty(
+    lambda build: build.getProperty('revision'),
+    WithProperties('%(revision)s'),
+    'master')
 
-def m_remote_run(recipe, **kwargs):
+def m_remote_run_chromium_src(recipe, **kwargs):
+  kwargs.setdefault('revision', revision_getter)
   return remote_run_factory.RemoteRunFactory(
       active_master=ActiveMaster,
-      repository='https://chromium.googlesource.com/chromium/tools/build.git',
+      repository='https://chromium.googlesource.com/chromium/src.git',
       recipe=recipe,
       factory_properties={'path_config': 'kitchen'},
+      use_gitiles=True,
       **kwargs)
 
 defaults['category'] = 'layout'
@@ -37,23 +46,23 @@ defaults['category'] = 'layout'
 #
 # FIXME: Rename this builder to indicate that it is running precise.
 B('WebKit Linux', 'f_webkit_linux_rel', scheduler='global_scheduler')
-F('f_webkit_linux_rel', m_remote_run('chromium'))
+F('f_webkit_linux_rel', m_remote_run_chromium_src('chromium'))
 
 B('WebKit Linux Trusty', 'f_webkit_linux_rel_trusty',
     scheduler='global_scheduler')
-F('f_webkit_linux_rel_trusty', m_remote_run('chromium'))
+F('f_webkit_linux_rel_trusty', m_remote_run_chromium_src('chromium'))
 
 B('WebKit Linux ASAN', 'f_webkit_linux_rel_asan', scheduler='global_scheduler',
     auto_reboot=True)
-F('f_webkit_linux_rel_asan', m_remote_run('chromium'))
+F('f_webkit_linux_rel_asan', m_remote_run_chromium_src('chromium'))
 
 B('WebKit Linux MSAN', 'f_webkit_linux_rel_msan', scheduler='global_scheduler',
     auto_reboot=True)
-F('f_webkit_linux_rel_msan', m_remote_run('chromium'))
+F('f_webkit_linux_rel_msan', m_remote_run_chromium_src('chromium'))
 
 B('WebKit Linux Leak', 'f_webkit_linux_leak_rel', scheduler='global_scheduler',
     category='layout')
-F('f_webkit_linux_leak_rel', m_remote_run('chromium'))
+F('f_webkit_linux_leak_rel', m_remote_run_chromium_src('chromium'))
 
 
 ################################################################################
@@ -66,7 +75,7 @@ F('f_webkit_linux_leak_rel', m_remote_run('chromium'))
 
 B('WebKit Linux (dbg)', 'f_webkit_dbg_tests', scheduler='global_scheduler',
     auto_reboot=True)
-F('f_webkit_dbg_tests', m_remote_run('chromium'))
+F('f_webkit_dbg_tests', m_remote_run_chromium_src('chromium'))
 
 
 def Update(_config, _active_master, c):
