7b383f2306154dcf37f8419c7d9c4bf4329b790e
iannucci <iannucci@chromium.org>
2016-09-08 15:59:31 -0700

    Revert of chromium.win: use chromium recipe in chromium/src (patchset #1 id:1 of https://codereview.chromium.org/2321943002/ )
    Reason for revert:
    This seems to work everywhere except for `Win7 Tests (1)` for some unknown reason.
    
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%281%29
    
    Reverting while trying to diagnose why that is.
    
    Original issue's description:
    > chromium.win: use chromium recipe in chromium/src
    >
    > This is similar to earlier https://codereview.chromium.org/2315313002
    >
    > BUG=584079
    >
    > Committed: https://chromium.googlesource.com/chromium/tools/build/+/a4f7f3a6ec84107e691d2924d761553abd298893
    
    TBR=emso@chromium.org,sergiyb@chromium.org,tandrii@chromium.org,phajdan.jr@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=584079
    
    Review-Url: https://codereview.chromium.org/2326873002

--- masters/master.chromium.win/master_win_cfg.py
+++ masters/master.chromium.win/master_win_cfg.py
@@ -2,31 +2,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from buildbot.process.properties import WithProperties
 from buildbot.scheduler import Triggerable
 from buildbot.schedulers.basic import SingleBranchScheduler
 
-from master import master_utils
 from master.factory import remote_run_factory
 
 import master_site_config
 ActiveMaster = master_site_config.ChromiumWin
 
 
-revision_getter = master_utils.ConditionalProperty(
-    lambda build: build.getProperty('revision'),
-    WithProperties('%(revision)s'),
-    'master')
-
-
-def m_remote_run_chromium_src(recipe, **kwargs):
-  kwargs.setdefault('revision', revision_getter)
+def m_remote_run(recipe, **kwargs):
   return remote_run_factory.RemoteRunFactory(
       active_master=ActiveMaster,
-      repository='https://chromium.googlesource.com/chromium/src.git',
+      repository='https://chromium.googlesource.com/chromium/tools/build.git',
       recipe=recipe,
       factory_properties={'path_config': 'kitchen'},
-      use_gitiles=True,
       **kwargs)
 
 
@@ -59,7 +49,7 @@ def Update(config, active_master, c):
   c['builders'].extend([
       {
         'name': spec['name'],
-        'factory': m_remote_run_chromium_src('chromium'),
+        'factory': m_remote_run('chromium'),
         'notify_on_missing': True,
         'category': '2windows',
       } for spec in specs
