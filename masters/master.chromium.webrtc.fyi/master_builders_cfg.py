6bd4fda23b42bcc12b44f69a39d3b3068755bddb
kjellander <kjellander@chromium.org>
2016-09-08 06:02:57 -0700

    Revert of chromium.webrtc.fyi: use chromium recipe in chromium/src (patchset #1 id:1 of https://codereview.chromium.org/2322933002/ )
    Reason for revert:
    Breaks Builders in this waterfall, see crbug.com/645083
    
    Original issue's description:
    > chromium.webrtc.fyi: use chromium recipe in chromium/src
    >
    > This is similar to earlier https://codereview.chromium.org/2315313002
    >
    > BUG=584079
    >
    > Committed: https://chromium.googlesource.com/chromium/tools/build/+/469d14ce35f71b4830b0ab6a692fa02e33b37c9f
    
    TBR=emso@chromium.org,sergiyb@chromium.org,tandrii@chromium.org,phajdan.jr@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=584079,645083
    
    Review-Url: https://codereview.chromium.org/2325643002

--- masters/master.chromium.webrtc.fyi/master_builders_cfg.py
+++ masters/master.chromium.webrtc.fyi/master_builders_cfg.py
@@ -3,31 +3,21 @@
 # found in the LICENSE file.
 
 from buildbot.changes.filter import ChangeFilter
-from buildbot.process.properties import WithProperties
 from buildbot.scheduler import Periodic
 from buildbot.schedulers.basic import SingleBranchScheduler
 
-from master import master_utils
 from master.factory import remote_run_factory
 
 import master_site_config
 ActiveMaster = master_site_config.ChromiumWebRTCFYI
 
 
-revision_getter = master_utils.ConditionalProperty(
-    lambda build: build.getProperty('revision'),
-    WithProperties('%(revision)s'),
-    'master')
-
-
-def m_remote_run_chromium_src(recipe, **kwargs):
-  kwargs.setdefault('revision', revision_getter)
+def m_remote_run(recipe, **kwargs):
   return remote_run_factory.RemoteRunFactory(
       active_master=ActiveMaster,
-      repository='https://chromium.googlesource.com/chromium/src.git',
+      repository='https://chromium.googlesource.com/chromium/tools/build.git',
       recipe=recipe,
       factory_properties={'path_config': 'kitchen'},
-      use_gitiles=True,
       **kwargs)
 
 
@@ -81,7 +71,7 @@ def Update(c):
   for spec in specs:
     builder_dict = {
       'name': spec['name'],
-      'factory': m_remote_run_chromium_src('chromium'),
+      'factory': m_remote_run('chromium'),
       'category': spec['category'],
       'notify_on_missing': True,
     }
