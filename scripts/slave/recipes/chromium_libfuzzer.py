a050d1bb1282da2e4fb25d322fc1fb657a1bda73
tikuta <tikuta@chromium.org>
2016-09-12 19:53:10 -0700

    Use [CACHE]/cipd/goma for goma_dir in recipes/chromium_libfuzzer.py
    * use chromium.ensure_goma()
    
    BUG=621828
    
    Review-Url: https://codereview.chromium.org/2331903002

--- scripts/slave/recipes/chromium_libfuzzer.py
+++ scripts/slave/recipes/chromium_libfuzzer.py
@@ -106,6 +106,7 @@ def RunSteps(api):
   checkout_results = api.bot_update.ensure_checkout(
       patch_root=bot_config.get('root_override'))
 
+  api.chromium.ensure_goma()
   api.chromium.runhooks()
   api.chromium.run_mb(mastername, buildername, use_goma=False)
 
@@ -174,4 +175,3 @@ def GenTests(api):
            api.step_data('calculate no_clusterfuzz',
                stdout=api.raw_io.output('target1'))
            )
-
