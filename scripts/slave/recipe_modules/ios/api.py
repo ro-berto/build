3a8bfe4caf559c1126a5761154c778dd7ff887de
smut <smut@google.com>
2016-10-24 13:25:45 -0700

    Support xctests
    BUG=608537
    
    Review-Url: https://codereview.chromium.org/2442813003

--- scripts/slave/recipe_modules/ios/api.py
+++ scripts/slave/recipe_modules/ios/api.py
@@ -481,6 +481,9 @@ class iOSApi(recipe_api.RecipeApi):
     cmd = [
       'src/ios/build/bots/scripts/run.py',
       '--app', '<(app_path)',
+      '--args-json', self.m.json.dumps({
+          'xctest': '<(xctest)',
+      }),
       '--out-dir', '${ISOLATED_OUTDIR}',
       '--xcode-version', '<(xcode_version)',
     ]
@@ -544,6 +547,7 @@ class iOSApi(recipe_api.RecipeApi):
           '--config-variable', 'app_path', app_path,
           '--config-variable', 'xcode_version', test.get(
             'xcode version', self.__config['xcode version']),
+          '--config-variable', 'xctest', test.get('xctest', False),
           '--isolate', isolate_template,
           '--isolated', tmp_dir.join('%s.isolated' % test['id']),
           '--path-variable', 'app_path', app_path,
