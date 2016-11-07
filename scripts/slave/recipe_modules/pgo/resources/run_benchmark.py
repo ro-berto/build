a91cfc13b4f5871ab77737cc174f959de3e7fbc8
sebmarchand <sebmarchand@chromium.org>
2016-11-07 14:18:21 -0800

    Reduce the profile data scale factor use during the PGO 32-bit builds.
    BUG=632864, 661902
    
    Review-Url: https://codereview.chromium.org/2482793004

--- scripts/slave/recipe_modules/pgo/resources/run_benchmark.py
+++ scripts/slave/recipe_modules/pgo/resources/run_benchmark.py
@@ -81,11 +81,14 @@ def main():
   env['PATH'] = str(os.pathsep.join([pgo_sweep_dir, options.build_dir,
                                      os.environ['PATH']]))
   env['PogoSafeMode'] = '1'
-  # Apply a scaling factor of 0.5 to the PGO profiling buffers for the 32-bit
+  # Apply a scaling factor of 0.25 to the PGO profiling buffers for the 32-bit
   # builds, without this the buffers will be too large and the process will
-  # fail to start. See crbug.com/632864#c22.
+  # fail to start, see crbug.com/632864#c22.
+  #
+  # This value was initially set to 0.5 and had to be lowered to 0.25, see
+  # crbug.com/661902#c8.
   if options.target_bits == 32:
-    env['VCPROFILE_ALLOC_SCALE'] = '0.5'
+    env['VCPROFILE_ALLOC_SCALE'] = '0.25'
 
   benchmark_command = [
       sys.executable,
