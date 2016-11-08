a42f4f913e28459c4406a1a4fe2e5438957e993a
ehmaldonado <ehmaldonado@chromium.org>
2016-11-08 03:48:56 -0800

    WebRTC: Re-enable rtc_stats_unittests on swarming.
    It has been working fine in the Win64 Debug FYI bot.
    
    R=kjellander@chromium.org
    BUG=497757
    
    Review-Url: https://codereview.chromium.org/2481743005

--- scripts/slave/recipe_modules/webrtc/steps.py
+++ scripts/slave/recipe_modules/webrtc/steps.py
@@ -10,14 +10,8 @@ def generate_tests(api, test_suite, revision, enable_swarming=False):
       # Skip rtc_unittests on swarming because it's flaky.
       # TODO(ehmaldonado): Get rid of this when http://bugs.webrtc.org/6500 is
       # fixed.
-      # Also skip rtc_stats_unittests on Windows64 Debug bots. It crashes with
-      # return code -1073741819.
-      # TODO(ehmaldonado): Get rid of this when http://crbug.com/660031 is
-      # fixed.
-      if ((test == 'rtc_unittests' and test_suite == 'desktop_swarming' and
-           api.mastername != 'client.webrtc.fyi') or
-          (test == 'rtc_stats_unittests' and
-           api.buildername in ('win_x64_dbg', 'Win64 Debug'))):
+      if (test == 'rtc_unittests' and test_suite == 'desktop_swarming' and
+          api.mastername != 'client.webrtc.fyi'):
         continue
       tests.append(WebRTCTest(test, enable_swarming=enable_swarming,
                               revision=revision, **extra_args))
@@ -27,10 +21,6 @@ def generate_tests(api, test_suite, revision, enable_swarming=False):
     if (test_suite == 'desktop_swarming' and
         api.mastername != 'client.webrtc.fyi'):
       tests.append(WebRTCTest('rtc_unittests', revision=revision))
-    # Execute rtc_status_unittests locally instead.
-    # TODO(ehmaldonado): Get rid of this when http://crbug.com/660031 is fixed.
-    if api.buildername in ('win_x64_dbg', 'Win64 Debug'):
-      tests.append(WebRTCTest('rtc_stats_unittests', revision=revision))
   elif test_suite == 'webrtc_baremetal':
     if api.m.platform.is_linux:
       f = api.m.path['checkout'].join
