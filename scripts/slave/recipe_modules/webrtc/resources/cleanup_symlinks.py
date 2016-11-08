11a160e371823f1c6bedca71a54069b40c810075
kjellander <kjellander@chromium.org>
2016-11-08 03:55:25 -0800

    WebRTC: Set the right path to clean_symlinks.py
    The path calculated in https://codereview.chromium.org/2484803004
    missed the fact that the checkout root was the dir that was passed
    to the script. Update the cleanup_symlinks.py script to set the
    right path and clarify that the directory argument to pass to it
    is expected to be the checkout root (not the path to the src/ dir).
    Also make the script not fail if the file is not found (a new bot don't have it, but
    still has the checkout root).
    
    BUG=663278
    TBR=ehmaldonado@chromium.org
    
    Review-Url: https://codereview.chromium.org/2482153002

--- scripts/slave/recipe_modules/webrtc/resources/cleanup_symlinks.py
+++ scripts/slave/recipe_modules/webrtc/resources/cleanup_symlinks.py
@@ -12,24 +12,21 @@ import sys
 
 def main(args):
   if len(args) != 1:
-    print >> sys.stderr, 'Please specify a single directory as an argument.'
+    print >> sys.stderr, ('Please specify a single directory as an argument, '
+                          'which points to the checkout root (dir above src/).'
     return 1
 
-  checkout_dir = args[0]
-  if not os.path.isdir(checkout_dir):
-    print 'Cannot find any directory at %s. Skipping cleaning.' % checkout_dir
+  checkout_root = args[0]
+  if not os.path.isdir(checkout_root):
+    print 'Cannot find any directory at %s. Skipping cleaning.' % checkout_root
     return 0
 
-  setup_links_file = os.path.join(checkout_dir, 'setup_links.py')
-  if not os.path.isfile(setup_links_file):
-    print 'Cannot find %s. Incomplete checkout?' % setup_links_file
-    return 1
-  else:
-    print 'Cleaning up symlinks in %s' % checkout_dir
+  setup_links_file = os.path.join(checkout_root, 'src', 'setup_links.py')
+  if os.path.isfile(setup_links_file):
+    print 'Cleaning up symlinks in %s' % checkout_root
     subprocess.check_call([sys.executable, setup_links_file, '--clean-only'],
-                          cwd=checkout_dir)
-    return 0
-
+                          cwd=checkout_root)
+  return 0
 
 
 if __name__ == '__main__':
