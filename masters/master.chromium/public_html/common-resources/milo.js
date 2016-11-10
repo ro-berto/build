48c11b45c0c209234d68833b33c5b81dd282e633
dnj <dnj@chromium.org>
2016-11-10 14:52:17 -0800

    Update waterfall templates to include Milo link.
    Milo is currently ingesting all of the build data for all of the
    waterfalls. Add a link to each waterfall with a parallel Milo view that
    clicks through to Milo.
    
    In order to do this, we add core "milo.js" and "milo.css" files to
    "chromium"'s "public_html" folders. We also augment "layout.html" to
    execute the "milo.js" injection function.
    
    Every waterfall with its own "layout.html" template has been updated to
    include the "milo.js" import and injection code.
    
    Every waterfall with its own "public_html" directory has had the Milo
    JavaScript, CSS, and image files symlinked to "chromium/" so they can
    access the resources referenced by their "layout.html".
    
    The injection function examines the current URL and decides if it maps
    to a view Milo supports, namely a builder or build view. If it does, a
    [LogDog] tab is injected into the header that links to the Milo
    equivalent view.
    
    The BuildBot base template is augmented to pass the master name to
    "milo.js". This is the actual name used by Milo, as opposed to the
    chromegw URL name which can occasionally vary. The modified template
    parameter is protected with the "|default" filter function so masters
    whose templates update but aren't restarted won't fail to render.
    
    BUG=None
    TEST=local
      - Started masters with affected "layout.html" pages locally, seem to
        be working!
    
    R=estaab@chromium.org, iannucci@chromium.org
    
    Review-Url: https://codereview.chromium.org/2491643003

---
+++ masters/master.chromium/public_html/common-resources/milo.js
@@ -0,0 +1,71 @@
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function MiloInject(root, mastername) {
+  // Get our ".header" <div> (in "layout.html").
+  var header = document.querySelector(".header");
+  if (! header) {
+    return;
+  }
+
+  // Do we have a Milo substitution available?
+  var path = window.location.pathname;
+  var desc = null;
+  var miloPath = path;
+  [
+    {
+      re: /^(?:\/[ip]\/([^\/]+))?\/builders\/([^\/]+)$/,
+      desc: "builder faster",
+      replacer: function(match, master, builder, offset, string) {
+        master = mastername || master;
+        if (! master) {
+          return string;
+        }
+        return ["buildbot", master, builder].join("/");
+      },
+    },
+    {
+      re: /^(?:\/[ip]\/([^\/]+))?\/builders\/([^\/]+)\/builds\/([^\/]+)\/$/,
+      desc: "build faster and forever",
+      replacer: function(match, master, builder, buildno, offset, string) {
+        master = mastername || master;
+        if (! master) {
+          return string;
+        }
+        return ["buildbot", master, builder, buildno].join("/");
+      },
+    },
+  ].every(function(e) {
+    miloPath = path.replace(e.re, e.replacer);
+    desc = e.desc;
+    return (miloPath === path);
+  });
+  if (miloPath === path) {
+    return;
+  }
+
+  // Build our milo elements.
+  miloPath = "https://luci-milo.appspot.com/" + miloPath;
+  var miloElement = document.createElement("span");
+  miloElement.className = "header-right-align";
+  miloElement.innerHTML += `
+    <div class="milo-container">
+      <a class="milo-link" href="${miloPath}">
+        <img class=".milo-logo"
+            src="${root}common-resources/chrome-infra-logo-32x32.png" /><!--
+            -->[LogDog]</a>
+      <div class="milo-info">
+        <div>View this ${desc} with LogDog / Milo!</div>
+        <p></p>
+        <div><a href="${miloPath}">Permalink</a></div>
+        <p></p>
+        <div class="milo-cit-info">
+          <img src="${root}common-resources/chrome-infra-logo-32x32.png"></img>
+          <span>Part of Chrome Infrastructure Team's LUCI project.</span>
+        </div>
+      </div>
+    </div>
+  `;
+  header.appendChild(miloElement);
+}
