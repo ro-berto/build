bfb253a39a7a0c48439da5953c5bb536401696b1
Dan Jacques <dnj@chromium.org>
2016-11-15 14:57:28 -0800

    chromium.linux: "tools/build" for recipes.
    BUG=chromium:584079
    TEST=None
    
    Change-Id: Ibbb96a354552504f61748e3419c8107dbe8598b9
    Reviewed-on: https://chromium-review.googlesource.com/411329
    Commit-Queue: Daniel Jacques <dnj@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>

--- masters/master.chromium.linux/master_linux_cfg.py
+++ masters/master.chromium.linux/master_linux_cfg.py
@@ -13,18 +13,12 @@ import master_site_config
 ActiveMaster = master_site_config.ChromiumLinux
 
 
-revision_getter = master_utils.ConditionalProperty(
-    lambda build: build.getProperty('revision'),
-    WithProperties('%(revision)s'),
-    'master')
-
-def m_remote_run_chromium_src(recipe, **kwargs):
+def m_remote_run(recipe, **kwargs):
   return remote_run_factory.RemoteRunFactory(
       active_master=ActiveMaster,
-      repository='https://chromium.googlesource.com/chromium/src.git',
+      repository='https://chromium.googlesource.com/chromium/tools/build.git',
       recipe=recipe,
       factory_properties={'path_config': 'kitchen'},
-      use_gitiles=True,
       **kwargs)
 
 
@@ -71,7 +65,7 @@ def Update(_config, active_master, c):
   c['builders'].extend([
       {
         'name': spec['name'],
-        'factory': m_remote_run_chromium_src('chromium'),
+        'factory': m_remote_run('chromium'),
         'notify_on_missing': True,
         'category': '4linux',
       } for spec in specs
