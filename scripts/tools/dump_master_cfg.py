953d4bca1510c987123b73c3a97d7ad12c1eae39
Dan Jacques <dnj@chromium.org>
2016-11-21 12:28:14 -0800

    Add the ability to dump all master configs as JSON
    BUG=None
    TEST=None
    
    Change-Id: I7d095b495d8ccfe080cf767df91dfe41a140cdb0
    Reviewed-on: https://chromium-review.googlesource.com/412938
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Daniel Jacques <dnj@chromium.org>

--- scripts/tools/dump_master_cfg.py
+++ scripts/tools/dump_master_cfg.py
@@ -13,6 +13,7 @@ in the same process. That's why this is a separate utility.
 import argparse
 import json
 import os
+import subprocess
 import sys
 
 SCRIPTS_DIR = os.path.abspath(
@@ -27,6 +28,8 @@ env.Install()
 from common import master_cfg_utils
 from master.factory.build_factory import BuildFactory
 
+SELF = sys.argv[0]
+
 
 class BuildbotJSONEncoder(json.JSONEncoder):
   def default(self, obj):  # pylint: disable=E0202
@@ -36,18 +39,75 @@ class BuildbotJSONEncoder(json.JSONEncoder):
     return repr(obj)
 
 
+def _dump_master((name, path)):
+  data = subprocess.check_output(
+      [sys.executable, SELF, path, '-'])
+  try:
+    return (name, json.loads(data))
+  except Exception as e:
+    return (name, e)
+
+
+def dump_all_masters(glob):
+  # Selective imports. We do this here b/c "dump_master_cfg" is part of
+  # a lot of production paths, and we don't want random import/pathing errors
+  # to break that.
+  import fnmatch
+  import multiprocessing
+
+  import config_bootstrap
+  from slave import bootstrap
+
+  # Homogenize master names: remove "master." from glob if present. We'll do the
+  # same with master names.
+  def strip_prefix(v, pfx):
+    if v.startswith(pfx):
+      v = v[len(pfx):]
+    return v
+  glob = strip_prefix(glob, 'master.')
+
+  bootstrap.ImportMasterConfigs(include_internal=True)
+  all_masters = {
+      strip_prefix(os.path.basename(mc.local_config_path), 'master.'): mc
+      for mc in config_bootstrap.Master.get_all_masters()}
+
+  pool = multiprocessing.Pool(multiprocessing.cpu_count())
+  m = dict(pool.map(_dump_master, (
+      (k, v.local_config_path) for k, v in sorted(all_masters.items())
+      if fnmatch.fnmatch(k, glob))))
+  pool.close()
+  pool.join()
+
+  for k, v in sorted(m.items()):
+    if isinstance(v, Exception):
+      print >>sys.stderr, 'Failed to load JSON from %s: %s' % (k, v)
+      m[k] = None
+
+  return m
+
+
 def main(argv):
   parser = argparse.ArgumentParser()
-  parser.add_argument('master')
+  parser.add_argument('-m', '--multi', action='store_true',
+      help='If specified, produce multi-master format and interpret the '
+           '"master" argument as a glob expression of masters to match.')
+  parser.add_argument('master',
+      help='The path of the master to dump. If "*" is provided, produce a '
+           'multi-master-format output list of all master configs.')
   parser.add_argument('output', type=argparse.FileType('w'), default=sys.stdout)
 
   args = parser.parse_args(argv)
 
-  result = master_cfg_utils.LoadConfig(args.master)
-  json.dump(result['BuildmasterConfig'],
+  if args.multi:
+    data = dump_all_masters(args.master)
+  else:
+    data = master_cfg_utils.LoadConfig(args.master)['BuildmasterConfig']
+
+  json.dump(data,
             args.output,
             cls=BuildbotJSONEncoder,
-            indent=4)
+            indent=4,
+            sort_keys=True)
   return 0
 
 
