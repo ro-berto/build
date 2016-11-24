348b7b282e6413be5210eb8877faccd57fff9050
Dan Jacques <dnj@chromium.org>
2016-11-23 20:58:02 -0800

    Update stable LogDog bootstrap revision.
    Update stable to: dd611fe7c2ee1214713f30c5a9dab8316242d23c
    
    TBR=agable@chromium.org
    BUG=chromium:667549
    TEST=canary
      - Canaried on "chromium.infra" waterfall, all architectures passed
        a round of green.
    
    Change-Id: Ia8ce1c880fd23312beb9c0e9dd841da05cef0c97
    Reviewed-on: https://chromium-review.googlesource.com/414185
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Commit-Queue: Daniel Jacques <dnj@chromium.org>

--- scripts/slave/logdog_bootstrap.py
+++ scripts/slave/logdog_bootstrap.py
@@ -31,7 +31,7 @@ class BootstrapError(Exception):
 
 
 # CIPD tag for LogDog Butler/Annotee to use.
-_STABLE_CIPD_TAG = 'git_revision:43e215ba861ba2130ffe74907d4d3c16e2791928'
+_STABLE_CIPD_TAG = 'git_revision:dd611fe7c2ee1214713f30c5a9dab8316242d23c'
 
 
 # Platform is the set of platform-specific LogDog bootstrapping
