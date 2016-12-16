c723bc339ad1e5fd00130c55f3a476708ca994a4
bpastene <bpastene@chromium.org>
2016-12-16 12:23:33 -0800

    Add a Nougat Tester to chromium.android.fyi
    BUG=663803
    
    Review-Url: https://codereview.chromium.org/2489853003

--- scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
@@ -175,5 +175,25 @@ SPEC = {
         'platform': 'linux',
       },
     },
+
+    'Nougat Phone Tester': {
+      'use_isolate': True,
+      'enable_swarming': True,
+      'chromium_config': 'android',
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['android'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Debug',
+        'TARGET_PLATFORM': 'android',
+      },
+      'parent_buildername': 'Android arm64 Builder (dbg)',
+      'parent_mastername': 'chromium.android',
+      'bot_type': 'tester',
+      'android_config': 'arm64_builder_mb',
+      'test_results_config': 'public_server',
+      'testing': {
+        'platform': 'linux',
+      },
+    },
   },
 }
