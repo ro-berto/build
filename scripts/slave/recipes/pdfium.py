61e29b63bd53227c31062f672e8d766ddbf6dcc6
Stephan Altmueller <stephana@google.com>
2017-01-13 08:30:21 -0500

    Adds Gold upload support to the pdfium recipe.
    BUG=skia:5973
    
    Change-Id: Ie416e5b52c4eab18507877ee06ba16e698e69238
    Reviewed-on: https://chromium-review.googlesource.com/424575
    Reviewed-by: Eric Boren <borenet@chromium.org>

--- scripts/slave/recipes/pdfium.py
+++ scripts/slave/recipes/pdfium.py
@@ -6,15 +6,31 @@ DEPS = [
   'depot_tools/bot_update',
   'depot_tools/depot_tools',
   'depot_tools/gclient',
+  'file',
   'goma',
+  'gsutil',
+  'recipe_engine/json',
   'recipe_engine/path',
   'recipe_engine/platform',
   'recipe_engine/properties',
   'recipe_engine/python',
+  'recipe_engine/shutil',
   'recipe_engine/step',
+  'recipe_engine/time',
 ]
 
 from recipe_engine.recipe_api import Property
+
+import calendar
+
+# DM_JSON is the file that gathers results for ingestion into Gold.
+DM_JSON = 'dm.json'
+
+# GS_BUCKET is the GS bucket to hold Gold Results.
+GS_BUCKET = 'skia-pdfium-gm'
+
+# Number of times the upload routine will retry to upload Gold results.
+UPLOAD_ATTEMPTS = 5
 
 PROPERTIES = {
   'skia': Property(default=False, kind=bool),
@@ -53,6 +69,8 @@ def _OutPath(memory_tool, skia, xfa, v8, clang, rel):
   return out_dir
 
 
+# _GNGenBuilds calls 'gn gen' and returns a dictionary of
+# the used build configuration to be used by Gold.
 def _GNGenBuilds(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel,
                  target_os, out_dir):
   api.goma.ensure_goma()
@@ -90,6 +108,8 @@ def _GNGenBuilds(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel,
               '--args=' + ' '.join(args)],
              cwd=checkout)
 
+  # convert the arguments to key values pairs for gold usage.
+  return gold_build_config(args)
 
 def _BuildSteps(api, clang, out_dir):
   # Build sample file using Ninja
@@ -103,8 +123,8 @@ def _BuildSteps(api, clang, out_dir):
       ninja_log_outdir=debug_path,
       ninja_log_compiler='clang' if clang else 'unknown')
 
-
-def _RunTests(api, memory_tool, v8, out_dir):
+# _RunTests runs the tests and uploads the results to Gold.
+def _RunTests(api, memory_tool, v8, out_dir, build_config):
   env = {}
   if memory_tool == 'asan':
     options = ['detect_leaks=1',
@@ -141,10 +161,19 @@ def _RunTests(api, memory_tool, v8, out_dir):
   api.python('pixel tests', pixel_tests_path, script_args,
              cwd=api.path['checkout'], env=env)
 
+  # Add the arguments needed to upload the resulting images.
+  gold_output_dir = api.path['checkout'].join('out', out_dir, 'gold_output')
+  gold_props, gold_key = get_gold_params(api, build_config)
+  script_args.extend([
+    '--gold_properties', gold_props,
+    '--gold_key', gold_key,
+    '--gold_output_dir', gold_output_dir,
+  ])
   corpus_tests_path = str(api.path['checkout'].join('testing', 'tools',
                                                     'run_corpus_tests.py'))
   api.python('corpus tests', corpus_tests_path, script_args,
              cwd=api.path['checkout'], env=env)
+  upload_dm_results(api, gold_output_dir)
 
 
 def RunSteps(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel, skip_test,
@@ -153,8 +182,8 @@ def RunSteps(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel, skip_test,
 
   out_dir = _OutPath(memory_tool, skia, xfa, v8, clang, rel)
 
-  _GNGenBuilds(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel,
-               target_os, out_dir)
+  build_config = _GNGenBuilds(api, memory_tool, skia, xfa, v8,
+                              target_cpu, clang, rel, target_os, out_dir)
 
   _BuildSteps(api, clang, out_dir)
 
@@ -162,8 +191,142 @@ def RunSteps(api, memory_tool, skia, xfa, v8, target_cpu, clang, rel, skip_test,
     return
 
   with api.step.defer_results():
-    _RunTests(api, memory_tool, v8, out_dir)
+    _RunTests(api, memory_tool, v8, out_dir, build_config)
 
+def get_gold_params(api, build_config):
+  """Get the parameters to be passed to the testing call to
+  generate the dm.json file expected by Gold and to upload
+  the generated images. Returns:
+      (properties_str, key_str)
+  These strings can be passed directly into run_corpus_tests.py.
+  """
+  builder_name = api.m.properties['buildername'].strip()
+  props = [
+    'gitHash', api.m.properties['revision'],
+    'master', api.m.properties['mastername'],
+    'builder', builder_name,
+    'build_number', api.m.properties['buildnumber']
+  ]
+
+  # Add the trybot information if this is a trybot run.
+  issue = str(api.m.properties.get('issue', ''))
+  patchset = str(api.m.properties.get('patchset', ''))
+  if api.m.properties.get('patch_storage', '') == 'gerrit':
+    issue = str(api.m.properties['patch_issue'])
+    patchset = str(api.m.properties['patch_set'])
+  if issue and patchset:
+    props.extend([
+      'issue', issue,
+      'patchset', patchset,
+      'patch_storage', api.m.properties.get('patch_storage', 'rietveld')
+    ])
+
+  # Add the os from the builder name to the set of unique identifers.
+  keys = build_config.copy()
+  keys["os"] = builder_name.split("_")[0]
+
+  return " ".join(props), dict_to_str(keys)
+
+def dict_to_str(props):
+  """Returns the given dictionary as a string of space
+     separated key/value pairs sorted by keys.
+  """
+  ret = []
+  for k in sorted(props.keys()):
+    ret += [k,props[k]]
+  return " ".join(ret)
+
+def gold_build_config(args):
+  """ Extracts key value pairs from the arguments handed to 'gn gen'
+      and returns them as a dictionary. Since these are used as
+      parameters in Gold we strip common prefixes and disregard
+      some arguments. i.e. 'use_goma' since we don't care about how
+      a binary was built.  Only arguments that follow the
+      'key=value' pattern are considered.
+  """
+  black_list = ["use_goma", "goma_dir", "use_sysroot", "is_component_build"]
+  strip_prefixes = ["is_", "pdf_enable_", "pdf_use_", "pdf_"]
+  build_config = {}
+  for arg in args:
+    # Catch multiple k/v pairs separated by spaces.
+    parts = arg.split()
+    for p in parts:
+      kv = [x.strip() for x in p.split("=")]
+      if len(kv) == 2:
+        k,v = kv
+        if k not in black_list:
+          for prefix in strip_prefixes:
+            if k.startswith(prefix):
+              k = k[len(prefix):]
+              break
+          build_config[k] = v
+  return build_config
+
+def upload_dm_results(api, results_dir):
+  """ Uploads results of the tests to Gold.
+  This assumes that results_dir contains a JSON file
+  and a set of PNGs.
+  Adapted from:
+  https://skia.googlesource.com/skia/+/master/infra/bots/recipes/upload_dm_results.py
+  """
+  builder_name = api.m.properties['buildername'].strip()
+  revision = api.m.properties['revision']
+
+  # Upload the images.
+  img_glob = results_dir.join("*.png")
+  files_to_upload = api.file.glob(
+      'find images',
+      img_glob,
+      test_data=['someimage.png'],
+      infra_step=True).get_result()
+
+  if len(files_to_upload) > 0:
+    gs_cp(api, 'images', img_glob, 'dm-images-v1')
+
+  # Upload the JSON summary and verbose.log.
+  now = api.time.utcnow()
+  summary_dest_path = '/'.join([
+      'dm-json-v1',
+      str(now.year ).zfill(4),
+      str(now.month).zfill(2),
+      str(now.day  ).zfill(2),
+      str(now.hour ).zfill(2),
+      revision,
+      builder_name,
+      str(int(calendar.timegm(now.utctimetuple())))])
+
+  # Trybot results are further siloed by issue/patchset.
+  issue = str(api.m.properties.get('issue', ''))
+  patchset = str(api.m.properties.get('patchset', ''))
+  if api.m.properties.get('patch_storage', '') == 'gerrit':
+    issue = str(api.m.properties['patch_issue'])
+    patchset = str(api.m.properties['patch_set'])
+  if issue and patchset:
+    summary_dest_path = '/'.join((
+        'trybot', summary_dest_path, issue, patchset))
+
+  summary_dest_path = '/'.join([summary_dest_path, DM_JSON])
+  local_dmjson = results_dir.join(DM_JSON)
+  gs_cp(api, 'JSON', local_dmjson, summary_dest_path, ['-z', 'json,log'])
+
+def gs_cp(api, name, src, dst, extra_args=None):
+  """
+  Copy the src to dst in Google storage.
+  """
+  name = 'upload %s' % name
+  for i in xrange(UPLOAD_ATTEMPTS):
+    try:
+      api.gsutil.upload(
+          name=name,
+          source=src,
+          bucket=GS_BUCKET,
+          dest=dst,
+          args=['-R'] + extra_args if extra_args else [],
+      )
+      break
+    except api.step.StepFailure: # pragma: no cover
+      if i == UPLOAD_ATTEMPTS - 1:
+        raise
 
 def GenTests(api):
   yield (
@@ -171,6 +334,8 @@ def GenTests(api):
       api.platform('win', 64) +
       api.properties(mastername="client.pdfium",
                      buildername='windows',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
   yield (
@@ -178,6 +343,8 @@ def GenTests(api):
       api.platform('linux', 64) +
       api.properties(mastername="client.pdfium",
                      buildername='linux',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
   yield (
@@ -185,6 +352,8 @@ def GenTests(api):
       api.platform('mac', 64) +
       api.properties(mastername="client.pdfium",
                      buildername='mac',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -194,6 +363,8 @@ def GenTests(api):
       api.properties(v8=False,
                      mastername="client.pdfium",
                      buildername='windows_no_v8',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
   yield (
@@ -202,6 +373,8 @@ def GenTests(api):
       api.properties(v8=False,
                      mastername="client.pdfium",
                      buildername='linux_no_v8',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
   yield (
@@ -210,6 +383,8 @@ def GenTests(api):
       api.properties(v8=False,
                      mastername="client.pdfium",
                      buildername='mac_no_v8',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -221,6 +396,8 @@ def GenTests(api):
                      skip_test=True,
                      mastername="client.pdfium",
                      buildername='windows_skia',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -231,6 +408,8 @@ def GenTests(api):
                      target_cpu='x86',
                      mastername="client.pdfium",
                      buildername='windows_xfa_32',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -240,6 +419,8 @@ def GenTests(api):
       api.properties(xfa=True,
                      mastername="client.pdfium",
                      buildername='windows_xfa',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -250,6 +431,8 @@ def GenTests(api):
                      rel=True,
                      mastername="client.pdfium",
                      buildername='windows_xfa_rel',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -261,6 +444,8 @@ def GenTests(api):
                      target_cpu='x86',
                      mastername="client.pdfium",
                      buildername='windows_xfa_clang_32',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -271,6 +456,8 @@ def GenTests(api):
                      clang=True,
                      mastername="client.pdfium",
                      buildername='windows_xfa_clang',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -282,6 +469,8 @@ def GenTests(api):
                      skip_test=True,
                      mastername="client.pdfium",
                      buildername='linux_skia',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -291,6 +480,8 @@ def GenTests(api):
       api.properties(xfa=True,
                      mastername="client.pdfium",
                      buildername='linux_xfa',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -301,6 +492,8 @@ def GenTests(api):
                      rel=True,
                      mastername="client.pdfium",
                      buildername='linux_xfa_rel',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -312,6 +505,8 @@ def GenTests(api):
                      skip_test=True,
                      mastername="client.pdfium",
                      buildername='mac_skia',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -321,6 +516,8 @@ def GenTests(api):
       api.properties(xfa=True,
                      mastername="client.pdfium",
                      buildername='mac_xfa',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -331,6 +528,8 @@ def GenTests(api):
                      rel=True,
                      mastername="client.pdfium",
                      buildername='mac_xfa_rel',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -340,6 +539,8 @@ def GenTests(api):
       api.properties(memory_tool='asan',
                      mastername="client.pdfium",
                      buildername='linux_asan_lsan',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -350,6 +551,8 @@ def GenTests(api):
                      memory_tool='asan',
                      mastername="client.pdfium",
                      buildername='linux_xfa_asan_lsan',
+                     revision='abc123',
+                     buildnumber='1234',
                      slavename="test_slave")
   )
 
@@ -359,7 +562,23 @@ def GenTests(api):
        api.properties.tryserver(xfa=True,
                                 memory_tool='asan',
                                 mastername='tryserver.client.pdfium',
+                                revision='abc123',
+                                buildnumber='1234',
                                 buildername='linux_xfa_asan_lsan')
+  )
+
+  yield (
+       api.test('try-linux-gerrit_xfa_asan_lsan') +
+       api.platform('linux', 64) +
+       api.properties.tryserver(xfa=True,
+                                memory_tool='asan',
+                                mastername='tryserver.client.pdfium',
+                                revision='abc123',
+                                buildnumber='1234',
+                                buildername='linux_xfa_asan_lsan',
+                                patch_issue='1234',
+                                patch_set=5,
+                                patch_storage='gerrit')
   )
 
   yield (
@@ -369,5 +588,7 @@ def GenTests(api):
                      buildername='android',
                      slavename='test_slave',
                      target_os='android',
+                     revision='abc123',
+                     buildnumber='1234',
                      skip_test=True)
   )
