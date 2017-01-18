1d19b1dfa232e1b0a12b16187f16078c54f81d10
Kenneth Russell <kbr@chromium.org>
2017-01-18 14:37:19 -0800

    Deploy experimental NVIDIA Retina bot. Remove old Intel bots.
    BUG=666077, 682432
    TBR=dpranke@chromium.org,zmo@chromium.org
    
    Change-Id: I1700c67285cf135106fd402e29a4a83aaaed3592
    Reviewed-on: https://chromium-review.googlesource.com/430191
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_gpu_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_gpu_fyi.py
@@ -175,40 +175,6 @@ SPEC = {
       # These new graphics cards are being tested at the moment.
       'enable_swarming': False,
     },
-    'Win10 Release (New Intel)': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['ninja_confirm_noop'],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 32,
-      },
-      'bot_type': 'tester',
-      'parent_buildername': 'GPU Win Builder',
-      'testing': {
-        'platform': 'win',
-      },
-      # Swarming is deliberately NOT enabled on this one-off configuration.
-      # This new hardware is being tested for reliability.
-      'enable_swarming': False,
-    },
-    'Win10 Debug (New Intel)': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['ninja_confirm_noop'],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 32,
-      },
-      'bot_type': 'tester',
-      'parent_buildername': 'GPU Win Builder (dbg)',
-      'testing': {
-        'platform': 'win',
-      },
-      # Swarming is deliberately NOT enabled on this one-off configuration.
-      # This new hardware is being tested for reliability.
-      'enable_swarming': False,
-    },
     'Win10 Release (Intel HD 530)': {
       'chromium_config': 'chromium',
       'chromium_apply_config': ['ninja_confirm_noop'],
@@ -482,42 +448,6 @@ SPEC = {
       'enable_swarming': True,
       'serialize_tests': True,
     },
-    'Linux Release (New Intel)': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb', 'ninja_confirm_noop'],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'tester',
-      'parent_buildername': 'GPU Linux Builder',
-      'testing': {
-        'platform': 'linux',
-      },
-      # Swarming is deliberately NOT enabled on this one-off configuration.
-      # Multiple copies of the machines have to be deployed into swarming
-      # in order to keep up with the faster cycle time of the tests.
-      'enable_swarming': False,
-    },
-    'Linux Debug (New Intel)': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb', 'ninja_confirm_noop'],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'tester',
-      'parent_buildername': 'GPU Linux Builder (dbg)',
-      'testing': {
-        'platform': 'linux',
-      },
-      # Swarming is deliberately NOT enabled on this one-off configuration.
-      # Multiple copies of the machines have to be deployed into swarming
-      # in order to keep up with the faster cycle time of the tests.
-      'enable_swarming': False,
-    },
     'Linux Release (Intel HD 530)': {
       'chromium_config': 'chromium',
       'chromium_apply_config': ['mb', 'ninja_confirm_noop'],
@@ -749,6 +679,24 @@ SPEC = {
       'serialize_tests': True,
     },
     'Mac 10.11 Retina Release (AMD)': {
+      'chromium_config': 'chromium',
+      'chromium_apply_config': ['ninja_confirm_noop'],
+      'gclient_config': 'chromium',
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Release',
+        'TARGET_BITS': 64,
+      },
+      'bot_type': 'tester',
+      'parent_buildername': 'GPU Mac Builder',
+      'testing': {
+        'platform': 'mac',
+      },
+      # Swarming is deliberately NOT enabled on this one-off configuration.
+      # Multiple copies of the machines have to be deployed into swarming
+      # in order to keep up with the faster cycle time of the tests.
+      'enable_swarming': False,
+    },
+    'Mac Experimental Retina Release (NVIDIA)': {
       'chromium_config': 'chromium',
       'chromium_apply_config': ['ninja_confirm_noop'],
       'gclient_config': 'chromium',
