c56cd10a7a3eb08b215b4c3158d7b358efb1f512
Ryan Macnak <rmacnak@google.com>
2017-02-01 09:57:17 -0800

    Dart: Also build the target 'runtime_kernel' on the kernel bots.
    Change-Id: If41a4cd92e703058bb875ed5297b60ad1297e5ae
    Reviewed-on: https://chromium-review.googlesource.com/435512
    Reviewed-by: William Hesse <whesse@chromium.org>
    Commit-Queue: Ryan Macnak <rmacnak@google.com>

--- scripts/slave/recipes/dart/dart_vm_kernel.py
+++ scripts/slave/recipes/dart/dart_vm_kernel.py
@@ -32,13 +32,14 @@ for platform in ['linux']:
       builders['vm-kernel-%s-%s-%s' % (platform, mode, arch)] = {
         'mode': mode,
         'target_arch': arch,
-        'build_args': [],
+        'build_args': ['runtime_kernel'],
         'test_args': ['-cdartk', '-rvm'] + general_test_args,
       }
       builders['vm-kernel-precomp-%s-%s-%s' % (platform, mode, arch)] = {
         'mode': mode,
         'target_arch': arch,
-        'build_args': (['dart_bootstrap',
+        'build_args': (['runtime_kernel',
+                        'dart_bootstrap',
                         'dart_precompiled_runtime']),
         'test_args': ['-cdartkp', '-rdart_precompiled'] + general_test_args,
         'archive_core_dumps': (platform == 'linux'),
