cdf12f8f1afbd634f702d65c24250db05c8e4c7f
Mike Case <mikecase@google.com>
2017-02-06 15:07:05 -0800

    Improve readability of test_results_presentation script.
    Change-Id: I1ee279f4d72bc2baf42b0e64b4dcb6ea421c689a
    Reviewed-on: https://chromium-review.googlesource.com/436488
    Reviewed-by: John Budorick <jbudorick@google.com>
    Commit-Queue: Michael Case <mikecase@chromium.org>

--- scripts/slave/recipe_modules/chromium_android/resources/test_results_presentation.py
+++ scripts/slave/recipe_modules/chromium_android/resources/test_results_presentation.py
@@ -21,103 +21,162 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape = True)
 
 
-def result_details(json_path, cs_base_url, master_name):
-  """Get result details from json path and then convert results to html."""
-  with open(json_path) as json_file:
-    json_object = json.loads(json_file.read())
-    results_list = []
-    if not 'per_iteration_data' in json_object:
-      return 'Error: json file missing per_iteration_data.'
-    results_dict = collections.defaultdict(list)
-    for testsuite_run in json_object['per_iteration_data']:
-      for test, test_runs in testsuite_run.iteritems():
-        results_dict[test].extend(test_runs)
-    return results_to_html(results_dict, cs_base_url, master_name)
+def cell(data, html_class='center'):
+  """Formats table cell data for processing in jinja template."""
+  return {
+    'data': data,
+    'class': html_class,
+  }
+
+
+def pre_cell(data, html_class='center'):
+  """Formats table <pre> cell data for processing in jinja template."""
+  return {
+    'cell_type': 'pre',
+    'data': data,
+    'class': html_class,
+  }
+
+
+class LinkTarget(object):
+  # Opens the linked document in a new window or tab.
+  NEW_TAB = '_blank'
+  # Opens the linked document in the same frame as it was clicked.
+  CURRENT_TAB = '_self'
+
+
+def link(data, href, target=LinkTarget.CURRENT_TAB):
+  """Formats <a> tag data for processing in jinja template.
+
+  Args:
+    data: String link appears as on HTML page.
+    href: URL where link goes.
+    target: Where link should be opened (e.g. current tab or new tab).
+  """
+  return {
+    'data': data,
+    'href': href,
+    'target': target,
+  }
+
+
+def links_cell(links, html_class='center', rowspan=None):
+  """Formats table cell with links for processing in jinja template.
+
+  Args:
+    links: List of link dictionaries. Use |link| function to generate them.
+    html_class: Class for table cell.
+    rowspan: Rowspan HTML attribute.
+  """
+  return {
+    'cell_type': 'links',
+    'class': html_class,
+    'links': links,
+    'rowspan': rowspan,
+  }
+
+
+def logs_cell(result):
+  """Formats result logs data for processing in jinja template."""
+  link_list = []
+  for name, href in result.get('links', {}).iteritems():
+    link_list.append(link(
+        data=name,
+        href=href,
+        target=LinkTarget.NEW_TAB))
+
+  if link_list:
+    return links_cell(links)
+  else:
+    return cell('(no logs)')
 
 
 def code_search(test, cs_base_url):
+  """Returns URL for test on codesearch."""
   search = test.replace('#', '.')
   return '%s/?q=%s&type=cs' % (cs_base_url, search)
 
 
 def status_class(status):
+  """Returns HTML class for test status."""
   status = status.lower()
   if status not in ('success', 'skipped'):
     return 'failure %s' % status
   return status
 
 
-def create_logs(result):
-  link_list = []
-  for name, link in result.get('links', {}).iteritems():
-    link_list.append({
-      'data': name,
-      'link': link,
-      'target': '_blank',
-    })
-
-  if link_list:
-    return {'link': link_list, 'class': 'center'}
-  else:
-    return {'data': '(no logs)', 'class': 'center'}
-
-
-def create_test_row_data(results_dict, cs_base_url):
+def create_test_table(results_dict, cs_base_url):
   """Format test data for injecting into HTML table."""
 
-  tests_list = []
+  header_row = [
+    cell(data='test_name', html_class='text'),
+    cell(data='status', html_class='flaky'),
+    cell(data='elapsed_time_ms', html_class='number'),
+    cell(data='logs', html_class='text'),
+    cell(data='output_snippet', html_class='text'),
+  ]
+
+  test_row_blocks = []
   for test_name, test_results in results_dict.iteritems():
     test_runs = []
     for index, result in enumerate(test_results):
       if index == 0:
-        test_run = [
-            {'link': [{'data': test_name,
-                       'link': code_search(test_name, cs_base_url),
-                       'target': '_blank'}],
-             'rowspan': len(test_results),
-             'first_row_of_the_block': True,
-             'class': 'left ' + test_name,
-            }]
+        test_run = [links_cell(
+            links=[
+                link(href=code_search(test_name, cs_base_url),
+                     target=LinkTarget.NEW_TAB,
+                     data=test_name)],
+            rowspan=len(test_results),
+            html_class='left %s' % test_name
+        )]                                        # test_name
       else:
         test_run = []
-      # class: The class of html element.
-      # link: href link of the element.
-      # target: The page to open link (new tab or same page).
+
       test_run.extend([
-          {'data': result['status'],
-           'class': 'center ' + status_class(result['status'])},
-          {'data': result['elapsed_time_ms'], 'class': 'center'},
-          create_logs(result),
-          {'data': result['output_snippet'],
-           'class': 'left', 'is_pre': True}
-          ])
+          cell(data=result['status'],             # status
+               html_class=('center %s' %
+                  status_class(result['status']))),
+          cell(data=result['elapsed_time_ms']),   # elapsed_time_ms
+          logs_cell(result),                      # logs
+          pre_cell(data=result['output_snippet'], # output_snippet
+                   html_class='left'),
+      ])
       test_runs.append(test_run)
-    tests_list.append(test_runs)
-  return tests_list
+    test_row_blocks.append(test_runs)
+  return header_row, test_row_blocks
 
 
-def create_suite_row_data(results_dict):
+def create_suite_table(results_dict):
   """Format test suite data for injecting into HTML table."""
 
-  # class: The class of html element.
-  # link: href link of the element.
-  # target: The page to open link (new tab or same page).
-  suites_summary = [{'link': [{'data': 'TOTAL',
-                               'link': ('?suite=%s' % 'TOTAL'),
-                               'target': '_self'}],
-                     'class': 'center'},
-                    {'data': 0, 'class': 'center'},
-                    {'data': 0, 'class': 'center'},
-                    {'data': 0, 'class': 'center'},
-                    {'data': 0, 'class': 'center'}]
+  SUCCESS_COUNT_INDEX = 1
+  FAIL_COUNT_INDEX = 2
+  ALL_COUNT_INDEX = 3
+  TIME_INDEX = 4
+
+  header_row = [
+    cell(data='suite_name', html_class='text'),
+    cell(data='number_success_tests', html_class='number'),
+    cell(data='number_fail_tests', html_class='number'),
+    cell(data='all_tests', html_class='number'),
+    cell(data='elapsed_time_ms', html_class='number'),
+  ]
+
+  footer_row = [
+    links_cell(
+        links=[
+            link(href=('?suite=%s' % 'TOTAL'),
+                 target=LinkTarget.CURRENT_TAB,
+                 data='TOTAL')
+        ],
+    ),             # suite_name
+    cell(data=0),  # number_success_tests
+    cell(data=0),  # number_fail_tests
+    cell(data=0),  # all_tests
+    cell(data=0),  # elapsed_time_ms
+  ]
 
   suite_row_dict = {}
-  # 'suite_row' is [name, success_count, fail_count, all_count, time].
-  SUCCESS_COUNT = 1
-  FAIL_COUNT = 2
-  ALL_COUNT = 3
-  TIME = 4
-
   for test_name, test_results in results_dict.iteritems():
     # TODO(mikecase): This logic doesn't work if there are multiple test runs.
     # That is, if 'per_iteration_data' has multiple entries.
@@ -129,66 +188,69 @@ def create_suite_row_data(results_dict):
     if suite_name in suite_row_dict:
       suite_row = suite_row_dict[suite_name]
     else:
-      suite_row = [{'link': [{'data': suite_name,
-                             'link': ('?suite=%s' % suite_name),
-                             'target': '_self'}],
-                    'class': 'left'},
-                   {'data': 0, 'class': 'center'},
-                   {'data': 0, 'class': 'center'},
-                   {'data': 0, 'class': 'center'},
-                   {'data': 0, 'class': 'center'}]
+      suite_row = [
+        links_cell(
+            links=[
+                link(href=('?suite=%s' % suite_name),
+                     target=LinkTarget.CURRENT_TAB,
+                     data=suite_name)],
+            html_class='left'
+        ),             # suite_name
+        cell(data=0),  # number_success_tests
+        cell(data=0),  # number_fail_tests
+        cell(data=0),  # all_tests
+        cell(data=0),  # elapsed_time_ms
+      ]
+
     suite_row_dict[suite_name] = suite_row
 
-    suite_row[ALL_COUNT]['data'] += 1
-    suites_summary[ALL_COUNT]['data'] += 1
+    suite_row[ALL_COUNT_INDEX]['data'] += 1
+    footer_row[ALL_COUNT_INDEX]['data'] += 1
+
     if result['status'] == 'SUCCESS':
-      suite_row[SUCCESS_COUNT]['data'] += 1
-      suites_summary[SUCCESS_COUNT]['data'] += 1
+      suite_row[SUCCESS_COUNT_INDEX]['data'] += 1
+      footer_row[SUCCESS_COUNT_INDEX]['data'] += 1
     elif result['status'] != 'SKIPPED':
-      suite_row[FAIL_COUNT]['data'] += 1
-      suites_summary[FAIL_COUNT]['data'] += 1
-    suite_row[TIME]['data'] += result['elapsed_time_ms']
-    suites_summary[TIME]['data'] += result['elapsed_time_ms']
+      suite_row[FAIL_COUNT_INDEX]['data'] += 1
+      footer_row[FAIL_COUNT_INDEX]['data'] += 1
+
+    suite_row[TIME_INDEX]['data'] += result['elapsed_time_ms']
+    footer_row[TIME_INDEX]['data'] += result['elapsed_time_ms']
 
   for suite in suite_row_dict.values():
-    if suite[FAIL_COUNT]['data'] > 0:
-      suite[FAIL_COUNT]['class'] += ' failure'
+    if suite[FAIL_COUNT_INDEX]['data'] > 0:
+      suite[FAIL_COUNT_INDEX]['class'] += ' failure'
     else:
-      suite[FAIL_COUNT]['class'] += ' success'
-  if suites_summary[FAIL_COUNT]['data'] > 0:
-    suites_summary[FAIL_COUNT]['class'] += ' failure'
+      suite[FAIL_COUNT_INDEX]['class'] += ' success'
+
+  if footer_row[FAIL_COUNT_INDEX]['data'] > 0:
+    footer_row[FAIL_COUNT_INDEX]['class'] += ' failure'
   else:
-    suites_summary[FAIL_COUNT]['class'] += ' success'
-  return [[suite_row] for suite_row in suite_row_dict.values()], suites_summary
+    footer_row[FAIL_COUNT_INDEX]['class'] += ' success'
+
+  return (header_row,
+          [[suite_row] for suite_row in suite_row_dict.values()],
+          footer_row)
 
 
 def results_to_html(results_dict, cs_base_url, master_name):
   """Convert list of test results into html format."""
 
-  test_row_blocks = create_test_row_data(results_dict, cs_base_url)
-  suite_row_blocks, suites_summary = create_suite_row_data(results_dict)
-
-  test_table_values = {
-    'table_id' : 'test-table',
-    'table_headers' : [('text', 'test_name'),
-                       ('flaky', 'status'),
-                       ('number', 'elapsed_time_ms'),
-                       ('text', 'logs'),
-                       ('text', 'output_snippet'),
-                      ],
-    'table_row_blocks' : test_row_blocks,
-  }
+  test_rows_header, test_rows = create_test_table(results_dict, cs_base_url)
+  suite_rows_header, suite_rows, suite_row_footer = create_suite_table(
+      results_dict)
 
   suite_table_values = {
-    'table_id' : 'suite-table',
-    'table_headers' : [('text', 'suite_name'),
-                       ('number', 'number_success_tests'),
-                       ('number', 'number_fail_tests'),
-                       ('number', 'all_tests'),
-                       ('number', 'elapsed_time_ms'),
-                      ],
-    'table_row_blocks' : suite_row_blocks,
-    'summary' : suites_summary,
+    'table_id': 'suite-table',
+    'table_headers': suite_rows_header,
+    'table_row_blocks': suite_rows,
+    'table_footer': suite_row_footer,
+  }
+
+  test_table_values = {
+    'table_id': 'test-table',
+    'table_headers': test_rows_header,
+    'table_row_blocks': test_rows,
   }
 
   main_template = JINJA_ENVIRONMENT.get_template(
@@ -196,6 +258,23 @@ def results_to_html(results_dict, cs_base_url, master_name):
   return main_template.render(
       {'tb_values': [suite_table_values, test_table_values],
        'master_name': master_name})
+
+
+def result_details(json_path, cs_base_url, master_name):
+  """Get result details from json path and then convert results to html."""
+
+  with open(json_path) as json_file:
+    json_object = json.loads(json_file.read())
+  results_list = []
+
+  if not 'per_iteration_data' in json_object:
+    return 'Error: json file missing per_iteration_data.'
+
+  results_dict = collections.defaultdict(list)
+  for testsuite_run in json_object['per_iteration_data']:
+    for test, test_runs in testsuite_run.iteritems():
+      results_dict[test].extend(test_runs)
+  return results_to_html(results_dict, cs_base_url, master_name)
 
 
 def main():
