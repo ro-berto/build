294dee1c042da6e1dd7207dfb14f544efcb2f25a
smut <smut@google.com>
2017-02-16 17:31:55 -0800

    Do not try to show view_url when isolated_out is None
    BUG=693131
    
    Change-Id: Ice571e8f899a94588addebd48b38f372fd568a34
    Reviewed-on: https://chromium-review.googlesource.com/444176
    Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>

--- scripts/slave/recipe_modules/ios/api.py
+++ scripts/slave/recipe_modules/ios/api.py
@@ -702,9 +702,10 @@ class iOSApi(recipe_api.RecipeApi):
         exit_code = (swarming_summary.get('exit_codes') or [None])[0]
 
         # Link to isolate file browser for files emitted by the test.
-        if swarming_summary.get('isolated_out', {}).get('view_url'):
-          step_result.presentation.links['test data'] = (
-              swarming_summary['isolated_out']['view_url'])
+        if swarming_summary.get('isolated_out'):
+          if swarming_summary['isolated_out'].get('view_url'):
+            step_result.presentation.links['test data'] = (
+                swarming_summary['isolated_out']['view_url'])
 
         # Interpret the result and set the display appropriately.
         if state == self.m.swarming.State.COMPLETED and exit_code is not None:
