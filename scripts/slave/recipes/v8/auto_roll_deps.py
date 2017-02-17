1ea29fcf4222b8f31d28036a703e1346f0fd8c73
Michael Achenbach <machenbach@chromium.org>
2017-02-17 12:57:31 +0100

    V8: Update reviewers on V8 rolls
    TBR=hablich@chromium.org,tandrii@chromium.org,kozyatinskiy@chromium.org
    
    Change-Id: I7618d74cbba9f468efd27a033997101bb3a44a64
    Reviewed-on: https://chromium-review.googlesource.com/444645
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

--- scripts/slave/recipes/v8/auto_roll_deps.py
+++ scripts/slave/recipes/v8/auto_roll_deps.py
@@ -157,7 +157,7 @@ def RunSteps(api):
            '--author', 'v8-autoroll@chromium.org',
            '--reviewer',
            'hablich@chromium.org,machenbach@chromium.org,'
-           'littledan@chromium.org,vogelheim@chromium.org',
+           'kozyatinskiy@chromium.org,vogelheim@chromium.org',
            '--roll',
            '--json-output', api.json.output(),
            '--work-dir', api.path['start_dir'].join('workdir')],
