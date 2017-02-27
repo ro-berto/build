2c5b6c1efd6c9cc8fc455738e3b43f6e2631ac48
Dan Jacques <dnj@chromium.org>
2017-02-27 10:44:07 -0800

    Add remote_run test, canarying, pin recipes
    Add canarying to "remote_run". Pin the "recipes-py" package
    version so that arbitrary updates don't lose the basic ability to launch
    recipes.
    
    Additionally, add a much-needed suite of "remote_run" tests that,
    similar to "annotated_run", assert the correctness of basic execution
    and expected command-line flags.
    
    Some refactoring was needed to accommodate the unit test approach and
    canarying. Some additional refactoring is probably present due to the
    fact that this was backported from its successor patch, which introduces
    Kitchen. The expected output behavior of this patch is that "remote_run"
    behavior remains unchanged, except for the pins.
    
    BUG=chromium:694911
    TEST=local
    
    Change-Id: I7197aa4cbe0b3578933b6f53371e9d85535fabe7
    Reviewed-on: https://chromium-review.googlesource.com/446904
    Commit-Queue: Daniel Jacques <dnj@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Dave Sansome <dsansome@chromium.org>

--- scripts/slave/remote_run.py
+++ scripts/slave/remote_run.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 import argparse
+import collections
 import copy
 import json
 import logging
@@ -22,6 +23,7 @@ from common import annotator
 from common import chromium_utils
 from common import env
 from slave import cipd
+from slave import infra_platform
 from slave import logdog_bootstrap
 from slave import monitoring_utils
 from slave import robust_tempdir
@@ -30,27 +32,38 @@ from slave import update_scripts
 
 LOGGER = logging.getLogger('remote_run')
 
+# Masters in this list will use the canary path.
+_CANARY_MASTERS = set((
+  'chromium.infra',
+  'chromium.infra.cron',
+))
 
 # The name of the recipe engine CIPD package.
 _RECIPES_PY_CIPD_PACKAGE = 'infra/recipes-py'
 
-# CIPD_PINS is a mapping of master name to pinned recipe engine CIPD package
-# version. If no pin is found, the CIPD pin for "None" will be used.
-_CIPD_PINS = {
-  # None is the default CIPD pin, and is used if nothing else matches. It MUST
-  # be defined.
-  None: 'latest',
-}
+# _CIPD_PINS is a mapping of master name to pinned CIPD package version to use
+# for that master.
+CipdPins = collections.namedtuple('CipdPins', ('recipes'))
 
-def _get_cipd_pin(mastername):
-  return _CIPD_PINS.get(mastername, _CIPD_PINS[None])
+# Stable CIPD pin set.
+_STABLE_CIPD_PINS = CipdPins(
+      recipes='git_revision:f9018a9957d36e4149083cb3a6a0fdca619c9706')
+
+# Canary CIPD pin set.
+_CANARY_CIPD_PINS = CipdPins(
+      recipes='git_revision:f9018a9957d36e4149083cb3a6a0fdca619c9706')
+
+
+def _get_cipd_pins(mastername):
+  return (_CANARY_CIPD_PINS if mastername in _CANARY_MASTERS else
+          _STABLE_CIPD_PINS)
 
 
 def all_cipd_packages():
   """Generator which yields all referenced CIPD packages."""
   # All CIPD packages are in top-level platform config.
-  for pin in _CIPD_PINS.values():
-    yield cipd.CipdPackage(name=_RECIPES_PY_CIPD_PACKAGE, version=pin)
+  for pins in (_STABLE_CIPD_PINS, _CANARY_CIPD_PINS):
+    yield cipd.CipdPackage(name=_RECIPES_PY_CIPD_PACKAGE, version=pins.recipes)
 
 
 # ENGINE_FLAGS is a mapping of master name to a engine flags. This can be used
@@ -94,18 +107,162 @@ def _install_cipd_packages(path, *packages):
     path (str): The CIPD installation root.
     packages (list of CipdPackage): The set of CIPD packages to install.
   """
-  cmd = [
-      sys.executable,
-      os.path.join(env.Build, 'scripts', 'slave', 'cipd.py'),
-      '--dest-directory', path,
-      '-vv' if logging.getLogger().level == logging.DEBUG else '-v',
-  ]
-  for p in packages:
-    cmd += ['-P', '%s@%s' % (p.name, p.version)]
+  # Build our CIPD manifest. We'll pass it via STDIN.
+  manifest = '\n'.join('%s %s' % (pkg.name, pkg.version) for pkg in packages)
 
-  exit_code = _call(cmd)
-  if exit_code != 0:
-    raise Exception('Failed to install CIPD packages.')
+  cmd = [
+      'cipd' + infra_platform.exe_suffix(), # TODO(dnj): Is the suffix needed?
+      'ensure',
+      '-ensure-file', '-',
+      '-root', path,
+  ]
+
+  LOGGER.info('Executing CIPD command: %s\nManifest:\n%s', cmd, manifest)
+  proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT)
+  stdout, _ = proc.communicate(input=manifest)
+  if proc.returncode != 0:
+    LOGGER.error('CIPD exited with non-zero return code (%s):\n%s',
+        proc.returncode, stdout)
+    raise ValueError('Failed to install CIPD packages (%d)' % (
+        proc.returncode,))
+
+
+def _exec_recipe(args, rt, stream, basedir):
+  tempdir = rt.tempdir(basedir)
+  LOGGER.info('Using temporary directory: [%s].', tempdir)
+
+  build_data_dir = rt.tempdir(basedir)
+  LOGGER.info('Using build data directory: [%s].', build_data_dir)
+
+  # Construct our properties.
+  properties = copy.copy(args.factory_properties)
+  properties.update(args.build_properties)
+  properties['build_data_dir'] = build_data_dir
+  properties['builder_id'] = 'master.%s:%s' % (
+    properties['mastername'], properties['buildername'])
+
+  # path_config property defines what paths a build uses for checkout, git
+  # cache, goma cache, etc.
+  # Unless it is explicitly specified by a builder, use paths for buildbot
+  # environment.
+  properties['path_config'] = properties.get('path_config', 'buildbot')
+  properties['bot_id'] = properties['slavename']
+  LOGGER.info('Using properties: %r', properties)
+
+  monitoring_utils.write_build_monitoring_event(build_data_dir, properties)
+
+  mastername = properties.get('mastername')
+
+  # Ensure that the CIPD client is installed and available on PATH.
+  from slave import cipd_bootstrap_v2
+  cipd_bootstrap_v2.high_level_ensure_cipd_client(
+    basedir, properties.get(mastername))
+
+  # Make switching to remote_run easier: we do not use buildbot workdir,
+  # and it takes disk space leading to out of disk errors.
+  buildbot_workdir = properties.get('workdir')
+  if buildbot_workdir:
+    try:
+      if os.path.exists(buildbot_workdir):
+        buildbot_workdir = os.path.realpath(buildbot_workdir)
+        cwd = os.path.realpath(os.getcwd())
+        if cwd.startswith(buildbot_workdir):
+          buildbot_workdir = cwd
+
+        LOGGER.info('Cleaning up buildbot workdir %r', buildbot_workdir)
+
+        # Buildbot workdir is usually used as current working directory,
+        # so do not remove it, but delete all of the contents. Deleting
+        # current working directory of a running process may cause
+        # confusing errors.
+        for p in (os.path.join(buildbot_workdir, x)
+                  for x in os.listdir(buildbot_workdir)):
+          LOGGER.info('Deleting %r', p)
+          chromium_utils.RemovePath(p)
+    except Exception as e:
+      # It's preferred that we keep going rather than fail the build
+      # on optional cleanup.
+      LOGGER.exception('Buildbot workdir cleanup failed: %s', e)
+
+  pins = _get_cipd_pins(mastername)
+
+  properties_file = os.path.join(tempdir, 'remote_run_properties.json')
+  with open(properties_file, 'w') as f:
+    json.dump(properties, f)
+
+  cipd_path = os.path.join(basedir, '.remote_run_cipd')
+
+  _install_cipd_packages(cipd_path,
+      cipd.CipdPackage(_RECIPES_PY_CIPD_PACKAGE, pins.recipes))
+
+  engine_flags = _get_engine_flags(mastername)
+
+  engine_args = []
+  if engine_flags:
+    engine_flags_path = os.path.join(tempdir, 'engine_flags.json')
+    with open(engine_flags_path, 'w') as f:
+      json.dump(engine_flags, f)
+
+    engine_args = ['--operational-args-path', engine_flags_path]
+
+  recipe_result_path = os.path.join(tempdir, 'recipe_result.json')
+  recipe_cmd = [
+      sys.executable,
+      os.path.join(cipd_path, 'recipes.py'),] + engine_args + [
+      '--verbose',
+      'remote',
+      '--repository', args.repository,
+      '--workdir', os.path.join(tempdir, 'rw'),
+  ]
+  if args.revision:
+    recipe_cmd.extend(['--revision', args.revision])
+  if args.use_gitiles:
+    recipe_cmd.append('--use-gitiles')
+  recipe_cmd.extend([
+      '--',] + (
+      engine_args) + [
+      '--verbose',
+      'run',
+      '--properties-file', properties_file,
+      '--workdir', os.path.join(tempdir, 'w'),
+      '--output-result-json', recipe_result_path,
+      properties.get('recipe') or args.recipe,
+  ])
+  # If we bootstrap through logdog, the recipe command line gets written
+  # to a temporary file and does not appear in the log.
+  LOGGER.info('Recipe command line: %r', recipe_cmd)
+
+  # Default to return code != 0 is for the benefit of buildbot, which uses
+  # return code to decide if a step failed or not.
+  recipe_return_code = 1
+  try:
+    bs = logdog_bootstrap.bootstrap(rt, args, basedir, tempdir, properties,
+                                    recipe_cmd)
+
+    LOGGER.info('Bootstrapping through LogDog: %s', bs.cmd)
+    bs.annotate(stream)
+    _ = _call(bs.cmd)
+    recipe_return_code = bs.get_result()
+  except logdog_bootstrap.NotBootstrapped as e:
+    LOGGER.info('Not using LogDog. Invoking `recipes.py` directly.')
+    recipe_return_code = _call(recipe_cmd)
+
+  # Try to open recipe result JSON. Any failure will result in an exception
+  # and an infra failure.
+  with open(recipe_result_path) as f:
+    return_value = json.load(f)
+
+  if engine_flags.get('use_result_proto'):
+    # If we failed, but aren't a step failure, we assume it was an
+    # exception.
+    f = return_value.get('failure')
+    if f is not None and not f.get('step_failure'):
+      # The recipe engine used to return -1, which got interpreted as 255
+      # by os.exit in python, since process exit codes are a single byte.
+      recipe_return_code = 255
+
+  return recipe_return_code
 
 
 def main(argv, stream):
@@ -134,150 +291,21 @@ def main(argv, stream):
 
   args = parser.parse_args(argv[1:])
 
+  try:
+    basedir = chromium_utils.FindUpward(os.getcwd(), 'b')
+  except chromium_utils.PathNotFound as e:
+    LOGGER.warn(e)
+    # Use base directory inside system temporary directory - if we use slave
+    # one (cwd), the paths get too long. Recipes which need different paths
+    # or persistent directories should do so explicitly.
+    basedir = tempfile.gettempdir()
+
   with robust_tempdir.RobustTempdir(
       prefix='rr', leak=args.leak) as rt:
-    try:
-      basedir = chromium_utils.FindUpward(os.getcwd(), 'b')
-    except chromium_utils.PathNotFound as e:
-      LOGGER.warn(e)
-      # Use base directory inside system temporary directory - if we use slave
-      # one (cwd), the paths get too long. Recipes which need different paths
-      # or persistent directories should do so explicitly.
-      basedir = tempfile.gettempdir()
-
     # Explicitly clean up possibly leaked temporary directories
     # from previous runs.
     rt.cleanup(basedir)
-
-    tempdir = rt.tempdir(basedir)
-    LOGGER.info('Using temporary directory: [%s].', tempdir)
-
-    build_data_dir = rt.tempdir(basedir)
-    LOGGER.info('Using build data directory: [%s].', build_data_dir)
-
-    properties = copy.copy(args.factory_properties)
-    properties.update(args.build_properties)
-    properties['build_data_dir'] = build_data_dir
-    # path_config property defines what paths a build uses for checkout, git
-    # cache, goma cache, etc.
-    # Unless it is explicitly specified by a builder, use paths for buildbot
-    # environment.
-    properties['path_config'] = properties.get('path_config', 'buildbot')
-    properties['bot_id'] = properties['slavename']
-    properties['builder_id'] = 'master.%s:%s' % (
-      properties['mastername'], properties['buildername'])
-    LOGGER.info('Using properties: %r', properties)
-    properties_file = os.path.join(tempdir, 'remote_run_properties.json')
-    with open(properties_file, 'w') as f:
-      json.dump(properties, f)
-
-    # put client in /b/cipd_client. Do import here to avoid ImportErrors
-    # tanking the update_scripts mechanism.
-    from slave import cipd_bootstrap_v2
-    cipd_bootstrap_v2.high_level_ensure_cipd_client(
-      basedir, properties.get('mastername'))
-
-    monitoring_utils.write_build_monitoring_event(build_data_dir, properties)
-
-    # Make switching to remote_run easier: we do not use buildbot workdir,
-    # and it takes disk space leading to out of disk errors.
-    buildbot_workdir = properties.get('workdir')
-    if buildbot_workdir:
-      try:
-        if os.path.exists(buildbot_workdir):
-          buildbot_workdir = os.path.realpath(buildbot_workdir)
-          cwd = os.path.realpath(os.getcwd())
-          if cwd.startswith(buildbot_workdir):
-            buildbot_workdir = cwd
-
-          LOGGER.info('Cleaning up buildbot workdir %r', buildbot_workdir)
-
-          # Buildbot workdir is usually used as current working directory,
-          # so do not remove it, but delete all of the contents. Deleting
-          # current working directory of a running process may cause
-          # confusing errors.
-          for p in (os.path.join(buildbot_workdir, x)
-                    for x in os.listdir(buildbot_workdir)):
-            LOGGER.info('Deleting %r', p)
-            chromium_utils.RemovePath(p)
-      except Exception as e:
-        # It's preferred that we keep going rather than fail the build
-        # on optional cleanup.
-        LOGGER.exception('Buildbot workdir cleanup failed: %s', e)
-
-    mastername = properties.get('mastername')
-
-    cipd_pin = _get_cipd_pin(mastername)
-    cipd_path = os.path.join(basedir, '.remote_run_cipd')
-    _install_cipd_packages(
-        cipd_path, cipd.CipdPackage(_RECIPES_PY_CIPD_PACKAGE, cipd_pin))
-
-    engine_flags = _get_engine_flags(mastername)
-
-    engine_args = []
-    if engine_flags:
-      engine_flags_path = os.path.join(tempdir, 'engine_flags.json')
-      with open(engine_flags_path, 'w') as f:
-        json.dump(engine_flags, f)
-
-      engine_args = ['--operational-args-path', engine_flags_path]
-
-    recipe_result_path = os.path.join(tempdir, 'recipe_result.json')
-    recipe_cmd = [
-        sys.executable,
-        os.path.join(cipd_path, 'recipes.py'),] + engine_args + [
-        '--verbose',
-        'remote',
-        '--repository', args.repository,
-        '--revision', args.revision,
-        '--workdir', os.path.join(tempdir, 'rw'),
-    ]
-    if args.use_gitiles:
-      recipe_cmd.append('--use-gitiles')
-    recipe_cmd.extend([
-        '--',] + (
-        engine_args) + [
-        '--verbose',
-        'run',
-        '--properties-file', properties_file,
-        '--workdir', os.path.join(tempdir, 'w'),
-        '--output-result-json', recipe_result_path,
-        properties.get('recipe') or args.recipe,
-    ])
-    # If we bootstrap through logdog, the recipe command line gets written
-    # to a temporary file and does not appear in the log.
-    LOGGER.info('Recipe command line: %r', recipe_cmd)
-
-    # Default to return code != 0 is for the benefit of buildbot, which uses
-    # return code to decide if a step failed or not.
-    recipe_return_code = 1
-    try:
-      bs = logdog_bootstrap.bootstrap(rt, args, basedir, tempdir, properties,
-                                      recipe_cmd)
-
-      LOGGER.info('Bootstrapping through LogDog: %s', bs.cmd)
-      bs.annotate(stream)
-      _ = _call(bs.cmd)
-      recipe_return_code = bs.get_result()
-    except logdog_bootstrap.NotBootstrapped as e:
-      LOGGER.info('Not using LogDog. Invoking `recipes.py` directly.')
-      recipe_return_code = _call(recipe_cmd)
-
-    # Try to open recipe result JSON. Any failure will result in an exception
-    # and an infra failure.
-    with open(recipe_result_path) as f:
-      return_value = json.load(f)
-
-    if engine_flags.get('use_result_proto'):
-      # If we failed, but aren't a step failure, we assume it was an
-      # exception.
-      f = return_value.get('failure')
-      if f is not None and not f.get('step_failure'):
-        # The recipe engine used to return -1, which got interpreted as 255
-        # by os.exit in python, since process exit codes are a single byte.
-        recipe_return_code = 255
-
-    return recipe_return_code
+    return _exec_recipe(args, rt, stream, basedir)
 
 
 def shell_main(argv):
---
+++ scripts/slave/unittests/remote_run_test.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Tests that the tools/build remote_run wrapper actually runs."""
+
+import base64
+import collections
+import json
+import logging
+import os
+import StringIO
+import subprocess
+import sys
+import tempfile
+import unittest
+import zlib
+
+import test_env  # pylint: disable=W0403,W0611
+
+import mock
+from common import annotator
+from common import env
+from slave import remote_run
+from slave import logdog_bootstrap
+from slave import robust_tempdir
+from slave import update_scripts
+
+# <build>/scripts/slave
+BASE_DIR = os.path.join(env.Build, 'scripts', 'slave')
+
+
+MockOptions = collections.namedtuple('MockOptions', (
+  'dry_run', 'factory_properties', 'build_properties', 'logdog_disable',
+  'repository', 'revision', 'use_gitiles', 'recipe', 'logdog_debug_out_file',
+))
+
+
+class RemoteRunTest(unittest.TestCase):
+
+  class TestException(Exception):
+    pass
+
+  def setUp(self):
+    # Because we modify system-level globals (yay!)
+    self._orig_env = os.environ.copy()
+
+  def tearDown(self):
+    os.environ = self._orig_env
+
+  def test_example(self):
+    build_properties = {
+      'mastername': 'tryserver.chromium.linux',
+      'buildername': 'builder',
+      'slavename': 'bot42-m1',
+      'true_prop': True,
+      'num_prop': 123,
+      'string_prop': '321',
+      'dict_prop': {'foo': 'bar'},
+    }
+
+    repo = 'https://chromium.googlesource.com/chromium/tools/build.git'
+    script_path = os.path.join(BASE_DIR, 'remote_run.py')
+    prop_gz = base64.b64encode(zlib.compress(json.dumps(build_properties)))
+    exit_code = subprocess.call([
+        sys.executable, script_path,
+        '--build-properties-gz=%s' % (prop_gz,),
+        '--recipe', 'remote_run_test',
+        '--repository', repo,
+        ])
+    self.assertEqual(exit_code, 0)
+
+  @mock.patch('slave.remote_run._call')
+  @mock.patch('slave.update_scripts._run_command')
+  @mock.patch('sys.platform', return_value='win')
+  @mock.patch('tempfile.mkstemp', side_effect=Exception('failure'))
+  def test_update_scripts_must_run(self, _tempfile_mkstemp, _sys_platform,
+                                   update_scripts_run_command, remote_run_call):
+    update_scripts_run_command.return_value = (0, "")
+    remote_run_call._call.return_value = 0
+
+    remote_run.shell_main(['remote_run.py', 'foo'])
+
+    gclient_path = os.path.join(env.Build, os.pardir, 'depot_tools',
+                                'gclient.bat')
+    update_scripts_run_command.assert_has_calls([
+        mock.call([gclient_path, 'sync',
+                   '--force', '--delete_unversioned_trees',
+                   '--break_repo_locks', '--verbose', '--jobs=2'],
+                  cwd=env.Build),
+        ])
+
+
+class RemoteRunExecTest(unittest.TestCase):
+  def setUp(self):
+    logging.basicConfig(level=logging.ERROR+1)
+
+    self.maxDiff = None
+    map(lambda x: x.start(), (
+        mock.patch('slave.remote_run._call'),
+        mock.patch('slave.remote_run._get_cipd_pins'),
+        mock.patch('slave.cipd_bootstrap_v2.high_level_ensure_cipd_client'),
+        mock.patch('slave.monitoring_utils.write_build_monitoring_event'),
+        mock.patch('os.path.exists'),
+        ))
+    self.addCleanup(mock.patch.stopall)
+
+    self.rt = robust_tempdir.RobustTempdir(prefix='remote_run_test')
+    self.addCleanup(self.rt.close)
+
+    self.stream_output = StringIO.StringIO()
+    self.stream = annotator.StructuredAnnotationStream(
+        stream=self.stream_output)
+    self.basedir = self.rt.tempdir()
+    self.tempdir = self.rt.tempdir()
+    self.build_data_dir = self.rt.tempdir()
+    self.opts = MockOptions(
+        dry_run=False,
+        logdog_disable=False,
+        factory_properties={},
+        build_properties={
+          'slavename': 'bot42-m1',
+          'mastername': 'tryserver.chromium.linux',
+          'buildername': 'builder',
+        },
+        repository='https://example.com/repo.git',
+        revision=None,
+        use_gitiles=True,
+        recipe='example/recipe',
+        logdog_debug_out_file=None,
+    )
+    self.rpy_path = os.path.join(env.Build, 'scripts', 'slave', 'recipes.py')
+
+    self.recipe_remote_args = [
+        sys.executable, self._bp('.remote_run_cipd', 'recipes.py'),
+        '--operational-args-path', self._tp('engine_flags.json'),
+        '--verbose', 'remote',
+        '--repository', self.opts.repository,
+        '--workdir', self._tp('rw'),
+        '--use-gitiles',
+    ]
+
+    self.recipe_args = [
+        '--operational-args-path', self._tp('engine_flags.json'),
+        '--verbose', 'run',
+        '--properties-file', self._tp('remote_run_properties.json'),
+        '--workdir', self._tp('w'),
+        '--output-result-json', self._tp('recipe_result.json'),
+        self.opts.recipe,
+    ]
+
+    # Use public recipes.py path.
+    os.path.exists.return_value = False
+
+    # Easily-configurable CIPD pins.
+    self.cipd_pins = remote_run._STABLE_CIPD_PINS
+    remote_run._get_cipd_pins = lambda _mastername: self.cipd_pins
+
+    # Written via '_write_recipe_result'.
+    self.recipe_result = None
+
+  def _bp(self, *p):
+    return os.path.join(*((self.basedir,) + p))
+
+  def _tp(self, *p):
+    return os.path.join(*((self.tempdir,) + p))
+
+  @staticmethod
+  def _default_namedtuple(typ, default=None):
+    return typ(**{f: default for f in typ._fields})
+
+  def _write_recipe_result(self):
+    with open(self._tp('recipe_result.json'), 'w') as fd:
+      json.dump(self.recipe_result, fd)
+
+  @mock.patch('slave.logdog_bootstrap.bootstrap',
+              side_effect=logdog_bootstrap.NotBootstrapped())
+  @mock.patch('slave.remote_run._install_cipd_packages')
+  @mock.patch('slave.robust_tempdir.RobustTempdir.tempdir')
+  def test_exec_without_logdog(self, rt_tempdir, _install_cipd_packages,
+                               _logdog_bootstrap):
+    remote_run._call.return_value = 0
+    rt_tempdir.side_effect = [self.tempdir, self.build_data_dir]
+    self._write_recipe_result()
+
+    rv = remote_run._exec_recipe(self.opts, self.rt, self.stream, self.basedir)
+    self.assertEqual(rv, 0)
+
+    args = self.recipe_remote_args + ['--'] + self.recipe_args
+    remote_run._call.assert_called_once_with(args)
+
+  @mock.patch('slave.logdog_bootstrap.bootstrap',
+              side_effect=logdog_bootstrap.NotBootstrapped())
+  @mock.patch('slave.remote_run._install_cipd_packages')
+  @mock.patch('slave.robust_tempdir.RobustTempdir.tempdir')
+  def test_canary_exec_without_logdog(self, rt_tempdir, _install_cipd_packages,
+                                      _logdog_bootstrap):
+    self.cipd_pins = remote_run._CANARY_CIPD_PINS
+    remote_run._call.return_value = 0
+    rt_tempdir.side_effect = [self.tempdir, self.build_data_dir]
+    self._write_recipe_result()
+
+    rv = remote_run._exec_recipe(self.opts, self.rt, self.stream, self.basedir)
+    self.assertEqual(rv, 0)
+
+    args = self.recipe_remote_args + ['--'] + self.recipe_args
+    remote_run._call.assert_called_once_with(args)
+
+  @mock.patch('slave.logdog_bootstrap.bootstrap')
+  @mock.patch('slave.logdog_bootstrap.BootstrapState.get_result')
+  @mock.patch('slave.remote_run._install_cipd_packages')
+  @mock.patch('slave.robust_tempdir.RobustTempdir.tempdir')
+  def test_exec_with_logdog(self, rt_tempdir, _install_cipd_packages,
+                            _logdog_bootstrap_result, bootstrap):
+
+    args = self.recipe_remote_args + ['--'] + self.recipe_args
+    cfg = self._default_namedtuple(logdog_bootstrap.Config)._replace(
+        params=self._default_namedtuple(logdog_bootstrap.Params)._replace(
+          project="project",
+        ),
+        prefix="prefix",
+        host="example.com",
+    )
+    bootstrap.return_value = logdog_bootstrap.BootstrapState(
+        cfg, ['logdog_bootstrap'] + args, '/path/to/result.json')
+    bootstrap.return_value.get_result.return_value = 0
+
+    remote_run._call.return_value = 0
+    rt_tempdir.side_effect = [self.tempdir, self.build_data_dir]
+    self._write_recipe_result()
+
+    rv = remote_run._exec_recipe(self.opts, self.rt, self.stream, self.basedir)
+    self.assertEqual(rv, 0)
+
+    remote_run._call.assert_called_once_with(bootstrap.return_value.cmd)
+    self.assertEqual(
+        [l for l in self.stream_output.getvalue().splitlines() if l],
+        [
+            '@@@SEED_STEP LogDog Bootstrap@@@',
+            '@@@STEP_CURSOR LogDog Bootstrap@@@',
+            '@@@STEP_STARTED@@@',
+            '@@@SET_BUILD_PROPERTY@logdog_project@"project"@@@',
+            '@@@SET_BUILD_PROPERTY@logdog_prefix@"prefix"@@@',
+            ('@@@SET_BUILD_PROPERTY@logdog_annotation_url@'
+             '"logdog://example.com/project/prefix/+/recipes/annotations"@@@'),
+            '@@@STEP_CURSOR LogDog Bootstrap@@@',
+            '@@@STEP_CLOSED@@@',
+        ]
+    )
+
+  @mock.patch('slave.logdog_bootstrap.bootstrap')
+  @mock.patch('slave.logdog_bootstrap.BootstrapState.get_result')
+  @mock.patch('slave.remote_run._install_cipd_packages')
+  @mock.patch('slave.robust_tempdir.RobustTempdir.tempdir')
+  def test_canary_exec_with_logdog(self, rt_tempdir, _install_cipd_packages,
+                                   _logdog_bootstrap_result, bootstrap):
+    self.cipd_pins = remote_run._CANARY_CIPD_PINS
+
+    args = self.recipe_remote_args + ['--'] + self.recipe_args
+    cfg = self._default_namedtuple(logdog_bootstrap.Config)._replace(
+        params=self._default_namedtuple(logdog_bootstrap.Params)._replace(
+          project="project",
+        ),
+        prefix="prefix",
+        host="example.com",
+    )
+    bootstrap.return_value = logdog_bootstrap.BootstrapState(
+        cfg, ['logdog_bootstrap'] + args, '/path/to/result.json')
+    bootstrap.return_value.get_result.return_value = 0
+
+    remote_run._call.return_value = 0
+    rt_tempdir.side_effect = [self.tempdir, self.build_data_dir]
+    self._write_recipe_result()
+
+    rv = remote_run._exec_recipe(self.opts, self.rt, self.stream, self.basedir)
+    self.assertEqual(rv, 0)
+
+    remote_run._call.assert_called_once_with(bootstrap.return_value.cmd)
+    self.assertEqual(
+        [l for l in self.stream_output.getvalue().splitlines() if l],
+        [
+            '@@@SEED_STEP LogDog Bootstrap@@@',
+            '@@@STEP_CURSOR LogDog Bootstrap@@@',
+            '@@@STEP_STARTED@@@',
+            '@@@SET_BUILD_PROPERTY@logdog_project@"project"@@@',
+            '@@@SET_BUILD_PROPERTY@logdog_prefix@"prefix"@@@',
+            ('@@@SET_BUILD_PROPERTY@logdog_annotation_url@'
+             '"logdog://example.com/project/prefix/+/recipes/annotations"@@@'),
+            '@@@STEP_CURSOR LogDog Bootstrap@@@',
+            '@@@STEP_CLOSED@@@',
+        ]
+    )
+
+
+if __name__ == '__main__':
+  unittest.main()
