dbf33572bcef6cb67f9085c29d198eb364462f77
Michael Goderbauer <goderbauer@google.com>
2017-03-01 10:34:07 -0800

    upload gen_snapshot for script snapshots on windows
    Change-Id: I8773d6fe0ba8ca2aac5ed1fff79fb6a0812baa1f
    Reviewed-on: https://chromium-review.googlesource.com/447909
    Reviewed-by: Adam Barth <abarth@chromium.org>
    Commit-Queue: Michael Goderbauer <goderbauer@google.com>

--- scripts/slave/recipes/flutter/engine.py
+++ scripts/slave/recipes/flutter/engine.py
@@ -268,11 +268,19 @@ def BuildIOS(api):
 
 
 def BuildWindows(api):
+  RunGN(api, '--runtime-mode', 'debug', '--unoptimized')
   RunGN(api, '--runtime-mode', 'profile', '--android')
   RunGN(api, '--runtime-mode', 'release', '--android')
 
+  Build(api, 'host_debug_unopt', 'flutter/lib/snapshot:generate_snapshot_bin')
   Build(api, 'android_profile', 'gen_snapshot')
   Build(api, 'android_release', 'gen_snapshot')
+
+  UploadArtifacts(api, 'windows-x64', [
+    'out/host_debug_unopt/gen_snapshot.exe',
+    'out/host_debug_unopt/gen/flutter/lib/snapshot/isolate_snapshot.bin',
+    'out/host_debug_unopt/gen/flutter/lib/snapshot/vm_isolate_snapshot.bin',
+  ])
 
   UploadArtifacts(api, "android-arm-profile" , [
     'out/android_profile/gen_snapshot.exe',
