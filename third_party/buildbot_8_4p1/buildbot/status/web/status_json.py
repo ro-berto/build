4113e253e0d026bfa4b368bd3c5bc4b3240146cd
Andrew Bonventre <andybons@chromium.org>
2017-03-02 17:08:15 +0000

    Revert "Excempt /json/clock and varz from the 5s delay"
    This reverts commit b0d00e06da1e68128009549a7e675a521df65004.
    
    Reason for revert: possible reason for https://crbug.com/697813
    
    Original change's description:
    > Excempt /json/clock and varz from the 5s delay
    > 
    > BUG=692708
    > 
    > Change-Id: I6fc35c391de3f410bc32d6800a1566b6e6f85833
    > Reviewed-on: https://chromium-review.googlesource.com/448699
    > Commit-Queue: Ryan Tseng <hinoka@chromium.org>
    > Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    > 
    
    TBR=iannucci@chromium.org,hinoka@chromium.org,chromium-reviews@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=692708
    
    Change-Id: I32052dccb5dddfc3d1b39be4bacdf562db93a08e
    Reviewed-on: https://chromium-review.googlesource.com/448796
    Reviewed-by: Andrew Bonventre <andybons@chromium.org>
    Commit-Queue: Andrew Bonventre <andybons@chromium.org>

--- third_party/buildbot_8_4p1/buildbot/status/web/status_json.py
+++ third_party/buildbot_8_4p1/buildbot/status/web/status_json.py
@@ -51,9 +51,6 @@ SERVER_STARTED = get_timeblock()
 _IS_INT = re.compile('^[-+]?\d+$')
 
 
-NO_DELAY_ENDPOINTS = ('/json/var', '/json/clock')
-
-
 FLAGS = """\
   - as_text
     - By default, application/json is used. Setting as_text=1 change the type
@@ -145,7 +142,7 @@ def FilterOut(data):
 
 def sleep(secs):
     d = defer.Deferred()
-    reactor.callLater(secs, d.callback, None)
+    reactor.callLater(secs, d.callback)
     return d
 
 
@@ -197,12 +194,9 @@ class JsonResource(resource.Resource):
             'user-agent', ['unknown'])[0]
         twlog.msg('Received request for %s from %s, id: %s' %
                   (request.uri, userAgent, id(request)))
-        if request.uri in NO_DELAY_ENDPOINTS:
-            d = defer.maybeDeferred(lambda : self.content(request))
-        else:
-            # Artifically delay things by 5 seconds.
-            d = sleep(5)
-            d.addCallback(lambda _: self.content(request))
+        # Artifically delay things by 5 seconds.
+        d = sleep(5)
+        d.addCallback(self.content, request)
         def handle(data):
             if isinstance(data, unicode):
                 data = data.encode("utf-8")
