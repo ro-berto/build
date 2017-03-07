42fb1824a16988676c015741261cdeedf84c5a45
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-03-07 17:03:41 +0100

    Remove buildbot recipe module (unused)
    This was found with new recipe simulation command
    I've been working on.
    
    BUG=699120
    
    Change-Id: I3f4e05d43e399bce0b56c15fa93b972d2cdab0ec
    Reviewed-on: https://chromium-review.googlesource.com/451420
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- scripts/slave/recipe_modules/buildbot/__init__.py
+++
@@ -1,9 +0,0 @@
-DEPS = [
-  'recipe_engine/path',
-  'recipe_engine/properties',
-  'recipe_engine/python',
-]
-
-
-# TODO(phajdan.jr): provide coverage (http://crbug.com/693058).
-DISABLE_STRICT_COVERAGE = True
