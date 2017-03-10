e3eedb9983fa7966f99536baae6ebc26272fd60d
smut <smut@google.com>
2017-03-09 17:13:47 -0800

    Retry tests without the patch on iOS try bot failures
    Previously only the build was retried, now also retry the tests. This will give some insight as to whether the test itself is broken on HEAD.
    
    Also remove the step suffixes and differentiate without patch retry using nested steps.
    
    BUG=697231
    
    Change-Id: I19e529461e0f506c2e61832724fe8de44bb469ec
    Reviewed-on: https://chromium-review.googlesource.com/452808
    Commit-Queue: smut <smut@chromium.org>
    Reviewed-by: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- scripts/slave/recipes/ios/try.py
+++ scripts/slave/recipes/ios/try.py
@@ -19,15 +19,21 @@ def RunSteps(api):
     # Ensure try bots mirror configs from chromium.mac.
     api.ios.read_build_config(master_name='chromium.mac')
     try:
-      api.ios.build(analyze=True, suffix='with patch')
+      api.ios.build(analyze=True)
+      api.ios.test_swarming()
     except api.step.StepFailure:
       bot_update_json = bot_update_step.json.output
       api.gclient.c.revisions['src'] = str(
           bot_update_json['properties']['got_revision'])
       api.ios.checkout(patch=False, update_presentation=False)
-      api.ios.build(suffix='without patch')
+      api.ios.build(suffix=' (without patch)')
+      api.ios.test_swarming(suffix=' (without patch)')
+      # TODO(smut): Compare test failures with and without the patch.
+      # If the same test cases failed with and without the patch, we
+      # could consider the build a success because ToT itself is broken
+      # and not the patch. See the test_utils recipe module and the
+      # determine_new_failures method.
       raise
-    api.ios.test_swarming()
 
 def GenTests(api):
   def suppress_analyze():
@@ -245,7 +251,11 @@ def GenTests(api):
       ],
     })
     + suppress_analyze()
-    + api.step_data('compile (with patch)', retcode=1)
+    + api.step_data(
+        'bootstrap swarming.swarming.py --version',
+        stdout=api.raw_io.output_text('1.2.3'),
+    )
+    + api.step_data('isolate.archive', retcode=1)
   )
 
   yield (
@@ -276,7 +286,7 @@ def GenTests(api):
       ],
     })
     + suppress_analyze()
-    + api.step_data('compile (with patch)', retcode=1)
+    + api.step_data('compile', retcode=1)
     + api.step_data('compile (without patch)', retcode=1)
   )
 
@@ -351,7 +361,11 @@ def GenTests(api):
         },
       ],
     })
-    + api.step_data('compile (with patch)', retcode=1)
+    + api.step_data('compile', retcode=1)
+    + api.step_data(
+        'bootstrap swarming (without patch).swarming.py --version',
+        stdout=api.raw_io.output_text('1.2.3'),
+    )
     + suppress_analyze()
   )
 
