973870a62af1f77ac5356fbabd9a471a4fdf6c91
Ivan Krasin <krasin@chromium.org>
2017-03-16 10:18:40 -0700

    Add 'CFI ThinLTO Linux ToT' buildbot (recipes part).
    See the src part for more details:
    https://codereview.chromium.org/2759443002/
    
    BUG=660216
    
    Change-Id: If1922c68bd2da060a84a5da2623387d83acdde62
    Reviewed-on: https://chromium-review.googlesource.com/456616
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Ivan Krasin <krasin@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
@@ -351,6 +351,20 @@ SPEC = {
       'test_results_config': 'staging_server',
       'testing': { 'platform': 'linux' },
     },
+    'CFI ThinLTO Linux ToT': {
+      'chromium_config': 'clang_tot_linux_lld',
+      'chromium_apply_config': ['mb'],
+      'gclient_config': 'chromium',
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Release',
+        'TARGET_BITS': 64,
+      },
+      'bot_type': 'builder_tester',
+      'test_results_config': 'staging_server',
+      'testing': {
+        'platform': 'linux',
+      },
+    },
     'LTO Linux': {
       'chromium_config': 'chromium_official',
       'chromium_apply_config': ['mb'],
