83ae262fa30b20b00f65fe9bc7bd50428e6e032d
Vyacheslav Egorov <vegorov@google.com>
2017-03-29 13:31:35 +0200

    On dartk/dartkp builders test all suites and not just co19 language kernel service.
    BUG=
    
    Change-Id: I04b4d198b59f8af18b85d3f9a869fc8f0b87ea9e
    Reviewed-on: https://chromium-review.googlesource.com/461825
    Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- scripts/slave/recipes/dart/dart_vm_kernel.py
+++ scripts/slave/recipes/dart/dart_vm_kernel.py
@@ -23,9 +23,6 @@ builders = {
   },
 }
 
-transformer_args = []
-general_test_args = ['co19', 'language', 'kernel', 'service']
-
 for platform in ['linux']:
   for arch in ['x64']:
     for mode in ['debug', 'release']:
@@ -36,7 +33,7 @@ for platform in ['linux']:
         'mode': mode,
         'target_arch': arch,
         'build_args': ['runtime_kernel'],
-        'test_args': ['-cdartk', '-rvm'] + extra_args + general_test_args,
+        'test_args': ['-cdartk', '-rvm'] + extra_args,
       }
       builders['vm-kernel-precomp-%s-%s-%s' % (platform, mode, arch)] = {
         'mode': mode,
@@ -44,8 +41,7 @@ for platform in ['linux']:
         'build_args': (['runtime_kernel',
                         'dart_bootstrap',
                         'dart_precompiled_runtime']),
-        'test_args': ['-cdartkp', '-rdart_precompiled'] + extra_args +
-            general_test_args,
+        'test_args': ['-cdartkp', '-rdart_precompiled'] + extra_args,
         'archive_core_dumps': (platform == 'linux' or platform == 'win'),
       }
 
