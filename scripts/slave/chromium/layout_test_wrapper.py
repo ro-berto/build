5b00be794346c76a12dec3a81d11c24b248b7537
Quinten Yearsley <qyearsley@google.com>
2017-04-05 14:37:26 -0700

    Simplify layout_test_wrapper.py more, again.
    This CL removes:
     - Logic related to opening and saving full_results.json
     - Logic related to cleaning up possibly-left-over files
    
    It keeps running the script via chromium_utils.RunCommand;
    previously it was run via slave_utils.RunPythonCommandInBuildDir,
    which adds src/tools/python to PYTHONPATH then invokes
    chromium_utils.RunCommand. Adding things to PYTHONPATH is likely
    unnecessary so this removes that call but keeps the call to RunCommand.
    
    I'm still not sure why RunCommand might be required for now, since
    RunCommand is a long and complicated function.
    
    If this is committed and it doesn't cause failures, then I think we can
    conclude that RunCommand is doing something that prevents "Unable to
    open X display" and "System dependencies check failed." errors.
    
    Bug: 695700
    Change-Id: I91a34d9438a27dee0d3e317eea37c45e37446bbd
    Reviewed-on: https://chromium-review.googlesource.com/469108
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>

--- scripts/slave/chromium/layout_test_wrapper.py
+++ scripts/slave/chromium/layout_test_wrapper.py
@@ -17,72 +17,17 @@ from slave import build_directory
 from slave import slave_utils
 
 
-def layout_test(options):
-  """Calls run-webkit-tests using Python from the tree."""
+def main():
+  assert sys.platform != 'win32', 'This script should not be run on Windows.'
+
   build_dir = os.path.abspath(build_directory.GetBuildOutputDirectory())
   blink_scripts_dir = chromium_utils.FindUpward(
       build_dir, 'third_party', 'WebKit', 'Tools', 'Scripts')
   run_blink_tests = os.path.join(blink_scripts_dir, 'run-webkit-tests')
 
   # Forward all command line arguments on to run-webkit-tests.
-  command = [run_blink_tests] + sys.argv[1:]
-
-  chromium_utils.RemoveDirectory(options.results_directory)
-
-  # Nuke anything that appears to be stale chrome items in the temporary
-  # directory from previous test runs (i.e.- from crashes or unit test leaks).
-  slave_utils.RemoveChromeTemporaryFiles()
-
-  try:
-    return slave_utils.RunPythonCommandInBuildDir(
-        build_dir, options.target, command)
-  finally:
-    results_dir = options.results_directory
-    results_json = os.path.join(results_dir, "full_results.json")
-
-    # If the json results file was not produced, then we produce no output
-    # file too and rely on a recipe to handle this as invalid result.
-    if not os.path.isfile(results_json):
-      print 'File %s not found!' % results_json
-    else:
-      with open(results_json, 'rb') as f:
-        data = f.read()
-
-      # data is in the form of:
-      # 'ADD_RESULTS({json object});' or just '{json object}'
-      start = data.find('{')
-      end = data.rfind('}')
-      # Find returns -1 when it didn't find something.
-      if start != -1 and end != -1:
-        data = data[start:end+1]
-        with open(options.json_test_results, 'wb') as f:
-          f.write(data)
-        print 'Wrote test results to %s (%d bytes)' % (
-            options.json_test_results, len(data))
-      else:
-        print 'JSON data not found in full_results.json'
-        print '-'*75
-
-      print data[:500], '...'
-      print '<snip>'
-      print '...', data[-500:]
-      print '-'*75
-
-
-def main():
-  assert sys.platform != 'win32', 'This script should not be run on Windows.'
-
-  # Some options are still used in what's left of this script.
-  # They will be parsed and used, but then passed on verbatim.
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--results-directory', required=True)
-  parser.add_argument('--json-test-results', required=True)
-  parser.add_argument('--target', required=True)
-  parsed_options, _ = parser.parse_known_args()
-
-  assert os.path.isabs(parsed_options.results_directory)
-
-  return layout_test(parsed_options)
+  command = ['python', run_blink_tests] + sys.argv[1:]
+  return chromium_utils.RunCommand(command)
 
 
 if '__main__' == __name__:
