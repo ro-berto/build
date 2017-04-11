90207ae6f8a07e574bcc52fe76d3efacab49cfb5
Robert Iannucci <iannucci@chromium.org>
2017-04-11 13:23:20 -0700

    Make recipe_test.py provide a helpful message.
    Bug: 708305
    Change-Id: Idfada40fd9ea45ba1eaae470ba6086be4badad4c
    Reviewed-on: https://chromium-review.googlesource.com/474388
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- scripts/slave/unittests/recipe_test.py
+++ scripts/slave/unittests/recipe_test.py
@@ -7,12 +7,28 @@
 
 import os
 import subprocess
+import sys
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+RECIPES_PY = os.path.join(ROOT_DIR, 'recipes.py')
+
+VERBOSE = '--verbose' in sys.argv or '-v' in sys.argv
+VERBOSE_ARG = ['--verbose'] if VERBOSE else []
+
+ALLOWED_ARGS = 1 if VERBOSE else 0
+
+if len(sys.argv) > ALLOWED_ARGS+1:
+  print 'It looks like you\'re manually invoking this test script. Please note'
+  print 'that this is just a wrapper to enable `git cl presubmit` testing.'
+  print
+  print 'To interact with the recipes, please use %s directly.' % RECIPES_PY
+  sys.exit(1)
+
 def recipes_py(*args):
   subprocess.check_call([
-      os.path.join(ROOT_DIR, 'recipes.py'), '--use-bootstrap'] + list(args))
+      os.path.join(ROOT_DIR, 'recipes.py'), '--use-bootstrap'
+  ] + VERBOSE_ARG + list(args))
 
 recipes_py('test', 'run')
 
