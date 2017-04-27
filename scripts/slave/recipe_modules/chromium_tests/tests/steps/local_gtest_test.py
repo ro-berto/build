d734dc32b3c4c59f9e451b7ce0c18bb92ff8735c
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-04-27 20:19:38 +0200

    Add tests for chromium_tests recipe module (patch #4)
    These focus on providing coverage, and going towards enforcing
    strict recipe coverage in chromium_tests as soon as possible
    to prevent further regressions.
    
    One the module has stable strict coverage, the tests can be refined
    further, e.g. adding more post_process hooks.
    
    Bug: 693058
    Change-Id: I5b2e5c739fd774f84a08c2e4123369a438e06549
    Reviewed-on: https://chromium-review.googlesource.com/489281
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

---
+++ scripts/slave/recipe_modules/chromium_tests/tests/steps/local_gtest_test.py
@@ -0,0 +1,50 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+    'chromium',
+    'chromium_tests',
+    'depot_tools/bot_update',
+    'recipe_engine/json',
+    'recipe_engine/properties',
+    'recipe_engine/step',
+    'test_results',
+    'test_utils',
+]
+
+
+def RunSteps(api):
+  api.chromium.set_config('chromium')
+  api.test_results.set_config('public_server')
+
+  test = api.chromium_tests.steps.LocalGTestTest('base_unittests')
+
+  test_options = api.chromium_tests.steps.TestOptions(
+      repeat_count=2,
+      test_filter=['foo.bar'],
+      retry_limit=3,
+      run_disabled=True)
+  test.test_options = test_options
+
+  try:
+    test.run(api, '')
+  finally:
+    api.step('details', [])
+    api.step.active_result.presentation.logs['details'] = [
+        'compile_targets: %r' % test.compile_targets(api),
+        'step_metadata: %r' % test.step_metadata(api, ''),
+        'pass_fail_counts: %r' % test.pass_fail_counts(''),
+        'uses_local_devices: %r' % test.uses_local_devices,
+    ]
+
+
+def GenTests(api):
+  yield (
+      api.test('basic') +
+      api.properties(
+          mastername='test_mastername',
+          buildername='test_buildername',
+          bot_id='test_bot_id',
+          buildnumber=123)
+  )
