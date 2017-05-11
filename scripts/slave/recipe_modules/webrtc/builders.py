26a68cd1302d673f1ca924b22340f52a125bf315
Henrik Kjellander <kjellander@chromium.org>
2017-05-11 09:29:17 +0000

    Revert "WebRTC: Tests for Android Studio project creation"
    This reverts commit 2d2c1a2cce4ecd784375fe0b78aa2273f45df3b3.
    
    Reason for revert: Fails build since --gtest_output flag is passed to the test *sigh*
    
    Original change's description:
    > WebRTC: Tests for Android Studio project creation
    > 
    > Only run it on one configuration for now, since it's sufficient
    > according to sakal@.
    > 
    > BUG=webrtc:7608
    > 
    > Change-Id: I31e38957bae3dfa0841ad286f6ba3a17690b3c6f
    > Reviewed-on: https://chromium-review.googlesource.com/502147
    > Commit-Queue: Henrik Kjellander <kjellander@chromium.org>
    > Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    > 
    
    TBR=kjellander@chromium.org,ehmaldonado@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=webrtc:7608
    
    Change-Id: Ibe6ccd374b6d2830050266fecb5f22ca6621c41c
    Reviewed-on: https://chromium-review.googlesource.com/503267
    Reviewed-by: Henrik Kjellander <kjellander@chromium.org>
    Commit-Queue: Henrik Kjellander <kjellander@chromium.org>

--- scripts/slave/recipe_modules/webrtc/builders.py
+++ scripts/slave/recipe_modules/webrtc/builders.py
@@ -529,7 +529,6 @@ BUILDERS = freeze({
         'bot_type': 'builder_tester',
         'testing': {'platform': 'linux'},
         'archive_apprtc': True,
-        'test_android_studio_project_generation': True,
         'enable_swarming': True,
         'use_isolate': True,
         'swarming_dimensions': {
@@ -1500,7 +1499,6 @@ BUILDERS = freeze({
         },
         'bot_type': 'builder_tester',
         'testing': {'platform': 'linux'},
-        'test_android_studio_project_generation': True,
         'use_isolate': True,
         'enable_swarming': True,
         'swarming_dimensions': {
