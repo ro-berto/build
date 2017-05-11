0903c31179747b6479b608eb5240eccca600084c
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-05-11 13:01:07 +0200

    chromium_checkout: do not run gclient validate if patch failed to apply
    Bug: 570091
    Change-Id: I092b463979f0b9a8eba71559933f9476c33b7bd2
    Reviewed-on: https://chromium-review.googlesource.com/503031
    Reviewed-by: Emma Söderberg <emso@chromium.org>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- scripts/slave/recipe_modules/chromium_checkout/api.py
+++ scripts/slave/recipe_modules/chromium_checkout/api.py
@@ -78,10 +78,11 @@ class ChromiumCheckoutApi(recipe_api.RecipeApi):
       # Run a non-fatal gclient validation step, allowing us to collect
       # metrics using event_mon.
       # TODO(phajdan.jr): always enable or remove (http://crbug.com/570091).
-      try:
-        self.m.gclient('validate', ['validate'])
-      except self.m.step.StepFailure:  # pragma: no cover
-        pass
+      if not update_step.json.output.get('patch_failure'):
+        try:
+          self.m.gclient('validate', ['validate'])
+        except self.m.step.StepFailure:  # pragma: no cover
+          pass
     assert update_step.json.output['did_run']
     # HACK(dnj): Remove after 'crbug.com/398105' has landed
     self.m.chromium.set_build_properties(update_step.json.output['properties'])
