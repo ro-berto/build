fc3f1c8e4f94ecba499a4a77518b56b76e1fa001
Robert Iannucci <iannucci@chromium.org>
2017-05-12 01:59:15 -0700

    [gyp] convert recipes and module to new context module
    This is a syntax-only change to convert to the new (documented) context
    module.
    
    R=agable@chromium.org
    
    Bug:
    Change-Id: Ia62bffe22e12cb6efc88831a24808a0dc4c3560b
    Reviewed-on: https://chromium-review.googlesource.com/504247
    Reviewed-by: Aaron Gable <agable@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/recipes/gyp.py
+++ scripts/slave/recipes/gyp.py
@@ -6,6 +6,7 @@ DEPS = [
   'chromium',
   'depot_tools/bot_update',
   'depot_tools/gclient',
+  'recipe_engine/context',
   'recipe_engine/path',
   'recipe_engine/properties',
   'recipe_engine/python',
@@ -18,7 +19,7 @@ def RunSteps(api):
 
   api.bot_update.ensure_checkout()
 
-  with api.step.context({'cwd': api.path['checkout']}):
+  with api.context(cwd=api.path['checkout']):
     api.python('run tests',
                api.path['checkout'].join('gyptest.py'), ['-a'])
 
