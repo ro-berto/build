f855de86ca9fc39081e10e7941b2a007e3f501a4
Misha Efimov <mef@chromium.org>
2017-05-17 13:24:16 -0400

    Use is_official_build=true for cronet builds.
    Bug:722868
    Change-Id: I95d9bd139d50f10239f147e87be35828ffcb3adf
    Reviewed-on: https://chromium-review.googlesource.com/507907
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_android/chromium_config.py
+++ scripts/slave/recipe_modules/chromium_android/chromium_config.py
@@ -69,6 +69,7 @@ def mipsel_builder_mb(c):
 
 @CONFIG_CTX(includes=['clobber'])
 def cronet_builder(c):
+  c.gn_args.append('is_official_build=true')
   c.gn_args.append('disable_file_support=true')
   c.gn_args.append('disable_ftp_support=true')
   c.gn_args.append('enable_websockets=false')
