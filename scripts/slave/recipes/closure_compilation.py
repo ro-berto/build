31b834a0e849ce66bd5a7c8544071dce1276ef2f
Dan Beam <dbeam@chromium.org>
2017-05-18 19:08:47 -0700

    Closure: turn down the old GYP format
    R=dpranke@chromium.org
    BUG=585553
    
    Change-Id: I6577ae78cfb1150f181c054ebc97fbdffd05665d
    Reviewed-on: https://chromium-review.googlesource.com/509129
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Dan Beam <dbeam@chromium.org>

--- scripts/slave/recipes/closure_compilation.py
+++ scripts/slave/recipes/closure_compilation.py
@@ -43,19 +43,10 @@ def RunSteps(api):
       'generate_gyp_files',
       [api.path['checkout'].join('build', 'gyp_chromium'),
        api.path['checkout'].join('third_party', 'closure_compiler',
-                                 'compiled_resources.gyp')],
-  )
-
-  api.chromium.compile()
-
-  api.step(
-      'generate_v2_gyp_files',
-      [api.path['checkout'].join('build', 'gyp_chromium'),
-       api.path['checkout'].join('third_party', 'closure_compiler',
                                  'compiled_resources2.gyp')],
   )
 
-  api.chromium.compile(name='compile_v2')
+  api.chromium.compile()
 
 
 def GenTests(api):
