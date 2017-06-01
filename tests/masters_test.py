550c0879e910ae1ac95a80763299fb49331caed6
Michael Achenbach <machenbach@chromium.org>
2017-06-01 20:47:11 +0200

    V8: Add new master client.v8.clusterfuzz
    Bug: 728539
    Change-Id: I6ddba5e9b378d7dfaf552d49b9434d4d7c04aa70
    Reviewed-on: https://chromium-review.googlesource.com/521144
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -268,6 +268,7 @@ def main(argv):
       'master.client.v8': 'V8',
       'master.client.v8.branches': 'V8Branches',
       'master.client.v8.chromium': 'V8Chromium',
+      'master.client.v8.clusterfuzz': 'ClientV8Clusterfuzz',
       'master.client.v8.fyi': 'V8FYI',
       'master.client.v8.ports': 'V8Ports',
       'master.client.wasm.llvm': 'WasmLlvm',
