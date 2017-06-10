80344a734b38d9124187654e17b6ab3e83463f0b
Robert Iannucci <iannucci@chromium.org>
2017-06-09 17:15:41 -0700

    [cipd] bump cipd client prod version.
    R=dnj@chromium.org, vadimsh@chromium.org
    
    Bug: 720603
    Change-Id: I40b86ad6b0aff17fa3b408368a49ec50e80d5a0d
    Reviewed-on: https://chromium-review.googlesource.com/529974
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/cipd_bootstrap_v2.py
+++ scripts/slave/cipd_bootstrap_v2.py
@@ -18,7 +18,7 @@ from slave import cipd
 CLIENT_NAME = 'cipd' + infra_platform.exe_suffix()
 
 
-DEFAULT_CIPD_VERSION = 'git_revision:76eadcd75c5ad2638e1fc098f81748aad150c7c0'
+DEFAULT_CIPD_VERSION = 'git_revision:05f9a685b72ee556899506ffd965dca91bd60efa'
 STAGING_CIPD_VERSION = 'git_revision:05f9a685b72ee556899506ffd965dca91bd60efa'
 
 STAGING = 'staging'
