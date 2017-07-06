15a964e5e4351f9116af980c5a2084d761d53055
Robert Iannucci <iannucci@chromium.org>
2017-07-05 17:09:19 -0700

    Switch libyuv recipes and module to recipe_engine/file
    R=kjellander@chromium.org, tikuta@chromium.org
    
    Bug:
    Change-Id: I9c62419da9d73fc457d1ae22aea6340964f98977
    Reviewed-on: https://chromium-review.googlesource.com/560720
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@google.com>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/recipe_modules/libyuv/__init__.py
+++ scripts/slave/recipe_modules/libyuv/__init__.py
@@ -5,7 +5,7 @@ DEPS = [
   'depot_tools/bot_update',
   'depot_tools/gclient',
   'depot_tools/tryserver',
-  'file',
+  'recipe_engine/file',
   'recipe_engine/path',
   'recipe_engine/platform',
   'recipe_engine/properties',
