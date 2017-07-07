0794121e0e14697821c3abbe9db45a589f571bf4
Robert Iannucci <iannucci@chromium.org>
2017-07-06 19:42:08 -0700

    [chromedriver] remove unused dependency shutil
    R=dnj@chromium.org
    
    Bug: 739984
    Change-Id: I0e48236237521c5be0beb2b2a031e7aaa515a466
    Reviewed-on: https://chromium-review.googlesource.com/562672
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/recipe_modules/chromedriver/__init__.py
+++ scripts/slave/recipe_modules/chromedriver/__init__.py
@@ -15,7 +15,6 @@ DEPS = [
     'recipe_engine/properties',
     'recipe_engine/python',
     'recipe_engine/raw_io',
-    'recipe_engine/shutil',
     'recipe_engine/step',
     'recipe_engine/tempfile',
 ]
