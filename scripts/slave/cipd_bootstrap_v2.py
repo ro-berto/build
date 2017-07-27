318fb5cb80725e503472d74b35c9ff178b645d2b
Dan Jacques <dnj@google.com>
2017-07-27 14:49:50 +0200

    [cipd_bootstrap_v2] Bump prod CIPD version.
    Bump production CIPD version to:
    e2a98b41adb3403b5b7771c4101b42a55dc2dd9e.
    
    BUG=chromium:748651
    TEST=canary
    
    Change-Id: I5cd224ff3124ea7dee6084b4d9f0fe20f0464862
    Reviewed-on: https://chromium-review.googlesource.com/589009
    Commit-Queue: Daniel Jacques <dnj@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/cipd_bootstrap_v2.py
+++ scripts/slave/cipd_bootstrap_v2.py
@@ -18,7 +18,7 @@ from slave import cipd
 CLIENT_NAME = 'cipd' + infra_platform.exe_suffix()
 
 
-DEFAULT_CIPD_VERSION = 'git_revision:5cf65fdf804a9b3f3023f79d5b3cab2a88ccd09e'
+DEFAULT_CIPD_VERSION = 'git_revision:e2a98b41adb3403b5b7771c4101b42a55dc2dd9e'
 STAGING_CIPD_VERSION = 'git_revision:e2a98b41adb3403b5b7771c4101b42a55dc2dd9e'
 
 # Latest Git bundle versions for packages that don't have a "latest" ref.
