838045dcdfc2d87d26c7223ca70de5e6f0858966
Alexander Thomas <athom@google.com>
2017-08-14 16:08:22 +0200

    Add sharded execution of isolate
    Add dart2js_swarming recipe which uploads an isolate
    
    Copy isolate to sdk root to change relative_cwd in dart2js_tests.isolated
    
    Bug:
    Change-Id: Ib8fd02d02df1221e60328d02d030192c68973add
    Reviewed-on: https://chromium-review.googlesource.com/612985
    Reviewed-by: Emma Söderberg <emso@chromium.org>
    Reviewed-by: William Hesse <whesse@google.com>
    Commit-Queue: William Hesse <whesse@google.com>

--- scripts/slave/README.recipes.md
+++ scripts/slave/README.recipes.md
@@ -208,6 +208,7 @@
   * [dart/dart](#recipes-dart_dart)
   * [dart/dart2js](#recipes-dart_dart2js)
   * [dart/dart2js_nobuild](#recipes-dart_dart2js_nobuild)
+  * [dart/dart2js_swarming](#recipes-dart_dart2js_swarming)
   * [dart/dart_cross_compiler](#recipes-dart_dart_cross_compiler)
   * [dart/dart_cross_tester](#recipes-dart_dart_cross_tester)
   * [dart/dart_vm](#recipes-dart_dart_vm)
@@ -4332,6 +4333,13 @@ Generates the sequence of steps that will be run by the slave.
 &mdash; **def [RunTests](/scripts/slave/recipes/dart/dart2js_nobuild.py#37)(api, test_args, test_specs, use_xvfb=False):**
 
 &mdash; **def [sdk\_url](/scripts/slave/recipes/dart/dart2js_nobuild.py#59)(channel, platform, arch, mode, revision):**
+### *recipes* / [dart/dart2js\_swarming](/scripts/slave/recipes/dart/dart2js_swarming.py)
+
+[DEPS](/scripts/slave/recipes/dart/dart2js_swarming.py#5): [swarming](#recipe_modules-swarming), [swarming\_client](#recipe_modules-swarming_client), [test\_utils](#recipe_modules-test_utils), [depot\_tools/bot\_update][depot_tools/recipe_modules/bot_update], [depot\_tools/depot\_tools][depot_tools/recipe_modules/depot_tools], [depot\_tools/gclient][depot_tools/recipe_modules/gclient], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/file][recipe_engine/recipe_modules/file], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/platform][recipe_engine/recipe_modules/platform], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/python][recipe_engine/recipe_modules/python], [recipe\_engine/raw\_io][recipe_engine/recipe_modules/raw_io], [recipe\_engine/step][recipe_engine/recipe_modules/step]
+
+&mdash; **def [RunSteps](/scripts/slave/recipes/dart/dart2js_swarming.py#60)(api):**
+
+&mdash; **def [RunTests](/scripts/slave/recipes/dart/dart2js_swarming.py#37)(api, test_args, test_specs, use_xvfb=False):**
 ### *recipes* / [dart/dart\_cross\_compiler](/scripts/slave/recipes/dart/dart_cross_compiler.py)
 
 [DEPS](/scripts/slave/recipes/dart/dart_cross_compiler.py#5): [test\_utils](#recipe_modules-test_utils), [trigger](#recipe_modules-trigger), [depot\_tools/bot\_update][depot_tools/recipe_modules/bot_update], [depot\_tools/gclient][depot_tools/recipe_modules/gclient], [depot\_tools/gsutil][depot_tools/recipe_modules/gsutil], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/platform][recipe_engine/recipe_modules/platform], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/python][recipe_engine/recipe_modules/python], [recipe\_engine/step][recipe_engine/recipe_modules/step]
---
+++ scripts/slave/recipes/dart/dart2js_swarming.py
@@ -0,0 +1,259 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/depot_tools',
+  'depot_tools/gclient',
+  'recipe_engine/context',
+  'recipe_engine/file',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/raw_io',
+  'recipe_engine/step',
+  'swarming_client',
+  'swarming',
+  'test_utils',
+]
+
+all_runtimes = ['none', 'd8', 'jsshell', 'ie9', 'ie10', 'ie11', 'ff',
+            'safari', 'chrome', 'safarimobilesim', 'drt', 'chromeff',
+            'ie10chrome', 'ie11ff']
+
+multiple_runtimes = {'chromeff': ['chrome', 'ff'],
+                     'ie10chrome': ['ie10', 'chrome'],
+                     'ie11ff': ['ie11', 'ff']}
+all_options = {'hostchecked': '--host-checked',
+               'minified': '--minified',
+               'cps': '--cps-ir',
+               'csp': '--csp',
+               'unittest': '',  # unittest is handled specially.
+               'debug': ''} # debug is handled specially.
+
+IsFirstTestStep = True
+def RunTests(api, test_args, test_specs, use_xvfb=False):
+  for test_spec in test_specs:
+    args = []
+    args.extend(test_args)
+    global IsFirstTestStep
+    if IsFirstTestStep:
+      IsFirstTestStep = False
+    else:
+      args.append('--append_logs')
+    args.extend(test_spec['tests'])
+
+    with api.context(cwd=api.path['checkout'],
+                     env_prefixes={'PATH':[api.depot_tools.root]}):
+      if use_xvfb:
+        xvfb_cmd = ['xvfb-run', '-a', '--server-args=-screen 0 1024x768x24']
+        xvfb_cmd.extend(['python', '-u', './tools/test.py'])
+        xvfb_cmd.extend(args)
+        api.step(test_spec['name'], xvfb_cmd)
+      else:
+        api.python(test_spec['name'],
+                   api.path['checkout'].join('tools', 'test.py'),
+                   args=args)
+
+def RunSteps(api):
+  builder_name = str(api.properties.get('buildername')) # Convert from unicode.
+  builder_fragments = builder_name.split('-')
+  assert len(builder_fragments) > 3
+  assert builder_fragments[0] == 'dart2js'
+  system = builder_fragments[1]
+  assert system in ['linux', 'mac10.11', 'win7', 'win8', 'win10']
+  runtime = builder_fragments[2]
+  assert runtime in all_runtimes
+  channel = builder_fragments[-1]
+  assert channel in ['be', 'dev', 'stable', 'integration', 'try']
+  try:
+    num_shards = int(builder_fragments[-2])
+    shard = int(builder_fragments[-3])
+    sharded = True
+    options_end = - 3
+  except ValueError:
+    sharded = False
+    options_end = - 1
+  options = builder_fragments[3:options_end]
+  for option in options:
+    assert all_options.has_key(option)
+  mode = 'debug' if 'debug' in options else 'release'
+  api.gclient.set_config('dart')
+  if channel == 'try':
+    api.gclient.c.solutions[0].url = 'https://dart.googlesource.com/sdk.git'
+
+  with api.context(cwd=api.path['cache'].join('builder')):
+    api.bot_update.ensure_checkout()
+    api.gclient.runhooks()
+
+  with api.context(cwd=api.path['checkout'],
+                   env_prefixes={'PATH':[api.depot_tools.root]}):
+    api.python('taskkill before building',
+               api.path['checkout'].join('tools', 'task_kill.py'),
+               args=['--kill_browsers=True'])
+
+    build_args = ['-m%s' % mode, '--arch=ia32', 'dart2js_bot']
+    if 'unittest' in options:
+      build_args.append('patched_dart2js_sdk')
+    api.python('build dart',
+               api.path['checkout'].join('tools', 'build.py'),
+               args=build_args)
+    api.swarming_client.checkout(
+      revision='5c8043e54541c3cee7ea255e0416020f2e3a5904')
+    api.file.copy('copy .isolate file to sdk root',
+            api.path['checkout'].join('tools', 'bots', 'dart2js_tests.isolate'),
+            api.path['checkout'])
+    step_result = api.python(
+      'upload testing isolate',
+      api.swarming_client.path.join('isolate.py'),
+      args= ['archive',
+             '-Ihttps://isolateserver.appspot.com',
+             '-i%s' % api.path['checkout'].join('dart2js_tests.isolate'),
+             '-s%s' % api.path['checkout'].join('dart2js_tests.isolated')],
+      stdout=api.raw_io.output('out'))
+    isolate_hash = step_result.stdout.strip()[:40]
+    step_result.presentation.step_text = 'isolate hash: %s' % isolate_hash
+
+    # Run different shards on isolates
+    api.swarming.set_default_dimension('os', 'Linux')
+    api.swarming.set_default_dimension('cpu', 'x86-64')
+    api.swarming.set_default_dimension('pool', 'Dart.LUCI')
+    test_args = ['-mrelease', '-aia32', '-cdart2js',
+                 '--dart2js-batch', '--reset-browser-configuration',
+                 '--report', '--time', '--use-sdk', '--progress=buildbot',
+                 '-v',
+                 '--exclude-suite=observatory_ui,service,co19',
+                 '-rd8']
+    num_shards = 6
+    tasks = []
+    for shard in range(num_shards):
+      task = api.swarming.task("dart2js_tests_shard_%s" % (shard + 1),
+                               isolate_hash,
+                               extra_args= test_args +
+                                 ['--shards=%s' % num_shards,
+                                  '--shard=%s' % (shard + 1)])
+      api.swarming.trigger_task(task)
+      tasks.append(task)
+    with api.step.defer_results():
+      for task in tasks:
+        api.swarming.collect_task(task)
+
+    test_args = ['-mrelease', '-aia32', '-cdart2js', '-rd8',
+                 '--dart2js-batch', '--reset-browser-configuration',
+                 '--report', '--time', '--use-sdk', '--progress=buildbot',
+                 '-v',
+                 'co19']
+    num_shards = 8
+    tasks = []
+    for shard in range(num_shards):
+      task = api.swarming.task("dart2js_co19_tests_shard_%s" % (shard + 1),
+                               isolate_hash,
+                               extra_args= test_args +
+                                 ['--shards=%s' % num_shards,
+                                  '--shard=%s' % (shard + 1)])
+      api.swarming.trigger_task(task)
+      tasks.append(task)
+    with api.step.defer_results():
+      for task in tasks:
+        api.swarming.collect_task(task)
+
+  with api.step.defer_results():
+    # Standard test steps, run on all runtimes.
+    runtimes = multiple_runtimes.get(runtime, [runtime])
+    for runtime in runtimes:
+      test_args = ['--mode=%s' % mode,
+                   '--arch=ia32',
+                   '--compiler=dart2js',
+                   '--dart2js-batch',
+                   '--runtime=%s' % runtime,
+                   '--progress=buildbot',
+                   '-v',
+                   '--reset-browser-configuration',
+                   '--report',
+                   '--time',
+                   '--write-debug-log',
+                   '--write-test-outcome-log']
+      for option in options:
+        if all_options[option] != '':
+          test_args.append(all_options[option])
+      if sharded:
+        test_args.extend(['--shards=%s' % num_shards, '--shard=%s' % shard])
+
+      if system in ['win7', 'win8', 'win10']:
+        test_args.append('--builder-tag=%s' % system)
+
+      if runtime in ['ie10', 'ie11']:
+        test_specs = [{'name': 'dart2js-%s tests' % runtime,
+                       'tests': ['html', 'pkg', 'samples']},
+                      {'name': 'dart2js-%s-co19 tests' % runtime,
+                       'tests': ['co19']}]
+      else:
+        test_specs = [
+          {'name': 'dart2js-%s-package tests' % runtime,
+           'tests': ['pkg']},
+          {'name': 'dart2js-%s-observatory-ui tests' % runtime,
+           'tests': ['observatory_ui']},
+          {'name': 'dart2js-%s-extra tests' % runtime,
+           'tests': ['dart2js_extra', 'dart2js_native']},
+        ]
+
+      needs_xvfb = (runtime in ['drt', 'dartium', 'chrome', 'ff'] and
+                    system == 'linux')
+      RunTests(api, test_args, test_specs, use_xvfb=needs_xvfb)
+      if runtime in ['d8', 'drt']:
+        test_args.append('--checked')
+        for spec in test_specs:
+          spec['name'] = spec['name'].replace(' tests', '-checked tests')
+        RunTests(api, test_args, test_specs, use_xvfb=needs_xvfb)
+
+    if 'unittest' in options:
+      with api.context(cwd=api.path['checkout']):
+        api.python('dart2js-unit tests',
+                   api.path['checkout'].join('tools', 'test.py'),
+                   args=["--mode=%s" % mode, "--compiler=none", "--runtime=vm",
+                         "--arch=ia32", "--time", "--use-sdk", "--report",
+                         "--write-debug-log", "--write-test-outcome-log",
+                         "--progress=buildbot", "-v", "--append_logs",
+                         "--reset-browser-configuration",
+                         "--shards=%s" % num_shards, "--shard=%s" % shard,
+                         "--checked", "dart2js"])
+
+    with api.context(cwd=api.path['checkout']):
+      # TODO(whesse): Add archive coredumps step from dart_factory.py.
+      api.python('taskkill after testing',
+                 api.path['checkout'].join('tools', 'task_kill.py'),
+                 args=['--kill_browsers=True'])
+      if api.platform.name == 'win':
+        api.step('debug log',
+                 ['cmd.exe', '/c', 'type', '.debug.log'])
+      else:
+        api.step('debug log',
+                 ['cat', '.debug.log'])
+
+def GenTests(api):
+   yield (
+      api.test('dart2js-linux-d8-hostchecked-csp-unittest-3-5-try') +
+      api.platform('linux', 64) +
+      api.properties.generic(
+        mastername='luci.dart.try',
+        buildername='dart2js-linux-d8-hostchecked-csp-unittest-3-5-try') +
+      api.step_data('upload testing isolate',
+                    stdout=api.raw_io.output('test isolate hash'))
+   )
+   yield (
+      api.test('dart2js-win7-ie10-debug-try') + api.platform('win', 32) +
+      api.properties.generic(
+        mastername='client.dart',
+        buildername='dart2js-win7-ie10-debug-try') +
+      api.step_data('upload testing isolate',
+                    stdout=api.raw_io.output('test isolate hash'))
+   )
+   yield (
+      api.test('dart2js-linux-drt-try') + api.platform('linux', 64) +
+      api.properties.generic(mastername='client.dart',
+                             buildername='dart2js-linux-drt-93-105-try') +
+      api.step_data('upload testing isolate',
+                    stdout=api.raw_io.output('test isolate hash'))
+   )
