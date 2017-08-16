bd6c09962c52d40c97dbcd8286dd6f0d80492edd
Tim 'mithro' Ansell <tansell@chromium.org>
2017-08-16 20:19:20 +1000

    Disable logdog_bootstrap_test:test_load
    This test is currently preventing anything landing of changes.
    
    BUG=755926
    TBR=dnj@chromium.org,hinoka@chromium.org
    
    Change-Id: I5b12c2bda7ea69302cdae885d0b891a906cd3036
    Reviewed-on: https://chromium-review.googlesource.com/616375
    Reviewed-by: Tim 'mithro' Ansell <tansell@chromium.org>
    Commit-Queue: Tim 'mithro' Ansell <tansell@chromium.org>

--- scripts/slave/unittests/logdog_bootstrap_test.py
+++ scripts/slave/unittests/logdog_bootstrap_test.py
@@ -555,6 +555,7 @@ class TestPublicParams(unittest.TestCase):
   def setUp(self):
     logging.basicConfig(level=logging.DEBUG)
 
+  @unittest.skip("https://crbug.com/755926")
   def test_load(self):
     params = ldbs._load_params_dict('chromium')
     self.assertIsInstance(params, dict)
