7a52f41eef3cb994d6cc629b8b83c3a015e019ec
Edward Lemur <ehmaldonado@chromium.org>
2017-09-08 13:15:43 +0200

    Opt out mbonadei@ from LUCI emulation because of SSL certificate problems on Windows trybots.
    Example of the issue:
    https://build.chromium.org/p/tryserver.chromium.win/builders/win_clang/builds/306041.
    
    I will opt in again later, I just want to run that trybot. :)
    
    TBR=iannucci@chromium.org
    Bug: 763305
    Change-Id: Id131364c9ed1b01f12980912a35430fb37e88075
    Reviewed-on: https://chromium-review.googlesource.com/657380
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    Commit-Queue: Edward Lesmes <ehmaldonado@chromium.org>

--- scripts/slave/remote_run.py
+++ scripts/slave/remote_run.py
@@ -92,7 +92,7 @@ _OPT_IN_USERS = set([
           'dnj', 'iannucci', 'hinoka', 'nodir', 'vadimsh', 'estaab', 'smut',
 
           # WebRTC Infrastructure Team Members
-          'kjellander', 'ehmaldonado', 'oprypin', 'mbonadei', 'phoglund',
+          'kjellander', 'ehmaldonado', 'oprypin', 'phoglund',
         ),
         ('chromium.org', 'google.com', 'webrtc.org')
       )
