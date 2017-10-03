9932198fc3dd7ad4e1d614501b3bcce49d92f75a
Vadim Shtayura <vadimsh@chromium.org>
2017-10-03 13:39:33 -0700

    Deploy 'authutil' into PATH on staging bots.
    R=iannucci@chromium.org
    BUG=770996
    
    Change-Id: I864240fe0d77b57c2ad1dec4adba206071de1417
    Reviewed-on: https://chromium-review.googlesource.com/699005
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/cipd_bootstrap_v2.py
+++ scripts/slave/cipd_bootstrap_v2.py
@@ -57,6 +57,11 @@ AUX_BINARY_PACKAGES = {
       cipd.CipdPackage(
           name='infra/git/${os}-${arch=386,amd64}',
           version='version:2.14.1.chromium11'),
+
+      # TODO(vadimsh): Enable after confirming staging works.
+      #cipd.CipdPackage(
+      #    name='infra/tools/authutil/${platform}',
+      #    version='git_revision:9c63809842a277ce10a86afd51b61c639a665d11')
     ),
 
     STAGING: (
@@ -77,6 +82,10 @@ AUX_BINARY_PACKAGES = {
       cipd.CipdPackage(
           name='infra/git/${os}-${arch=386,amd64}',
           version='version:2.14.1.chromium12'),
+
+      cipd.CipdPackage(
+          name='infra/tools/authutil/${platform}',
+          version='git_revision:9c63809842a277ce10a86afd51b61c639a665d11'),
     ),
 
     CANARY: (
@@ -102,6 +111,10 @@ AUX_BINARY_PACKAGES = {
       # don't have cross-compile support for Git packages yet.
       cipd.CipdPackage(
           name='infra/git/${os}-${arch=386,amd64}',
+          version='latest'),
+
+      cipd.CipdPackage(
+          name='infra/tools/authutil/${platform}',
           version='latest'),
     ),
 }
