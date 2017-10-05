b81ea22fbaac7926080ec98437d953ba0981ed51
bsheedy <bsheedy@chromium.org>
2017-10-05 13:12:48 -0700

    Add Nougat Phone Tester to chromium.android
    Adds a new "Nougat Phone Tester" builder to chromium.android and sets it
    to be triggered by "Android arm64 Builder (dbg)".
    
    The src-side change to add the testspecs is at
    https://chromium-review.googlesource.com/c/chromium/src/+/703478
    
    Bug: 739935
    Change-Id: Ied9f0b59059dc820ed81e9df649f06983316293a
    Reviewed-on: https://chromium-review.googlesource.com/703461
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Benjamin Pastene <bpastene@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android.py
@@ -318,6 +318,25 @@ SPEC = {
       },
     },
 
+    'Nougat Phone Tester': {
+      'enable_swarming': True,
+      'chromium_config': 'android',
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['android'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Debug',
+        'TARGET_PLATFORM': 'android',
+      },
+      'parent_buildername': 'Android arm64 Builder (dbg)',
+      'bot_type': 'tester',
+      'android_config': 'main_builder_mb',
+      'android_apply_config': ['use_devil_provision'],
+      'test_results_config': 'public_server',
+      'testing': {
+        'platform': 'linux',
+      },
+    },
+
     'Android N5X Swarm Builder': {
       'enable_swarming': True,
       'chromium_config': 'android',
