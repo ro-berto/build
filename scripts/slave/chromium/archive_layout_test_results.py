ro-berto/build

commit 9bd4cee3f80ec6896cf13d5b773be41b425422b4

Make GSUtilCopyFile work for cloud->cloud, fix typos in archive.
TBR=hinoka@chromium.org
BUG=776334

Change-Id: If1558c3703dda11a658b5fa45ae58eb6e99d86d9
Reviewed-on: https://chromium-review.googlesource.com/730855
Reviewed-by: Dirk Pranke <dpranke@chromium.org>
Commit-Queue: Dirk Pranke <dpranke@chromium.org>

scripts/slave/chromium/archive_layout_test_results.py
@@ -91,12 +91,12 @@ def archive_layout(args):
   gs_build_zip_file_path = gs_build_base + '/' + os.path.basename(zip_file)
   gs_build_dir_path = gs_build_base + '/' + results_dir_basename
   gs_build_last_change_path = (gs_build_base + '/' + results_dir_basename +
-          os.path.basename(last_change_file))
+          '/' + os.path.basename(last_change_file))
 
   gs_latest_base = '/'.join([args.gs_bucket, builder_name, 'results'])
   gs_latest_zip_file_path = gs_latest_base + '/' + os.path.basename(zip_file)
   gs_latest_last_change_path = (gs_latest_base + '/' + results_dir_basename +
-          os.path.basename(last_change_file))
+          '/' + os.path.basename(last_change_file))
 
   gs_acl = args.gs_acl
 
@@ -109,7 +109,7 @@ def archive_layout(args):
                              gs_acl=gs_acl,
                              cache_control=cache_control,
                              add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   start = time.time()
@@ -118,7 +118,7 @@ def archive_layout(args):
                             gs_acl=gs_acl,
                             cache_control=cache_control,
                             add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   start = time.time()
@@ -127,7 +127,7 @@ def archive_layout(args):
                              gs_acl=gs_acl,
                              cache_control=cache_control,
                              add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   # The 'latest' results need to be not cached at all (Cloud Storage defaults to
@@ -141,7 +141,7 @@ def archive_layout(args):
                              gs_acl=gs_acl,
                              cache_control=cache_control,
                              add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   start = time.time()
@@ -150,7 +150,7 @@ def archive_layout(args):
                             gs_acl=gs_acl,
                             cache_control=cache_control,
                             add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   start = time.time()

scripts/slave/slave_utils.py
@@ -370,7 +370,8 @@ def GSUtilCopyFile(filename, gs_base, subdir=None, mimetype=None, gs_acl=None,
     The status code returned from running the generated gsutil command.
   """
 
-  source = 'file://' + filename
+  if not filename.startswith('gs://') and not filename.startswith('file://'):
+      source = 'file://' + filename
   dest = gs_base
   if subdir:
     # HACK(nsylvain): We can't use normpath here because it will break the

commit 4fc12761a87e2e0729a529472a3cd4f9afb2497f

Fix typos in archive_script introduced in 9bd4cee.
TBR=hinoka@chromium.org
BUG=776334

Change-Id: I1f55fa1e8df151db3c4339ce5a030cd275ea0466
Reviewed-on: https://chromium-review.googlesource.com/731888
Reviewed-by: Dirk Pranke <dpranke@chromium.org>
Commit-Queue: Dirk Pranke <dpranke@chromium.org>

scripts/slave/chromium/archive_layout_test_results.py
@@ -159,7 +159,7 @@ def archive_layout(args):
                              gs_acl=gs_acl,
                              cache_control=cache_control,
                              add_quiet_flag=True)
-  print "took %.1d seconds" % (time.time() - start)
+  print "took %.1f seconds" % (time.time() - start)
   sys.stdout.flush()
 
   return 0

scripts/slave/slave_utils.py
@@ -370,8 +370,9 @@ def GSUtilCopyFile(filename, gs_base, subdir=None, mimetype=None, gs_acl=None,
     The status code returned from running the generated gsutil command.
   """
 
-  if not filename.startswith('gs://') and not filename.startswith('file://'):
-      source = 'file://' + filename
+  source = filename
+  if not (filename.startswith('gs://') or filename.startswith('file://')):
+    source = 'file://' + filename
   dest = gs_base
   if subdir:
     # HACK(nsylvain): We can't use normpath here because it will break the