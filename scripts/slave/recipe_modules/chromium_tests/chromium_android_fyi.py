dd56472d3949c97c2da73ca40c0e2d6fc23a55af
Zhiling Huang <hzl@chromium.org>
2017-10-30 16:40:59 -0700

    Set up an upstream Oreo FYI bot.
    Bug:763951
    Change-Id: I06de61db8231a62bb743153b8f8329a6ecd9be23
    Reviewed-on: https://chromium-review.googlesource.com/667945
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Zhiling Huang <hzl@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
@@ -332,6 +332,27 @@ SPEC = {
       },
     },
 
+    'Oreo Phone Tester': {
+      'enable_swarming': True,
+      'chromium_config': 'android',
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['android'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Debug',
+        'TARGET_PLATFORM': 'android',
+      },
+      'parent_buildername': 'Android arm64 Builder (dbg)',
+      'parent_mastername': 'chromium.android',
+      'bot_type': 'tester',
+      'android_config': 'arm64_builder_mb',
+      'android_apply_config': ['use_devil_provision'],
+      'test_results_config': 'public_server',
+      'test_results_config': 'public_server',
+      'testing': {
+        'platform': 'linux',
+      },
+    },
+
     'x64 Device Tester': {
       'chromium_config': 'android',
       'gclient_config': 'chromium',
