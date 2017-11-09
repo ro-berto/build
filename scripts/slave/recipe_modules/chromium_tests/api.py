2dc2478a4d4c29f016612c99f76b97843a50bdcc
John Abd-El-Malek <jam@chromium.org>
2017-11-09 15:41:14 -0800

    Fix test filter changes in testing/buildbot/filters causing retry-without-patch to not work.
    Bug:
    Change-Id: I6dbb43740a47fe949119dc0778fc8ddcaeb78e25
    Reviewed-on: https://chromium-review.googlesource.com/758799
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/api.py
+++ scripts/slave/recipe_modules/chromium_tests/api.py
@@ -7,6 +7,7 @@ import contextlib
 import copy
 import itertools
 import json
+import re
 
 from recipe_engine.types import freeze
 from recipe_engine import recipe_api
@@ -19,7 +20,8 @@ from . import steps
 # Paths which affect recipe config and behavior in a way that survives
 # deapplying user's patch.
 RECIPE_CONFIG_PATHS = [
-    'testing/buildbot',
+  'testing/buildbot/.*json$',
+  'testing/buildbot/.*pyl$',
 ]
 
 
@@ -746,11 +748,14 @@ class ChromiumTestsApi(recipe_api.RecipeApi):
 
     deapply_patch = True
     deapply_patch_reason = 'unknown reason'
-    for path in RECIPE_CONFIG_PATHS:
-      if any([f.startswith(path) for f in affected_files]):
-        deapply_patch = False
-        deapply_patch_reason = 'build config changes detected'
-        break
+
+    exclusion_regexs = [re.compile(path) for path in RECIPE_CONFIG_PATHS]
+    for f in affected_files:
+      for regex in exclusion_regexs:
+        if regex.match(f):
+          deapply_patch = False
+          deapply_patch_reason = 'build config changes detected'
+
     if disable_deapply_patch:
       deapply_patch = False
       deapply_patch_reason = 'disabled in recipes'
