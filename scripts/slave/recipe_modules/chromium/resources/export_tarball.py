ro-berto/build

commit 3eab95a2d59d2ed3d983079c2fb1a0e449b858f3

Make the export_tarball script generate gpu_lists_version.h
Since https://chromium-review.googlesource.com/c/chromium/src/+/728950,
gpu_lists_version.h is generated by lastchange.py, but the script that
generates source tarballs hadn't been updated accordingly.

Bug: 772177

Change-Id: If62b273567c83bef18e140904613b3971ecbadb2
Reviewed-on: https://chromium-review.googlesource.com/744009
Reviewed-by: Aaron Gable <agable@chromium.org>
Commit-Queue: Aaron Gable <agable@chromium.org>

scripts/slave/recipe_modules/chromium/resources/export_tarball.py
@@ -163,6 +163,13 @@ def main(argv):
                      cwd=options.src_dir) != 0:
     print 'Could not run build/util/lastchange.py to update skia_commit_hash.h.'
     return 1
+  if subprocess.call(['python', 'build/util/lastchange.py',
+                      '-m', 'GPU_LISTS_VERSION',
+                      '--revision-id-only',
+                      '--header', 'gpu/config/gpu_lists_version.h'],
+                     cwd=options.src_dir) != 0:
+    print 'Could not run build/util/lastchange.py to update gpu_lists_version.h.'
+    return 1
 
   output_fullname = args[0] + '.tar'
   output_basename = options.basename or os.path.basename(args[0])

commit b2c6eb1666c6393c4932ce3431f63333aa668eca

export_tarball: Only generate gpu_lists_version.h in M64+.
Commit 3eab95a2 ("Make the export_tarball script generate
gpu_lists_version.h") caused the script to run lastchange.py
--revision-id-only to generate gpu_lists_version.h.

However, the --revision-id-only option was introduced in M64, which led to
tarball export failures for M62 and M63. For example:
https://ci.chromium.org/buildbot/chromium.infra.cron/publish_tarball/8895

We now read chrome/VERSION and run that specific hook only if we're building
M64 or later.

Change-Id: If27fe8fb3919e0b75a45e14903b85304879b4fa3
Reviewed-on: https://chromium-review.googlesource.com/758409
Reviewed-by: Aaron Gable <agable@chromium.org>
Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>

scripts/slave/recipe_modules/chromium/resources/export_tarball.py
@@ -145,6 +145,14 @@ def main(argv):
     print 'Cannot find the src directory ' + options.src_dir
     return 1
 
+  # Read Chromium's version number so we can take different code paths
+  # depending on the milestone.
+  # For now, we are only interested in the milestone number.
+  version_major = int(subprocess.check_output(
+      ['python', 'build/util/version.py', '-f', 'chrome/VERSION',
+       '-t', '@MAJOR@'],
+      cwd=options.src_dir).strip())
+
   # These commands are from src/DEPS; please keep them in sync.
   if subprocess.call(['python', 'build/util/lastchange.py', '-o',
                       'build/util/LASTCHANGE'], cwd=options.src_dir) != 0:
@@ -163,13 +171,17 @@ def main(argv):
                      cwd=options.src_dir) != 0:
     print 'Could not run build/util/lastchange.py to update skia_commit_hash.h.'
     return 1
-  if subprocess.call(['python', 'build/util/lastchange.py',
-                      '-m', 'GPU_LISTS_VERSION',
-                      '--revision-id-only',
-                      '--header', 'gpu/config/gpu_lists_version.h'],
-                     cwd=options.src_dir) != 0:
-    print 'Could not run build/util/lastchange.py to update gpu_lists_version.h.'
-    return 1
+  # The --revision-id-only option was introduced in M64; so we need to skip
+  # this call when building earlier milestones.
+  if version_major >= 64:
+    if subprocess.call(['python', 'build/util/lastchange.py',
+                        '-m', 'GPU_LISTS_VERSION',
+                        '--revision-id-only',
+                        '--header', 'gpu/config/gpu_lists_version.h'],
+                       cwd=options.src_dir) != 0:
+      print('Could not run build/util/lastchange.py to update '
+            'gpu_lists_version.h.')
+      return 1
 
   output_fullname = args[0] + '.tar'
   output_basename = options.basename or os.path.basename(args[0])