6696beab89278ff4587e3d7589a0c9107f88beef
John Budorick <jbudorick@chromium.org>
2017-11-20 19:22:05 -0800

    Flip chromium.lkgr bots from lkcr to master.
    Bug: 787088
    Change-Id: I064abda67dbd6e3b86dbd3d5d18670d5e749c0b3
    Reviewed-on: https://chromium-review.googlesource.com/780373
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>

--- masters/master.chromium.lkgr/master_lkgr_cfg.py
+++ masters/master.chromium.lkgr/master_lkgr_cfg.py
@@ -31,22 +31,22 @@ def m_remote_run(recipe, **kwargs):
 defaults['category'] = '1lkcr'
 
 # Global scheduler
-S(name='chromium_lkcr', branch='lkcr')
+S(name='chromium_master', branch='master')
 
 ################################################################################
 ## Windows
 ################################################################################
 
 # ASan/Win bot.
-B('Win ASan Release', 'win_asan_rel', scheduler='chromium_lkcr')
+B('Win ASan Release', 'win_asan_rel', scheduler='chromium_master')
 F('win_asan_rel', m_remote_run('chromium'))
 
 # ASan/Win media bot.
-B('Win ASan Release Media', 'win_asan_rel_media', scheduler='chromium_lkcr')
+B('Win ASan Release Media', 'win_asan_rel_media', scheduler='chromium_master')
 F('win_asan_rel_media', m_remote_run('chromium'))
 
 # Win SyzyASan bot.
-B('Win SyzyASAN LKGR', 'win_syzyasan_lkgr', 'compile', 'chromium_lkcr')
+B('Win SyzyASAN LKGR', 'win_syzyasan_lkgr', 'compile', 'chromium_master')
 F('win_syzyasan_lkgr', m_remote_run('chromium', timeout=7200))
 
 ################################################################################
@@ -55,13 +55,13 @@ F('win_syzyasan_lkgr', m_remote_run('chromium', timeout=7200))
 
 asan_mac_gyp = 'asan=1 v8_enable_verify_heap=1 enable_ipc_fuzzer=1 '
 
-B('Mac ASAN Release', 'mac_asan_rel', 'compile', 'chromium_lkcr')
+B('Mac ASAN Release', 'mac_asan_rel', 'compile', 'chromium_master')
 F('mac_asan_rel', m_remote_run('chromium'))
 
-B('Mac ASAN Release Media', 'mac_asan_rel_media', 'compile', 'chromium_lkcr')
+B('Mac ASAN Release Media', 'mac_asan_rel_media', 'compile', 'chromium_master')
 F('mac_asan_rel_media', m_remote_run('chromium'))
 
-B('Mac ASAN Debug', 'mac_asan_dbg', 'compile', 'chromium_lkcr')
+B('Mac ASAN Debug', 'mac_asan_dbg', 'compile', 'chromium_master')
 F('mac_asan_dbg', m_remote_run('chromium'))
 
 ################################################################################
@@ -69,22 +69,22 @@ F('mac_asan_dbg', m_remote_run('chromium'))
 ################################################################################
 
 
-B('ASAN Release', 'linux_asan_rel', 'compile', 'chromium_lkcr')
+B('ASAN Release', 'linux_asan_rel', 'compile', 'chromium_master')
 F('linux_asan_rel', m_remote_run('chromium',
     # We started seeing 29 minute links, bug 360158
     timeout=2400))
 
 B('ASAN Release Media', 'linux_asan_rel_media',
-  'compile', 'chromium_lkcr')
+  'compile', 'chromium_master')
 F('linux_asan_rel_media', m_remote_run('chromium',
     # We started seeing 29 minute links, bug 360158
     timeout=2400))
 
-B('ASAN Debug', 'linux_asan_dbg', 'compile', 'chromium_lkcr')
+B('ASAN Debug', 'linux_asan_dbg', 'compile', 'chromium_master')
 F('linux_asan_dbg', m_remote_run('chromium'))
 
 B('ChromiumOS ASAN Release', 'linux_chromiumos_asan_rel', 'compile',
-  'chromium_lkcr')
+  'chromium_master')
 F('linux_chromiumos_asan_rel', m_remote_run('chromium',
     # We started seeing 29 minute links, bug 360158
     timeout=2400))
@@ -93,46 +93,46 @@ F('linux_chromiumos_asan_rel', m_remote_run('chromium',
 # https://sites.google.com/a/chromium.org/dev/developers/testing/addresssanitizer#TOC-Building-with-v8_target_arch-arm
 B('ASan Debug (32-bit x86 with V8-ARM)',
   'linux_asan_dbg_ia32_v8_arm',
-  'compile', 'chromium_lkcr')
+  'compile', 'chromium_master')
 F('linux_asan_dbg_ia32_v8_arm', m_remote_run('chromium'))
 
 B('ASan Release (32-bit x86 with V8-ARM)',
   'linux_asan_rel_ia32_v8_arm',
-  'compile', 'chromium_lkcr')
+  'compile', 'chromium_master')
 F('linux_asan_rel_ia32_v8_arm', m_remote_run('chromium'))
 
 B('ASan Release Media (32-bit x86 with V8-ARM)',
   'linux_asan_rel_media_ia32_v8_arm',
-  'compile', 'chromium_lkcr')
+  'compile', 'chromium_master')
 F('linux_asan_rel_media_ia32_v8_arm', m_remote_run('chromium'))
 
 # TSan bots.
-B('TSAN Release', 'linux_tsan_rel', 'compile', 'chromium_lkcr')
+B('TSAN Release', 'linux_tsan_rel', 'compile', 'chromium_master')
 F('linux_tsan_rel', m_remote_run('chromium'))
 
-B('TSAN Debug', 'linux_tsan_dbg', 'compile', 'chromium_lkcr')
+B('TSAN Debug', 'linux_tsan_dbg', 'compile', 'chromium_master')
 F('linux_tsan_dbg', m_remote_run('chromium'))
 
 # MSan bots.
 B('MSAN Release (no origins)', 'linux_msan_rel_no_origins', 'compile',
-  'chromium_lkcr')
+  'chromium_master')
 F('linux_msan_rel_no_origins', m_remote_run('chromium'))
 
 B('MSAN Release (chained origins)', 'linux_msan_rel_chained_origins', 'compile',
-  'chromium_lkcr')
+  'chromium_master')
 F('linux_msan_rel_chained_origins', m_remote_run('chromium'))
 
 # UBSan bots.
-B('UBSan Release', 'linux_ubsan_rel', 'compile', 'chromium_lkcr')
+B('UBSan Release', 'linux_ubsan_rel', 'compile', 'chromium_master')
 # UBSan builds very slowly with edge level coverage
 F('linux_ubsan_rel', m_remote_run('chromium', timeout=5400))
 
-B('UBSan vptr Release', 'linux_ubsan_vptr_rel', 'compile', 'chromium_lkcr')
+B('UBSan vptr Release', 'linux_ubsan_vptr_rel', 'compile', 'chromium_master')
 F('linux_ubsan_vptr_rel', m_remote_run('chromium'))
 
 def Update(_config, active_master, c):
   lkcr_poller = gitiles_poller.GitilesPoller(
       'https://chromium.googlesource.com/chromium/src',
-      branches=['lkcr'])
+      branches=['master'])
   c['change_source'].append(lkcr_poller)
   return helper.Update(c)
