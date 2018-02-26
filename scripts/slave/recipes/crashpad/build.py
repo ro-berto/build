89561a92d671068b9831497ac6c771685f74cbdd
Scott Graham <scottmg@chromium.org>
2018-02-26 17:38:48 +0000

    Revert "fuchsia: Disable running of tests until frequent flake is fixed"
    This reverts commit 63d2138e3b6aae344f4f87027946774820b94980.
    
    Reason for revert: Reverting this in favour of removing these bots from the CQ at https://chromium-review.googlesource.com/c/crashpad/crashpad/+/937809 -- need to do some tests of test running.
    
    Original change's description:
    > fuchsia: Disable running of tests until frequent flake is fixed
    > 
    > Bug: crashpad:219, crashpad:212
    > Change-Id: Id48fdd1a53cd9dc04f381b76b236c6582285713c
    > Reviewed-on: https://chromium-review.googlesource.com/935154
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Scott Graham <scottmg@chromium.org>
    
    TBR=scottmg@chromium.org,mark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: crashpad:219, crashpad:212
    Change-Id: I2daeaac1071a171cba0b3d1b2dd53584123b0085
    Reviewed-on: https://chromium-review.googlesource.com/937841
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>

--- scripts/slave/recipes/crashpad/build.py
+++ scripts/slave/recipes/crashpad/build.py
@@ -113,7 +113,7 @@ def RunSteps(api):
     path = api.path['checkout'].join('out', dirname)
 
   def run_tests(build_dir, env=None):
-    if is_fuchsia: # pragma: no cover
+    if is_fuchsia:
       # Start a QEMU instance.
       api.python('start qemu',
                  api.path['checkout'].join('build', 'run_fuchsia_qemu.py'),
@@ -125,7 +125,7 @@ def RunSteps(api):
                 args=[build_dir],
                 timeout=5*60)
 
-    if is_fuchsia: # pragma: no cover
+    if is_fuchsia:
       # Shut down the QEMU instance.
       api.python('stop qemu',
                  api.path['checkout'].join('build', 'run_fuchsia_qemu.py'),
@@ -138,12 +138,7 @@ def RunSteps(api):
     run_tests(x64_path, env={'CRASHPAD_TEST_32_BIT_OUTPUT':x86_path})
   else:
     api.step('compile with ninja', ['ninja', '-C', path])
-
-    # TODO(scottmg): Temporarily disable running on Fuchsia, as something about
-    # QEMU or networking got very flaky recently.
-    # https://crashpad.chromium.org/bug/219.
-    if not is_fuchsia:
-      run_tests(path)
+    run_tests(path)
 
   test_spec_path = api.path['checkout'].join('build', 'swarming_test_spec.pyl')
   if api.path.exists(test_spec_path):
