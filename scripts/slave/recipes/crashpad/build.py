d0c15994a87c0232ffc272cdea13030ac129e755
Scott Graham <scottmg@chromium.org>
2018-02-28 11:13:15 -0800

    Crashpad Linux: Set flag to link libstdcpp statically
    Set GN arg to take advantage of
    https://chromium-review.googlesource.com/c/chromium/mini_chromium/+/939731/.
    
    Bug: crashpad:220
    Change-Id: I6e3ac16fd2de65c459ef536fce7731f0f297d321
    Reviewed-on: https://chromium-review.googlesource.com/941882
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>

--- scripts/slave/recipes/crashpad/build.py
+++ scripts/slave/recipes/crashpad/build.py
@@ -86,6 +86,10 @@ def RunSteps(api):
       # There's no libcurl in the sysroot, stub out the HTTPTransport
       # implementation that relies on it.
       args += ' enable_http_transport_libcurl=false'
+
+      # The 14.04 systems do not include a compatible libstdc++ that can deal
+      # with -std=c++14, so force static linkage of libstdc++ from the sysroot.
+      args += ' link_libstdcpp_statically = true'
     with api.context(cwd=api.path['checkout']):
       api.step('generate build files', ['gn', 'gen', path, '--args=' + args])
   elif is_win:
