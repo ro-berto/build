8ab8262306374bddc1152f4d4d9148bb251a0f28
Michael Achenbach <machenbach@chromium.org>
2018-03-16 09:15:50 +0100

    [V8] Add triggered mac64 trybots
    TBR=sergiyb@chromium.org
    
    Bug: 822593
    Change-Id: Ia93f4c61c0abac2babe35103b59a64770053c828
    Reviewed-on: https://chromium-review.googlesource.com/966083
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>

--- scripts/slave/recipe_modules/v8/builders.py
+++ scripts/slave/recipe_modules/v8/builders.py
@@ -2888,6 +2888,74 @@ BUILDERS = {
         },
         'testing': {'platform': 'mac'},
       },
+      'v8_mac64_rel_ng': {
+        'chromium_apply_config': [
+          'v8_ninja', 'default_compiler', 'goma', 'mb'],
+        'v8_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_BITS': 64,
+        },
+        'bot_type': 'builder',
+        'enable_swarming': True,
+        'slim_swarming_builder': True,
+        'triggers': [
+          'v8_mac64_rel_ng_triggered',
+        ],
+        'testing': {'platform': 'mac'},
+      },
+      'v8_mac64_rel_ng_triggered': {
+        'v8_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_BITS': 64,
+        },
+        'bot_type': 'tester',
+        'parent_buildername': 'v8_mac64_rel_ng',
+        'enable_swarming': True,
+        'swarming_dimensions': {
+          'os': 'Mac-10.13',
+          'cpu': 'x86-64',
+        },
+        'tests': [
+          V8Testing,
+          Test262,
+          Mozilla,
+        ] + with_extra_variants([V8Testing]),
+        'testing': {'platform': 'linux'},
+      },
+      'v8_mac64_dbg_ng': {
+        'chromium_apply_config': [
+          'v8_ninja', 'default_compiler', 'goma', 'mb'],
+        'v8_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_BITS': 64,
+        },
+        'bot_type': 'builder',
+        'enable_swarming': True,
+        'slim_swarming_builder': True,
+        'triggers': [
+          'v8_mac64_dbg_ng_triggered',
+        ],
+        'testing': {'platform': 'mac'},
+      },
+      'v8_mac64_dbg_ng_triggered': {
+        'v8_config_kwargs': {
+          'BUILD_CONFIG': 'Debug',
+          'TARGET_BITS': 64,
+        },
+        'bot_type': 'tester',
+        'parent_buildername': 'v8_mac64_dbg_ng',
+        'enable_swarming': True,
+        'swarming_dimensions': {
+          'os': 'Mac-10.13',
+          'cpu': 'x86-64',
+        },
+        'tests': [
+          V8Testing(3),
+          Test262,
+          Mozilla,
+        ] + with_extra_variants([V8Testing(2)]),
+        'testing': {'platform': 'linux'},
+      },
       'v8_mac64_rel': {
         'chromium_apply_config': [
           'v8_ninja', 'default_compiler', 'goma', 'mb'],
