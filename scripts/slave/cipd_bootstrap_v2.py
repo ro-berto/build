1829dcbcf1e7f5b46fdbd72764db935174915524
Sergey Berezin <sergeyberezin@google.com>
2018-03-21 18:53:14 -0700

    Roll CIPD staging to git_revision:4d19637ec2c3d1efd8c6a1b05285118b786919e2
    Relevant changes:
      fe4e3046 [cipd] Treat io.ErrUnexpectedEOF as a corruption error.
      0d68f886 Proofread CIPD README.md and fix links
      e18d39eb [cipd] Rename cipd/client/cipd/common into cipd/common to use it from server.
      1db11af6 [cipd] Fix how the file size is presented in the progress output.
      3de4acd8 [cipd] Fix handling of file mode flags for packages with format "1".
      7ed42e5c CIPD: add option to preserve mtime and writable bit
    
    BUG=803158
    R=vadimsh@google.com
    
    Change-Id: I904bf43bdc381b3f459e49642684dd174b5ae8eb
    Reviewed-on: https://chromium-review.googlesource.com/974756
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Sergey Berezin <sergeyberezin@chromium.org>

--- scripts/slave/cipd_bootstrap_v2.py
+++ scripts/slave/cipd_bootstrap_v2.py
@@ -19,7 +19,7 @@ CLIENT_NAME = 'cipd' + infra_platform.exe_suffix()
 
 
 DEFAULT_CIPD_VERSION = 'git_revision:ae28364c740acff97ae118adcb2808b6cb5129c5'
-STAGING_CIPD_VERSION = 'git_revision:ae28364c740acff97ae118adcb2808b6cb5129c5'
+STAGING_CIPD_VERSION = 'git_revision:4d19637ec2c3d1efd8c6a1b05285118b786919e2'
 
 PROD = None
 STAGING = 'staging'
@@ -78,7 +78,7 @@ AUX_BINARY_PACKAGES = {
     STAGING: (
       cipd.CipdPackage(
           name='infra/tools/luci/vpython/${platform}',
-          version='git_revision:e1abc57be62d198b5c2f487bfb2fa2d2eb0e867c'),
+          version='git_revision:4d19637ec2c3d1efd8c6a1b05285118b786919e2'),
 
       cipd.CipdPackage(
           name='infra/tools/git/${platform}',
