c49b3feb5f733c5fbed99c2e9cd6289be7395d49
Misha Efimov <mef@chromium.org>
2018-03-23 18:16:28 -0400

    [Cronet] Set "use_crash_key_stubs=true" in official Cronet builds on Android.
    Bug: 811308
    Change-Id: I4b65d51b09c4cd7618944619566def64603d249d
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Reviewed-on: https://chromium-review.googlesource.com/978394
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_android/chromium_config.py
+++ scripts/slave/recipe_modules/chromium_android/chromium_config.py
@@ -75,6 +75,7 @@ def cronet_builder(c):
   c.gn_args.append('enable_reporting=false')
   c.gn_args.append('enable_websockets=false')
   c.gn_args.append('include_transport_security_state_preload_list=false')
+  c.gn_args.append('use_crash_key_stubs=true')
   c.gn_args.append('use_partition_alloc=false')
   c.gn_args.append('use_platform_icu_alternatives=true')
   c.compile_py.default_targets=['cronet_package',
