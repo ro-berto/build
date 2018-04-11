41b811891972e8aa6ffefe5ffe56c98f08de1755
John Budorick <jbudorick@chromium.org>
2018-04-11 10:16:35 -0700

    Gracefully handle None step results in isolated test collection, pt 3.
    TBR=hzl@chromium.org,martiniss@chromium.org
    
    Change-Id: I63eca4d46f8f48274f6e21368135271a8b2dbed6
    Reviewed-on: https://chromium-review.googlesource.com/1007634
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>

--- scripts/slave/README.recipes.md
+++ scripts/slave/README.recipes.md
@@ -2733,11 +2733,11 @@ This value can be changed per individual task.
 
 &emsp; **@default_user.setter**<br>&mdash; **def [default\_user](/scripts/slave/recipe_modules/swarming/api.py#331)(self, value):**
 
-&mdash; **def [get\_collect\_cmd\_args](/scripts/slave/recipe_modules/swarming/api.py#1205)(self, task):**
+&mdash; **def [get\_collect\_cmd\_args](/scripts/slave/recipe_modules/swarming/api.py#1208)(self, task):**
 
 SwarmingTask -> argument list for 'swarming.py' command.
 
-&mdash; **def [get\_step\_name](/scripts/slave/recipe_modules/swarming/api.py#1135)(self, prefix, task):**
+&mdash; **def [get\_step\_name](/scripts/slave/recipe_modules/swarming/api.py#1138)(self, prefix, task):**
 
 SwarmingTask -> name of a step of a waterfall.
 
--- scripts/slave/recipe_modules/swarming/api.py
+++ scripts/slave/recipe_modules/swarming/api.py
@@ -943,20 +943,21 @@ class SwarmingApi(recipe_api.RecipeApi):
       step_result = None
       try:
         step_result = self.m.step.active_result
-        step_result.presentation.step_text = text_for_task(task)
-        summary_json = step_result.swarming.summary
-        self._handle_summary_json(task, summary_json, step_result)
+        if step_result is not None:
+          step_result.presentation.step_text = text_for_task(task)
+          summary_json = step_result.swarming.summary
+          self._handle_summary_json(task, summary_json, step_result)
 
-        links = {}
-        if hasattr(step_result, 'json') and hasattr(step_result.json, 'output'):
-          links = step_result.json.output.get('links', {})
-        elif (hasattr(step_result, 'test_utils') and
-              hasattr(step_result.test_utils, 'gtest_results')):
-          links = step_result.test_utils.gtest_results.raw.get('links', {})
-        for k, v in links.iteritems():
-          step_result.presentation.links[k] = v
+          links = {}
+          if hasattr(step_result, 'json') and hasattr(step_result.json, 'output'):
+            links = step_result.json.output.get('links', {})
+          elif (hasattr(step_result, 'test_utils') and
+                hasattr(step_result.test_utils, 'gtest_results')):
+            links = step_result.test_utils.gtest_results.raw.get('links', {})
+          for k, v in links.iteritems():
+            step_result.presentation.links[k] = v
       except Exception as e:
-        if step_result:
+        if step_result is not None:
           step_result.presentation.logs['no_results_exc'] = [
             str(e), '\n', self.m.traceback.format_exc()]
 
@@ -1111,26 +1112,28 @@ class SwarmingApi(recipe_api.RecipeApi):
       try:
         step_result = self.m.step.active_result
 
-        outdir = filter_outdir(
-            self.m.json.dumps, step_result.raw_io.output_dir)
-        outdir_json = self.m.json.dumps(outdir, indent=2)
-        step_result.presentation.logs['outdir_json'] = outdir_json.splitlines()
+        if step_result is not None:
+          outdir = filter_outdir(
+              self.m.json.dumps, step_result.raw_io.output_dir)
+          outdir_json = self.m.json.dumps(outdir, indent=2)
+          step_result.presentation.logs['outdir_json'] = outdir_json.splitlines()
 
-        step_result.isolated_script_results = step_result.json.output
+          step_result.isolated_script_results = step_result.json.output
 
-        # Obtain perftest results if present
-        perftest_results, is_histogramset = \
-          self._merge_isolated_script_perftest_output_shards(task, step_result)
-        step_result.isolated_script_perf_results = {
-          'is_histogramset': is_histogramset,
-          'data': perftest_results
-        }
+          # Obtain perftest results if present
+          perftest_results, is_histogramset = \
+            self._merge_isolated_script_perftest_output_shards(task, step_result)
+          step_result.isolated_script_perf_results = {
+            'is_histogramset': is_histogramset,
+            'data': perftest_results
+          }
 
       except Exception as e:
-        self.m.step.active_result.presentation.logs[
-          'no_isolated_results_exc'] = [
-            str(e), '\n', self.m.traceback.format_exc()]
-        self.m.step.active_result.isolated_script_results = None
+        if self.m.step.active_result is not None:
+          self.m.step.active_result.presentation.logs[
+            'no_isolated_results_exc'] = [
+              str(e), '\n', self.m.traceback.format_exc()]
+          self.m.step.active_result.isolated_script_results = None
 
   def get_step_name(self, prefix, task):
     """SwarmingTask -> name of a step of a waterfall.
