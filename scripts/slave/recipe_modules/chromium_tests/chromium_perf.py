765660b2fe6ded363df3b8ab3a297dc884dbbaf0
Emily Hanley <eyaich@google.com>
2018-04-25 09:46:30 -0400

    Removing deprecated configurations from perf waterfall.
    Android Nexus 7v2 Perf
    Android Nexus6 Perf
    Win 8 Perf
    Win 7 ATI GPU Perf
    Win 7 x64 Perf
    
    Bug:836798
    Change-Id: Iae27a0eba5f7c9a818190df09987ba8aa2a675d3
    Reviewed-on: https://chromium-review.googlesource.com/1027992
    Reviewed-by: David Tu <dtu@chromium.org>
    Commit-Queue: Emily Hanley <eyaich@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
@@ -211,10 +211,6 @@ _AddIsolatedTestSpec('Android Nexus5X Perf', 'android-nexus5X', 'android',
                      parent_buildername='Android Compile Perf', target_bits=32)
 _AddIsolatedTestSpec('Android Nexus5 Perf', 'android-nexus5', 'android',
                      target_bits=32, parent_buildername='Android Compile Perf')
-_AddIsolatedTestSpec('Android Nexus6 Perf', 'android-nexus6', 'android',
-                     target_bits=32, parent_buildername='Android Compile Perf')
-_AddIsolatedTestSpec('Android Nexus7v2 Perf', 'android-nexus7v2', 'android',
-                     target_bits=32, parent_buildername='Android Compile Perf')
 _AddIsolatedTestSpec('Android One Perf', 'android-one', 'android',
                      target_bits=32, parent_buildername='Android Compile Perf',
                      use_private_swarming_server=True)
@@ -230,11 +226,8 @@ _AddIsolatedTestSpec('Android Nexus6 WebView Perf', 'android-webview-nexus6',
 
 _AddIsolatedTestSpec('Win 10 High-DPI Perf', 'win-high-dpi', 'win')
 _AddIsolatedTestSpec('Win 10 Perf', 'chromium-rel-win10', 'win')
-_AddIsolatedTestSpec('Win 8 Perf', 'chromium-rel-win8-dual', 'win')
 _AddIsolatedTestSpec('Win 7 Perf', 'chromium-rel-win7-dual', 'win',
                      target_bits=32)
-_AddIsolatedTestSpec('Win 7 x64 Perf', 'chromium-rel-win7-x64-dual', 'win')
-_AddIsolatedTestSpec('Win 7 ATI GPU Perf', 'chromium-rel-win7-gpu-ati', 'win')
 _AddIsolatedTestSpec('Win 7 Intel GPU Perf', 'chromium-rel-win7-gpu-intel',
                      'win')
 _AddIsolatedTestSpec('Win 7 Nvidia GPU Perf', 'chromium-rel-win7-gpu-nvidia',
