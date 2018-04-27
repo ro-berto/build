8b81e5e850677ca59ac1d425be84981c3fcdfc20
Fumitoshi Ukai <ukai@google.com>
2018-04-27 13:19:37 +0900

    add RBE staging bots
      release clobber
      debug clobber
      debug
    
    Bug: 835731
    Change-Id: Ie711f696ffd616f2d10224cb53fc743b4d9c66ec
    Reviewed-on: https://chromium-review.googlesource.com/1029568
    Reviewed-by: Shinya Kawanaka <shinyak@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_goma.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_goma.py
@@ -84,7 +84,7 @@ SPEC = {
        'platform': 'win',
      },
    },
-   'Chromium Linux Goma RBE Staging': {
+   'Chromium Linux Goma RBE Staging (clobber)': {
      'chromium_config': 'chromium',
      # use canary until client ready for RBE is released.
      # TODO(ukai): remove this. crbug.com/831046
@@ -92,6 +92,51 @@ SPEC = {
      'gclient_config': 'chromium',
      'chromium_config_kwargs': {
        'BUILD_CONFIG': 'Release',
+       'TARGET_BITS': 64,
+     },
+     'goma_staging': True,
+     'testing': {
+       'platform': 'linux',
+     },
+   },
+   'Chromium Linux Goma RBE Staging': {
+     'chromium_config': 'chromium',
+     # use canary until client ready for RBE is released.
+     # TODO(ukai): remove this. crbug.com/831046
+     'chromium_apply_config': ['goma_rbe', 'goma_canary', 'mb'],
+     'gclient_config': 'chromium',
+     'chromium_config_kwargs': {
+       'BUILD_CONFIG': 'Release',
+       'TARGET_BITS': 64,
+     },
+     'goma_staging': True,
+     'testing': {
+       'platform': 'linux',
+     },
+   },
+   'Chromium Linux Goma RBE Staging (dbg) (clobber)': {
+     'chromium_config': 'chromium',
+     # use canary until client ready for RBE is released.
+     # TODO(ukai): remove this. crbug.com/831046
+     'chromium_apply_config': ['goma_rbe', 'goma_canary', 'clobber', 'mb'],
+     'gclient_config': 'chromium',
+     'chromium_config_kwargs': {
+       'BUILD_CONFIG': 'Debug',
+       'TARGET_BITS': 64,
+     },
+     'goma_staging': True,
+     'testing': {
+       'platform': 'linux',
+     },
+   },
+   'Chromium Linux Goma RBE Staging (dbg)': {
+     'chromium_config': 'chromium',
+     # use canary until client ready for RBE is released.
+     # TODO(ukai): remove this. crbug.com/831046
+     'chromium_apply_config': ['goma_rbe', 'goma_canary', 'mb'],
+     'gclient_config': 'chromium',
+     'chromium_config_kwargs': {
+       'BUILD_CONFIG': 'Debug',
        'TARGET_BITS': 64,
      },
      'goma_staging': True,
