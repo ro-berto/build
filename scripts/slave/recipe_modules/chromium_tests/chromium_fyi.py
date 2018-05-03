0bf14a68fd07533d058ff816bf3d8f336edfbd4e
John Budorick <jbudorick@chromium.org>
2018-05-03 06:04:13 -0700

    chromium_tests staging bots: s/builder_type/bot_type/
    TBR=martiniss@chromium.org
    
    Bug: 838767
    Change-Id: I786f578b3a8d88e34c4f9398687a39cf7c96b212
    Reviewed-on: https://chromium-review.googlesource.com/1042265
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
@@ -959,9 +959,9 @@ SPEC['builders'].update([
     stock_config('linux-blink-heap-incremental-marking', config='Debug'),
     stock_config('linux-blink-heap-verification'),
     stock_config('linux-chromium-tests-staging-builder',
-                 builder_type='builder'),
+                 bot_type='builder'),
     stock_config('linux-chromium-tests-staging-tests',
-                 builder_type='tester',
+                 bot_type='tester',
                  parent_buildername='linux-chromium-tests-staging-builder'),
     stock_config('linux-gcc-rel'),
 ])
