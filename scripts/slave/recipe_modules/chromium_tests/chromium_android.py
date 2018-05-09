8e45615122dde5f53bc1f2d871ce88d5207569ab
John Budorick <jbudorick@chromium.org>
2018-05-09 09:06:25 -0700

    Add recipe config for android-kitkat-arm-rel.
    TBR=bpastene@chromium.org
    Bug: 731986,790619
    Change-Id: Ib42f5f751a64b31b2045949b4dbacd4ebecb68bc
    Reviewed-on: https://chromium-review.googlesource.com/1052369
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android.py
@@ -220,6 +220,27 @@ SPEC = {
       },
     },
 
+    'android-kitkat-arm-rel': {
+      'chromium_config': 'android',
+      'chromium_apply_config': [
+        'chrome_with_codecs',
+        'download_vr_test_apks',
+        'mb'
+      ],
+      'gclient_config': 'chromium',
+      'gclient_apply_config': ['android'],
+      'chromium_config_kwargs': {
+        'BUILD_CONFIG': 'Release',
+        'TARGET_BITS': 32,
+        'TARGET_PLATFORM': 'android',
+      },
+      'android_config': 'main_builder',
+      'bot_type': 'builder_tester',
+      'testing': {
+        'platform': 'linux',
+      },
+    },
+
     'Cast Android (dbg)' : {
       'chromium_config': 'android',
       'chromium_apply_config': ['chrome_with_codecs', 'mb'],
