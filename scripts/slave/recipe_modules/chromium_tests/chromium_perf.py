94c257daaa8cc1ac4cbbad2ff4e84b6993d36d7e
Emily Hanley <eyaich@google.com>
2018-05-10 11:27:12 -0400

    Removing Win 7 Intel GPU Perf from master/slaves
    Bug:804982
    Change-Id: If8de95b02ec5a1639a58d49dedc5f10335273da3
    Reviewed-on: https://chromium-review.googlesource.com/1053761
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Emily Hanley <eyaich@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
@@ -232,8 +232,6 @@ _AddIsolatedTestSpec('Win 10 Perf', 'chromium-rel-win10', 'win',
                       use_private_swarming_server=True)
 _AddIsolatedTestSpec('Win 7 Perf', 'chromium-rel-win7-dual', 'win',
                      target_bits=32)
-_AddIsolatedTestSpec('Win 7 Intel GPU Perf', 'chromium-rel-win7-gpu-intel',
-                     'win')
 _AddIsolatedTestSpec('Win 7 Nvidia GPU Perf', 'chromium-rel-win7-gpu-nvidia',
                      'win')
 
