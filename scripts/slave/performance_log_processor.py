eb6cd41984c0d6f7d1ee8d1222cb83d0516fa287
Shenghua Zhang <shenghuazhang@chromium.org>
2018-05-14 12:15:30 -0700

    [LUCI] Explicitly set perf_graph_list in performance_log_processor.py
    LUCI builder bot 'Headless Linux (dbg)' tests failed http://shortn/_x1fDdWjY82
    This is because performance_log_processor.py reads value perf_graph_list
    which is set in //build/site_config. Offline discussed with nodir@,
    performance_log_processor.py is the only place 1) where we read perf_graph_list
    2) that is still relevant on LUCI. This CL sets the value as an explicit param.
    
    Bug:790367
    Change-Id: I75f7f1ccf9538b628376e1e3124ece6e3049ff97
    Reviewed-on: https://chromium-review.googlesource.com/1058077
    Commit-Queue: Shenghua Zhang <shenghuazhang@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- scripts/slave/performance_log_processor.py
+++ scripts/slave/performance_log_processor.py
@@ -23,9 +23,6 @@ import re
 
 from common import chromium_utils
 
-# TODO(crbug.com/403564).
-import config
-
 # Status codes that can be returned by the evaluateCommand method.
 # From buildbot.status.builder.
 # See: http://docs.buildbot.net/current/developer/results.html
@@ -402,8 +399,9 @@ class GraphingLogProcessor(PerformanceLogProcessor):
   """
 
   # The file into which the GraphingLogProcessor will save a list of graph
-  # names for use by the JS doing the plotting.
-  GRAPH_LIST = config.Master.perf_graph_list
+  # names for use by the JS doing the plotting. Match the value of
+  # Master.perf_graph_list in //build/site_config/config_bootstrap.py.
+  GRAPH_LIST = 'graphs.dat'
 
   RESULTS_REGEX = re.compile(r'(?P<IMPORTANT>\*)?RESULT '
                              r'(?P<GRAPH>[^:]*): (?P<TRACE>[^=]*)= '
