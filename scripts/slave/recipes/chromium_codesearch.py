2c37a1cf0e405c936ec6ef2c7edbbac39936fa3d
Joey Scarr <jsca@chromium.org>
2018-05-15 03:53:22 +0000

    Revert "Start committing Android generated files to master."
    This reverts commit 4cdb24e3b222c702efc8b6a8f22aebdd256ca72b.
    
    Reason for revert: Turns out you can't have a platform name in the output directory. http://crbug.com/548283
    
    Original change's description:
    > Start committing Android generated files to master.
    > 
    > This will cause them to appear at https://cs.chromium.org/chromium/src/out/android/.
    > 
    > This also changes the out directory from 'chromium-android' to just
    > 'android'. It's cleaner.
    > 
    > Bug: 665761
    > Change-Id: Iba93124918f4ce9df69c79f5bfc40ed291210cfb
    > Reviewed-on: https://chromium-review.googlesource.com/1049410
    > Reviewed-by: Aaron Gable <agable@chromium.org>
    > Commit-Queue: Joey Scarr <jsca@chromium.org>
    
    TBR=agable@chromium.org,jsca@chromium.org
    
    Change-Id: I2f95cf6e51710d384378916e5ce30c307e2cae84
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 665761
    Reviewed-on: https://chromium-review.googlesource.com/1058947
    Reviewed-by: Joey Scarr <jsca@chromium.org>
    Commit-Queue: Joey Scarr <jsca@chromium.org>

--- scripts/slave/recipes/chromium_codesearch.py
+++ scripts/slave/recipes/chromium_codesearch.py
@@ -95,9 +95,9 @@ SPEC = freeze({
       'package_filename': 'chromium-android-src',
       'platform': 'android',
       'sync_generated_files': True,
-      'gen_repo_branch': 'master',
-      # Generated files will end up in out/android/Debug/gen.
-      'gen_repo_out_dir': 'android',
+      'gen_repo_branch': 'android',
+      # Generated files will end up in out/chromium-android/Debug/gen.
+      'gen_repo_out_dir': 'chromium-android',
       'corpus': 'chromium',
       'root': 'chromium-android',
     },
