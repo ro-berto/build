366cab97fe962e783c31dc7dc0e3034cd56cc2f8
Don Garrett <dgarrett@google.com>
2018-05-17 18:15:29 -0700

    chromiumos.tryserver: Shutting it down.
    This CL removes the configuration for the chromiumos.tryserver
    waterfall. The waterfall is out of production, and this can be
    submitted when ready.
    
    BUG=chromium:752342
    
    Change-Id: I39f854bf8027cc2662e1250d3e08f30efa907190
    Reviewed-on: https://chromium-review.googlesource.com/1003636
    Commit-Queue: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Elliott Friedman <friedman@chromium.org>

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -237,7 +237,6 @@ def main(argv):
       'master.chromium.webrtc.fyi': 'ChromiumWebRTCFYI',
       'master.chromium.win': 'ChromiumWin',
       'master.chromiumos': 'ChromiumOS',
-      'master.chromiumos.tryserver': 'ChromiumOSTryServer',
       'master.client.art': 'ART',
       'master.client.boringssl': 'Boringssl',
       'master.client.catapult': 'Catapult',
