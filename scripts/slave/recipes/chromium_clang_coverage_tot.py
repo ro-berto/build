d2c674f80fbcd7a2df45498a1869a2e51c758b1f
Yuke Liao <liaoyuke@chromium.org>
2018-05-21 11:02:22 -0700

    Infra: Establish test for coverage script on clang ToT waterfall.
    This CL adds recipes to run coverage script on clang ToT waterfall on
    both Mac and Linux platforms.
    
    Bug: 821840
    Change-Id: I18f8942234856c5619cad3e4a7458b8a22e38010
    Reviewed-on: https://chromium-review.googlesource.com/1064695
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>

---
+++ scripts/slave/recipes/chromium_clang_coverage_tot.py
@@ -0,0 +1,113 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from recipe_engine.types import freeze
+
+DEPS = [
+    'chromium',
+    'depot_tools/bot_update',
+    'recipe_engine/file',
+    'recipe_engine/json',
+    'recipe_engine/path',
+    'recipe_engine/platform',
+    'recipe_engine/properties',
+    'recipe_engine/python',
+    'recipe_engine/step'
+]
+
+BUILDERS = freeze({
+  'chromium.clang': {
+    'builders': {
+      'ToTMacCoverage': {
+        'chromium_config': 'chromium_clang',
+        'chromium_apply_config': ['clang_tot'],
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'mac',
+          'TARGET_BITS': 64,
+        },
+      },
+      'ToTLinuxCoverage': {
+        'chromium_config': 'chromium_clang',
+        'chromium_apply_config': ['clang_tot'],
+        'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_PLATFORM': 'linux',
+          'TARGET_BITS': 64,
+        },
+      },
+    },
+  },
+})
+
+# Sample targets that are used to test the coverage script against clang tot
+# coverage tools.
+SAMPLE_TARGETS = ['base_unittests', 'url_unittests']
+SAMPLE_FUZZER_TARGETS = ['pdfium_fuzzer']
+
+
+def RunSteps(api):
+  mastername = api.m.properties['mastername']
+  buildername, bot_config = api.chromium.configure_bot(BUILDERS, ['mb'])
+  api.bot_update.ensure_checkout(patch_root=bot_config.get('root_override'))
+
+  api.chromium.ensure_goma()
+  api.chromium.runhooks()
+  api.chromium.run_mb(mastername, buildername, use_goma=True)
+
+  coverage_script = 'coverage.py'
+  coverage_script_path = api.path['checkout'].join('tools', 'code_coverage',
+                                                   coverage_script)
+  output_dir_name = 'clang_tot_coverage_report'
+  output_dir_path = api.path['checkout'].join('out', output_dir_name)
+  cmd_args = []
+  cmd_args.extend(SAMPLE_TARGETS)
+
+  # TODO(crbug.com/790747): Test fuzzer targets on Mac when the bug is fixed.
+  if not api.platform.is_mac:
+    cmd_args.extend(SAMPLE_FUZZER_TARGETS)
+
+  for target in SAMPLE_TARGETS:
+    cmd_args.extend(['-c', api.path['checkout'].join('out', 'Release', target)])
+
+  # TODO(crbug.com/790747): Test fuzzer targets on Mac when the bug is fixed.
+  if not api.platform.is_mac:
+    for fuzzer_target in SAMPLE_FUZZER_TARGETS:
+      cmd_args.extend([
+          '-c',
+          '%s -runs=1' % api.path['checkout'].join('out', 'Release',
+                                                   fuzzer_target)
+      ])
+
+  cmd_args.extend(['-b', api.path['checkout'].join('out', 'Release')])
+  cmd_args.extend(['-o', output_dir_path])
+
+  coverage_tools_dir_path = api.path['checkout'].join(
+      'third_party', 'llvm-build', 'Release+Asserts', 'bin')
+  cmd_args.extend(['--coverage-tools-dir', coverage_tools_dir_path])
+
+  cmd_args.extend(['-v'])
+  api.python('run coverage script', coverage_script_path, cmd_args)
+
+  # TODO(crbug.com/790747): Test fuzzer targets on Mac when the bug is fixed.
+  executed_targets = SAMPLE_TARGETS if api.platform.is_mac else (
+      SAMPLE_TARGETS + SAMPLE_FUZZER_TARGETS)
+
+  # Following steps are added for debugging purpose.
+  for target in executed_targets:
+    log_file_name = '%s_output.log' % target
+    log_file_path = output_dir_path.join(api.platform.name, 'logs',
+                                         log_file_name)
+
+    log_content = api.file.read_text('read log output of %s' % target,
+                                     log_file_path)
+    api.step.active_result.presentation.logs[log_file_name] = log_content
+
+  summary_file_name = 'summary.json'
+  summary_file_path = output_dir_path.join(api.platform.name, summary_file_name)
+  api.json.read('read %s' % summary_file_name, summary_file_path)
+
+
+def GenTests(api):
+  for test in api.chromium.gen_tests_for_builders(BUILDERS):
+    yield test
