4f9c18d6b762d34e3dc29401e9f76ba6cc1ca8e2
Ryan Tseng <hinoka@google.com>
2018-05-30 13:56:26 -0700

    Remove chromium.lkgr
    This has been migrated to LUCI
    
    Bug: 840631
    Change-Id: I8ea640b2a8bfb08719590ab97482ed8ef65ebaaf
    Reviewed-on: https://chromium-review.googlesource.com/1079853
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Commit-Queue: Ryan Tseng <hinoka@chromium.org>

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -226,7 +226,6 @@ def main(argv):
       'master.chromium.infra': 'Infra',
       'master.chromium.infra.codesearch': 'InfraCodesearch',
       'master.chromium.infra.cron': 'InfraCron',
-      'master.chromium.lkgr': 'ChromiumLKGR',
       'master.chromium.memory': 'ChromiumMemory',
       'master.chromium.perf': 'ChromiumPerf',
       'master.chromium.perf.fyi': 'ChromiumPerfFyi',
