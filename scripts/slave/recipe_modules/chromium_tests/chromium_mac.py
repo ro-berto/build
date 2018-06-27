b3550b9f0d00c5dca2f4d07f4ef5f0f153801b3f
John Budorick <jbudorick@chromium.org>
2018-06-27 23:10:44 +0000

    Recipe configuration for mac-cocoa-rel.
    Bug: 855301
    Change-Id: Ic781592f993d2db63b3c9a027b2ab4fb4e96dc3b
    Reviewed-on: https://chromium-review.googlesource.com/1115809
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_mac.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_mac.py
@@ -13,6 +13,23 @@ SPEC = {
     'src_side_runtest_py': True,
   },
   'builders': {
+    'mac-cocoa-rel': {
+      'bot_type': 'tester',
+      'chromium_apply_config': [
+          'mb',
+          'ninja_confirm_noop',
+      ],
+      'chromium_config': 'chromium',
+      'chromium_config_kwargs': {
+          'BUILD_CONFIG': 'Release',
+          'TARGET_BITS': 64,
+      },
+      'gclient_config': 'chromium',
+      'parent_buildername': 'Mac Builder',
+      'testing': {
+          'platform': 'mac',
+      },
+    },
     'mac-jumbo-rel': {
       'chromium_config': 'chromium',
       'gclient_config': 'chromium',
