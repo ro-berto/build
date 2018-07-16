3ca64e74397fd04c31b3b43004071618ef3696b1
Vadim Shtayura <vadimsh@chromium.org>
2018-07-16 22:25:00 +0000

    [cipd] Update CIPD client to 2.1.0 (b9c4670197 -> 521b9b6477).
    The new version supports '$ParanoidMode CheckPresence' in ensure files.
    
    R=iannucci@chromium.org
    BUG=794764
    
    Change-Id: I79531b18e378a4b14ae2c45256319591b9a664c4
    Reviewed-on: https://chromium-review.googlesource.com/1138735
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- scripts/slave/cipd_bootstrap_v2.py
+++ scripts/slave/cipd_bootstrap_v2.py
@@ -18,8 +18,8 @@ from slave import cipd
 CLIENT_NAME = 'cipd' + infra_platform.exe_suffix()
 
 
-DEFAULT_CIPD_VERSION = 'git_revision:b9c4670197dcefd8762d6e509302acd3efc6e303'
-STAGING_CIPD_VERSION = 'git_revision:b9c4670197dcefd8762d6e509302acd3efc6e303'
+DEFAULT_CIPD_VERSION = 'git_revision:521b9b64770d3b43a221b1e2bd277687616adacf'
+STAGING_CIPD_VERSION = 'git_revision:521b9b64770d3b43a221b1e2bd277687616adacf'
 
 PROD = None
 STAGING = 'staging'
