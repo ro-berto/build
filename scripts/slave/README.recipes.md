20294d0ec9f33c119a279431b6a8c589cd20d50e
Corentin Wallez <cwallez@chromium.org>
2018-08-23 12:30:57 +0000

    Add initial recipe for Dawn
    It is heavily based on the ANGLE recipe.
    
    Test expectations were generated using
      ./scripts/slave/recipes.py -O depot_tools=$HOME/dev/depot_tools test train
    
    The local depot_tools modification had this CL applied:
      https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1183432
    
    BUG=chromium:875245
    
    Change-Id: I77e51b0e48138894fb88af37c300106ae5a43491
    Reviewed-on: https://chromium-review.googlesource.com/1183500
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- scripts/slave/README.recipes.md
+++ scripts/slave/README.recipes.md
@@ -224,6 +224,7 @@
   * [dart:examples/example](#recipes-dart_examples_example)
   * [dart:examples/example-checkout](#recipes-dart_examples_example-checkout)
   * [dart:examples/example-get_secret](#recipes-dart_examples_example-get_secret)
+  * [dawn](#recipes-dawn)
   * [disk:examples/full](#recipes-disk_examples_full)
   * [docker:examples/full](#recipes-docker_examples_full)
   * [emulator](#recipes-emulator)
@@ -4862,6 +4863,11 @@ Generates the sequence of steps that will be run by the slave.
 [DEPS](/scripts/slave/recipe_modules/dart/examples/example-get_secret.py#7): [dart](#recipe_modules-dart), [recipe\_engine/platform][recipe_engine/recipe_modules/platform]
 
 &mdash; **def [RunSteps](/scripts/slave/recipe_modules/dart/examples/example-get_secret.py#12)(api):**
+### *recipes* / [dawn](/scripts/slave/recipes/dawn.py)
+
+[DEPS](/scripts/slave/recipes/dawn.py#5): [goma](#recipe_modules-goma), [depot\_tools/bot\_update][depot_tools/recipe_modules/bot_update], [depot\_tools/depot\_tools][depot_tools/recipe_modules/depot_tools], [depot\_tools/gclient][depot_tools/recipe_modules/gclient], [depot\_tools/gsutil][depot_tools/recipe_modules/gsutil], [recipe\_engine/buildbucket][recipe_engine/recipe_modules/buildbucket], [recipe\_engine/context][recipe_engine/recipe_modules/context], [recipe\_engine/file][recipe_engine/recipe_modules/file], [recipe\_engine/json][recipe_engine/recipe_modules/json], [recipe\_engine/path][recipe_engine/recipe_modules/path], [recipe\_engine/platform][recipe_engine/recipe_modules/platform], [recipe\_engine/properties][recipe_engine/recipe_modules/properties], [recipe\_engine/python][recipe_engine/recipe_modules/python], [recipe\_engine/step][recipe_engine/recipe_modules/step], [recipe\_engine/time][recipe_engine/recipe_modules/time]
+
+&mdash; **def [RunSteps](/scripts/slave/recipes/dawn.py#101)(api, target_cpu, debug, clang):**
 ### *recipes* / [disk:examples/full](/scripts/slave/recipe_modules/disk/examples/full.py)
 
 [DEPS](/scripts/slave/recipe_modules/disk/examples/full.py#5): [disk](#recipe_modules-disk), [recipe\_engine/json][recipe_engine/recipe_modules/json], [recipe\_engine/platform][recipe_engine/recipe_modules/platform], [recipe\_engine/properties][recipe_engine/recipe_modules/properties]
---
+++ scripts/slave/recipes/dawn.py
@@ -0,0 +1,137 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+DEPS = [
+  'depot_tools/bot_update',
+  'depot_tools/depot_tools',
+  'depot_tools/gclient',
+  'goma',
+  'depot_tools/gsutil',
+  'recipe_engine/buildbucket',
+  'recipe_engine/context',
+  'recipe_engine/file',
+  'recipe_engine/json',
+  'recipe_engine/path',
+  'recipe_engine/platform',
+  'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/step',
+  'recipe_engine/time',
+]
+
+from recipe_engine.recipe_api import Property
+
+PROPERTIES = {
+  'target_cpu': Property(default=None, kind=str),
+  'debug': Property(default=False, kind=bool),
+  'clang': Property(default=None, kind=bool),
+}
+
+DAWN_REPO = "https://dawn.googlesource.com/dawn"
+
+
+def _checkout_steps(api):
+  # Checkout dawn and its dependencies (specified in DEPS) using gclient.
+  api.gclient.set_config('dawn')
+  api.gclient.c.got_revision_mapping['dawn'] = 'got_revision'
+  # Standalone developer dawn builds want the dawn checkout in the same
+  # directory the .gclient file is in.  Bots want it in a directory called
+  # 'dawn'.  To make both cases work, the dawn DEPS file pulls deps and runs
+  # hooks relative to the variable "root" which is set to . by default and then
+  # to 'dawn' on bots here:
+  api.gclient.c.solutions[0].custom_vars = {'dawn_root': 'dawn'}
+  api.bot_update.ensure_checkout()
+  api.gclient.runhooks()
+
+def _out_path(target_cpu, debug, clang):
+  out_dir = 'debug' if debug else 'release'
+  if clang:
+    out_dir += '_clang'
+  if target_cpu:
+    out_dir += '_' + target_cpu
+  return out_dir
+
+def _gn_gen_builds(api, target_cpu, debug, clang, out_dir):
+  """calls 'gn gen'"""
+  api.goma.ensure_goma()
+  gn_bool = {True: 'true', False: 'false'}
+  # Generate build files by GN.
+  checkout = api.path['checkout']
+  gn_cmd = api.depot_tools.gn_py_path
+
+  # Prepare the arguments to pass in.
+  args = [
+      'is_debug=%s' % gn_bool[debug],
+      'is_component_build=false',
+      'use_goma=true',
+      'goma_dir="%s"' % api.goma.goma_dir,
+  ]
+  if clang is not None:
+    args.append('is_clang=%s' % gn_bool[clang])
+
+  if target_cpu:
+    args.append('target_cpu="%s"' % target_cpu)
+
+  with api.context(cwd=checkout):
+    api.python('gn gen', gn_cmd,
+               ['--root=' + str(checkout), 'gen', '//out/' + out_dir,
+                '--args=' + ' '.join(args)])
+
+def _get_compiler_name(api, clang):
+  # Clang is used as the default compiler.
+  if clang or clang is None:
+    return 'clang'
+  # The non-Clang compiler is OS-dependent.
+  if api.platform.is_win:
+    return 'msvc'
+  else:
+    return 'gcc'
+
+def _build_steps(api, out_dir, clang):
+  debug_path = api.path['checkout'].join('out', out_dir)
+  ninja_cmd = [api.depot_tools.ninja_path, '-C', debug_path,
+               '-j', api.goma.recommended_goma_jobs]
+  api.goma.build_with_goma(
+      name='compile with ninja',
+      ninja_command=ninja_cmd,
+      ninja_log_outdir=debug_path,
+      ninja_log_compiler=_get_compiler_name(api, clang))
+
+def RunSteps(api, target_cpu, debug, clang):
+  _checkout_steps(api)
+  out_dir = _out_path(target_cpu, debug, clang)
+  _gn_gen_builds(api, target_cpu, debug, clang, out_dir)
+  _build_steps(api, out_dir, clang)
+
+def GenTests(api):
+  yield (
+      api.test('linux') +
+      api.platform('linux', 64) +
+      api.buildbucket.ci_build(project='dawn', builder='linux', git_repo=DAWN_REPO)
+  )
+  yield (
+      api.test('linux_gcc') +
+      api.platform('linux', 64) +
+      api.properties(clang=False) +
+      api.buildbucket.ci_build(project='dawn', builder='linux', git_repo=DAWN_REPO)
+  )
+  yield (
+      api.test('win') +
+      api.platform('win', 64) +
+      api.buildbucket.ci_build(project='dawn', builder='win', git_repo=DAWN_REPO)
+  )
+  yield (
+      api.test('win_clang') +
+      api.platform('win', 64) +
+      api.properties(clang=True) +
+      api.buildbucket.ci_build(project='dawn', builder='win', git_repo=DAWN_REPO)
+  )
+  yield (
+      api.test('win_rel_msvc_x86') +
+      api.platform('win', 64) +
+      api.properties(clang=False,
+                     debug=False,
+                     target_cpu='x86') +
+      api.buildbucket.ci_build(project='dawn', builder='win', git_repo=DAWN_REPO)
+  )
