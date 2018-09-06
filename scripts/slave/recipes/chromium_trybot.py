d484f426acd9beee73d9b0ebdceb2ed04fbe3c75
Nodir Turakulov <nodir@chromium.org>
2018-09-06 18:48:28 +0000

    Revert "Roll depot_tools 7b9e6b16..01f0c698"
    This reverts commit 078443c3dca3daea93b9f0699bc42c3a7d50dfca.
    
    Reason for revert: https://ci.chromium.org/p/chromium/builders/luci.chromium.try/win7_chromium_rel_ng/79921
    
    Original change's description:
    > Roll depot_tools 7b9e6b16..01f0c698
    > 
    > 01f0c69 bot_update: Specify the target branch for patch_refs. ehmaldonado@chromium.org
    > 
    > Recipe-Nontrivial-Roll: build_limited_scripts_slave
    > Recipe-Nontrivial-Roll: infra
    > Recipe-Nontrivial-Roll: release_scripts
    > Bug: 870279
    > Change-Id: I4472562be1832ab00bbe3240b7b0f3965fcbccdc
    > Reviewed-on: https://chromium-review.googlesource.com/1208364
    > Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    > Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    
    TBR=nodir@chromium.org,sergeyberezin@chromium.org,tandrii@chromium.org,martiniss@chromium.org,ehmaldonado@chromium.org,jbudorick@chromium.org
    
    Change-Id: Idc5f33fb4d4a1fc03b8092f3de6715e1369e7093
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 870279
    Reviewed-on: https://chromium-review.googlesource.com/1211422
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- scripts/slave/recipes/chromium_trybot.py
+++ scripts/slave/recipes/chromium_trybot.py
@@ -620,21 +620,16 @@ def GenTests(api):
     api.platform('linux', 64) +
     props(mastername='tryserver.chromium.linux',
           buildername='linux_chromium_rel_ng',
-          patch_project='chromium/src') +
+          gerrit_project='chromium/src') +
     suppress_analyze() +
     base_unittests_additional_compile_target() +
     api.step_data('compile (with patch)', retcode=1) +
     api.step_data(
-      'gerrit get change info for '
-      'https://chromium-review.googlesource.com/c/456789/12',
-      api.gerrit.get_one_change_response_data(
-          branch='experimental/feature',
-          patchset=12,
-          change=456789,
-          o_params=['DOWNLOAD_COMMANDS'])) + 
+      'gerrit get_patch_destination_branch',
+      api.gerrit.get_one_change_response_data(branch='experimental/feature'),
+     ) +
     api.post_process(
-        Filter('gerrit get change info for '
-               'https://chromium-review.googlesource.com/c/456789/12',
+        Filter('gerrit get_patch_destination_branch',
                'bot_update'))
   )
 
