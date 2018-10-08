943df9bcb6456aa6730eb017563ea51b7ee8bea7
Stephen Martinis <martiniss@chromium.org>
2018-10-08 23:12:23 +0000

    Fix pylint issues for /scripts/slave/recipe_modules/ndk
    This CL fixes pylint issues in this directory. It is
    part of a large refactor; pylint will be required for
    recipe modules once most of the existing issues have been
    fixed.
    
    This CL was uploaded by git cl split.
    
    Bug: 893240
    Change-Id: I02ca2a58229bf24303f9379cac4d3f3a8f141a29
    Reviewed-on: https://chromium-review.googlesource.com/c/1270018
    Auto-Submit: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Garrett Beaty <gbeaty@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>

--- scripts/slave/recipe_modules/ndk/chromite_config.py
+++ scripts/slave/recipe_modules/ndk/chromite_config.py
@@ -8,4 +8,5 @@ CONFIG_CTX = DEPS['chromite'].CONFIG_CTX
 
 @CONFIG_CTX(includes=['base'])
 def chromite_config(c):
-  c.cbb.config_repo = 'https://android.googlesource.com/toolchain/ndk_chromite_config'
+  c.cbb.config_repo = (
+      'https://android.googlesource.com/toolchain/ndk_chromite_config')
