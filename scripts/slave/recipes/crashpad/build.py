1decaa3966b3283d1c98b5d19b3896a924667903
Scott Graham <scottmg@chromium.org>
2018-10-19 16:52:40 +0000

    win: Attempt to make the x86 part of the build actually x86
    Bug: chromium:892712
    Change-Id: I9affffeabac50493a38ee588fdaa5246462489e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1289299
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Auto-Submit: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/recipes/crashpad/build.py
+++ scripts/slave/recipes/crashpad/build.py
@@ -70,130 +70,138 @@ def RunSteps(api, buildername, config, target_os, target_cpu):
   assert is_fuchsia or is_linux or is_mac or is_win
 
   @contextlib.contextmanager
-  def noop():
-    yield
-  sdk = noop()
-  if api.runtime.is_luci:
-    if is_win:
-      sdk = api.windows_sdk()
-    elif is_mac:
-      sdk = api.osx_sdk('mac')
+  def sdk(os, arch='x64'):
+    if api.runtime.is_luci:
+      if os == 'mac':
+        with api.osx_sdk('mac'):
+          yield
+      elif os == 'win':
+        assert arch in ('x86', 'x64')
+        with api.windows_sdk(target_arch=arch):
+          yield
+    else:
+      yield
 
-  with sdk:
-    api.gclient.c.target_os = {target_os}
+  api.gclient.c.target_os = {target_os}
 
-    is_debug = config == 'Debug'
+  is_debug = config == 'Debug'
 
+  if is_linux:
+    # The Linux build uses the system compiler by default, but bots do not
+    # have a clang installed by default, so use a local copy. Setting this
+    # variable causes DEPS to download a clang cipd package in runhooks. See
+    # also the setting of clang_path below.
+    api.gclient.c.solutions[0].custom_vars['pull_linux_clang'] = True
+
+  api.bot_update.ensure_checkout()
+
+  # buildbot sets 'clobber' to the empty string which is falsey, check with
+  # 'in'
+  if 'clobber' in api.properties:
+    api.file.rmtree('out', api.path['checkout'].join('out'))
+
+  with api.context(env=env):
+    api.gclient.runhooks()
+
+  dirname = config
+  if is_fuchsia or is_linux or is_mac:
+    gn = api.path['start_dir'].join(
+        'buildtools', 'mac' if is_mac else 'linux64', 'gn')
+    # Generic GN build.
+    path = api.path['checkout'].join('out', dirname)
+    if target_cpu is "":
+      target_cpu = 'x64'
+    args = (
+      'target_os="' + target_os + '" target_cpu="' + target_cpu + '"' +
+      ' is_debug=' + ('true' if is_debug else 'false'))
     if is_linux:
-      # The Linux build uses the system compiler by default, but bots do not
-      # have a clang installed by default, so use a local copy. Setting this
-      # variable causes DEPS to download a clang cipd package in runhooks. See
-      # also the setting of clang_path below.
-      api.gclient.c.solutions[0].custom_vars['pull_linux_clang'] = True
+      # Point at the local copy of clang and a sysroot that were downloaded by
+      # gclient runhooks.
+      args += ' clang_path="//third_party/linux/clang/linux-amd64"'
+      args += ' target_sysroot="//third_party/linux/sysroot"'
 
-    api.bot_update.ensure_checkout()
-
-    # buildbot sets 'clobber' to the empty string which is falsey, check with
-    # 'in'
-    if 'clobber' in api.properties:
-      api.file.rmtree('out', api.path['checkout'].join('out'))
-
-    with api.context(env=env):
-      api.gclient.runhooks()
-
-    dirname = config
-    if is_fuchsia or is_linux or is_mac:
-      gn = api.path['start_dir'].join(
-          'buildtools', 'mac' if is_mac else 'linux64', 'gn')
-      # Generic GN build.
-      path = api.path['checkout'].join('out', dirname)
-      if target_cpu is "":
-        target_cpu = 'x64'
-      args = (
-        'target_os="' + target_os + '" target_cpu="' + target_cpu + '"' +
-        ' is_debug=' + ('true' if is_debug else 'false'))
-      if is_linux:
-        # Point at the local copy of clang and a sysroot that were downloaded by
-        # gclient runhooks.
-        args += ' clang_path="//third_party/linux/clang/linux-amd64"'
-        args += ' target_sysroot="//third_party/linux/sysroot"'
-
-        # The 14.04 systems do not include a compatible libstdc++ that can deal
-        # with -std=c++14, so force static linkage of libstdc++ from the
-        # sysroot.
-        args += ' link_libstdcpp_statically = true'
-      with api.context(cwd=api.path['checkout']):
+      # The 14.04 systems do not include a compatible libstdc++ that can deal
+      # with -std=c++14, so force static linkage of libstdc++ from the
+      # sysroot.
+      args += ' link_libstdcpp_statically = true'
+    with api.context(cwd=api.path['checkout']):
+      with sdk(target_os):
         api.step('generate build files', [gn, 'gen', path, '--args=' + args])
-    elif is_win:
-      gn = api.path['start_dir'].join('buildtools', 'win', 'gn.exe')
-      # On Windows, we ought to test:
-      # a) x64 OS, x64 handler, x64 client
-      # b) x64 OS, x64 handler, x86 client
-      # c) x64 OS, x86 handler, x86 client
-      # d) x86 OS, x86 handler, x86 client
-      #
-      # d) used to be tested on _x86 bots, but they have been removed.
-      #
-      # Two build directories are generated, one for x86 and one for x64, which
-      # allows testing a), b), and c). As the bot time is dominated by machine
-      # setup and build time, we do not separate these onto separate slaves.
-      # Additionally, they're all on the same physical machine currently, so
-      # there's no upside in parallelism.
+  elif is_win:
+    gn = api.path['start_dir'].join('buildtools', 'win', 'gn.exe')
+    # On Windows, we ought to test:
+    # a) x64 OS, x64 handler, x64 client
+    # b) x64 OS, x64 handler, x86 client
+    # c) x64 OS, x86 handler, x86 client
+    # d) x86 OS, x86 handler, x86 client
+    #
+    # d) used to be tested on _x86 bots, but they have been removed.
+    #
+    # Two build directories are generated, one for x86 and one for x64, which
+    # allows testing a), b), and c). As the bot time is dominated by machine
+    # setup and build time, we do not separate these onto separate slaves.
+    # Additionally, they're all on the same physical machine currently, so
+    # there's no upside in parallelism.
 
-      x86_path = api.path['checkout'].join('out', dirname + '_x86')
-      x64_path = api.path['checkout'].join('out', dirname + '_x64')
-      args = 'target_os="win" is_debug=' + ('true' if is_debug else 'false')
-      with api.context(cwd=api.path['checkout']):
+    x86_path = api.path['checkout'].join('out', dirname + '_x86')
+    x64_path = api.path['checkout'].join('out', dirname + '_x64')
+    args = 'target_os="win" is_debug=' + ('true' if is_debug else 'false')
+    with api.context(cwd=api.path['checkout']):
+      with sdk(target_os, 'x86'):
         api.step('generate build files x86',
                  [gn, 'gen', x86_path, '--args=' + args + ' target_cpu="x86"'])
+      with sdk(target_os, 'x64'):
         api.step('generate build files x64',
                  [gn, 'gen', x64_path, '--args=' + args + ' target_cpu="x64"'])
 
-    def run_tests(build_dir, env=None):
-      if is_fuchsia:
-        # QEMU instances are very flaky. Disable test running for now. This used
-        # to (and should) do `build/run_fuchsia_qemu.py start` before, and `...
-        # stop` after calling run_tests.py.
-        # https://bugs.chromium.org/p/crashpad/issues/detail?id=219.
-        return
+  def run_tests(build_dir, env=None):
+    if is_fuchsia:
+      # QEMU instances are very flaky. Disable test running for now. This used
+      # to (and should) do `build/run_fuchsia_qemu.py start` before, and `...
+      # stop` after calling run_tests.py.
+      # https://bugs.chromium.org/p/crashpad/issues/detail?id=219.
+      return
 
-      with api.context(env=env):
-        api.python('run tests',
-                  api.path['checkout'].join('build', 'run_tests.py'),
-                  args=[build_dir],
-                  timeout=5*60)
+    with api.context(env=env):
+      api.python('run tests',
+                api.path['checkout'].join('build', 'run_tests.py'),
+                args=[build_dir],
+                timeout=5*60)
 
-    ninja = api.depot_tools.ninja_path
-    if is_win:
+  ninja = api.depot_tools.ninja_path
+  if is_win:
+    with sdk(target_os, 'x86'):
       api.step('compile with ninja x86', [ninja, '-C', x86_path])
+    with sdk(target_os, 'x64'):
       api.step('compile with ninja x64', [ninja, '-C', x64_path])
-      run_tests(x86_path)
-      run_tests(x64_path, env={'CRASHPAD_TEST_32_BIT_OUTPUT':x86_path})
-    else:
+    run_tests(x86_path)
+    run_tests(x64_path, env={'CRASHPAD_TEST_32_BIT_OUTPUT':x86_path})
+  else:
+    with sdk(target_os):
       api.step('compile with ninja', [ninja, '-C', path])
-      run_tests(path)
+    run_tests(path)
 
-    test_spec_path = api.path['checkout'].join(
-      'build', 'swarming_test_spec.pyl')
-    if api.path.exists(test_spec_path):
-      file_contents = api.file.read_text('read swarming_test_spec',
-          test_spec_path, test_data=FAKE_SWARMING_TEST_SPEC)
+  test_spec_path = api.path['checkout'].join(
+    'build', 'swarming_test_spec.pyl')
+  if api.path.exists(test_spec_path):
+    file_contents = api.file.read_text('read swarming_test_spec',
+        test_spec_path, test_data=FAKE_SWARMING_TEST_SPEC)
 
-      try:
-        swarming_test_spec = ast.literal_eval(file_contents)
-      except Exception: # pragma: no cover
-        # TODO(crbug.com/743139) figure out how to handle different kinds of
-        # errors cleanly.
-        api.step.fail()
-        return
+    try:
+      swarming_test_spec = ast.literal_eval(file_contents)
+    except Exception: # pragma: no cover
+      # TODO(crbug.com/743139) figure out how to handle different kinds of
+      # errors cleanly.
+      api.step.fail()
+      return
 
-      for spec in swarming_test_spec:
-        if buildername in spec['builders']:
-          api.python(
-              spec['step_name'],
-              api.path['checkout'].join('build', 'run_on_swarming.py'),
-              args=[api.path['checkout'].join(spec['isolate'][2:])] +
-                   spec['args'])
+    for spec in swarming_test_spec:
+      if buildername in spec['builders']:
+        api.python(
+            spec['step_name'],
+            api.path['checkout'].join('build', 'run_on_swarming.py'),
+            args=[api.path['checkout'].join(spec['isolate'][2:])] +
+                 spec['args'])
 
 
 def GenTests(api):
@@ -210,7 +218,6 @@ def GenTests(api):
       ('luci.crashpad_try_mac_rel', 'mac', ''),
       ('luci.crashpad_try_win_dbg', 'win', ''),
       ('crashpad_try_mac_rel', 'mac', ''),
-      ('crashpad_try_win_dbg', 'win', ''),
       ('crashpad_linux_debug', 'linux', ''),
       ('crashpad_fuchsia_x64_dbg', 'fuchsia', 'x64'),
       ('crashpad_fuchsia_arm64_rel', 'fuchsia', 'arm64'),
