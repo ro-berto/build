2cd4a8fd89522d45c27b29a82a09cfd311a210fc
Mirko Bonadei <mbonadei@chromium.org>
2018-10-23 07:18:43 +0000

    Bump Xcode version on ios_api_framework.
    The compiler configuration is assuming Xcode 10.0+ [1] while this trybot
    was still using Xcode 9.
    
    This is blocking the Chromium roll into WebRTC.
    
    [1] - https://chromium-review.googlesource.com/c/chromium/src/+/1290572
    
    Bug: None
    Change-Id: I1f1785a3cd4288fcdcaba42b020883178fcf0a86
    Reviewed-on: https://chromium-review.googlesource.com/c/1293010
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>

--- scripts/slave/recipes/webrtc/ios_api_framework.py
+++ scripts/slave/recipes/webrtc/ios_api_framework.py
@@ -33,7 +33,7 @@ def RunSteps(api):
   api.goma.start()
   build_exit_status = 1
 
-  api.ios.ensure_xcode("9C40b")
+  api.ios.ensure_xcode("10l232m")
 
   try:
     build_script = api.path['checkout'].join('tools_webrtc', 'ios',
