f9fac0cf41172642fb5ca1808a9ba35d53dee1e6
Mathieu Binette <mbinette@google.com>
2018-11-08 00:44:09 +0000

    Fix CELab's gsutil.upload call
    Addressed comments from:
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1321891
    
    Added branch for bin directory to upload to prepare for Windows builders.
    
    Changed build_number to build.id.
    
    Tested with LED:
    https://ci.chromium.org/swarming/task/4109693186395e10?server=chromium-swarm.appspot.com
    
    Change-Id: I2333166024fa7901d407d4bdd011503be3af52b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1324752
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Commit-Queue: Mathieu Binette <mbinette@google.com>

--- scripts/slave/recipes/celab.py
+++ scripts/slave/recipes/celab.py
@@ -10,6 +10,7 @@ DEPS = [
   'recipe_engine/context',
   'recipe_engine/file',
   'recipe_engine/path',
+  'recipe_engine/platform',
   'recipe_engine/python',
   'recipe_engine/time',
 ]
@@ -19,8 +20,17 @@ from recipe_engine.recipe_api import Property
 CELAB_REPO = "https://chromium.googlesource.com/enterprise/cel"
 
 
+def _get_bin_directory(api, checkout):
+  bin_dir = checkout.join('out')
+  if api.platform.is_linux:
+    bin_dir = bin_dir.join('linux_amd64', 'bin')
+  elif api.platform.is_win:
+    bin_dir = bin_dir.join('windows_amd64', 'bin')
+  return bin_dir
+
+
 def RunSteps(api):
-  # 1. Checkout the CELab repo
+  # Checkout the CELab repo
   go_root = api.path['start_dir'].join('go')
   src_root = go_root.join('src', "chromium.googlesource.com", "enterprise")
   api.file.ensure_directory('init src_root if not exists', src_root)
@@ -31,31 +41,31 @@ def RunSteps(api):
     api.gclient.runhooks()
   checkout = api.path['checkout']
 
-  # 2. Install Go & Protoc
-  bootstrap_script = str(checkout.join('infra', 'bootstrap.py'))
+  # Install Go & Protoc
+  bootstrap_script = checkout.join('infra', 'bootstrap.py')
   bootstrap_root = api.path['start_dir'].join("bootstrap")
-  api.python('celab bootstrap', bootstrap_script, [str(bootstrap_root)])
+  api.python('celab bootstrap', bootstrap_script, [bootstrap_root])
   add_paths = [
     go_root.join('bin'),
     bootstrap_root.join('golang', 'go', 'bin'),
     bootstrap_root.join('protoc', 'bin'),
   ]
 
-  # 3. Build CELab
+  # Build CELab
   goenv = {"GOPATH": go_root}
   with api.context(cwd=checkout, env=goenv, env_suffixes={'PATH': add_paths}):
     api.python('install deps', 'build.py', ['deps', '--install', '--verbose'])
     api.python('build', 'build.py', ['build', '--verbose'])
 
-  # 4. Upload binaries for CI builds
+  # Upload binaries for CI builds
   if api.buildbucket.build.builder.bucket == 'ci':
     today = api.time.utcnow().date()
     gs_dest = '%s/%s/%s' % (
       api.buildbucket.builder_name,
       today.strftime('%Y/%m/%d'),
-      api.buildbucket.build.number)
+      api.buildbucket.build.id)
     api.gsutil.upload(
-      source=checkout.join("out", 'linux_amd64', 'bin', '**'),
+      source=_get_bin_directory(api, checkout).join('**'),
       bucket='celab',
       dest=gs_dest,
       name='upload CELab binaries',
@@ -68,6 +78,12 @@ def GenTests(api):
       api.buildbucket.try_build(project='celab', bucket='try', git_repo=CELAB_REPO)
   )
   yield (
-      api.test('basic_ci') +
+      api.test('basic_ci_linux') +
+      api.platform('linux', 64) +
+      api.buildbucket.ci_build(project='celab', bucket='ci', git_repo=CELAB_REPO)
+  )
+  yield (
+      api.test('basic_ci_windows') +
+      api.platform('win', 64) +
       api.buildbucket.ci_build(project='celab', bucket='ci', git_repo=CELAB_REPO)
   )
