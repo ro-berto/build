2a7418a0063dcf26e6eff813abd81bdc5ae696a6
Yuke Liao <liaoyuke@chromium.org>
2018-11-08 20:03:04 +0000

    [code coverage] Implement rebasing git diff
    This CL implements the functionality to calculate the line number
    mapping of added lines by a patch given git diffs reltive to two
    different revisions.
    
    Bug: 902404
    Change-Id: Ia40cd8d7ae78f0cd60e5b8fe9a6b1b7a590fbc20
    Reviewed-on: https://chromium-review.googlesource.com/c/1324751
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Shuotao Gao <stgao@chromium.org>

---
+++ scripts/slave/recipe_modules/clang_coverage/unittests/rebase_git_diff_test.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env vpython
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import unittest
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0,
+                os.path.abspath(os.path.join(THIS_DIR, os.pardir, 'resources')))
+
+import rebase_git_diff
+
+
+class RebaseGitDiffTest(unittest.TestCase):
+
+  # This test tests that the scenario that only one file is changed, and the
+  # changed lines are all within the same diff section.
+  def test_one_file_one_diff_section(self):
+    gerrit_diff = ('diff --git a/path/test.txt b/path/test.txt\n'
+                   'index 0719398930..4a2b716881 100644\n'
+                   '--- a/path/test.txt\n'
+                   '+++ b/path/test.txt\n'
+                   '@@ -10,2 +10,3 @@\n'
+                   ' Line 10\n'
+                   '-Line 11\n'
+                   '+A different line 11\n'
+                   '+A newly added line 12\n')
+
+    # The difference between |local_diff| and |gerrit_diff| is that some other
+    # CL added 5 lines before Line 10.
+    local_diff = ('diff --git a/path/test.txt b/path/test.txt\n'
+                  'index 0719398930..4a2b716881 100644\n'
+                  '--- a/path/test.txt\n'
+                  '+++ b/path/test.txt\n'
+                  '@@ -15,2 +15,3 @@\n'
+                  ' Line 10\n'
+                  '-Line 11\n'
+                  '+A different line 11\n'
+                  '+A newly added line 12\n')
+
+    expected_mapping = {
+        'path/test.txt': {
+            16: (11, 'A different line 11'),
+            17: (12, 'A newly added line 12')
+        }
+    }
+
+    mapping = rebase_git_diff.generate_diff_mapping(
+        local_diff.split('\n'), gerrit_diff.split('\n'))
+    self.assertEqual(expected_mapping, mapping)
+
+  def test_one_file_multiple_sections(self):
+    gerrit_diff = ('diff --git a/path/test.txt b/path/test.txt\n'
+                   'index 0719398930..4a2b716881 100644\n'
+                   '--- a/path/test.txt\n'
+                   '+++ b/path/test.txt\n'
+                   '@@ -10,2 +10,3 @@\n'
+                   ' Line 10\n'
+                   '-Line 11\n'
+                   '+A different line 11\n'
+                   '+A newly added line 12\n'
+                   '@@ -20,1 +21,1 @@\n'
+                   '-Line 20\n'
+                   '+A different line 20\n')
+
+    # The difference between |local_diff| and |gerrit_diff| is that some other
+    # CL added 5 lines before Line 10.
+    local_diff = ('diff --git a/path/test.txt b/path/test.txt\n'
+                  'index 0719398930..4a2b716881 100644\n'
+                  '--- a/path/test.txt\n'
+                  '+++ b/path/test.txt\n'
+                  '@@ -15,2 +15,3 @@\n'
+                  ' Line 10\n'
+                  '-Line 11\n'
+                  '+A different line 11\n'
+                  '+A newly added line 12\n'
+                  '@@ -25,1 +26,1 @@\n'
+                  '-Line 20\n'
+                  '+A different line 20\n')
+
+    expected_mapping = {
+        'path/test.txt': {
+            16: (11, 'A different line 11'),
+            17: (12, 'A newly added line 12'),
+            26: (21, 'A different line 20')
+        }
+    }
+
+    mapping = rebase_git_diff.generate_diff_mapping(
+        local_diff.split('\n'), gerrit_diff.split('\n'))
+    self.assertEqual(expected_mapping, mapping)
+
+  def test_multiple_files_multiple_sections(self):
+    gerrit_diff = ('diff --git a/path/test1.txt b/path/test1.txt\n'
+                   'index 0719398930..4a2b716881 100644\n'
+                   '--- a/path/test1.txt\n'
+                   '+++ b/path/test1.txt\n'
+                   '@@ -10,2 +10,3 @@\n'
+                   ' Line 10\n'
+                   '-Line 11\n'
+                   '+A different line 11\n'
+                   '+A newly added line 12\n'
+                   'diff --git a/path/test2.txt b/path/test2.txt\n'
+                   'index 0719398930..4a2b716881 100644\n'
+                   '--- a/path/test2.txt\n'
+                   '+++ b/path/test2.txt\n'
+                   '@@ -10,2 +10,3 @@\n'
+                   ' Line 10\n'
+                   '-Line 11\n'
+                   '+A different line 11\n'
+                   '+A newly added line 12\n'
+                   '@@ -20,1 +21,1 @@\n'
+                   '-Line 20\n'
+                   '+A different line 20\n')
+
+    # The difference between |local_diff| and |gerrit_diff| is that some other
+    # CL added 5 lines before Line 10 in both files.
+    local_diff = ('diff --git a/path/test1.txt b/path/test1.txt\n'
+                  'index 0719398930..4a2b716881 100644\n'
+                  '--- a/path/test1.txt\n'
+                  '+++ b/path/test1.txt\n'
+                  '@@ -15,2 +15,3 @@\n'
+                  ' Line 10\n'
+                  '-Line 11\n'
+                  '+A different line 11\n'
+                  '+A newly added line 12\n'
+                  'diff --git a/path/test2.txt b/path/test2.txt\n'
+                  'index 0719398930..4a2b716881 100644\n'
+                  '--- a/path/test2.txt\n'
+                  '+++ b/path/test2.txt\n'
+                  '@@ -15,2 +15,3 @@\n'
+                  ' Line 10\n'
+                  '-Line 11\n'
+                  '+A different line 11\n'
+                  '+A newly added line 12\n'
+                  '@@ -25,1 +26,1 @@\n'
+                  '-Line 20\n'
+                  '+A different line 20\n')
+
+    expected_mapping = {
+        'path/test1.txt': {
+            16: (11, 'A different line 11'),
+            17: (12, 'A newly added line 12')
+        },
+        'path/test2.txt': {
+            16: (11, 'A different line 11'),
+            17: (12, 'A newly added line 12'),
+            26: (21, 'A different line 20')
+        }
+    }
+
+    mapping = rebase_git_diff.generate_diff_mapping(
+        local_diff.split('\n'), gerrit_diff.split('\n'))
+
+    self.assertEqual(expected_mapping, mapping)
+
+
+if __name__ == '__main__':
+  unittest.main()
