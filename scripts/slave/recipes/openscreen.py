48f0fa90f461047f3485874636c93de2fe389b94
Yuri Wiitala <miu@chromium.org>
2018-11-20 01:30:32 +0000

    Fix openscreen recipes to allow Mac builds.
    Adds use of Mac OSX SDK "ensure installed" context. Also, uses GN from
    depot_tools rather than the manual "install-build-tools" step. Also,
    ensure debug flag is plumbed-through to gn args.
    
    TBR=iannucci@chromium.org
    Change-Id: I43a29d7e0335a679693bb01da23b7611089d472a
    Reviewed-on: https://chromium-review.googlesource.com/c/1337708
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- scripts/slave/recipes/openscreen.py
+++ scripts/slave/recipes/openscreen.py
@@ -7,6 +7,7 @@
 DEPS = [
   'depot_tools/depot_tools',
   'depot_tools/git',
+  'depot_tools/osx_sdk',
   'goma',
   'recipe_engine/context',
   'recipe_engine/path',
@@ -26,21 +27,25 @@ def RunSteps(api):
   openscreen_root = build_root.join('openscreen')
   repository = api.properties['repository']
   ref = api.properties.get('patch_ref', None)
+  is_debug = api.properties.get('debug', False)
   api.git.checkout(
       repository, dir_path=openscreen_root, ref=ref, recursive=True)
   api.goma.ensure_goma()
   checkout_path = api.path['checkout']
   output_path = checkout_path.join('out', BUILD_CONFIG)
   with api.context(cwd=checkout_path):
-    api.step('install build tools',
-             [checkout_path.join('tools', 'install-build-tools.sh')])
-    api.step('gn gen', [checkout_path.join('gn'), 'gen', output_path])
-    ninja_cmd = [api.depot_tools.ninja_path, '-C', output_path,
-                 '-j', api.goma.recommended_goma_jobs] + BUILD_TARGET
-    api.goma.build_with_goma(
-        name='compile',
-        ninja_command=ninja_cmd,
-        ninja_log_outdir=output_path)
+    api.step('gn gen',
+             [api.depot_tools.gn_py_path, 'gen', output_path,
+              '--args=is_debug="' + str(is_debug).lower() + '"'])
+    # NOTE: The following just runs Ninja without setting up the Mac toolchain
+    # if this is being run on a non-Mac platform.
+    with api.osx_sdk('mac'):
+      ninja_cmd = [api.depot_tools.ninja_path, '-C', output_path,
+                   '-j', api.goma.recommended_goma_jobs] + BUILD_TARGET
+      api.goma.build_with_goma(
+          name='compile',
+          ninja_command=ninja_cmd,
+          ninja_log_outdir=output_path)
     api.step('Run unit tests', [output_path.join('unittests')])
 
 
@@ -50,6 +55,16 @@ def GenTests(api):
       api.platform('linux', 64) +
       api.properties(
           repository=OPENSCREEN_REPO,
-          patch_ref='refs/changes/123/456/1'
+          patch_ref='refs/changes/123/456/1',
+          debug=True
+      )
+  )
+  yield (
+      api.test('mac_debug') +
+      api.platform('mac', 64) +
+      api.properties(
+          repository=OPENSCREEN_REPO,
+          patch_ref='refs/changes/123/456/1',
+          debug=True
       )
   )
