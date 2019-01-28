b295f0b4042a2f46dd27be4309b4fa5c77dad444
Takuto Ikuta <tikuta@chromium.org>
2019-01-28 08:24:56 +0000

    [chromium] enable ninja confirm noop by default
    This CL switches to do noop check by default.
    
    But I blacklisted 'Mac ASAN Debug builder' that fails the check frequently.
    https://console.cloud.google.com/bigquery?project=cr-buildbucket&j=bquxjob_57647d44_16892e53358&page=queryresults
    
    I will remove ninja_confirm_noop config in later patch.
    
    Bug: 914264, 921902
    Change-Id: Ic5cd8c73469b1bf9ee590d428e6a49964b4f5a53
    Reviewed-on: https://chromium-review.googlesource.com/c/1436424
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_memory.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_memory.py
@@ -157,7 +157,11 @@ SPEC = {
         'BUILD_CONFIG': 'Release',
         'TARGET_BITS': 64,
       },
-      'chromium_apply_config': ['mb'],
+      'chromium_apply_config': [
+          'mb',
+          # TODO(tiktua): remove this (https://crbug.com/921902)
+          'ninja_confirm_noop_warn',
+      ],
       'bot_type': 'builder',
       'testing': {'platform': 'mac'},
       'checkout_dir': 'mac',
@@ -169,7 +173,11 @@ SPEC = {
         'BUILD_CONFIG': 'Release',
         'TARGET_BITS': 64,
       },
-      'chromium_apply_config': ['mb'],
+      'chromium_apply_config': [
+          'mb',
+          # TODO(tiktua): remove this (https://crbug.com/921902)
+          'ninja_confirm_noop_warn',
+      ],
       'bot_type': 'tester',
       'parent_buildername': 'Mac ASan 64 Builder',
       'testing': {'platform': 'mac'},
