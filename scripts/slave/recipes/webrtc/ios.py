b3295a8d7b807337122afce25b49c167d7e5af95
Oleh Prypin <oprypin@webrtc.org>
2019-02-05 15:54:44 +0000

    WebRTC: Generate iOS expectations from real files
    The explicitly added test configs are copied from
    https://webrtc.googlesource.com/src/+/e54287a4aeb21ec8b6b3f084353c9aa9078cd1a8/tools_webrtc/ios/
    -- they are temporary and will be removed right after.
    This is just to get a diff in expectations on the next CL
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1443196
    
    Bug: webrtc:10253
    Change-Id: I6b795a736a2f04b37f15b16b64fbf0ad195a72ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1443195
    Reviewed-by: Artem Titarenko <artit@chromium.org>
    Commit-Queue: Oleh Prypin <oprypin@chromium.org>

--- scripts/slave/recipes/webrtc/ios.py
+++ scripts/slave/recipes/webrtc/ios.py
@@ -9,12 +9,317 @@ DEPS = [
   'depot_tools/gclient',
   'ios',
   'recipe_engine/buildbucket',
+  'recipe_engine/json',
   'recipe_engine/path',
   'recipe_engine/platform',
   'recipe_engine/properties',
   'recipe_engine/raw_io',
+  'recipe_engine/runtime',
   'webrtc'
 ]
+
+
+_WEBRTC_CONFIGS = {
+  'luci_webrtc_ci_ios32_debug': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'ios_enable_code_signing=false',
+      'is_debug=true',
+      'target_cpu="arm"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_ci_ios32_release': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'dcheck_always_on=true',
+      'ios_enable_code_signing=false',
+      'is_debug=false',
+      'target_cpu="arm"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Release',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_ci_ios64_debug': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'ios_enable_code_signing=false',
+      'is_debug=true',
+      'target_cpu="arm64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_ci_ios64_release': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'dcheck_always_on=true',
+      'ios_enable_code_signing=false',
+      'is_debug=false',
+      'target_cpu="arm64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Release',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_ci_ios64_sim_debug_ios_9_0': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'is_component_build=false',
+      'is_debug=true',
+      'target_cpu="x64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphonesimulator9.3',
+    'tests': [
+      {
+        'include': 'common_tests.json',
+        'device type': 'iPhone 6s',
+        'os': '9.3',
+        'host os': 'Mac-10.13',
+        'pool': 'Chrome',
+        'priority': 30
+      }
+    ]
+  },
+
+  'luci_webrtc_ci_ios64_sim_debug_ios_10_0': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'is_component_build=false',
+      'is_debug=true',
+      'target_cpu="x64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphonesimulator10.3',
+    'tests': [
+      {
+        'include': 'common_tests.json',
+        'device type': 'iPhone 6s',
+        'os': '10.3',
+        'host os': 'Mac-10.13',
+        'pool': 'Chrome',
+        'priority': 30
+      }
+    ]
+  },
+
+  'luci_webrtc_try_ios_compile_arm_dbg': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'ios_enable_code_signing=false',
+      'is_debug=true',
+      'target_cpu="arm"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_try_ios_compile_arm_rel': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'dcheck_always_on=true',
+      'ios_enable_code_signing=false',
+      'is_debug=false',
+      'target_cpu="arm"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Release',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_try_ios_compile_arm64_dbg': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'ios_enable_code_signing=false',
+      'is_debug=true',
+      'target_cpu="arm64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_try_ios_compile_arm64_rel': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'dcheck_always_on=true',
+      'ios_enable_code_signing=false',
+      'is_debug=false',
+      'target_cpu="arm64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Release',
+    'sdk': 'iphoneos10.3',
+    'tests': [
+    ]
+  },
+
+  'luci_webrtc_try_ios_sim_x64_dbg_ios9': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'is_debug=true',
+      'target_cpu="x64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphonesimulator9.3',
+    'tests': [
+      {
+        'include': 'common_tests.json',
+        'device type': 'iPhone 6s',
+        'os': '9.3',
+        'host os': 'Mac-10.13',
+        'pool': 'Chrome',
+        'priority': 30
+      }
+    ]
+  },
+
+  'luci_webrtc_try_ios_sim_x64_dbg_ios10': {
+    'xcode build version': '10l232m',
+    'gn_args': [
+      'goma_dir="$(goma_dir)"',
+      'is_debug=true',
+      'target_cpu="x64"',
+      'target_os="ios"',
+      'use_goma=true'
+    ],
+    'additional_compile_targets': [ 'all' ],
+    'configuration': 'Debug',
+    'sdk': 'iphonesimulator10.3',
+    'tests': [
+      {
+        'include': 'common_tests.json',
+        'device type': 'iPhone 6s',
+        'os': '10.3',
+        'host os': 'Mac-10.13',
+        'pool': 'Chrome',
+        'priority': 30
+      }
+    ]
+  },
+}
+
+_WEBRTC_COMMON_TESTS = {
+  'tests': [
+    {
+      'app': 'apprtcmobile_tests',
+      'xctest': True
+    },
+    {
+       'app': 'sdk_unittests',
+       'xctest': True
+    },
+    {
+       'app': 'sdk_framework_unittests',
+       'xctest': True
+    },
+    {
+      'app': 'audio_decoder_unittests'
+    },
+    {
+      'app': 'common_audio_unittests'
+    },
+    {
+      'app': 'common_video_unittests'
+    },
+    {
+      'app': 'modules_tests'
+    },
+    {
+      'app': 'modules_unittests'
+    },
+    {
+      'app': 'rtc_media_unittests'
+    },
+    {
+      'app': 'rtc_pc_unittests'
+    },
+    {
+      'app': 'rtc_stats_unittests'
+    },
+    {
+      'app': 'rtc_unittests'
+    },
+    {
+      'app': 'system_wrappers_unittests'
+    },
+    {
+      'app': 'test_support_unittests'
+    },
+    {
+      'app': 'tools_unittests'
+    },
+    {
+      'app': 'video_capture_tests'
+    },
+    {
+      'app': 'video_engine_tests'
+    },
+    {
+      'app': 'webrtc_nonparallel_tests'
+    }
+  ]
+}
 
 
 def RunSteps(api):
@@ -32,89 +337,31 @@ def RunSteps(api):
   api.ios.build(mb_path=mb_path)
   api.ios.test_swarming()
 
+
 def GenTests(api):
-  yield (
-    api.test('basic')
-    + api.platform('mac', 64)
-    + api.properties(
-      buildername='ios debug',
-      buildnumber='0',
-      mastername='chromium.fake',
-      bot_id='fake-vm',
-      path_config='kitchen',
+  for name, config in sorted(_WEBRTC_CONFIGS.items()):
+    properties = dict(
+        buildername=name.split('_', 3)[-1],
+        buildnumber='0',
+        mastername='chromium.fake',
+        bot_id='fake-vm',
+        path_config='kitchen')
+    if '_try_' in name:
+      properties['gerrit_project'] = 'webrtc'
+      properties = api.properties.tryserver(**properties)
+    else:
+      properties = api.properties(**properties)
+    test = (
+        api.test(name) +
+        api.runtime(is_luci=True, is_experimental=False) +
+        api.platform('mac', 64) +
+        properties +
+        api.ios.make_test_build_config(config) +
+        api.step_data('bootstrap swarming.swarming.py --version',
+                      stdout=api.raw_io.output_text('1.2.3'))
     )
-    + api.ios.make_test_build_config({
-      'xcode version': 'fake xcode version',
-      'gn_args': [
-          'is_debug=true',
-          'target_cpu="x86"',
-      ],
-      'tests': [
-        {
-          'app': 'fake tests 1',
-          'device type': 'fake device',
-          'os': '8.0',
-        },
-        {
-          'app': 'fake tests 2',
-          'device type': 'fake device',
-          'os': '7.1',
-        },
-      ],
-    })
-    + api.step_data(
-        'bootstrap swarming.swarming.py --version',
-         stdout=api.raw_io.output_text('1.2.3'),
-    )
-  )
+    if '_sim_' in name:
+      test += api.step_data('include common_tests.json',
+                            api.json.output(_WEBRTC_COMMON_TESTS))
+    yield test
 
-  yield (
-    api.test('no_tests')
-    + api.platform('mac', 64)
-    + api.properties(
-      buildername='ios',
-      buildnumber='0',
-      mastername='chromium.fake',
-      bot_id='fake-vm',
-      path_config='kitchen',
-    )
-    + api.ios.make_test_build_config({
-      'xcode version': 'fake xcode version',
-      'gn_args': [
-        'is_debug=false',
-        'target_cpu="arm"',
-      ],
-      'tests': [
-      ],
-    })
-    + api.step_data(
-        'bootstrap swarming.swarming.py --version',
-         stdout=api.raw_io.output_text('1.2.3'),
-    )
-  )
-
-  yield (
-    api.test('trybot')
-    + api.platform('mac', 64)
-    + api.properties.tryserver(
-      buildername='ios',
-      buildnumber='0',
-      mastername='chromium.fake',
-      bot_id='fake-vm',
-      path_config='kitchen',
-      gerrit_project='webrtc'
-    )
-    + api.ios.make_test_build_config({
-      'xcode version': 'fake xcode version',
-      'gn_args': [
-        'is_debug=false',
-        'target_cpu="arm"',
-      ],
-      'tests': [
-      ],
-    })
-    + api.step_data(
-        'bootstrap swarming.swarming.py --version',
-         stdout=api.raw_io.output_text('1.2.3'),
-    )
-  )
