1398983af5a589633dc84926c7bbbcffaa6bb939
Eric Foo <efoo@chromium.org>
2019-03-01 21:28:36 +0000

    Remove master chromium.chromedriver as per approved by johnchen@
    Bug:854852, 790174, 790156, 790155
    Change-Id: I0b7debe06f7fd045a14e7277b5af131d2a4288e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1490692
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>
    Commit-Queue: Eric Foo <efoo@chromium.org>

--- tests/masters_test.py
+++ tests/masters_test.py
@@ -216,7 +216,6 @@ def main(argv):
       'master.chromium': 'Chromium',
       'master.chromium.android': 'ChromiumAndroid',
       'master.chromium.android.fyi': 'ChromiumAndroidFyi',
-      'master.chromium.chromedriver': 'ChromiumChromeDriver',
       'master.chromium.clang': 'ChromiumClang',
       'master.chromium.perf': 'ChromiumPerf',
       'master.client.art': 'ART',
