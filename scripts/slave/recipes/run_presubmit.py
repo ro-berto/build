377da5cb93df9b58ea5523b7e664de5b55c4571b
Andrii Shyshkalov <tandrii@chromium.org>
2019-03-27 00:22:38 +0000

    Reland "run_presubmit: solution_name shouldn't matter, but allow overrides."
    This is a reland of 93e087e4ad6862e2e3c144fc96dbd99fec87ca80
    it was actually good, led-verified: https://chromium-swarm.appspot.com/task?id=43d549a6a0593310
    
    Original change's description:
    > run_presubmit: solution_name shouldn't matter, but allow overrides.
    >
    > R=mmoss
    >
    > Bug: 942819
    > Change-Id: I67086121d73e1c460fc41b61456a365f506df928
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1538695
    > Reviewed-by: Michael Moss <mmoss@chromium.org>
    > Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    
    Tbr: mmoss
    Bug: 942819
    Change-Id: I415e951ee41eb71d9371faec1588bf17a78fd735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1539273
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>

--- scripts/slave/recipes/run_presubmit.py
+++ scripts/slave/recipes/run_presubmit.py
@@ -38,7 +38,9 @@ def _RunStepsInternal(api):
     solution = gclient_config.solutions.add()
     solution.url = api.properties.get(
         'repository_url', api.tryserver.gerrit_change_repo_url)
-    solution.name = api.properties['solution_name']
+    # Solution name should matter for most users, particularly if there is no
+    # DEPS file, but if someone wants to override it, fine.
+    solution.name = api.properties.get('solution_name', 's')
     gclient_config.got_revision_mapping[solution.name] = 'got_revision'
 
   bot_update_step = api.bot_update.ensure_checkout(
@@ -265,6 +267,17 @@ def GenTests(api):
 
   yield (
     api.test('repository_url') +
+    api.properties.tryserver(
+        mastername='tryserver.chromium.linux',
+        buildername='chromium_presubmit',
+        repository_url='https://skia.googlesource.com/skia.git',
+        gerrit_project='skia') +
+    api.step_data('presubmit',
+                  api.json.output([['chromium_presubmit', ['compile']]]))
+  )
+
+  yield (
+    api.test('repository_url_with_solution_name') +
     api.properties.tryserver(
         mastername='tryserver.chromium.linux',
         buildername='chromium_presubmit',
