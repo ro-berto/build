a6ddcab7b7264cc3fbd9559a47faba7c24dc0c60
Erik Chen <erikchen@chromium.org>
2019-03-28 23:26:35 +0000

    Turn off CQ-retries for test failures on win7-rel.
    Now that we have 'retry shards with patch', the utility of CQ-retries is close
    to 0. We should be able to turn it off to reduce CQ cycle time with minimal
    impact on false rejects. See
    https://bugs.chromium.org/p/chromium/issues/detail?id=939568#c18
    
    Bug: 915319
    Change-Id: Ied834d2feee0ac73302ae823f200cd26f66a637d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1544834
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/trybots.py
+++ scripts/slave/recipe_modules/chromium_tests/trybots.py
@@ -904,6 +904,7 @@ TRYBOTS = freeze({
       },
       'win7-rel': {
         'retry_failed_shards': True,
+        'test_failures_prevent_cq_retry': True,
         'bot_ids': [
           {
             'mastername': 'chromium.win',
