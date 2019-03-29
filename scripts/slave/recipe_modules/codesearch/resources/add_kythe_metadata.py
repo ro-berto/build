dfdd0fd4ce93e14d1739547cc315467107bba1f9
Owen Rodley <orodley@chromium.org>
2019-03-29 00:07:26 +0000

    Add a script to turn Mojom annotations into Kythe metadata.
    The annotations were added in https://crrev.com/c/1534342.
    
    This script isn't used in the codesearch recipe yet. That will come in a
    later CL, which can be rolled back independently if it causes issues in
    prod.
    
    Bug: 615202
    Change-Id: I9673951b9f572ad14321eee675839bd6a9371228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1542379
    Reviewed-by: Joey Scarr <jsca@chromium.org>
    Commit-Queue: Owen Rodley <orodley@chromium.org>

---
+++ scripts/slave/recipe_modules/codesearch/resources/add_kythe_metadata.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A tool which turns annotations into Kythe metadata.
+
+These annotations are inserted by the Mojom compiler when the
+"enable_kythe_annotations" gn arg is set. We use them to identify the locations
+of identifiers in the generated file, and insert inline metadata tying these
+locations to the name of the entity in the source Mojom file which generates
+them.
+"""
+
+import argparse
+import base64
+import collections
+import json
+import os
+import re
+import sys
+
+def _FindAllMojomGeneratedFiles(root_dir):
+  for dirpath, _, filenames in os.walk(root_dir):
+    for filename in filenames:
+      if any(filename.endswith(suffix)
+             for suffix in ('mojom.h', 'mojom-forward.h', 'mojom-shared.h',
+                            'mojom-shared-internal.h')):
+        yield os.path.join(dirpath, filename)
+
+
+def _GenerateMetadata(filename, file_contents, corpus, verbose):
+  Annotation = collections.namedtuple('Annotation',
+                                      ['signature', 'begin', 'end'])
+  annotations = []
+  for match in re.finditer(r'@generated_from: ([a-zA-Z0-9_.]*)$', file_contents,
+                           re.MULTILINE):
+    signature = match.group(1)
+    components = signature.split('.')
+    entity_name = components[-1]
+    containing_name = components[-2] if len(components) >= 2 else ''
+
+    end = match.end(0) + 1
+    next_line = file_contents[end : file_contents.index('\n', end)]
+
+    # Union fields produce methods with the name of the field preceded by "is_",
+    # "get_" and "set_". Inner types are prefixed with the containing type name
+    # and an underscore. Union field tags are an uppercase version of the name,
+    # hence the IGNORECASE flag.
+    entity_name_match = re.search(
+        r'\W((?:is|get|set|%s)?_?%s)(?:\W|$)' % (containing_name, entity_name),
+        next_line,
+        re.IGNORECASE)
+    if not entity_name_match:
+      # We should always find the entity, but don't fail completely if we don't.
+      print "Couldn't find name after annotation in %s for %s, at offset %d" % (
+          filename, signature, match.start(0))
+      continue
+
+    annotations.append(Annotation(signature, end + entity_name_match.start(1),
+                        end + entity_name_match.end(1)))
+
+  if verbose:
+    print 'found %d annotations' % len(annotations)
+
+  return {
+      # This has to be 'kythe0'. I guess it's a version number.
+      'type': 'kythe0',
+      'meta': [
+          {
+              # 'anchor_defines' maps from an anchor to a semantic node. The
+              # resulting edge will be between the specified semantic node and
+              # the semantic node which is defined by the anchor. So in our case
+              # we have the semantic node of a Mojom object, and the location of
+              # a C++ identifier in the generated code.
+              'type': 'anchor_defines',
+              'begin': annotation.begin,
+              'end': annotation.end,
+              'vname': {
+                  'signature': annotation.signature,
+                  'corpus': corpus
+              },
+              # The leading '%' indicates a reverse edge, which means from VName
+              # to anchor.
+              'edge': '%/kythe/edge/generates'
+          }
+          for annotation in annotations
+      ]
+  }
+
+
+def _FormatMetadata(metadata):
+  b64_metadata = base64.encodestring(json.dumps(metadata))
+
+  # base64.encodedstring returns multi-line output. This is fine by us, as we
+  # want to wrap the comment anyway. The first line will be longer than the rest
+  # since we add the magic comment string, but we don't care too much about
+  # prettiness of the output.
+  return '// Metadata comment ' + b64_metadata[:-1].replace('\n', '\n// ')
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('gen_dir',
+                      help='The generated output dir, which contains all the '
+                           'Mojom generated files to add metadata to')
+  parser.add_argument('--corpus',
+                      default='chromium.googlesource.com/chromium/src',
+                      help='The corpus to use in VNames in the metadata')
+  parser.add_argument('--verbose', action='store_true')
+  opts = parser.parse_args()
+
+  for filename in _FindAllMojomGeneratedFiles(opts.gen_dir):
+    if opts.verbose:
+      print 'Adding metadata to %s' % filename
+    with open(filename, 'r+') as f:
+      contents = f.read()
+      metadata = _GenerateMetadata(filename, contents, opts.corpus,
+                                   opts.verbose)
+
+      # read() already put us at the end of the file
+      if contents[-1] != '\n':
+        f.write('\n')
+
+      # Leave a blank line before the metadata comment.
+      f.write('\n')
+      f.write(_FormatMetadata(metadata))
+
+
+if '__main__' == __name__:
+  sys.exit(main())
---
+++ scripts/slave/recipe_modules/codesearch/resources/add_kythe_metadata_test.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env vpython
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+import add_kythe_metadata
+
+class AddKytheMetadataTest(unittest.TestCase):
+  def _GenerateMetadata(self, contents):
+    return add_kythe_metadata._GenerateMetadata('', contents, 'corpus', False)
+
+  def testMetadataBasic(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: Foobar
+class Foobar {};
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 31,
+                'end': 37,
+                'vname': {
+                    'signature': 'Foobar',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataNameInModule(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: foo.mojom.Foobar
+class Foobar {};
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 41,
+                'end': 47,
+                'vname': {
+                    'signature': 'foo.mojom.Foobar',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataUnionAccessors(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: Union.field
+  bool is_field();
+
+@generated_from: Union.field
+  int get_field();
+
+@generated_from: Union.field
+  void set_field(int x);
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 37,
+                'end': 45,
+                'vname': {
+                    'signature': 'Union.field',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            },
+            {
+                'type': 'anchor_defines',
+                'begin': 85,
+                'end': 94,
+                'vname': {
+                    'signature': 'Union.field',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            },
+            {
+                'type': 'anchor_defines',
+                'begin': 135,
+                'end': 144,
+                'vname': {
+                    'signature': 'Union.field',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataUnionTags(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: Union.field_name
+  FIELD_NAME,
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 37,
+                'end': 47,
+                'vname': {
+                    'signature': 'Union.field_name',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataNestedTypes(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: foo.mojom.Struct.Enum
+enum Struct_Enum {};
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 45,
+                'end': 56,
+                'vname': {
+                    'signature': 'foo.mojom.Struct.Enum',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataUsingStatement(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: foo.mojom.Struct.Enum
+  using Enum = Struct_Enum;
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 48,
+                'end': 52,
+                'vname': {
+                    'signature': 'foo.mojom.Struct.Enum',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+  def testMetadataMethod(self):
+    metadata = self._GenerateMetadata("""
+@generated_from: foo.mojom.Interface.Method
+  virtual void Method(MethodArg a) = 0;
+""")
+    self.assertEqual({
+        'type': 'kythe0',
+        'meta': [
+            {
+                'type': 'anchor_defines',
+                'begin': 60,
+                'end': 66,
+                'vname': {
+                    'signature': 'foo.mojom.Interface.Method',
+                    'corpus': 'corpus',
+                },
+                'edge': '%/kythe/edge/generates',
+            }
+        ]}, metadata)
+
+
+  def testFormatMetadataSingleLine(self):
+    self.assertEqual('// Metadata comment eyJmb28iOiAiYmFyIn0=',
+                     add_kythe_metadata._FormatMetadata({'foo': 'bar'}))
+
+  def testFormatMetadataMultiLine(self):
+    self.assertEqual('// Metadata comment WyJhIiwgImEiLCAiYSIsICJhIiwgImEiLCA' +
+                                 'iYSIsICJhIiwgImEiLCAiYSIsICJhIiwgImEiLCAi\n' +
+                     '// YSIsICJhIiwgImEiLCAiYSIsICJhIiwgImEiLCAiYSIsICJhIiwg' +
+                                                  'ImEiLCAiYSIsICJhIiwgImEi\n' +
+                     '// LCAiYSIsICJhIiwgImEiLCAiYSIsICJhIiwgImEiLCAiYSJd',
+                     add_kythe_metadata._FormatMetadata(['a'] * 30))
+
+if __name__ == '__main__':
+  unittest.main()
