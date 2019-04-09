b538c888ac3f517a60d0a9f8ac7802477a20c559
Derek Schuff <dschuff@chromium.org>
2019-04-09 22:05:24 +0000

    [Emscripten-releases] Build Wabt target
    Sync the required buildtools and build one target.
    
    Bug: v8:8921
    Change-Id: Ie6446ef1d6491ba45799bcceec639f4a325fe3bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1559263
    Commit-Queue: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>

--- scripts/slave/recipes/emscripten_releases.py
+++ scripts/slave/recipes/emscripten_releases.py
@@ -4,15 +4,34 @@
 
 DEPS = [
     'depot_tools/bot_update',
+    'depot_tools/depot_tools',
     'depot_tools/gclient',
     'recipe_engine/buildbucket',
+    'recipe_engine/path',
+    'recipe_engine/python',
     'recipe_engine/step'
 ]
 
 def RunSteps(api):
-  api.step('Print Hello World', cmd=['echo', 'hello', 'world'])
   api.gclient.set_config('emscripten_releases')
   api.bot_update.ensure_checkout()
+  sync_dir = api.path['start_dir'].join('sync')
+  build_dir = api.path['cache'].join('builder', 'build')
+  waterfall_build = api.path['start_dir'].join('waterfall', 'src', 'build.py')
+  dir_flags = ['--sync-dir=%s' % sync_dir,
+               '--build-dir=%s' % build_dir]
+  # The bot_update call above handles the sources in DEPS. All that's left for
+  # this sync step is updating the prebuilt host toolchain (eg. clang) and CMake
+  # TODO: Consider CIPD packages for these, or whatever mechanism other
+  # recipes use.
+  api.python('Sync Buildtools', waterfall_build,
+             dir_flags + ['--no-build', '--no-test',
+              '--sync-include=tools-clang,host-toolchain,cmake'])
+  # Depot tools on path is for ninja
+  with api.depot_tools.on_path():
+    api.python('Build Wabt', waterfall_build,
+               dir_flags + ['--no-sync', '--no-test',
+               '--build-include=wabt'])
 
 def GenTests(api):
   yield api.test('basic')
