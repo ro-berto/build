0fccdbd0e5f5a50b556c7729cacbf6d1caeeba35
Ned Nguyen <nednguyen@google.com>
2019-05-20 20:18:09 +0000

    Roll depot_tools to newer version
    The main change is in chromite/api.py. The rest is just test expectation
    update from running `./scripts/slave/recipes.py test train`.
    
    This update is to ensure depot_tools version is in synced with:
    * https://chrome-internal-review.googlesource.com/c/chromeos/manifest-internal/+/1323633
    * https://chromium-review.googlesource.com/c/chromiumos/manifest/+/1619158
    
    Bug:chromium:964720
    Change-Id: Ibf2c4c5d06d4315ab93bb38bd5600f049eb53af5
    
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Change-Id: Ibf2c4c5d06d4315ab93bb38bd5600f049eb53af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1619387
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>

--- scripts/slave/recipe_modules/chromite/api.py
+++ scripts/slave/recipe_modules/chromite/api.py
@@ -13,7 +13,7 @@ class ChromiteApi(recipe_api.RecipeApi):
       'https://chromium.googlesource.com/chromium/tools/depot_tools.git')
   # Keep this pin in sync with manifest pin in:
   #   https://chromium.googlesource.com/chromiumos/manifest/+/master/full.xml
-  depot_tools_pin = '80a1cf66b83c394c911b14312274b26dba888ae0'
+  depot_tools_pin = '7639f1999ac1d8992fc4fbb169d164b156d9a7f7'
 
   # Only used by the internal goma recipe.
   manifest_url = 'https://chromium.googlesource.com/chromiumos/manifest.git'
