c4d099a56c540b28e13137482cef4fe94037dcf9
Yuke Liao <liaoyuke@chromium.org>
2019-07-09 18:35:51 +0000

    [code coverage] Write unit tests for repository_util
    This CL writes an unit test to exercise the logic to get last updated
    revisions for files.
    
    Change-Id: Ic2b81171da98f9adece5857b49a47d3a18d70b41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1691458
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- scripts/slave/recipe_modules/code_coverage/unittests/repository_util_test.py
+++ scripts/slave/recipe_modules/code_coverage/unittests/repository_util_test.py
@@ -4,7 +4,9 @@
 # found in the LICENSE file.
 
 import os
+import subprocess
 import sys
+import textwrap
 import unittest
 
 import mock
@@ -18,7 +20,55 @@ import repository_util
 
 class RepositoryUtilTest(unittest.TestCase):
 
-  @mock.patch.object(repository_util, 'GetFileRevisions')
+  @mock.patch('repository_util.os.path.isdir', autospec=True)
+  @mock.patch('repository_util.subprocess.check_output', autospec=True)
+  def test_get_file_revisions(self, mock_subprocess, mock_is_dir):
+    deps_file_content = textwrap.dedent('''
+      vars = {
+        'chromium_git': 'https://chromium.googlesource.com',
+      }
+
+      deps = {
+        'src/third_party/repo':
+          Var('chromium_git') + '/repo.git' + '@' + 'abcd1234',
+      }''')
+
+    def is_dir_side_effect(path):
+      path = os.path.normpath(path)
+      return path == '/src' or path == '/src/third_party/repo'
+
+    mock_is_dir.side_effect = is_dir_side_effect
+
+    def mock_subprocess_side_effect(commands, cwd):
+      cwd = os.path.normpath(cwd)
+      if commands[:2] == ['git', 'ls-files']:
+        if cwd == '/src':
+          return 'file1.cc'
+        elif cwd == '/src/third_party/repo':
+          return 'file2.cc'
+      elif commands[:-1] == ['git', 'log', '-n', '1', '--pretty=format:%H:%ct']:
+        if commands[-1] == 'file1.cc' and cwd == '/src':
+          return 'file1hash:12345'
+        elif commands[-1] == 'file2.cc' and cwd == '/src/third_party/repo':
+          return 'file2hash:12345'
+
+      assert False, 'Unexpected subprocess call'
+
+    mock_subprocess.side_effect = mock_subprocess_side_effect
+
+    with mock.patch('repository_util.open',
+                    mock.mock_open(read_data=deps_file_content)) as m:
+      file_revisions = repository_util.GetFileRevisions(
+          '/src', 'DEPS', ['//file1.cc', '//third_party/repo/file2.cc'])
+
+      expected_file_revisions = {
+          '//file1.cc': ('file1hash', 12345),
+          '//third_party/repo/file2.cc': ('file2hash', 12345)
+      }
+      self.assertDictEqual(expected_file_revisions, file_revisions)
+      m.assert_called_once_with('/src/DEPS', 'r')
+
+  @mock.patch.object(repository_util, 'GetFileRevisions', autospec=True)
   def test_add_git_revisions_to_coverage_files_metadata(
       self, mock_get_file_revisions):
     mock_get_file_revisions.return_value = {
