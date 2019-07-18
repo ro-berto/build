c508669f88862eca4e0e44c904bc61f207bf4ef2
Tamer Tas <tmrts@chromium.org>
2019-07-18 07:02:42 +0000

    [chromium_tests] add more tests to {chromium_integration} recipe
    This CL improves the test names and adds 2 more end-to-end tests for flakes and
    bugs introduced to the target codebase before integration.
    
    R=jbudorick@chromium.org
    CC=machenbach@chromium.org
    
    Bug: 950731
    Change-Id: I4ba95549ffd26bdf647d2bb3e4b2e3b3a6d6644d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1706748
    Auto-Submit: Tamer Tas <tmrts@chromium.org>
    Commit-Queue: Tamer Tas <tmrts@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipes/chromium_integration.py
+++ scripts/slave/recipes/chromium_integration.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from recipe_engine.post_process import MustRun, DropExpectation
+from recipe_engine import post_process
 
 DEPS = [
   'chromium',
@@ -47,24 +47,29 @@ def GenTests(api):
       )
     )
 
-  yield (
-    api.test('deapply_deps_after_failure') +
-    ci_props(extra_swarmed_tests=['blink_web_tests']) +
-    api.platform.name('linux') +
-    api.chromium_tests.read_source_side_spec(
+  def blink_test_setup():
+    return (
+      ci_props(extra_swarmed_tests=['blink_web_tests']) +
+      api.platform.name('linux') +
+      api.chromium_tests.read_source_side_spec(
         'chromium.linux', {
-            'Linux Builder': {
-              'isolated_scripts': [
-                    {
-                      'isolate_name': 'blink_web_tests',
-                      'name': 'blink_web_tests',
-                      'swarming': {'can_use_on_swarming_builders': True},
-                      'results_handler': 'layout tests',
-                    },
-                ],
-            },
+          'Linux Builder': {
+            'isolated_scripts': [
+              {
+                'isolate_name': 'blink_web_tests',
+                'name': 'blink_web_tests',
+                'swarming': {'can_use_on_swarming_builders': True},
+                'results_handler': 'layout tests',
+              },
+            ],
+          },
         }
-    ) +
+      )
+    )
+
+  yield (
+    api.test('bug_introduced_by_commit') +
+    blink_test_setup() +
     api.override_step_data('blink_web_tests (with patch)',
         api.chromium_swarming.canned_summary_output(
             api.test_utils.canned_isolated_script_output(
@@ -84,8 +89,66 @@ def GenTests(api):
             api.test_utils.canned_isolated_script_output(
                 passing=True, swarming=True,
                 isolated_script_passing=True))) +
-    api.post_process(MustRun, 'blink_web_tests (with patch)') +
-    api.post_process(MustRun, 'blink_web_tests (retry shards with patch)') +
-    api.post_process(MustRun, 'blink_web_tests (without patch)') +
-    api.post_process(DropExpectation)
+    api.post_process(post_process.MustRun, 'blink_web_tests (with patch)') +
+    api.post_process(
+      post_process.MustRun, 'blink_web_tests (retry shards with patch)') +
+    api.post_process(post_process.MustRun, 'blink_web_tests (without patch)') +
+    api.post_process(post_process.StatusFailure) +
+    api.post_process(post_process.DropExpectation)
+  )
+
+  yield (
+    api.test('bug_introduced_by_chromium') +
+    blink_test_setup() +
+    api.override_step_data('blink_web_tests (with patch)',
+        api.chromium_swarming.canned_summary_output(
+            api.test_utils.canned_isolated_script_output(
+                passing=False, swarming=True,
+                isolated_script_passing=False,
+                isolated_script_retcode=125),
+            failure=True)) +
+    api.override_step_data('blink_web_tests (retry shards with patch)',
+        api.chromium_swarming.canned_summary_output(
+            api.test_utils.canned_isolated_script_output(
+                passing=False, swarming=True,
+                isolated_script_passing=False,
+                isolated_script_retcode=125),
+            failure=True)) +
+    api.override_step_data('blink_web_tests (without patch)',
+        api.chromium_swarming.canned_summary_output(
+            api.test_utils.canned_isolated_script_output(
+                passing=False, swarming=True,
+                isolated_script_passing=False),
+            failure=True)) +
+    api.post_process(
+      post_process.MustRun, 'blink_web_tests (with patch)') +
+    api.post_process(
+      post_process.MustRun, 'blink_web_tests (retry shards with patch)') +
+    api.post_process(post_process.MustRun, 'blink_web_tests (without patch)') +
+    api.post_process(post_process.StatusSuccess) +
+    api.post_process(post_process.DropExpectation)
+  )
+
+  yield (
+    api.test('flaky_test') +
+    blink_test_setup() +
+    api.override_step_data('blink_web_tests (with patch)',
+        api.chromium_swarming.canned_summary_output(
+            api.test_utils.canned_isolated_script_output(
+                passing=False, swarming=True,
+                isolated_script_passing=False,
+                isolated_script_retcode=125),
+            failure=True)) +
+    api.override_step_data('blink_web_tests (retry shards with patch)',
+        api.chromium_swarming.canned_summary_output(
+            api.test_utils.canned_isolated_script_output(
+                passing=True, swarming=True,
+                isolated_script_passing=True))) +
+    api.post_process(post_process.MustRun, 'blink_web_tests (with patch)') +
+    api.post_process(
+      post_process.MustRun, 'blink_web_tests (retry shards with patch)') +
+    api.post_process(
+      post_process.DoesNotRun, 'blink_web_tests (without patch)') +
+    api.post_process(post_process.StatusSuccess) +
+    api.post_process(post_process.DropExpectation)
   )
