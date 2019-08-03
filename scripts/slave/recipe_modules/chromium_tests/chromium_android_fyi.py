890b224f0251dbcb3e2e13b81eab8fbdd34e7242
John Budorick <jbudorick@chromium.org>
2019-08-03 00:02:24 +0000

    chromium_tests: clean out old chromium.android.fyi configurations.
    Noticed these while setting up android-pie-x86-fyi-rel.
    
    Change-Id: Ia48960f84fee75ab62825d82d46ab91bcb5605cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1733888
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
@@ -11,64 +11,6 @@ SPEC = {
     'build_gs_bucket': 'chromium-android-archive',
   },
   'builders': {
-    # TODO(jbudorick): Move the three cronet bots over to chromium.android.
-    'Android Cronet ARMv6 Builder': {
-      'android_config': 'main_builder_mb',
-      'bot_type': 'builder_tester',
-      'chromium_config': 'main_builder_mb',
-      'chromium_apply_config': [
-        'cronet_builder',
-        'cronet_official',
-      ],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 32,
-        'TARGET_PLATFORM': 'android',
-      },
-      'testing': {
-        'platform': 'linux',
-      }
-    },
-    'Android Cronet Builder (dbg)': {
-      'android_config': 'main_builder_mb',
-      'bot_type': 'builder_tester',
-      'chromium_config': 'main_builder_mb',
-      'chromium_apply_config': [
-        'cronet_builder',
-        'cronet_official',
-      ],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 32,
-        'TARGET_PLATFORM': 'android',
-      },
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-    'Android Cronet KitKat Builder': {
-      'android_config': 'main_builder_mb',
-      'bot_type': 'builder_tester',
-      'chromium_config': 'main_builder_mb',
-      'chromium_apply_config': [
-        'cronet_builder',
-        'cronet_official',
-      ],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 32,
-        'TARGET_PLATFORM': 'android',
-      },
-      'testing': {
-        'platform': 'linux',
-      }
-    },
     'Android WebView O NetworkService (dbg)': {
       'chromium_config': 'android',
       'chromium_apply_config': [
@@ -104,59 +46,6 @@ SPEC = {
         'platform': 'linux',
       },
     },
-
-    'NDK Next arm Builder': {
-      'chromium_config': 'android',
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android', 'ndk_next'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_PLATFORM': 'android',
-      },
-      'bot_type': 'builder',
-      'android_config': 'main_builder_mb',
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-
-    'NDK Next arm64 Builder': {
-      'chromium_config': 'android',
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android', 'ndk_next'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_PLATFORM': 'android',
-      },
-      'bot_type': 'builder',
-      'android_config': 'arm64_builder_mb',
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-
-    'Nougat Phone Tester': {
-      'chromium_config': 'android',
-      'chromium_apply_config': [
-        'download_vr_test_apks',
-      ],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 64,
-        'TARGET_PLATFORM': 'android',
-      },
-      'parent_buildername': 'Android arm64 Builder (dbg)',
-      'parent_mastername': 'chromium.android',
-      'bot_type': 'tester',
-      'android_config': 'main_builder_mb',
-      'android_apply_config': ['use_devil_provision'],
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-
     'android-pie-x86-fyi-rel': {
       'chromium_config': 'android',
       'chromium_apply_config': ['mb'],
