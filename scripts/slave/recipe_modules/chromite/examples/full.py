1da6818b0a7ee7a7ded38c08e5f4845d0c2bc287
Garrett Beaty <gbeaty@chromium.org>
2019-09-18 22:32:45 +0000

    Switch recipe test definitions to pass test data to api.test.
    Enforced formatting has been enabled for the repo. When formatted with
    yapf, test data created by combining with + results in awkward
    formatting where multiple terms will be put on a single line if they
    fit. Arguments to functions are nicely handled by yapf.
    
    Some unrelated lines may be changed due to yapf always formatting lines
    that are improperly indented even when line ranges are passed to limit
    formatting to changed lines.
    
    This CL was uploaded by git cl split.
    
    R=jclinton@chromium.org
    
    Bug: 998166
    Change-Id: I92e0ec0616df9d9b04c75fed4124c25b4b05398b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1793334
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>

--- scripts/slave/recipe_modules/chromite/examples/full.py
+++ scripts/slave/recipe_modules/chromite/examples/full.py
@@ -35,53 +35,59 @@ def RunSteps(api):
 
 
 def GenTests(api):
-  yield (
-      api.test('basic') +
+  yield api.test(
+      'basic',
       # chromite module uses path['root'] which exists only in Buildbot.
-      api.properties(path_config='buildbot', cbb_config='auron-paladin') +
+      api.properties(path_config='buildbot', cbb_config='auron-paladin'),
       api.buildbucket.try_build(
           'basic',
           git_repo='https://chromium.googlesource.com/chromiumos/manifest',
-          revision='deadbeef'))
+          revision='deadbeef'),
+  )
 
-  yield (
-      api.test('pass_repo_sync_args') +
-      # chromite module uses path['root'] which exists only in Buildbot.
-      api.properties(
-          path_config='buildbot',
-          cbb_config='auron-paladin',
-          repo_sync_args=['-j16']) +
-      api.buildbucket.try_build(
-          'basic',
-          git_repo='https://chromium.googlesource.com/chromiumos/manifest',
-          revision='deadbeef'))
-
-  yield (api.test('chromiumos_coverage') +
-         # chromite module uses path['root'] which exists only in Buildbot.
-         api.properties(
-             path_config='buildbot',
-             clobber=None,
-             cbb_config='cros-x86-generic-tot-chrome-pfq-informational',
-             cbb_master_build_id='24601',
-             cbb_branch='master',
-             config_repo='https://fake.googlesource.com/myconfig/repo') +
-         api.buildbucket.try_build(
-             'chromiumos.coverage',
-             build_number=12345,
-             git_repo='https://chromium.googlesource.com/chromium/src',
-             revision='b8819267417da248aa4fe829c5fcf0965e17b0c3') +
-         api.post_process(post_process.MustRun, 'setup board') +
-         api.post_process(post_process.StatusSuccess) + api.post_process(
-             post_process.DropExpectation))
-
-  yield (
-      api.test('pass_branch') +
+  yield api.test(
+      'pass_repo_sync_args',
       # chromite module uses path['root'] which exists only in Buildbot.
       api.properties(
           path_config='buildbot',
           cbb_config='auron-paladin',
-          branch='foobarnch') + api.post_process(post_process.DropExpectation) +
+          repo_sync_args=['-j16']),
       api.buildbucket.try_build(
           'basic',
           git_repo='https://chromium.googlesource.com/chromiumos/manifest',
-          revision='deadbeef'))
+          revision='deadbeef'),
+  )
+
+  yield api.test(
+      'chromiumos_coverage',
+      # chromite module uses path['root'] which exists only in Buildbot.
+      api.properties(
+          path_config='buildbot',
+          clobber=None,
+          cbb_config='cros-x86-generic-tot-chrome-pfq-informational',
+          cbb_master_build_id='24601',
+          cbb_branch='master',
+          config_repo='https://fake.googlesource.com/myconfig/repo'),
+      api.buildbucket.try_build(
+          'chromiumos.coverage',
+          build_number=12345,
+          git_repo='https://chromium.googlesource.com/chromium/src',
+          revision='b8819267417da248aa4fe829c5fcf0965e17b0c3'),
+      api.post_process(post_process.MustRun, 'setup board'),
+      api.post_process(post_process.StatusSuccess),
+      api.post_process(post_process.DropExpectation),
+  )
+
+  yield api.test(
+      'pass_branch',
+      # chromite module uses path['root'] which exists only in Buildbot.
+      api.properties(
+          path_config='buildbot',
+          cbb_config='auron-paladin',
+          branch='foobarnch'),
+      api.post_process(post_process.DropExpectation),
+      api.buildbucket.try_build(
+          'basic',
+          git_repo='https://chromium.googlesource.com/chromiumos/manifest',
+          revision='deadbeef'),
+  )
