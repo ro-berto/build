4e5d273c1dd8cd906513a687cc443731cb0d88c4
Yun Liu <yliuyliu@google.com>
2019-09-20 19:02:45 +0000

    [code coverage] Create android M coverage trybot - infra
    This change runs "git cl format", which is suggested in Presubmit Warnings.
    
    Bug: 1005003
    Change-Id: I2288852fe6e4ebbce0dffa51c3b3439e9310e35a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1809107
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Yun Liu <yliuyliu@google.com>

--- scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_android.py
+++ scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_android.py
@@ -4,43 +4,60 @@
 
 
 SPEC = {
-  'builders': {
-    'android-kitkat-arm-coverage-rel': {
-      'chromium_config': 'android',
-      'chromium_apply_config': [
-        'download_vr_test_apks',
-        'mb'
-      ],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 32,
-        'TARGET_PLATFORM': 'android',
-      },
-      'android_config': 'main_builder',
-      'bot_type': 'builder_tester',
-      'testing': {
-        'platform': 'linux',
-      },
+    'builders': {
+        'android-kitkat-arm-coverage-rel': {
+            'chromium_config': 'android',
+            'chromium_apply_config': ['download_vr_test_apks', 'mb'],
+            'gclient_config': 'chromium',
+            'gclient_apply_config': ['android'],
+            'chromium_config_kwargs': {
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 32,
+                'TARGET_PLATFORM': 'android',
+            },
+            'android_config': 'main_builder',
+            'bot_type': 'builder_tester',
+            'testing': {
+                'platform': 'linux',
+            },
+        },
+        'android-marshmallow-arm64-coverage-rel': {
+            'chromium_config':
+                'android',
+            'chromium_apply_config': [
+                'download_vr_test_apks', 'mb', 'goma_high_parallel'
+            ],
+            'gclient_config':
+                'chromium',
+            'gclient_apply_config': ['android'],
+            'chromium_config_kwargs': {
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 64,
+                'TARGET_PLATFORM': 'android',
+            },
+            'android_config':
+                'main_builder',
+            'bot_type':
+                'builder_tester',
+            'testing': {
+                'platform': 'linux',
+            },
+        },
+        'android_blink_rel': {
+            'android_config': 'main_builder',
+            'chromium_apply_config': ['mb',],
+            'chromium_config': 'android',
+            'chromium_config_kwargs': {
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 32,
+                'TARGET_PLATFORM': 'android',
+            },
+            'gclient_config': 'chromium',
+            'gclient_apply_config': ['android'],
+            'bot_type': 'builder_tester',
+            'testing': {
+                'platform': 'linux',
+            },
+        },
     },
-    'android_blink_rel': {
-      'android_config': 'main_builder',
-      'chromium_apply_config': [
-        'mb',
-      ],
-      'chromium_config': 'android',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 32,
-        'TARGET_PLATFORM': 'android',
-      },
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['android'],
-      'bot_type': 'builder_tester',
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-  },
 }
