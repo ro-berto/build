66fbf5e40d21ff0c93853c9374686bb598345b65
Eric Foo <efoo@chromium.org>
2019-09-26 00:00:11 +0000

    Remove master.chromium. This is the last running master and marks the completion of the LUCI migration project.
    Bug: 1007583
    Change-Id: I7ea6011675422bab49f4dd359f58b449efa3c768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1824008
    Commit-Queue: Eric Foo <efoo@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -66,34 +66,9 @@ def CommitChecks(input_api, output_api):
 
   tests = []
 
-  # masters_test can be very slow, so only add it if relevant files have been
-  # touched.
-  tests_to_run = []
-  conditional_tests = {
-      'tests/masters_test.py': [
-          r'^masters/.*',
-          r'^scripts/common/.*',
-          r'^scripts/master/.*',
-          r'^third_party/buildbot_8_4p1/.*',
-          r'^third_party/twisted_10_2/.*',
-      ],
-  }
-  affected_files = set([
-      f.LocalPath() for f in input_api.change.AffectedFiles()])
-  for test, regexes in conditional_tests.iteritems():
-    for path in affected_files:
-      if any(re.match(r, path) for r in regexes):
-        tests_to_run.append(test)
-        break
-
-  tests.extend(input_api.canned_checks.GetUnitTests(
-      input_api, output_api, tests_to_run))
-
   whitelist = [r'.+_test\.py$']
-  blacklist = [r'masters_test.py$']
   tests.extend(input_api.canned_checks.GetUnitTestsInDirectory(
-      input_api, output_api, 'tests', whitelist=whitelist,
-      blacklist=blacklist))
+      input_api, output_api, 'tests', whitelist=whitelist))
   tests.extend(input_api.canned_checks.GetUnitTestsInDirectory(
       input_api,
       output_api,
