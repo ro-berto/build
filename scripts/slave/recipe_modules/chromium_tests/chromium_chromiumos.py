964b99a9b32d7aa2db0238af53241a2ca39a1949
Ben Pastene <bpastene@chromium.org>
2019-09-26 17:52:58 +0000

    Ensure every builder on chromium.chromimos gets the 'chromeos' gclient config.
    crrev.com/c/1825512 introduced a regression where chromeos-kevin-rel
    lost its 'chromeos' gclient config. (Since it specifies the 'arm' one.)
    
    This fixes that by adding 'chromeos' to every builder.
    
    Bug: 1008291
    Change-Id: I3e07de1ed7a052ce88e8809be089e4186c038177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1827757
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_chromiumos.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_chromiumos.py
@@ -70,8 +70,9 @@ def _config(name,
             target_arch='intel',
             target_bits=64,
             gclient_apply_config=None):
-  if not gclient_apply_config:
-    gclient_apply_config = ['chromeos']
+  gclient_apply_config = gclient_apply_config or []
+  if 'chromeos' not in gclient_apply_config:
+    gclient_apply_config.append('chromeos')
   build_config = 'Release' if '-rel' in name else 'Debug'
   cfg = {
     'chromium_config': 'chromium',
@@ -102,17 +103,23 @@ SPEC['builders'].update([
     # Replicate linux-chromeos-rel for code coverage experiment.
     # TODO(crbug.com/1000367): Remove once linux-chromeos-coverage-rel is folded
     # into linux-chromeos-rel.
-    _config('linux-chromeos-coverage-rel-dummy',
-            gclient_apply_config=['chromeos', 'use_clang_coverage']),
+    _config(
+        'linux-chromeos-coverage-rel-dummy',
+        gclient_apply_config=['use_clang_coverage']),
     _config('linux-chromeos-dbg'),
-
-    _config('chromeos-amd64-generic-asan-rel',
-            cros_board='amd64-generic'),
-    _config('chromeos-amd64-generic-cfi-thin-lto-rel',
-            cros_board='amd64-generic'),
+    _config('chromeos-amd64-generic-asan-rel', cros_board='amd64-generic'),
+    _config(
+        'chromeos-amd64-generic-cfi-thin-lto-rel', cros_board='amd64-generic'),
     _config('chromeos-amd64-generic-rel', cros_board='amd64-generic'),
-    _config('chromeos-arm-generic-rel', cros_board='arm-generic',
-            target_arch='arm', target_bits=32),
-    _config('chromeos-kevin-rel', cros_board='kevin',
-            target_arch='arm', target_bits=32, gclient_apply_config=['arm'])
+    _config(
+        'chromeos-arm-generic-rel',
+        cros_board='arm-generic',
+        target_arch='arm',
+        target_bits=32),
+    _config(
+        'chromeos-kevin-rel',
+        cros_board='kevin',
+        target_arch='arm',
+        target_bits=32,
+        gclient_apply_config=['arm'])
 ])
