ecd78de4cd611d244daac29ff97e1519a8659f7f
Yves Gerey <yvesg@google.com>
2019-10-09 23:20:35 +0000

    Add recipes for Android Opus Tests.
    This is a follow-up to:
    chromium-review.googlesource.com/c/chromium/src/+/1816555
    
    Bug: chromium:1002973
    Change-Id: I7e9efa318cd65886f1a416e2f367791fbdb70297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1829222
    Commit-Queue: Yves Gerey <yvesg@google.com>
    Reviewed-by: John Budorick <jbudorick@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_android.py
+++ scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_android.py
@@ -27,6 +27,22 @@ SPEC = {
                 'platform': 'linux',
             },
         },
+        'android-opus-kitkat-arm-rel': {
+            'chromium_config': 'android',
+            'chromium_apply_config': ['mb'],
+            'gclient_config': 'chromium',
+            'gclient_apply_config': ['android'],
+            'chromium_config_kwargs': {
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 32,
+                'TARGET_PLATFORM': 'android',
+            },
+            'android_config': 'main_builder',
+            'bot_type': 'builder_tester',
+            'testing': {
+                'platform': 'linux',
+            },
+        },
         'android_blink_rel': {
             'android_config': 'main_builder',
             'chromium_apply_config': ['mb',],
