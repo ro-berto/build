062c5b92b231292d4bd85f89df24703eeff18dd1
Aaron Gable <agable@chromium.org>
2019-10-17 01:03:11 +0000

    Fixup fuchsia test launcher invocation
    The underscores version of --test-launcher-summary-output
    was removed in crrev.com/c/1031564. This wasn't caught until
    now because the vast majority of Cr-Fuchsia bots now run
    their tests via Swarming instead of locally.
    
    Bug: 992496
    Recipe-Nontrivial-Roll: release_scripts
    Change-Id: If012be7d55f57b091cce57c4b8eb24b01f939773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1865973
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Auto-Submit: Aaron Gable <agable@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/steps.py
+++ scripts/slave/recipe_modules/chromium_tests/steps.py
@@ -1055,7 +1055,7 @@ class LocalGTestTest(Test):
       elif is_fuchsia:
         script = api.chromium.output_dir.join('bin',
                                               'run_%s' % self.target_name)
-        args.extend(['--test_launcher_summary_output', gtest_results_file])
+        args.extend(['--test-launcher-summary-output', gtest_results_file])
         args.extend(['--system-log-file', '${ISOLATED_OUTDIR}/system_log'])
         api.python(self.target_name, script, args)
       else:
