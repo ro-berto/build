634032dc0b1e41c1843a397bdbf1a90a0e32ac60
Mohamed Heikal <mheikal@chromium.org>
2019-10-21 15:48:03 +0000

    Binary Size trybot manually checks for expectation failures
    The contract of how expectation file update failures changed (when a dev fails
    to update the expectation file). Instead of failing the build and
    causing there to be no binary size results displayed, the new contract
    writes the failures to a directory which is then checked by
    trybot_failed_expectations_checker.py
    
    Bug: 1011227
    Change-Id: I16a981eef7038260f7e7ddd687c3836a6a41e4e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1857099
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>

---
+++ scripts/slave/recipes/binary_size_trybot.resources/trybot_failed_expectations_checker.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Checks for expectation failures stamp files created by enabling the gn arg
+check_android_configuration. This is used by the size trybot to monitor
+expectation file regressions."""
+
+import argparse
+import json
+import os
+import shutil
+
+
+def _ClearExpectationsDir(dir_path):
+  shutil.rmtree(dir_path, ignore_errors=True)
+
+
+def _CheckExpectationsDir(dir_path):
+  failed_expectations = []
+  try:
+    for failed_expectation_file in os.listdir(dir_path):
+      with open(os.path.join(dir_path, failed_expectation_file)) as f:
+        failed_expectations.append(f.read())
+  except OSError:
+    # if there is no failure directory created, then everything passed.
+    pass
+  return {
+      'success': len(failed_expectations) == 0,
+      'failed_messages': failed_expectations,
+  }
+
+
+def _GetExpectationsDir(build_vars_path):
+  with open(build_vars_path) as f:
+    build_vars = dict(l.rstrip().split('=', 1) for l in f)
+    return build_vars['android_configuration_failure_dir']
+
+
+def _RebasePath(path, new_cwd, old_cwd):
+  """Makes the given path(s) relative to new_cwd, or absolute if not specified.
+
+  If new_cwd is not specified, absolute paths are returned.
+  """
+  old_cwd = os.path.abspath(old_cwd)
+  if new_cwd:
+    new_cwd = os.path.abspath(new_cwd)
+    return os.path.relpath(os.path.join(old_cwd, path), new_cwd)
+  return os.path.abspath(os.path.join(old_cwd, path))
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  mode_args = parser.add_mutually_exclusive_group(required=True)
+  mode_args.add_argument(
+      '--clear-expectations',
+      action='store_true',
+      help='Delete expectations files so they dont clober a new build.')
+  mode_args.add_argument(
+      '--check-expectations',
+      action='store_true',
+      help='Check for the existance of failed expecation files after a build.')
+  parser.add_argument(
+      '--build-vars-path',
+      required=True,
+      help='Path to build_vars.txt.')
+  parser.add_argument(
+      '--results-path', help='Output path for the trybot result .json file.')
+  args = parser.parse_args()
+
+  expectations_dir = _GetExpectationsDir(args.build_vars_path)
+  expectations_dir = _RebasePath(
+      expectations_dir, os.getcwd(), os.path.dirname(args.build_vars_path))
+
+  if args.clear_expectations:
+    _ClearExpectationsDir(expectations_dir)
+  else:
+    result = _CheckExpectationsDir(expectations_dir)
+    if not args.results_path:
+      raise Exception('--results-path is required when passing '
+                      '--check-expectations')
+    with open(args.results_path, 'w') as f:
+      json.dump(result, f)
+
+
+if __name__ == '__main__':
+  main()
