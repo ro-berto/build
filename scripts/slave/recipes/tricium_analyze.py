fd63edc831e59b845085c3da6ffbd8a7c38809ce
Tina Lu <ltina@google.com>
2019-11-01 22:31:19 +0000

    Recipe should provide Tricium clang tidy with latest CL without rebase
    Bug: 1020384
    
    Change-Id: If37bf9485e6bf5b601f1e9383b2e285656fc9a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1893590
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Tina Lu <ltina@google.com>

--- scripts/slave/recipes/tricium_analyze.py
+++ scripts/slave/recipes/tricium_analyze.py
@@ -128,7 +128,12 @@ def RunSteps(api):
     bot_config = {}
     checkout_dir = api.chromium_checkout.get_checkout_dir(bot_config)
     with api.context(cwd=checkout_dir):
-      api.chromium_checkout.ensure_checkout(bot_config)
+      # Do not rebase the patch, so that the Tricium analyzer observes
+      # the correct line numbers. Otherwise, line numbers would be
+      # relative to origin/master, which may be synced to
+      # include changes subsequent to the actual patch.
+      api.chromium_checkout.ensure_checkout(
+          bot_config, gerrit_no_rebase_patch_ref=True)
 
     api.chromium.runhooks(name='runhooks (with patch)')
 
