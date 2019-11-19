302407e1cb9a5e2a1abb7e640eff0d3a53a090b4
Caleb Rouleau <crouleau@chromium.org>
2019-11-19 18:02:24 +0000

    Revert "[Perf] Build WebLayer APKs and add weblayer builder."
    This reverts commit 1fe96ce842407ae2be243dc1cc9ac28a1c75fe8d.
    
    Reason for revert: this broke compile on the arm64 perf compiler
    
    Original change's description:
    > [Perf] Build WebLayer APKs and add weblayer builder.
    > 
    > Note that presubmit forced unrelated changes by requiring
    > `git cl format`.
    > 
    > Also cleaned up some comments that organized things in a
    > confusing way.
    > 
    > Bug: 1022823
    > Change-Id: I15b1091c4739cf74a83eca8e864bdaedfd9093cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1922468
    > Reviewed-by: Haiyang Pan <hypan@google.com>
    > Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    
    TBR=crouleau@chromium.org,hypan@google.com
    
    Change-Id: I05b5dada47c36986a8835a3be2191dd318eb1459
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1022823, 1026245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1924552
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_perf.py
@@ -182,22 +182,15 @@ _AddBuildSpec('android-builder-perf', 'android', target_bits=32,
                                      'system_webview_shell_apk',])
 
 # LUCI builder
-_AddBuildSpec(
-    'android_arm64-builder-perf',
-    'android',
-    target_bits=64,
-    add_to_bisect=True,
-    extra_compile_targets=[
-        'android_tools',
-        'cc_perftests',
-        'chrome_public_apk',
-        'gpu_perftests',
-        'push_apps_to_background_apk',
-        'system_webview_apk',
-        'system_webview_shell_apk',
-        'weblayer_shell_apk',
-        'weblayer_support_apk',
-    ])
+_AddBuildSpec('android_arm64-builder-perf', 'android', target_bits=64,
+              add_to_bisect=True,
+              extra_compile_targets=['android_tools',
+                                     'cc_perftests',
+                                     'chrome_public_apk',
+                                     'gpu_perftests',
+                                     'push_apps_to_background_apk',
+                                     'system_webview_apk',
+                                     'system_webview_shell_apk',])
 
 _AddBuildSpec('win32-builder-perf', 'win', target_bits=32)
 _AddBuildSpec('win64-builder-perf', 'win', add_to_bisect=True)
@@ -205,8 +198,7 @@ _AddBuildSpec('mac-builder-perf', 'mac', add_to_bisect=True)
 
 _AddBuildSpec('linux-builder-perf', 'linux', add_to_bisect=True)
 
-
-# Android: Clank, Webview, WebLayer
+# 32 bit android swarming
 _AddIsolatedTestSpec('android-nexus5x-perf', 'android',
                      'android-builder-perf', target_bits=32)
 _AddIsolatedTestSpec('Android Nexus5 Perf', 'android',
@@ -221,9 +213,9 @@ _AddIsolatedTestSpec('android-pixel2-perf', 'android',
                      'android_arm64-builder-perf')
 _AddIsolatedTestSpec('android-pixel2_webview-perf', 'android',
                      'android_arm64-builder-perf')
-_AddIsolatedTestSpec('android-pixel2_weblayer-perf', 'android',
-                     'android_arm64-builder-perf')
 
+
+# Webview
 _AddIsolatedTestSpec('Android Nexus5X WebView Perf', 'android',
                      'android_arm64-builder-perf')
 _AddIsolatedTestSpec('Android Nexus6 WebView Perf', 'android',
