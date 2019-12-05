b5d7ac61bae1c2318acb56c339562f544b51429c
Erik Chen <erikchen@chromium.org>
2019-12-05 23:26:38 +0000

    switch android-arm64-proguard-rel to use a stock config.
    Bug: 1029936
    Change-Id: I16a55a3ff6df33f6fbb1cfb0373530d7da2fe878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1954024
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android.py
@@ -90,22 +90,6 @@ SPEC = {
                 'platform': 'linux',
             },
         },
-        'android-arm64-proguard-rel': {
-            'chromium_config': 'android',
-            'chromium_apply_config': ['download_vr_test_apks',],
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'main_builder_mb',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
         'Cast Android (dbg)': {
             'chromium_config': 'android',
             'chromium_apply_config': ['mb',],
@@ -464,36 +448,43 @@ def stock_cronet_config(name, config='Release', **kwargs):
 
 
 SPEC['builders'].update([
-  stock_config('android-incremental-dbg', config='Debug'),
-  stock_config(
-      'android-kitkat-arm-rel',
-      chromium_apply_config=['download_vr_test_apks'],
-      chromium_config_kwargs={'TARGET_BITS': 32}),
-  stock_config(
-      'android-marshmallow-arm64-rel',
-      chromium_apply_config=[
-          'download_vr_test_apks',
-          # This is specified because 'android-marshmallow-arm64-rel' builder
-          # is one of the slowest builder in CQ (crbug.com/804251).
-          'goma_high_parallel'],
-      chromium_config_kwargs={'TARGET_BITS': 64}),
-  stock_config(
-      'android-pie-arm64-rel',
-      chromium_config_kwargs={'TARGET_BITS': 64}),
-  stock_cronet_config('android-cronet-arm-dbg', config='Debug'),
-  stock_cronet_config('android-cronet-arm-rel'),
-  stock_cronet_config('android-cronet-arm64-dbg', config='Debug'),
-  stock_cronet_config('android-cronet-arm64-rel'),
-  stock_cronet_config('android-cronet-asan-arm-rel'),
-  stock_cronet_config('android-cronet-kitkat-arm-rel',
-      bot_type='tester',
-      parent_buildername='android-cronet-arm-rel'),
-  stock_cronet_config('android-cronet-lollipop-arm-rel',
-      bot_type='tester',
-      parent_buildername='android-cronet-arm-rel'),
-  stock_cronet_config('android-cronet-marshmallow-arm64-rel',
-      bot_type='tester',
-      parent_buildername='android-cronet-arm64-rel'),
-  stock_cronet_config('android-cronet-x86-dbg', config='Debug'),
-  stock_cronet_config('android-cronet-x86-rel'),
+    stock_config(
+        'android-arm64-proguard-rel',
+        chromium_apply_config=['download_vr_test_apks'],
+        chromium_config_kwargs={'TARGET_BITS': 64}),
+    stock_config('android-incremental-dbg', config='Debug'),
+    stock_config(
+        'android-kitkat-arm-rel',
+        chromium_apply_config=['download_vr_test_apks'],
+        chromium_config_kwargs={'TARGET_BITS': 32}),
+    stock_config(
+        'android-marshmallow-arm64-rel',
+        chromium_apply_config=[
+            'download_vr_test_apks',
+            # This is specified because 'android-marshmallow-arm64-rel' builder
+            # is one of the slowest builder in CQ (crbug.com/804251).
+            'goma_high_parallel'
+        ],
+        chromium_config_kwargs={'TARGET_BITS': 64}),
+    stock_config(
+        'android-pie-arm64-rel', chromium_config_kwargs={'TARGET_BITS': 64}),
+    stock_cronet_config('android-cronet-arm-dbg', config='Debug'),
+    stock_cronet_config('android-cronet-arm-rel'),
+    stock_cronet_config('android-cronet-arm64-dbg', config='Debug'),
+    stock_cronet_config('android-cronet-arm64-rel'),
+    stock_cronet_config('android-cronet-asan-arm-rel'),
+    stock_cronet_config(
+        'android-cronet-kitkat-arm-rel',
+        bot_type='tester',
+        parent_buildername='android-cronet-arm-rel'),
+    stock_cronet_config(
+        'android-cronet-lollipop-arm-rel',
+        bot_type='tester',
+        parent_buildername='android-cronet-arm-rel'),
+    stock_cronet_config(
+        'android-cronet-marshmallow-arm64-rel',
+        bot_type='tester',
+        parent_buildername='android-cronet-arm64-rel'),
+    stock_cronet_config('android-cronet-x86-dbg', config='Debug'),
+    stock_cronet_config('android-cronet-x86-rel'),
 ])
