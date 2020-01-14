b3d3181d453ff7e65e679dabd47aa39531e7cdff
Garrett Beaty <gbeaty@chromium.org>
2020-01-14 02:03:09 +0000

    Rewrite bot specs using BotSpec.
    BotSpec provides additional validation and type safety relative to raw
    dictionaries.
    This CL was uploaded by git cl split.
    
    R=bpastene@chromium.org
    
    Change-Id: Iac3eb1f1535652c5492863da28a16d6aa569a5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1999506
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_android_fyi.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from . import steps
+from . import bot_spec
 
 RESULTS_URL = 'https://chromeperf.appspot.com'
 
@@ -11,99 +11,105 @@ SPEC = {
         'build_gs_bucket': 'chromium-android-archive',
     },
     'builders': {
-        'Android WebView P Blink-CORS FYI (rel)': {
-            'chromium_config': 'android',
-            'chromium_apply_config': ['mb'],
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'main_builder',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
-        'Android WebView P FYI (rel)': {
-            'chromium_config': 'android',
-            'chromium_apply_config': ['mb'],
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'main_builder',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
-        'Memory Infra Tester': {
-            'chromium_config': 'android',
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 32,
-                'TARGET_PLATFORM': 'android',
-            },
-            'bot_type': 'builder_tester',
-            'android_config': 'main_builder_mb',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
-        'android-marshmallow-x86-fyi-rel': {
-            'chromium_config': 'android',
-            'chromium_apply_config': ['mb'],
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 32,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'x86_builder',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
-        'android-pie-x86-fyi-rel': {
-            'chromium_config': 'android',
-            'chromium_apply_config': ['mb'],
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 32,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'x86_builder',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
-        'android-bfcache-debug': {
-            'chromium_config': 'android',
-            'gclient_config': 'chromium',
-            'gclient_apply_config': ['android'],
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Debug',
-                'TARGET_BITS': 32,
-                'TARGET_PLATFORM': 'android',
-            },
-            'android_config': 'main_builder_mb',
-            'bot_type': 'builder_tester',
-            'testing': {
-                'platform': 'linux',
-            },
-        },
+        'Android WebView P Blink-CORS FYI (rel)':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                    'TARGET_PLATFORM': 'android',
+                },
+                android_config='main_builder',
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'Android WebView P FYI (rel)':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                    'TARGET_PLATFORM': 'android',
+                },
+                android_config='main_builder',
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'Memory Infra Tester':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 32,
+                    'TARGET_PLATFORM': 'android',
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                android_config='main_builder_mb',
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'android-marshmallow-x86-fyi-rel':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 32,
+                    'TARGET_PLATFORM': 'android',
+                },
+                android_config='x86_builder',
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'android-pie-x86-fyi-rel':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 32,
+                    'TARGET_PLATFORM': 'android',
+                },
+                android_config='x86_builder',
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'android-bfcache-debug':
+            bot_spec.BotSpec.create(
+                chromium_config='android',
+                gclient_config='chromium',
+                gclient_apply_config=['android'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Debug',
+                    'TARGET_BITS': 32,
+                    'TARGET_PLATFORM': 'android',
+                },
+                android_config='main_builder_mb',
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
     },
 }
