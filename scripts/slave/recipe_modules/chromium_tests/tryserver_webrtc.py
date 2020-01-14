690cfeb376310f5c0211fe4bfb90319e863378cc
Garrett Beaty <gbeaty@chromium.org>
2020-01-14 08:48:10 +0000

    Rewrite bot specs using BotSpec.
    BotSpec provides additional validation and type safety relative to raw
    dictionaries.
    This CL was uploaded by git cl split.
    
    R=phoglund@chromium.org
    
    Change-Id: I286ac1a637c567b2cf2f51ed849d42d256c68d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1999510
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>
    Reviewed-by: Oleh Prypin <oprypin@chromium.org>
    Commit-Queue: Patrik Höglund <phoglund@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/tryserver_webrtc.py
+++ scripts/slave/recipe_modules/chromium_tests/tryserver_webrtc.py
@@ -2,97 +2,87 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from . import bot_spec
+
 SPEC = {
     'settings': {
         'luci_project': 'webrtc'
     },
     'builders': {
-        'android_chromium_compile': {
-            'android_config': 'base_config',
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb', 'android'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_ARCH': 'arm',
-                'TARGET_BITS': 64,
-                'TARGET_PLATFORM': 'android'
-            },
-            'gclient_apply_config': ['android'],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'linux'
-            }
-        },
-        'linux_chromium_compile': {
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64
-            },
-            'gclient_apply_config': [],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'linux'
-            }
-        },
-        'linux_chromium_compile_dbg': {
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64
-            },
-            'gclient_apply_config': [],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'linux'
-            }
-        },
-        'mac_chromium_compile': {
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64
-            },
-            'gclient_apply_config': [],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'mac'
-            }
-        },
-        'win_chromium_compile': {
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64
-            },
-            'gclient_apply_config': [],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'win'
-            }
-        },
-        'win_chromium_compile_dbg': {
-            'bot_type': 'builder',
-            'chromium_apply_config': ['dcheck', 'mb'],
-            'chromium_config': 'chromium',
-            'chromium_config_kwargs': {
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64
-            },
-            'gclient_apply_config': [],
-            'gclient_config': 'chromium_no_telemetry_dependencies',
-            'testing': {
-                'platform': 'win'
-            }
-        }
+        'android_chromium_compile':
+            bot_spec.BotSpec.create(
+                android_config='base_config',
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb', 'android'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_ARCH': 'arm',
+                    'TARGET_BITS': 64,
+                    'TARGET_PLATFORM': 'android'
+                },
+                gclient_apply_config=['android'],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'linux'}),
+        'linux_chromium_compile':
+            bot_spec.BotSpec.create(
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64
+                },
+                gclient_apply_config=[],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'linux'}),
+        'linux_chromium_compile_dbg':
+            bot_spec.BotSpec.create(
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64
+                },
+                gclient_apply_config=[],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'linux'}),
+        'mac_chromium_compile':
+            bot_spec.BotSpec.create(
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64
+                },
+                gclient_apply_config=[],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'mac'}),
+        'win_chromium_compile':
+            bot_spec.BotSpec.create(
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64
+                },
+                gclient_apply_config=[],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'win'}),
+        'win_chromium_compile_dbg':
+            bot_spec.BotSpec.create(
+                bot_type=bot_spec.BUILDER,
+                chromium_apply_config=['dcheck', 'mb'],
+                chromium_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64
+                },
+                gclient_apply_config=[],
+                gclient_config='chromium_no_telemetry_dependencies',
+                testing={'platform': 'win'}),
     },
 }
