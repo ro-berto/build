47e1016b278c661db155721f99328b367a5c00d1
Garrett Beaty <gbeaty@chromium.org>
2020-01-15 10:16:04 +0000

    Rewrite bot specs using BotSpec.
    BotSpec provides additional validation and type safety relative to raw
    dictionaries.
    This CL was uploaded by git cl split.
    
    R=machenbach@chromium.org
    
    Change-Id: Iffffa161639da8f97179f9b70315b892d6ec3d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1999515
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/client_v8_chromium.py
+++ scripts/slave/recipe_modules/chromium_tests/client_v8_chromium.py
@@ -2,60 +2,65 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from . import bot_spec
+
 SPEC = {
-  'settings': {
-    'build_gs_bucket': 'chromium-v8',
-    'luci_project': 'v8',
-  },
-  'builders': {
-    'Linux - Future': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': [
-        'mb',
-      ],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'builder_tester',
-      'compile_targets': [
-        'all',
-      ],
-      'testing': {
-        'platform': 'linux',
-      },
+    'settings': {
+        'build_gs_bucket': 'chromium-v8',
+        'luci_project': 'v8',
     },
-    'Linux - Future (dbg)': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb'],
-      'gclient_config': 'chromium',
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'builder_tester',
-      'testing': {
-        'platform': 'linux',
-      },
+    'builders': {
+        'Linux - Future':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=[
+                    'mb',
+                ],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                compile_targets=[
+                    'all',
+                ],
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'Linux - Future (dbg)':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Debug',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'Linux V8 API Stability':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['v8_canary', 'with_branch_heads'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                compile_targets=[
+                    'all',
+                ],
+                test_results_config='staging_server',
+                testing={
+                    'platform': 'linux',
+                },
+            ),
     },
-    'Linux V8 API Stability': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb'],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['v8_canary', 'with_branch_heads'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Release',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'builder_tester',
-      'compile_targets': [
-        'all',
-      ],
-      'test_results_config': 'staging_server',
-      'testing': {
-        'platform': 'linux',
-      },
-    },
-  },
 }
