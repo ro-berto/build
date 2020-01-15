034735d39ec4ffdc0344601b1ff43bfb32d73d5b
Garrett Beaty <gbeaty@chromium.org>
2020-01-15 21:05:18 +0000

    Rewrite bot specs using BotSpec.
    BotSpec provides additional validation and type safety relative to raw
    dictionaries.
    This CL was uploaded by git cl split.
    
    R=mfoltz@chromium.org
    
    Change-Id: Ib68a06f7a2947a2334d033ba9c1fe9d83973570e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/1999434
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/client_openscreen_chromium.py
+++ scripts/slave/recipe_modules/chromium_tests/client_openscreen_chromium.py
@@ -2,44 +2,48 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from . import bot_spec
+
 SPEC = {
-  'settings': {
-    'luci_project': 'openscreen',
-  },
-  'builders': {
-    'chromium_linux64_debug': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb'],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['openscreen_tot'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'builder_tester',
-      'compile_targets': [
-        'chrome/browser/media/router',
-      ],
-      'testing': {
-        'platform': 'linux',
-      },
+    'settings': {
+        'luci_project': 'openscreen',
     },
-    'chromium_mac_debug': {
-      'chromium_config': 'chromium',
-      'chromium_apply_config': ['mb'],
-      'gclient_config': 'chromium',
-      'gclient_apply_config': ['openscreen_tot'],
-      'chromium_config_kwargs': {
-        'BUILD_CONFIG': 'Debug',
-        'TARGET_BITS': 64,
-      },
-      'bot_type': 'builder_tester',
-      'compile_targets': [
-        'chrome/browser/media/router',
-      ],
-      'testing': {
-        'platform': 'mac',
-      },
+    'builders': {
+        'chromium_linux64_debug':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['openscreen_tot'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Debug',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                compile_targets=[
+                    'chrome/browser/media/router',
+                ],
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'chromium_mac_debug':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb'],
+                gclient_config='chromium',
+                gclient_apply_config=['openscreen_tot'],
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Debug',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                compile_targets=[
+                    'chrome/browser/media/router',
+                ],
+                testing={
+                    'platform': 'mac',
+                },
+            ),
     },
-  },
 }
