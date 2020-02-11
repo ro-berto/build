e2b71ba2e595ca204986836edbe4daa0d4e62379
Liviu Rau <liviurau@chromium.org>
2020-02-11 15:43:50 +0000

    [v8] Refactoring collection script to accommodate new things to collect and merge
    Bug: v8:10168
    Change-Id: I44573de94180048dddfab048c8e148d79449b2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2030410
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Tamer Tas <tmrts@chromium.org>

--- scripts/slave/recipe_modules/v8/resources/collect_v8_task.py
+++ scripts/slave/recipe_modules/v8/resources/collect_v8_task.py
@@ -27,6 +27,61 @@ Please examine logs to figure out what happened.
 """
 
 
+class BadShards:
+
+  def __init__(self):
+    self.missing = []
+    self.incomplete = []
+
+  def add_incomplete(self, shard):
+    self.incomplete.append(shard)
+
+  def add_missing(self, shard):
+    self.missing.append(shard)
+
+  def not_empty(self):
+    return self.missing or self.incomplete
+
+  def as_str(self):
+    return ', '.join(map(str, sorted(self.missing + self.incomplete)))
+
+  def missing_count(self):
+    return len(self.missing)
+
+
+class AggregatedResults:
+
+  def __init__(self):
+    self.archs = []
+    self.modes = []
+    self.slowest_tests = []
+    self.results = []
+
+  def append(self, json_data):
+    # On continuous bots, the test driver outputs exactly one item in the
+    # test results list for one architecture.
+    assert len(json_data) == 1
+    self.archs.append(json_data[0]['arch'])
+    self.modes.append(json_data[0]['mode'])
+    self.slowest_tests.extend(json_data[0]['slowest_tests'])
+    self.results.extend(json_data[0]['results'])
+
+  def as_json(self, tags):
+    # Each shard must have used the same arch and mode configuration.
+    assert len(set(self.archs)) == 1
+    assert len(set(self.modes)) == 1
+    sorted_tests = sorted(
+        self.slowest_tests, key=lambda t: t['duration'], reverse=True)
+    cut_off = 10
+    return [{
+        'arch': self.archs[0],
+        'mode': self.modes[0],
+        'slowest_tests': sorted_tests[:cut_off],
+        'results': self.results,
+        'tags': sorted(tags),
+    }]
+
+
 def emit_warning(title, log=None):
   print '@@@STEP_WARNINGS@@@'
   print title
@@ -34,8 +89,21 @@ def emit_warning(title, log=None):
     slave_utils.WriteLogLines(title, log.split('\n'))
 
 
-def merge_shard_results(
-    output_dir, sancov_merger=None, coverage_dir=None):
+def get_shards_info(output_dir):
+  # summary.json is produced by swarming.py itself. We are mostly interested
+  # in the number of shards.
+  try:
+    with open(os.path.join(output_dir, 'summary.json')) as f:
+      summary = json.load(f)
+    return summary['shards']
+  except (IOError, ValueError):
+    emit_warning(
+        'summary.json is missing or can not be read',
+        'Something is seriously wrong with swarming_client/ or the bot.')
+    return None
+
+
+def merge_shard_results(output_dir, shards):
   """Reads JSON test output from all shards and combines them into one.
 
   Also merges sancov coverage data if coverage_dir is spefied.
@@ -43,88 +111,67 @@ def merge_shard_results(
   Returns dict with merged test output on success or None on failure. Emits
   annotations.
   """
-  # summary.json is produced by swarming.py itself. We are mostly interested
-  # in the number of shards.
-  try:
-    with open(os.path.join(output_dir, 'summary.json')) as f:
-      summary = json.load(f)
-  except (IOError, ValueError):
-    emit_warning(
-        'summary.json is missing or can not be read',
-        'Something is seriously wrong with swarming_client/ or the bot.')
+  if not shards:
     return None
 
   # Merge all JSON files together.
-  archs = []
-  modes = []
+
   tags = set()
-  slowest_tests = []
-  results = []
-  incomplete_shards = []
-  missing_shards = []
-  for index, result in enumerate(summary['shards']):
+  aggregated_results = AggregatedResults()
+  bad_shards = BadShards()
+  for index, result in enumerate(shards):
     if result is not None:
-      json_data = load_shard_json(output_dir, result['task_id'])
       if int(result.get('exit_code', 0)):
         # When receiving a sigterm, the test runner terminates gracefully
         # with json output, but has a non-zero return code.
-        incomplete_shards.append(index)
+        bad_shards.add_incomplete(index)
+      json_data = load_shard_json(output_dir, result['task_id'], 'output.json')
       if json_data:
-        # On continuous bots, the test driver outputs exactly one item in the
-        # test results list for one architecture.
-        assert len(json_data) == 1
-        archs.append(json_data[0]['arch'])
-        modes.append(json_data[0]['mode'])
-
-        slowest_tests.extend(json_data[0]['slowest_tests'])
-        results.extend(json_data[0]['results'])
+        aggregated_results.append(json_data)
         continue
-    missing_shards.append(index)
+    bad_shards.add_missing(index)
 
   # If some shards are missing, make it known. Continue parsing anyway. Step
   # should be red anyway, since swarming.py return non-zero exit code in that
   # case.
-  if missing_shards or incomplete_shards:
+  if bad_shards.not_empty():
     # Not all tests run, combined JSON summary can not be trusted.
     tags.add('UNRELIABLE_RESULTS')
-    as_str = ', '.join(map(str, sorted(missing_shards + incomplete_shards)))
+    as_str = bad_shards.as_str()
     emit_warning(
         'some shards did not complete: %s' % as_str,
         MISSING_SHARDS_MSG % as_str)
 
   # Handle the case when all shards fail. Return minimalistic dict that has all
   # fields that a calling recipe expects to avoid recipe-level exceptions.
-  if len(missing_shards) == len(summary['shards']):
+  if bad_shards.missing_count() == len(shards):
     return [{'slowest_tests': [], 'results': [], 'tags': sorted(tags)}]
 
-  # Each shard must have used the same arch and mode configuration.
-  assert len(set(archs)) == 1
-  assert len(set(modes)) == 1
+  return aggregated_results.as_json(tags)
 
+
+def merge_test_results(output_dir, shards, options):
+  with open(options.merged_test_output, 'wb') as f:
+    json.dump(merge_shard_results(output_dir, shards), f, separators=(',', ':'))
+
+
+def merge_coverage_data(output_dir, shards, options):
   # Merge coverage data if specified.
-  if coverage_dir:
-    for index, result in enumerate(summary['shards']):
+  if options.coverage_dir:
+    for index, result in enumerate(shards):
       exit_code = subprocess.call([
-        sys.executable, '-u', sancov_merger,
-        '--coverage-dir', coverage_dir,
-        '--swarming-output-dir', os.path.join(output_dir, result['task_id'])])
+          sys.executable, '-u', options.sancov_merger, '--coverage-dir',
+          options.coverage_dir, '--swarming-output-dir',
+          os.path.join(output_dir, result['task_id'])
+      ])
       if exit_code:
         emit_warning('error when merging coverage data of shard %d' % index)
 
-  return [{
-    'arch': archs[0],
-    'mode': modes[0],
-    'slowest_tests': sorted(
-        slowest_tests, key=lambda t: t['duration'], reverse=True)[:10],
-    'results': results,
-    'tags': sorted(tags),
-  }]
 
-
-def load_shard_json(output_dir, task_id):
+def load_shard_json(output_dir, task_id, file_name):
   """Reads JSON output of a single shard."""
   # 'output.json' is set in v8/testing.py, V8SwarmingTest.
-  path = os.path.join(output_dir, task_id, 'output.json')
+  path = os.path.join(output_dir, task_id, file_name)
   try:
     with open(path) as f:
       return json.load(f)
@@ -133,7 +180,26 @@ def load_shard_json(output_dir, task_id):
     return None
 
 
-def main(args):
+def swarming_cmd(swarming_args, options):
+  # Prepare a directory to store JSON files fetched from isolate.
+  task_output_dir = tempfile.mkdtemp(
+      suffix='_swarming', dir=options.temp_root_dir)
+  # Start building the command line for swarming.py.
+  cmd = [
+      'swarming',
+  ]
+
+  cmd.extend(swarming_args)
+  cmd.extend([
+      '-output-dir',
+      task_output_dir,
+      '-task-summary-json',
+      os.path.join(task_output_dir, 'summary.json'),
+  ])
+  return cmd, task_output_dir
+
+
+def parse_args(args):
   # Split |args| into options for shim and options for swarming.py script.
   if '--' in args:
     index = args.index('--')
@@ -155,45 +221,32 @@ def main(args):
   if options.coverage_dir and not options.sancov_merger:
     parser.error('--sancov-merger is required for merging coverage data')
 
-  # Prepare a directory to store JSON files fetched from isolate.
-  task_output_dir = tempfile.mkdtemp(
-      suffix='_swarming', dir=options.temp_root_dir)
+  return options, swarming_args
 
-  # Start building the command line for swarming.py.
-  args = [
-    'swarming',
-  ]
 
-  args.extend(swarming_args)
-  args.extend([
-    '-output-dir', task_output_dir,
-    '-task-summary-json',
-    os.path.join(task_output_dir, 'summary.json'),
-  ])
+def main(args):
+  options, swarming_args = parse_args(args)
+  cmd, output_dir = swarming_cmd(swarming_args, options)
 
   exit_code = 1
   try:
     # Run the real script, regardless of an exit code try to find and parse
     # JSON output files, since exit code may indicate that the isolated task
     # failed, not the swarming.py invocation itself.
-    exit_code = subprocess.call(args)
+    exit_code = subprocess.call(cmd)
 
     # Output parsing should not change exit code no matter what, so catch any
     # exceptions and just log them.
     try:
-      with open(options.merged_test_output, 'wb') as f:
-        json.dump(
-            merge_shard_results(
-                task_output_dir,
-                options.sancov_merger,
-                options.coverage_dir),
-            f, separators=(',', ':'))
+      shards = get_shards_info(output_dir)
+      merge_test_results(output_dir, shards, options)
+      merge_coverage_data(output_dir, shards, options)
     except Exception:
       emit_warning(
           'failed to process v8 output JSON', traceback.format_exc())
 
   finally:
-    shutil.rmtree(task_output_dir, ignore_errors=True)
+    shutil.rmtree(output_dir, ignore_errors=True)
 
   return exit_code
 
