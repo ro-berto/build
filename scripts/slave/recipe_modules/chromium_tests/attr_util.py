f2d6aeacf2288b1fd058551c4030ee109c4bc415
Garrett Beaty <gbeaty@chromium.org>
2020-02-14 22:14:57 +0000

    Remove chromium_tests/attr_util.py.
    Bug: 1051608
    Change-Id: Ie7aa2ae5b1888a3198d5eea3e8369923bf1010e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2056992
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/attr_util.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2020 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-"""Utilities to simplify declaring attr-created types."""
-
-# TODO(gbeaty) Switch other importers to use the attr_utils recipe module
-from RECIPE_MODULES.build.attr_utils import *  # pylint: disable=wildcard-import
