d61d5e8d749b67e08caee66f5f87bdcf2487fc43
Mirko Bonadei <mbonadei@chromium.org>
2020-02-18 19:12:08 +0000

    Remove --gerrit flag.
    This flag was a noop and has been removed in [1].
    
    [1] - https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/2040572
    
    Fixed: webrtc:11369
    Change-Id: I89a77314a7ae79cc882eae52c9e16f1190ec480e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2062968
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>

--- scripts/slave/recipes/webrtc/auto_roll_webrtc_deps.py
+++ scripts/slave/recipes/webrtc/auto_roll_webrtc_deps.py
@@ -78,7 +78,7 @@ def RunSteps(api):
       else:
         with api.context(env={'SKIP_GCE_AUTH_FOR_GIT': '1'}):
           with api.depot_tools.on_path():
-            api.git('cl', 'set-close', '--gerrit', '-i', commits[0]['_number'])
+            api.git('cl', 'set-close', '-i', commits[0]['_number'])
           api.step.active_result.presentation.step_text = (
               'Stale roll found. Abandoned.')
 
