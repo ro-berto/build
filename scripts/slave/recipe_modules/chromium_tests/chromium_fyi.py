e6c7b6d5dae8423c3bdb2c5cec82a243119c476b
Mikhail Khokhlov <khokhlov@google.com>
2020-03-27 17:43:23 +0000

    Move perfetto bots to chromium_fyi
    Bug: 1050517
    Change-Id: I33a0954da59b1c87609898684afd88104a8fe5c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2123475
    Reviewed-by: Aaron Gable <agable@chromium.org>
    Commit-Queue: Aaron Gable <agable@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_fyi.py
@@ -860,6 +860,48 @@ SPEC = {
                     'platform': 'win',
                 },
             ),
+        'linux-upload-perfetto':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb', 'mb_luci_auth'],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'linux',
+                },
+            ),
+        'mac-upload-perfetto':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb', 'mb_luci_auth'],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'mac',
+                },
+            ),
+        'win-upload-perfetto':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb', 'mb_luci_auth'],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                bot_type=bot_spec.BUILDER_TESTER,
+                testing={
+                    'platform': 'win',
+                },
+            ),
     },
 }
 
--- scripts/slave/recipe_modules/chromium_tests/chromium_mac.py
+++ scripts/slave/recipe_modules/chromium_tests/chromium_mac.py
@@ -291,22 +291,5 @@ SPEC = {
                     'platform': 'mac',
                 },
             ),
-        'mac-upload-perfetto':
-            bot_spec.BotSpec.create(
-                chromium_config='chromium',
-                chromium_apply_config=[
-                    'mb',
-                    'mb_luci_auth',
-                ],
-                gclient_config='chromium',
-                chromium_config_kwargs={
-                    'BUILD_CONFIG': 'Release',
-                    'TARGET_BITS': 64,
-                },
-                bot_type=bot_spec.BUILDER_TESTER,
-                testing={
-                    'platform': 'mac',
-                },
-            ),
     },
 }
