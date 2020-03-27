e2572f30c98e58d4bb2cdf00717b187390387477
Mirko Bonadei <mbonadei@chromium.org>
2020-03-27 10:27:59 +0000

    Reland "Add 'gn analyze' step to WebRTC trybots."
    This is a reland of 4846af3189137d42de142b58f9ea9ecabcf0f58e
    
    Original change's description:
    > Add 'gn analyze' step to WebRTC trybots.
    > 
    > Since trybots are used by CQ and they gate submit they are time
    > sensitive (e.g. if you change a file in docs/ you don't to build and
    > test everything), this CL intoduces a 'gn analyze' step to ask GN
    > which binaries and tests are affected by the current CL. This way it
    > is possible to only build and test affected targets.
    > 
    > Bug: webrtc:11262
    > Change-Id: I9f4c661d3b51e93f8362ca8d66b3832fa20276ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2089768
    > Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    > Reviewed-by: Patrik Höglund <phoglund@chromium.org>
    
    Bug: webrtc:11262
    Change-Id: I3045f29068bed11ba2c41686e73b57b0d22eba39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2123469
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>

--- scripts/slave/recipes/webrtc/ios.py
+++ scripts/slave/recipes/webrtc/ios.py
@@ -492,6 +492,12 @@ def RunSteps(api):
   api.chromium.runhooks()
 
   webrtc.check_swarming_version()
+
+  if not webrtc.is_compile_needed(is_ios=True):
+    step_result = api.step('No further steps are necessary.', cmd=None)
+    step_result.presentation.status = api.step.SUCCESS
+    return
+
   webrtc.configure_isolate()
 
   with api.step.nest('apply build config'):
@@ -528,7 +534,9 @@ def RunSteps(api):
 
 def GenTests(api):
   builders = BUILDERS
-  generate_builder = functools.partial(api.webrtc.generate_builder, builders)
+  recipe_configs = RECIPE_CONFIGS
+  generate_builder = functools.partial(api.webrtc.generate_builder, builders,
+                                       recipe_configs)
 
   for bucketname in builders.keys():
     master_config = builders[bucketname]
@@ -551,3 +559,11 @@ def GenTests(api):
     api.post_process(post_process.StatusFailure) +
     api.post_process(post_process.DropExpectation)
   )
+
+  gn_analyze_no_deps_output = {'status': ['No dependency']}
+  yield generate_builder(
+      'luci.webrtc.try',
+      'ios_compile_arm_dbg',
+      revision='a' * 40,
+      suffix='_gn_analyze_no_dependency',
+      gn_analyze_output=gn_analyze_no_deps_output)
