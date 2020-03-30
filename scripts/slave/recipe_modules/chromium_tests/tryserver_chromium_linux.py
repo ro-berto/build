a0c15b632057d9718c2afb81630fdfe963ea7b56
Mathias Carlen <mcarlen@chromium.org>
2020-03-30 09:19:24 +0000

    Add config for linux-autofill-assistant build bot.
    Bug: b/152289442
    Change-Id: I0f42e91337f416736a64b68d6df54e84cdc146bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2123474
    Reviewed-by: Aaron Gable <agable@chromium.org>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_linux.py
+++ scripts/slave/recipe_modules/chromium_tests/tryserver_chromium_linux.py
@@ -6,6 +6,21 @@ from . import bot_spec
 
 SPEC = {
     'builders': {
+        'linux-autofill-assistant':
+            bot_spec.BotSpec.create(
+                chromium_config='chromium',
+                chromium_apply_config=['mb', 'mb_luci_auth'],
+                gclient_config='chromium',
+                chromium_config_kwargs={
+                    'BUILD_CONFIG': 'Release',
+                    'TARGET_BITS': 64,
+                },
+                tests=[],
+                test_results_config='staging_server',
+                testing={
+                    'platform': 'linux',
+                },
+            ),
         'linux-layout-tests-edit-ng':
             bot_spec.BotSpec.create(
                 chromium_config='chromium',
