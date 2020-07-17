978da95ef0d8b12aba92498697b1685d2029c1d7
Sven Zheng <svenzheng@chromium.org>
2020-07-17 01:56:36 +0000

    Fix linux-lacros-rel tryjob failure
    Currently tryjob failed with:
    No configuration present for builder BuilderId(master='tryserver.chromium.linux', builder=u'linux-lacros-rel')
    
    Bug: 1104291
    Change-Id: I63c16e26ab7b88c348fd61db59bdd26cf904a0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2303175
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/trybots.py
+++ scripts/slave/recipe_modules/chromium_tests/trybots.py
@@ -896,6 +896,12 @@ TRYBOTS = try_spec.TryDatabase.create({
                 buildername='GPU Linux Builder',
                 tester='Linux Release (NVIDIA)',
             ),
+        'linux-lacros-rel':
+            try_spec.TrySpec.create_for_single_mirror(
+                mastername='chromium.fyi',
+                buildername='linux-lacros-builder-rel',
+                tester='linux-lacros-tester-rel',
+            ),
         'linux-trusty-rel':
             try_spec.TrySpec.create_for_single_mirror(
                 mastername='chromium.linux',
@@ -964,12 +970,6 @@ TRYBOTS = try_spec.TryDatabase.create({
             try_spec.TrySpec.create_for_single_mirror(
                 mastername='chromium.chromiumos',
                 buildername='linux-chromeos-rel',
-            ),
-        'linux-lacros-rel':
-            try_spec.TrySpec.create_for_single_mirror(
-                mastername='chromium.fyi',
-                buildername='linux-lacros-builder-rel',
-                tester='linux-lacros-tester-rel',
             ),
         # Manually triggered GPU trybots.
         'gpu-fyi-try-chromeos-amd64-generic':
