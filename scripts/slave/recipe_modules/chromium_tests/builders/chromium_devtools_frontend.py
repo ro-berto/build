cb38781d0ac9e4d055e3f221293334b459580f89
Takuto Ikuta <tikuta@chromium.org>
2020-08-25 19:08:41 +0000

    flip devtools frontend builders to send swarming command lines.
    Bug: 1108005
    Change-Id: Ie62abe66ce7bf486fa9934a4cb595e5a1ebeaf8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2371790
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>

--- scripts/slave/recipe_modules/chromium_tests/builders/chromium_devtools_frontend.py
+++ scripts/slave/recipe_modules/chromium_tests/builders/chromium_devtools_frontend.py
@@ -17,6 +17,8 @@ SPEC = {
         _chromium_devtools_frontend_spec(
             chromium_config='chromium',
             chromium_apply_config=['mb'],
+            chromium_tests_apply_config=['use_swarming_command_lines'],
+            isolate_server='https://isolateserver.appspot.com',
             gclient_config='chromium',
             chromium_config_kwargs={
                 'BUILD_CONFIG': 'Release',
