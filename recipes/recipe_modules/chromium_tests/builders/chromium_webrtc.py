f78110a5ab5c68dd1baab7b8d4e30172aaeb3fa8
Ye Kuang <yekuang@google.com>
2020-10-19 07:59:54 +0000

    Enable 'use_swarming_recipe_to_trigger' for webrtc builders
    I've added the isolated property in https://webrtc-review.googlesource.com/c/src/+/188560
    
    Bug: 894045
    Change-Id: Ifd4a9944370296de4d02137ad96023a0465a850f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2484066
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Ye Kuang <yekuang@google.com>

--- recipes/recipe_modules/chromium_tests/builders/chromium_webrtc.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_webrtc.py
@@ -38,7 +38,10 @@ def browser_perf_test(perf_id):
 
 
 def _chromium_webrtc_spec(**kwargs):
-  return bot_spec.BotSpec.create(build_gs_bucket='chromium-webrtc', **kwargs)
+  return bot_spec.BotSpec.create(
+      build_gs_bucket='chromium-webrtc',
+      chromium_tests_apply_config=['use_swarming_recipe_to_trigger'],
+      **kwargs)
 
 
 SPEC = {
