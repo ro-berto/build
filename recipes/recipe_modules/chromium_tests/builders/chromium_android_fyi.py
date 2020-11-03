2a2961bd8719401fcc8448b007ba3a70801e83b1
Rakib M. Hasan <rmhasan@google.com>
2020-11-03 19:32:27 +0000

    weblayer, skew tests: Remove deprecated Android 10 tester
    This CL removes an Android 10 tester which was replaced another
    tester which has a name that follows the
    android-$feature-$version-$architecture-$build_type[-tests]
    format.
    
    Bug:1114563
    Change-Id: I8a6a20ccc3c1d4429ee82ca6562936fc8f651642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2513750
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>

--- recipes/recipe_modules/chromium_tests/builders/chromium_android_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_android_fyi.py
@@ -131,23 +131,6 @@ SPEC = {
             android_config='x86_builder',
             simulation_platform='linux',
         ),
-    'android-weblayer-x86-fyi-rel-10-tests':
-        _chromium_android_fyi_spec(
-            chromium_config='android',
-            chromium_apply_config=['mb'],
-            isolate_server='https://isolateserver.appspot.com',
-            gclient_config='chromium',
-            gclient_apply_config=['android'],
-            chromium_config_kwargs={
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 32,
-                'TARGET_PLATFORM': 'android',
-            },
-            parent_buildername='android-weblayer-x86-fyi-rel',
-            execution_mode=bot_spec.TEST,
-            android_config='x86_builder',
-            simulation_platform='linux',
-        ),
     'android-weblayer-10-x86-rel-tests':
         _chromium_android_fyi_spec(
             chromium_config='android',
