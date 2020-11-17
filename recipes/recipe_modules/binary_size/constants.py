0d44aecfdc24a2f3dd16b8468e85d06fca7d0c72
Samuel Huang <huangs@chromium.org>
2020-11-17 21:07:31 +0000

    [Binary Size] android-binary-size: Switch to Trichrome.
    This CL changes android-binary-size to analyze Trichrome instead of
    MonochromePublic. Both are already being built.
    
    To facilitate the transition and avoid diffing cached MonochromePublic
    .size files with new Trichrome .size files, prior to landing this change
    we should invalidate the gs:// cache of "before patch" builds (created
    by android-binary-size-generator) by doing the following:
    * Truncate
      gs://chromium-binary-size-trybot-results/android-binary-size/commit_size_analysis/LATEST
      to size 0.
    * Cancel current job for android-binary-size-generator bot, and pause
      the bot (to be resumed after this CL lands).
    
    Bug: 1040645
    Change-Id: Ib6551abe8a60342ac0d43580ea269d5c68b2f684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2545043
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>

--- recipes/recipe_modules/binary_size/constants.py
+++ recipes/recipe_modules/binary_size/constants.py
@@ -12,8 +12,9 @@ DEFAULT_COMPILE_TARGETS = [
     'monochrome_static_initializers',
     'trichrome_minimal_apks',
 ]
+
 # Path is relative to Chromium output directory.
-DEFAULT_SIZE_CONFIG_JSON = 'config/MonochromePublic_size_config.json'
+DEFAULT_SIZE_CONFIG_JSON = 'config/Trichrome_size_config.json'
 
 EXPECTATIONS_STEP_NAME = 'Checking for expectation failures'
 
