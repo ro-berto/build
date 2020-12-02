928f74cfaae4c7671a11d30f57c7a12f04065724
Gary Tong <gatong@chromium.org>
2020-12-02 01:55:19 +0000

    Infra: Adding example builder for Noogler talk
    Change-Id: I2f77032572674676f8af4485664ca5e5e518785f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2568644
    Auto-Submit: Gary Tong <gatong@chromium.org>
    Reviewed-by: Garrett Beaty <gbeaty@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_fyi.py
@@ -571,6 +571,18 @@ SPEC = {
             },
             simulation_platform='linux',
         ),
+    'linux-example-builder':
+        bot_spec.BotSpec.create(
+            chromium_config='chromium',
+            chromium_apply_config=['mb'],
+            isolate_server='https://isolateserver.appspot.com',
+            gclient_config='chromium',
+            chromium_config_kwargs={
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 64,
+            },
+            simulation_platform='linux',
+        ),
     'linux-chromeos-js-code-coverage':
         bot_spec.BotSpec.create(
             chromium_config='chromium',
