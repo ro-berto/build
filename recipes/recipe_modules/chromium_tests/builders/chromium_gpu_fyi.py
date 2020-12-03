ee49a3942366c6844b576863be8019455e2db8ca
Brian Sheedy <bsheedy@chromium.org>
2020-12-03 01:22:04 +0000

    Decommission "Win10 FYI x64 Release (AMD RX 550)"
    We already have stable tests running on RX 5500 XTs with more capacity,
    so no reason to keep the old AMD builder around.
    
    Bug: 1154895
    Change-Id: I7b7005fd72476caa2ec2258041022416a6e94e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2570170
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_gpu_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_gpu_fyi.py
@@ -339,22 +339,6 @@ SPEC = {
             simulation_platform='win',
             serialize_tests=True,
         ),
-    'Win10 FYI x64 Release (AMD RX 550)':
-        _chromium_gpu_fyi_spec(
-            chromium_config='chromium',
-            chromium_apply_config=[
-                'mb',
-            ],
-            gclient_config='chromium',
-            chromium_config_kwargs={
-                'BUILD_CONFIG': 'Release',
-                'TARGET_BITS': 64,
-            },
-            execution_mode=bot_spec.TEST,
-            parent_buildername='GPU FYI Win x64 Builder',
-            simulation_platform='win',
-            serialize_tests=True,
-        ),
     'Win10 FYI x64 Release (AMD RX 5500 XT)':
         _chromium_gpu_fyi_spec(
             chromium_config='chromium',
