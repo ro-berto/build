5b3bb954ea11e849e8c9f362e2eaddba6ec87498
George Burgess IV <gbiv@chromium.org>
2020-12-07 23:40:07 +0000

    tricium_clang_tidy_wrapper: remove unused imports
    these were left lying around after a refactor. since they're now unused,
    remove them.
    
    Bug: None
    Change-Id: I750d30266ad0238a3775e8038e44ef5f234bc54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2575510
    Auto-Submit: George Burgess <gbiv@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>

--- recipes/recipes/tricium_clang_tidy_wrapper.py
+++ recipes/recipes/tricium_clang_tidy_wrapper.py
@@ -2,9 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import collections
-import json
-
 from recipe_engine import post_process
 from RECIPE_MODULES.build.tricium_clang_tidy import _clang_tidy_path
 
