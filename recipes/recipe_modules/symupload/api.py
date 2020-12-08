db71a0d857d6a39c6987b9b93099efb541c40566
Jeff Yoon <jeffyoon@google.com>
2020-12-08 23:44:49 +0000

    Revert "Support for V2 symupload in Symupload recipe module"
    This reverts commit af02d8489fa5e46eb44890ae98b9b437373f2902.
    
    Reason for revert: I need to fix the script. Reverting this.
    
    Original change's description:
    > Support for V2 symupload in Symupload recipe module
    >
    > Symupload V2 protocol is invoked by passing in additional arguments
    > (-p for win, -p sym-upload-v2 for linux/mac) alongside an api_key.
    > Updating the symupload recipe module to accept a path to the api_key
    > that's read and passed to the symupload command if provided. Passing
    > the api_key will default usage to V2 protocol. Otherwise, default
    > to V1 behavior.
    >
    > Platform checks are changed from target_platform to host_platform.
    > Sanity checks for the api key are done by length, like the
    > official_symbols.py script referenced in comment.
    >
    > Bug: 912681
    > Change-Id: I97f291144095e01d393b0c40403fcf7b35a6970d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2569925
    > Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    > Reviewed-by: Michael Moss <mmoss@chromium.org>
    
    TBR=mmoss@chromium.org,jeffyoon@google.com,infra-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I0a71b6ef329368c1d1c1542eda568d1a49d4c418
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 912681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2579699
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>

--- recipes/recipe_modules/symupload/api.py
+++ recipes/recipe_modules/symupload/api.py
@@ -12,60 +12,40 @@ class SymuploadApi(recipe_api.RecipeApi):
     super(SymuploadApi, self).__init__(**kwargs)
     self._properties = properties
 
-  @property
-  def platform(self):
-    return self.m.chromium.c.HOST_PLATFORM
-
-  def symupload(self, symupload_binary, artifact, server_url):
-    """Uploads the given symbols file with the V1 protocol.
-
-    Note that V1 is deprecated and will be removed once all consumers migrate
-    to V2.
+  def symupload(self, binary, artifact, url):
+    """Uploads the given symbols file.
 
     Args:
       artifact: Name of the artifact to upload. Will be found relative to the
         out directory, so must have already been compiled.
       url: URL of the symbol server to upload to.
-      api_key: API key used for Symupload V2
     """
-    cmd = [symupload_binary]
-    # This logic below ported from:
-    # https://chrome-internal.googlesource.com/chrome/tools/release/scripts/+/
-    # 7dde2e2b66e163680e6a30153096fc4de422e7e3/recipes/recipe_modules/chrome/
-    # resources/official_utils.py#96
-    if self.platform.startswith('win'):
-      cmd.extend(['--timeout', '0', artifact, server_url])
-    else:
-      cmd.extend([artifact, server_url])
-
+    cmd = [
+        binary,
+        artifact,
+        url,
+    ]
     self.m.step('symupload %s' % artifact, cmd)
 
   @property
   def symupload_binary(self):
+    platform = self.m.chromium.c.TARGET_PLATFORM
     binary_name = 'symupload'
-    if self.platform.startswith('win'):
+    if platform.startswith('win'):
       binary_name = 'symupload.exe'
 
     return binary_name
 
-  def __call__(self,
-               build_dir,
-               api_key_path=None,
-               kms_crypto_key=None,
-               experimental=False):
+  def __call__(self, build_dir):
     """
     Args:
       build_dir: The absolute path to the build output directory, e.g.
-                 [slave-build]/src/out/Release.
-      kms_crypto_key: (str) The name of the encryption key.
-      api_key_path: (Path) to the input (ciphertext) file for symupload V2.
-      experimental: (bool) flag for experimental, meaning it will skip
-                    symuploads.
+                 [slave-build]/src/out/Release
     """
     if not self._properties.symupload_datas:
       return
 
-    with self.m.step.nest('symupload') as presentation:
+    with self.m.step.nest('symupload'):
       # Check binary before moving on
       symupload_binary = self.m.path.join(build_dir, self.symupload_binary)
       if not self.m.path.exists(symupload_binary):
@@ -74,18 +54,9 @@ class SymuploadApi(recipe_api.RecipeApi):
                                       'are being built such that the binaries '
                                       'are generated.' % str(symupload_binary))
 
-      output_api_key = None
-      if kms_crypto_key and api_key_path:
-        output_api_key = self.m.path['cleanup'].join('symupload-api-key.txt')
-        self.m.cloudkms.decrypt(kms_crypto_key, api_key_path, output_api_key)
-
-        api_key = self.m.file.read_raw(
-            'read api key', output_api_key, test_data='test_key')
-        presentation.logs['api_key sanity check'] = str(len(api_key))
-
       uploads = []
       for symupload_data in self._properties.symupload_datas:
-        # Gather all artifacts that are needed to upload via file glob.
+        # File globs
         for filename in symupload_data.file_globs:
           for f in self.m.file.glob_paths(
               'expand file globs',
@@ -97,45 +68,12 @@ class SymuploadApi(recipe_api.RecipeApi):
             assert build_dir.base == f.base
             assert build_dir.is_parent_of(f)
             common_pieces = f.pieces[len(build_dir.pieces):]
-            uploads.append('/'.join(common_pieces))
+            uploads.append(('/'.join(common_pieces), symupload_data.url))
 
         if symupload_data.artifact:
-          uploads.append(self.m.path.join(build_dir, symupload_data.artifact))
+          uploads.append(
+              (self.m.path.join(build_dir,
+                                symupload_data.artifact), symupload_data.url))
 
-        if experimental:
-          presentation.step_text = 'Experimental mode. Skipping symupload.'
-          presentation.status = self.m.step.SUCCESS
-          return
-
-        # Use the symupload resource to mask the API key for v2 protocol.
-        # Because the symupload script accepts all urls to upload for a set of
-        # artifacts, we'll invoke it here for each symupload data config.
-        if output_api_key:
-          self.m.build.python(
-              'symupload_v2',
-              self.resource('symupload.py'),
-              [
-                  '--artifacts',
-                  ','.join(uploads),
-                  '--api-key-file',
-                  output_api_key,
-                  '--binary-path',
-                  symupload_binary,
-                  '--platform',
-                  self.platform,
-                  '--server-urls',
-                  # the recipe currently only supports one url at the
-                  # moment, so this should be reworked to pass it a
-                  # comma-delimited list of urls when supported.
-                  symupload_data.url,
-              ])
-        else:
-          # We'll continue to invoke the binary directly for V1.
-          # TODO: Note that V1 is deprecated and will be removed once all
-          # consumers migrate to V2.
-          for artifact in uploads:
-            self.symupload(
-                symupload_binary,
-                artifact,  # artifact
-                symupload_data.url,
-            )
+      for artifact in uploads:
+        self.symupload(symupload_binary, artifact[0], artifact[1])
