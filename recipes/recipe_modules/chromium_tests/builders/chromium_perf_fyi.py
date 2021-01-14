9d4d36c39ab6b7e3f54c426fc47f2593df57092c
Takuto Ikuta <tikuta@chromium.org>
2021-01-14 06:03:29 +0000

    Revert "chromium_tests: switch to go client for perf builders"
    This reverts commit 591fe42ced985e0a07ea390faff48013c0af62f1.
    
    Reason for revert: 
    https://ci.chromium.org/p/chrome/builders/ci/win-10-perf/11938
    There is a delay taking src side change https://crrev.com/c/2626686.
    
    Original change's description:
    > chromium_tests: switch to go client for perf builders
    >
    > Testers in fyi seems working fine.
    > https://ci.chromium.org/p/chrome/g/chrome.perf.fyi/console
    > e.g.
    > https://ci.chromium.org/p/chrome/builders/ci/linux-perf-fyi
    >
    > Bug: 1127205
    > Change-Id: Ie8e43458df8fb33bf5bb47bee0ecbd270350dccb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2626862
    > Commit-Queue: Junji Watanabe <jwata@google.com>
    > Reviewed-by: Junji Watanabe <jwata@google.com>
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    
    TBR=tikuta@chromium.org,jwata@google.com,yekuang@google.com,infra-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I58c3304c00f8455c11c4d46a2b95b6171c023aa2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1127205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2628867
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_perf_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_perf_fyi.py
@@ -45,6 +45,9 @@ def _AddIsolatedTestSpec(name,
   if parent_builder_group:
     spec = spec.evolve(parent_builder_group=parent_builder_group)
 
+  SPEC[name] = spec.extend(
+      chromium_tests_apply_config=['use_swarming_go_in_trigger_script'])
+
 
 _AddIsolatedTestSpec(
     'android-nexus5x-perf-fyi',
