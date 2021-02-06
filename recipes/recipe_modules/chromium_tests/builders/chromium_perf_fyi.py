c0c9bb7508b80e95aa027e37e8cb169c4d828f42
Rohan Pavone <rohpavone@chromium.org>
2021-02-06 00:08:08 +0000

    Turn fuchsia-perf-fyi back on
    Bug: 1128105
    Change-Id: I236ef49017934237edcdc752b751ce2ad014f652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2676828
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Rohan Pavone <rohpavone@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_perf_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_perf_fyi.py
@@ -86,13 +86,13 @@ _AddBuildSpec(
     target_arch='arm',
     extra_gclient_apply_config=['fuchsia_arm64', 'fuchsia_arm64_host'])
 
-#_AddIsolatedTestSpec(
-#    'fuchsia-perf-fyi',
-#    'fuchsia',
-#    target_bits=64,
-#    target_arch='arm',
-#    parent_buildername='fuchsia-builder-perf-fyi',
-#    parent_builder_group='chromium.perf.fyi')
+_AddIsolatedTestSpec(
+    'fuchsia-perf-fyi',
+    'fuchsia',
+    target_bits=64,
+    target_arch='arm',
+    parent_buildername='fuchsia-builder-perf-fyi',
+    parent_builder_group='chromium.perf.fyi')
 
 _AddIsolatedTestSpec(
     'linux-perf-fyi',
