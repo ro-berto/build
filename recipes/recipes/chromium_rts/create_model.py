ae588d5858c9a5622c7e7f9ad17defe5df8485db
Nodir Turakulov <nodir@chromium.org>
2021-02-16 23:50:31 +0000

    [rts] Fix create_model.py
    1) Upload packages for all platforms, not only the current one
    2) Actually add files to the model.
    
    Bug: 1172372
    Change-Id: I412eb99ad584b5c29480a97ce5410ce6940f691b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2697119
    Auto-Submit: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Gregory Guterman <guterman@google.com>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- recipes/recipes/chromium_rts/create_model.py
+++ recipes/recipes/chromium_rts/create_model.py
@@ -21,21 +21,30 @@ DEPS = [
     'recipe_engine/time',
 ]
 
-MODEL_CIPD_PACKAGE = 'chromium/rts/model'
-RTS_EXEC_PACKAGE = 'chromium/rts/rts-chromium/${platform}'
+MODEL_CIPD_PREFIX = 'chromium/rts/model/'
+RTS_EXEC_CIPD_PREFIX = 'chromium/rts/rts-chromium/'
+PLATFORMS = [
+    'linux-amd64',
+    'mac-amd64',
+    'windows-amd64',
+]
 REJECTION_DATA_WINDOW = datetime.timedelta(weeks=12)
 TEST_DURATION_DATA_WINDOW = datetime.timedelta(weeks=1)
 # Processing 10% of 1w-worth test durations takes 7h on a 32-core bot.
 TEST_DURATION_DATA_PERCENTAGE = 1
-RTS_EXECUTABLE_VERSION = 'latest'
 
 
 def RunSteps(api):
   # Start bot_update in background.
   checkout_dir_fut = api.futures.spawn_immediate(checkout_chromium, api)
 
-  # Install rts-chromium.
-  exec_path = api.cipd.ensure_tool(RTS_EXEC_PACKAGE, RTS_EXECUTABLE_VERSION)
+  # Install rts-chromium executables.
+  exec_pkg_paths = install_rts_executables(api)
+
+  assert api.platform.is_linux
+  assert api.platform.arch == 'intel'
+  assert api.platform.bits == 64
+  exec_path = exec_pkg_paths['linux-amd64'].join('rts-chromium')
 
   # Fetch the dataset.
   # Ignore today because we might fetch incomplete data.
@@ -70,25 +79,98 @@ def RunSteps(api):
   # TODO(crbug.com/1172372): ensure the new model is not significantly worse
   # than the current one.
 
-  # Add the executable to the model.
-  exe_ext = '.exe' if api.platform.is_win else ''
-  api.file.copy(
-      'add rts-chromium to the model',
-      exec_path,
-      model_dir.join('rts-chromium' + exe_ext),
+  # Create CIPD packages.
+  futures = []
+  with api.step.nest('Create CIPD packages'):
+    for platform, pkg_dir in exec_pkg_paths.iteritems():
+      futures.append(
+          api.futures.spawn_immediate(create_cipd_package, api, platform,
+                                      pkg_dir, model_dir))
+  # Check success.
+  for f in futures:
+    f.result()
+
+
+def create_cipd_package(api, platform, exec_pkg_dir, model_dir):
+  with api.step.nest('Upload CIPD package - %s' % platform):
+    # Copy the model files, such that create_cipd_package can be called for
+    # different platforms concurrently.
+    # Note that we add a platform-specific executable below.
+    pkg_dir = api.path['cleanup'].join('model-pkg-dir', platform)
+    api.file.copytree('Copy the model files', model_dir, pkg_dir)
+
+    # Add the executable to the model.
+    exe_ext = '.exe' if platform.startswith('windows-') else ''
+    exe_base_name = 'rts-executable' + exe_ext
+    api.file.copy(
+        'Include the executable',
+        exec_pkg_dir.join(exe_base_name),
+        pkg_dir.join(exe_base_name),
+    )
+
+    # Upload to CIPD.
+    pkg = api.cipd.PackageDefinition(
+        package_name=MODEL_CIPD_PREFIX + platform,
+        package_root=pkg_dir,
+    )
+    pkg.add_dir(pkg_dir)
+    api.cipd.create_from_pkg(
+        pkg,
+        refs=['latest'],
+        tags={
+            'build': 'https://ci.chromium.org/b/%d' % api.buildbucket.build.id,
+        },
+    )
+
+
+def install_rts_executables(api):
+  """Installs rts-chromium executables for all platforms.
+
+  Returns:
+    Mapping {platform: package_path}, where
+    - platform is a CIPD platform, e.g. "linux-amd64".
+    - package_path is a api.path.Path object.
+  """
+  ver = pick_executable_version(api)
+  install_dir = api.path['cleanup'].join('rts-chromium')
+
+  ret = {}
+  ensure_file = api.cipd.EnsureFile()
+  for plat in PLATFORMS:
+    ensure_file.add_package(
+        name=RTS_EXEC_CIPD_PREFIX + plat,
+        version=ver,
+        subdir=plat,
+    )
+    ret[plat] = install_dir.join(plat)
+
+  api.cipd.ensure(install_dir, ensure_file, name='install RTS executables')
+  return ret
+
+
+def pick_executable_version(api):
+  """Returns the CIPD version of rts-executable CIPD packages to use."""
+
+  # Find the git_revision of the latest linux-amd64 package.
+  # This guarantees that packages for all platforms are built from the same
+  # source code.
+  descr = api.cipd.describe(
+      RTS_EXEC_CIPD_PREFIX + 'linux-amd64',
+      'latest',
+      test_data_tags=['git_revision:c1ee0e03d15281730ebedf1f7151474f7a523001'],
   )
 
-  # Upload to CIPD.
-  api.cipd.create_from_pkg(
-      api.cipd.PackageDefinition(
-          package_name=MODEL_CIPD_PACKAGE,
-          package_root=model_dir,
-      ),
-      refs=['latest'],
-      tags={
-          'build': 'https://ci.chromium.org/b/%d' % api.buildbucket.build.id,
-      },
-  )
+  # Pick any git_revision.
+  # It doesn't matter which one if they produced the same instance hash.
+  for t in descr.tags:
+    if t.tag.startswith('git_revision:'):
+      return t.tag
+
+  raise api.step.StepFailure(
+      # pragma: no cover
+      'git_revision tag not found in '
+      'https://chrome-infra-packages.appspot.com/p/'
+      'chromium/rts/rts-chromium/linux-amd64/+/latest')
 
 
 def checkout_chromium(api):
@@ -146,4 +228,9 @@ def _date_range_flags(date_range):
 
 
 def GenTests(api):
-  yield api.test('basic')
+  linux_amd64 = (
+      api.platform.name('linux') + \
+      api.platform.arch('intel') +
+      api.platform.bits(64))
+
+  yield api.test('basic') + linux_amd64
