57b69fafa1f57b93096d81d8ef41dff28ead2011
Ben Pastene <bpastene@chromium.org>
2021-02-18 20:50:44 +0000

    Add recipe configs for new linux-extended-tracing-rel CI + try bot.
    Just a normal linux rel builder with extended_tracing_enabled=true.
    
    Bug: 1143485
    Change-Id: I6848354aec60bd567b1aec70be4facec6d60b475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2705278
    Reviewed-by: Garrett Beaty <gbeaty@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_linux.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_linux.py
@@ -65,6 +65,18 @@ SPEC = {
             },
             simulation_platform='linux',
         ),
+    'linux-extended-tracing-rel':
+        _chromium_linux_spec(
+            chromium_config='chromium',
+            chromium_apply_config=['mb'],
+            isolate_server='https://isolateserver.appspot.com',
+            gclient_config='chromium',
+            chromium_config_kwargs={
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 64,
+            },
+            simulation_platform='linux',
+        ),
     'linux-gcc-rel':
         _chromium_linux_spec(
             chromium_config='chromium_no_goma',
