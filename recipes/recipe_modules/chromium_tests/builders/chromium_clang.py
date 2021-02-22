3f44510182888d559d6e372ec88b75e07e1ecb79
Xinan Lin <linxinan@chromium.org>
2021-02-22 07:15:19 +0000

    Revert "Remove recipe side test_specs config from ToTAndroid* builders"
    This reverts commit 81584e8623189f47e3e599421900fe9e27955520.
    
    Reason for revert: chrome_sizes caused all of the ToTAndroid bots to fail since it doesn't exist on Android.
    
    Original change's description:
    > Remove recipe side test_specs config from ToTAndroid* builders
    >
    > ToTAndroid* are including below builders:
    > - ToTAndroid
    > - ToTAndroid64
    > - ToTAndroidCFI
    > - ToTAndroidOfficial
    >
    > BUG=1150896
    > TEST=train
    >
    > Change-Id: I4b71b20a00daaa149a8eb2aa62f931b09b74337c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2702727
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Reviewed-by: Garrett Beaty <gbeaty@chromium.org>
    > Commit-Queue: Xinan Lin <linxinan@chromium.org>
    
    Bug: 1150896
    Change-Id: I6c6dd0fd8bf381e4abcca6b4ed17b7aaa734cb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2711018
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xinan Lin <linxinan@chromium.org>

--- recipes/recipe_modules/chromium_tests/builders/chromium_clang.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_clang.py
@@ -39,6 +39,9 @@ def config(name,
     cfg['android_config'] = android_config
     cfg['chromium_config_kwargs']['TARGET_PLATFORM'] = 'android'
     cfg['gclient_apply_config'].append('android')
+    cfg['test_specs'] = {
+        steps.SizesStepSpec.create(results_url=RESULTS_URL, perf_id=name)
+    }
 
   if official:
     cfg['gclient_apply_config'].append('chrome_internal')
