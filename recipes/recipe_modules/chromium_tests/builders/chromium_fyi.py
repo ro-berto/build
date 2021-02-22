9bb32eb66bc4e26e6f237b209745f5d6ffec5328
Michael Savigny <msavigny@google.com>
2021-02-22 14:35:39 +0000

    [reclient] Add 'TSAN Release (reclient)' and 'TSAN Debug (reclient)' shadows in chromium_tests.
    Bug: 1141780
    Change-Id: I9ef978746e74e28d6394e4955da2f1151ed1cc95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2705034
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Michael Savigny <msavigny@google.com>

--- recipes/recipe_modules/chromium_tests/builders/chromium_fyi.py
+++ recipes/recipe_modules/chromium_tests/builders/chromium_fyi.py
@@ -864,6 +864,36 @@ SPEC = {
             simulation_platform='linux',
             build_gs_bucket='chromium-memory-archive',
         ),
+    'TSAN Release (reclient)':
+        bot_spec.BotSpec.create(
+            chromium_config='chromium_clang',
+            chromium_apply_config=['mb', 'tsan2', 'clobber'],
+            isolate_server='https://isolateserver.appspot.com',
+            gclient_config='chromium',
+            gclient_apply_config=['enable_reclient'],
+            chromium_config_kwargs={
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 64,
+            },
+            # Set archive build to false since this is a shadow.
+            cf_archive_build=False,
+            simulation_platform='linux',
+        ),
+    'TSAN Debug (reclient)':
+        bot_spec.BotSpec.create(
+            chromium_config='chromium_clang',
+            chromium_apply_config=['mb', 'tsan2', 'clobber'],
+            isolate_server='https://isolateserver.appspot.com',
+            gclient_config='chromium',
+            gclient_apply_config=['enable_reclient'],
+            chromium_config_kwargs={
+                'BUILD_CONFIG': 'Debug',
+                'TARGET_BITS': 64,
+            },
+            # Set archive_build to false since this is a shadow.
+            cf_archive_build=False,
+            simulation_platform='linux',
+        ),
 }
 
 SPEC.update([
