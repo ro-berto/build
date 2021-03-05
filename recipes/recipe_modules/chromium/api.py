510ba03be63886210077a78916361f935d3f24ef
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2021-03-05 02:37:05 +0000

    Remove Goma Staging builder.
    Bug: b/143328098
    Recipe-Manual-Change: chrome_release
    Change-Id: I36595ab93f60c554cff4bf5b991674caa85a61a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2733375
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>

--- recipes/recipe_modules/chromium/api.py
+++ recipes/recipe_modules/chromium/api.py
@@ -129,8 +129,6 @@ class ChromiumApi(recipe_api.RecipeApi):
     if self.c.env.PATH:
       ret['PATH'] = self.m.path.pathsep.join(
           map(str, self.c.env.PATH) + ['%(PATH)s'])
-    if self.c.env.GOMA_SERVER_HOST:
-      ret['GOMA_SERVER_HOST'] = self.c.env.GOMA_SERVER_HOST
     if self.c.env.GOMA_STORE_ONLY:
       ret['GOMA_STORE_ONLY'] = self.c.env.GOMA_STORE_ONLY
     if self.c.env.FORCE_MAC_TOOLCHAIN:
