aefaa23f8c6f8bf21b56971344d232af16613d9c
Nico Weber <thakis@chromium.org>
2021-03-23 02:04:26 +0000

    goma_client recipe: Add some support for a bot that sets arch to arm
    With this setup, we'll build a dedicated cipd package for mac-arm64.
    It assumes that we'll teach build/archive.py in the client.git repo
    to accept `--platform=mac-arm64` for mac/arm64 builds.
    
    It also requires that we update cipd_manifests.txt in depot_tools to
    pull goma/client from ${platform} instead of from ${os}-amd64 like
    currently.
    
    And we'll have to create new bots that set platform.arch to 'arm',
    somehow, in
    https://chrome-internal.googlesource.com/infra/infra_internal.git/+/refs/heads/main/infra/config/subprojects/goma.star
    
    (And, if you want try converage for this, also in several of the files in
    https://chromium.googlesource.com/infra/goma/client/+/refs/heads/infra/config )
    
    Bug: b/183118231
    Change-Id: I0c619e5ba034f9b36f9579d21e31961baeac8160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2780615
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>

--- recipes/recipes/goma_client.py
+++ recipes/recipes/goma_client.py
@@ -35,7 +35,13 @@ def RunSteps(api):
 
   with api.osx_sdk('mac'):
     # 2-1. gn
-    gn_args = ['is_debug=false', 'cpu_arch="x64"', 'enable_revision_check=true']
+    gn_args = ['is_debug=false', 'enable_revision_check=true']
+    assert api.platform.bits == 64
+    if api.platform.arch == 'arm':
+      gn_args += ['target_cpu="arm64"']
+    else:
+      gn_args += ['cpu_arch="x64"']
+
     if api.tryserver.is_tryserver:
       gn_args.append('dcheck_always_on=true')
       gn_args.append('use_link_time_optimization=false')
@@ -54,26 +60,33 @@ def RunSteps(api):
     api.step('build', [api.depot_tools.ninja_path, '-C', build_dir])
 
     # 3. Run test
-    api.python(
-        name='tests',
-        script=api.path['checkout'].join('build', 'run_unittest.py'),
-        args=['--build-dir', build_out_dir,
-              '--target', build_target, '--non-stop'])
+    # Not on arm because we're cross-building and the intel builder can't run
+    # arm test binaries.
+    if api.platform.arch != 'arm':
+      api.python(
+          name='tests',
+          script=api.path['checkout'].join('build', 'run_unittest.py'),
+          args=['--build-dir', build_out_dir,
+                '--target', build_target, '--non-stop'])
 
   # 4. Create archive.
+  platform = api.platform.name
+  if api.platform.arch == 'arm':
+    platform += '-arm64'
   api.python(
       name='archive',
       script=api.path['checkout'].join('build', 'archive.py'),
-      args=['--platform', api.platform.name,
+      args=['--platform', platform,
             '--build_dir', build_out_dir,
             '--target_dir', build_target,
             '--dist_dir', api.path['tmp_base']])
 
   # 5. Build CIPD package.
   # archive.py creates goma-<platform>/ in out/Release.
-  root = build_out_dir.join(build_target, 'goma-%s' % api.platform.name)
+  root = build_out_dir.join(build_target, 'goma-%s' % platform)
   pkg_file = api.path['tmp_base'].join('package.cipd')
-  pkg_name = 'infra/goma/client/${platform}'
+  tag = '${platform}' if api.platform.arch == 'intel' else platform
+  pkg_name = 'infra/goma/client/' + tag
   api.cipd.build(root, pkg_file, pkg_name, install_mode='copy')
 
   # 6. Register CIPD package if prod.
@@ -85,21 +98,28 @@ def RunSteps(api):
 
 def GenTests(api):
   for platform in ['linux', 'mac', 'win']:
-    yield api.test(
-        'goma_client_try_%s_rel' % platform,
-        api.platform(platform, 64),
-        api.buildbucket.try_build(
-            builder='%s_rel' % platform,
-            git_repo='chromium.googlesource.com/infra/goma/client',
-            change_number=456789,
-            patch_set=12),
-    )
-    for bucket in ['prod', 'luci.goma-client.ci']:
+    archs = ['intel']
+    if platform == 'mac':
+      archs += ['arm']
+    for arch in archs:
+      bot_name = platform
+      if arch != 'intel':
+        bot_name += '_' + arch
       yield api.test(
-          'goma_client_%s_%s_rel' % (platform, bucket),
-          api.platform(platform, 64),
-          api.buildbucket.ci_build(
-              bucket=bucket,
+          'goma_client_try_%s_rel' % bot_name,
+          api.platform(platform, 64, arch),
+          api.buildbucket.try_build(
+              builder='%s_rel' % platform,
               git_repo='chromium.googlesource.com/infra/goma/client',
-              revision='8b3cd40a25a512033cc8c0797e41de9ecfc2432c'),
+              change_number=456789,
+              patch_set=12),
       )
+      for bucket in ['prod', 'luci.goma-client.ci']:
+        yield api.test(
+            'goma_client_%s_%s_rel' % (bot_name, bucket),
+            api.platform(platform, 64, arch),
+            api.buildbucket.ci_build(
+                bucket=bucket,
+                git_repo='chromium.googlesource.com/infra/goma/client',
+                revision='8b3cd40a25a512033cc8c0797e41de9ecfc2432c'),
+        )
