9f0b1b4bd30a89024e45515d70d326282e1288da
Wenbin Zhang <wenbinzhang@google.com>
2021-05-20 22:24:52 +0000

    [benchmarking] add calibration for pixel2
    Adding the spec for android-pixel2-perf-calibration.
    
    Bug:chromium:1140389
    Change-Id: Ifb1613cdde9d1fd567e23f6d5fa181d4a9dfd42e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2911201
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_perf_calibration.py
+++ recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_perf_calibration.py
@@ -35,3 +35,4 @@ def _AddCalibrationTestSpec(name, platform, target_bits):
 
 
 _AddCalibrationTestSpec('linux-perf-calibration', 'linux', 64)
+_AddCalibrationTestSpec('android-pixel2-perf-calibration', 'linux', 64)
