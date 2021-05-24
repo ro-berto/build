b498bdf48a297a7e71022213c65a5abc641f4919
Garrett Beaty <gbeaty@chromium.org>
2021-05-24 18:07:35 +0000

    Reland "Add a step with the gclient config."
    This is a reland of c57314f91f5345931e5e39c65a0e3235fc26e61d
    
    The output for revision resolvers has been updated so that
    deterministic expectation files would be produced.
    
    Original change's description:
    > Add a step with the gclient config.
    >
    > Many issues that we deal with involve having to debug gclient configs,
    > which usually involves trying to work backwards from the logs and/or
    > trying to work forward from the builder spec to determine what the
    > gclient config is. Providing an easy way to see what the actual value
    > of the gclient config is will simplify debugging such issues.
    >
    > Change-Id: I6a2373aca8ea4903169c4fd2dd928696a6426b5b
    > Recipe-Nontrivial-Roll: chrome_release
    > Recipe-Nontrivial-Roll: build_limited_scripts_slave
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2909920
    > Auto-Submit: Garrett Beaty <gbeaty@chromium.org>
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    
    Change-Id: I1cbfbfc108b78d3abd228c22f923db4a8451bd31
    Recipe-Nontrivial-Roll: chrome_release
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2912984
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>

--- recipes/recipe_modules/chromium_checkout/__init__.py
+++ recipes/recipe_modules/chromium_checkout/__init__.py
@@ -1,18 +1,19 @@
 from PB.recipe_modules.build.chromium_checkout import properties
 
 DEPS = [
-  'chromium',
-  'depot_tools/bot_update',
-  'depot_tools/gclient',
-  'depot_tools/tryserver',
-  'recipe_engine/buildbucket',
-  'recipe_engine/context',
-  'recipe_engine/file',
-  'recipe_engine/path',
-  'recipe_engine/platform',
-  'recipe_engine/properties',
-  'recipe_engine/runtime',
-  'recipe_engine/step',
+    'chromium',
+    'depot_tools/bot_update',
+    'depot_tools/gclient',
+    'depot_tools/tryserver',
+    'recipe_engine/buildbucket',
+    'recipe_engine/context',
+    'recipe_engine/file',
+    'recipe_engine/json',
+    'recipe_engine/path',
+    'recipe_engine/platform',
+    'recipe_engine/properties',
+    'recipe_engine/runtime',
+    'recipe_engine/step',
 ]
 
 PROPERTIES = properties.InputProperties
--- recipes/recipe_modules/chromium_checkout/tests/full.py
+++ recipes/recipe_modules/chromium_checkout/tests/full.py
@@ -2,9 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
+
 from recipe_engine import post_process
 
-from RECIPE_MODULES.build import chromium
+from RECIPE_MODULES.build import chromium_tests_builder_config as ctbc
+from RECIPE_MODULES.depot_tools.gclient import (api as gclient, CONFIG_CTX as
+                                                GCLIENT_CONFIG_CTX)
 
 DEPS = [
     'chromium',
@@ -17,9 +21,15 @@ DEPS = [
 ]
 
 
+@GCLIENT_CONFIG_CTX()
+def revision_resolver(c):
+  s = c.solutions.add()
+  s.name = 'src'
+  c.revisions['src-internal'] = gclient.RevisionFallbackChain('refs/heads/main')
+
+
 def RunSteps(api):
-  _, builder_config = api.chromium_tests_builder_config.lookup_builder(
-      chromium.BuilderId.create_for_group('chromium.linux', 'Linux Builder'))
+  _, builder_config = api.chromium_tests_builder_config.lookup_builder()
 
   api.chromium_tests.configure_build(builder_config)
 
@@ -34,27 +44,57 @@ def RunSteps(api):
 
 
 def GenTests(api):
-  yield api.test('full')
+  yield api.test(
+      'full',
+      api.chromium_tests_builder_config.generic_build(),
+  )
 
   def verify_checkout_dir(check, step_odict, expected_path):
     step = step_odict['git diff to analyze patch']
-    check(step.cwd == str(expected_path))
+    expected_path = str(expected_path)
+    check(step.cwd == expected_path)
     return {}
-
-  def try_build():
-    return api.chromium.try_build(builder='builder')
 
   yield api.test(
       'win',
-      try_build(),
-      api.platform('win', 64),
-      api.post_process(verify_checkout_dir, api.path['cache'].join(
-          'builder', 'src')),
+      api.chromium_tests_builder_config.try_build(
+          builder_group='tryserver.chromium.win',
+          builder='win10_chromium_x64_rel_ng',
+      ),
+      api.post_process(verify_checkout_dir,
+                       api.path['cache'].join('builder', 'src')),
   )
 
   yield api.test(
       'linux',
-      try_build(),
-      api.post_process(verify_checkout_dir, api.path['cache'].join(
-          'builder', 'src')),
+      api.chromium_tests_builder_config.try_build(
+          builder_group='tryserver.chromium.linux',
+          builder='linux-rel',
+      ),
+      api.post_process(verify_checkout_dir,
+                       api.path['cache'].join('builder', 'src')),
+  )
+
+  def verify_revision_resolver_in_log(check, steps, expected):
+    gclient_config = json.loads(steps["gclient config"].logs["config"])
+    check(gclient_config["revisions"]["src-internal"] == expected)
+
+  yield api.test(
+      'revision-resolver',
+      api.chromium_tests_builder_config.generic_build(
+          builder_group='fake-group',
+          builder='fake-builder',
+          builder_db=ctbc.BuilderDatabase.create({
+              'fake-group': {
+                  'fake-builder':
+                      ctbc.BuilderSpec.create(
+                          chromium_config='chromium',
+                          gclient_config='revision_resolver',
+                      ),
+              },
+          }),
+      ),
+      api.post_check(verify_revision_resolver_in_log,
+                     "*RevisionFallbackChain*"),
+      api.post_process(post_process.DropExpectation),
   )
