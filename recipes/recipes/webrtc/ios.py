aa7e8cd4350921e5a7ce53d20a6ecdabe7d0a005
Björn Terelius <terelius@chromium.org>
2021-05-25 13:28:03 +0000

    Drop minor OS version (11.0 -> 11) for some more WebRTC builders.
    Change-Id: Ie73afaed7561588cc1492e5aa909506699190028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2916386
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Björn Terelius <terelius@chromium.org>

--- recipes/recipes/webrtc/ios.py
+++ recipes/recipes/webrtc/ios.py
@@ -134,7 +134,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '14.0',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
             'iOS64 Sim Debug (iOS 13)': {
@@ -153,7 +153,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '13.6',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
             'iOS64 Sim Debug (iOS 12)': {
@@ -172,7 +172,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '12.4',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
         },
@@ -266,7 +266,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '14.0',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
             'ios_sim_x64_dbg_ios13': {
@@ -285,7 +285,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '13.6',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
             'ios_sim_x64_dbg_ios12': {
@@ -304,7 +304,7 @@ BUILDERS = freeze({
                 'ios_testing': {
                     'device type': 'iPhone X',
                     'os': '12.4',
-                    'host os': 'Mac-11.0',
+                    'host os': 'Mac-11',
                 },
             },
         },
