984f2c975c612a1c4120593938e76f8b3f44fa11
Liviu Rau <liviurau@chromium.org>
2021-05-25 13:10:23 +0000

    Add Spike recipe
    Empty recipe to be used (when modified) in infra experiments. Led does
    not work when I reference a recipe that is not already present in the
    repo.
    
    Bug: v8:11169
    Change-Id: I9a32e1db7880889aa38f11632e00098a3a15ad5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2916383
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

---
+++ recipes/recipes/v8/spike.py
@@ -0,0 +1,20 @@
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine import recipe_test_api
+from recipe_engine.post_process import (
+    Filter, DoesNotRun, DoesNotRunRE, DropExpectation, MustRun,
+    ResultReasonRE, StatusException, StatusFailure, StepException)
+from recipe_engine.recipe_api import Property
+
+DEPS = [
+]
+
+def RunSteps(api):
+  pass
+
+
+
+def GenTests(api):
+  yield api.test("empty")
