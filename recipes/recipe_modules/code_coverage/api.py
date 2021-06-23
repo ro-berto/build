f9ad8f94e174df15f25747dbe09ca6135bf9d529
Nodir Turakulov <nodir@chromium.org>
2021-06-23 20:07:21 +0000

    Revert "[dirmd] Switch to new "read" subcommand"
    This reverts commit 28352f43d790b7c635022a6fa575a29a604a7bb3.
    
    Reason for revert: need to rollback the underlying CL
    
    Original change's description:
    > [dirmd] Switch to new "read" subcommand
    >
    > dirmd subcommand "export" was renamed to "read". Use the new name
    >
    > Change-Id: I1d19cdbc39c110d6f1d5f7b8a4edd65c93512757
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2912985
    > Reviewed-by: Prakhar Asthana <pasthana@google.com>
    > Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    
    Change-Id: I0f0ff099b7ba1929a3e6dfaa412e43c9932e7689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/2983643
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>

--- recipes/recipe_modules/code_coverage/api.py
+++ recipes/recipe_modules/code_coverage/api.py
@@ -824,7 +824,7 @@ class CodeCoverageApi(recipe_api.RecipeApi):
     with self.m.context(cwd=self.m.path['checkout']):
       self.m.step('Extract directory metadata', [
           self.m.path['checkout'].join('third_party', 'depot_tools', 'dirmd'),
-          'read', '-out', dir_metadata,
+          'export', '-out', dir_metadata
       ])
     return dir_metadata
 
