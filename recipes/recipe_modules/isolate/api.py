188ad42b3c53b8ce4cb48bc6441a40cd5a2addd6
Takuto Ikuta <tikuta@chromium.org>
2021-07-21 09:50:56 +0000

    isolate: update client
    This is for https://crrev.com/c/3041206
    
    Bug: 1231072
    Change-Id: I355bd6ae0038bc5145061345d07c4978eed0dbd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3041207
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>

--- recipes/recipe_modules/isolate/api.py
+++ recipes/recipe_modules/isolate/api.py
@@ -17,7 +17,7 @@ _MAX_SWARM_HASHES_PROPERTY_LENGTH = 200
 
 # Take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
-_LUCI_GO_REVISION = 'git_revision:175b811c1eebfa8868ab96b58a4dc7b5bcabf756'
+_LUCI_GO_REVISION = 'git_revision:9ee8b1d719c0d3c268e0e19282351ca78024af2d'
 
 
 class IsolateApi(recipe_api.RecipeApi):
