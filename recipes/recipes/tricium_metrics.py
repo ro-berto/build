a84fe44202c8f36e4bb2351892315e7d0a07a10e
Weilun Shi <sweilun@chromium.org>
2021-07-22 23:06:56 +0000

    Handle the missing file error in `git show`
    Tricium uses `git show` to get the previous version of the changed
    files. However, `git show` throws an error when the file doesn't exist
    in FETCH_HEAD~. This could happen when users just added a new
    histograms.xml. We should catch this error and touch an empty file for
    the tricium metrics analyzer to use.
    
    An example failure in https://ci.chromium.org/ui/p/chromium/builders/try/tricium-metrics-analysis/87004/overview
    
    Bug: 1231326
    Change-Id: Ie9babb3323f837710f3f94d2d5bca145b009a4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3044828
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>

--- recipes/recipes/tricium_metrics.py
+++ recipes/recipes/tricium_metrics.py
@@ -102,10 +102,16 @@ def RunSteps(api):
         prev_dir_path = prev_dir.join(path)
         api.file.ensure_directory('create_directories',
                                   api.path.dirname(prev_dir_path))
-        api.git(
-            'show',
-            'FETCH_HEAD~:' + path,
-            stdout=api.raw_io.output(leak_to=prev_dir_path))
+        # `git show` throws an error if the file doesn't exist. This could
+        # happen when users just added a new histograms.xml. In this case,
+        # we just need to touch an empty file as the placeholder.
+        try:
+          api.git(
+              'show',
+              'FETCH_HEAD~:' + path,
+              stdout=api.raw_io.output(leak_to=prev_dir_path))
+        except Exception:
+          api.step('touch an empty file', ['touch', prev_dir_path])
 
       # Get the diff itself, with paths formatted as Tricium analyzer expects.
       patch_path = api.path['cleanup'].join('tricium_generated_diff.patch')
@@ -222,3 +228,27 @@ def GenTests(api):
           lambda check, steps: '[ERROR]: Removed' in
             steps['metrics.write_results'].output_properties['tricium']
       ) + api.post_process(post_process.DropExpectation))
+
+  yield (
+      test_with_patch(
+          'show_file_path_not_found_but_succeed',
+          affected_files=['some/test/test2/histograms.xml'],
+          include_parse=True) +
+      # Simulate a file missing error, this could happen if users add a new file
+      # Make sure the exception is captured and the analyzer shouldn't fail.
+      api.step_data('git show', retcode=128) + api.step_data(
+          'metrics.metrics_output',
+          api.file.read_json({
+              "comments": [{
+                  "category": "Metrics/Removed",
+                  "message": "[ERROR]: Removed",
+                  "path": "testdata/src/rm/remove_histogram.xml"
+              }]
+          })) +
+      api.post_process(post_process.DoesNotRun, 'metrics.load_test_analyzer') +
+      api.post_process(post_process.StepSuccess, 'metrics.load_live_analyzer') +
+      api.post_process(post_process.StepSuccess, 'metrics') +
+      api.post_process(post_process.StatusSuccess) +
+      api.post_check(lambda check, steps: '[ERROR]: Removed' in steps[
+          'metrics.write_results'].output_properties['tricium']) +
+      api.post_process(post_process.DropExpectation))
