0040d6eb36c59dbda399ee93835d1aa38d6218a7
Ben Pastene <bpastene@chromium.org>
2021-07-28 16:03:27 +0000

    Revert "Run infra.services.lkgr_finder via vpython, not run.py."
    This reverts commit 7f77f44335d40aad0834e31cf5b3592ecfbe9372.
    
    Reason for revert: "No module named requests" on lkgr finder bots
    https://ci.chromium.org/p/infra/builders/cron/chromium-lkgr-finder/b8840456683896993456
    
    Original change's description:
    > Run infra.services.lkgr_finder via vpython, not run.py.
    >
    > Bug: 1229907
    > Change-Id: I0fa43ad21fb3561488e23c2cebb90b87b83a8045
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3056139
    > Commit-Queue: Owen Rodley <orodley@chromium.org>
    > Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    
    Bug: 1229907, 1233851
    Change-Id: Id58e6043bc8b5b2f3b08c44c9c0be6cfd9f6abeb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3057147
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- recipes/recipes/lkgr_finder.py
+++ recipes/recipes/lkgr_finder.py
@@ -120,7 +120,6 @@ def RunSteps(api, project, repo, ref, config, lkgr_status_gs_path, allowed_lag,
 
   api.file.ensure_directory('mkdirs builder/lw', checkout_dir.join('lw'))
   args = [
-      '-m',
       'infra.services.lkgr_finder',
       '--project=%s' % project,
       '--verbose',
@@ -151,8 +150,10 @@ def RunSteps(api, project, repo, ref, config, lkgr_status_gs_path, allowed_lag,
 
   try:
     with api.context(cwd=checkout_dir.join('infra')):
-      api.step(
-          'calculate %s lkgr' % project, ['vpython'] + args,
+      api.python(
+          'calculate %s lkgr' % project,
+          checkout_dir.join('infra', 'run.py'),
+          args,
           step_test_data=lambda: step_test_data)
   except api.step.StepFailure as e:
     # Don't fail the build if the LKGR is just stale.
