fc365d9428646a72c73dcd0fe6c31c6c29975202
Greg Guterman <guterman@google.com>
2021-08-10 21:58:52 +0000

    Add quick run bug template link; better support rts builders
    The current conditional behavior prevents the step from showing when
    rts experimental builders do their normal builds (without Quick Run),
    which is not a desired behavior.
    
    Bug: 1218503
    Change-Id: I993e37a06b37b5434fe43a9b1ad3b7a979150711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3086703
    Commit-Queue: Gregory Guterman <guterman@google.com>
    Auto-Submit: Gregory Guterman <guterman@google.com>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>

--- recipes/recipe_modules/chromium_tests/api.py
+++ recipes/recipe_modules/chromium_tests/api.py
@@ -1826,13 +1826,14 @@ class ChromiumTestsApi(recipe_api.RecipeApi):
         self.m.cq.active and self.m.cq.run_mode == self.m.cq.QUICK_DRY_RUN and
         builder_config.regression_test_selection == try_spec.QUICK_RUN_ONLY
     ) or builder_config.regression_test_selection == try_spec.ALWAYS
-    if self.m.cq.active and self.m.cq.run_mode == self.m.cq.QUICK_DRY_RUN:
+    if use_rts:
       step_result = self.m.step('quick run options', [])
       step_result.presentation.properties['rts_was_used'] = use_rts
-      if use_rts:
-        step_result.presentation.links[
-            'use_rts: true'] = 'https://bit.ly/chromium-rts'
-        step_result.presentation.links['info'] = 'https://bit.ly/chromium-rts'
+      step_result.presentation.links[
+          'use_rts: true'] = 'https://bit.ly/chromium-rts'
+      step_result.presentation.links['file a bug'] = (
+          'https://bugs.chromium.org/p/chromium/issues/entry?'
+          'template=Quick%20Run%20Issue')
     # RTS-enabled Quick Run builds can't be reused for non-quick runs because
     # they are slightly less safe than normal builds
     if (use_rts and
