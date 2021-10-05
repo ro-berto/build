b9dc2cd6f690357ac72f367dc9d459625d67f951
Jamie Madill <jmadill@chromium.org>
2021-10-05 18:59:36 +0000

    [angle] Add GLES 3 trace tests to CI.
    Bug: angleproject:6522
    Change-Id: I176bacfe06d061e008486bb6d2b2a4489fdd7a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3119525
    Auto-Submit: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>

--- recipes/recipe_modules/angle/api.py
+++ recipes/recipe_modules/angle/api.py
@@ -97,6 +97,8 @@ class ANGLEApi(recipe_api.RecipeApi):
       try:
         self._run_trace_tests(checkout, '*/ES2_Vulkan_SwiftShader',
                               'GLES 2.0 trace tests')
+        self._run_trace_tests(checkout, '*/ES3_Vulkan_SwiftShader',
+                              'GLES 3.0 trace tests')
         self._run_trace_tests(checkout, '*/ES1_Vulkan_SwiftShader',
                               'GLES 1.0 trace tests')
       finally:
