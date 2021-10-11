4621577eee759bc79d80f5f61036a88f45f589b7
Garrett Beaty <gbeaty@chromium.org>
2021-10-11 18:32:44 +0000

    Decouple chromium_export_metadata recipe from chromium_tests.
    The chromium_export_metadata recipe doesn't perform any build or
    testing. It uses the chromium_tests configuration code in order to get a
    checkout, but the static maps containing the configuration will
    eventually be removed as builder configuration is moved src-side. This
    replaces the use of the chromium_tests module with direct use of the
    gclient and bot_update modules.
    
    Change-Id: If108fa55e3db532fe7b34d97ca7dd318c8ce6ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3216258
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>

--- recipes/recipes/chromium_export_metadata.py
+++ recipes/recipes/chromium_export_metadata.py
@@ -16,15 +16,16 @@ See more on forms in
 https://source.chromium.org/chromium/infra/infra/+/main:go/src/infra/tools/dirmd/proto/mapping.proto
 """
 
-from RECIPE_MODULES.build import chromium
 
 
 PYTHON_VERSION_COMPATIBILITY = "PY2"
 
 DEPS = [
-    'chromium_tests',
-    'chromium_tests_builder_config',
+    'build/chromium',
+    'depot_tools/bot_update',
     'depot_tools/depot_tools',
+    'depot_tools/gclient',
+    'recipe_engine/context',
     'recipe_engine/path',
     'recipe_engine/step',
 ]
@@ -33,13 +34,18 @@ DEST_BUCKET = 'chrome-metadata'
 DEST_BUCKET_LEGACY = 'chromium-owners'
 
 def RunSteps(api):
-  # Replicate the config of a vanilla linux builder.
-  _, builder_config = api.chromium_tests_builder_config.lookup_builder(
-      chromium.BuilderId.create_for_group('chromium.linux', 'Linux Builder'))
-  # configure_build() is required by prepare-checkout
-  api.chromium_tests.configure_build(builder_config)
-  api.chromium_tests.prepare_checkout(
-      builder_config, set_output_commit=False, report_cache_state=False)
+  api.gclient.set_config('chromium')
+  # TODO(gbeaty) This config causes a hook to be executed that updates the clang
+  # coverage tools, it's probably unnecessary, but preserves the behavior from
+  # when this recipe was relying on the config for Linux Builder
+  api.gclient.apply_config('use_clang_coverage')
+  with api.context(cwd=api.path['cache'].join('builder')):
+    api.bot_update.ensure_checkout()
+  # TODO(gbeaty) If none of the hooks are downloading directories containing
+  # DIR_METADATA files, then it shouldn't be necessary to run the hooks as part
+  # of this recipe
+  api.chromium.set_config('chromium')
+  api.chromium.runhooks()
 
   api.step('dirmd chromium-update', [
     api.path['checkout'].join('third_party', 'depot_tools', 'dirmd'),
