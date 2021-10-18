610acb915e1e9a33556580d179b9d574f8af178a
Fumitoshi Ukai <ukai@google.com>
2021-10-18 00:35:57 +0000

    add 'Mac Builder (reclient compare)' builder
    add builder that runs reclient with RBE_compare=true
    to check artifacts matches with local compilation.
    
    Bug: 1260232
    Change-Id: I0e1af14f4937e33ab7c1b37c6ae258a31f567e68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3225100
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>

--- recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_fyi.py
+++ recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_fyi.py
@@ -1392,6 +1392,22 @@ SPEC = {
             },
             simulation_platform='mac',
         ),
+    'Mac Builder (reclient compare)':
+        builder_spec.BuilderSpec.create(
+            chromium_config='chromium',
+            chromium_apply_config=['mb'],
+            gclient_config='chromium',
+            gclient_apply_config=[
+                'use_clang_coverage',
+                'enable_reclient',
+                'reclient_gomaip_exp',
+            ],
+            chromium_config_kwargs={
+                'BUILD_CONFIG': 'Release',
+                'TARGET_BITS': 64,
+            },
+            simulation_platform='mac',
+        ),
     # TODO(crbug.com/1252626): remove after the migration.
     'mac-arm64-on-arm64-rel-reclient':
         builder_spec.BuilderSpec.create(
