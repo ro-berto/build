a97e6e80257236e5c690d594d294667eda76d860
Ben Pastene <bpastene@chromium.org>
2021-10-19 20:24:11 +0000

    Add StatusSuccess asserts to all recipe tests in resultdb_experiment.py.
    Just some clean-up.
    
    Bug: 1135718
    Change-Id: Ie2d918b9be3017c77ec0b8b8424f61a8e1aa03cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3232384
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>

--- recipes/recipe_modules/chromium_tests/tests/steps/resultdb_experiment.py
+++ recipes/recipe_modules/chromium_tests/tests/steps/resultdb_experiment.py
@@ -131,6 +131,7 @@ def GenTests(api):
       api.post_process(post_process.DoesNotRun, 'swarming_gtest invalid'),
       api.post_process(post_process.MustRun,
                        'Upload to test-results [swarming_gtest (with patch)]'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -146,6 +147,7 @@ def GenTests(api):
           stdout=api.raw_io.output_text(api.test_utils.rdb_results())),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest failure'),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest invalid'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -167,6 +169,7 @@ def GenTests(api):
                            'deterministic failures [caused step to fail]',
                            'swarming_gtest_test_case1'
                        ]),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -183,6 +186,7 @@ def GenTests(api):
               api.test_utils.rdb_results(failing_suites=['swarming_gtest']))),
       api.post_process(post_process.MustRun, 'swarming_gtest failure'),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest invalid'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -195,6 +199,7 @@ def GenTests(api):
               api.json.output({}), failure=True)),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest failure'),
       api.post_process(post_process.MustRun, 'swarming_gtest invalid'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -208,6 +213,7 @@ def GenTests(api):
               api.json.output({}), failure=False)),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest failure'),
       api.post_process(post_process.MustRun, 'swarming_gtest invalid'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -227,6 +233,7 @@ def GenTests(api):
                        'Upload to test-results [layout_tests (with patch)]'),
       api.post_process(post_process.MustRun,
                        'archive results for layout_tests (with patch)'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
@@ -243,6 +250,7 @@ def GenTests(api):
               api.test_utils.rdb_results(skipped_suites=['swarming_gtest']))),
       api.post_process(post_process.MustRun, 'swarming_gtest failure'),
       api.post_process(post_process.DoesNotRun, 'swarming_gtest invalid'),
+      api.post_process(post_process.StatusSuccess),
       api.post_process(post_process.DropExpectation),
   )
 
