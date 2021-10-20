6bdd141416402796da4f80f6fc3b68cba728f420
Jamie Madill <jmadill@chromium.org>
2021-10-20 13:00:54 +0000

    [angle] Remove old configs.
    This can land after the switch to the new configs on the
    ANGLE CQ.
    
    Bug: angleproject:6496
    Change-Id: I5189dd6e5aa18d4c1ebcab05e2868bed98c1132d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3231875
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- recipes/recipe_modules/angle/builders.py
+++ recipes/recipe_modules/angle/builders.py
@@ -72,106 +72,67 @@ def _create_android_tester_config(target_bits, parent_builder):
   )
 
 
-# TODO(jmadill): De-duplicate. http://anglebug.com/6496
 _SPEC = {
     'android-arm-compile':
         _create_android_builder_config('Release', 32),
-    'android-arm-dbg':
-        _create_android_builder_config('Debug', 32),
     'android-arm-dbg-compile':
         _create_android_builder_config('Debug', 32),
-    'android-arm-rel':
-        _create_android_builder_config('Release', 32),
     'android-arm64-dbg':
         _create_android_builder_config('Debug', 64),
     'android-arm64-dbg-compile':
         _create_android_builder_config('Debug', 64),
     'android-arm64-pixel4':
-        _create_android_tester_config(64, 'android-arm64-rel'),
+        _create_android_tester_config(64, 'android-arm64-test'),
     'android-arm64-pixel4-perf':
         _create_android_tester_config(64, 'android-perf'),
-    'android-arm64-rel':
-        _create_android_builder_config('Release', 64),
     'android-arm64-test':
         _create_android_builder_config('Release', 64),
     'android-perf':
         _create_android_builder_config('Release', 64, perf_isolate_upload=True),
-    'linux-clang-dbg':
-        _create_builder_config('linux', 'Debug', 64),
-    'linux-clang-rel':
-        _create_builder_config('linux', 'Release', 64),
     'linux-dbg-compile':
         _create_builder_config('linux', 'Debug', 64),
-    'linux-gcc-dbg':
-        _create_builder_config('linux', 'Debug', 64, is_clang=False),
-    'linux-gcc-rel':
-        _create_builder_config('linux', 'Release', 64, is_clang=False),
     'linux-intel':
-        _create_tester_config('linux', 64, 'linux-clang-rel'),
+        _create_tester_config('linux', 64, 'linux-test'),
     'linux-intel-perf':
         _create_tester_config('linux', 64, 'linux-perf'),
     'linux-nvidia':
-        _create_tester_config('linux', 64, 'linux-clang-rel'),
+        _create_tester_config('linux', 64, 'linux-test'),
     'linux-nvidia-perf':
         _create_tester_config('linux', 64, 'linux-perf'),
     'linux-perf':
         _create_builder_config(
             'linux', 'Release', 64, perf_isolate_upload=True),
     'linux-swiftshader':
-        _create_tester_config('linux', 64, 'linux-clang-rel'),
+        _create_tester_config('linux', 64, 'linux-test'),
     'linux-test':
         _create_builder_config('linux', 'Release', 64),
     'linux-trace':
         _create_builder_config('linux', 'Release', 64),
-    'linux-trace-rel':
-        _create_builder_config('linux', 'Release', 64),
     'mac-amd':
-        _create_tester_config('mac', 64, 'mac-rel'),
-    'mac-dbg':
-        _create_builder_config('mac', 'Debug', 64),
+        _create_tester_config('mac', 64, 'mac-test'),
     'mac-dbg-compile':
         _create_builder_config('mac', 'Debug', 64),
     'mac-intel':
-        _create_tester_config('mac', 64, 'mac-rel'),
+        _create_tester_config('mac', 64, 'mac-test'),
     'mac-nvidia':
-        _create_tester_config('mac', 64, 'mac-rel'),
-    'mac-rel':
-        _create_builder_config('mac', 'Release', 64),
+        _create_tester_config('mac', 64, 'mac-test'),
     'mac-test':
         _create_builder_config('mac', 'Release', 64),
-    'win-clang-x64-dbg':
-        _create_builder_config('win', 'Debug', 64),
-    'win-clang-x64-rel':
-        _create_builder_config('win', 'Release', 64),
-    'win-clang-x86-dbg':
-        _create_builder_config('win', 'Debug', 32),
-    'win-clang-x86-rel':
-        _create_builder_config('win', 'Release', 32),
     'win-dbg-compile':
         _create_builder_config('win', 'Debug', 64),
     'win-msvc-compile':
         _create_builder_config('win', 'Release', 64, is_clang=False),
     'win-msvc-dbg-compile':
         _create_builder_config('win', 'Debug', 64, is_clang=False),
-    'win-msvc-x64-dbg':
-        _create_builder_config('win', 'Debug', 64, is_clang=False),
-    'win-msvc-x64-rel':
-        _create_builder_config('win', 'Release', 64, is_clang=False),
     'win-msvc-x86-compile':
         _create_builder_config('win', 'Release', 32, is_clang=False),
-    'win-msvc-x86-dbg':
-        _create_builder_config('win', 'Debug', 32, is_clang=False),
     'win-msvc-x86-dbg-compile':
         _create_builder_config('win', 'Debug', 32, is_clang=False),
-    'win-msvc-x86-rel':
-        _create_builder_config('win', 'Release', 32, is_clang=False),
     'win-perf':
         _create_builder_config('win', 'Release', 64, perf_isolate_upload=True),
     'win-test':
         _create_builder_config('win', 'Release', 64),
     'win-trace':
-        _create_builder_config('win', 'Release', 64),
-    'win-trace-rel':
         _create_builder_config('win', 'Release', 64),
     'win-x86-dbg-compile':
         _create_builder_config('win', 'Debug', 32),
@@ -181,22 +142,18 @@ _SPEC = {
         _create_builder_config('win', 'Release', 64, is_clang=False),
     'winuwp-dbg-compile':
         _create_builder_config('win', 'Debug', 64, is_clang=False),
-    'winuwp-x64-dbg':
-        _create_builder_config('win', 'Debug', 64, is_clang=False),
-    'winuwp-x64-rel':
-        _create_builder_config('win', 'Release', 64, is_clang=False),
     'win7-x86-amd':
-        _create_tester_config('win', 32, 'win-clang-x86-rel'),
+        _create_tester_config('win', 32, 'win-x86-test'),
     'win10-x64-intel':
-        _create_tester_config('win', 64, 'win-clang-x64-rel'),
+        _create_tester_config('win', 64, 'win-test'),
     'win10-x64-intel-perf':
         _create_tester_config('win', 64, 'win-perf'),
     'win10-x64-nvidia':
-        _create_tester_config('win', 64, 'win-clang-x64-rel'),
+        _create_tester_config('win', 64, 'win-test'),
     'win10-x64-nvidia-perf':
         _create_tester_config('win', 64, 'win-perf'),
     'win10-x86-swiftshader':
-        _create_tester_config('win', 32, 'win-clang-x86-rel'),
+        _create_tester_config('win', 32, 'win-x86-test'),
 }
 
 BUILDERS = builder_db.BuilderDatabase.create({
--- recipes/recipe_modules/angle/trybots.py
+++ recipes/recipe_modules/angle/trybots.py
@@ -15,32 +15,15 @@ def _create_compile_spec(buildername):
   )
 
 
-# TODO(jmadill): De-duplicate. http://anglebug.com/6496
 _SPEC = {
     'android-arm-compile':
         _create_compile_spec('android-arm-compile'),
-    'android-arm-dbg':
-        _create_compile_spec('android-arm-dbg'),
     'android-arm-dbg-compile':
         _create_compile_spec('android-arm-dbg-compile'),
-    'android-arm-rel':
-        _create_compile_spec('android-arm-rel'),
     'android-arm64-dbg':
         _create_compile_spec('android-arm64-dbg'),
     'android-arm64-dbg-compile':
         _create_compile_spec('android-arm64-dbg-compile'),
-    'android-arm64-rel':
-        try_spec.TrySpec.create(
-            mirrors=[
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='android-arm64-rel',
-                    tester='android-arm64-pixel4',
-                ),
-            ],
-            analyze_names=['angle'],
-            retry_failed_shards=False,
-        ),
     'android-arm64-test':
         try_spec.TrySpec.create(
             mirrors=[
@@ -65,36 +48,8 @@ _SPEC = {
             analyze_names=['angle'],
             retry_failed_shards=False,
         ),
-    'linux-clang-dbg':
-        _create_compile_spec('linux-clang-dbg'),
-    'linux-clang-rel':
-        try_spec.TrySpec.create(
-            mirrors=[
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='linux-clang-rel',
-                    tester='linux-intel',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='linux-clang-rel',
-                    tester='linux-nvidia',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='linux-clang-rel',
-                    tester='linux-swiftshader',
-                ),
-            ],
-            analyze_names=['angle'],
-            retry_failed_shards=False,
-        ),
     'linux-dbg-compile':
         _create_compile_spec('linux-dbg-compile'),
-    'linux-gcc-dbg':
-        _create_compile_spec('linux-gcc-dbg'),
-    'linux-gcc-rel':
-        _create_compile_spec('linux-gcc-rel'),
     'linux-intel-hd630-perf':
         try_spec.TrySpec.create(
             mirrors=[
@@ -143,34 +98,8 @@ _SPEC = {
         ),
     'linux-trace':
         _create_compile_spec('linux-trace'),
-    'linux-trace-rel':
-        _create_compile_spec('linux-trace-rel'),
-    'mac-dbg':
-        _create_compile_spec('mac-dbg'),
     'mac-dbg-compile':
         _create_compile_spec('mac-dbg-compile'),
-    'mac-rel':
-        try_spec.TrySpec.create(
-            mirrors=[
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='mac-rel',
-                    tester='mac-amd',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='mac-rel',
-                    tester='mac-intel',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='mac-rel',
-                    tester='mac-nvidia',
-                ),
-            ],
-            analyze_names=['angle'],
-            retry_failed_shards=False,
-        ),
     'mac-test':
         try_spec.TrySpec.create(
             mirrors=[
@@ -193,62 +122,16 @@ _SPEC = {
             analyze_names=['angle'],
             retry_failed_shards=False,
         ),
-    'win-clang-x64-dbg':
-        _create_compile_spec('win-clang-x64-dbg'),
-    'win-clang-x64-rel':
-        try_spec.TrySpec.create(
-            mirrors=[
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='win-clang-x64-rel',
-                    tester='win10-x64-intel',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='win-clang-x64-rel',
-                    tester='win10-x64-nvidia',
-                ),
-            ],
-            analyze_names=['angle'],
-            retry_failed_shards=False,
-        ),
-    'win-clang-x86-dbg':
-        _create_compile_spec('win-clang-x86-dbg'),
-    'win-clang-x86-rel':
-        try_spec.TrySpec.create(
-            mirrors=[
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='win-clang-x86-rel',
-                    tester='win7-x86-amd',
-                ),
-                try_spec.TryMirror.create(
-                    builder_group='angle',
-                    buildername='win-clang-x86-rel',
-                    tester='win10-x86-swiftshader',
-                ),
-            ],
-            analyze_names=['angle'],
-            retry_failed_shards=False,
-        ),
     'win-dbg-compile':
         _create_compile_spec('win-dbg-compile'),
     'win-msvc-compile':
         _create_compile_spec('win-msvc-compile'),
     'win-msvc-dbg-compile':
         _create_compile_spec('win-msvc-dbg-compile'),
-    'win-msvc-x64-dbg':
-        _create_compile_spec('win-msvc-x64-dbg'),
-    'win-msvc-x64-rel':
-        _create_compile_spec('win-msvc-x64-rel'),
     'win-msvc-x86-compile':
         _create_compile_spec('win-msvc-x86-compile'),
-    'win-msvc-x86-dbg':
-        _create_compile_spec('win-msvc-x86-dbg'),
     'win-msvc-x86-dbg-compile':
         _create_compile_spec('win-msvc-x86-dbg-compile'),
-    'win-msvc-x86-rel':
-        _create_compile_spec('win-msvc-x86-rel'),
     'win-test':
         try_spec.TrySpec.create(
             mirrors=[
@@ -311,16 +194,10 @@ _SPEC = {
         ),
     'win-trace':
         _create_compile_spec('win-trace'),
-    'win-trace-rel':
-        _create_compile_spec('win-trace-rel'),
     'winuwp-compile':
         _create_compile_spec('winuwp-compile'),
     'winuwp-dbg-compile':
         _create_compile_spec('winuwp-dbg-compile'),
-    'winuwp-x64-dbg':
-        _create_compile_spec('winuwp-x64-dbg'),
-    'winuwp-x64-rel':
-        _create_compile_spec('winuwp-x64-rel'),
 }
 
 TRYBOTS = try_spec.TryDatabase.create({
