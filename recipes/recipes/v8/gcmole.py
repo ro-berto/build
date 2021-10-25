cd5dc7a95a5281e654811fe67837edc7a63bee3e
Liviu Rau <liviurau@chromium.org>
2021-10-25 09:38:17 +0000

    [V8] Add gcmole builder recipe
    This recipe is building, packaging and uploading gcmole.
    
    Successful led run: https://luci-milo.appspot.com/raw/build/logs.chromium.org/v8-internal/led/liviurau_google.com/3d81fbbb07c30c7d893962e8109a34d4d474f25d0bdb6eda6168bba7f919e1ac/+/build.proto?server=chrome-swarming.appspot.com with original buildbucket id 8832868969570685873
    Created auto-update CL: https://crrev.com/c/3237407
    
    The recipe is using the bucket expected in DEPS (chrome-v8-gcmole) rather than the one proposed in the bug description.
    https://chromium.googlesource.com/v8/v8/+/e26aced708418cf5521fb6eed8e13eec2d12088f/DEPS#413
    
    Bug: v8:11169
    Change-Id: I41415b2bd21b481a0b28973092d6ecf08e639782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3220593
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

---
+++ recipes/recipes/v8/gcmole.py
@@ -0,0 +1,77 @@
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine import post_process
+
+PYTHON_VERSION_COMPATIBILITY = "PY2"
+
+DEPS = [
+    'chromium',
+    'depot_tools/depot_tools',
+    'depot_tools/gclient',
+    'depot_tools/gsutil',
+    'depot_tools/git',
+    'recipe_engine/context',
+    'recipe_engine/path',
+    'recipe_engine/python',
+    'recipe_engine/raw_io',
+    'recipe_engine/step',
+    'v8',
+]
+
+GS_BUCKET = 'chrome-v8-gcmole'
+
+
+def RunSteps(api):
+  api.gclient.set_config('v8')
+  api.chromium.set_config('v8')
+  api.v8.checkout()
+  api.v8.runhooks()
+
+  depot_tools_path = api.path['checkout'].join('third_party', 'depot_tools')
+  with api.context(env_prefixes={'PATH': [depot_tools_path]}):
+    api.git('branch', '-D', 'gcmole_update', ok_ret='any')
+    api.git('clean', '-ffd')
+    api.git('new-branch', 'gcmole_update')
+
+    gcmole_root = api.path['checkout'].join('tools', 'gcmole')
+    api.step('Build gcmole', [gcmole_root.join('bootstrap.sh')])
+    api.step('Package gcmole', [gcmole_root.join('package.sh')])
+
+    api.python('upload_to_google_storage',
+               api.depot_tools.upload_to_google_storage_path,
+               ['-b', GS_BUCKET,
+                gcmole_root.join('gcmole-tools.tar.gz')])
+
+    changes = api.git(
+        'status', '--porcelain',
+        stdout=api.raw_io.output_text()).stdout.strip()
+    if changes:
+      api.git('commit', '-am', '[tools] Update gcmole')
+      api.git(
+          'cl',
+          'upload',
+          '-f',
+          '--use-commit-queue',
+          '--bypass-hooks',
+          '--send-mail',
+          '--r-owners',
+      )
+
+
+def GenTests(api):
+  yield (api.test("default test") + api.override_step_data(
+      'git status',
+      api.raw_io.stream_output('some change', stream='stdout'),
+  ) + api.post_process(post_process.StatusSuccess) +
+         api.post_process(post_process.MustRun, 'git commit', 'git cl') +
+         api.post_process(
+             post_process.Filter('Build gcmole', 'Package gcmole',
+                                 'upload_to_google_storage', 'git cl')))
+  yield (api.test("no change test") + api.override_step_data(
+      'git status',
+      api.raw_io.stream_output('', stream='stdout'),
+  ) + api.post_process(post_process.StatusSuccess) +
+         api.post_process(post_process.DoesNotRun, 'git commit', 'git cl') +
+         api.post_process(post_process.DropExpectation))
