14f07ba0928aef128cae118be29208ee8d356313
Chris McDonald <cjmcdonald@chromium.org>
2021-10-26 22:48:31 +0000

    Update depot_tools to the latest revision
    BUG=None
    TEST=recipes.py test train
    
    Change-Id: I7e7e3b687234ced91501775149fb7499da7b9463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3245220
    Auto-Submit: Chris McDonald <cjmcdonald@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Mike Nichols <mikenichols@chromium.org>
    Commit-Queue: Chris McDonald <cjmcdonald@chromium.org>
    Commit-Queue: Mike Nichols <mikenichols@chromium.org>

--- recipes/recipe_modules/chromite/api.py
+++ recipes/recipe_modules/chromite/api.py
@@ -13,7 +13,7 @@ class ChromiteApi(recipe_api.RecipeApi):
       'https://chromium.googlesource.com/chromium/tools/depot_tools.git')
   # Keep this pin in sync with manifest pin in:
   #   https://chromium.googlesource.com/chromiumos/manifest/+/HEAD/full.xml
-  depot_tools_pin = '09f358bae36c316e3c4c39cd344de964bd0fed7c'
+  depot_tools_pin = '5c9e8b474f47653bcc6da5195f4b1b4e4437bf25'
 
   # Only used by the internal goma recipe.
   manifest_url = 'https://chromium.googlesource.com/chromiumos/manifest.git'
