fc939257a8a863ba962a9d67eae2e86b2e412b89
Josip Sokcevic <sokcevic@google.com>
2021-10-27 19:11:39 +0000

    Use py3 for codesearch recipes
    R=gavinmak@google.com
    
    Fixed: 1256395, 1256052
    Change-Id: I6050d0e624245dd171ad0acf38732ad6c4510151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3247916
    Reviewed-by: Gavin Mak <gavinmak@google.com>
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>

--- recipes/recipes/chromium_codesearch.py
+++ recipes/recipes/chromium_codesearch.py
@@ -10,7 +10,7 @@ from recipe_engine.recipe_api import Property
 
 from PB.go.chromium.org.luci.buildbucket.proto import common as common_pb
 
-PYTHON_VERSION_COMPATIBILITY = "PY2+3"
+PYTHON_VERSION_COMPATIBILITY = "PY3"
 
 DEPS = [
   'build',
