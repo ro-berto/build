f1c89036aedb1bb3211b555389cb26f344bf91cc
Ben Pastene <bpastene@chromium.org>
2021-10-28 22:13:04 +0000

    Remove the old non-optimized VM checkout from the CrOS GPU VM bot.
    The GN args have been switched to the new VM, so should be safe to
    stop checking out the old one.
    
    Bug: 1254850
    Change-Id: Ib28320686416b6234069009c77b0b67ccd4230d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3252126
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>

--- recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_gpu_fyi.py
+++ recipes/recipe_modules/chromium_tests_builder_config/builders/chromium_gpu_fyi.py
@@ -1136,9 +1136,7 @@ SPEC = {
                 'BUILD_CONFIG': 'Release',
                 'TARGET_ARCH': 'intel',
                 'TARGET_BITS': 64,
-                # TODO(crbug.com/1254850): Remove the old amd64-generic target
-                # once the GN args have swapped over to the VM one.
-                'CROS_BOARDS_WITH_QEMU_IMAGES': 'amd64-generic:amd64-generic-vm',
+                'CROS_BOARDS_WITH_QEMU_IMAGES': 'amd64-generic-vm',
                 'TARGET_PLATFORM': 'chromeos',
             },
             simulation_platform='linux',
