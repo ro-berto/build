f03ede431e7961d52cd573d1da6527e922aeddc7
Victor Gomes <victorgomes@chromium.org>
2021-10-29 15:27:26 +0000

    [bazel] Change target to ICU variant
    Change-Id: I62e7c32ae3b264350b2ef7ffa91c45b1e9a8fc95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3250681
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Liviu Rau <liviurau@chromium.org>

--- recipes/recipes/v8/bazel.py
+++ recipes/recipes/v8/bazel.py
@@ -33,7 +33,7 @@ def RunSteps(api):
       cwd=api.path['checkout'],
       env_prefixes={'PATH': [clang, api.v8.depot_tools_path]}):
     try:
-      api.step('Bazel build', [bazel, 'build', '--verbose_failures', ':d8'])
+      api.step('Bazel build', [bazel, 'build', '--verbose_failures', ':icu/d8'])
     finally:
       api.step('Bazel shutdown', [bazel, 'shutdown'])
 
