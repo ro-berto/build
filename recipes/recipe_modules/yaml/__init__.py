be93b5e16c841adcc0e5844a804c87762498c888
Garrett Beaty <gbeaty@chromium.org>
2021-11-01 18:15:13 +0000

    Remove unused yaml recipe module.
    Change-Id: Ia6138c17e045fa4cbf6b4da194efee869ee42c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3254894
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>

--- recipes/recipe_modules/yaml/__init__.py
+++
@@ -1,9 +0,0 @@
-PYTHON_VERSION_COMPATIBILITY = "PY2"
-
-DEPS = [
-    'recipe_engine/json',
-    'recipe_engine/python',
-    'recipe_engine/file',
-    'recipe_engine/path',
-    'recipe_engine/step',
-]
