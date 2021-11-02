0ade4d67137503476252eb9925031367d6d8c62a
David Dinata <dinata@google.com>
2021-11-02 21:01:28 +0000

    Update tricium_metrics to python 3
    Bug: 1256442
    Change-Id: I1920dc44448935c2471cb1253a1501ad47f27d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3256002
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: David Dinata <dinata@google.com>

--- recipes/recipes/tricium_metrics.py
+++ recipes/recipes/tricium_metrics.py
@@ -6,7 +6,7 @@ from recipe_engine import post_process
 from google.protobuf import json_format
 from PB.tricium.data import Data
 
-PYTHON_VERSION_COMPATIBILITY = "PY2"
+PYTHON_VERSION_COMPATIBILITY = "PY2+3"
 
 DEPS = [
     'chromium',
