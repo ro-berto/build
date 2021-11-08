36791707ed15469875e12cdd57907c370be50d67
Garrett Beaty <gbeaty@chromium.org>
2021-11-08 21:37:33 +0000

    Remove unused crrev recipe module.
    Change-Id: I5e200279d07c9eff395f8977144fa97290e20ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3260116
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>

--- recipes/recipe_modules/crrev/__init__.py
+++
@@ -1,9 +0,0 @@
-PYTHON_VERSION_COMPATIBILITY = "PY2"
-
-DEPS = [
-    'build',
-    'recipe_engine/commit_position',
-    'recipe_engine/python',
-    'recipe_engine/json',
-    'recipe_engine/step',
-]
