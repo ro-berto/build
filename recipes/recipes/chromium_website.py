73300bb646631ebf23a06dcc52abe89749bd0d11
Dirk Pranke <dpranke@google.com>
2021-11-10 21:29:59 +0000

    Add deployment to chromium_website recipe.
    This CL modifies the chromium_website to deploy the built website
    after it is done building.
    
    For now, builds will be unconditionally deployed to the staging
    project website by either a try job or a CI job, and if the
    build is being done as part of a try job, it should post a
    comment back on the CL via Tricium.
    
    Subsequent CLs will change things so that the CI job deploys to
    the live site using the production project, and
    the try job deploys to preview URLs for the live site (also using
    the production project).
    
    Bug: 1267501
    Change-Id: I55f9dfc5aab7c43b6a02766553888f604f835bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3270733
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>

--- recipes/recipes/chromium_website.py
+++ recipes/recipes/chromium_website.py
@@ -14,13 +14,18 @@ from RECIPE_MODULES.depot_tools.gclient import CONFIG_CTX
 DEPS = [
     'depot_tools/bot_update',
     'depot_tools/gclient',
+    'depot_tools/tryserver',
     'recipe_engine/buildbucket',
     'recipe_engine/context',
     'recipe_engine/path',
     'recipe_engine/properties',
+    'recipe_engine/raw_io',
     'recipe_engine/runtime',
     'recipe_engine/step',
+    'recipe_engine/tricium',
 ]
+
+PYTHON_VERSION_COMPATIBILITY = "PY3"
 
 
 def RunSteps(api):
@@ -33,14 +38,43 @@ def RunSteps(api):
     npmw_path = api.m.path['checkout'].join('npmw')
     api.step('build', [npmw_path, 'build'])
 
+    # TODO(crbug.com/1268676): Change this to `'deploy:prod'` when ready.
+    # TODO(crbug.com/1267501): Change this to `'deploy:prod <channel-id>`
+    # when running as a tryjob.
+    out = api.step(
+        'deploy', [npmw_path, 'deploy:staging'],
+        stdout=api.raw_io.output(add_output_log=True)).stdout
+    out = out.decode('utf-8').strip()
+
+    # pylint: disable=line-too-long
+    msg = (
+        "Deployed site but don't know where.\n"
+        "Please [file a bug](https://bugs.chromium.org/p/chromium/issues/entry?template=Chromium.org+bug)\n"
+    )
+    # pylint: enable=line-too-long
+    url = None
+    for line in out.splitlines():
+      if 'Hosting URL' in line:
+        url = line.split()[-1]
+        msg = 'Deployed to %s' % url
+
+  if api.m.tryserver.is_tryserver:  # pragma: no cover
+    # TODO(crbug.com/1267501): Change to 'FirebaseHosting/Preview' when
+    # deploying to a preview site using the <channel-id>, above.
+    api.m.tricium.add_comment('FirebaseHosting/Deploy', msg, path='')
+    api.m.tricium.write_comments()
+
   return result_pb2.RawResult(
       status=common_pb.SUCCESS,
-      summary_markdown="Built chromium website",
+      summary_markdown=msg,
   )
 
 def GenTests(api):
   yield api.test(
-      'runs',
+      'ci',
+      api.step_data(
+          'deploy',
+          stdout=api.raw_io.output('[1mHosting URL:[22m example.com\n')),
       api.post_process(DropExpectation),
   )
 
