8eb8cd68de9dab9c44e77911dd6161b384577c41
Garrett Beaty <gbeaty@chromium.org>
2021-11-16 23:40:15 +0000

    Mark swarming/deterministic_build recipe as python3 compatible.
    Bug: 1256438
    Change-Id: I029d99f9f79d64c6fd3acb2550655f469c3bf091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3286670
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>

--- recipes/recipes/swarming/deterministic_build.py
+++ recipes/recipes/swarming/deterministic_build.py
@@ -14,7 +14,7 @@ from recipe_engine.recipe_api import Property
 from PB.go.chromium.org.luci.buildbucket.proto import common as common_pb
 from RECIPE_MODULES.build import chromium
 
-PYTHON_VERSION_COMPATIBILITY = "PY2"
+PYTHON_VERSION_COMPATIBILITY = "PY2+3"
 
 DEPS = [
     'builder_group',
