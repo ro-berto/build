847b9d78a3d90540ec1de339e4b17af40d3590f8
Jeremy Leconte <jleconte@google.com>
2021-11-22 08:34:01 +0000

    Upload WebRTC tests results to ResultDB.
    For a start only tests running with gtest_parallel will be uploaded.
    This is because gtest and gtest_parallel are not dumping the same json output format.
    
    Bug: b/197492097
    Change-Id: Id748cf6ae64e14ff87ad951e8d73a3348127c988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3291447
    Reviewed-by: Christoffer Jansson <jansson@chromium.org>
    Commit-Queue: Jeremy Leconte <jleconte@google.com>

--- recipes/recipe_modules/webrtc/steps.py
+++ recipes/recipe_modules/webrtc/steps.py
@@ -4,6 +4,7 @@
 
 from __future__ import absolute_import
 
+import attr
 import functools
 import json
 import os
@@ -12,6 +13,7 @@ import sys
 
 from recipe_engine import recipe_api
 from recipe_engine.engine_types import freeze
+from RECIPE_MODULES.build.chromium_tests.resultdb import ResultDB
 
 THIS_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(os.path.dirname(THIS_DIR)))
@@ -20,12 +22,11 @@ sys.path.append(os.path.join(os.path.dirname(THIS_DIR)))
 # adb path relative to out dir (e.g. out/Release)
 ADB_PATH = '../../third_party/android_sdk/public/platform-tools/adb'
 
-ANDROID_CIPD_PACKAGES = [
-    ("bin",
-     "infra/tools/luci/logdog/butler/${platform}",
-     "git_revision:ff387eadf445b24c935f1cf7d6ddd279f8a6b04c",
-    )
-]
+ANDROID_CIPD_PACKAGES = [(
+    "bin",
+    "infra/tools/luci/logdog/butler/${platform}",
+    "git_revision:ff387eadf445b24c935f1cf7d6ddd279f8a6b04c",
+)]
 
 
 def generate_tests(phase, bot, platform_name, build_out_dir, checkout_path,
@@ -81,9 +82,13 @@ def generate_tests(phase, bot, platform_name, build_out_dir, checkout_path,
       if platform_name == 'linux':
         tests.append(baremetal_test('isac_fix_test'))
 
-      tests.append(baremetal_test('webrtc_perf_tests',
-          args=['--force_fieldtrials=WebRTC-QuickPerfTest/Enabled/',
-                '--nologs']))
+      tests.append(
+          baremetal_test(
+              'webrtc_perf_tests',
+              args=[
+                  '--force_fieldtrials=WebRTC-QuickPerfTest/Enabled/',
+                  '--nologs'
+              ]))
 
   if test_suite == 'desktop_perf_swarming':
 
@@ -313,7 +318,15 @@ class WebRtcIsolatedGtest(object):
 
     self._task = self.create_task(api, task_input)
 
-    return api.chromium_swarming.trigger_task(self._task)
+    resultdb = None
+    for s in self._task.request[0].command:
+      if '--dump_json_test_results=' in s:
+        resultdb = attr.evolve(
+            ResultDB.create(),
+            result_format='json',
+            result_file=s[len('--dump_json_test_results='):],
+        )
+    return api.chromium_swarming.trigger_task(self._task, resultdb)
 
   @recipe_api.composite_step
   def run(self, api):
@@ -342,7 +355,6 @@ class WebRtcIsolatedGtest(object):
 
     self._apply_swarming_task_config(task, api)
     return task
-
 
   def _apply_swarming_task_config(self, task, api):
     """Applies shared configuration for swarming tasks.
@@ -375,8 +387,12 @@ class WebRtcIsolatedGtest(object):
 
     task.extra_args.extend(self._args)
 
+    # Add tags.
+    tags = {'test_suite': [self.name]}
+
     task.request = (
-        task.request.with_slice(0, task_slice).with_name(self.step_name))
+        task.request.with_slice(0, task_slice).with_name(
+            self.step_name).with_tags(tags))
     return task
 
 
