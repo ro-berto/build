7e6899478bef2a230cfdfc7d4473998bb0963b48
Lindsay Pasricha <lindsayw@google.com>
2021-12-02 17:49:27 +0000

    Make mac-rel mirror Mac11 Tests
    Bug:1254953
    Change-Id: I2c6f2194b2c8d2e684596c5615e21c0ec7cef77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3307537
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>

--- recipes/recipes/chromium_trybot.py
+++ recipes/recipes/chromium_trybot.py
@@ -203,7 +203,7 @@ def GenTests(api):
       swarm_hashes(extra_swarmed_tests=['telemetry_gpu_unittests']),
       api.chromium_tests.read_source_side_spec(
           'chromium.mac', {
-              'Mac10.15 Tests': {
+              'Mac11 Tests': {
                   'isolated_scripts': [{
                       'isolate_name': 'telemetry_gpu_unittests',
                       'name': 'telemetry_gpu_unittests',
