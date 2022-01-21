65b308fd3b763d63c15bef258da8ebbd3cdd0ff7
Garrett Beaty <gbeaty@chromium.org>
2022-01-21 21:51:18 +0000

    Update recipe tester for M98 and M99.
    Change-Id: Ieb509848cb7651f9f78641bdc692d0b998326d6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3405585
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>

--- recipes/recipes/led_recipes_tester.py
+++ recipes/recipes/led_recipes_tester.py
@@ -164,6 +164,18 @@ CHROMIUM_SRC_TEST_CLS = collections.OrderedDict([
         'fast':
             'https://chromium-review.googlesource.com/c/chromium/src/+/3271783',
     }),
+    ('luci.chromium-m98.try', {
+        'slow':
+            'https://chromium-review.googlesource.com/c/chromium/src/+/3408113',
+        'fast':
+            'https://chromium-review.googlesource.com/c/chromium/src/+/3408118',
+    }),
+    ('luci.chromium-m99.try', {
+        'slow':
+            'https://chromium-review.googlesource.com/c/chromium/src/+/3408116',
+        'fast':
+            'https://chromium-review.googlesource.com/c/chromium/src/+/3408121',
+    }),
 ])
 
 
