ead57ab3b8b271fffa4584c1c2baf10fe8c1e408
Michael Achenbach <machenbach@chromium.org>
2022-01-26 09:49:05 +0000

    [V8] Flip presubmit P2+3 compatibility
    Bug: 1256456
    Change-Id: Ib2c17d74c5c6d49684921d78e0c1c5ed709b4534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3416912
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Auto-Submit: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Tamer Tas <tmrts@google.com>
    Commit-Queue: Tamer Tas <tmrts@google.com>
    Reviewed-by: Liviu Rau <liviurau@google.com>

--- recipes/recipes/v8/presubmit.py
+++ recipes/recipes/v8/presubmit.py
@@ -8,7 +8,7 @@ Recipe for running presubmit in V8 CI.
 
 from recipe_engine.post_process import Filter
 
-PYTHON_VERSION_COMPATIBILITY = "PY2"
+PYTHON_VERSION_COMPATIBILITY = "PY2+3"
 
 DEPS = [
   'chromium',
