62d33d5d188f94d110f70937790925b6df830387
Ramy Medhat <abdelaal@google.com>
2022-02-03 18:42:31 +0000

    Update reclient test to 0.53
    Bug: b/169675226
    Change-Id: Ic08f45e0627498700d896c52d4cbf0f44a0a6152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3437246
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Garrett Beaty <gbeaty@google.com>
    Commit-Queue: Ramy Medhat <abdelaal@google.com>

--- recipes/recipe_modules/chromium/gclient_config.py
+++ recipes/recipe_modules/chromium/gclient_config.py
@@ -417,7 +417,7 @@ def reclient_staging(c):
 @CONFIG_CTX()
 def reclient_test(c):
   cv = c.solutions[0].custom_vars
-  cv['reclient_version'] = 're_client_version:0.52.0.c81afce-gomaip'
+  cv['reclient_version'] = 're_client_version:0.53.0.f790546-gomaip'
 
 
 # This configuration overrides the default reclient version
