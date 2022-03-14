63fa1b93123f4ca9d74ba4a83e849bdb5415ea6e
David Goldsmith <davidgoldsmith@google.com>
2022-03-14 17:52:22 +0000

    [reclient] Update Staging Version of Reclient.
    Bug: b/169675226
    Change-Id: Ibaaa4ce4616c5c54a40879818c64e95a76026425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3522441
    Auto-Submit: David Goldsmith <davidgoldsmith@google.com>
    Reviewed-by: Bozydar Szadkowski <bozydar@google.com>
    Commit-Queue: Bozydar Szadkowski <bozydar@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.56.0.8e249fc-gomaip'
+STAGING_VERSION = 're_client_version:0.57.0.4865132-gomaip'
 TEST_VERSION = 're_client_version:0.57.0.4865132-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
