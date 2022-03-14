d541d96ee67433d2ac5242de24f0be14b467a9ea
Garrett Beaty <gbeaty@chromium.org>
2022-03-14 22:39:02 +0000

    Switch reclient examples test cases to pass test data as arguments.
    The formatter plays nicer with passing arguments since if the last
    argument has a trailing comma, each argument will be put on its own line
    whereas with + the formatter will put as much of the expression on a
    single line as possible, making it harder to read.
    
    Change-Id: Id3bdb6ac03efcf5201366cd5ad7223416cc7cf05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3523526
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Stephanie Kim <kimstephanie@google.com>
    Commit-Queue: Stephanie Kim <kimstephanie@google.com>

--- recipes/recipe_modules/reclient/examples/full.py
+++ recipes/recipe_modules/reclient/examples/full.py
@@ -44,86 +44,121 @@ def MakeTestRBEStats(num_records=0, total_verified=None, total_mismatches=None):
 
 
 def GenTests(api):
-  yield (api.test('basic'))
-  yield (api.test('basic windows') + api.platform('win', 64))
-  yield (api.test('override instance') +
-         api.reclient.properties(instance='goma') +
-         api.post_process(post_process.DropExpectation))
-  yield (api.test('override_metrics_project') +
-         api.reclient.properties(metrics_project='goma'))
+  yield api.test('basic')
+
+  yield api.test(
+      'basic windows',
+      api.platform('win', 64),
+  )
+
+  yield api.test(
+      'override instance',
+      api.reclient.properties(instance='goma'),
+      api.post_process(post_process.DropExpectation),
+  )
+
+  yield api.test(
+      'override_metrics_project',
+      api.reclient.properties(metrics_project='goma'),
+  )
 
   def env_checker(check, steps):
     env = steps[_NINJA_STEP_NAME].env
     check(env['RBE_FOO'] == 'foo')
     check(env['RBE_BAR'] == 'bar')
 
-  yield (api.test('proper_rewrapper_flags') +
-         api.reclient.properties(rewrapper_env={
-             'RBE_FOO': 'foo',
-             'RBE_BAR': 'bar'
-         }) + api.post_check(env_checker))
-  yield (api.test('incorrect_rewrapper_flags') +
-         api.reclient.properties(rewrapper_env={
-             'MISSING_RBE_PREFIX': 'foo',
-         }) + api.expect_exception('MalformedREWrapperFlag') +
-         api.post_process(post_process.DropExpectation))
+  yield api.test(
+      'proper_rewrapper_flags',
+      api.reclient.properties(rewrapper_env={
+          'RBE_FOO': 'foo',
+          'RBE_BAR': 'bar'
+      }),
+      api.post_check(env_checker),
+  )
 
-  yield (api.test('profiler') +
-         api.reclient.properties(profiler_service='reclient') +
-         api.post_process(
-             post_process.Filter(
-                 'preprocess for reclient.start reproxy via bootstrap')))
+  yield api.test(
+      'incorrect_rewrapper_flags',
+      api.reclient.properties(rewrapper_env={
+          'MISSING_RBE_PREFIX': 'foo',
+      }),
+      api.expect_exception('MalformedREWrapperFlag'),
+      api.post_process(post_process.DropExpectation),
+  )
 
-  yield (api.test('trace') + api.reclient.properties(publish_trace=True) +
-         api.post_process(
-             post_process.Filter(
-                 'postprocess for reclient.upload reclient traces')))
+  yield api.test(
+      'profiler',
+      api.reclient.properties(profiler_service='reclient'),
+      api.post_process(
+          post_process.Filter(
+              'preprocess for reclient.start reproxy via bootstrap')),
+  )
 
-  yield (api.test('ensure_verified_succeed') +
-         api.reclient.properties(ensure_verified=True) + api.step_data(
-             'postprocess for reclient.load rbe_metrics.pb',
-             api.file.read_raw(
-                 content=MakeTestRBEStats(num_records=1, total_verified=1))) +
-         api.post_process(post_process.StepSuccess,
-                          'postprocess for reclient.verification') +
-         api.post_process(
-             post_process.Filter('postprocess for reclient.verification')))
+  yield api.test(
+      'trace',
+      api.reclient.properties(publish_trace=True),
+      api.post_process(
+          post_process.Filter(
+              'postprocess for reclient.upload reclient traces')),
+  )
 
-  yield (api.test('ensure_verified_no_records') +
-         api.reclient.properties(ensure_verified=True) + api.step_data(
-             'postprocess for reclient.load rbe_metrics.pb',
-             api.file.read_raw(content=MakeTestRBEStats(num_records=0))) +
-         api.post_process(post_process.StepSuccess,
-                          'postprocess for reclient.verification') +
-         api.post_process(
-             post_process.Filter('postprocess for reclient.verification')))
+  yield api.test(
+      'ensure_verified_succeed',
+      api.reclient.properties(ensure_verified=True),
+      api.step_data(
+          'postprocess for reclient.load rbe_metrics.pb',
+          api.file.read_raw(
+              content=MakeTestRBEStats(num_records=1, total_verified=1))),
+      api.post_process(post_process.StepSuccess,
+                       'postprocess for reclient.verification'),
+      api.post_process(
+          post_process.Filter('postprocess for reclient.verification')),
+  )
 
-  yield (api.test('ensure_verified_no_verified_field') +
-         api.reclient.properties(ensure_verified=True) + api.step_data(
-             'postprocess for reclient.load rbe_metrics.pb',
-             api.file.read_raw(content=MakeTestRBEStats(num_records=1))) +
-         api.post_process(post_process.StepException,
-                          'postprocess for reclient.verification') +
-         api.post_process(
-             post_process.Filter('postprocess for reclient.verification')))
+  yield api.test(
+      'ensure_verified_no_records',
+      api.reclient.properties(ensure_verified=True),
+      api.step_data('postprocess for reclient.load rbe_metrics.pb',
+                    api.file.read_raw(content=MakeTestRBEStats(num_records=0))),
+      api.post_process(post_process.StepSuccess,
+                       'postprocess for reclient.verification'),
+      api.post_process(
+          post_process.Filter('postprocess for reclient.verification')),
+  )
 
-  yield (api.test('ensure_verified_no_verification') +
-         api.reclient.properties(ensure_verified=True) + api.step_data(
-             'postprocess for reclient.load rbe_metrics.pb',
-             api.file.read_raw(
-                 content=MakeTestRBEStats(num_records=1, total_verified=0))) +
-         api.post_process(post_process.StepException,
-                          'postprocess for reclient.verification') +
-         api.post_process(
-             post_process.Filter('postprocess for reclient.verification')))
+  yield api.test(
+      'ensure_verified_no_verified_field',
+      api.reclient.properties(ensure_verified=True),
+      api.step_data('postprocess for reclient.load rbe_metrics.pb',
+                    api.file.read_raw(content=MakeTestRBEStats(num_records=1))),
+      api.post_process(post_process.StepException,
+                       'postprocess for reclient.verification'),
+      api.post_process(
+          post_process.Filter('postprocess for reclient.verification')),
+  )
 
-  yield (api.test('ensure_verified_mismatches') +
-         api.reclient.properties(ensure_verified=True) + api.step_data(
-             'postprocess for reclient.load rbe_metrics.pb',
-             api.file.read_raw(
-                 content=MakeTestRBEStats(
-                     num_records=1, total_verified=1, total_mismatches=1))) +
-         api.post_process(post_process.StepException,
-                          'postprocess for reclient.verification') +
-         api.post_process(
-             post_process.Filter('postprocess for reclient.verification')))
+  yield api.test(
+      'ensure_verified_no_verification',
+      api.reclient.properties(ensure_verified=True),
+      api.step_data(
+          'postprocess for reclient.load rbe_metrics.pb',
+          api.file.read_raw(
+              content=MakeTestRBEStats(num_records=1, total_verified=0))),
+      api.post_process(post_process.StepException,
+                       'postprocess for reclient.verification'),
+      api.post_process(
+          post_process.Filter('postprocess for reclient.verification')),
+  )
+
+  yield api.test(
+      'ensure_verified_mismatches',
+      api.reclient.properties(ensure_verified=True),
+      api.step_data(
+          'postprocess for reclient.load rbe_metrics.pb',
+          api.file.read_raw(
+              content=MakeTestRBEStats(
+                  num_records=1, total_verified=1, total_mismatches=1))),
+      api.post_process(post_process.StepException,
+                       'postprocess for reclient.verification'),
+      api.post_process(
+          post_process.Filter('postprocess for reclient.verification')),
+  )
