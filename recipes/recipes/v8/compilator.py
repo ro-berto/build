a3d8c3b31aaaf7fe05f1f5c6d910e715173059d9
Michael Achenbach <machenbach@chromium.org>
2022-03-16 12:09:34 +0000

    [V8] Add recipe skeletons for an orchestrator prototype
    Bug: 890222
    Change-Id: I7302cc8b33218eb16a78166342b8a6856d4591b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3528754
    Reviewed-by: Liviu Rau <liviurau@google.com>
    Reviewed-by: Alexander Schulze <alexschulze@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>

---
+++ recipes/recipes/v8/compilator.py
@@ -0,0 +1,20 @@
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Compilator prototype.
+"""
+
+
+PYTHON_VERSION_COMPATIBILITY = "PY3"
+
+DEPS = [
+]
+
+
+def RunSteps(api):
+  pass
+
+
+def GenTests(api):
+  yield api.test("basic")
