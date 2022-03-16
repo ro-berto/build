59dcd03bb551f902ba9042b5ca117ef851490efa
Michael Achenbach <machenbach@chromium.org>
2022-03-16 14:38:04 +0000

    [V8] Experimental data for compilator prototype
    This lets the compilator recipe emit fixed production data for testing.
    
    Bug: 890222
    Change-Id: Ib74d6d426fe01b933ae8561473d1771d0cdde452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3528759
    Reviewed-by: Alexander Schulze <alexschulze@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Auto-Submit: Michael Achenbach <machenbach@chromium.org>

--- recipes/recipes/v8/compilator.py
+++ recipes/recipes/v8/compilator.py
@@ -9,12 +9,44 @@
 PYTHON_VERSION_COMPATIBILITY = "PY3"
 
 DEPS = [
+  'recipe_engine/json',
+  'recipe_engine/step',
 ]
 
+# Temporary test data.
+swarm_hashes = {
+  "bot_default":
+    "800df08e7c70a1b55e262c29646b7df8c8a9a9704c9625190cb6d130899536df/319",
+}
+
+parent_test_spec = {
+  "swarming_dimensions": {
+    "cpu": "x86-64-avx2",
+    "os": "Ubuntu-18.04"
+  },
+  "swarming_task_attrs": {},
+  "tests": [
+    [
+      "v8testing", 4, None, "", [], {}, {}
+    ]
+  ]
+}
 
 def RunSteps(api):
-  pass
+  with api.step.nest('build'):
+    api.step('generate_build_files', cmd=None)
+    api.step('compile', cmd=None)
 
+  trigger_properties = {}
+  trigger_properties['swarm_hashes'] = swarm_hashes
+  trigger_properties['parent_test_spec'] = parent_test_spec
+
+  properties_step = api.step('swarming trigger properties', [])
+  properties_step.presentation.properties[
+      'swarming_trigger_properties'] = trigger_properties
+  properties_step.presentation.logs[
+      'swarming_trigger_properties'] = api.json.dumps(
+          trigger_properties, indent=2)
 
 def GenTests(api):
   yield api.test("basic")
