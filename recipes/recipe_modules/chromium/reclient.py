db526b667a7f4afe0d5cf8978ec6023121ddbe97
Ramy Medhat <abdelaal@google.com>
2022-03-24 21:07:56 +0000

    Update Chromium test to reclient 0.59.0
    Bug: b/169675226
    
    Change-Id: Iad3dfc0b4acd45dd862318319ce5cbc54110858e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3550500
    Reviewed-by: Bozydar Szadkowski <bozydar@google.com>
    Commit-Queue: Ramy Medhat <abdelaal@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.57.0.4865132-gomaip'
-TEST_VERSION = 're_client_version:0.57.0.4865132-gomaip'
+TEST_VERSION = 're_client_version:0.59.0.7914303-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
