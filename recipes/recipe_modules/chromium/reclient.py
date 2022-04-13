8300a926f31265981406f26bea951fa1ce0701c0
Ramy Medhat <abdelaal@google.com>
2022-04-13 18:44:23 +0000

    Release reclient 0.62 to test
    Change-Id: I0197e301f69f2f6c41a22ad0e49d8f853f17298f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3585669
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Ramy Medhat <abdelaal@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.61.0.947cef8-gomaip'
-TEST_VERSION = 're_client_version:0.61.0.947cef8-gomaip'
+TEST_VERSION = 're_client_version:0.62.0.0a58116-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
