e81552d69f4ec72f4312bd2d6e5728d9ced24a70
Ramy Medhat <abdelaal@google.com>
2022-04-19 15:08:36 +0000

    Update reclient staging to 0.62
    Change-Id: I3a13c20f985f52952d252f42ebbc660f177a6ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3593919
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Ramy Medhat <abdelaal@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.61.0.947cef8-gomaip'
+STAGING_VERSION = 're_client_version:0.62.0.0a58116-gomaip'
 TEST_VERSION = 're_client_version:0.62.0.0a58116-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
