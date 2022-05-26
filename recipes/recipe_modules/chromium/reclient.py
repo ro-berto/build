c9981942c59af60214d5a8fd175e926a900e6798
Bozydar Szadkowski <bozydar@google.com>
2022-05-26 18:21:07 +0000

    [reclient] Update Test Version of Reclient to 0.67.0
    Bug: b/169675226
    Change-Id: I5a2eecac7c16a8635aea9c41b2ad4ad7ab6af21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3670189
    Reviewed-by: Ben Segall <bentekkie@google.com>
    Commit-Queue: Bozydar Szadkowski <bozydar@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.64.0.d7886b3-gomaip'
-TEST_VERSION = 're_client_version:0.66.0.e4d450a-gomaip'
+TEST_VERSION = 're_client_version:0.67.0.d011fd9-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
