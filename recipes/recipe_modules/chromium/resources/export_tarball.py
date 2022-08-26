5f0c9264bd04346e4e39cd2cdbf59b34caa0874d
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2022-08-26 16:35:31 +0000

    export_tarball: Remove version check for M64 and earlier.
    M64 is several years old, so we can drop any version checks since this
    script only produces recent Chromium releases.
    
    Change-Id: Icc901961c314658db05dd3e549cbe23f2e41f900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3859383
    Auto-Submit: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>

--- recipes/recipe_modules/chromium/resources/export_tarball.py
+++ recipes/recipe_modules/chromium/resources/export_tarball.py
@@ -172,17 +172,15 @@ def main(argv):
                        cwd=options.src_dir) != 0:
       print 'Could not run build/util/lastchange.py to update DAWN_VERSION.'
       return 1
-  # The --revision-id-only option was introduced in M64; so we need to skip
-  # this call when building earlier milestones.
-  if version >= [64, 0, 0, 0]:
-    if subprocess.call(['python', 'build/util/lastchange.py',
-                        '-m', 'GPU_LISTS_VERSION',
-                        '--revision-id-only',
-                        '--header', 'gpu/config/gpu_lists_version.h'],
-                       cwd=options.src_dir) != 0:
-      print('Could not run build/util/lastchange.py to update '
-            'gpu_lists_version.h.')
-      return 1
+  if subprocess.call([
+      'python', 'build/util/lastchange.py', '-m', 'GPU_LISTS_VERSION',
+      '--revision-id-only', '--header', 'gpu/config/gpu_lists_version.h'
+  ],
+                     cwd=options.src_dir) != 0:
+    print(
+        'Could not run build/util/lastchange.py to update '
+        'gpu_lists_version.h.')
+    return 1
 
   output_fullname = args[0] + '.tar'
   output_basename = options.basename or os.path.basename(args[0])
