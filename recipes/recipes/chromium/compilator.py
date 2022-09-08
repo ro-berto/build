8d3750e2f5ffc9206c1b2cc4de6c3b443993c0d0
Zhaoyang Li <zhaoyangli@chromium.org>
2022-09-08 18:17:13 +0000

    Manual roll recipes.cfg
    Changelog:
    
    infra:
      https://crrev.com/bc5527b88e3a44f0c6684de1b56390457c613a43 Remove weetbix proto definitions in recipe (zhaoyangli@chromium.org)
    
    Change-Id: Idecbed457242c59b30039cb12d61f08aa990d535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3884095
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Garrett Beaty <gbeaty@google.com>

--- recipes/recipes/chromium/compilator.py
+++ recipes/recipes/chromium/compilator.py
@@ -17,7 +17,7 @@ from PB.go.chromium.org.luci.resultdb.proto.v1 \
     import common as resultdb_common
 from PB.go.chromium.org.luci.resultdb.proto.v1 \
     import test_result as test_result_pb2
-from PB.infra.appengine.weetbix.proto.v1 import test_history
+from PB.go.chromium.org.luci.analysis.proto.v1 import test_history
 
 DEPS = [
     'chromium',
