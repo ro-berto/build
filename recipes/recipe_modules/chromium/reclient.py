b4f7722a0fb3379c4f6949e88f8b8c816459c46a
Ben Segall <bentekkie@google.com>
2022-09-14 18:43:30 +0000

    [reclient] Update Test Version of Reclient.
    Bug: b/169675226
    Change-Id: Id962360ec6af3ece69ba0851b509779e03d0cce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3892372
    Commit-Queue: David Goldsmith <davidgoldsmith@google.com>
    Commit-Queue: Ben Segall <bentekkie@google.com>
    Reviewed-by: David Goldsmith <davidgoldsmith@google.com>
    Auto-Submit: Ben Segall <bentekkie@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.78.0.6f1e751-gomaip'
-TEST_VERSION = 're_client_version:0.78.0.6f1e751-gomaip'
+TEST_VERSION = 're_client_version:0.79.0.38ac02f-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
