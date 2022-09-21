84f3351a2c1510445579ee23837aa62b29e4bca6
Liviu Rau <liviurau@chromium.org>
2022-09-21 10:20:11 +0000

    [resultdb] Switch swarming tasks to vpython instead of python
    This solves the CQ failures in https://crrev.com/c/3905385 where `requests` module is made available in the .vpython3 file.
    
    Led test:
     - builder: https://chromium-swarm.appspot.com/task?id=5d78380aa29c8c10
     - tester: https://chromium-swarm.appspot.com/task?id=5d783ef97a476010
     - task: https://chromium-swarm.appspot.com/task?id=5d783fe08e205710
      - proof: `run_command(['/b/s/w/ir/.task_template_packages/vpython3']`
    
    Bug: v8:13316
    Change-Id: I1535163d6a75f97dce8df2e8d06d21b5b4daa729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3907595
    Reviewed-by: Alexander Schulze <alexschulze@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Auto-Submit: Liviu Rau <liviurau@chromium.org>

--- recipes/recipe_modules/v8/testing.py
+++ recipes/recipe_modules/v8/testing.py
@@ -237,7 +237,7 @@ class BaseTest(object):
     assert '/' in cas_digest
 
     if raw_cmd[0].endswith('.py'):
-      exe = 'python3' if self.api.v8.scripts_use_python3 else 'python'
+      exe = 'vpython3' if self.api.v8.scripts_use_python3 else 'vpython'
       raw_cmd = [exe, '-u'] + raw_cmd
 
     return self.api.chromium_swarming.task(
