ff1e37be7827ed32dcc8af4543461b324640bc97
Prakhar <pasthana@google.com>
2022-09-27 21:21:02 +0000

    Temporarily stop uploading unit test coverage reports to zoss
    Downstream pipeline is failing https://b.corp.google.com/issues/244612755#comment8
    
    Bug: 1305015
    Change-Id: Ib0919ffc7331e08177ce3debe1bb76ce75d321e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3919296
    Auto-Submit: Prakhar Asthana <pasthana@google.com>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>

--- recipes/recipe_modules/code_coverage/api.py
+++ recipes/recipe_modules/code_coverage/api.py
@@ -655,7 +655,9 @@ class CodeCoverageApi(recipe_api.RecipeApi):
         self.m.step('Generate Java coverage metadata', cmd, **kwargs)
 
         # Upload data to zoss to show it on code search
-        if self._export_coverage_to_zoss:
+        # TODO(b/244612755): Remove test type check once bug is fixed.
+        if (self._export_coverage_to_zoss and
+            self._current_processing_test_type == 'overall'):
           self.m.gsutil.upload(
               source=coverage_dir.join('coverage.xml'),
               bucket=constants.ZOSS_BUCKET_NAME,
@@ -1021,7 +1023,9 @@ class CodeCoverageApi(recipe_api.RecipeApi):
            len(self.m.profiles.profile_subdirs)), cmd)
     finally:
       # Upload data to zoss to show it on code search
-      if self._export_coverage_to_zoss:
+      # TODO(b/244612755): Remove test type check once bug is fixed.
+      if (self._export_coverage_to_zoss and
+          self._current_processing_test_type == 'overall'):
         self.m.gsutil.upload(
             source=self.metadata_dir.join('coverage.json'),
             bucket=constants.ZOSS_BUCKET_NAME,
