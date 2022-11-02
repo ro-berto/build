b86a21a55aba484763368cadece8d2d965bc83ea
Kousik Kumar <kousikk@google.com>
2022-11-02 01:11:30 +0000

    [reclient] Update staging version to 0.83.0.
    Bug: b/169675226
    Change-Id: I0a2b7258f8170b6a557c512b85e14f762322f18f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3996143
    Reviewed-by: Ramy Medhat <abdelaal@google.com>
    Commit-Queue: Kousik Kumar <kousikk@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.82.1.d528eb4-gomaip'
+STAGING_VERSION = 're_client_version:0.83.0.da55f4f-gomaip'
 TEST_VERSION = 're_client_version:0.83.0.da55f4f-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
