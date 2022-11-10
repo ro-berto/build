865f3693f39f2f8ed69845c9369a3b44c79843a4
Garrett Beaty <gbeaty@chromium.org>
2022-11-10 19:26:10 +0000

    Fix the builder config presubmit check.
    Unlike glob, the fnmatch function doesn't treat directory separators
    specially, so migration/*.json will match the filter json files within
    migration/scripts/filters. When one of the filter files is modified,
    this causes presubmit to fail because it tries to process the filter
    file as a grouping file which fails due to incompatible types. This CL
    changes it so that it makes sure that the json file is located directly
    within the migration directory.
    
    Additionally, only check that no new entries have been added to the
    groupings file if there were affected files that can actually add new
    builders. This will prevent changes to the migration filter files that
    add additional entries from being blocked.
    
    Change-Id: Id138da76f077d9ff70dd20e7504847b16fdb524a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4018698
    Reviewed-by: Stephanie Kim <kimstephanie@google.com>
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Commit-Queue: Stephanie Kim <kimstephanie@google.com>

--- recipes/recipe_modules/chromium_tests_builder_config/PRESUBMIT.py
+++ recipes/recipe_modules/chromium_tests_builder_config/PRESUBMIT.py
@@ -31,6 +31,20 @@ def CheckGroupings(input_api, output_api):
   if output:
     return output
 
+  # Changes to the migration files can't actually add new builders, but they
+  # might update the groupings files to add new entries. We don't want to
+  # prevent that, so only perform the check to prevent new entries if trybots.py
+  # or one of the files in builders is modified.
+  trybots_py = _path(input_api, 'trybots.py')
+  builders_dir = _path(input_api, f'builders{input_api.os_path.sep}')
+
+  def can_add_builders(af):
+    path = af.AbsoluteLocalPath()
+    return path == trybots_py or path.startswith(builders_dir)
+
+  if not input_api.AffectedFiles(file_filter=can_add_builders):
+    return []
+
   # The groupings files are up-to-date, make sure new builders aren't being
   # unnecessarily added
   def groupings_file_filter(af):
@@ -38,7 +52,8 @@ def CheckGroupings(input_api, output_api):
       return False
     path = input_api.os_path.relpath(af.AbsoluteLocalPath(),
                                      input_api.PresubmitLocalPath())
-    return input_api.fnmatch.fnmatch(path, 'migration/*.json')
+    return (input_api.os_path.dirname(path) == 'migration' and
+            input_api.os_path.splitext(path)[1] == '.json')
 
   output = []
   for af in input_api.AffectedFiles(file_filter=groupings_file_filter):
