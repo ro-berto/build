8fb6588747644746a9da6e54f0a5c924f6824b5d
Ben Segall <bentekkie@google.com>
2022-11-14 17:21:50 +0000

    [reclient] Update staging version to 0.86.0.
    Bug: b/169675226
    Change-Id: I971623494d783ba426bdbb6c58676fc0a0a3e4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4021952
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Michael Savigny <msavigny@google.com>
    Auto-Submit: Ben Segall <bentekkie@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.85.0.91db7be-gomaip'
+STAGING_VERSION = 're_client_version:0.86.0.25feac0-gomaip'
 TEST_VERSION = 're_client_version:0.86.0.25feac0-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
