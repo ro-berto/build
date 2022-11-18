330c1a60b0560c6231af359595841a72b19f6b65
Ben Joyce <bjoyce@google.com>
2022-11-18 22:40:04 +0000

    Remove android_blink_rel recipe after it's move src-side.
    This is moved src-side in https://chromium-review.googlesource.com/c/chromium/src/+/4038604
    
    Bug:1368221
    Change-Id: I7cb92e5b512242284d3c6ae4b8a97173cfcba214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/3999999
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Reviewed-by: Garrett Beaty <gbeaty@google.com>

--- recipes/recipe_modules/chromium_tests_builder_config/builders/__init__.py
+++ recipes/recipe_modules/chromium_tests_builder_config/builders/__init__.py
@@ -26,7 +26,6 @@ from . import client_devtools_frontend_integration
 from . import client_openscreen_chromium
 from . import client_v8_chromium
 from . import client_v8_fyi
-from . import tryserver_chromium_android
 from . import tryserver_chromium_linux
 from . import tryserver_devtools_frontend
 from . import tryserver_v8
@@ -97,8 +96,6 @@ BUILDERS = builder_db.BuilderDatabase.create({
         client_v8_chromium.SPEC,
     'client.v8.fyi':
         client_v8_fyi.SPEC,
-    'tryserver.chromium.android':
-        tryserver_chromium_android.SPEC,
     'tryserver.chromium.linux':
         tryserver_chromium_linux.SPEC,
     'tryserver.devtools-frontend':
