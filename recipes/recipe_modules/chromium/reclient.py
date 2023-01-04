c9f2c2443402b6e330034c572766b239f0b3c288
Kousik Kumar <kousikk@google.com>
2023-01-04 20:59:52 +0000

    [reclient] Update test version to 0.93.0.
    Bug: b/169675226
    Change-Id: Ida8e0e810552e35a69e3e2cf775393a536213c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4137050
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Michael Savigny <msavigny@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.91.1.20929bd-gomaip'
-TEST_VERSION = 're_client_version:0.91.1.20929bd-gomaip'
+TEST_VERSION = 're_client_version:0.93.0.9ed3cef-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
