9067b28de1e19740cc83d4c1327b06afe7fc8f40
Ben Segall <bentekkie@google.com>
2023-01-12 20:10:36 +0000

    [reclient] Update test version to 0.93.0.9ed3cef-gomaip.
    Bug: b/169675226
    Change-Id: I584385bef88a5c3846a1f6f1de4de42009d922c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4163299
    Commit-Queue: David Goldsmith <davidgoldsmith@google.com>
    Reviewed-by: David Goldsmith <davidgoldsmith@google.com>
    Commit-Queue: Ben Segall <bentekkie@google.com>
    Auto-Submit: Ben Segall <bentekkie@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.91.1.20929bd-gomaip'
+STAGING_VERSION = 're_client_version:0.93.0.9ed3cef-gomaip'
 TEST_VERSION = 're_client_version:0.93.0.9ed3cef-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
