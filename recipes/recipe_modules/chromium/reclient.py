ae2171f5f43c7f80d82b9b0beb4f73fdf77fbd21
andusyu <andusyu@google.com>
2023-02-03 21:19:05 +0000

    [reclient] Update test and staging version to 0.96.1.
    Bug: b/169675226
    Change-Id: I6b23d00092681a754363591f23cce1793831fd1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4220526
    Reviewed-by: David Goldsmith <davidgoldsmith@google.com>
    Commit-Queue: Andus Yu <andusyu@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.93.0.9ed3cef-gomaip'
-TEST_VERSION = 're_client_version:0.96.0.4c47197-gomaip'
+STAGING_VERSION = 're_client_version:0.96.1.d76aa2e-gomaip'
+TEST_VERSION = 're_client_version:0.96.1.d76aa2e-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
