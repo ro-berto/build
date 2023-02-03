cd0fbc6268e71f3ff6df7b941b61c5e2b2e0e3e5
Bozydar Szadkowski <bozydar@google.com>
2023-02-03 15:51:38 +0000

    [reclient] Fix health check on Windows
    Reclient's health check is looking for logs named
    [tool_name].[LEVEL] (e.g. rewrapper.FATAL). However,
    on Windows the same logs have an executable suffix
    added to them (e.g. rewrapper.exe.FATAL).
    Due to this, health check logic was not correctly
    checking the status of the builds on Windows.
    Thic CL addresses the issue.
    
    Bug: 1411882
    Change-Id: I1f810c546c28c61975556f53e04d58bdfaba3871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4214671
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Bozydar Szadkowski <bozydar@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Bozydar Szadkowski <bozydar@google.com>

--- recipes/recipe_modules/reclient/resources/perform_health_check.py
+++ recipes/recipe_modules/reclient/resources/perform_health_check.py
@@ -12,7 +12,14 @@ from os import path
 FAILURE_STATUS = "1"
 
 
-def check_for_reclient_fatals(log_dir):
+def _is_win():
+  """
+  Returns true if the script runs on Windows platform
+  """
+  return sys.platform.startswith(('win', 'cygwin'))
+
+
+def check_for_reclient_fatals(log_dir, exe_suffix):
   """Performs reclient health check by determining
   whether any FATAL log entries were produced
   during compilation
@@ -21,19 +28,19 @@ def check_for_reclient_fatals(log_dir):
     Exception if FATAL log file is found in the log_dir
   """
   for tool_name in ['bootstrap', 'reproxy', 'rewrapper']:
-    if path.exists(path.join(log_dir, tool_name + '.FATAL')):
+    if path.exists(path.join(log_dir, tool_name + exe_suffix + '.FATAL')):
       raise Exception("Found {} FATAL log entries".format(tool_name))
 
   print("no reclient's FATAL log entries found in %s" % log_dir)
 
 
-def check_for_ip_timeouts(log_dir, build_exit_status):
+def check_for_ip_timeouts(log_dir, exe_suffix, build_exit_status):
   if build_exit_status != FAILURE_STATUS:
     print("build_exit_status({}) != {}, skipping IP timeouts check".format(
         build_exit_status, FAILURE_STATUS))
     return
 
-  full_path = path.join(log_dir, 'reproxy.ERROR')
+  full_path = path.join(log_dir, 'reproxy' + exe_suffix + '.ERROR')
   if not path.exists(full_path):
     print("no reproxy errors found")
     return
@@ -58,15 +65,16 @@ def main():
 
   args = parser.parse_args()
   log_dir = args.reclient_log_dir
+  exe_suffix = '.exe' if _is_win() else ''
 
   try:
-    check_for_reclient_fatals(log_dir)
+    check_for_reclient_fatals(log_dir, exe_suffix)
   except Exception as err:
     print("There was a FATAL error during reclient execution: {}".format(err))
     return 1
 
   try:
-    check_for_ip_timeouts(log_dir, args.build_exit_status)
+    check_for_ip_timeouts(log_dir, exe_suffix, args.build_exit_status)
   except Exception as err:
     print(err)
     return 1
