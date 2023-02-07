d9571c0faf45a50a47761652031634a8ce9a2391
Kousik Kumar <kousikk@google.com>
2023-02-07 16:21:54 +0000

    [reclient] Update test version to 0.96.2
    This is to test fix for ReadcommandOutputByPOpen
    
    Bug: b/169675226
    Change-Id: I13885302dcc12b0d7babc0bc53f1b94692ed4bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4226493
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Ramy Medhat <abdelaal@google.com>
    Auto-Submit: Kousik Kumar <kousikk@google.com>
    Reviewed-by: Andus Yu <andusyu@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -4,5 +4,5 @@
 """Reclient-related constants."""
 
 STAGING_VERSION = 're_client_version:0.96.1.d76aa2e-gomaip'
-TEST_VERSION = 're_client_version:0.96.1.d76aa2e-gomaip'
+TEST_VERSION = 're_client_version:0.96.2.d36a87c-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
