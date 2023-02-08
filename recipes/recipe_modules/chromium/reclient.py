835b90427b07821f166b063524d841a13463ae95
Bozydar Szadkowski <bozydar@google.com>
2023-02-08 20:40:56 +0000

    [reclient] Update staging version to 0.96.2.
    Bug: b/169675226
    Change-Id: I63ca18d0b47ea23b1ec5196d84fbfc0abe109f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/build/+/4233362
    Reviewed-by: Kousik Kumar <kousikk@google.com>
    Commit-Queue: Bozydar Szadkowski <bozydar@google.com>

--- recipes/recipe_modules/chromium/reclient.py
+++ recipes/recipe_modules/chromium/reclient.py
@@ -3,6 +3,6 @@
 # found in the LICENSE file.
 """Reclient-related constants."""
 
-STAGING_VERSION = 're_client_version:0.96.1.d76aa2e-gomaip'
+STAGING_VERSION = 're_client_version:0.96.2.d36a87c-gomaip'
 TEST_VERSION = 're_client_version:0.96.2.d36a87c-gomaip'
 CLANG_SCAN_DEPS_VERSION = 're_client_version:0.41.4.3f0d8bb'
